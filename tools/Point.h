f579f40cbffe1bf4c80342a4a17df138b169cfc4
Charles Ofria <charles.ofria@gmail.com>
2014-10-22 10:59:33 -0400

    Cleanup of Point and new functionality

--- tools/Point.h
+++ tools/Point.h
@@ -25,17 +25,24 @@ namespace emp {
     inline double GetX() const { return x; }
     inline double GetY() const { return y; }
 
-    Point & Set(double _x, double _y) { x=_x; y=_y; return *this; }
-    Point & Translate(double shift_x, double shift_y) { x += shift_x; y += shift_y; return *this; }
-    Point & TranslateX(double shift) { x += shift; return *this; }
-    Point & TranslateY(double shift) { y += shift; return *this; }
-
     double Magnitude() const { return sqrt( x*x + y*y ); }
+
+    // Determine a new point, but don't change this one
+    Point GetMidpoint(const Point & p2) const { return Point((x+p2.x)/2, (y+p2.y)/2); }
+    Point & GetRot90() const { return Point(y, -x); }
+    Point & GetRot180() const { return Point(-x, -y); }
+    Point & GetRot270() const { return Point(-y, x); }
 
     Point operator+(const Point & _in) const { return Point(x + _in.x , y + _in.y); }
     Point operator-(const Point & _in) const { return Point(x - _in.x , y - _in.y); }
     Point operator*(double mult) const { return Point(x * mult, y * mult); }
     Point operator/(double div) const { return Point(x / div, y / div); }
+
+    // Modify this point.
+    Point & Set(double _x, double _y) { x=_x; y=_y; return *this; }
+    Point & Translate(double shift_x, double shift_y) { x += shift_x; y += shift_y; return *this; }
+    Point & TranslateX(double shift) { x += shift; return *this; }
+    Point & TranslateY(double shift) { y += shift; return *this; }
 
     Point & operator+=(const Point & _in) { x += _in.x; y += _in.y; return *this; }
     Point & operator-=(const Point & _in) { x -= _in.x; y -= _in.y; return *this; }
@@ -48,7 +55,7 @@ namespace emp {
 
     // Square-roots are slow to calculate; if we're just doing comparisons, square-distance
     // is usualy going to be sufficient.
-    double SquareDistance(const Point & _in) {
+    double SquareDistance(const Point & _in) const {
       const double x_dist = x - _in.x;
       const double y_dist = y - _in.y;
       return x_dist*x_dist + y_dist*y_dist;
