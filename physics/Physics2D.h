c27c1c7a62056ea1a567a840c36c2d8e925fda99
Charles Ofria <charles.ofria@gmail.com>
2014-10-25 15:24:05 -0400

    Built simple Sector2D and Physics2D

--- physics/Physics2D.h
+++ physics/Physics2D.h
@@ -12,7 +12,7 @@
 
 
 #include <vector>
-#include <set>
+#include <unordered_set>
 
 #include "../tools/Circle.h"
 
@@ -26,8 +26,8 @@ namespace emp {
     BODY_INFO * info;
     Sector2D<CircleBody2D<BODY_INFO>, BODY_INFO> * sector;  // What sector is this body in?
   public:
-    Body2D(const Circle & _p, BODY_INFO * _i) : perimeter(_p), info(_i) { ; }
-    ~Body2D() { ; }
+    CircleBody2D(const Circle & _p, BODY_INFO * _i) : perimeter(_p), info(_i) { ; }
+    ~CircleBody2D() { ; }
 
     const Circle & GetPerimeter() const { return perimeter; }
     const Point & GetAnchor() const { return perimeter.GetCenter(); }
@@ -35,17 +35,45 @@ namespace emp {
     BODY_INFO * GetInfo() { return info; }
     Sector2D<CircleBody2D<BODY_INFO>, BODY_INFO> * GetSector() { return sector; }
 
-    auto & MoveTo(const cPoint & new_pos) { perimeter.SetCenter(new_pos); return *this; }
+    CircleBody2D<BODY_INFO> & MoveTo(const Point & new_pos) { perimeter.SetCenter(new_pos); return *this; }
   };
 
   template <typename BODY_TYPE, typename BODY_INFO> class Sector2D {
   private:
+    std::unordered_set<BODY_TYPE> body_set;
+
   public:
+    Sector2D() { ; }
+    ~Sector2D() { ; }
+
+    bool HasBody(BODY_TYPE * test_body) const { return body_set.count(test_body); }
+
+    Sector2D<BODY_TYPE, BODY_INFO> & AddBody(BODY_TYPE * in_body) {
+      body_set.insert(in_body);
+      return *this;
+    }
+    Sector2D<BODY_TYPE, BODY_INFO> & RemoveBody(BODY_TYPE * out_body) {
+      body_set.erase(out_body);
+      return *this;
+    }
+
+    const std::unordered_set<BODY_TYPE> & GetBodySet() const { return body_set; }
   };
 
-  class Physics2D {
+  template <typename BODY_TYPE, typename BODY_INFO> class Physics2D {
   private:
+    const int sector_cols;  // Number of sectors in the x-dimension.
+    const int sector_rows;  // Number of sectors in the y-dimension.
+    const int num_sectors;
+    std::vector<Sector2D<BODY_TYPE, BODY_INFO> > sector_matrix; // Flattend matrix of sectors;
+
   public:
+    Physics2D() 
+      : sector_cols(100), sector_rows(100)
+      , num_sectors(sector_cols * sector_rows)
+      , sector_matrix(num_sectors)
+    { ; }
+
   };
 
 };
