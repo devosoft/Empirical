129509ea8ca00e7e8142c74e6eed713577b8ddf7
Charles Ofria <charles.ofria@gmail.com>
2014-11-02 09:13:03 -0500

    Added macro to help time functions

---
+++ tools/functions.h
@@ -0,0 +1,22 @@
+#ifndef EMP_FUNCTIONS_H
+#define EMP_FUNCTIONS_H
+
+#include <ctime>
+
+#define EMP_FUNCTION_TIMER(TEST_FUN) {                                  \
+    std::clock_t emp_start_time = std::clock();                         \
+    auto emp_result = TEST_FUN;                                         \
+    std::clock_t emp_tot_time = std::clock() - emp_start_time;          \
+    std::cout << "Time: "                                               \
+              << 1000.0 * ((double) emp_tot_time) / (double) CLOCKS_PER_SEC      \
+              << " ms" << std::endl;                                    \
+    std::cout << "Result: " << emp_result << std::endl;                 \
+  }
+
+namespace emp {
+
+  // Generally useful functions here...  
+
+};
+
+#endif
