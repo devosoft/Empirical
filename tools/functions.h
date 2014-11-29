bc0359a47be4cc81af65e4e23942394b71bd10c5
Charles Ofria <charles.ofria@gmail.com>
2014-11-29 09:49:02 -0500

    Setup some generally useful functions

--- tools/functions.h
+++ tools/functions.h
@@ -17,6 +17,32 @@
 
 namespace emp {
 
+  // Toggle an input bool.
+  bool toggle(bool & in_bool) { return (in_bool = !in_bool); }
+
+  // % is actually remainder; this is a proper modulus command that handles negative #'s correctly.
+  int mod(int in_val, int mod_val) { return (in_val < 0) ? (in_val % mod_val + in_val) : (in_val % mod_val); }
+
+  // A fast (O(log p)) integer-power command.
+  int Pow(int base, int p) {
+    if (p == 0) return 1;
+    if (p < 0) return 0;
+    
+    int r = 1;
+    while (true) {
+      if (p & 1) r *= base;
+      if (!(p >>= 1)) return r;
+      base *= base;
+    }
+  }
+
+  // Swap the value of two variables.
+  template <typename TYPE> void swap(TYPE & in1, TYPE & in2) { TYPE tmp = in1; in1 = in2; in2 = tmp; }
+
+  // Run both min and max on a value to put it into a desired range.
+  template <typename TYPE> TYPE to_range(const TYPE & value, const TYPE & in_min, const TYPE & in_max) {
+    return (value < in_min) ? in_min : ((value > in_max) ? in_max : value);
+  }
 };
 
 #endif
