1b65cccce40999622f7e289d59bf7d35997fe35e
Charles Ofria <charles.ofria@gmail.com>
2014-12-04 10:07:58 -0500

    Remove sector info from bodies; remove collision claculations

--- geometry/Body2D.h
+++ geometry/Body2D.h
@@ -6,7 +6,6 @@
 //  This file defines templated classes to represent bodies that exist on a 2D surface.
 //  Each class should be able to:
 //   * Maintain a pointer to information about the full organism associated with this body.
-//   * Maintain a pointer to which sector this body is currently located in.
 //   * provide a circular perimeter of the body (for phase1 of collision detection)
 //   * Provide body an anchor point and center point of the body (typically the same)
 //
@@ -20,15 +19,12 @@
 
 namespace emp {
 
-  template <typename BODY_TYPE, typename BODY_INFO, typename BASE_TYPE> class Sector2D;
-
   template <typename BODY_INFO, typename BASE_TYPE> class CircleBody2D {
   private:
     Circle<BASE_TYPE> perimeter;     // Includes position and size.
-    BASE_TYPE target_radius;         // For growing/shrinking
     Angle orientation;               // Which way is body facing?
+    BASE_TYPE target_radius;         // For growing/shrinking
     BODY_INFO * info;                // External information about individual
-    Sector2D<CircleBody2D<BODY_INFO, BASE_TYPE>, BODY_INFO, BASE_TYPE> * sector;  // Track location
     Point<BASE_TYPE> velocity;       // Speed and direction of movement
     BASE_TYPE mass;                  // "Weight" of this object
     int color_id;                    // Which color should this body appear?
@@ -44,10 +40,9 @@ namespace emp {
     const Point<BASE_TYPE> & GetAnchor() const { return perimeter.GetCenter(); }
     const Point<BASE_TYPE> & GetCenter() const { return perimeter.GetCenter(); }
     BASE_TYPE GetRadius() const { return perimeter.GetRadius(); }
-    BASE_TYPE GetTargetRadius() const { return target_radius; }
     const Angle & GetOrientation() const { return orientation; }
+    BASE_TYPE GetTargetRadius() const { return target_radius; }
     BODY_INFO * GetInfo() { return info; }
-    Sector2D<CircleBody2D<BODY_INFO, BASE_TYPE>, BODY_INFO, BASE_TYPE> * GetSector() { return sector; }
     const Point<BASE_TYPE> & GetVelocity() const { return velocity; }
     BASE_TYPE GetMass() const { return mass; }
     int GetColorID() const { return color_id; }
@@ -63,14 +58,14 @@ namespace emp {
     CircleBody2D<BODY_INFO, BASE_TYPE> &
     SetTargetRadius(BASE_TYPE _target) { target_radius = _target; return *this; }
     CircleBody2D<BODY_INFO, BASE_TYPE> &
-    SetSector(Sector2D<CircleBody2D<BODY_INFO, BASE_TYPE>, BODY_INFO, BASE_TYPE> * new_sector) {
-      sector = new_sector;
-      return *this;
-    }
-    CircleBody2D<BODY_INFO, BASE_TYPE> &
     SetVelocity(BASE_TYPE _x, BASE_TYPE _y) { velocity.Set(_x, _y); return *this; }
     CircleBody2D<BODY_INFO, BASE_TYPE> &
+    SetVelocity(const Point<BASE_TYPE> & in_vel) { velocity = in_vel; return *this; }
+    CircleBody2D<BODY_INFO, BASE_TYPE> &
     SetColorID(int in_id) { color_id = in_id; return *this; }
+
+    CircleBody2D<BODY_INFO, BASE_TYPE> &
+    AddShift(const Point<BASE_TYPE> & inc_val) { shift += inc_val; return *this; }
 
     // If a body is not at its target radius, grow it or shrink it, as needed.
     CircleBody2D<BODY_INFO, BASE_TYPE> & BodyUpdate(BASE_TYPE grow_factor=1) {
@@ -121,69 +116,6 @@ namespace emp {
       return *this;
     }
     
-    bool CollisionTest(CircleBody2D<BODY_INFO, BASE_TYPE> & object2)
-    {
-      const Point<BASE_TYPE> dist = GetCenter() - object2.GetCenter();
-      const BASE_TYPE sq_pair_dist = dist.SquareMagnitude();
-      const BASE_TYPE radius_sum = GetRadius() + object2.GetRadius();
-      const BASE_TYPE sq_min_dist = radius_sum * radius_sum;
-
-      // std::cerr << "Object 1: " << GetCenter() << " ; radius=" << GetRadius() << std::endl;
-      // std::cerr << "Object 2: " << object2.GetCenter() << " ; radius=" << object2.GetRadius() << std::endl;
-      // std::cerr << "dist: " << dist << std::endl;
-
-  
-      // If there was no collision, return.
-      if (sq_pair_dist >= sq_min_dist) { return false; }
-
-      // @CAO If objects can phase or explode, identify that here.
-      
-      // Re-adjust position to remove overlap.
-      const double true_dist = sqrt(sq_pair_dist);
-      const double overlap_frac = ((double) radius_sum) / true_dist - 1.0;
-      const Point<BASE_TYPE> cur_shift = dist * (overlap_frac / 2.0);
-      shift += cur_shift;
-      object2.shift -= cur_shift;
-
-      // @CAO if we have inelastic collisions, we just take the weighted average of velocites
-      // and let the move together.
-      
-
-      // Assume elastic: Re-adjust velocity to reflect bounce.
-  
-      double x1, y1, x2, y2;
-  
-      if (dist.GetX() == 0) {
-        x1 = velocity.GetX();          y1 = object2.velocity.GetY();
-        x2 = object2.velocity.GetX();  y2 = velocity.GetY();
-
-        velocity = Point<BASE_TYPE>(x1, y1);
-        object2.velocity = Point<BASE_TYPE>(x2, y2);
-      }
-      else if (dist.GetY() == 0) {
-        x1 = object2.velocity.GetX();  y1 = velocity.GetY();
-        x2 = velocity.GetX();          y2 = object2.velocity.GetY();
-
-        velocity = Point<BASE_TYPE>(x1, y1);
-        object2.velocity = Point<BASE_TYPE>(x2, y2);
-      }
-      else {
-        const Point<BASE_TYPE> rel_velocity(object2.velocity - velocity);
-        double normal_a = dist.GetY() / dist.GetX();
-        x1 = ( rel_velocity.GetX() + normal_a * rel_velocity.GetY() )
-          / ( normal_a * normal_a + 1 );
-        y1 = normal_a * x1;
-        x2 = rel_velocity.GetX() - x1;
-        y2 = - (1 / normal_a) * x2;
-
-        object2.velocity = velocity + Point<BASE_TYPE>(x2, y2);
-        velocity = velocity + Point<BASE_TYPE>(x1, y1);
-      }
-  
-
-      return true;
-    }
-
     // After all collisions are accounted for, we should shift this object into its final place.
     void FinalizePosition()
     {
