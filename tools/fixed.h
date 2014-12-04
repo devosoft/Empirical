726b5353f1225aeaa98eecffae6d943d860c6c0e
Charles Ofria <charles.ofria@gmail.com>
2014-12-03 22:47:34 -0500

    More improvements to fixed-point values.

--- tools/fixed.h
+++ tools/fixed.h
@@ -20,10 +20,9 @@ namespace emp {
   private:
     int value;
     static const int int_bits = 31 - FRAC_BITS;
-    static const int frac_mask = (1 << FRAC_BITS) - 1;
 
     // Private constructor to allow first direct setting of internal value
-    fixed(int in_value, bool test_bool) : value(in_value) { (void) test_bool; }
+    fixed(int in_value, bool) : value(in_value) { ; }
   public:
     fixed() : value(0) { ; }
     fixed(const fixed & in_value) : value (in_value.value) { ; }
@@ -31,33 +30,65 @@ namespace emp {
     fixed(double in_value) : value(in_value * (1 << FRAC_BITS)) { ; }
     ~fixed() { ; }
 
+    int bits() const { return value; }
+    int AsInt() const { return value >> FRAC_BITS; }
+    double AsDouble() const { return ((double) value) / ((double) (1 << FRAC_BITS)); }
+
     fixed & operator=(const fixed & other) { value = other.value; return *this; }
     fixed & operator=(int other) { value = other << FRAC_BITS; return *this; }
     fixed & operator=(double other) { value = other * (1 << FRAC_BITS); return *this; }
 
-    int AsInt() const { return value >> FRAC_BITS; }
-    double AsDouble() const { return ((double) value) / ((double) (1 << FRAC_BITS)); }
+    fixed & operator++() { value += (1 << FRAC_BITS); }
+    fixed operator++(int) { int old_val = value; operator++(); return fixed(old_val); }
+    fixed & operator--() { value -= (1 << FRAC_BITS); }
+    fixed operator--(int) { int old_val = value; operator--(); return fixed(old_val); }
 
-    fixed operator+(const fixed & other) const { return fixed(value + other.value, true); }
-    fixed operator-(const fixed & other) const { return fixed(value - other.value, true); }
-    fixed operator*(const fixed & other) const {
+    friend fixed<FRAC_BITS> operator+(const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs) {
+      return fixed<FRAC_BITS>(lhs.bits() + rhs.bits(), true);
+    }
+    friend fixed<FRAC_BITS> operator-(const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs) {
+      return fixed<FRAC_BITS>(lhs.bits() - rhs.bits(), true);
+    }
+    friend fixed<FRAC_BITS> operator*(const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs) {
       // Take advantage of (a+b)*(c+d) = ac+ad+bc+bd.  Since bd is too low precision: b*c + a*(c+d)
-      const int new_value = (value & frac_mask) * (other.value >> FRAC_BITS)
-        + (value >> FRAC_BITS) * other.value
-        + ((value & frac_mask) * (other.value & frac_mask) >> FRAC_BITS);
-      return fixed(new_value, true);
+      const int frac_mask = (1 << FRAC_BITS) - 1;
+      const int new_value = (lhs.bits() & frac_mask) * (rhs.bits() >> FRAC_BITS)
+        + (lhs.bits() >> FRAC_BITS) * rhs.bits()
+        + ((lhs.bits() & frac_mask) * (rhs.bits() & frac_mask) >> FRAC_BITS);
+      return fixed<FRAC_BITS>(new_value, true);
     }
-    fixed operator/(const fixed & other) const {
+    friend fixed<FRAC_BITS> operator/(const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs) {
       // @CAO can we take advantage of (a+b)/c = a/c + b/c
-      const int new_value = (((long) value) << FRAC_BITS) / other.value;
-      return fixed(new_value, true);
+      const int new_value = (((long) lhs.bits()) << FRAC_BITS) / rhs.bits();
+      return fixed<FRAC_BITS>(new_value, true);
     }
+    
+    friend bool operator==(const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs)
+    { return lhs.bits() == rhs.bits(); }
+    friend bool operator!=(const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs)
+    { return lhs.bits() != rhs.bits(); }
+    friend bool operator< (const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs)
+    { return lhs.bits() <  rhs.bits(); }
+    friend bool operator<=(const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs)
+    { return lhs.bits() <= rhs.bits(); }
+    friend bool operator> (const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs)
+    { return lhs.bits() >  rhs.bits(); }
+    friend bool operator>=(const fixed<FRAC_BITS> & lhs, const fixed<FRAC_BITS> & rhs)
+    { return lhs.bits() >= rhs.bits(); }
+  
   };
+
   
-  
-  // Overload ostream to work with fixed.
+  // Overload istream and ostream to work with fixed.
   template <int FRAC_BITS> std::ostream & operator<<(std::ostream & os, const fixed<FRAC_BITS> & input) { 
     return os << input.AsDouble();
+  }
+
+  template <int FRAC_BITS> std::istream & operator>>(std::istream & is, fixed<FRAC_BITS> & input) { 
+    double tmp_val;
+    is >> tmp_val;
+    input = tmp_val;
+    return is;
   }
   
 };
