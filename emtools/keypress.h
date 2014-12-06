f024dddf1e81f39514756543cd9b7d8ace8e94ea
Charles Ofria <charles.ofria@gmail.com>
2014-12-06 09:36:34 -0500

    Added initial keypress object (not working yet)

---
+++ emtools/keypress.h
@@ -0,0 +1,80 @@
+#ifndef EMP_KEYPRESS_H
+#define EMP_KEYPRESS_H
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  An tracker for keypresses in HTML5 pages.
+//
+
+#include <functional>
+// #include <map>
+#include "../tools/callbacks.h"
+
+namespace emp {
+
+  using namespace std::placeholders;
+
+  class KeypressManager {
+  private:
+    //std::map<int, std::function<bool(const EventInfo &)> > fun_map;
+    std::function<bool(const EventInfo &)> fun_cb;
+    int next_order;  // Ordering to use if not specified (always last)
+
+    void DoCallback(const EventInfo & evt_info) {
+      fun_cb(evt_info);
+      // bool handled = false;
+      // for (auto fun_entry : fun_map) {
+      //   if (fun_entry.second(evt_info) == true) {
+      //     handled = true;
+      //     break;
+      //   }
+      // }
+
+      // // If an event has been handled, don't pass it on!
+      // if (handled) { ; }
+    };
+
+
+  public:
+    KeypressManager() : next_order(0) {
+      auto keypress_callback =
+        new MethodCallback_Event<KeypressManager>(this, &KeypressManager::DoCallback);
+
+      EM_ASM_ARGS({
+          document.addEventListener('keydown', function(evt) {
+              var ptr = Module._malloc(32); // 8 ints @ 4 bytes each...
+              setValue(ptr,    evt.layerX,   'i32');
+              setValue(ptr+4,  evt.layerY,   'i32');
+              setValue(ptr+8,  evt.button,   'i32');
+              setValue(ptr+12, evt.keyCode,  'i32');
+              setValue(ptr+16, evt.altKey,   'i32');
+              setValue(ptr+20, evt.ctrlKey,  'i32');
+              setValue(ptr+24, evt.metaKey,  'i32');
+              setValue(ptr+28, evt.shiftKey, 'i32');
+
+              empJSDoCallback($0, ptr);
+              Module._free(ptr);
+              evt.preventDefault();
+            }, false);
+
+        }, (int) keypress_callback);
+    }
+    ~KeypressManager() {
+      // @CAO Technically we should make sure to remove the event listener at this point.
+      // This would require us to keep track of the function that it is calling so that we can
+      // pass it back in to trigger the removal.
+    }
+
+    template <class T>
+    void AddKeydownCallback(T & obj, bool (T::*_method_ptr)(const EventInfo &), int order=-1) {
+      if (order == -1) order = next_order;
+      if (order >= next_order) next_order = order+1;
+
+      // fun_map[order] = std::bind( std::mem_fn(_method_ptr), obj, _1);
+      fun_cb = std::bind( std::mem_fn(_method_ptr), obj, _1);
+    }
+  };
+
+};
+
+#endif
