8476e0c4688025ec093b807e6511f32f26647d05
Charles Ofria <charles.ofria@gmail.com>
2014-12-26 16:23:16 -0500

    Dynamically load nops based on CPU type.

--- hardware/load_inst_lib.h
+++ hardware/load_inst_lib.h
@@ -14,21 +14,23 @@ using namespace std::placeholders;
 
 namespace emp {
 
-  void Load4StackDefault(InstLib<HardwareCPU<>, Instruction> & inst_lib) {
-    inst_lib.AddInst("Nop-A", std::bind(&HardwareCPU<>::Inst_Nop, _1), 1, 0);
-    inst_lib.AddInst("Nop-B", std::bind(&HardwareCPU<>::Inst_Nop, _1), 1, 1);
-    inst_lib.AddInst("Nop-C", std::bind(&HardwareCPU<>::Inst_Nop, _1), 1, 2);
-    inst_lib.AddInst("Nop-D", std::bind(&HardwareCPU<>::Inst_Nop, _1), 1, 3);
-    inst_lib.AddInst("Nop-E", std::bind(&HardwareCPU<>::Inst_Nop, _1), 1, 4);
-    inst_lib.AddInst("Nop-F", std::bind(&HardwareCPU<>::Inst_Nop, _1), 1, 5);
-    inst_lib.AddInst("Nop-G", std::bind(&HardwareCPU<>::Inst_Nop, _1), 1, 6);
-    inst_lib.AddInst("Nop-H", std::bind(&HardwareCPU<>::Inst_Nop, _1), 1, 7);
+  // void Load4StackDefault(InstLib<HardwareCPU<>, Instruction> & inst_lib) {
+  template <int NUM_STACKS=8, int STACK_SIZE=16, int NUM_ARG_NOPS=8>
+  void Load4StackDefault(InstLib<HardwareCPU<NUM_STACKS, STACK_SIZE, NUM_ARG_NOPS>, Instruction> & inst_lib) {
+    // Load as many nops as we need.  This we be called Nop-0, Nop-1, Nop-2, etc.
+    for (int i = 0; i < NUM_ARG_NOPS; i++) {
+      std::string inst_name = "Nop-";
+      inst_name += std::to_string(i);
+      inst_lib.AddInst(inst_name, std::bind(&HardwareCPU<>::Inst_Nop, _1), 1, i);
+    }
 
+    // Load in single-argument math operations.
     inst_lib.AddInst("Inc", std::bind(&HardwareCPU<>::Inst_AddConst, _1, 1, 1, 1));
     inst_lib.AddInst("Dec", std::bind(&HardwareCPU<>::Inst_AddConst, _1, -1, 1, 1));
     inst_lib.AddInst("Shift-L", std::bind(&HardwareCPU<>::Inst_Shift, _1, 1, 1, 1));
     inst_lib.AddInst("Shift-R", std::bind(&HardwareCPU<>::Inst_Shift, _1, -1, 1, 1));
 
+    // Load in double-argument math operations.
     inst_lib.AddInst("Nand", std::bind(&HardwareCPU<>::Inst_Nand, _1, 1, 2, 3));
     inst_lib.AddInst("Add",  std::bind(&HardwareCPU<>::Inst_Add,  _1, 1, 2, 3));
     inst_lib.AddInst("Sub",  std::bind(&HardwareCPU<>::Inst_Sub,  _1, 1, 2, 3));
