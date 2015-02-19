afe06c74ade392937372d818f03af44d3f10a5a4
Charles Ofria <charles.ofria@gmail.com>
2015-02-18 22:36:16 -0500

    Added functions for hamming distance and edit distance
    Also, new multi-argument min and max functions, under development.

--- tools/functions.h
+++ tools/functions.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <sstream>
 #include <type_traits>
+#include <initializer_list>
 
 #define EMP_FUNCTION_TIMER(TEST_FUN) {                                       \
     std::clock_t emp_start_time = std::clock();                              \
@@ -39,14 +40,36 @@ namespace emp {
     }
   }
 
-  // Swap the value of two variables.
-  template <typename TYPE> void swap(TYPE & in1, TYPE & in2) { TYPE tmp = in1; in1 = in2; in2 = tmp; }
+  // Swap the value of two variables. // @CAO -- better to use STL version that does a MOVE.
+  // template <typename TYPE> void swap(TYPE & in1, TYPE & in2) { TYPE tmp = in1; in1 = in2; in2 = tmp; }
 
   // Run both min and max on a value to put it into a desired range.
   template <typename TYPE> TYPE to_range(const TYPE & value, const TYPE & in_min, const TYPE & in_max) {
     return (value < in_min) ? in_min : ((value > in_max) ? in_max : value);
   }
 
+  template <typename T> const T & min(const T& in1, const T& in2, const T& in3) {
+    return std::min(std::min(in1,in2), in3);
+  }
+
+  // Build a min and max that allows a variable number of inputs to be compared.
+  template <typename T> const T & min(std::initializer_list<const T&> list) {
+    emp_assert(list.size > 0); // Nothing to return if nothing in the list!
+    auto min_found = list.begin();
+    for (auto it = list.begin()+1; it < list.end(); it++) {
+      if (*it < *min_found) min_found = it;
+    }
+    return *min_found;
+  }
+
+  template <typename T> const T & max(std::initializer_list<const T&> list) {
+    emp_assert(list.size > 0); // Nothing to return if nothing in the list!
+    auto max_found = list.begin();
+    for (auto it = list.begin()+1; it < list.end(); it++) {
+      if (*it > *max_found) max_found = it;
+    }
+    return *max_found;
+  }
 
   //  ----- Variadic Template Helpers! -----
 
@@ -108,6 +131,130 @@ namespace emp {
   }
 
   int find_bit(unsigned long long val) { return count_bits( ~val & (val-1) ); }
+
+
+  // --- Distance functions for any array-type objects ---
+
+  // Hamming distance is a simple count of substitutions needed to convert on array to another.
+  template <typename TYPE>
+  int calc_hamming_distance(const TYPE & in1, const TYPE & in2, int offset=0) {
+    if (offset < 0) return calc_hamming_distance(in2, in1, -offset);
+
+    const auto size1 = in1.size();
+    const auto size2 = in2.size();
+
+    // Calculate by how much the strings overlap.
+    int overlap = std::min( size1 - offset,  size2 );
+
+    // Initialize the distance to that part of the strings which do not overlap.
+    int num_diffs = size1 + size2 - 2 * overlap;
+
+    // Step through the overlapped section and add on additional differences.
+    for (size_t i = 0; i < overlap; i++) {
+      if (in1[i + offset] != in2[i]) num_diffs++;
+    }
+    
+    return num_diffs;
+  }
+
+  // Edit distance is the minimum number of insertions, deletions and substitutions to convert
+  // one array to another.
+  template <typename TYPE>
+  int calc_edit_distance(const TYPE & in1, const TYPE & in2) {
+    const auto size1 = in1.size();
+    const auto size2 = in2.size();
+
+    // If either size is zero, other size indicates number of insertions needed to produce it.
+    if (size1 == 0) return size2;
+    if (size2 == 0) return size1;
+
+    std::vector<int> cur_row(size1);   // The row we are calculating
+    std::vector<int> prev_row(size1);  // The previous row we calculated
+
+    // Initialize the previous row to record the differece from nothing.
+    for (int i = 0; i < size1; i++) prev_row[i] = i + 1;
+
+    // Loop through all other rows
+    for (int row = 0; row < (int) size2; row++) {
+      // Initialize the first entry in the current row.
+      if (in1[0] == in2[row]) cur_row[0] = row;
+      else cur_row[0] = std::min(row, prev_row[0]) + 1;
+
+      // Move through the cur_row and fill it in.
+      for (int col = 1; col < (int) size1; col++) {
+        // If the values are equal, keep the value in the upper left.
+        if (in1[col] == in2[row]) cur_row[col] = prev_row[col-1];
+
+        // Otherwise, set the current position the the minimal of the three
+        // numbers to the upper right in the chart plus one.
+        else {
+          cur_row[col] = emp::min(prev_row[col], prev_row[col-1], cur_row[col-1]) + 1;
+        }
+      }
+
+      // Swap cur_row and prev_row (keep cur vals in prev row, recycle vector cur_row)
+      std::swap(cur_row, prev_row);
+    }
+
+    // Now that we are done, return the bottom-right corner of the chart.
+    return prev_row[size1 - 1];
+  }
+
+  // Use edit distance to find the minimum number of insertions, deletions and substitutions
+  // to convert one array to another, and then insert gaps into the arrays appropriately.
+  template <typename TYPE, typename GAP_TYPE>
+  int align(TYPE & in1, TYPE & in2, GAP_TYPE gap) {
+    const auto size1 = in1.size();
+    const auto size2 = in2.size();
+    bool multi = false; // Are there multiple ways of solving this problem?
+
+    // If either size is zero, other size indicates number of insertions needed to produce it.
+    if (size1 == 0) return size2;
+    if (size2 == 0) return size1;
+
+    std::vector<int> cur_row(size1);   // The row we are calculating
+    std::vector<int> prev_row(size1);  // The previous row we calculated
+    std::vector<std::vector<char> > edit_info(size2, std::vector<char>(size1));
+
+    // Initialize the previous row to record the differece from nothing.
+    for (int i = 0; i < size1; i++) {
+      prev_row[i] = i + 1;
+      edit_info[0][i] = 'i';
+    }
+
+    // Loop through all other rows
+    for (int row = 0; row < (int) size2; row++) {
+      // Initialize the first entry in the current row.
+      if (in1[0] == in2[row]) { cur_row[0] = row; edit_info[row][0] = 's'; }
+      else { cur_row[0] = prev_row[0] + 1; edit_info[row][0] = 'd'; }
+      
+      // Move through the cur_row and fill it in.
+      for (int col = 1; col < (int) size1; col++) {
+        // If the values are equal, keep the value in the upper left.
+        if (in1[col] == in2[row]) { cur_row[col] = prev_row[col-1]; edit_info[row][col] = 's'; }
+
+        // Otherwise, set the current position to the minimum of the three
+        // numbers to the left, upper, or upper left in the chart plus one.
+        else {
+          cur_row[col] = emp::min(prev_row[col], prev_row[col-1], cur_row[col-1]) + 1;
+          int match_count = 0;
+          if (cur_row[col] == prev_row[col]) { edit_info[row][col] == 'd'; match_count++; }
+          if (cur_row[col] == prev_row[col-1]) { edit_info[row][col] == 's'; match_count++; }
+          if (cur_row[col] == cur_row[col-1]) { edit_info[row][col] == 'i'; match_count++; }
+          if (match_count > 1) { multi = true; }
+        }
+      }
+
+      // Swap cur_row and prev_row (keep cur vals in prev row, recycle vector cur_row)
+      std::swap(cur_row, prev_row);
+    }
+
+    // Now that we are done, return the bottom-right corner of the chart.
+    return prev_row[size1 - 1];
+  }
+
+
+
 };
 
 #endif
