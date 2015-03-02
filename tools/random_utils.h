d237156d6a6ef14c7227221fa2d4540e5aba8f5f
Charles Ofria <charles.ofria@gmail.com>
2015-03-02 10:53:18 -0500

    Added random_utils.h
    This file should contain functions focused on randomness; for the moment we have one that will
    shuffle the elements in a vector.

---
+++ tools/random_utils.h
@@ -0,0 +1,21 @@
+#ifndef EMP_RANDOM_UTILS_H
+#define EMP_RANDOM_UTILS_H
+
+#include <vector>
+
+#include "Random.h"
+
+namespace emp {
+
+  template <typename TYPE>
+  void shuffle(std::vector<TYPE> & v, Random & random) {
+    for (int i = 1; i < (int) v.size(); i++) {
+      int new_pos = random.GetInt(i+1);
+      if (new_pos == i) continue;
+      std::swap(v[i], v[new_pos]);
+    }
+  }
+
+};
+
+#endif
