7b462557d5f05a21361d776155624cc6905e7ae6
Charles Ofria <charles.ofria@gmail.com>
2015-03-19 22:19:21 -0400

    Overhaul of Ptr

--- tools/Ptr.h
+++ tools/Ptr.h
@@ -9,26 +9,60 @@
 //  pointers are released.
 //
 
+#define EMP_TRACK_MEM
+
 #include <map>
 
+#include "assert.h"
+
+#undef EMP_IF_MEMTRACK
 #ifdef EMP_TRACK_MEM
+#define EMP_IF_MEMTRACK(STATEMENTS) { STATEMENTS }
+#else
+#define EMP_IF_MEMTRACK(STATEMENTS)
 #endif
 
 namespace emp {
 
   class PtrInfo {
   private:
-    int count;
-    bool active;
+    int count;     // How many of this pointer do we have?
+    bool active;   // Has this pointer been deleted? (i.e., we should no longer access it!)
+    bool owner;    // Are we responsible for deleting this pointer?
   public:
-    PtrInfo() : count(0), active(true) { ; }
+    PtrInfo(bool is_owner=false) : count(0), active(true), owner(is_owner) { ; }
+    ~PtrInfo() { ; }
 
     int GetCount() const { return count; }
     bool IsActive() const { return active; }
+    bool IsOwner() const { return owner; }
 
     void Inc() { count++; }
-    void Dec() { count--; }
-    void MarkDeleted() { active = false; }
+    void Dec() {
+      emp_assert(count > 0);        // Do not decrement more copies than we have!
+
+      // Make sure one of these conditions is true:
+      // * We are not getting rid of the last copy, -or-
+      // * We've already deleted this pointer, -or-
+      // * We're not responsible for deleting this pointer.
+      emp_assert(count > 1 || active == false || owner == false);
+      count--;
+    }
+
+    void MarkDeleted() {
+      emp_assert(active == true);   // Do not delete a pointer more than once!
+      emp_assert(owner == true);    // We should only be deleting pointers we own!
+      active = false;
+    }
+    void Claim() {
+      emp_assert(owner == false);    // We can only claim pointer we don't already own!
+      owner = true;
+    }
+    void Surrender() {
+      emp_assert(active == true);   // Do not surrender an inactive pointer!
+      emp_assert(owner == true);    // We can only surrender a pointer we own!
+      owner = false;
+    }
   };
   
   
@@ -44,16 +78,29 @@ namespace emp {
   public:
     ~PtrTracker() { ; }
 
+    // Treat this class as a singleton with a single Get() method to retrieve it.
     static PtrTracker<TYPE> & Get() { static PtrTracker<TYPE> tracker; return tracker; }
 
-    void Inc(TYPE * ptr) { ptr_count[ptr].Inc(); }
+    // This pointer was just created as a Ptr!
+    void New(TYPE * ptr) {
+      emp_assert(ptr_count.find(ptr) == ptr_count.end()); // Make sure pointer does not exist!
+      ptr_count[ptr] = PtrInfo(true);
+    }
+ 
+    // This pointer was already created, but given to Ptr.
+    void Old(TYPE * ptr) {
+      emp_assert(ptr_count.find(ptr) == ptr_count.end()); // Make sure pointer does not exist!
+      ptr_count[ptr] = PtrInfo(false);
+    }
+    void Inc(TYPE * ptr) {
+      emp_assert(ptr_count.find(ptr) != ptr_count.end()); // Make sure pointer already exists!
+      ptr_count[ptr].Inc();
+    }
     void Dec(TYPE * ptr) {
-      // @CAO Make sure this pointer is not dropping below zero! (Internal error?)
-      // @CAO Make sure this pointer is deleted before it drops to zero.
       ptr_count[ptr].Dec();
     }
     void MarkDeleted(TYPE * ptr) {
-      // @CAO Make sure this pointer exists before deleting it!
+      emp_assert(ptr_count.find(ptr) != ptr_count.end()); // Make sure pointer actually exists!
       ptr_count[ptr].MarkDeleted();
     }
   };
@@ -62,16 +109,23 @@ namespace emp {
   class Ptr {
   private:
     TYPE * ptr;
+
+#ifdef EMP_TRACK_MEM
+    static PtrTracker<TYPE> & Tracker() { return PtrTracker<TYPE>::Get(); }
+#endif
   public:
     Ptr() : ptr(NULL) { ; }
-    Ptr(TYPE * in_ptr) : ptr(in_ptr) { ; }
-    Ptr(TYPE & obj) : ptr(&obj) { ; }
-    Ptr(const Ptr<TYPE> & _in) : ptr(_in.ptr) { ; }
-    ~Ptr() { ; }
+    Ptr(TYPE * in_ptr) : ptr(in_ptr) { EMP_IF_MEMTRACK( Tracker().Old(ptr); ); }
+    Ptr(TYPE & obj) : ptr(&obj) { EMP_IF_MEMTRACK( Tracker().Old(ptr); ); }
+    Ptr(const Ptr<TYPE> & _in) : ptr(_in.ptr) { EMP_IF_MEMTRACK( Tracker().Inc(ptr); ); }
+    ~Ptr() { EMP_IF_MEMTRACK( Tracker().Dec(ptr); ); }
 
     bool IsNull() { return ptr == NULL; }
 
-    void New() { ptr = new TYPE; }
+    void New() {
+      ptr = new TYPE;
+      EMP_IF_MEMTRACK(Tracker().New(ptr););
+    }
     void New(const TYPE & init_val) { ptr = new TYPE(init_val); }
     void Delete() { delete ptr; }
 
