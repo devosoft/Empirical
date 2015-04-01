0a351fec336fe02007cab8c6f4d0003cc39cf1c2
Charles Ofria <charles.ofria@gmail.com>
2015-03-31 22:43:15 -0400

    Started to frame puzzle classes

--- tools/puzzle_grid.h
+++ tools/puzzle_grid.h
@@ -14,23 +14,59 @@
 //
 //  Grid components (Cells, Edges, and Points) have three versions.  The default versions have
 //  int states, but there are also binary versions (eg, PuzzleGrid::CellBinary), non-state
-//  versions (eg: PuzzleGrid::CellNoState), and null version (eg: PuzzleGrid::CellNull).
-//  The difference between CellNoState and CellNull is that the former allows you to navigate
+//  versions (eg: PuzzleGrid::CellNoState), and null version (eg: PuzzleGrid::CellBase).
+//  The difference between CellNoState and CellBase is that the former allows you to navigate
 //  relative to the cell, but the latter does not.
 //
 //  PuzzleGrid::Board object are templated based on which of the components they work with.
 //
 //  A Sudoku puzzle (which only uses cells and regions) might be defined as:
-//    PuzzleGrid::Board<PuzzleGrid::Cell, PuzzleGrid::EdgeNull, PuzzleGrid::PointNULL>
+//    PuzzleGrid::Board<PuzzleGrid::Cell, PuzzleGrid::EdgeBase, PuzzleGrid::PointNULL>
 //  
 //  A Sliterlink puzzle has binary states at edges and possibly cells (inside/outside), and will
 //  use relative positions at points, so:
 //    PuzzleGrid::Board<PuzzleGrid::CellBinary, PuzzleGrid::EdgeBinary, PuzzleGrid::PointNoState>
 //
 
+#include <vector>
+
+#include "assert.h"
+
 namespace emp {
 namespace PuzzleGrid {
-  
+
+  // Quick pre-declarations of all types
+  class CellBase;    class CellNoState;    class CellBinary;    class Cell;
+  class EdgeBase;    class EdgeNoState;    class EdgeBinary;    class Edge;
+  class PointBase;   class PointNoState;   class PointBinary;   class Point;
+  class Region;
+  class Board;
+
+  // Full declarations
+  class CellBase {
+  public:
+    virtual EdgeBase & GetEdgeUp();
+    virtual EdgeBase & GetEdgeDown();
+    virtual EdgeBase & GetEdgeLeft();
+    virtual EdgeBase & GetEdgeRight();
+  };
+
+  class EdgeBase {
+  public:
+    ;
+  };
+
+  class PointBase {
+  public:
+    ;
+  };
+
+  template <class CELL=Cell, class EDGE=Edge, class POINT=Point>
+  class Board {
+  };
+
+
+  EdgeBase & CellBase::GetEdgeUp() { emp_assert(false); return Board::NullCell; }
 };
 };
 
