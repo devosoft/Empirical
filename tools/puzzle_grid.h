f181577938bc4de3ebe0289ba9b8e611ae8eaea2
Charles Ofria <charles.ofria@gmail.com>
2015-04-01 21:07:57 -0400

    Restructured grid boards more sensibly

--- tools/puzzle_grid.h
+++ tools/puzzle_grid.h
@@ -35,38 +35,84 @@
 namespace emp {
 namespace PuzzleGrid {
 
-  // Quick pre-declarations of all types
-  class CellBase;    class CellNoState;    class CellBinary;    class Cell;
-  class EdgeBase;    class EdgeNoState;    class EdgeBinary;    class Edge;
-  class PointBase;   class PointNoState;   class PointBinary;   class Point;
-  class Region;
-  class Board;
+  // States for a Cell, Edge, or Point can be void, bool, or int.
 
-  // Full declarations
-  class CellBase {
+  class Layout;    // No state, but tracks connections.
+  class StateSet;  // A matrix of board states
+  class Board;     // All states (for cells, edges, and points) on a board.
+
+  class Layout {
+  private:
+    int width;
+    int height;
+    std::vector< std::vector<int> > regions;
   public:
-    virtual EdgeBase & GetEdgeUp();
-    virtual EdgeBase & GetEdgeDown();
-    virtual EdgeBase & GetEdgeLeft();
-    virtual EdgeBase & GetEdgeRight();
+    Layout(int w, int h) : width(w), height(h) { ; }
+    Layout(const Layout &) = default;
+    ~Layout() { ; }
+    opterator=(const Layout &) = default;
+
+    int GetWidth() const { return width; }
+    int GetHeight() const { return height; }
   };
 
-  class EdgeBase {
+  template <typename STATE_TYPE>
+  class StateSet {
+  private:
+    int width;
+    std::vector<STATE_TYPE> states;
+
   public:
-    ;
+    StateSet(int _w, int _h) : width(_w), states(_w*_h) { ; }
+    StateSet(const StateSet &) = default;
+    ~StateSet() { ; }
+    StateSet & operator=(const StateSet &) = default;
+
+    int GetWidth() const { return width; }
+    int GetHeight() const { return states.size() / width; }
+    int GetSize() const { return states.size(); }
+
+    STATE_TYPE operator()(int x, int y) { return states[y*width+x]; }
+    STATE_TYPE operator()(int id) { return states[id]; }
+    STATE_TYPE operator[](int id) { return states[id]; }
   };
 
-  class PointBase {
+  // StateSet is specialized on void: no data is stored.
+  template <>
+  class StateSet<void> {
   public:
-    ;
+    StateSet(int _w, int _h) { (void) _w; (void) _h; }
+    StateSet(const StateSet &) { ; }
+    ~StateSet() { ; }
+    StateSet & operator=(const StateSet &) { return *this; }
+
+    int GetWidth() const { return -1; }
+    int GetHeight() const { return -1; }
+    int GetSize() const { return -1; }
+
+    void operator()(int x, int y) { (void) x; (void) y; }
+    void operator()(int id) { (void) id; }
+    void operator[](int id) { (void) id; }
   };
 
-  template <class CELL=Cell, class EDGE=Edge, class POINT=Point>
+  template <typename CELL_TYPE=int, typename EDGE_TYPE=void, class POINT_TYPE=void>
   class Board {
+  private:
+    const Layout & layout;
+    StateSet<CELL_TYPE> cell_states;
+    StateSet<EDGE_TYPE> edge_states;
+    StateSet<POINT_TYPE> point_states;
+
+  public:
+    Board(const BoardLayout & in_layout)
+      : layout(in_layout)
+      , cell_states(layout.GetWidth(), layout.GetHeight())
+      , edge_states(layout.GetWidth(), layout.GetHeight())
+      , point_states(layout.GetWidth(), layout.GetHeight())
+    {
+    }
   };
 
-
-  EdgeBase & CellBase::GetEdgeUp() { emp_assert(false); return Board::NullCell; }
 };
 };
 
