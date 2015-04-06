03346cdab32d7fe6e0410825978a141353dd3be3
Charles Ofria <charles.ofria@gmail.com>
2015-04-05 23:52:50 -0400

    Testing different types for board template

--- UTests/tools/grid.cc
+++ UTests/tools/grid.cc
@@ -20,5 +20,17 @@ int main(int argc, char* argv[])
 
   emp_assert(layout.GetNumRegions() == 1);
 
-  emp::Grid::Board<bool, int, bool> board(layout);
+  emp::Grid::Board<void, void, void> board1(layout);
+  emp::Grid::Board<bool, bool, bool> board2(layout);
+  emp::Grid::Board<int, int, int> board3(layout);
+  emp::Grid::Board<int, void, void> board4(layout);
+  emp::Grid::Board<void, int, void> board5(layout);
+  emp::Grid::Board<void, void, int> board6(layout);
+
+  std::cout << sizeof(board1) << std::endl;
+  std::cout << sizeof(board2) << std::endl;
+  std::cout << sizeof(board3) << std::endl;
+  std::cout << sizeof(board4) << std::endl;
+  std::cout << sizeof(board5) << std::endl;
+  std::cout << sizeof(board6) << std::endl;
 }
