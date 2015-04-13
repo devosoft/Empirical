159b1bf373906b3b0a09762224c29a44e144ec17
Charles Ofria <charles.ofria@gmail.com>
2015-04-13 12:32:28 -0400

    Added ability to track authors

---
+++ scholar/Author.h
@@ -0,0 +1,24 @@
+#ifndef EMP_AUTHOR_H
+#define EMP_AUTHOR_H
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Basic information about an author
+//
+
+#include <string>
+
+namespace emp {
+
+  class Author {
+  private:
+    std::string first_name;
+    std::string middle_name;
+    std::string last_name;
+  public:
+    
+  };
+
+};
+
+#endif
