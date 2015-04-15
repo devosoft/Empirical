629e1606c548d94c3a06899a034c1e44a9883038
Charles Ofria <charles.ofria@gmail.com>
2015-04-15 00:36:24 -0400

    Info about a single citation

---
+++ scholar/Citation.h
@@ -0,0 +1,39 @@
+#ifndef EMP_CITATION_H
+#define EMP_CITATION_H
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Information about a single citation for a paper.
+//
+
+#include <set>
+#include <string>
+#include <vector>
+
+#include "Author.h"
+
+namespace emp {
+
+  class Citation {
+  private:
+    std::vector<Author> author_list;
+    std::string title;
+    std::string journal;
+    std::string volume;
+    int year;
+    int start_page;
+    int end_page;
+    std::set<std::string> keywords;
+  public:
+    Citation() { ; }
+    Citation(const Citation &) = default;
+    ~Citation() { ; }
+
+    Citation & operator=(const Citation &) = default;
+
+    void AddAuthor(const Author & author) { author_list.push_back(author); }
+  };
+
+};
+
+#endif
