ab1c822444a2b29e4a88774ff0c6f49031da8b03
Charles Ofria <charles.ofria@gmail.com>
2015-04-14 22:04:28 -0400

    Added methods

--- scholar/Author.h
+++ scholar/Author.h
@@ -16,7 +16,27 @@ namespace emp {
     std::string middle_name;
     std::string last_name;
   public:
-    
+    Author(const std::string & first, const std::string & middle, const std::string & last)
+      : first_name(first), middle_name(middle), last_name(last) { ; }
+    Author(const std::string & first, const std::string & last)
+      : first_name(first), last_name(last) { ; }
+    Author(const Author &) = delete;
+    ~Author() { ; }
+    Author & operator=(const Author &) = delete;
+
+    bool operator==(const Author & other) {
+      return (first_name == other.first_name) &&
+        (midd_name == other.middle_name) &&
+        (last_name == other.last_name);
+    }
+
+    const std::string GetFirstName() const { return first_name; }
+    const std::string GetMiddleName() const { return middle_name; }
+    const std::string GetLastName() const { return last_name; }
+
+    char GetFirstInitial() const { return first_name.size() ? first_name[0] : 0; }
+    char GetMiddleInitial() const { return middle_name.size() ? middle_name[0] : 0; }
+    char GetLastInitial() const { return last_name.size() ? last_name[0] : 0; }
   };
 
 };
