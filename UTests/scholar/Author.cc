546c866a2e38391d1030bf170bd0a6b8ca57ddb0
Charles Ofria <charles.ofria@gmail.com>
2015-04-17 12:58:25 -0400

    Unit tests for Author.h

---
+++ UTests/scholar/Author.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include "../../tools/assert.h"
+#include "../../tools/command_line.h"
+#include "../../scholar/Author.h"
+
+int main(int argc, char* argv[])
+{
+  std::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
+  const bool verbose = emp::cl::use_arg(args, "-v");
+
+  emp::Author fml("First", "Middle", "Last");
+  emp_assert(fml.GetFullName() == "First Middle Last");
+
+  emp::Author fl("No", "Mid");
+  emp_assert(fl.GetFullName() == "No Mid");
+
+  emp::Author l("JustLast");
+  emp_assert(l.GetFullName() == "JustLast");
+
+  emp::Author fml2("First", "Middiff", "Last");
+  emp_assert(fml2.GetFullName() == "First Middiff Last");
+  emp_assert(fml2 < fml);
+
+  emp::Author auth1("aaa", "bbb", "ccc");
+  emp::Author auth2("aaa", "bbb", "ccd");
+  emp::Author auth3("aaa", "bbd", "ccc");
+  emp::Author auth4("aad", "bbb", "ccc");
+  emp::Author auth5("aaa", "bbb", "ccc");
+
+  emp_assert(auth1 < auth2);
+  emp_assert(auth1 < auth3);
+  emp_assert(auth1 < auth4);
+  emp_assert(auth1 == auth5);
+
+  emp_assert(auth2 > auth3);
+  emp_assert(auth2 > auth4);
+  emp_assert(auth2 > auth5);
+
+  emp_assert(auth3 < auth4);
+  emp_assert(auth3 > auth5);
+
+  emp_assert(auth4 > auth5);
+
+  // Try out <= operator
+  emp_assert(auth1 <= auth2);
+  emp_assert(auth1 <= auth3);
+  emp_assert(auth1 <= auth4);
+  emp_assert(auth1 <= auth5);
+}
