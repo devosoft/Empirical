65f614b19784241e0c3cd7af48c2a347aff58310
Charles Ofria <charles.ofria@gmail.com>
2015-04-26 23:47:17 -0400

    Tests of comparisons and sets

--- UTests/geometry/Point2D.cc
+++ UTests/geometry/Point2D.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include "../../tools/assert.h"
 #include "../../tools/command_line.h"
@@ -14,6 +15,7 @@ int main(int argc, char* argv[])
   emp::Point<double> point_set(1.5, 2.0);
   emp::Point<double> point_copy(point_set);
   emp::Point<double> point_scale(point_set, 5.0);
+  emp::Point<double> point_list({3.0, 4.0});
 
   emp_assert(point_base.Magnitude() == 0.0);
   emp_assert(point_set.Magnitude() == 2.5);
@@ -23,4 +25,31 @@ int main(int argc, char* argv[])
   if (verbose) {
     std::cout << "Constructor tests passed." << std::endl;
   }
+
+  // Test comparisons
+  emp_assert(point_set == point_copy);
+  emp_assert(point_set != point_scale);
+  emp_assert(point_scale == point_list);
+
+  if (verbose) {
+    std::cout << "Comparison tests passed." << std::endl;
+  }
+
+  // Test setting points
+  std::vector<emp::Point<double> > points(10);
+  points[0].SetX(13);
+  points[1].SetY(13);
+  points[2].Set(5, 12);
+
+  emp_assert(points[0].Magnitude() == 13);
+  emp_assert(points[1].Magnitude() == 13);
+  emp_assert(points[2].Magnitude() == 13);
+
+  if (verbose) {
+    std::cout << "Point Setting tests passed." << std::endl;
+  }
+
+
+  // Explicitly test magnitude-related member functions
+  
 }
