74aa78f4b27badc017a3ecbccad0700c7afc9ea0
Charles Ofria <charles.ofria@gmail.com>
2015-04-28 23:08:37 -0400

    Added initial test for building offspring

--- UTests/geometry/Body2D.cc
+++ UTests/geometry/Body2D.cc
@@ -13,6 +13,9 @@ int main(int argc, char* argv[])
   emp::Circle<double> body_outline(5.0);
   emp::CircleBody2D<int, double> body1(body_outline);
 
+
+  // Test to make sure shifts and pressure are being calculated correctly.
+
   emp::Point<double> shift1(5.0, 6.0);
   emp::Point<double> shift2(-2.0, -2.0);
 
@@ -24,4 +27,14 @@ int main(int argc, char* argv[])
 
   emp_assert(body1.GetShift() == shift1 + shift2);
   emp_assert(body1.CalcPressure() == 32);
+
+  if (verbose) {
+    std::cout << "Shifting & Pressure tests passed." << std::endl;
+  }
+
+  // Start a round of replication for tests
+
+  emp::CircleBody2D<int, double> * body2 = body1.BuildOffspring({0.3, 0.4});
+  (void) body2;
+  
 }
