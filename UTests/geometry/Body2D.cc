d828659451173c2b5c1bba8f1e4af9b1cd6686cb
Charles Ofria <charles.ofria@gmail.com>
2015-04-29 22:34:51 -0400

    Added tests for body linkage

--- UTests/geometry/Body2D.cc
+++ UTests/geometry/Body2D.cc
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
   std::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
   const bool verbose = emp::cl::use_arg(args, "-v");
 
-  emp::Circle<double> body_outline(5.0);
+  emp::Circle<double> body_outline(10.0);
   emp::CircleBody2D<int, double> body1(body_outline);
 
 
@@ -34,7 +34,19 @@ int main(int argc, char* argv[])
 
   // Start a round of replication for tests
 
-  emp::CircleBody2D<int, double> * body2 = body1.BuildOffspring({0.3, 0.4});
+  emp::CircleBody2D<int, double> & body2 = *(body1.BuildOffspring({3.0, -4.0}));
+
+  // Make sure original organism is linked to offspring.
+  emp_assert(body1.IsLinked(body2));
+  emp_assert(body2.IsLinked(body1));
+  emp_assert(body1.GetLinkDist(body2) == 5.0);
+  emp_assert(body2.GetLinkDist(body1) == 5.0);
+  emp_assert(body1.GetTargetLinkDist(body2) == 20.0);
+  emp_assert(body2.GetTargetLinkDist(body1) == 20.0);
+
+  std::cout << body1.GetTargetLinkDist(body2) << std::endl;
+  std::cout << body2.GetTargetLinkDist(body1) << std::endl;
+
   (void) body2;
   
 }
