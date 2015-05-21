b7cbc941cd7dea83d86efb27314e4cfeab3984bb
Charles Ofria <charles.ofria@gmail.com>
2015-05-20 23:24:08 -0400

    Setup with new Initialize() function

--- UTests/emtools/JSWrap.cc
+++ UTests/emtools/JSWrap.cc
@@ -2,7 +2,7 @@
 
 #include "../../tools/alert.h"
 #include "../../emtools/JSWrap.h"
-
+#include "../../emtools/init.h"
 
 void TestFun(int w, int x, int y, double z) {
   emp::Alert(w + x*y*z);
@@ -10,11 +10,13 @@ void TestFun(int w, int x, int y, double z) {
 
 
 int main() {
+
+  emp::Initialize();
+
   unsigned int fun_id = emp::JSWrap(TestFun);
 
-  EM_ASM({
-      emp.cb_args = [100, 2.5, -4, 8.5];
-    });
 
-  empCppCallback(fun_id);
+  EM_ASM_ARGS({
+      emp.CppCallback($0, 5, 17, 4, 1.5);
+    }, fun_id);
 }
