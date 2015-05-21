995d6b75d5788e67be73d303ea3026a8d3fc4d13
Charles Ofria <charles.ofria@gmail.com>
2015-05-20 21:37:54 -0400

    Added CppCallback method to emp

--- emtools/library_emp.js
+++ emtools/library_emp.js
@@ -1,5 +1,16 @@
 mergeInto(LibraryManager.library, {
-    $emp: { cb_args:[] },
+    $emp: { cb_args:[],
+            CppCallback: function() {
+                // Copy over the additional arguments
+                emp.cb_args = [];
+                for (var i = 1; i < arguments.length; i++) {
+                    emp.cb_args[i-1] = arguments[i];
+                }
+
+                // Callback to the original function.
+                empCppCallback(arguments[0]);
+            }
+          },
 
     EMP_GetCBArgCount__deps: ['$emp'],
     EMP_GetCBArgCount: function() { return emp.cb_args.length; },
