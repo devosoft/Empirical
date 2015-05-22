518a5c8473fcd167459e73805e76351b6ffe8935
Charles Ofria <charles.ofria@gmail.com>
2015-05-21 22:56:48 -0400

    Test with multiple functions

--- UTests/emtools/JSWrap.cc
+++ UTests/emtools/JSWrap.cc
@@ -4,8 +4,12 @@
 #include "../../emtools/JSWrap.h"
 #include "../../emtools/init.h"
 
-void TestFun(int w, int x, int y, double z) {
+void TestFun1(int w, int x, int y, double z) {
   emp::Alert(w + x*y*z);
+}
+
+void TestFun2(double a, double b) {
+  emp::Alert( a - b );
 }
 
 
@@ -13,10 +17,23 @@ int main() {
 
   emp::Initialize();
 
-  unsigned int fun_id = emp::JSWrap(TestFun);
+  uint32_t fun_id1 = emp::JSWrap(TestFun1, "TestName1", false);
+  uint32_t fun_id2 = emp::JSWrap(TestFun2, "TestName2", false);
 
 
   EM_ASM_ARGS({
-      emp.CppCallback($0, 5, 17, 4, 1.5);
-    }, fun_id);
+      emp.Callback($0, 5, 17, 4, 1.5);
+    }, fun_id1);
+
+  EM_ASM_ARGS({
+      emp.Callback($0, 4.0, 2.5);
+    }, fun_id2);
+
+  EM_ASM({
+      emp.TestName1(20, 10, 1, 0.5);
+    });
+
+  EM_ASM({
+      emp.TestName2(1.5, 1.5);
+    });
 }
