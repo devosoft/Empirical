41f27d7183e343a394461df70fe9d0780b43f824
Charles Ofria <charles.ofria@gmail.com>
2015-05-22 22:48:14 -0400

    Added return-value tests and no-arg tests

--- UTests/emtools/JSWrap.cc
+++ UTests/emtools/JSWrap.cc
@@ -12,6 +12,17 @@ void TestFun2(double a, double b) {
   emp::Alert( a - b );
 }
 
+void TestFun3(std::string str1, int copies) {
+  std::string out_str;
+  for (int i = 0; i < copies; i++) out_str += str1;
+  emp::Alert( out_str );
+}
+
+double TestFun4(double a, double b) {
+  return  a / b ;
+}
+
+double TestFun5() { return 10000.1; }
 
 int main() {
 
@@ -19,11 +30,30 @@ int main() {
 
   uint32_t fun_id1 = emp::JSWrap(TestFun1, "TestName1", false);
   uint32_t fun_id2 = emp::JSWrap(TestFun2, "TestName2", false);
+  uint32_t fun_id3 = emp::JSWrap(TestFun3, "TestName3", false);
+  uint32_t fun_id4 = emp::JSWrap(TestFun4, "TestName4", false);
+  uint32_t fun_id5 = emp::JSWrap(TestFun5, "TestName5", false);
+  (void) fun_id4;
+  (void) fun_id5;
+
+
+  double in1 = 4.5;
+  double in2 = 1.5;
+  double val = EM_ASM_INT({ return emp.TestName4($0, $1); }, in1, in2);
+  emp::Alert(in1, " / ", in2, " = ", val);
+
+  EM_ASM_ARGS({
+      alert( emp.Callback($0) );
+    }, fun_id5);
 
 
   EM_ASM_ARGS({
       emp.Callback($0, 5, 17, 4, 1.5);
     }, fun_id1);
+
+  EM_ASM_ARGS({
+      emp.Callback($0, "abc", 3);
+    }, fun_id3);
 
   EM_ASM_ARGS({
       emp.Callback($0, 4.0, 2.5);
@@ -36,4 +66,7 @@ int main() {
   EM_ASM({
       emp.TestName2(1.5, 1.5);
     });
+
+
+
 }
