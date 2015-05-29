1299cc051f887e1463e4528bbd1a379851b90715
Charles Ofria <charles.ofria@gmail.com>
2015-05-29 10:55:42 -0400

    Moved Element.cc tests to ElementSlate.cc

---
+++ UTests/jquery/ElementSlate.cc
@@ -0,0 +1,19 @@
+#include "../../jquery/jquery.h"
+
+int main() {
+
+  emp::JQ::Initialize();
+
+  int test_num = 42;
+
+  emp::JQ::document
+    << "<h1>This is a test site!</h1>"
+    << "<p>The value of test_num is " << test_num << ".</p>";
+
+  emp::JQ::document.Update();
+
+  std::stringstream ss;
+  emp::JQ::document.PrintHTML(ss);
+  emp::Alert(ss.str());
+}
+
