4f4887fe4d9c7b33e9d9a4232b7427d956ba2be5
Charles Ofria <charles.ofria@gmail.com>
2015-06-06 12:27:24 -0400

    Moved detail classes into internal namespace

--- UI/UI_base.h
+++ UI/UI_base.h
@@ -8,139 +8,157 @@
 namespace emp {
 namespace UI {
 
-  // Widget_core is a base class containing information needed by all GUI widget classes
-  // (Buttons, Images, etc...).
-
-  struct CSS_Info {
-    std::string setting;
-    std::string value;
-
-    CSS_Info(const std::string & s, const std::string & v) : setting(s), value(v) { ; }
-  };
-
-  class Widget_base {
-  protected: 
-    std::string temp_name;
-    int width;
-    int height;
+  namespace internal {
+  
+    // CSS_Info contains information about a single CSS Setting.
     
-    std::vector<CSS_Info> css_mods;
+    struct CSS_Info {
+      std::string setting;
+      std::string value;
+      
+      CSS_Info(const std::string & s, const std::string & v) : setting(s), value(v) { ; }
+    };
 
-    Widget_base() : temp_name(""), width(-1), height(-1) { ; }
-  };
-
-  // Widget_wrap is a template wrapper to make sure all derived widgets return the
-  // correct types when setting values.
-
-  template <typename DETAIL_TYPE, typename... ARG_TYPES>
-  class Widget_wrap : public DETAIL_TYPE {
-  public:
-    Widget_wrap(ARG_TYPES... args, const std::string & in_name="") : DETAIL_TYPE(args...)
-    { Widget_base::temp_name = in_name; }
-
-    Widget_wrap & TempName(const std::string & in_name) { Widget_base::temp_name = in_name; return *this; }
-    Widget_wrap & Width(int w) { Widget_base::width = w; return *this; }
-    Widget_wrap & Height(int h) { Widget_base::height = h; return *this; }
-    Widget_wrap & Size(int w, int h) {
-      Widget_base::width = w;
-      Widget_base::height = h;
-      return *this;
-    }
-
-    Widget_wrap & Background(const std::string & color) {
-      Widget_base::css_mods.emplace_back("background-color", color);
-      return *this;
-    }
-
-    const std::string & GetTempName() const { return Widget_base::temp_name; }
-  };
-
-
-  // Class to detail specs of buttons.
-
-  class Button_detail;
-  using Button = Widget_wrap<Button_detail, const std::function<void()> &, const std::string &>;
-
-  class Button_detail : public Widget_base {
-  protected:
-    std::function<void()> callback;
-    std::string label;
-
-    bool autofocus;
-    bool disabled;
-    std::string title;
-
-    uint32_t callback_id;
+    // Widget_base is a base class containing information needed by all GUI widget classes
+    // (Buttons, Images, etc...).
     
-    void WriteHTML(std::ostream & os) {
-      os << "<button";                          // Start the button tag.
-      if (title != "") {                          // If the button has a title, add it!
-        os << " title=\"" << title << "\"";
+    class Widget_base {
+    protected: 
+      std::string temp_name;
+      int width;
+      int height;
+      
+      std::vector<CSS_Info> css_mods;
+      
+      Widget_base() : temp_name(""), width(-1), height(-1) { ; }
+    };
+    
+    // Widget_wrap is a template wrapper to make sure all derived widgets return the
+    // correct types when setting values.
+    
+    template <typename DETAIL_TYPE, typename... ARG_TYPES>
+    class Widget_wrap : public DETAIL_TYPE {
+    public:
+      Widget_wrap(ARG_TYPES... args, const std::string & in_name="") : DETAIL_TYPE(args...)
+      { Widget_base::temp_name = in_name; }
+
+      Widget_wrap & TempName(const std::string & in_name) { Widget_base::temp_name = in_name; return *this; }
+      Widget_wrap & Width(int w) { Widget_base::width = w; return *this; }
+      Widget_wrap & Height(int h) { Widget_base::height = h; return *this; }
+      Widget_wrap & Size(int w, int h) {
+        Widget_base::width = w;
+        Widget_base::height = h;
+        return *this;
       }
-      if (width > 0 || height > 0) {              // If the button has a style, add it!
-        os << " style=\"";
-        if (width > 0) os << "width:" << width << "px;";
-        if (height > 0) os << "height:" << height << "px;";
-        os << "\"";
+      
+      Widget_wrap & Background(const std::string & color) {
+        Widget_base::css_mods.emplace_back("background-color", color);
+        return *this;
       }
-
-      // Indicate action on click.
-      os << " onclick=\"empCppCallback(" << std::to_string(callback_id) << ")\"";
-
-      os << ">" << label << "</button>";       // Close and label the button.
-    }
-  public:
-    Button_detail(const std::function<void()> & in_cb, const std::string & in_label)
-      : callback(in_cb), label(in_label)
-      , autofocus(false), disabled(false), title("")
-      , callback_id(JSWrap(callback))
-    { ; }
-    ~Button_detail() {
-      // @CAO Need to cleanup callback! 
-    }
-
-    Button & Callback(const std::function<void()> & in_cb) {
-      // @CAO Need to cleanup old callback before setting up new one.
-      callback = in_cb;
-      callback_id = JSWrap(callback);
-      return (Button &) *this;
-    }
-    Button & Label(const std::string & in_label) { label = in_label; return (Button &) *this; }
-
-    Button & Autofocus(bool in_af) { autofocus = in_af; return (Button &) *this; }
-    Button & Disabled(bool in_dis) { disabled = in_dis; return (Button &) *this; }
-    Button & Title(const std::string & t) { title = t; return (Button &) *this; }
+      
+      const std::string & GetTempName() const { return Widget_base::temp_name; }
+    };
+    
+    
+    // Class to detail specs of buttons.
+    
+    class Button_detail;
   };
 
+  using Button = internal::Widget_wrap<internal::Button_detail,
+                                       const std::function<void()> &,
+                                       const std::string &>;
 
-  // Class to detail specifications for images.
+  namespace internal {
 
-  class Image_detail;
-  using Image = Widget_wrap<Image_detail, const std::string &>;
+    class Button_detail : public Widget_base {
+    protected:
+      std::function<void()> callback;
+      std::string label;
+      
+      bool autofocus;
+      bool disabled;
+      std::string title;
 
-  class Image_detail : public Widget_base {
-  protected:
-    std::string url;
-    std::string alt_text;
-
-    void WriteHTML(std::ostream & os) {
-      os << "<img src=\"" << url << "\" alt=\"" << alt_text << "\"";
-      if (width > 0 || height > 0) {
-        os << " style=\"";
-        if (width > 0) os << "width:" << width << "px;";
-        if (height > 0) os << "height:" << height << "px;";
-        os << "\"";
+      uint32_t callback_id;
+      
+      void WriteHTML(std::ostream & os) {
+        os << "<button";                          // Start the button tag.
+        if (title != "") {                          // If the button has a title, add it!
+          os << " title=\"" << title << "\"";
+        }
+        if (width > 0 || height > 0) {              // If the button has a style, add it!
+          os << " style=\"";
+          if (width > 0) os << "width:" << width << "px;";
+          if (height > 0) os << "height:" << height << "px;";
+          os << "\"";
+        }
+        
+        // Indicate action on click.
+        os << " onclick=\"empCppCallback(" << std::to_string(callback_id) << ")\"";
+        
+        os << ">" << label << "</button>";       // Close and label the button.
       }
-      os << ">";
-    }
+    public:
+      Button_detail(const std::function<void()> & in_cb, const std::string & in_label)
+        : callback(in_cb), label(in_label)
+        , autofocus(false), disabled(false), title("")
+        , callback_id(JSWrap(callback))
+      { ; }
+      ~Button_detail() {
+        // @CAO Need to cleanup callback! 
+      }
 
-  public:
-    Image_detail(const std::string & in_url)
-      : url(in_url), alt_text("") { ; }
+      Button & Callback(const std::function<void()> & in_cb) {
+        // @CAO Need to cleanup old callback before setting up new one.
+        callback = in_cb;
+        callback_id = JSWrap(callback);
+        return (Button &) *this;
+      }
+      Button & Label(const std::string & in_label) { label = in_label; return (Button &) *this; }
 
-    Image & URL(const std::string & in_url) { url = in_url; return (Image &) *this; }
-    Image & Alt(const std::string & in_alt) { alt_text = in_alt; return (Image &) *this; }
+      Button & Autofocus(bool in_af) { autofocus = in_af; return (Button &) *this; }
+      Button & Disabled(bool in_dis) { disabled = in_dis; return (Button &) *this; }
+      Button & Title(const std::string & t) { title = t; return (Button &) *this; }
+    };
+
+
+    // Class to detail specifications for images.
+
+    class Image_detail;
   };
+
+  using Image = internal::Widget_wrap<internal::Image_detail, const std::string &>;
+
+  namespace internal {
+    class Image_detail : public Widget_base {
+    protected:
+      std::string url;
+      std::string alt_text;
+      
+      void WriteHTML(std::ostream & os) {
+        os << "<img src=\"" << url << "\" alt=\"" << alt_text << "\"";
+        if (width > 0 || height > 0) {
+          os << " style=\"";
+          if (width > 0) os << "width:" << width << "px;";
+          if (height > 0) os << "height:" << height << "px;";
+          os << "\"";
+        }
+        os << ">";
+      }
+
+    public:
+      Image_detail(const std::string & in_url)
+        : url(in_url), alt_text("") { ; }
+      
+      Image & URL(const std::string & in_url) { url = in_url; return (Image &) *this; }
+      Image & Alt(const std::string & in_alt) { alt_text = in_alt; return (Image &) *this; }
+    };
+    
+    class Table_detail;
+  }
+  
+  using Table = internal::Widget_wrap<internal::Table_detail, int, int>;
 
   struct TableCell {
     int row;      // In which row is the upper-left corner of this cell?
@@ -149,30 +167,25 @@ namespace UI {
     int height;   // How many rows tall is this cell?
     bool header;  // Is this cell a header?
     bool masked;  // Is this cell hidden by another?
-    // @CAO color?
   };
 
-  // Class to detail specifications for images.
-
-  class Table_detail;
-  using Table = Widget_wrap<Table_detail, int, int>;
-
-  class Table_detail : public Widget_base {
-  protected:
-    int cols;
-    int rows;
-
-    std::vector<TableCell> cell_info;
-
-    void WriteHTML(std::ostream & os) {
-    }
-  public:
-    Table_detail(int in_cols, int in_rows) : cols(in_cols), rows(in_rows) { ; }
-
-    Table & Cols(int c) { cols = c; return (Table &) *this; }
-    Table & Rows(int r) { rows = r; return (Table &) *this; }
-  };
-
+  namespace internal {
+    class Table_detail : public Widget_base {
+    protected:
+      int cols;
+      int rows;
+      
+      std::vector<TableCell> cell_info;
+      
+      void WriteHTML(std::ostream & os) {
+      }
+    public:
+      Table_detail(int in_cols, int in_rows) : cols(in_cols), rows(in_rows) { ; }
+      
+      Table & Cols(int c) { cols = c; return (Table &) *this; }
+      Table & Rows(int r) { rows = r; return (Table &) *this; }
+    };
+  }
 
   // class Text : public UI_base {
   // protected:
