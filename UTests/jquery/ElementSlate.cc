f815a467a9e37ef759d4317fbe41ba5c517f50f5
Charles Ofria <charles.ofria@gmail.com>
2015-06-07 23:42:27 -0400

    Updated ElementSlate test with several new widgets

--- UTests/jquery/ElementSlate.cc
+++ UTests/jquery/ElementSlate.cc
@@ -14,14 +14,20 @@ int main() {
                << "<p>And here is some regular text."
                << "<p>Here is a variable: " << myvar;
 
-  UI::document << "<p>Here is an updating variable: " << UI::Var(myvar)
+  UI::document << UI::Text("my_text").Background("#DDDDFF").CSS("color", "#550055")
+               << "Is this text formatted?";
+
+  UI::document << UI::Text() << "<p>Here is an updating variable: " << UI::Var(myvar)
                << "<br>" << UI::Button(IncVar, "MyButton", "but");
 
-  UI::document << "<br>" << UI::Image("motivator.jpg").Width(600);
+
+  UI::document << UI::Slate("new_slate").CSS("width", 100) << "Testing out the new slate object with some wide text!";
+
+  UI::document << "<br>" << UI::Image("motivator.jpg").Width(600).Opacity(0.8);
 
   myvar = 100;
 
-  UI::document.Button("but").Height(50);
+  UI::document.Button("but").Height(50).Background("green");
 
   UI::document.Update();
 }
