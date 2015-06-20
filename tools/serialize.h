b8ffbb98dd9b9e294c9c46da64c12f03d9604750
Charles Ofria <charles.ofria@gmail.com>
2015-06-19 23:14:31 -0400

    Make serialize create custom constructor.

--- tools/serialize.h
+++ tools/serialize.h
@@ -9,16 +9,17 @@
 //  * Macro should setup a constructor that takes TextIO as an argument to build class.
 //  * Need second macro for derived classes so base class can be called recursively.
 //  * To deal with pointers we should recurse, but keep map to new pointer locations.
+//  * Add a mechanism to set value to constant rather than previous value.
 //
 
 #include <iostream>
 
 // Use this macro to automatically build methods in a class to save and load data.
-#define EMP_SETUP_SERIALIZE(...)                 \
+#define EMP_SETUP_SERIALIZE(CLASS_NAME, ...)     \
   void EMP_StoreText(std::ostream & os) {        \
     emp::serialize::StoreText(os, __VA_ARGS__);  \
   }                                              \
-  void EMP_LoadText(std::istream & is) {         \
+  CLASS_NAME(std::istream & is) {         \
     emp::serialize::LoadText(is, __VA_ARGS__);   \
   }
 
@@ -42,7 +43,7 @@ namespace serialize {
       static void LoadText(std::istream & is, FIRST_TYPE & arg1, OTHER_TYPES&... others) {
         char tmp_char;
         is >> tmp_char >> arg1;
-        std::cout << "[" << tmp_char << "]" << std::endl;
+        // std::cout << "[" << tmp_char << "]" << std::endl;
         serial_impl<OTHER_TYPES...>::LoadText(is, others...);
       }
     };
