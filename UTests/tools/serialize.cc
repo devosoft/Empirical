16260b88c2cbaba5ee91bbff40415b1fd40b3242
Charles Ofria <charles.ofria@gmail.com>
2015-06-19 23:13:40 -0400

    Converted serialize.cc to proper unit test.

--- UTests/tools/serialize.cc
+++ UTests/tools/serialize.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 #include "../../tools/assert.h"
 #include "../../tools/command_line.h"
@@ -9,8 +10,9 @@ struct SerializeTest {
   int a;
   float b;
   std::string c;
-
-  EMP_SETUP_SERIALIZE(a,c);
+  
+  SerializeTest(int _a, float _b, std::string _c) : a(_a), b(_b), c(_c) { ; }
+  EMP_SETUP_SERIALIZE(SerializeTest, a,c);
 };
 
 int main(int argc, char* argv[])
@@ -18,26 +20,19 @@ int main(int argc, char* argv[])
   std::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
   const bool verbose = emp::cl::use_arg(args, "-v");
 
-  SerializeTest st = { 7, 2.34, "my test string" };
-  st.EMP_StoreText(std::cout);
+  std::stringstream ss;
+  SerializeTest st(7, 2.34, "my test string");
+  st.EMP_StoreText(ss);
 
-  std::cout << std::endl << "Hello!" << std::endl;
+  if (verbose) {
+    std::cout << ss.str() << std::endl;
+  }
+
+  SerializeTest st2(ss);
+
+  emp_assert(st2.a == 7);
+
+  if (verbose) {
+    std::cout << st2.a << std::endl;
+  }
 }
-
-// class apple {
-// public:
-//   int a;
-//   bool isTasty;
-//   float unimportantData;
-  
-//   SET_SAVED_MEMBERS(a, isTasty);
-// };
-
-
-// int main() {
-//   apple a = {7, false, 2.34};
-//   a.isTasty=true;
-//   a.serialize("Test: ");
-
-// }
-
