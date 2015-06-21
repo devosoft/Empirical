cc53fa9cba1e279144b9685acd6a5d24e3575125
Charles Ofria <charles.ofria@gmail.com>
2015-06-21 09:31:25 -0400

    Serialize test now works correctly!

--- UTests/tools/serialize.cc
+++ UTests/tools/serialize.cc
@@ -15,24 +15,44 @@ struct SerializeTest {
   EMP_SETUP_SERIALIZE(SerializeTest, a,c);
 };
 
+struct SerializeTest_D :public SerializeTest {
+  char d = '$';
+
+  SerializeTest_D(int _a, float _b, std::string _c, char _d)
+    : SerializeTest(_a, _b, _c), d(_d) { ; }
+  EMP_SETUP_SERIALIZE_D(SerializeTest_D, SerializeTest, d);
+};
+
 int main(int argc, char* argv[])
 {
   std::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
   const bool verbose = emp::cl::use_arg(args, "-v");
 
   std::stringstream ss;
-  SerializeTest st(7, 2.34, "my test string");
-  st.EMP_StoreText(ss);
+  emp::serialize::TextIO io(ss);
+  SerializeTest st(7, 2.34, "my_test_string");
+  st.EMP_Store(io);
 
   if (verbose) {
-    std::cout << ss.str() << std::endl;
+    std::cout << "Finished save on base class.\nSaved stream: " << ss.str() << std::endl;
   }
 
-  SerializeTest st2(ss);
+  SerializeTest st2(io);
 
-  emp_assert(st2.a == 7);
+  emp_assert(st2.a == 7);      // Make sure a was reloaded correctly.
+  emp_assert(st2.c == "my_test_string");  // Make sure ss was fully emptied!
 
   if (verbose) {
-    std::cout << st2.a << std::endl;
+    std::cout << "a reloaded as: " << st2.a << std::endl;
+    std::cout << "c reloaded as: " << st2.c << std::endl;
+    std::cout << "b (not) reloaded as: " << st2.b << std::endl;
+    std::cout << "After reload, stream: " << ss.str() << std::endl;
+  }
+
+  SerializeTest_D stD(10,0.2,"three",'D');
+  stD.EMP_Store(io);
+
+  if (verbose) {
+    std::cout << "Finished save on derived class.\nSaved stream: " << ss.str() << std::endl;
   }
 }
