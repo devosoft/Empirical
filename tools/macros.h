407b7ee7270b3ad0a879babbac24d4a8676f9d32
Charles Ofria <charles.ofria@gmail.com>
2015-06-22 11:49:51 -0400

    Started a file for useful macros

---
+++ tools/macros.h
@@ -0,0 +1,13 @@
+#ifndef EMP_MACROS_H
+#define EMP_MACROS_H
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Generally useful macros that can perform cools tricks.  As with all macros, use only
+//  after careful exclusion of all alternatives!
+//
+
+#define EMP_COMMA_MERGE(A,B) A,B
+
+
+#endif
