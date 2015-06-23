5d69b8c9eb7da35b3bad995e0b7395b76adb03b9
Charles Ofria <charles.ofria@gmail.com>
2015-06-23 00:09:46 -0400

    Added arg count macros, arg accessors, and stringify macros

--- tools/macros.h
+++ tools/macros.h
@@ -4,10 +4,41 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 //
 //  Generally useful macros that can perform cools tricks.  As with all macros, use only
-//  after careful exclusion of all alternatives!
+//  after careful exclusion of alternative approaches!
 //
 
+#define EMP_COMMA ,
+
+// EMP_STRINGIFY takes any input, processes macros, and puts the result in quotes.
+#define EMP_STRINGIFY(A) EMP_STRINGIFY_IMPL(A)
+#define EMP_STRINGIFY_IMPL(A) #A
+
 #define EMP_COMMA_MERGE(A,B) A,B
+
+#define EMP_GET_ARG_1(A1, ...) A1
+#define EMP_GET_ARG_2(A1, A2, ...) A2
+#define EMP_GET_ARG_3(A1, A2, A3, ...) A3
+#define EMP_GET_ARG_4(A1, A2, A3, A4, ...) A4
+#define EMP_GET_ARG_5(A1, A2, A3, A4, A5, ...) A5
+#define EMP_GET_ARG_6(A1, A2, A3, A4, A5, A6, ...) A6
+#define EMP_GET_ARG_7(A1, A2, A3, A4, A5, A6, A7, ...) A7
+#define EMP_GET_ARG_8(A1, A2, A3, A4, A5, A6, A7, A8, ...) A8
+#define EMP_GET_ARG_9(A1, A2, A3, A4, A5, A6, A7, A8, A9, ...) A9
+#define EMP_GET_ARG_10(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, ...) A10
+#define EMP_GET_ARG_20(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, ...) A20
+#define EMP_GET_ARG_30(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25, A26, A27, A28, A29, A30, ...) A30
+#define EMP_GET_ARG_40(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25, A26, A27, A28, A29, A30, A31, A32, A33, A34, A35, A36, A37, A38, A39, A40, ...) A40
+#define EMP_GET_ARG_50(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25, A26, A27, A28, A29, A30, A31, A32, A33, A34, A35, A36, A37, A38, A39, A40, A41, A42, A43, A44, A45, A46, A47, A48, A49, A50, ...) A50
+#define EMP_GET_ARG_60(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25, A26, A27, A28, A29, A30, A31, A32, A33, A34, A35, A36, A37, A38, A39, A40, A41, A42, A43, A44, A45, A46, A47, A48, A49, A50, A51, A52, A53, A54, A55, A56, A57, A58, A59, A60, ...) A60
+#define EMP_GET_ARG_64(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25, A26, A27, A28, A29, A30, A31, A32, A33, A34, A35, A36, A37, A38, A39, A40, A41, A42, A43, A44, A45, A46, A47, A48, A49, A50, A51, A52, A53, A54, A55, A56, A57, A58, A59, A60, A61, A62, A63, A64...) A64
+
+#define EMP_COUNT_ARGS(...) EMP_GET_ARG_64(__VA_ARGS__, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+
+#define EMP_ASSEMBLE_MACRO(BASE, ARG_COUNT, ...) BASE ## ARG_COUNT(__VA_ARGS__)
+
+#define EMP_MERGE(...) EMP_ASSEMBLE_MACRO(EMP_MERGE_, EMP_COUNT_ARGS(__VA_ARGS__), __VA_ARGS__)
+#define EMP_MERGE_2(A,B) A ## B
+#define EMP_MERGE_3(A,B,C) A ## B ## C
 
 
 #endif
