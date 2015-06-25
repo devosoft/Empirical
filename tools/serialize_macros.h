63e6b5f8b8951b4edca86574260f1c380ccc1ca4
Charles Ofria <charles.ofria@gmail.com>
2015-06-25 13:01:58 -0400

    Moved serialize macros into their own file.

---
+++ tools/serialize_macros.h
@@ -0,0 +1,42 @@
+#ifndef EMP_SERIALIZE_MACROS_H
+#define EMP_SERIALIZE_MACROS_H
+
+#include "macros.h"
+
+#define EMP_SERIALIZE_INIT_VAR(VAR) VAR(emp::serialize::SetupLoad<decltype(VAR)>(pod))
+
+// Use this macro to automatically build methods in a class to save and load data.
+#define EMP_SETUP_DATAPOD_BASEINFO(CLASS_NAME, BASE_LOAD, BASE_STORE, ...) \
+  typedef emp::serialize::DataPod EMP_DataPod_t;                        \
+  void EMP_Store(emp::serialize::DataPod & pod) {                       \
+    BASE_STORE;                                                         \
+    emp::serialize::Store(pod, __VA_ARGS__);                            \
+  }                                                                     \
+  explicit CLASS_NAME(emp::serialize::DataPod & pod) BASE_LOAD {        \
+    emp::serialize::Load(pod, __VA_ARGS__);                             \
+  }
+
+
+// Version to use in stand-along classes.
+#define EMP_SETUP_DATAPOD(CLASS_NAME, ...) \
+  EMP_SETUP_DATAPOD_BASEINFO(CLASS_NAME, , , __VA_ARGS__)
+
+// Version to use in derived classes (with a base that also needs to be serialized).
+#define EMP_SETUP_DATAPOD_D(CLASS_NAME, BASE_CLASS, ...)    \
+  EMP_SETUP_DATAPOD_BASEINFO(CLASS_NAME,                    \
+                             :BASE_CLASS(pod),              \
+                             BASE_CLASS::EMP_Store(pod),    \
+                             __VA_ARGS__)
+
+// Version to use in derived classes (with TWO bases that need to be serialized).
+
+#define EMP_SETUP_DATAPOD_D2(CLASS_NAME, BASE_CLASS1, BASE_CLASS2, ...) \
+  EMP_SETUP_DATAPOD_BASEINFO(CLASS_NAME,                                \
+                             EMP_CALL_BASE_2(BASE_CLASS1, BASE_CLASS2), \
+                             BASE_CLASS1::EMP_Store(pod); BASE_CLASS2::EMP_Store(pod), \
+                             __VA_ARGS__)
+
+#define EMP_CALL_BASE_2(BASE1, BASE2) :BASE1(pod), BASE2(pod)
+
+
+#endif
