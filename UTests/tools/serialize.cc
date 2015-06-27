753269629d4b59552a9c923cf7555599be5cbb1d
Charles Ofria <charles.ofria@gmail.com>
2015-06-27 13:10:57 -0400

    Added substantial verbose comments for serialize unit test

--- UTests/tools/serialize.cc
+++ UTests/tools/serialize.cc
@@ -49,14 +49,14 @@ int main(int argc, char* argv[])
   st.EMP_Store(pod);
 
   if (verbose) {
-    std::cout << "Finished storing to DataPod for base class.\nSaved stream: "
-              << ss.str() << std::endl;
+    std::cout << "Finished storing to DataPod for base class.\n"
+              << "Saved stream: " << ss.str() << std::endl;
   }
 
   SerializeTest st2(pod);
 
   if (verbose) {
-    std::cout << "Reloaded DataPod for base class.\n  st2.a = " << st2.a
+    std::cout << "Reloaded DataPod for base class.  Results:\n  st2.a = " << st2.a
               << "\n  st2.b = " << st2.b << " (uninitialized)\n  st2.c = " << st2.c
               << std::endl;
   }
@@ -65,9 +65,6 @@ int main(int argc, char* argv[])
   emp_assert(st2.c == "my_test_string");  // Make sure c was reloaded correctly.
 
   if (verbose) {
-    std::cout << "a reloaded as: " << st2.a << std::endl;
-    std::cout << "c reloaded as: " << st2.c << std::endl;
-    std::cout << "b (not) reloaded as: " << st2.b << std::endl;
     std::cout << "After reload, stream: " << ss.str() << std::endl;
   }
 
@@ -75,14 +72,27 @@ int main(int argc, char* argv[])
   stD.EMP_Store(pod);
 
   if (verbose) {
-    std::cout << "Finished save on derived class.\nSaved stream: " << ss.str() << std::endl;
+    std::cout << "Finished store on derived class.\nSaved stream: " << ss.str() << std::endl;
   }
 
   SerializeTest_D stD2(pod);
 
+  if (verbose) {
+    std::cout << "Reloaded DataPod for derived class.  Results:\n"
+              << "  stD2.a = " << stD2.a << "\n"
+              << "  stD2.b = " << stD2.b << " (uninitialized)\n"
+              << "  stD2.c = " << stD2.c << "\n"
+              << "  stD2.d = " << stD2.d
+              << std::endl;
+  }
+
   emp_assert(stD2.a == 10);
   emp_assert(stD2.c == "three");
   emp_assert(stD2.d == 'D');
+
+  if (verbose) {
+    std::cout << "Reload successful!" << std::endl;
+  }
 
   MultiTest stM(111,2.22,"ttt",4.5,true);
   stM.EMP_Store(pod);
@@ -93,6 +103,16 @@ int main(int argc, char* argv[])
 
   MultiTest stM2(pod);
 
+  if (verbose) {
+    std::cout << "Reloaded DataPod for multi-derived class.  Results:\n"
+              << "  stM2.a = " << stM2.a << "\n"
+              << "  stM2.b = " << stM2.b << " (uninitialized)\n"
+              << "  stM2.c = " << stM2.c << "\n"
+              << "  stM2.e = " << stM2.e << "\n"
+              << "  stM2.f = " << stM2.f
+              << std::endl;
+  }
+
   emp_assert(stM2.a == 111);
   emp_assert(stM2.c == "ttt");
   emp_assert(stM2.e == 4.5);
@@ -101,7 +121,7 @@ int main(int argc, char* argv[])
 
   if (verbose) std::cout << "All reloads successful!!!" << std::endl;
 
-  int test_int;
-  emp::serialize::SetupLoad<int>(pod, &test_int, true);
-  emp::serialize::SetupLoad(pod, &stM2, true);
+  // int test_int;
+  // emp::serialize::SetupLoad<int>(pod, &test_int, true);
+  // emp::serialize::SetupLoad(pod, &stM2, true);
 }
