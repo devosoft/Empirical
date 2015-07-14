1ca7a1c5d055f17be9a9cad7eef679be8cb99908
Charles Ofria <charles.ofria@gmail.com>
2015-07-13 21:43:37 -0400

    Unit tests for emp::vector

---
+++ UTests/tools/vector.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../../tools/assert.h"
+#include "../../tools/command_line.h"
+#include "../../tools/vector.h"
+
+int main(int argc, char* argv[])
+{
+  std::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
+  const bool verbose = emp::cl::use_arg(args, "-v");
+
+  emp::vector<int> v(20);
+
+  for (int i = 0; i < 20; i++) {
+    v[i] = i * i;
+  }
+
+  int total = 0;
+  for (int i : v) {
+    total += i;
+  }
+
+  if (verbose) {
+    std::cout << "total = " << total << std::endl;
+  }
+
+  emp_assert(total == 2470);
+
+  return 0;
+}
