acc390531f81ed95abdf72f91b6ded30a35be1ac
Charles Ofria <charles.ofria@gmail.com>
2015-07-15 22:29:46 -0400

    Basic canvas test

--- UTests/UI/Canvas.cc
+++ UTests/UI/Canvas.cc
@@ -5,8 +5,17 @@ namespace UI = emp::UI;
 int main() {
 
   UI::Initialize();
+  
+  UI::document << "<h1>Testing Canvas Object!</h1>"
+               << UI::Image("motivator.jpg", "im").Size(200,200)
+               << UI::Canvas(300,300,"cvs").StrokeColor("blue");
 
-
+  auto & canvas = UI::document.Canvas("cvs");
+  
+  canvas.Rect(100,25,100,100, "red");
+  canvas.Circle(50, 50, 45, "green", "purple");
+  canvas.Rect(100,175,100,100, "yellow");
+  
   UI::document.Update();
 }
 
