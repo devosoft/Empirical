6608c8f5a4c02aca33b123daabb15e802573ebb7
Charles Ofria <charles.ofria@gmail.com>
2015-07-16 23:51:36 -0400

    Setup Change demonstration

--- UTests/UI/mini_evoke.cc
+++ UTests/UI/mini_evoke.cc
@@ -8,8 +8,8 @@
 
 namespace UI = emp::UI;
 
-int cw = 300;
-int ch = 300;
+double cw = 300.0;
+double ch = 300.0;
 
 using dBRAIN = int;
 using dBODY = emp::CircleBody2D<dBRAIN>;
@@ -25,15 +25,13 @@ UI::Document doc("emp_base");
 Evoke evoke(cw,ch);
 UI::Animate anim;
 
-void EvokeAnim() 
+void EvokeAnim(const UI::Animate &)
 {
-  // std::cerr << EMP_TRACK_STATUS << std::endl;
-  // emp::CappedAlert(10, EMP_TRACK_STATUS);
-
   evoke.physics.Update();
 
   // Test which organisms should replicate.
-  double repro_prob = 0.003;
+  //double repro_prob = 0.003;
+  double repro_prob = 0.01;
 
   auto & body_set = evoke.physics.GetBodySet();
 
@@ -62,29 +60,66 @@ void EvokeAnim()
   canvas.Refresh();
 }
 
+void EvokeAnim_Change(const UI::Animate & anim)
+{
+  evoke.physics.Update();
+
+  // Test which organisms should replicate.
+  double repro_prob = 0.015;
+
+  auto & body_set = evoke.physics.GetBodySet();
+
+  const int cur_color = 0;
+  const int alt_color = 230;
+
+  if (body_set.size()) {
+    for (auto * body : body_set) {
+      // Bodies that are reproducing cannot produce a second offspring until they are done.
+      // Bodies under pressure do not produce offspring.
+      if (body->IsReproducing() || body->GetPressure() > 1.0) continue;
+      if (evoke.random.P(repro_prob) || body_set.size() == 1) {
+        emp::Angle repro_angle(evoke.random.GetDouble(2.0 * emp::PI));
+        auto * new_body = body->BuildOffspring( repro_angle.GetPoint(0.1) );
+
+        int cur_time = anim.GetRunTime() / 1000;
+        if (anim.GetCurTime() > 20 && (cur_time/10 % 2) ) {
+          new_body->SetColorID(alt_color);
+        }
+        else {
+          new_body->SetColorID( cur_color );
+        }
+        evoke.physics.AddBody( new_body );
+      }
+    }
+  }
+
+  auto & canvas = doc.Canvas("can");
+  emp::UI::Draw(canvas, evoke.physics.GetSurface(), emp::GetHueMap(360));
+  
+  canvas.Refresh();
+}
+
 int main() {
   UI::Initialize();
 
   // Add bodies to the physics...
-  for (int i = 0; i < 300; i++) {
-    auto new_circle = emp::Circle<>(evoke.random.GetDouble(cw), evoke.random.GetDouble(ch), 7);
-    dBODY * new_body = new dBODY( new_circle );
-    new_body->SetColorID(evoke.random.GetInt(360));  // Set color to random hue.
-    evoke.physics.AddBody( new_body );
-  }
+  // for (int i = 0; i < 300; i++) {
+  //   auto new_circle = emp::Circle<>(evoke.random.GetDouble(cw), evoke.random.GetDouble(ch), 7);
+  //   dBODY * new_body = new dBODY( new_circle );
+  //   new_body->SetColorID(evoke.random.GetInt(360));  // Set color to random hue.
+  //   evoke.physics.AddBody( new_body );
+  // }
 
-  anim.SetCallback(EvokeAnim);
+  auto new_circle = emp::Circle<>(cw/2.0, ch/2.0, 7);
+  dBODY * new_body = new dBODY( new_circle );
+  new_body->SetColorID( 0 );  // Set color to random hue.
+  evoke.physics.AddBody( new_body );
+
+  anim.SetCallback(EvokeAnim_Change);
 
   auto & canvas = doc.AddCanvas(cw, ch, "can");
   doc << "<br>";
-  doc.AddButton([](){
-      anim.ToggleActive();
-      auto & but = doc.Button("toggle");
-      if (anim.GetActive()) but.Label("Pause");
-      else but.Label("Start");
-      but.Update();
-    }, "Start", "toggle").Size(50,30);
-
+  doc.AddAnimToggle(anim);
   doc.Update();
 
   // Draw the surface on a new canvas!
