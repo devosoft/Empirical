e3119f11acb0b7029e15ccccc10e5f1e7ca926c5
Charles Ofria <charles.ofria@gmail.com>
2015-07-22 05:09:04 -0400

    Added functions to generate color strings by HSL, RGB, or RGBa

--- emtools/color_map.h
+++ emtools/color_map.h
@@ -6,6 +6,7 @@
 //  Tools to dynamically build (and cache) color maps.
 //
 
+#include <iomanip>
 #include <map>
 #include <string>
 #include <tuple>
@@ -21,7 +22,39 @@ namespace emp {
 
     dHueMap hue_maps;
   }
-  
+
+  // Generate a string to describe a JS color out of HSL values.
+  std::string ColorHSL(double h, double s, double l) {
+    emp_assert(h >= 0 && h <= 360);
+    emp_assert(s >= 0 && s <= 100);
+    emp_assert(l >= 0 && l <= 100);
+    std::stringstream ss;
+    ss << "hsl(" <<  h << ',' << s << "%," <<  l << "%)";
+    return ss.str();
+  }
+
+  // Generate a string to describe a JS color out of RGB values.
+  std::string ColorRGB(int r, int g, int b) {
+    emp_assert(r >= 0 && r < 255);
+    emp_assert(g >= 0 && g < 255);
+    emp_assert(b >= 0 && b < 255);
+    std::stringstream ss;
+    ss << '#' << std::setw(2) << std::setfill('0') << std::hex << r
+       << std::setw(2) << std::setfill('0') << std::hex << g
+       << std::setw(2) << std::setfill('0') << std::hex << b;
+    return ss.str();
+  }
+
+  // Generate a string to describe a JS color with an alpha channel.
+  std::string ColorRGB(int r, int g, int b, double a) {
+    emp_assert(r >= 0 && r < 255);
+    emp_assert(g >= 0 && g < 255);
+    emp_assert(b >= 0 && b < 255);
+    std::stringstream ss;
+    ss << "rgba(" << r << ',' << g << ',' << b << ',' << a << ')';    
+    return ss.str();
+  }
+
   const emp::vector<std::string> &
   GetHueMap(int map_size, double min_h=0.0, double max_h=360.0, int s=100, int l=50) {
     internal::dHueMapKey map_key = std::make_tuple(map_size, min_h, max_h, s, l);
