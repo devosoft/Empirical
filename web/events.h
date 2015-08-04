896c13a45e7083f37e03fbd3c5a0bd0c72d1b23c
Charles Ofria <charles.ofria@gmail.com>
2015-08-03 22:00:16 -0400

    Added OnDocumentReady function; place for other events

---
+++ web/events.h
@@ -0,0 +1,25 @@
+#ifndef EMP_WEB_EVENTS_H
+#define EMP_WEB_EVENTS_H
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Event handlers that use JQuerry.
+//
+
+#include <utility>
+
+#include "../emtools/html5_events.h"
+
+namespace emp {
+namespace web {
+
+  template <typename FUN_TYPE> void OnDocumentReady(FUN_TYPE && fun) {
+    // const int fun_id = JSWrapOnce(fun);
+    const int fun_id = JSWrap(std::forward<FUN_TYPE>(fun), "", true);
+    EM_ASM_ARGS({  $( document ).ready(function() { emp.Callback($0); });  }, fun_id);
+  }
+ 
+};
+};
+
+#endif
