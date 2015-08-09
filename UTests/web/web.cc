f198c0830c914ee1614b8f5dc301c11171ecdfe7
Charles Ofria <charles.ofria@gmail.com>
2015-08-08 21:50:53 -0400

    Added several new tests for web framework Widgets

--- UTests/web/web.cc
+++ UTests/web/web.cc
@@ -8,12 +8,38 @@ UI::Document doc("emp_base");
 std::string TestFun() { return "abcd"; }
 
 int x = 20;
+void IncVar() { x++; }
 
 int main()
 {
-  doc << "Testing testing!!!"
-      << "<br>" << std::function<std::string()>(TestFun)
-      << "<br>" << x;
+  UI::Text text("my_text");
 
-  doc << UI::Button([](){ x++; }, "Test");
+  UI::Image motiv("../UI/motivator.jpg");
+
+
+
+  text << "Testing testing!!!"
+       << "<br>" << std::function<std::string()>(TestFun)
+       << "<br>" << [](){ return emp::to_string(x); };
+
+  doc << text << "<br>" << UI::Button(IncVar, "Test", "my_but") << "<br>" << motiv;
+
+  UI::Button my_but = doc.FindButton("my_but");
+  UI::Canvas canvas(200, 200);
+  UI::Selector sel("sel");
+
+  sel.SetOption("Option 1");
+  sel.SetOption("Option B");
+  sel.SetOption("Option the Forth");
+  sel.SetOption("Option IV");
+
+  doc << canvas << sel;
+  canvas.Rect(0,0,200,200, "#AAAAAA", "black");
+  canvas.Circle(100,75,50, "red", "black");
+
+  my_but.AddDependent(text);
+  my_but.Size(100,100);
+  my_but.Background("blue");
+
+  motiv.Size(100,100);
 }
