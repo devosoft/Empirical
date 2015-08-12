8841a8a64b52c497fad7d4cbc94cee1feae7677a
Charles Ofria <charles.ofria@gmail.com>
2015-08-11 23:15:05 -0400

    Added Add shortcuts to document, lookups for specific widget types, and ported Live() function

--- web/web.h
+++ web/web.h
@@ -44,14 +44,86 @@ namespace web {
   //  A Document is a root-level slate that automatically initializes Empirical and starts
   //  out in an active state.
 
-  class Document : public Slate {
+  class Document : public web::Slate {
   public:
-    Document(const std::string & doc_id) : Slate(doc_id) {
+    Document(const std::string & doc_id) : web::Slate(doc_id) {
       emp::web::Initialize();
       Activate();
     }
     ~Document() { ; }
+
+    // Retrieve specific types of widgets.
+
+    // Shortcut adders for Widgets
+    template <class... T> web::Button AddButton(T... args){
+      web::Button new_widget(std::forward<T>(args)...);
+      info->Append(new_widget);
+      return new_widget;
+    }
+    template <class... T> web::Canvas AddCanvas(T... args){
+      web::Canvas new_widget(std::forward<T>(args)...);
+      info->Append(new_widget);
+      return new_widget;
+    }
+    template <class... T> web::Image AddImage(T... args) {
+      web::Image new_widget(std::forward<T>(args)...);
+      info->Append(new_widget);
+      return new_widget;
+    }
+    template <class... T> web::Selector AddSelector(T... args){
+      web::Selector new_widget(std::forward<T>(args)...);
+      info->Append(new_widget);
+      return new_widget;
+    }
+    template <class... T> web::Slate AddSlate(T... args) {
+      web::Slate new_widget(std::forward<T>(args)...);
+      info->Append(new_widget);
+      return new_widget;
+    }
+    template <class... T> web::Table AddTable(T... args) {
+      web::Table new_widget(std::forward<T>(args)...);
+      info->Append(new_widget);
+      return new_widget;
+    }
+    template <class... T> web::Text AddText(T... args)  {
+      web::Text new_widget(std::forward<T>(args)...);
+      info->Append(new_widget);
+      return new_widget;
+    }
+
+    // Setup a quick way to retrieve old widgets by name.
+    Button &   Button  (const std::string & in_id) { return (web::Button &)   Find(in_id); }
+    Canvas &   Canvas  (const std::string & in_id) { return (web::Canvas &)   Find(in_id); }
+    Image &    Image   (const std::string & in_id) { return (web::Image &)    Find(in_id); }
+    Selector & Selector(const std::string & in_id) { return (web::Selector &) Find(in_id); }
+    Slate &    Slate   (const std::string & in_id) { return (web::Slate &)    Find(in_id); }
+    Table &    Table   (const std::string & in_id) { return (web::Table &)    Find(in_id); }
+    Text &     Text    (const std::string & in_id) { return (web::Text &)     Find(in_id); }
+
   };
+
+
+  // Some helper functions.
+  // Live keyword means that whatever is passed in needs to be re-evaluated every update.
+  namespace internal {
+
+    // If a variable is passed in to live, construct a function to look up its current value.
+    template <typename VAR_TYPE>
+    std::function<std::string()> Live_impl(VAR_TYPE & var, bool) {
+      return [&var](){ return emp::to_string(var); };
+    }
+    
+    // If anything else is passed in, assume it is a function!
+    template <typename IN_TYPE>
+    std::function<std::string()> Live_impl(IN_TYPE && fun, int) {
+      return [fun](){ return emp::to_string(fun()); };
+    }
+  }
+
+  template <typename T>
+  std::function<std::string()> Live(T && val) {
+    return internal::Live_impl(std::forward<T>(val), true);
+  }
 
 };
 };
