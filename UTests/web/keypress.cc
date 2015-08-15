57fa20f032631120617af7bc208e2cf5ee6d106b
Charles Ofria <charles.ofria@gmail.com>
2015-08-14 22:21:24 -0400

    Filled out keypress.cc into proper test of object

--- UTests/web/keypress.cc
+++ UTests/web/keypress.cc
@@ -1,9 +1,34 @@
+#include "../../web/keypress.h"
 #include "../../web/web.h"
 
 namespace web = emp::web;
 web::Document doc("emp_base");
+web::KeypressManager keypress_manager;
+std::string other_str;
+
+int x = 0;
+
+bool OtherKey(const emp::html5::KeyboardEvent & evt)
+{
+  x=0;
+  other_str = "Why would you press ";
+  if (evt.altKey) other_str += "[ALT]";
+  if (evt.ctrlKey) other_str += "[CTRL]";
+  if (evt.metaKey) other_str += "[META]";
+  if (evt.shiftKey) other_str += "[SHIFT]";
+  other_str += (char) evt.keyCode;
+  other_str += "???";
+  doc.Redraw();
+
+  return true;
+}
 
 int main()
 {
-  doc << "Testing!";
+  doc << "Press X or Z!  " << web::Live(x)
+      << "<br>" << web::Live(other_str);
+
+  keypress_manager.AddKeydownCallback('X', [](){ x=100; other_str=""; doc.Redraw(); });
+  keypress_manager.AddKeydownCallback('Z', [](){ x=5; other_str=""; doc.Redraw(); });
+  keypress_manager.AddKeydownCallback(&OtherKey);
 }
