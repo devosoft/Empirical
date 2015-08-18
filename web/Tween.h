6c994b86c6e0a43d939839a732486c2d820e9eb3
Charles Ofria <charles.ofria@gmail.com>
2015-08-17 22:28:02 -0400

    Tweens now work properly

--- web/Tween.h
+++ web/Tween.h
@@ -6,6 +6,8 @@
 //  This class manages the gradual movement of a widget from one state to another.
 //
 
+#include "../emtools/emfunctions.h"
+
 #include "Widget.h"
 
 
@@ -19,6 +21,11 @@ namespace web {
       double start_val;                      // What value should the path begin with?
       double end_val;                        // What value should the path end with?
       std::function<double(double)> timing;  // Time dilation to use (e.g., to ease in and out)
+
+      void Set(const double frac) {
+        const double cur_val = (end_val - start_val) * timing(frac) + start_val;
+        set_fun(cur_val);
+      }
     };
 
     struct Event {
@@ -26,16 +33,55 @@ namespace web {
       std::function<void()> trigger;
     };
 
-    double duration;
-    Widget default_target;
-    emp::vector<Path*> paths;
-    emp::vector<Event*> events;
+    double duration;                 // How long should this Tween take?
+    Widget default_target;           // Allow paths to be added w/o specifying widget each time.
+    emp::vector<Path*> paths;        // Paths to be updated as part of this tween.
+    emp::vector<Event*> events;      // Events to be triggered as specific steps.
+    emp::vector<Widget> dependants;  // Widgets to be refreshed at each frame.
+
+    bool running;
+    int callback_id;
+
+    double start_time;  // When did the most recent run start?
+    double cur_time;    // What time is it now?
+    double run_time;    // How long did this previously run?
 
     static double LINEAR(double in) { return in; }
 
+    void AdvanceFrame() {
+      if (!running) return;             // If Stop has been called, halt animating.
+
+      cur_time = emp::GetTime();
+      double frac = (cur_time - start_time + run_time) / duration;
+
+      // If we're done, make sure we go to the last frame and stop!
+      if (frac > 1.0) { frac = 1.0; running = false; }
+
+      //@CAO test if any events should be triggered...
+
+      // Loop through each path and adjust accordingly...
+      for (auto * p : paths) {
+        p->Set(frac);
+      }
+
+      // Loop through all dependants be redrawn and do so.
+      for (auto & w : dependants) { w.Redraw(); }
+
+      // Setup the callback for the next frame of the Tween.
+      EM_ASM_ARGS({
+          requestAnimFrame(function() { emp.Callback($0); });
+        }, callback_id);
+    }
+
   public:
-    Tween(double d, const Widget & t=nullptr) : duration(d), default_target(t) { ; }
-    ~Tween() {
+    Tween(double d=1.0, const Widget & t=nullptr)
+      : duration(d*1000), default_target(t), running(false)
+      , start_time(0.0), cur_time(0.0), run_time(0.0)
+    {
+      callback_id = JSWrap( std::function<void()>([this](){ this->AdvanceFrame(); }) );
+    }
+    ~Tween()
+    {
       for (auto * p : paths) delete p;
       for (auto * e : events) delete e;
     }
@@ -43,13 +89,33 @@ namespace web {
     double GetDuration() const { return duration; }
     Widget GetDefaultTarget() const { return default_target; }
     
-    void AddPath(double duration, std::function<void(double)> set_fun,
+    Tween & SetDuration(double d) { duration = d*1000; return *this; }
+    Tween & SetDefaultTarget(const Widget & w) { default_target = w; return *this; }
+
+    Tween & AddPath(std::function<void(double)> set_fun,
                  double start_val, double end_val, std::function<double(double)> timing=LINEAR) {
       Path * new_path = new Path({set_fun, start_val, end_val, timing});
       paths.push_back(new_path);
+      return *this;
     }
 
-    void AddPath(double duration, Widget w, std::string setting, double start_val, double end_val) {
+    Tween & AddPath(Widget w, std::string setting, double start_val, double end_val) {
+      emp_assert(false && "need to fill in!");
+      return *this;
+    }
+
+    Tween & AddDependant(Widget w) { dependants.push_back(w); return *this; }
+
+    void Start() {
+      if (running) return;          // Already running!
+      running = true;
+      start_time = emp::GetTime();  // Record the time that we started the animation.
+      cur_time = start_time;        // Initialize cur_time to now.
+      AdvanceFrame();               // Take the first animation step to get going.
+    }
+    void Stop() {
+      run_time += emp::GetTime() - start_time;
+      running = false;
     }
   };
 
