433232bee4790513358c7c40981baf8c94761db0
Charles Ofria <charles.ofria@gmail.com>
2015-09-18 15:20:08 -0400

    We can now use EMP_ADD on any values 0 to 255!

--- UTests/tools/macro_math.cc
+++ UTests/tools/macro_math.cc
@@ -12,8 +12,8 @@ int main(int argc, char* argv[])
   const bool verbose = emp::cl::use_arg(args, "-v");
 
   // Test converting between binary and decimal.
-  EMP_TEST_MACRO( EMP_VAL_TO_BIN(9), "1, 0, 0, 1");
-  EMP_TEST_MACRO( EMP_BIN_TO_VAL(1,0,1,1), "11");
+  EMP_TEST_MACRO( EMP_VAL_TO_BIN(9), "0, 0, 0, 0, 1, 0, 0, 1");
+  EMP_TEST_MACRO( EMP_BIN_TO_VAL(0,0,0,0,1,0,1,1), "11");
 
   // Test simple math.
   EMP_TEST_MACRO( EMP_COUNT_BITS(1, 1), "2");
@@ -26,8 +26,9 @@ int main(int argc, char* argv[])
   EMP_TEST_MACRO( EMP_ADD(1, 2), "3");
   EMP_TEST_MACRO( EMP_ADD(5, 5), "10");
   EMP_TEST_MACRO( EMP_ADD(7, 7), "14");
-  EMP_TEST_MACRO( EMP_ADD(7, 1), "8");
-  EMP_TEST_MACRO( EMP_ADD(15, 1), "0");
+  EMP_TEST_MACRO( EMP_ADD(111, 112), "223");
+  EMP_TEST_MACRO( EMP_ADD(127, 1), "128");
+  EMP_TEST_MACRO( EMP_ADD(255, 1), "0");
 
   if (verbose) std::cout << "All tests passed." << std::endl;
 }
