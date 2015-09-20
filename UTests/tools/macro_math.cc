3666184a191996233c16a24b5054790018c39162
Charles Ofria <charles.ofria@gmail.com>
2015-09-20 16:35:08 -0400

    Restored unit tests to work with 10-bit macro_math (+ new tests)

--- UTests/tools/macro_math.cc
+++ UTests/tools/macro_math.cc
@@ -12,29 +12,28 @@ int main(int argc, char* argv[])
   const bool verbose = emp::cl::use_arg(args, "-v");
 
   // Test converting between binary, decimal, and sum formats.
-  EMP_TEST_MACRO( EMP_DEC_TO_BIN(9), "0, 0, 0, 0, 1, 0, 0, 1");
-  EMP_TEST_MACRO( EMP_DEC_TO_BIN(91), "0, 1, 0, 1, 1, 0, 1, 1");
-  EMP_TEST_MACRO( EMP_BIN_TO_DEC(0,0,0,0,1,0,1,1), "11");
-  EMP_TEST_MACRO( EMP_BIN_TO_DEC(0,1,0,1,1,0,1,1), "91");
-  EMP_TEST_MACRO( EMP_BIN_TO_SUM(0,1,0,1,1,0,1,1), "0, 64, 0, 16, 8, 0, 2, 1");
-  EMP_TEST_MACRO( EMP_DEC_TO_SUM(91), "0, 64, 0, 16, 8, 0, 2, 1");
+  EMP_TEST_MACRO( EMP_DEC_TO_BIN(9), "0, 0, 0, 0, 0, 0, 1, 0, 0, 1");
+  EMP_TEST_MACRO( EMP_DEC_TO_BIN(91), "0, 0, 0, 1, 0, 1, 1, 0, 1, 1");
+  EMP_TEST_MACRO( EMP_DEC_TO_BIN(999), "1, 1, 1, 1, 1, 0, 0, 1, 1, 1");
 
-//   DEC - Standard decimal values (e.g., 91)
-//   BIN - Binary numbers, with bits separated by commas (e.g.  0,  1, 0,  1, 1, 0, 1, 1 )
-//   SUM - Like BIN, but stored as zero or magnitude.    (e.g., 0, 64, 0, 16, 8, 0, 2, 1
+  EMP_TEST_MACRO( EMP_BIN_TO_DEC(0,0,0,0,0,0,1,0,1,1), "11");
+  EMP_TEST_MACRO( EMP_BIN_TO_DEC(0,0,0,1,0,1,1,0,1,1), "91");
+  EMP_TEST_MACRO( EMP_BIN_TO_DEC(1,0,1,0,1,0,1,0,1,0), "682");
 
+  EMP_TEST_MACRO( EMP_BIN_TO_SUM(0,0,0,1,0,1,1,0,1,1), "0, 0, 0, 64, 0, 16, 8, 0, 2, 1");
+  EMP_TEST_MACRO( EMP_DEC_TO_SUM(91), "0, 0, 0, 64, 0, 16, 8, 0, 2, 1");
 
-  // Test other helper functions.
+  // Test other helper math functions.
   EMP_TEST_MACRO( EMP_MATH_VAL_TIMES_0(222), "0" );
   EMP_TEST_MACRO( EMP_MATH_VAL_TIMES_1(222), "222" );
 
-  EMP_TEST_MACRO( EMP_MATH_BIN_TIMES_0(1,0,1,0,1,0,1,0), "0, 0, 0, 0, 0, 0, 0, 0" );
-  EMP_TEST_MACRO( EMP_MATH_BIN_TIMES_1(1,0,1,0,1,0,1,0), "1, 0, 1, 0, 1, 0, 1, 0" );
+  EMP_TEST_MACRO( EMP_MATH_BIN_TIMES_0(0,0,1,0,1,0,1,0,1,0), "0, 0, 0, 0, 0, 0, 0, 0, 0, 0" );
+  EMP_TEST_MACRO( EMP_MATH_BIN_TIMES_1(0,0,1,0,1,0,1,0,1,0), "0, 0, 1, 0, 1, 0, 1, 0, 1, 0" );
 
-  // Test simple math.
   EMP_TEST_MACRO( EMP_MATH_COUNT_BITS(1, 1), "2");
   EMP_TEST_MACRO( EMP_MATH_COUNT_BITS(1, N), "0");
   EMP_TEST_MACRO( EMP_MATH_COUNT_BITS(0, N), "N");
+
   EMP_TEST_MACRO( EMP_MATH_GET_CARRY(2), "1");
   EMP_TEST_MACRO( EMP_MATH_CLEAR_CARRY(2), "0");
   
@@ -47,20 +46,25 @@ int main(int argc, char* argv[])
   EMP_TEST_MACRO( EMP_ADD(7, 7), "14");
   EMP_TEST_MACRO( EMP_ADD(111, 112), "223");
   EMP_TEST_MACRO( EMP_ADD(127, 1), "128");
-  EMP_TEST_MACRO( EMP_ADD(255, 1), "0");
+  EMP_TEST_MACRO( EMP_ADD(123, 789), "912");
+  EMP_TEST_MACRO( EMP_ADD(1023, 1), "0");      // Overflow
 
   // Basic Subtraction...
   EMP_TEST_MACRO( EMP_SUB(10, 7), "3");
   EMP_TEST_MACRO( EMP_SUB(128, 1), "127");
   EMP_TEST_MACRO( EMP_SUB(250, 250), "0");
+  EMP_TEST_MACRO( EMP_SUB(250, 100), "150");
+  EMP_TEST_MACRO( EMP_SUB(91, 66), "25");
+  EMP_TEST_MACRO( EMP_SUB(99, 100), "1023");   // Underflow
 
   // Combination of add and sub
   EMP_TEST_MACRO( EMP_ADD( EMP_SUB(250, 100), EMP_SUB(91, 66)), "175");
 
   // Shifting
   EMP_TEST_MACRO( EMP_SHIFTL(17), "34");
-  EMP_TEST_MACRO( EMP_SHIFTL(111), "222");
-  EMP_TEST_MACRO( EMP_SHIFTL(255), "254");   // Relies on overflow...
+  EMP_TEST_MACRO( EMP_SHIFTL(111), "222");  
+  EMP_TEST_MACRO( EMP_SHIFTL(444), "888");  
+  EMP_TEST_MACRO( EMP_SHIFTL(1023), "1022");   // Overflow...
 
   EMP_TEST_MACRO( EMP_SHIFTR(100), "50");
   EMP_TEST_MACRO( EMP_SHIFTR(151), "75");
@@ -69,12 +73,17 @@ int main(int argc, char* argv[])
   EMP_TEST_MACRO( EMP_INC(20), "21");
   EMP_TEST_MACRO( EMP_INC(55), "56");
   EMP_TEST_MACRO( EMP_INC(63), "64");
+  EMP_TEST_MACRO( EMP_INC(801), "802");
+
   EMP_TEST_MACRO( EMP_DEC(20), "19");
   EMP_TEST_MACRO( EMP_DEC(55), "54");
   EMP_TEST_MACRO( EMP_DEC(63), "62");
+  EMP_TEST_MACRO( EMP_DEC(900), "899");
+
   EMP_TEST_MACRO( EMP_HALF(17), "8");
   EMP_TEST_MACRO( EMP_HALF(18), "9");
   EMP_TEST_MACRO( EMP_HALF(60), "30");
+  EMP_TEST_MACRO( EMP_HALF(1001), "500");
 
   // Multiply!
   EMP_TEST_MACRO( EMP_MULT(1, 1), "1");
@@ -83,6 +92,7 @@ int main(int argc, char* argv[])
   EMP_TEST_MACRO( EMP_MULT(10, 7), "70");
   EMP_TEST_MACRO( EMP_MULT(25, 9), "225");
   EMP_TEST_MACRO( EMP_MULT(65, 3), "195");
+  EMP_TEST_MACRO( EMP_MULT(65, 15), "975");
 
 
   if (verbose) std::cout << "All tests passed." << std::endl;
