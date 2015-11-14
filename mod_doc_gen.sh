e202180db87198d649d3afa8595362ef8eec11cf
Jake Fenton <bocajnotnef@gmail.com>
2015-11-13 19:32:07 -0500

    Updated docs to have docs for all the modules

--- mod_doc_gen.sh
+++ mod_doc_gen.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ls "$1/*" > file_list
-python doc/gen_module_docfile.py
-#rm file_list
+# invoked like: ./mod_doc_gen.sh web doc/library/web.rst
+# from the root dir of empirical
+# (gotta get relative paths right for doxygen to be happy)
 
+dir="$1*.h"
+ls $dir > file_list
+python doc/gen_module_docfile.py
+rm file_list
 mv docfile.rst $2
+echo "Done!"
