ccc97d9f49d0ad9e10c75bf6addca7adf03fd204
Charles Ofria <charles.ofria@gmail.com>
2016-02-04 17:46:55 -0500

    Implemented a range of methods to determine fitness in an NK Landscape

--- EA/NK.h
+++ EA/NK.h
@@ -10,6 +10,8 @@
 
 #include <array>
 
+#include "../tools/assert.h"
+#include "../tools/BitSet.h"
 #include "../tools/const_utils.h"
 #include "../tools/Random.h"
 
@@ -19,7 +21,8 @@ namespace EA {
   template <int N, int K>
   class NKLandscape {
   private:
-    static constexpr int state_count = emp::constant::IntPow(2,K);
+    static constexpr int state_count = emp::constant::IntPow(2,K+1);
+    static constexpr int total_count = N * state_count;
     std::array< std::array<double, state_count>, N > landscape;
 
   public:
@@ -34,8 +37,29 @@ namespace EA {
     constexpr int GetN() const { return N; }
     constexpr int GetK() const { return K; }
     constexpr int GetStateCount() const { return state_count; }
+    constexpr int GetTotalCount() const { return total_count; }
+    
+    double GetFitness(int n, uint32_t state) {
+      emp_assert(state < state_count);
+      return landscape[n][state];
+    }
+    double GetFitness( std::array<uint32_t, N> states ) {
+      double total = landscape[0][states[0]];
+      for (int i = 1; i < N; i++) total += GetFitness(i,states[i]);
+      return total;
+    }
+    double GetFitness(const BitSet<N> & genome) {
+      // Create a double-length genome to easily handle wrap-around.
+      BitSet<N*2> genome2( genome.template Export<N*2>() );
+      genome2 |= (genome2 << N);
 
-    double Get(int n, uint32_t state) { return landscape[n][state]; }
+      double total = 0;
+      constexpr uint32_t mask = emp::constant::MaskLow<uint32_t>(K+1);
+      for (int i = 0; i < N; i++) {
+	total += GetFitness(i, (genome2 >> i) & mask);
+      }
+      return total;
+    }
   };
   
 }
