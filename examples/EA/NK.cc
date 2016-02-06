4ce5d76b06d9f3253489f076cba4e250e14b0628
Charles Ofria <charles.ofria@gmail.com>
2016-02-06 18:43:50 -0500

    Tuned NK example to explore more interesting evolution on NK landscapes.

--- examples/EA/NK.cc
+++ examples/EA/NK.cc
@@ -12,8 +12,8 @@
 #include "../../tools/BitSet.h"
 #include "../../tools/Random.h"
 
-constexpr int K = 2;
-constexpr int N = 10;
+constexpr int K = 3;
+constexpr int N = 50;
 
 constexpr int POP_SIZE = 100;
 constexpr int UD_COUNT = 100;
@@ -23,7 +23,7 @@ using BitOrg = emp::BitSet<N>;
 int main()
 {
   emp::Random random;
-  emp::EA::NKLandscape<N,K> landscape;
+  emp::EA::NKLandscape<N,K> landscape(random);
   emp::EA::Population<BitOrg> pop;
 
   // Build a random initial population
@@ -35,23 +35,26 @@ int main()
 
   // Loop through updates
   for (int ud = 0; ud < UD_COUNT; ud++) {
+    // Print current state.
+    // for (int i = 0; i < pop.GetSize(); i++) std::cout << pop[i] << std::endl;
+    // std::cout << std::endl;
+    std::cout << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
+
     // Keep the best individual.
     pop.EliteSelect([landscape](BitOrg * org){ return landscape.GetFitness(*org); }, 1);
 
     // Run a tournament for the rest...
     pop.TournamentSelect([landscape](BitOrg * org){ return landscape.GetFitness(*org); }
-			 , 5, random, POP_SIZE-1);
+			 , 2, random, POP_SIZE-1);
     pop.Update();
 
     // Do mutations...
     for (int i = 1; i < pop.GetSize(); i++) {
       pop[i][random.GetInt(N)] = random.P(0.5);
+      pop[i][random.GetInt(N)] = random.P(0.5);
+      pop[i][random.GetInt(N)] = random.P(0.5);
     }
 
-    // Print results
-    // for (int i = 0; i < pop.GetSize(); i++) std::cout << pop[i] << std::endl;
-    // std::cout << std::endl;
-    std::cout << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
   }
   
 
