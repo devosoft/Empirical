820a85361dbfc49885f8cfded3c46c7205a04eab
Charles Ofria <charles.ofria@gmail.com>
2016-02-13 09:16:09 -0500

    Setup a test file for Symbulation

---
+++ examples/evo/Symbulation.cc
@@ -0,0 +1,23 @@
+//  This file is part of Empirical, https://github.com/mercere99/Empirical/
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  This file explores the Symbulation setup.
+
+#include <iostream>
+
+#include "../../evo/Symbulation.h"
+#include "../../evo/Population.h"
+#include "../../tools/Random.h"
+
+constexpr int POP_SIZE = 1000;
+constexpr int UD_COUNT = 1000;
+
+
+int main()
+{
+  emp::Random random;
+  emp::evo::Population<emp::evo::SymbulationOrg> pop;
+
+}
