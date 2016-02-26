ebdffa25a2b751f825584154963e67b35bc435fc
Charles Ofria <charles.ofria@gmail.com>
2016-02-25 23:23:18 -0500

    Setup organisms signals to work more smoothly with populations.

--- evo/OrgSignals.h
+++ evo/OrgSignals.h
@@ -1,4 +1,4 @@
-//  This file is part of Empirical, https://github.com/mercere99/Empirical/
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
 //  Copyright (C) Michigan State University, 2016.
 //  Released under the MIT Software license; see doc/LICENSE
 //
@@ -16,11 +16,20 @@
 namespace emp {
 namespace evo {
 
-  struct OrgSignals {    
-    Signal<> repro_sig;
+  struct OrgSignals_NONE {
+    static std::string GetName() { return "OrgSignals_NONE"; }
 
-    OrgSignals(const std::string & sig_prefix)
-      : repro_sig(to_string(sig_prefix, ":repro"))
+    OrgSignals_NONE(const std::string & sig_prefix) { ; }
+  };
+  
+  struct OrgSignals_Basic {
+    static std::string GetName() { return "OrgSignals_Basic"; }
+
+    Signal<> repro_sig;
+    std::string prefix;
+
+    OrgSignals_Basic(const std::string & sig_prefix)
+      : repro_sig(to_string(sig_prefix, ":repro")), prefix(sig_prefix)
     { ; }
   };
 
