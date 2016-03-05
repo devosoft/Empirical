5886b3619eca79961036f8b55dd60130430ea5d6
Charles Ofria <charles.ofria@gmail.com>
2016-03-05 12:30:35 -0500

    Removed tuple struct tests as development is going in a different direction now.

--- tests/test_tools.cc
+++ tests/test_tools.cc
@@ -4,6 +4,7 @@
 #undef NDEBUG
 #define TDEBUG 1
 
+#include <array>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -1005,97 +1006,6 @@ TEST_CASE("Test trait", "[tools]")
   //emp::TraitSet trait_set(tm);
 }
 
-
-
-struct TestStruct {
-  int myvar;
-
-  EMP_BUILD_TUPLE( int, MyInt,
-                   int, MyInt2,
-                   int, MyInt3,
-                   int, MyInt4,
-                   int, MyInt5,
-                   int, MyInt6,
-                   int, MyInt7,
-                   int, MyInt8,
-                   int, MyInt9,
-                   int, MyInt10,
-                   std::string, MyString, 
-                   std::string, MyString2, 
-                   std::string, MyString3, 
-                   std::string, MyString4, 
-                   std::string, MyString5, 
-                   std::string, MyString6, 
-                   std::string, MyString7, 
-                   std::string, MyString8, 
-                   std::string, MyString9, 
-                   std::string, MyString10, 
-                   char, MyChar,
-                   char, MyChar2,
-                   char, MyChar3,
-                   char, MyChar4,
-                   char, MyChar5,
-                   char, MyChar6,
-                   char, MyChar7,
-                   char, MyChar8,
-                   char, MyChar9,
-                   char, MyChar10)
-};
-
-struct TestIntrospectiveStruct {
-  EMP_BUILD_INTROSPECTIVE_TUPLE( int, MyInt,
-                   int, MyInt2,
-                   int, MyInt3,
-                   int, MyInt4,
-                   int, MyInt5,
-                   int, MyInt6,
-                   int, MyInt7,
-                   int, MyInt8,
-                   int, MyInt9,
-                   int, MyInt10,
-                   std::string, MyString, 
-                   std::string, MyString2, 
-                   std::string, MyString3, 
-                   std::string, MyString4, 
-                   std::string, MyString5, 
-                   std::string, MyString6, 
-                   std::string, MyString7, 
-                   std::string, MyString8, 
-                   std::string, MyString9, 
-                   std::string, MyString10, 
-                   char, MyChar,
-                   char, MyChar2,
-                   char, MyChar3,
-                   char, MyChar4,
-                   char, MyChar5,
-                   char, MyChar6,
-                   char, MyChar7,
-                   char, MyChar8,
-                   char, MyChar9,
-                   char, MyChar10)
-};
-
-TEST_CASE("Test tuple_struct", "[tools]")
-{
-
-  TestStruct test;
-  test.myvar = 12;
-  test.MyInt(15);
-  test.MyInt() = 20;
-
-  REQUIRE(test.myvar == 12);
-  REQUIRE(test.MyInt() == 20);
-  // std::cout << "myvar = " << test.myvar << std::endl;
-  // std::cout << "MyInt = " << test.MyInt() << std::endl;
-
-  TestIntrospectiveStruct test2;
-  test2.MyInt(8);
-  REQUIRE(test2.MyInt() == 8);
-  REQUIRE(test2.var_names[0] == "MyInt");
-  // std::cout << "MyInt = " << test2.MyInt() << std::endl;
-  // std::cout << "First variable name: " << test2.var_names[0] << std::endl;
-  
-}
 
 TEST_CASE("Test vector", "[tools]")
 {
