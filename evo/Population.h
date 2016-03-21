dcf51d0405c0a8593626b9a536eda2ca26989435
Charles Ofria <charles.ofria@gmail.com>
2016-03-21 16:56:55 -0400

    Simplified creation of reflective calls + formatting fixed in Population

--- evo/Population.h
+++ evo/Population.h
@@ -44,20 +44,19 @@
 //  void TournamentSelect(int t_size, int tourny_count=1)
 //  void RunTournament(std::vector<double> fitness, int t_size, int tourny_count=1)
 //
-//  void FitnessSharingTournamentSelect(std::function<double(MEMBER*)> fit_fun, 
+//  void FitnessSharingTournamentSelect(std::function<double(MEMBER*)> fit_fun,
 //					std::function<double(MEMBER*, MEMBER*)> dist_fun,
 //					double sharing_threshhold, double alpha,
 //					int t_size, int tourny_count=1)
 //  void FitnessSharingTournamentSelect(std::function<double(MEMBER*, MEMBER*)>
-//					dist_fun, double sharing_threshold, 
-//					double alpha, int t_size, 
+//					dist_fun, double sharing_threshold,
+//					double alpha, int t_size,
 //					int tourny_count=1)
 //
 // Advancing Population
 //  void Update()                  -- Shift to the next generation.
 //  void Execute(ARGS... args)     -- Execute each organism in the population.
 
-	
 
 #ifndef EMP_POPULATION_H
 #define EMP_POPULATION_H
@@ -85,20 +84,20 @@
 //   GetDefaultFitnessFun()  -- Return the current default fitness function being used.
 //   SetDefaultFitnessFun(new_fun)  -- Set the default fitness function to be new_fun.
 
-#define EMP_SETUP_EVO_POP_DEFAULT(TYPE, FUN_VAR, METHOD)		\
-  TYPE FUN_VAR;								\
-  template <class T> void Setup_ ## METHOD ## _impl(emp_bool_decoy(T::METHOD)) { \
-    FUN_VAR = [](T* org){ return org->GetMETHOD(); };			\
-  }									\
-  template <class T> void Setup_ ## METHOD ## _impl(int) { ; }		\
-  void Setup_ ## METHOD() { Setup_ ## METHOD ## _impl<MEMBER>(true); }	\
-  public:								\
-  const TYPE & GetDefault ## METHOD ## Fun() const {			\
-    return FUN_VAR;							\
-  }									\
-  void SetDefault ## METHOD ## Fun(const TYPE & f) {			\
-    FUN_VAR = f;							\
-  }									\
+#define EMP_SETUP_EVO_POP_DEFAULT(RTYPE, FUN_VAR, METHOD)                         \
+  std::function<RTYPE(MEMBER*)> FUN_VAR;                                          \
+  template <class T> void Setup_ ## METHOD ## _impl(emp_bool_decoy(T::METHOD)) {  \
+    FUN_VAR = [](T* org){ return org->METHOD(); };                                \
+  }                                                                               \
+  template <class T> void Setup_ ## METHOD ## _impl(int) { ; }                    \
+  void Setup_ ## METHOD() { Setup_ ## METHOD ## _impl<MEMBER>(true); }            \
+  public:                                                                         \
+  const std::function<RTYPE(MEMBER*)> & GetDefault ## METHOD ## Fun() const {    \
+    return FUN_VAR;                                                               \
+  }                                                                               \
+  void SetDefault ## METHOD ## Fun(const std::function<RTYPE(MEMBER*)> & f) {    \
+    FUN_VAR = f;                                                                  \
+  }                                                                               \
   protected:
 
 
@@ -106,15 +105,15 @@ namespace emp {
 namespace evo {
 
   // Main population class...
-  
+
   template <typename MEMBER>
   class Population {
   protected:
     emp::vector<MEMBER *> pop;
     emp::vector<MEMBER *> next_pop;
     emp::Random * random_ptr;
-    EMP_SETUP_EVO_POP_DEFAULT(std::function<double(MEMBER*)>, default_fit_fun, Fitness);
-    EMP_SETUP_EVO_POP_DEFAULT(std::function<bool(MEMBER*)>, default_mut_fun, Mutate);
+    EMP_SETUP_EVO_POP_DEFAULT(double, default_fit_fun, Fitness);
+    EMP_SETUP_EVO_POP_DEFAULT(bool, default_mut_fun, Mutate);
 
     // Determine the callback type; by default this will be OrgSignals_NONE, but it can be
     // overridden by setting the type callback_t in the organism class.
@@ -123,9 +122,9 @@ namespace evo {
 
     // Build a Setup method in population that calls .Setup() on whatever is passed in, but
     // only if it exists.
-   EMP_CREATE_OPTIONAL_METHOD(SetupOrg, Setup);
+    EMP_CREATE_OPTIONAL_METHOD(SetupOrg, Setup);
 
-    
+
     // AddOrg and ReplaceOrg should be the only ways new organisms come into a population.
     // AddOrg inserts them into the end of the designated population.
     // ReplaceOrg places them at a specific position, replacing anyone who may already be there.
@@ -139,7 +138,7 @@ namespace evo {
       pop[pos] = new_org;
       SetupOrg(*new_org, &callbacks, pos);
     }
-    
+
     void DoRepro(int id) {
       emp_assert(random_ptr != nullptr && "DoRepro() requires a random number generator.");
       std::cout << "Repro " << id << std::endl;
@@ -148,7 +147,7 @@ namespace evo {
       const int target_id = random_ptr->GetInt((int) pop.size());
       ReplaceOrg(pop, target_id, new_org);
     }
-    
+
     void DoSymbiontRepro(int id) {
       emp_assert(random_ptr != nullptr && "DoSymbiontRepro() requires a random number generator.");
       std::cout << "SymbRepro " << id << std::endl;
@@ -158,7 +157,7 @@ namespace evo {
       // @CAO Call member class function to determine which symboiont is being comied from
       // the host in id, and how (and if) they should going into the target id.
     }
-    
+
     void SetupCallbacks(OrgSignals_NONE &) { ; }
     void SetupCallbacks(OrgSignals_Basic & sigs) {
       sigs.repro_sig.AddAction([this](int id){DoRepro(id);});
@@ -167,7 +166,7 @@ namespace evo {
       sigs.repro_sig.AddAction([this](int id){DoRepro(id);});
       sigs.symbiont_repro_sig.AddAction([this](int id){DoSymbiontRepro(id);});
     }
-    
+
   public:
     Population(const std::string & pop_name="emp::evo::Population")
       : random_ptr(nullptr), callbacks(pop_name)
@@ -201,12 +200,12 @@ namespace evo {
     }
     template <typename... ARGS>
     void Insert(Random & random, ARGS... args) {
-      AddOrg(pop, new MEMBER(random, std::forward<ARGS>(args)...));      
+      AddOrg(pop, new MEMBER(random, std::forward<ARGS>(args)...));
     }
     template <typename... ARGS>
     void InsertRandomOrg(ARGS... args) {
       emp_assert(random_ptr != nullptr && "InsertRandomOrg() requires active random_ptr");
-      AddOrg(pop, new MEMBER(*random_ptr, std::forward<ARGS>(args)...));      
+      AddOrg(pop, new MEMBER(*random_ptr, std::forward<ARGS>(args)...));
     }
     void InsertNext(const MEMBER & mem, int copy_count=1) {
       for (int i = 0; i < copy_count; i++) AddOrg(next_pop, new MEMBER(mem));
@@ -216,7 +215,7 @@ namespace evo {
     int Mutate(std::function<bool(MEMBER*)> mut_fun, const int first_mut=1) {
       int mut_count = 0;
       for (int i = 1; i < (int) pop.size(); i++) {
-	if (mut_fun(pop[i])) mut_count++;
+        if (mut_fun(pop[i])) mut_count++;
       }
       return mut_count;
     }
@@ -224,7 +223,7 @@ namespace evo {
     int Mutate(const int first_mut=1) {
       return Mutate(default_mut_fun, first_mut);
     }
-    
+
     // Selection mechanisms choose organisms for the next generation.
 
     // Elite Selection picks a set of the most fit individuals from the population to move to
@@ -256,7 +255,7 @@ namespace evo {
     // User provides the fitness function, the tournament size, and (optionally) the
     // number of tournaments to run.
     void TournamentSelect(std::function<double(MEMBER*)> fit_fun, int t_size,
-			  int tourny_count=1) {
+        int tourny_count=1) {
       emp_assert(t_size > 0 && t_size <= (int) pop.size());
 
       // Pre-calculate fitnesses.
@@ -278,7 +277,7 @@ namespace evo {
         std::vector<int> entries = random_ptr->Choose(pop.size(), t_size);
         double best_fit = fitness[entries[0]];
         int best_id = entries[0];
-      
+
         // Search for a higher fit org in the tournament.
         for (int i = 1; i < t_size; i++) {
           const double cur_fit = fitness[entries[i]];
@@ -287,7 +286,7 @@ namespace evo {
             best_id = entries[i];
           }
         }
-        
+
         // Place the highest fitness into the next generation!
         InsertNext( *(pop[best_id]) );
       }
@@ -299,32 +298,32 @@ namespace evo {
     // a sharing threshold (sigma share) that defines which members are
     // in the same niche, and a value of alpha (which controls the shape of
     // the fitness sharing curve
-    void FitnessSharingTournamentSelect(std::function<double(MEMBER*)> fit_fun, 
-					std::function<double(MEMBER*, MEMBER*)> dist_fun,
-					double sharing_threshhold, double alpha,
-					int t_size, int tourny_count=1){
-
+    void FitnessSharingTournamentSelect(std::function<double(MEMBER*)> fit_fun,
+          std::function<double(MEMBER*, MEMBER*)> dist_fun,
+          double sharing_threshhold, double alpha,
+          int t_size, int tourny_count=1)
+    {
       emp_assert(t_size > 0 && t_size <= (int) pop.size());
 
       // Pre-calculate fitnesses.
       std::vector<double> fitness(pop.size());
       for (int i = 0; i < (int) pop.size(); ++i) {
-	double niche_count = 0;
-	for (int j = 0; j < (int) pop.size(); ++j) {
-	  double dij = dist_fun(pop[i], pop[j]);
-	  niche_count += std::max(1 - std::pow(dij/sharing_threshhold, alpha), 0.0);
-	}
-	fitness[i] = fit_fun(pop[i])/niche_count;
+        double niche_count = 0;
+        for (int j = 0; j < (int) pop.size(); ++j) {
+          double dij = dist_fun(pop[i], pop[j]);
+          niche_count += std::max(1 - std::pow(dij/sharing_threshhold, alpha), 0.0);
+        }
+        fitness[i] = fit_fun(pop[i])/niche_count;
       }
 
-      RunTournament(fitness, t_size, tourny_count);      
+      RunTournament(fitness, t_size, tourny_count);
     }
 
     // Fitness sharing Tournament Selection can use the default fitness function
     void FitnessSharingTournamentSelect(std::function<double(MEMBER*, MEMBER*)>
-					dist_fun, double sharing_threshold, 
-					double alpha, int t_size, 
-					int tourny_count=1) {
+          dist_fun, double sharing_threshold,
+          double alpha, int t_size,
+          int tourny_count=1) {
       TournamentSelect(default_fit_fun, dist_fun, sharing_threshold, alpha, t_size, tourny_count);
     }
 
@@ -342,10 +341,10 @@ namespace evo {
     template <typename... ARGS>
     void Execute(ARGS... args) {
       for (MEMBER * m : pop) {
-	m->Execute(std::forward<ARGS>(args)...);
+        m->Execute(std::forward<ARGS>(args)...);
       }
     }
-    
+
   };
 
 }  // END evo namespace
