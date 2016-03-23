83040a178129eb88b2131fd41b9112a73a926a85
Charles Ofria <charles.ofria@gmail.com>
2016-03-22 22:16:20 -0400

    Updated population example code to test use of members with Fitess() method

--- examples/evo/Population.cc
+++ examples/evo/Population.cc
@@ -1,5 +1,4 @@
 //  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  This file is part of Empirical, https://github.com/devosoft/Empirical
 //  Copyright (C) Michigan State University, 2016.
 //  Released under the MIT Software license; see doc/LICENSE
 //
@@ -10,6 +9,15 @@
 
 #include "../../evo/Population.h"
 #include "../../tools/Random.h"
+
+struct TestOrg1 {
+  int fitness;
+
+  TestOrg1() { fitness = 0; }
+  TestOrg1(int f) { fitness = f; }
+  double Fitness() { return (double) fitness; }
+  bool Mutate() { return false; }
+};
 
 int main()
 {
@@ -31,12 +39,33 @@ int main()
   // Populations can be supplied a default fitness function so that it doesn't
   // need to keep being specified.
   pop.SetDefaultFitnessFun([](int * i){ return (double) *i; });
-  
+
   // pop.EliteSelect([](int * i){ return (double) *i; }, 10, 10);
   pop.EliteSelect(10, 10);
   pop.Update();
   std::cout << "Post-Elite Size = " << pop.GetSize() << std::endl;
   for (int i = 0; i < pop.GetSize(); i++) std::cout << pop[i] << " ";
+  std::cout << std::endl << std::endl;
+
+
+
+  emp::evo::Population<TestOrg1> pop1(random);
+  for (int i = 0; i < 100; i++) pop1.Insert(i+200);
+
+  std::cout << "Start Size = " << pop1.GetSize() << std::endl;
+  for (int i = 0; i < pop1.GetSize(); i++) std::cout << pop1[i].Fitness() << " ";
   std::cout << std::endl;
 
+  pop1.TournamentSelect(5, 100);
+  pop1.Update();
+  std::cout << "Post-Tourney Size = " << pop1.GetSize() << std::endl;
+  for (int i = 0; i < pop1.GetSize(); i++) std::cout << pop1[i].Fitness() << " ";
+  std::cout << std::endl;
+
+  // pop.EliteSelect([](int * i){ return (double) *i; }, 10, 10);
+  pop1.EliteSelect(10, 10);
+  pop1.Update();
+  std::cout << "Post-Elite Size = " << pop1.GetSize() << std::endl;
+  for (int i = 0; i < pop1.GetSize(); i++) std::cout << pop1[i].Fitness() << " ";
+  std::cout << std::endl << std::endl;
 }
