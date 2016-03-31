eaf9c2dc20451cdd4174fe86f4d06e69ef7e406a
Charles Ofria <charles.ofria@gmail.com>
2016-03-31 11:12:19 -0400

    Created example for EMP_CHOOSE_TYPE_WITH_MEMBER

--- examples/tools/reflection.cc
+++ examples/tools/reflection.cc
@@ -9,6 +9,7 @@
 
 #include "../../tools/reflection.h"
 
+// Testing EMP_CHOOSE_MEMBER_TYPE...
 struct A {
   using test_type = double;
 
@@ -21,16 +22,39 @@ struct B {
 
 template <typename T>
 struct Wrapper {
-  EMP_CREATE_TYPE_FALLBACK(new_type, T, test_type, int);
+  EMP_CHOOSE_MEMBER_TYPE(new_type, test_type, int, T);
 
   static constexpr new_type VALUE = 5;
 };
 
+// Testing EMP_CHOOSE_TYPE_WITH_MEMBER...
+struct C {
+  static constexpr int class_id = 3;
+  bool use_this;
+};
+struct D {
+  static constexpr int class_id = 4;
+};
+struct E {
+  static constexpr int class_id = 5;
+  bool use_this;
+};
+struct Fallback {
+  static constexpr int class_id = -1;
+  bool use_this;
+};
+
+EMP_CHOOSE_TYPE_WITH_MEMBER(auto_type, use_this, Fallback, C, D, E);
+
 int main()
 {
+  // Continuing test of EMP_CHOOSE_MEMBER_TYPE...
   Wrapper<A> wrap_A;
   Wrapper<B> wrap_B;
 
-  std::cout << Wrapper<A>::VALUE/2 << std::endl;
-  std::cout << Wrapper<B>::VALUE/2 << std::endl;
+  std::cout << Wrapper<A>::VALUE/2 << std::endl;  // 2.5
+  std::cout << Wrapper<B>::VALUE/2 << std::endl;  // 2
+
+  // Continuing test of EMP_CHOOSE_TYPE_WITH_MEMBER...
+  std::cout << auto_type::class_id << std::endl;
 }
