4e400e94b57c1918bbae98d9e26f1a754dc0bb60
Charles Ofria <charles.ofria@gmail.com>
2016-05-12 22:25:34 -0400

    Added example file for emp::vector

---
+++ examples/tools/vector.cc
@@ -0,0 +1,20 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+
+#include "../../tools/vector.h"
+
+int main()
+{
+  emp::vector<int> v;
+  for (int i = 0; i < 10; i++) {
+    v.push_back( 10*i );
+  }
+
+  v.insert(v.begin() + 3, -1);
+
+  for (auto x : v) {
+    std::cout << x << ' ';
+  }
+  std::cout << std::endl;
+}
