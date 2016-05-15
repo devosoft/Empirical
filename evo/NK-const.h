249f3c05a72dc7473dd71292faf902bceb25abb8
Charles Ofria <charles.ofria@gmail.com>
2016-05-14 22:40:13 -0400

    Backed up current version of NK to NK-const; will convert default NK to be more flexible.

---
+++ evo/NK-const.h
@@ -0,0 +1,75 @@
+//  This file is part of Empirical, https://github.com/mercere99/Empirical/
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  This file provides code to build NK-based algorithms.
+
+#ifndef NK_H
+#define NK_H
+
+#include <array>
+
+#include "../tools/assert.h"
+#include "../tools/BitSet.h"
+#include "../tools/const_utils.h"
+#include "../tools/Random.h"
+
+namespace emp {
+namespace evo {
+
+  template <int N, int K>
+  class NKLandscape {
+  private:
+    static constexpr int state_count = emp::constant::IntPow(2,K+1);
+    static constexpr int total_count = N * state_count;
+    std::array< std::array<double, state_count>, N > landscape;
+
+  public:
+    NKLandscape() = delete; // { ; }
+    NKLandscape(emp::Random & random) {
+      for ( std::array<double, state_count> & ltable : landscape) {
+        for (double & pos : ltable) {
+          pos = random.GetDouble();
+        }
+      }
+    }
+    NKLandscape(const NKLandscape &) = delete;
+    ~NKLandscape() { ; }
+    NKLandscape & operator=(const NKLandscape &) = delete;
+
+    constexpr int GetN() const { return N; }
+    constexpr int GetK() const { return K; }
+    constexpr int GetStateCount() const { return state_count; }
+    constexpr int GetTotalCount() const { return total_count; }
+
+    double GetFitness(int n, uint32_t state) const {
+      emp_assert(state < state_count, state, state_count);
+      // std::cout << n << " : " << state << " : " << landscape[n][state] << std::endl;
+      return landscape[n][state];
+    }
+    double GetFitness( std::array<uint32_t, N> states ) const {
+      double total = landscape[0][states[0]];
+      for (int i = 1; i < N; i++) total += GetFitness(i,states[i]);
+      return total;
+    }
+    double GetFitness(const BitSet<N> & genome) const {
+      // Create a double-length genome to easily handle wrap-around.
+      BitSet<N*2> genome2( genome.template Export<N*2>() );
+      genome2 |= (genome2 << N);
+
+      double total = 0.0;
+      constexpr uint32_t mask = emp::constant::MaskLow<uint32_t>(K+1);
+      for (int i = 0; i < N; i++) {
+	const uint32_t cur_val = (genome2 >> i).GetUInt(0) & mask;
+	const double cur_fit = GetFitness(i, cur_val);
+	total += cur_fit;
+      }
+      return total;
+    }
+  };
+
+}
+}
+
+#endif
---
+++ examples/evo/NK-const.cc
@@ -0,0 +1,65 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  This file explores the template defined in evo::Population.h
+
+#include <iostream>
+
+#include "../../evo/NK.h"
+#include "../../evo/World.h"
+#include "../../tools/BitSet.h"
+#include "../../tools/Random.h"
+
+constexpr int K = 13;
+constexpr int N = 50;
+
+constexpr int POP_SIZE = 10;
+constexpr int UD_COUNT = 10;
+
+using BitOrg = emp::BitSet<N>;
+
+int main()
+{
+  emp::Random random;
+  emp::evo::NKLandscape<N,K> landscape(random);
+  // emp::evo::EAWorld<BitOrg> pop(random);
+  //
+  // // Build a random initial population
+  // for (int i = 0; i < POP_SIZE; i++) {
+  //   BitOrg next_org;
+  //   for (int j = 0; j < N; j++) next_org[j] = random.P(0.5);
+  //   pop.Insert(next_org);
+  // }
+  //
+  // pop.SetDefaultMutateFun( [](BitOrg* org, emp::Random& random) {
+  //     (*org)[random.GetInt(N)] = random.P(0.5);
+  //     (*org)[random.GetInt(N)] = random.P(0.5);
+  //     (*org)[random.GetInt(N)] = random.P(0.5);
+  //     return true;
+  //   } );
+  //
+  //
+  // // Loop through updates
+  // for (int ud = 0; ud < UD_COUNT; ud++) {
+  //   // Print current state.
+  //   // for (int i = 0; i < pop.GetSize(); i++) std::cout << pop[i] << std::endl;
+  //   // std::cout << std::endl;
+  //   std::cout << ud << " : " << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
+  //
+  //   // Keep the best individual.
+  //   pop.EliteSelect([&landscape](BitOrg * org){ return landscape.GetFitness(*org); }, 100);
+  //
+  //   // Run a tournament for the rest...
+  //   pop.TournamentSelect([&landscape](BitOrg * org){ return landscape.GetFitness(*org); }
+	// 		 , 5, POP_SIZE-1);
+  //   pop.Update();
+  //   pop.MutatePop();
+  // }
+  //
+  //
+  // std::cout << UD_COUNT << " : " << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
+
+  std::cout << sizeof(emp::evo::NKLandscape<N,K>) << std::endl;
+}
