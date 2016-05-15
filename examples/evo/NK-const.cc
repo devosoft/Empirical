44998a56bfdc639ecfe7b5c3ca8257aaabfee3de
Charles Ofria <charles.ofria@gmail.com>
2016-05-15 10:12:25 -0400

    Updated examples for NK and NK-const to fit new versions of landscapes

--- examples/evo/NK-const.cc
+++ examples/evo/NK-const.cc
@@ -7,7 +7,7 @@
 
 #include <iostream>
 
-#include "../../evo/NK.h"
+#include "../../evo/NK-const.h"
 #include "../../evo/World.h"
 #include "../../tools/BitSet.h"
 #include "../../tools/Random.h"
@@ -15,51 +15,51 @@
 constexpr int K = 13;
 constexpr int N = 50;
 
-constexpr int POP_SIZE = 10;
-constexpr int UD_COUNT = 10;
+constexpr int POP_SIZE = 1000;
+constexpr int UD_COUNT = 2000;
 
 using BitOrg = emp::BitSet<N>;
 
 int main()
 {
   emp::Random random;
-  emp::evo::NKLandscape<N,K> landscape(random);
-  // emp::evo::EAWorld<BitOrg> pop(random);
-  //
-  // // Build a random initial population
-  // for (int i = 0; i < POP_SIZE; i++) {
-  //   BitOrg next_org;
-  //   for (int j = 0; j < N; j++) next_org[j] = random.P(0.5);
-  //   pop.Insert(next_org);
-  // }
-  //
-  // pop.SetDefaultMutateFun( [](BitOrg* org, emp::Random& random) {
-  //     (*org)[random.GetInt(N)] = random.P(0.5);
-  //     (*org)[random.GetInt(N)] = random.P(0.5);
-  //     (*org)[random.GetInt(N)] = random.P(0.5);
-  //     return true;
-  //   } );
-  //
-  //
-  // // Loop through updates
-  // for (int ud = 0; ud < UD_COUNT; ud++) {
-  //   // Print current state.
-  //   // for (int i = 0; i < pop.GetSize(); i++) std::cout << pop[i] << std::endl;
-  //   // std::cout << std::endl;
-  //   std::cout << ud << " : " << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
-  //
-  //   // Keep the best individual.
-  //   pop.EliteSelect([&landscape](BitOrg * org){ return landscape.GetFitness(*org); }, 100);
-  //
-  //   // Run a tournament for the rest...
-  //   pop.TournamentSelect([&landscape](BitOrg * org){ return landscape.GetFitness(*org); }
-	// 		 , 5, POP_SIZE-1);
-  //   pop.Update();
-  //   pop.MutatePop();
-  // }
-  //
-  //
-  // std::cout << UD_COUNT << " : " << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
+  emp::evo::NKLandscapeConst<N,K> landscape(random);
+  emp::evo::EAWorld<BitOrg> pop(random);
 
-  std::cout << sizeof(emp::evo::NKLandscape<N,K>) << std::endl;
+  // Build a random initial population
+  for (int i = 0; i < POP_SIZE; i++) {
+    BitOrg next_org;
+    for (int j = 0; j < N; j++) next_org[j] = random.P(0.5);
+    pop.Insert(next_org);
+  }
+
+  pop.SetDefaultMutateFun( [](BitOrg* org, emp::Random& random) {
+      (*org)[random.GetInt(N)] = random.P(0.5);
+      (*org)[random.GetInt(N)] = random.P(0.5);
+      (*org)[random.GetInt(N)] = random.P(0.5);
+      return true;
+    } );
+
+
+  // Loop through updates
+  for (int ud = 0; ud < UD_COUNT; ud++) {
+    // Print current state.
+    // for (int i = 0; i < pop.GetSize(); i++) std::cout << pop[i] << std::endl;
+    // std::cout << std::endl;
+    std::cout << ud << " : " << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
+
+    // Keep the best individual.
+    pop.EliteSelect([&landscape](BitOrg * org){ return landscape.GetFitness(*org); }, 1);
+
+    // Run a tournament for the rest...
+    pop.TournamentSelect([&landscape](BitOrg * org){ return landscape.GetFitness(*org); }
+			 , 5, POP_SIZE-1);
+    pop.Update();
+    pop.MutatePop();
+  }
+
+
+  std::cout << UD_COUNT << " : " << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
+
+  std::cout << sizeof(emp::evo::NKLandscapeConst<N,K>) << std::endl;
 }
