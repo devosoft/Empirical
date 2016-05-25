bd7a04d894eb750571e100bd16f4a3cc6476f926
Jake Fenton <bocajnotnef@gmail.com>
2016-05-25 18:05:10 -0400

    Everything is set up and tests compile; need to add more tests
    The templating in this is truly evil

--- evo/PopulationManager.h
+++ evo/PopulationManager.h
@@ -13,6 +13,7 @@
 #define EMP_EVO_POPULATION_MANAGER_H
 
 #include "../tools/random_utils.h"
+#include <set>
 
 namespace emp {
 namespace evo {
@@ -199,6 +200,33 @@ namespace evo {
 
     int GetWidth() const { return width; }
     int GetHeight() const { return height; }
+
+    // method to get all the von-neuman (sp?) neighbors of a particular organism
+    // does not include the organism itself
+    std::set<ORG> get_org_neighbors (int org_id) {
+      std::set<ORG> neighbors;
+      int org_x, org_y;
+      int possx[3] = {0,0,0}, possy[3] = {0,0,0}; //arrays to hold possible offsets
+      org_x = ToX(org_id);
+      org_y = ToY(org_id);
+
+      if (org_y > 0) {possy[2] = 1;} // enable going up
+      if (org_y < height - 1) {possy[0] = -1;} // enable going down
+      if (org_x > 0) { possx[0] = -1;} // enable going left
+      if (org_x < width - 1) {possx[2] = 1;} // enable going right
+
+      // iterate over all possible spaces && add organisms to set
+      // using the set will prevent duplictes, since we *WILL* traverse the same spaces
+      // multiple times.
+      for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+          neighbors.add(pop[ToID(org_x + possx[i], org_y + possy[j])]);
+        }
+      }
+
+      neighbors.remove(pop[org_id]); // remove focal node from set
+      return neighbors;
+    }
 
     void ConfigPop(int w, int h) { width = w; height = h; pop.resize(width*height, nullptr); }
 
--- evo/QuorumOrg.h
+++ evo/QuorumOrg.h
@@ -12,8 +12,10 @@
 #include <iostream>
 
 /* TODO: Implement the config stuff
- *      - # pts donated when co-operating
+ *    - # pts donated when co-operating
  * TODO: implement actual use of the org's id in the state object
+ * TODO: impelement some kind of 'tick' processing function, to handle
+ *     tracking cost of AI production && if co-operating
  */
 
 /* Simple class to contain the genome for a QuorumOrg
@@ -26,6 +28,7 @@
 
 // constants that will eventually be configurable
 const unsigned int NUM_TO_DONATE = 5;
+const unsigned int NEEDED_TO_REPRODUCE = 10;
 const double MUTATION_AMOUNT = 0.1;
 const double MUTATION_RATE = 0.005;
 
@@ -33,148 +36,184 @@ const double MUTATION_RATE = 0.005;
 namespace emp {
 namespace evo {
 
+struct QuorumOrgGenome {
+  double co_op_prob;
+  double ai_gen_prob;
+  double quorum_threshold;
+
+  QuorumOrgGenome () {
+    co_op_prob = 0;
+    ai_gen_prob = 0;
+    quorum_threshold = 1.1;
+  }
+
+  QuorumOrgGenome(double cprob, double aprob, double qthresh) :
+    co_op_prob(cprob), ai_gen_prob(aprob), quorum_threshold(qthresh) {};
+
+  // prints co-op prob, ai_gen_prob, quorum_thresh
+  void print (std::ostream & out) {
+    out << co_op_prob << ", " << ai_gen_prob << ", " << quorum_threshold;
+  }
+
+  std::ostream & operator << (std::ostream & out) {
+    print(out);
+    return out;
+  }
+};
+
+
+std::ostream & operator<< (std::ostream & out, QuorumOrgGenome & genome) {
+  genome.print(out);
+  return out;
+}
+
+  /* Simple class to contain the current state of the organism
+   * since everything is a primitive a bitwise copy for the constructor should work fine!
+   * ...right?
+   */
+
+struct QuorumOrgState {
+  QuorumOrgGenome genome;
+
+  bool producing_ai;
+  bool mutate;    // dunno if we'll want to do this by-org, but eh
+
+  unsigned int points;
+  unsigned int age;
+  unsigned int loc;
+  unsigned int num_offspring; // gonna use this as/in a basic fitness function
+
+  QuorumOrgState() {
+    producing_ai = false;
+    mutate = false;
+  };
+
+  QuorumOrgState (double cprob, double aprob, double qthresh, bool mut, unsigned int pts) :
+    genome(cprob, aprob, qthresh), mutate(mut), points(pts) {
+
+    age = 0;
+    loc = 0;
+    num_offspring = 0;
+  };
+
+  // prints out loc, age, points, if_producing_ai, if_mutation_on, {genome}
+  void print (std::ostream & out) {
+    out << loc << ", " << age << ", " << points << ", " << num_offspring;
+    out << ", " << producing_ai << ", " << mutate << ", (";
+    //genome.print(out); // dunno why streaming doesn't work here
+    out << genome;
+    out << ")";
+  }
+
+};
+
+// for some reason these have to be outside of the class to work properly
+std::ostream & operator<< (std::ostream & out, QuorumOrgState & state) {
+    state.print(out);
+    return out;
+}
+
 
 /* Here we see the QuorumOrganism in its natural habitat....
- * 
- * 
  */
 
 class QuorumOrganism {
 
-    struct QuorumOrgGenome {
-        double co_op_prob;
-        double ai_gen_prob;
-        double quorum_threshold;
-
-        QuorumOrgGenome () {
-            co_op_prob = 0;
-            ai_gen_prob = 0;
-            quorum_threshold = 1.1;
-        }
-
-        QuorumOrgGenome(double cprob, double aprob, double qthresh) :
-            co_op_prob(cprob), ai_gen_prob(aprob), quorum_threshold(qthresh) {};
-
-        // prints co-op prob, ai_gen_prob, quorum_thresh
-        void print (std::ostream & out) {
-            out << co_op_prob << ", " << ai_gen_prob << ", " << quorum_threshold;
-        }
-
-        std::ostream & operator << (std::ostream & out) {
-            print(out);
-            return out;
-        }
-    };
-
-
-    /* Simple class to contain the current state of the organism
-     * since everything is a primitive a bitwise copy for the constructor should work fine!
-     * ...right?
-     */
-
-    struct QuorumOrgState {
-        QuorumOrgGenome genome;
-        
-        bool producing_ai;
-        bool mutate;        // dunno if we'll want to do this by-org, but eh
-        
-        unsigned int points;
-        unsigned int age;
-        unsigned int loc;
-
-        QuorumOrgState() {
-            producing_ai = false;
-            mutate = false;
-        };
-
-        QuorumOrgState (double cprob, double aprob, double qthresh, bool mut, unsigned int pts) : 
-            genome(cprob, aprob, qthresh), mutate(mut), points(pts) {
-            
-            age = 0;
-            loc = 0;
-        };
-
-        // prints out loc, age, points, if_producing_ai, if_mutation_on, {genome}
-        void print (std::ostream & out) {
-            out << loc << ", " << age << ", " << points << ", " << producing_ai;
-            out << ", " << mutate << ", (";
-            genome.print(out); // dunno why streaming doesn't work here
-            out << ")";
-        }
-
-    };
-
 protected:
-    QuorumOrgState state;
-    emp::Random * random;
+  QuorumOrgState state;
+  emp::Random * random;
 
 public:
-    // Default Constructor
-    QuorumOrganism () {};
+  // Default Constructor
+  QuorumOrganism () {};
 
-    // Config constructor
-    QuorumOrganism (double cprob, double aprob, double qthresh, bool mut, unsigned int pts,
-                    emp::Random * rand) {
-        this->QuorumOrganism::state = QuorumOrgState(cprob, aprob, qthresh, mut, pts);
-        this->random = rand;
-        this->state.producing_ai = random->P(aprob);  
+  // Config constructor
+  QuorumOrganism (double cprob, double aprob, double qthresh, bool mut, unsigned int pts,
+          emp::Random * rand) {
+    this->QuorumOrganism::state = QuorumOrgState(cprob, aprob, qthresh, mut, pts);
+    this->random = rand;
+    this->state.producing_ai = random->P(aprob);
+  }
+
+  // copy constructor; probably the primary way of reproduction will be to
+  // copy the current org & call "mutate" on it
+  QuorumOrganism (const QuorumOrganism & source) {
+    // explicit is better than implicit...
+    this->state = source.state;
+    this->random = source.random;
+  }
+
+  // the aforementioned mutate function
+  bool mutate () {
+    if (!state.mutate) {return false;} // if mutation is disabled fail out
+    bool flag = false;
+    if (random->P(MUTATION_RATE)) {
+      state.genome.quorum_threshold += random->GetRandNormal(0, MUTATION_AMOUNT);
+      flag = true;
+    } if (random->P(MUTATION_RATE)) {
+      state.genome.ai_gen_prob += random->GetRandNormal(0, MUTATION_AMOUNT);
+      flag = true;
+    } if (random->P(MUTATION_RATE)) {
+      state.genome.co_op_prob += random->GetRandNormal(0, MUTATION_AMOUNT);
+      flag = true;
+    } // that felt silly
+
+    return flag;
+  }
+
+  // utility/accessor methods
+  void set_state (QuorumOrgState new_state) {state = new_state;}
+  void set_id (unsigned int new_id) {state.loc = new_id;}
+  unsigned int get_loc() {return state.loc;}
+  unsigned int add_points(unsigned int points) {return state.points += points;}
+  bool making_ai () const {return state.producing_ai;}
+
+  // methods for interacting with the world / neighbors
+  int get_contribution (double current_quorum) {
+    if (random->P(state.genome.co_op_prob) &&
+      current_quorum >= state.genome.quorum_threshold) {
+
+      // gonna add a check to see if we contribute when we're poor
+      // dunno if I should
+      if (state.points >= NUM_TO_DONATE) {
+        state.points -= NUM_TO_DONATE;
+        return NUM_TO_DONATE;
+      }
+    }
+    return 0;
+  }
+
+  void print (std::ostream & out) {
+    out << state;
+  }
+
+  // function to make offspring of this org; does mutation, if enabled
+  QuorumOrganism * make_offspring() {
+    QuorumOrganism * offspring = new QuorumOrganism(*this);
+    offspring->mutate();
+    return offspring;
+  }
+
+  // function to handle reproduction of this organism
+  // will return pointer to offspring if capable of reproducing
+  // nullptr otherwise
+  // will decrement the points needed to reproduce from state
+  QuorumOrganism * reproduce() {
+    if (state.points >= NEEDED_TO_REPRODUCE) {
+      state.points -= NEEDED_TO_REPRODUCE;
+      state.num_offspring++;
+      return make_offspring();
     }
 
-    // copy constructor; probably the primary way of reproduction will be to 
-    // copy the current org & call "mutate" on it
-    QuorumOrganism (const QuorumOrganism & source) {
-        // explicit is better than implicit...
-        this->state = source.state;
-        this->random = source.random;
-    }
+    return nullptr;
+  }
 
-    // the aforementioned mutate function
-    bool mutate () {
-        if (!state.mutate) {return false;} // if mutation is disabled fail out
-        bool flag = false;
-        if (random->P(MUTATION_RATE)) {
-            state.genome.quorum_threshold += random->GetRandNormal(0, MUTATION_AMOUNT);
-            flag = true;
-        } if (random->P(MUTATION_RATE)) {
-            state.genome.ai_gen_prob += random->GetRandNormal(0, MUTATION_AMOUNT);
-            flag = true;
-        } if (random->P(MUTATION_RATE)) {
-            state.genome.co_op_prob += random->GetRandNormal(0, MUTATION_AMOUNT);
-            flag = true;
-        } // that felt silly
-
-        return flag;
-    }
-
-    // utility/accessor methods
-    void set_state (QuorumOrgState new_state) {state = new_state;}
-    unsigned int add_points(unsigned int points) {return state.points += points;}
-    bool making_ai () const {return state.producing_ai;}
-
-    // methods for interacting with the world / neighbors
-    int get_contribution (double current_quorum) {
-        if (random->P(state.genome.co_op_prob) &&
-            current_quorum <= state.genome.quorum_threshold) {
-
-            // gonna add a check to see if we contribute when we're poor
-            // dunno if I should
-            if (state.points >= NUM_TO_DONATE) {
-                state.points -= NUM_TO_DONATE;
-                return NUM_TO_DONATE;
-            }
-        }
-        return 0;
-    }
-
-    void print (std::ostream & out) {
-        
-    }
-
-    std::ostream & operator << (std::ostream & out) {
-       print(out);
-       return out;
-    }
 };
+
+std::ostream & operator << (std::ostream & out, QuorumOrganism & org) {
+   org.print(out);
+   return out;
+}
 
 } // close evo::
 } // close emp::
