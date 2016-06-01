c2a9af1605ce2afd7ba9c6ce84ddc7f2191d111e
Emily Dolson <EmilyLDolson@gmail.com>
2016-06-01 17:17:53 -0400

    Testing visualization stuff

--- examples/evo/NK_OEE.cc
+++ examples/evo/NK_OEE.cc
@@ -6,9 +6,14 @@
 //  This file explores the template defined in evo::Population.h
 
 
-//#include "../../emtools/init.h"
-//#include "../../emtools/JSWrap.h"
-//#include "../../../d3-emscripten/selection.h"
+#include "../../web/web_init.h"
+#ifdef EMSCRIPTEN
+#include "../../emtools/JSWrap.h"
+#include "../../../d3-emscripten/selection.h"
+#include "../../../d3-emscripten/scales.h"
+#include "../../../d3-emscripten/axis.h"
+#include "../../web/Animate.h"
+#endif
 
 #include <iostream>
 #include <functional>
@@ -24,23 +29,46 @@
 constexpr int K = 3;
 constexpr int N = 20;
 
-constexpr int POP_SIZE = 200;
-constexpr int UD_COUNT = 500;
-constexpr int TOURNAMENT_SIZE = 15;
+constexpr int POP_SIZE = 10;
+constexpr int UD_COUNT = 1000;
+constexpr int TOURNAMENT_SIZE = 5;
 
 using BitOrg = emp::BitSet<N>;
 
+
+/*double return_d(double d, int i=0, int k=0){
+  return d;
+}*/
+
+int return_i(double d, int i, int k=0){
+  return i;
+}
+
+#ifdef EMSCRIPTEN
+template <typename WORLD>
+void Evolve(WORLD & pop, D3::Selection & circles) {
+    //for (auto org : pop.popM){
+        //std::cout << *org << std::endl;
+    //}
+    std::cout << "in evolve " << pop.popM.size() << std::endl;
+    pop.TournamentSelect(pop.statsM.fit_fun, TOURNAMENT_SIZE, POP_SIZE);
+    pop.Update();
+    pop.MutatePop();
+    emp::vector<double> fitnesses = RunFunctionOnContainer(pop.statsM.fit_fun, pop.popM);
+    circles = circles.Data(fitnesses);
+    //circles.EnterAppend("circle");
+    //circles.SetAttr("cx", "scaled_i");
+    circles.Transition().SetAttr("cy", "scaled_d");
+}
+#endif
+
 int main()
 {
+
   std::cout << "N: " << N << ", K: " << K << ", POP_SIZE: " << POP_SIZE << ", Selection: " << "Standard_tournament" << ", TournamentSize: " << TOURNAMENT_SIZE << std::endl;
   emp::Random random;
   emp::evo::NKLandscapeConst<N,K> landscape(random);
-  emp::evo::World<BitOrg, emp::evo::OEEStats > pop(random);
-
-  //#ifdef EMSCRIPTEN
-  //std::cout << n_objects() << std::endl;
-  //D3::Selection svg = D3::Selection("body").Append("svg");
-  //#endif
+  emp::evo::World<BitOrg, emp::evo::LineageTracker_Pruned<emp::evo::PopBasic>, emp::evo::OEEStats > pop(random);
 
   // Build a random initial population
   for (int i = 0; i < POP_SIZE; i++) {
@@ -56,23 +84,73 @@ int main()
       return true;
     } );
 
+  std::function<double(BitOrg* org)> fit_fun = [&landscape](BitOrg * org){ return landscape.GetFitness(*org);};
+  pop.SetDefaultFitnessFun(fit_fun);
 
-pop.SetDefaultFitnessFun([&landscape](BitOrg * org){ return landscape.GetFitness(*org); });
+  emp::vector<double> fitnesses = RunFunctionOnContainer(fit_fun, pop.popM);
+
+  #ifdef EMSCRIPTEN
+  emp::web::Initialize();
+  std::cout << n_objects() << std::endl;
+  D3::Selection svg = D3::Selection("body").Append("svg");
+  svg.SetAttr("height", 500);
+  svg.SetAttr("width", 500);
+  D3::Selection circles = svg.SelectAll("circle").Data(fitnesses);
+  circles.EnterAppend("circle");
+  std::cout << "Circles appended" << std::endl;
+  D3::LinearScale fitness_scale;
+  fitness_scale.SetDomain(std::array<double, 2>({30, 5}));
+  fitness_scale.SetRange(std::array<double, 2>({10, 490}));
+  D3::Axis<D3::LinearScale> ax;
+  ax.SetScale(fitness_scale);
+  ax.SetOrientation("right");
+  ax.Draw(svg);
+  D3::LinearScale x_scale;
+  x_scale.SetDomain(std::array<double, 2>({0, POP_SIZE-1}));
+  x_scale.SetRange(std::array<double, 2>({40, 490}));
+
+  std::function<double(double, int, int)> scaled_d = [&](double d, int i, int k){
+      return fitness_scale.ApplyScale(d);
+  };
+
+  std::function<double(double, int, int)> scaled_i = [&](double d, int i, int k){
+      return x_scale.ApplyScale(i);
+  };
+
+  /*std::function<void(double, int, int)> mousover = [&](double d, int i, int k){
+      svg.Append("text").SetText(pop[i]);
+  };*/
+
+  emp::JSWrap(scaled_d, "scaled_d");
+  emp::JSWrap(scaled_i, "scaled_i");
+  circles.SetAttr("r", 5);
+  circles.SetAttr("cx", "scaled_i");
+  circles.SetAttr("cy", "scaled_d");
+  circles.SetStyle("fill", "green");
+  //circles = circles.Transition();
+  std::function<void()> animate_function = [&](){Evolve(pop, circles);};
+  emp::web::Animate anim;
+  anim.SetCallback(animate_function);
+  anim.Start();
+  #endif
+
 
   // Loop through updates
   for (int ud = 0; ud < UD_COUNT+1; ud++) {
-    // Print current state.
+    //// Print current state.
     // for (int i = 0; i < pop.GetSize(); i++) std::cout << pop[i] << std::endl;
     // std::cout << std::endl;
     //std::cout << ud << " : " << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
-
-    // Run a tournament for the rest...
-    pop.TournamentSelect([&landscape](BitOrg * org){ return landscape.GetFitness(*org); }
-    , TOURNAMENT_SIZE, POP_SIZE);
-    //pop.FitnessSharingTournamentSelect([landscape](BitOrg * org){ return landscape.GetFitness(*org); }, [](BitOrg* org1, BitOrg* org2){ return (double)(org1->XOR(*org2)).CountOnes();}, 10, 1, TOURNAMENT_SIZE, POP_SIZE);
+    pop.TournamentSelect(pop.statsM.fit_fun, TOURNAMENT_SIZE, POP_SIZE);
     pop.Update();
     pop.MutatePop();
-  }
+    // Run a tournament for the rest...
+    //std::cout << fitnesses[0] << std::endl;
+    #ifdef EMSCRIPTEN
+    anim.Step();
+    std::cout << ud << std::endl;
+    #endif
+}
 
 
   //std::cout << UD_COUNT << " : " << pop[0] << " : " << landscape.GetFitness(pop[0]) << std::endl;
