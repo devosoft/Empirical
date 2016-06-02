d9b2437e9e55496f0867fd8d9e5bfcac6db89d47
Charles Ofria <charles.ofria@gmail.com>
2016-06-02 10:29:36 -0400

    Streamlined errors.h and added LibraryError() and InternalError

--- tools/errors.h
+++ tools/errors.h
@@ -1,42 +1,58 @@
-// This file is part of Empirical, https://github.com/mercere99/Empirical/, and is 
-// Copyright (C) Michigan State University, 2015. It is licensed 
-// under the MIT Software license; see doc/LICENSE
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//  These tools help manage error handling for command-line or Emscripten-based applications.
+//
+//  There are three possible recipiants for all errors.
+//  * The end-user if the problem stems from inputs they provided to the executable.
+//  * The library user if the problem is due to mis-use of library functionality.
+//  * The library developers if something that should be impossible occurs.
 
 #ifndef EMP_ERRORS_H
 #define EMP_ERRORS_H
 
-////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//  These tools help manage error handling for command-line or Emscripten-based applications.
-//
-
 #include <iostream>
+#include <sstream>
 #include <string>
+
+#include "meta.h"
 
 namespace emp {
 
-  void Notify(std::string msg) {
+  // namespace internal {
+  //   template <typename T>
+  //   Notify_append(T in)
+  // }
+
+  template <typename... Ts>
+  void Notify(Ts... args) {
+    std::stringstream ss;
+    EMP_EXPAND_PPACK( (ss << args) );
 #ifdef EMSCRIPTEN
-    EM_ASM_ARGS({ msg = Pointer_stringify($0); alert(msg); }, msg.c_str());
+    EM_ASM_ARGS({ msg = Pointer_stringify($0); alert(msg); }, ss.str().c_str());
 #else
-    std::cerr << msg << std::endl;
+    std::cerr << ss.str() << std::endl;
 #endif
   }
 
-  void NotifyWarning(std::string msg) {
-    std::string print_msg = "WARNING: ";
-    print_msg += msg;
-    Notify(print_msg);
-  }
+  // End user has done something possibly a problem.
+  template <typename... Ts>
+  void NotifyWarning(Ts... msg) { Notify("WARNING: ", msg...); }
 
-  void NotifyError(std::string msg) {
-    std::string print_msg = "ERROR: ";
-    print_msg += msg;
-    Notify(print_msg);
-  }
+  // End user has done something definitely a problem.
+  template <typename... Ts>
+  void NotifyError(Ts... msg) { Notify("ERROR: ", msg...); }
+
+  // Library user has made an error in how they are using the library.
+  template <typename... Ts>
+  void LibraryError(Ts... msg) { Notify("EMPIRICAL USE ERROR: ", msg...); }
+
+  // Library implementers must have made an error.
+  template <typename... Ts>
+  void InternalError(Ts... msg) { Notify("INTERNAL EMPIRICAL ERROR: ", msg...); }
 
 };
 
 
 #endif
-
