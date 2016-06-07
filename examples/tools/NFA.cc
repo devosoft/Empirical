066bf802899942d8363ce7f926fb64665d65b945
Charles Ofria <charles.ofria@gmail.com>
2016-06-07 13:59:38 -0400

    Added example of NFA state tricaker

--- examples/tools/NFA.cc
+++ examples/tools/NFA.cc
@@ -23,6 +23,10 @@ int main()
   nfa.AddTransition(2, 3, 'c');
   nfa.AddTransition(3, 4, 'd');
 
+  nfa.AddTransition(0, 1, 'e');
+  nfa.AddTransition(0, 1, 'f');
+  nfa.AddTransition(0, 1, 'g');
+
   nfa.AddTransition(2, 3, 'a');
   nfa.AddTransition(3, 4, 'a');
   nfa.AddTransition(2, 4, 'a');
@@ -30,4 +34,11 @@ int main()
   nfa.AddTransition(2, 2, 'e');
   nfa.AddTransition(3, 3, 'e');
   nfa.AddTransition(4, 4, 'e');
+
+  emp::NFA_State state(nfa);
+  state.Print();
+  state.Next('a');
+  state.Print();
+  state.Next('a');
+  state.Print();
 }
