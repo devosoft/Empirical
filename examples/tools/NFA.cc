4385a5fefe66dd144bbc4403a8aa2ef63e680ea8
Charles Ofria <charles.ofria@gmail.com>
2016-06-07 13:07:51 -0400

    Example file for using NFAs

---
+++ examples/tools/NFA.cc
@@ -0,0 +1,33 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some examples code for using emp::NFA
+
+#include <iostream>
+
+#include "../../tools/NFA.h"
+
+int main()
+{
+  std::cout << "Ping!" << std::endl;
+
+  emp::NFA nfa(5);
+  nfa.AddTransition(0, 1, 'a');
+  nfa.AddTransition(0, 2, 'a');
+  nfa.AddTransition(0, 3, 'a');
+  nfa.AddTransition(0, 4, 'a');
+
+  nfa.AddTransition(1, 2, 'b');
+  nfa.AddTransition(2, 3, 'c');
+  nfa.AddTransition(3, 4, 'd');
+
+  nfa.AddTransition(2, 3, 'a');
+  nfa.AddTransition(3, 4, 'a');
+  nfa.AddTransition(2, 4, 'a');
+
+  nfa.AddTransition(2, 2, 'e');
+  nfa.AddTransition(3, 3, 'e');
+  nfa.AddTransition(4, 4, 'e');
+}
