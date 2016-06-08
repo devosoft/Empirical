c6cd7b1ea1679f0f8a85d94a579f7b7a5cd1d3ea
Charles Ofria <charles.ofria@gmail.com>
2016-06-07 23:18:49 -0400

    NFAs now work with free transitions.

--- tools/NFA.h
+++ tools/NFA.h
@@ -11,6 +11,7 @@
 #include <set>
 
 #include "BitSet.h"
+#include "set_utils.h"
 #include "vector.h"
 
 namespace emp {
@@ -24,7 +25,9 @@ namespace emp {
       opts_t symbols;
     };
     struct State {
-      std::map<int, Transition> trans;
+      std::map<int, Transition> trans;   // What symbol transitions are available?
+      std::set<int> free_to;             // What other states can you move to for free?
+      std::set<int> free_from;           // What other states can move here for free?
     };
 
     emp::vector<State> states;
@@ -37,7 +40,10 @@ namespace emp {
     std::set<int> GetNext(char sym, int from_id=0) {
       std::set<int> to_set;
       for (auto & t : states[from_id].trans) {
-        if (t.second.symbols[sym]) to_set.insert(t.first);
+        if (t.second.symbols[sym]) {
+          to_set.insert(t.first);
+          insert(to_set, states[t.first].free_to);
+        }
       }
       return to_set;
     }
@@ -45,7 +51,10 @@ namespace emp {
       std::set<int> to_set;
       for (int from_id : from_set) {
         for (auto & t : states[from_id].trans) {
-          if (t.second.symbols[sym]) to_set.insert(t.first);
+          if (t.second.symbols[sym]) {
+            to_set.insert(t.first);
+            insert(to_set, states[t.first].free_to);
+          }
         }
       }
       return to_set;
@@ -65,24 +74,49 @@ namespace emp {
 
       states[from].trans[to].symbols |= sym_set;
     }
+    void AddFreeTransition(int from, int to) {
+      emp_assert(from >= 0 && from < (int) states.size(), from, states.size());
+      emp_assert(to >= 0 && to < (int) states.size(), to, states.size());
+
+      // Keep track of where free transitions could have come from and can continue to.
+      auto extend_to = states[to].free_to;
+      auto extend_from = states[from].free_from;
+
+      // Update the immediate free moves with the new transition.
+      states[from].free_to.insert(to);
+      states[to].free_from.insert(from);
+
+      // from can get to all other free moves from to (and vice versa)
+      states[from].free_to.insert(extend_to.begin(), extend_to.end());
+      states[to].free_from.insert(extend_from.begin(), extend_from.end());
+
+      // everything that can get to from can now get to everything to can get to (and vv).
+      for (int x : extend_from) states[x].free_to.insert(extend_to.begin(), extend_to.end());
+      for (int x : extend_to) states[x].free_from.insert(extend_from.begin(), extend_from.end());
+    }
 
   };
 
   class NFA_State {
   private:
     NFA & nfa;
-    std::set<int> state;
+    std::set<int> state_set;
   public:
-    NFA_State(NFA & _nfa) : nfa(_nfa) { state.insert(nfa.GetStart()); }
+    NFA_State(NFA & _nfa) : nfa(_nfa) { state_set.insert(nfa.GetStart()); }
     ~NFA_State() { ; }
 
+    const NFA & GetNFA() { return nfa; }
+    const std::set<int> & GetStateSet() const { return state_set; }
+
+    void SetStateSet(const std::set<int> & in) { state_set = in; }
+
     void Next(char sym) {
-      state = nfa.GetNext(sym, state);
+      state_set = nfa.GetNext(sym, state_set);
     }
 
     void Print() {
       std::cout << "cur states:";
-      for (int s : state) {
+      for (int s : state_set) {
         std::cout << " " << s;
       }
       std::cout << std::endl;
