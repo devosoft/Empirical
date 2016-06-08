d8b1d5d5fb07b6d33e8e8962f21190fb18502012
Charles Ofria <charles.ofria@gmail.com>
2016-06-08 11:52:17 -0400

    Initial example DFA.cc

---
+++ examples/tools/DFA.cc
@@ -0,0 +1,17 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some examples code for using emp::NFA
+
+#include <iostream>
+
+#include "../../tools/DFA.h"
+
+int main()
+{
+  std::cout << "Ping!" << std::endl;
+
+  emp::DFA dfa(10);
+}
