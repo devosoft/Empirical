941e97cfaf3642ed2c7abeedb948a62500bbb261
Steven Jorgensen <spjorgensen94@gmail.com>
2016-06-09 08:35:04 -0700

    Added configs to grid and mixed files

--- examples/evo/grid-exp.cc
+++ examples/evo/grid-exp.cc
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 
+#include "../../config/ArgManager.h"
 #include "../../evo/NK.h"
 #include "../../evo/World.h"
 #include "../../tools/BitSet.h"
@@ -15,20 +16,39 @@
 #include "../../evo/EvoStats.h"
 #include "../../evo/StatsManager.h"
 
-// k controls # of hills in the fitness landscape
-constexpr int K = 0;
-constexpr int N = 10;
-constexpr double MUTATION_RATE = 0.005;
-
-constexpr int TOURNAMENT_SIZE = 20;
-constexpr int POP_SIZE = 300;
-constexpr int UD_COUNT = 1000;
+EMP_BUILD_CONFIG( NKConfig,
+  GROUP(DEFAULT, "Default settings for NK model"),
+  VALUE(K, int, 10, "Level of epistasis in the NK model"),
+  VALUE(N, int, 50, "Number of bits in each organisms (must be > K)"), ALIAS(GENOME_SIZE),
+  VALUE(SEED, int, 0, "Random number seed (0 for based on time)"),
+  CONST(POP_SIZE, int, 1000, "Number of organisms in the popoulation."),
+  VALUE(MAX_GENS, int, 2000, "How many generations should we process?"),
+  VALUE(MUT_COUNT, double, 0.005, "How many bit positions should be randomized?"), ALIAS(NUM_MUTS),
+  VALUE(TOUR_SIZE, int, 20, "How many organisms should be picked in each Tournament?"),
+)
 
 using BitOrg = emp::BitVector;
 
 int main(int argc, char* argv[])
 {
-  emp::Random random;
+  NKConfig config;
+  config.Read("Grid.cfg");
+
+  auto args = emp::cl::ArgManager(argc, argv);
+  if (args.ProcessConfigOptions(config, std::cout, "Grid.cfg", "NK-macros.h") == false) exit(0);
+  if (args.TestUnknown() == false) exit(0);  // If there are leftover args, throw an error.
+
+  // k controls # of hills in the fitness landscape
+  const int K = config.K();
+  const int N = config.N();
+  const double MUTATION_RATE = config.MUT_COUNT();
+
+  const int TOURNAMENT_SIZE = config.TOUR_SIZE();
+  const int POP_SIZE = config.POP_SIZE();
+  const int UD_COUNT = config.MAX_GENS();
+
+
+  emp::Random random(config.SEED());
   emp::evo::NKLandscape landscape(N, K, random);
 
   std::string prefix;
@@ -72,7 +92,7 @@ int main(int argc, char* argv[])
   // mutation function:
   // for every site in the gnome there is a MUTATION_RATE chance that the 
   // site will flip it's value.
-  grid_pop.SetDefaultMutateFun( [](BitOrg* org, emp::Random& random) {
+  grid_pop.SetDefaultMutateFun( [MUTATION_RATE, N](BitOrg* org, emp::Random& random) {
     bool mutated = false;    
       for (size_t site = 0; site < N; site++) {
         if (random.P(MUTATION_RATE)) {
--- examples/evo/mixed-exp.cc
+++ examples/evo/mixed-exp.cc
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 
+#include "../../config/ArgManager.h"
 #include "../../evo/NK.h"
 #include "../../evo/World.h"
 #include "../../tools/BitSet.h"
@@ -15,14 +16,17 @@
 #include "../../evo/EvoStats.h"
 #include "../../evo/StatsManager.h"
 
-// k controls # of hills in the fitness landscape
-constexpr int K = 0;
-constexpr int N = 10;
-constexpr double MUTATION_RATE = 0.005;
+EMP_BUILD_CONFIG( NKConfig,
+  GROUP(DEFAULT, "Default settings for NK model"),
+  VALUE(K, int, 10, "Level of epistasis in the NK model"),
+  VALUE(N, int, 50, "Number of bits in each organisms (must be > K)"), ALIAS(GENOME_SIZE),
+  VALUE(SEED, int, 0, "Random number seed (0 for based on time)"),
+  CONST(POP_SIZE, int, 1000, "Number of organisms in the popoulation."),
+  VALUE(MAX_GENS, int, 2000, "How many generations should we process?"),
+  VALUE(MUT_COUNT, double, 0.005, "How many bit positions should be randomized?"), ALIAS(NUM_MUTS),
+  VALUE(TOUR_SIZE, int, 20, "How many organisms should be picked in each Tournament?"),
+)
 
-constexpr int TOURNAMENT_SIZE = 20;
-constexpr int POP_SIZE = 300;
-constexpr int UD_COUNT = 1000;
 
 using BitOrg = emp::BitVector;
 
@@ -31,7 +35,23 @@ using MixedWorld = emp::evo::World<ORG, emp::evo::PopulationManager_Base<ORG>>;
 
 int main(int argc, char* argv[])
 {
-  emp::Random random;
+  NKConfig config;
+  config.Read("Mixed.cfg");
+
+  auto args = emp::cl::ArgManager(argc, argv);
+  if (args.ProcessConfigOptions(config, std::cout, "Mixed.cfg", "NK-macros.h") == false) exit(0);
+  if (args.TestUnknown() == false) exit(0);  // If there are leftover args, throw an error.
+
+  // k controls # of hills in the fitness landscape
+  const int K = config.K();
+  const int N = config.N();
+  const double MUTATION_RATE = config.MUT_COUNT();
+
+  const int TOURNAMENT_SIZE = config.TOUR_SIZE();
+  const int POP_SIZE = config.POP_SIZE();
+  const int UD_COUNT = config.MAX_GENS();
+
+  emp::Random random(config.SEED());
   emp::evo::NKLandscape landscape(N, K, random);
 
   std::string prefix;
@@ -74,7 +94,7 @@ int main(int argc, char* argv[])
   // mutation function:
   // for every site in the gnome there is a MUTATION_RATE chance that the 
   // site will flip it's value.
-  mixed_pop.SetDefaultMutateFun( [](BitOrg* org, emp::Random& random) {
+  mixed_pop.SetDefaultMutateFun( [MUTATION_RATE, N](BitOrg* org, emp::Random& random) {
     bool mutated = false;    
       for (size_t site = 0; site < N; site++) {
         if (random.P(MUTATION_RATE)) {
