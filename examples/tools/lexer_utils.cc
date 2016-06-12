80d7e29be899fc9b6414ef67a068428d3f006d26
Charles Ofria <charles.ofria@gmail.com>
2016-06-12 16:30:46 -0400

    Initial example setup of NFA

--- examples/tools/lexer_utils.cc
+++ examples/tools/lexer_utils.cc
@@ -12,6 +12,15 @@
 #include "../../tools/NFA.h"
 #include "../../tools/lexer_utils.h"
 
+void TestNFA(const emp::NFA & nfa, const std::string & str) {
+  emp::NFA_State nfa_state(nfa);
+  nfa_state.Next(str);
+  std::cout << "String: " << str
+            << "  valid=" << nfa_state.IsActive()
+            << "  stop=" << nfa_state.IsStop()
+            << std::endl;
+}
+
 int main()
 {
   emp::NFA nfa2c(3);  // Must have two c's with any number of a's or b's.
@@ -21,4 +30,14 @@ int main()
   nfa2c.AddTransition(1,2,"c");
   nfa2c.AddTransition(2,2,"ab");
   nfa2c.SetStop(2);
+
+  TestNFA(nfa2c, "cc");
+  TestNFA(nfa2c, "ccc");
+  TestNFA(nfa2c, "ababcbc");
+  TestNFA(nfa2c, "cbabab");
+  TestNFA(nfa2c, "ccbabab");
+  TestNFA(nfa2c, "ccbababc");
+
+
+  auto dfa2c = to_DFA(nfa2c);
 }
