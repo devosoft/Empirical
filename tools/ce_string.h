eb22a92e50d13f3698b6fe19955dc9fb86ee5571
Charles Ofria <charles.ofria@gmail.com>
2016-06-25 15:20:48 -0400

    Added inequality operators to ce_string.h; started using size_t.

--- tools/ce_string.h
+++ tools/ce_string.h
@@ -4,6 +4,8 @@
 //
 //  ce_string defines a limited string object for use within a constexpr class or function.
 //
+//  Developer note:
+//   @CAO: Not ready from prime time-- for some reason I can't get Set to work as constexpr.
 //  REQUIRES: C++14
 
 #ifndef EMP_CS_STRING_HH
@@ -15,30 +17,38 @@
 namespace emp {
 
   class ce_string {
+  public:
+    using size_t = std::size_t;
+
   private:
     const char * m_str;
-    int m_size;
+    size_t m_size;
 
   public:
-    template<std::size_t N>
+    template<size_t N>
     constexpr ce_string(const char (&in)[N]) : m_str(in), m_size(N-1) { ; }
     constexpr ce_string(const ce_string & in) = default;
 
-    // constexpr ce_string & operator=(const ce_string & S) {
-    //   for (int i=0; i < SIZE; i++) str[i] = S[i];
-    //   return *this;
-    // }
+    constexpr bool operator==(const ce_string & in) const {
+      if (m_size != in.m_size) return false;
+      for (auto i = 0; i < m_size; i++) if (m_str[i] != in.m_str[i]) return false;
+      return true;
+    }
+    constexpr bool operator!=(const ce_string & in) const { return !(*this == in); }
+    constexpr bool operator<(const ce_string & in) const {
+      auto min_size = std::min(m_size, in.m_size);
+      for (auto i = 0; i < min_size; i++) {
+        if (m_str[i] != in.m_str[i]) return (m_str[i] < in.m_str[i]);
+      }
+      return m_size < in.m_size;
+    }
+    constexpr bool operator>(const ce_string & in) const { return (in < *this); }
+    constexpr bool operator<=(const ce_string & in) const { return !(in < *this); }
+    constexpr bool operator>=(const ce_string & in) const { return !(*this < in); }
 
-    constexpr int size() const { return m_size; }
+    constexpr size_t size() const { return m_size; }
 
-    template<std::size_t N>
-    constexpr bool Set(const char (&in)[N]) { m_str = in; m_size = N; }
-
-    // constexpr void Set(const int pos, const char val) {
-    //   // static_assert(pos < m_size, "ce_array index out of range.");
-    //   return m_str[pos] = val;
-    // }
-    constexpr char operator[](const int pos) const {
+    constexpr char operator[](const size_t pos) const {
       // static_assert(pos < m_size, "ce_array index out of range.");
       return m_str[pos];
     }
