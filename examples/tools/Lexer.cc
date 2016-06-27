e7e22e6dbb314a3b11f1233ddb25591514892b08
Charles Ofria <charles.ofria@gmail.com>
2016-06-26 23:23:18 -0400

    Added a Lexer example file.

---
+++ examples/tools/Lexer.cc
@@ -0,0 +1,30 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some examples code for using emp::Lexer
+
+#include <iostream>
+
+#include "../../tools/Lexer.h"
+
+int main()
+{
+  emp::Lexer lexer;
+  lexer.AddToken("Integer", "[0-9]+");
+  lexer.AddToken("Float", "[0-9]*.[0-9]+");
+  lexer.AddToken("Lower", "[a-z]+");
+  lexer.AddToken("Upper", "[A-Z]+");
+  lexer.AddToken("Mixed", "[a-zA-Z]+");
+  lexer.AddToken("Whitespace", "[ \t\n\r]");
+
+  std::stringstream ss;
+  ss << "This is a 123 TEST.";
+
+  int token_id = 1;
+  while (token_id >= 0) {
+    token_id = lexer.Process(ss);
+    std::cout << lexer.GetTokenName(token_id) << " : " << lexer.GetLexeme() << std::endl;
+  }
+}
