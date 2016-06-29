95d825751fc9bac18aa78027b566c4c20317880a
Charles Ofria <charles.ofria@gmail.com>
2016-06-28 23:26:09 -0400

    Expanded Lexer.cc example code.

--- examples/tools/Lexer.cc
+++ examples/tools/Lexer.cc
@@ -13,18 +13,21 @@ int main()
 {
   emp::Lexer lexer;
   lexer.AddToken("Integer", "[0-9]+");
-  lexer.AddToken("Float", "[0-9]*.[0-9]+");
+  lexer.AddToken("Float", "[0-9]*\\.[0-9]+");
   lexer.AddToken("Lower", "[a-z]+");
   lexer.AddToken("Upper", "[A-Z]+");
   lexer.AddToken("Mixed", "[a-zA-Z]+");
   lexer.AddToken("Whitespace", "[ \t\n\r]");
+  lexer.AddToken("Other", ".");
+
+  lexer.Print();
 
   std::stringstream ss;
-  ss << "This is a 123 TEST.";
+  ss << "This is a 123 TEST.  It should also have 1. .2 123.456 789 FLOATING point NUMbers!";
 
   int token_id = 1;
   while (token_id >= 0) {
     token_id = lexer.Process(ss);
-    std::cout << lexer.GetTokenName(token_id) << " : " << lexer.GetLexeme() << std::endl;
+    std::cout << lexer.GetTokenName(token_id) << " : \"" << lexer.GetLexeme() << "\"" << std::endl;
   }
 }
