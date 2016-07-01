1b8e1b7ca33357f8cf273cd53497f147fc4f9634
Charles Ofria <charles.ofria@gmail.com>
2016-06-30 23:31:18 -0400

    Example file for general purpose parser

---
+++ examples/tools/Parser.cc
@@ -0,0 +1,26 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some examples code for using emp::Parser
+
+#include <iostream>
+
+#include "../../tools/Lexer.h"
+#include "../../tools/Parser.h"
+
+int main()
+{
+  emp::Lexer lexer;
+  lexer.AddToken("Integer", "[0-9]+");
+  lexer.AddToken("Float", "[0-9]*\\.[0-9]+");
+  lexer.AddToken("Lower", "[a-z]+");
+  lexer.AddToken("Upper", "[A-Z]+");
+  lexer.AddToken("Mixed", "[a-zA-Z]+");
+  lexer.AddToken("Whitespace", "[ \t\n\r]");
+  lexer.AddToken("Other", ".");
+
+  emp::Parser parser(lexer);
+  
+}
