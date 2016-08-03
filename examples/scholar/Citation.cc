96cfb7fbff62420fc52b6fcc52db615e91659205
Charles Ofria <charles.ofria@gmail.com>
2016-08-03 16:50:21 -0400

    Actually build a citation in Citation.cc example.

--- examples/scholar/Citation.cc
+++ examples/scholar/Citation.cc
@@ -6,4 +6,7 @@ int main()
 {
   std::cout << "Testing..." << std::endl;
   emp::Citation cite1(emp::Citation::ARTICLE);
+  emp::Author aut1("I", "Literate");
+  cite1.SetTitle("This it the Best Title").SetJournal("The Best Journal").SetPages(100,200)
+       .AddAuthor(aut1).AddAuthor("Me", "Myself", "I");
 }
