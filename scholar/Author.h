f071a3df73394006e4334ac7589131a38752ac6a
Charles Ofria <charles.ofria@gmail.com>
2016-08-04 22:44:50 -0400

    Allow any number of middle names for an author.

--- scholar/Author.h
+++ scholar/Author.h
@@ -7,11 +7,11 @@
 //
 //
 //  Developer notes:
-//  * Any number of middle names should be allowed
 //  * Prefixs (Dr., Prof., etc) and Suffixes (Jr., Sr., III, etc.) should be allowed.
 //  * Most parts of names can be auto-detected.
 //  * GetName() (with formatting) needs to be implemented (notes below)
 //  * Set functiions need to be added for precise configuration.
+//  * Consider using ce_string for inputs?  And making other aspects const experession?
 
 #ifndef EMP_AUTHOR_H
 #define EMP_AUTHOR_H
@@ -23,11 +23,11 @@ namespace emp {
   class Author {
   private:
     std::string first_name;
-    std::string middle_name;
+    emp::vector<std::string> middle_names;
     std::string last_name;
   public:
     Author(const std::string & first, const std::string & middle, const std::string & last)
-      : first_name(first), middle_name(middle), last_name(last) { ; }
+      : first_name(first), last_name(last) { middle_names.push_back(middle); }
     Author(const std::string & first, const std::string & last)
       : first_name(first), last_name(last) { ; }
     Author(const std::string & last)
@@ -38,30 +38,42 @@ namespace emp {
 
     bool operator==(const Author & other) const {
       return (first_name == other.first_name) &&
-        (middle_name == other.middle_name) &&
+        (middle_names == other.middle_names) &&
         (last_name == other.last_name);
     }
     bool operator!=(const Author & other) const { return !(*this == other); }
     bool operator<(const Author & other) const {
       if (last_name != other.last_name) return (last_name < other.last_name);
       if (first_name != other.first_name) return (first_name < other.first_name);
-      return (middle_name < other.middle_name);
+      for (auto i = 0; i < middle_names.size(); i++) {
+        if (other.middle_names.size() <= i) return false;
+        if (middle_names[i] != other.middle_names[i]) {
+          return (middle_names[i] < other.middle_names[i]);
+        }
+      }
+      if (middle_names.size() < other.middle_names.size()) return true;
+      return false; // Must be equal!
     }
     bool operator>(const Author & other) const { return other < *this; }
     bool operator>=(const Author & other) const { return !(*this < other); }
     bool operator<=(const Author & other) const { return !(*this > other); }
 
     bool HasFirstName() const { return first_name.size(); }
-    bool HasMiddleName() const { return middle_name.size(); }
+    bool HasMiddleName() const { return middle_names.size(); }
     bool HasLastName() const { return last_name.size(); }
 
     const std::string & GetFirstName() const { return first_name; }
-    const std::string & GetMiddleName() const { return middle_name; }
+    const std::string & GetMiddleName(int id=0) const {
+      if (middle_names.size() == 0) return emp::empty_string();
+      return middle_names[id];
+    }
     const std::string & GetLastName() const { return last_name; }
     std::string GetFullName() const {
       std::string full_name(first_name);
-      if (full_name.size() && HasMiddleName()) full_name += " ";
-      full_name += middle_name;
+      for (const auto & middle_name : middle_names) {
+        if (full_name.size()) full_name += " ";
+        full_name += middle_name;
+      }
       if (full_name.size() && HasLastName()) full_name += " ";
       full_name += last_name;
       return full_name;
@@ -70,19 +82,25 @@ namespace emp {
       std::string full_name(last_name);
       if (full_name.size() && HasFirstName()) full_name += ", ";
       full_name += first_name;
-      if (full_name.size() && HasMiddleName()) full_name += " ";
-      full_name += middle_name;
+      for (const auto & middle_name : middle_names) {
+        if (full_name.size()) full_name += " ";
+        full_name += middle_name;
+      }
       return full_name;
     }
 
-    char GetFirstInitial() const { return HasFirstName() ? first_name[0] : 0; }
-    char GetMiddleInitial() const { return HasMiddleName() ? middle_name[0] : 0; }
-    char GetLastInitial() const { return HasLastName() ? last_name[0] : 0; }
+    std::string GetFirstInitial() const { return HasFirstName() ? to_string(first_name[0]) : emp::empty_string(); }
+    std::string GetMiddleInitials() const {
+      std::string out;
+      for (const auto & m : middle_names) out += m[0];
+      return out;
+    }
+    std::string GetLastInitial() const { return HasLastName() ? to_string(last_name[0]) : emp::empty_string(); }
     std::string GetInitials() const {
       std::string inits;
-      if (HasFirstName()) inits.push_back(first_name[0]);
-      if (HasMiddleName()) inits.push_back(middle_name[0]);
-      if (HasLastName()) inits.push_back(last_name[0]);
+      inits += GetFirstInitial();
+      inits += GetMiddleInitials();
+      inits += GetLastInitial();
       return inits;
     }
 
