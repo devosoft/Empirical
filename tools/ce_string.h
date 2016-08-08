e5b03e885ccfa80a324a3a052cf88cb6859d1311
Charles Ofria <charles.ofria@gmail.com>
2016-08-07 23:08:48 -0400

    Made ce_string compatable with c++11

--- tools/ce_string.h
+++ tools/ce_string.h
@@ -24,23 +24,33 @@ namespace emp {
     const char * m_str;
     size_t m_size;
 
+    constexpr bool IsEqual(const ce_string & in, int first=0) const {
+      return (m_size == in.m_size) &&
+             ((m_size == first) || (m_str[first] == in.m_str[first] && IsEqual(in, first+1)));
+    }
+    constexpr bool IsLess(const ce_string & in, int first=0) const {
+      return (first == in.m_size) ? false :
+             ((first == m_size) || (m_str[first] < in.m_str[first] || IsLess(in, first+1)));
+    }
   public:
     template<size_t N>
     constexpr ce_string(const char (&in)[N]) : m_str(in), m_size(N-1) { ; }
     constexpr ce_string(const ce_string & in) = default;
 
     constexpr bool operator==(const ce_string & in) const {
-      if (m_size != in.m_size) return false;
-      for (auto i = 0; i < m_size; i++) if (m_str[i] != in.m_str[i]) return false;
-      return true;
+      return IsEqual(in);
+      // if (m_size != in.m_size) return false;
+      // for (auto i = 0; i < m_size; i++) if (m_str[i] != in.m_str[i]) return false;
+      // return true;
     }
     constexpr bool operator!=(const ce_string & in) const { return !(*this == in); }
     constexpr bool operator<(const ce_string & in) const {
-      auto min_size = std::min(m_size, in.m_size);
-      for (auto i = 0; i < min_size; i++) {
-        if (m_str[i] != in.m_str[i]) return (m_str[i] < in.m_str[i]);
-      }
-      return m_size < in.m_size;
+      return IsLess(in);
+      // auto min_size = std::min(m_size, in.m_size);
+      // for (auto i = 0; i < min_size; i++) {
+      //   if (m_str[i] != in.m_str[i]) return (m_str[i] < in.m_str[i]);
+      // }
+      // return m_size < in.m_size;
     }
     constexpr bool operator>(const ce_string & in) const { return (in < *this); }
     constexpr bool operator<=(const ce_string & in) const { return !(in < *this); }
