3bc44bfcf590f86e278b58c5065f55bbecf0f0c9
Charles Ofria <charles.ofria@gmail.com>
2016-08-08 22:13:42 -0400

    Added errors.cc to provide example code for using errors.

---
+++ examples/tools/errors.cc
@@ -0,0 +1,15 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some examples code demonstrating use of the error system.
+
+#include <iostream>
+
+#include "../../tools/errors.h"
+
+int main()
+{
+  std::cout << "Testing." << std::endl;
+}
