db8a5729f45471ce9552adf487a17fabcefc1229
Charles Ofria <charles.ofria@gmail.com>
2016-08-10 11:28:07 -0400

    Added information theory tools file (so far, just entropy).

---
+++ tools/info_theory.h
@@ -0,0 +1,33 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Tools to calculate Information Theory metrics.
+
+#ifndef EMP_INFO_THEORY_H
+#define EMP_INFO_THEORY_H
+
+#include <cmath>
+
+#include "vector.h"
+
+namespace emp {
+
+  // Entropy assumes that you are given a vector of weights, calculates the probability of
+  // each, and returns the entropy.
+  template<typename T>
+  double Entropy(const emp::vector<T> & weights) {
+    double total = 0.0;
+    double entropy = 0.0;
+    for (auto w : weights) total += w;
+    for (auto w : weights) {
+      double p = ((double) w) / total;
+      entropy -= p * log2(p);
+    }
+    return entropy;
+  }
+
+}
+
+#endif
