489c3c82c331e2859d8f2dd6e88130fd126f73a5
Charles Ofria <charles.ofria@gmail.com>
2016-08-15 21:48:14 -0400

    Added example of logs of different bases.

--- examples/tools/const_utils.cc
+++ examples/tools/const_utils.cc
@@ -14,6 +14,7 @@ int main()
   for (int i = 1; i <= 20; i++) {
     std::cout << "Log2(" << i << ") = " << emp::constant::Log2(i)
       //              << "   Log2(" << i << ".0) = " << emp::constant::Log2((double) i)
+              << "   Log(10, " << i << ") = " << emp::constant::Log(10, i)
               << std::endl;
   }
 }
