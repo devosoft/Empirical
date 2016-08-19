27d72dd2370ac4436f2681b6337befa1c28116c6
Jake Fenton <bocajnotnef@gmail.com>
2016-08-19 14:38:58 -0400

    Changed mutations to come from standard dist

--- evo/QuorumOrg.h
+++ evo/QuorumOrg.h
@@ -211,7 +211,8 @@ public:
   // the aforementioned mutate function
   bool mutate (Random & random) {
     if (state.mutate) {
-      state.genome.co_op_prob += random.GetRandNormal(0, mutation_amount);
+      //state.genome.co_op_prob += random.GetRandNormal(0, mutation_amount);
+      state.genome.co_op_prob += random.GetDouble(0, 1);
       if (state.genome.co_op_prob < 0) {state.genome.co_op_prob = 0;}
       else if (state.genome.co_op_prob > 1) { state.genome.co_op_prob = 1;}
 
