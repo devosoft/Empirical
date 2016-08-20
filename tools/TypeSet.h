5ff6b609660aa645e3175476087add769a546c96
Charles Ofria <charles.ofria@gmail.com>
2016-08-19 22:55:47 -0400

    More tools to manipulate type sets.

--- tools/TypeSet.h
+++ tools/TypeSet.h
@@ -11,20 +11,23 @@
 
 namespace emp {
 
-  template <typename... Ts>
+  template <typename T1, typename... Ts>
   struct TypeSet {
     // Type ID's can be retrieved with
     //   GetID<my_type>() to get the ID associated with specific type my_type
     //   GetID(owner) to get the ID associated with the type of 'owner'
-    template <typename T> constexpr static int GetID() { return get_type_index<T,Ts...>(); }
-    template <typename T> constexpr static int GetID(const T &) { return get_type_index<T,Ts...>(); }
+    template <typename T> constexpr static int GetID() { return get_type_index<T,T1,Ts...>(); }
+    template <typename T> constexpr static int GetID(const T &) { return get_type_index<T,T1,Ts...>(); }
 
-    constexpr static int GetSize() { return sizeof...(Ts); }
+    constexpr static int GetSize() { return 1+sizeof...(Ts); }
 
-    constexpr static bool IsUnique() { return has_unique_types<Ts...>(); }
+    constexpr static bool IsEmpty() { return false; }
+    constexpr static bool IsUnique() { return has_unique_types<T1,Ts...>(); }
 
-    template <typename T>
-    using AddType = TypeSet<Ts...,T>;
+    template <typename T> using add_t = TypeSet<T1,Ts...,T>;
+    using first_t = T1;
+    using last_t = last_type<T1,Ts...>;
+    using pop_front_t = TypeSet<Ts...>;
   };
 
 };
