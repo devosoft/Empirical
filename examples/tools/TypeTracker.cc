e79fe5992bb06e9024353924a164869f8a3cdb48
Charles Ofria <charles.ofria@gmail.com>
2016-08-25 15:04:17 -0400

    Cleaned up and streamlined both TypeTracker and example file.

--- examples/tools/TypeTracker.cc
+++ examples/tools/TypeTracker.cc
@@ -2,7 +2,6 @@
 //  Copyright (C) Michigan State University, 2016.
 //  Released under the MIT Software license; see doc/LICENSE
 //
-//
 //  Some example code for using TypeTracker
 
 #include <iostream>
@@ -11,26 +10,27 @@
 #include "../../tools/TypeTracker.h"
 #include "../../tools/vector.h"
 
-void fun_int_int(int x, int y) { std::cout << x+y << std::endl; }
-void fun_int_double(int x, double y) { std::cout << y * (double) x << std::endl; }
-void fun_string_double(std::string x, int y) { std::cout << x << " : " << y << std::endl; }
+// Build some sample functions that we want called by type.
+void fun_int_int(int x, int y) { std::cout << x << "," << y << " : " << x+y << std::endl; }
+void fun_int_double(int x, double y) { std::cout << x << "," << y << " : " << y * (double) x << std::endl; }
+void fun_string_double(std::string x, int y) { std::cout << x << " -> " << y << std::endl; }
 
 int main()
 {
-  std::cout << "Testing!" << std::endl;
+  using tracker_t = emp::TypeTracker<int, std::string, double>;  // Tracker must know all types.
+  tracker_t tt;                                                  // Build the tracker.
+  tt.AddFunction(fun_int_int);                                   // Add the functions...
+  tt.AddFunction(fun_int_double);
+  tt.AddFunction(fun_string_double);
 
-  using tracker_t = emp::TypeTracker<int, std::string, double>;
-  tracker_t tt;
-  tt.AddFunction<int,int>(fun_int_int);
-  tt.AddFunction<int,double>(fun_int_double);
-  tt.AddFunction<std::string,int>(fun_string_double);
-
-  emp::vector< tracker_t::base_t *> objs;
+  // Crate a vector of objects of the generic, tracked type that will need to be converted back.
+  emp::vector<emp::TrackedType *> objs;
   objs.push_back( tt.New<int>(12) );
   objs.push_back( tt.New<int>(100) );
   objs.push_back( tt.New<double>(1.25) );
   objs.push_back( tt.New<std::string>("Ping!") );
 
+  // Now run the appropriate function for any pair of objects.  Undefined ones should be skipped.
   for (auto x : objs) {
     for (auto y : objs) {
       tt.RunFunction(x,y);
--- tools/TypeTracker.h
+++ tools/TypeTracker.h
@@ -10,12 +10,12 @@
 
 #include "assert.h"
 #include "meta.h"
-#include "vector.h"
+#include "array.h"
 
 namespace emp {
 
   // The base class of the types to be tracked.
-  struct TypeTracker_Base {
+  struct TrackedType {
     virtual int GetTypeTrackerID() const noexcept {
       emp_assert(false); // Should never run GetTypeTrackerID from the base class!
       return -1;
@@ -25,13 +25,13 @@ namespace emp {
   // The derived classes to be tracked should inherit from TypeTracker_Class<ID>
   // where ID is the position in the type list for TypeTracker.  Note that this value can
   // be obtained dyanmically at compile type by using TypeTracker<...>::GetID<TYPE>()
-  template <typename OWNER, int ID>
-  struct TypeTracker_Class : public TypeTracker_Base {
-    using owner_t = OWNER;
-    OWNER owner;
+  template <typename REAL_T, int ID>
+  struct TypeTracker_Class : public TrackedType {
+    using real_t = REAL_T;
+    REAL_T value;
 
-    TypeTracker_Class(const OWNER & in) : owner(in) { ; }
-    TypeTracker_Class(OWNER && in) : owner(std::forward<OWNER>(in)) { ; }
+    TypeTracker_Class(const REAL_T & in) : value(in) { ; }
+    TypeTracker_Class(REAL_T && in) : value(std::forward<REAL_T>(in)) { ; }
     TypeTracker_Class(const TypeTracker_Class &) = default;
     TypeTracker_Class(TypeTracker_Class &&) = default;
     TypeTracker_Class & operator=(const TypeTracker_Class &) = default;
@@ -42,38 +42,42 @@ namespace emp {
   template <typename... TYPES>
   struct TypeTracker {
     using this_t = TypeTracker<TYPES...>;
-    using base_t = TypeTracker_Base;
-    template <typename OWNER>
-    using wrap_t = TypeTracker_Class< OWNER, get_type_index<OWNER,TYPES...>() >;
+    template <typename REAL_T>
+    using wrap_t = TypeTracker_Class< REAL_T, get_type_index<REAL_T,TYPES...>() >;
 
     constexpr static int GetNumTypes() { return sizeof...(TYPES)+1; }
     constexpr static int GetNumCombos() { return GetNumTypes() * GetNumTypes(); }
 
-    emp::vector< std::function<void(base_t*, base_t*)> > redirects;
+    emp::array< std::function<void(TrackedType*, TrackedType*)>, GetNumCombos() > redirects;
 
-    TypeTracker() : redirects(GetNumCombos()) { ; }
+    TypeTracker() { ; }
     TypeTracker(const TypeTracker &) = default;
     TypeTracker(TypeTracker &&) = default;
     TypeTracker & operator=(const TypeTracker &) = default;
     TypeTracker & operator=(TypeTracker &&) = default;
 
-    template <typename OWNER>
-    wrap_t<OWNER> Wrap(OWNER && val) { return wrap_t<OWNER>(std::forward<OWNER>(val)); }
-    template <typename OWNER>
-    wrap_t<OWNER> * New(OWNER && val) { return new wrap_t<OWNER>(std::forward<OWNER>(val)); }
+    template <typename REAL_T>
+    wrap_t<REAL_T> Wrap(REAL_T && val) { return wrap_t<REAL_T>(std::forward<REAL_T>(val)); }
+    template <typename REAL_T>
+    wrap_t<REAL_T> * New(REAL_T && val) { return new wrap_t<REAL_T>(std::forward<REAL_T>(val)); }
 
     template <typename T1, typename T2>
     this_t & AddFunction( std::function<void(T1,T2)> fun ) {
       constexpr int ID1 = get_type_index<T1,TYPES...>();
       constexpr int ID2 = get_type_index<T2,TYPES...>();
       constexpr int POS = ID1 * GetNumTypes() + ID2;
-      redirects[POS] = [fun](base_t* b1, base_t* b2) {
-        fun( ((wrap_t<T1> *) b1)->owner, ((wrap_t<T2> *) b2)->owner );
+      redirects[POS] = [fun](TrackedType* b1, TrackedType* b2) {
+        fun( ((wrap_t<T1> *) b1)->value, ((wrap_t<T2> *) b2)->value );
       };
       return *this;
     }
 
-    void RunFunction( base_t * b1, base_t * b2 ) {
+    template <typename T1, typename T2>
+    this_t & AddFunction( void (*fun)(T1,T2) ) {
+      return AddFunction( std::function<void(T1,T2)>(fun) );
+    }
+
+    void RunFunction( TrackedType * b1, TrackedType * b2 ) {
       const int id1 = b1->GetTypeTrackerID();
       const int id2 = b2->GetTypeTrackerID();
       const int pos = id1 * GetNumTypes() + id2;
