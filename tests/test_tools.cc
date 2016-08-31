dad3c7deccc660a0ea7febcd63a8dd5738627ee8
Charles Ofria <charles.ofria@gmail.com>
2016-08-30 22:45:11 -0400

    Added unit tests for map_utils.h

--- tests/test_tools.cc
+++ tests/test_tools.cc
@@ -31,6 +31,7 @@
 #include "../tools/info_theory.h"
 #include "../tools/macro_math.h"
 #include "../tools/macros.h"
+#include "../tools/map_utils.h"
 #include "../tools/mem_track.h"
 #include "../tools/meta.h"
 #include "../tools/reflection.h"
@@ -871,6 +872,25 @@ TEST_CASE("Test macros", "[tools]")
   EMP_TEST_MACRO( EMP_STRINGIFY_EACH(some, words), "\"some\" , \"words\"" );
 }
 
+TEST_CASE("Test map_utils", "[tools]")
+{
+  std::map<int, char> test_map;
+  test_map[0] = 'a';
+  test_map[4] = 'e';
+  test_map[8] = 'i';
+  test_map[14] = 'o';
+  test_map[20] = 'u';
+
+  REQUIRE( emp::Has(test_map, 8) == true );
+  REQUIRE( emp::Has(test_map, 18) == false );
+  REQUIRE( emp::Find(test_map, 14, 'x') == 'o'); // 14 should be there as 'o'
+  REQUIRE( emp::Find(test_map, 15, 'x') == 'x'); // 15 shouldn't be there, so return default.
+  REQUIRE( emp::Has(test_map, 15) == false );    // Make sure 15 hasn't been added to the map.
+
+  auto flipped = emp::flip_map(test_map);        // Make sure we can reverse the map.
+  REQUIRE( emp::Has(flipped, 'u') == true);      // And the reversed map should have proper info.
+  REQUIRE( emp::Has(flipped, 'x') == false);
+}
 
 struct TestClass1 {
   TestClass1() {
