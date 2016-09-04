34b7426f992c1c323941d8916807167e7be62c33
Charles Ofria <charles.ofria@gmail.com>
2016-09-03 22:48:33 -0400

    Added in lexer tests.

--- tests/test_tools.cc
+++ tests/test_tools.cc
@@ -20,6 +20,7 @@
 #include "../tools/Graph.h"
 #include "../tools/NFA.h"
 #include "../tools/Ptr.h"
+#include "../tools/RegEx.h"
 #include "../tools/Random.h"
 // #include "../tools/Trait.h"
 
@@ -31,6 +32,7 @@
 #include "../tools/graph_utils.h"
 //#include "../tools/grid.h"
 #include "../tools/info_theory.h"
+#include "../tools/lexer_utils.h"
 #include "../tools/macro_math.h"
 #include "../tools/macros.h"
 #include "../tools/map_utils.h"
@@ -588,6 +590,65 @@ TEST_CASE("Test info_theory", "[tools]")
   REQUIRE( emp::Entropy(dweights) == 2.5 );
 
   REQUIRE( emp::Entropy2(0.5) == 1.0 );
+}
+
+TEST_CASE("Test lexer_utils", "[tools]")
+{
+    emp::NFA nfa2c(3);  // Must have zero or two c's with any number of a's or b's.
+    nfa2c.AddTransition(0,0,"ab");
+    nfa2c.AddTransition(0,1,"c");
+    nfa2c.AddTransition(1,1,"ab");
+    nfa2c.AddTransition(1,2,"c");
+    nfa2c.AddTransition(2,2,"ab");
+    nfa2c.AddFreeTransition(0,2);
+    nfa2c.SetStop(2);
+
+    emp::RegEx re2f("[de]*f[de]*f[de]*");
+    // emp::RegEx re2f("([de]*)f([de]*)f([de]*)");
+    emp::NFA nfa2f = to_NFA(re2f);
+    emp::DFA dfa2f = to_DFA(nfa2f);
+    REQUIRE( nfa2f.GetSize() == 12 );
+    REQUIRE( dfa2f.GetSize() == 3 );
+
+    int state;
+    state = dfa2f.Next(0, "a");        REQUIRE(state == -1); REQUIRE(dfa2f.IsStop(state) == false);
+    state = dfa2f.Next(0, "d");        REQUIRE(state == 0); REQUIRE(dfa2f.IsStop(state) == false);
+    state = dfa2f.Next(0, "defdef");   REQUIRE(state == 2); REQUIRE(dfa2f.IsStop(state) == true);
+    state = dfa2f.Next(0, "fedfed");   REQUIRE(state == 2); REQUIRE(dfa2f.IsStop(state) == true);
+    state = dfa2f.Next(0, "ffed");     REQUIRE(state == 2); REQUIRE(dfa2f.IsStop(state) == true);
+    state = dfa2f.Next(0, "edffed");   REQUIRE(state == 2); REQUIRE(dfa2f.IsStop(state) == true);
+    state = dfa2f.Next(0, "edffedf");  REQUIRE(state == -1); REQUIRE(dfa2f.IsStop(state) == false);
+    state = dfa2f.Next(0, "defed");    REQUIRE(state == 1); REQUIRE(dfa2f.IsStop(state) == false);
+    state = dfa2f.Next(0, "ff");       REQUIRE(state == 2); REQUIRE(dfa2f.IsStop(state) == true);
+
+    emp::RegEx re_lower("[a-z]+");
+    emp::RegEx re_upper("[A-Z]+");
+    emp::RegEx re_inc("[a-z]+[A-Z]+");
+    emp::NFA nfa_lower = to_NFA(re_lower);
+    emp::NFA nfa_upper = to_NFA(re_upper);
+    emp::NFA nfa_inc = to_NFA(re_inc);
+    emp::NFA nfa_all = MergeNFA(nfa_lower, nfa_upper, nfa_inc);
+    emp::DFA dfa_lower = to_DFA(nfa_lower);
+    emp::DFA dfa_upper = to_DFA(nfa_upper);
+    emp::DFA dfa_inc = to_DFA(nfa_inc);
+    emp::DFA dfa_all = to_DFA(nfa_all);
+
+    emp::NFA_State lstate(nfa_lower);
+    lstate.Reset(); lstate.Next("abc");      REQUIRE(lstate.IsActive() == true);
+    lstate.Reset(); lstate.Next("DEF");      REQUIRE(lstate.IsActive() == false);
+    lstate.Reset(); lstate.Next("abcDEF");   REQUIRE(lstate.IsActive() == false);
+    lstate.Reset(); lstate.Next("ABDdef");   REQUIRE(lstate.IsActive() == false);
+    lstate.Reset(); lstate.Next("ABCDEF");   REQUIRE(lstate.IsActive() == false);
+    lstate.Reset(); lstate.Next("abcdefghijklmnopqrstuvwxyz");  REQUIRE(lstate.IsActive() == true);
+    lstate.Reset(); lstate.Next("ABC-DEF");  REQUIRE(lstate.IsActive() == false);
+
+    REQUIRE( dfa_all.Next(0, "abc") == 2 );
+    REQUIRE( dfa_all.Next(0, "DEF") == 1 );
+    REQUIRE( dfa_all.Next(0, "abcDEF") == 3 );
+    REQUIRE( dfa_all.Next(0, "ABDdef") == -1 );
+    REQUIRE( dfa_all.Next(0, "ABCDEF") == 1 );
+    REQUIRE( dfa_all.Next(0, "abcdefghijklmnopqrstuvwxyz") == 2 );
+    REQUIRE( dfa_all.Next(0, "ABC-DEF") == -1 );
 }
 
 TEST_CASE("Test macro_math", "[tools]")
