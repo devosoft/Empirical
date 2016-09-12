446ecbcc339644e0fe9c9911237588ebe49a9652
Charles Ofria <charles.ofria@gmail.com>
2016-09-11 23:50:38 -0400

    Added example for constexpr Random number generator (requires c++14)

---
+++ examples/tools/Random14.cc
@@ -0,0 +1,15 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some examples code for using emp::Random
+
+#include <iostream>
+#include <vector>
+#include "../../tools/Random14.h"
+
+int main()
+{
+  constexpr emp14::Random random(1);
+}
