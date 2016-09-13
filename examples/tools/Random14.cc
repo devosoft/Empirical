59eed2594eba81a7ca7c889331bffa363c7b427e
Charles Ofria <charles.ofria@gmail.com>
2016-09-13 18:37:53 -0400

    Actually test constexpr random number generator.s

--- examples/tools/Random14.cc
+++ examples/tools/Random14.cc
@@ -9,7 +9,23 @@
 #include <vector>
 #include "../../tools/Random14.h"
 
+constexpr double TestConstRandom(int seed) {
+  emp14::Random random(seed);
+  return random.GetDouble(100.0,200.0);
+}
+
+constexpr double TestConstRandom(emp14::Random random) {
+  return random.GetDouble(100.0,200.0);
+}
+
 int main()
 {
-  constexpr emp14::Random random(1);
+  constexpr double x = TestConstRandom(1);
+  std::cout << "x=" << x << std::endl;
+  constexpr double y = TestConstRandom(2);
+  std::cout << "y=" << y << std::endl;
+
+  constexpr emp14::Random random(3);
+  constexpr double z = TestConstRandom(random);
+  std::cout << "z=" << z << std::endl;
 }
