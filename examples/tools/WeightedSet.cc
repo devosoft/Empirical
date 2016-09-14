44169366fc07782e4f79ca83a03b79524b14668f
Charles Ofria <charles.ofria@gmail.com>
2016-09-13 23:08:34 -0400

    Starting building WeightedSet example.

---
+++ examples/tools/WeightedSet.cc
@@ -0,0 +1,16 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some examples code for using emp::Random
+
+#include <iostream>
+#include <vector>
+#include "../../tools/Random.h"
+#include "../../tools/WeightedSet.h"
+
+int main()
+{
+  std::cout << "Ping!" << std::endl;
+}
