e5d21713d9653a0a7c741ed8e252b1ae86334395
Charles Ofria <charles.ofria@gmail.com>
2016-09-14 15:57:36 -0400

    Use indices to setup weighted set in example, and test it's output.

--- examples/tools/WeightedSet.cc
+++ examples/tools/WeightedSet.cc
@@ -6,12 +6,32 @@
 //  Some examples code for using emp::Random
 
 #include <iostream>
-#include <vector>
 #include "../../tools/Random.h"
+#include "../../tools/vector.h"
 #include "../../tools/WeightedSet.h"
 
 int main()
 {
   emp::WeightedSet test_set(10);
+  test_set[0] = 0.0;
+  test_set[1] = 0.5;
+  test_set[2] = 1.0;
+  test_set[3] = 1.5;
+  test_set[4] = 2.0;
+  test_set[5] = 2.5;
+  test_set[6] = 3.0;
+  test_set[7] = 3.5;
+  test_set[8] = 4.0;
+  test_set[9] = 4.5;
   std::cout << "Ping!" << std::endl;
+
+  emp::Random random;
+  emp::vector<int> count(10,0);
+  for (int i = 0; i < 45000; i++) {
+    int id = test_set.Index(random.GetDouble(0,test_set.GetWeight()));
+    count[id]++;
+  }
+  for (int id = 0; id < 10; id++) {
+    std::cout << id << " : " << count[id] << std::endl;
+  }
 }
