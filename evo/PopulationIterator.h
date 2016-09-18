bd76993998f1cf2a4960512385003a098a81ef67
Charles Ofria <charles.ofria@gmail.com>
2016-09-18 15:03:30 -0400

    Overhaul on spacing and headers in PopulationIterator.h

--- evo/PopulationIterator.h
+++ evo/PopulationIterator.h
@@ -1,79 +1,77 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//  This file defines iterators for use with a PopulationManager in an emp::evo::World
+
 #ifndef EMP_EVO_POP_ITER_H
 #define EMP_EVO_POP_ITER_H
 
 namespace emp{
 namespace evo{
 
-template <typename POP_MANAGER>
-class PopulationIterator{
-private:
-  using ORG = typename POP_MANAGER::value_type;
-  POP_MANAGER * pop;
-  int curr = 0;
-  int size;
+  template <typename POP_MANAGER>
+  class PopulationIterator {
+  private:
+    using ORG = typename POP_MANAGER::value_type;
 
-public:
-  //typedef  iterator;
-  //typedef ptrdiff_t difference_type;
-  //typedef size_t size_type;
-  //typedef T value_type;
-  //typedef T * pointer;
-  //typedef T & reference;
+    POP_MANAGER * pop;
+    int curr = 0;
+    int size;
 
-  PopulationIterator(POP_MANAGER * w, int ind = 0){
-    pop  = w;
-    curr = ind;
-    size = pop->size();
-    while ((curr < size) && (((*pop)[curr]) == nullptr)){
-      ++curr;
+  public:
+
+    PopulationIterator(POP_MANAGER * pm, int ind=0) : pop(pm), curr(ind) {
+      size = pop->size();
+      while ((curr < size) && (((*pop)[curr]) == nullptr)){
+        ++curr;
+      }
     }
-  }
 
-  PopulationIterator(PopulationIterator * w){
-    pop  = w->pop;
-    curr = w->curr;
-    size = w->size;
-    while (curr < size && (((*pop)[curr]) == nullptr)) {
-      ++curr;
+    PopulationIterator(PopulationIterator * w){
+      pop  = w->pop;
+      curr = w->curr;
+      size = w->size;
+      while (curr < size && (((*pop)[curr]) == nullptr)) {
+        ++curr;
+      }
     }
-  }
 
-  PopulationIterator<POP_MANAGER> & operator++() {
-    ++curr;
-    while (curr < size && (((*pop)[curr]) == nullptr)) {
+    PopulationIterator<POP_MANAGER> & operator++() {
       ++curr;
+      while (curr < size && (((*pop)[curr]) == nullptr)) {
+        ++curr;
+      }
+      return *this;
     }
-    return *this;
-  }
 
-  PopulationIterator<POP_MANAGER> & operator--() {
-    --curr;
-    while (curr < size && (((*pop)[curr]) == nullptr)) {
+    PopulationIterator<POP_MANAGER> & operator--() {
       --curr;
+      while (curr < size && (((*pop)[curr]) == nullptr)) {
+        --curr;
+      }
+      return *this;
     }
-    return *this;
-  }
 
-  bool operator==(const PopulationIterator<POP_MANAGER>& rhs){
-    return curr == rhs.curr;
-  }
+    bool operator==(const PopulationIterator<POP_MANAGER>& rhs) {
+      return curr == rhs.curr;
+    }
 
-  bool operator!=(const PopulationIterator<POP_MANAGER>& rhs){
-    return curr != rhs.curr;
-  }
+    bool operator!=(const PopulationIterator<POP_MANAGER>& rhs) {
+      return curr != rhs.curr;
+    }
 
-  ORG & operator*() {return (*pop)[curr];}
+    ORG & operator*() { return (*pop)[curr]; }
 
-  PopulationIterator<POP_MANAGER> begin() {
-    return PopulationIterator<POP_MANAGER>(pop, 0);
-  }
+    PopulationIterator<POP_MANAGER> begin() {
+      return PopulationIterator<POP_MANAGER>(pop, 0);
+    }
 
-  PopulationIterator<POP_MANAGER> end() {
-    return PopulationIterator<POP_MANAGER>(pop, size);
-  }
+    PopulationIterator<POP_MANAGER> end() {
+      return PopulationIterator<POP_MANAGER>(pop, size);
+    }
 
-
-};
+  };
 }
 }
 #endif
