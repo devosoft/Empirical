582c320d4650f8e3e21d0dd1a41fc2766114983f
Emily Dolson <EmilyLDolson@gmail.com>
2016-09-21 18:23:46 -0400

    unzip verbosely so we can diagnose problem

--- third-party/install_emscripten.sh
+++ third-party/install_emscripten.sh
@@ -1,6 +1,6 @@
 wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
-gunzip emsdk-portable.tar.gz
-tar -xf emsdk-portable.tar
+gunzip emsdk-portable.tar.gz -v
+tar -xf emsdk-portable.tar -v
 cd emsdk-portable
 ./emsdk update
 ./emsdk install latest
