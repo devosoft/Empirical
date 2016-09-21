542637f5836a67e0aad36e397f77ca5862583563
Charles Ofria <charles.ofria@gmail.com>
2016-09-21 13:13:42 -0400

    Sigificantly fleshed out description of DataNodes, dividing them into Collection, Storage, Processing, and long-term Recording.

--- data/DataNode.h
+++ data/DataNode.h
@@ -3,20 +3,34 @@
 //  Released under the MIT Software license; see doc/LICENSE
 //
 //
-//  DataNode object track a specific type of data over the course of a run.
+//  DataNode objects track a specific type of data over the course of a run.
 //
-//  Raw DataNode objects are a base class that store only the most recent input and a count
-//  of how many inputs it has received.  Note that the input type does not need to be
-//  mathematical.
+//  DataNodes are templated objects that must specify a stored type and provide multiple types of
+//  internal classes.  By default, the stored type may be anything, but stats require a type that
+//  math can be performed on.  The internal classed are:
 //
-//  Derived classes include:
+//  DataStore   : What information should be kept about the values passing through?
+//  DataCollect : How should new input data be found?
+//  DataProcess : How should data be processed on Reset?
+//  DataRecord  : What should be done with previous data after a Reset?
 //
-//  DataNode_Full  : Stores ALL values that are provided.
-//  DataNode_Stats : Collect basic stats (min, max, input count, mean, standard deviation,
-//                   skew, kertosis), as values are supplied, but does not keep those values.
+//  More details on the specific options for each:
 //
-//  DataNode_Pull  : Wraps one of the other data nodes (?), but know where to pull data from
-//                   when needed.
+//  DataStore        : Keep the most recent input value and a count of inputs since the last Reset()
+//  DataStore_Full   : Stores ALL values that are provided since last Reset()
+//  DataStore_Stats  : Maintain basic stats (min, max, input count, mean, standard deviation,
+//                     skew, kertosis), as values are supplied, but not individual values.
+//
+//  DataCollect      : All data needs to be pushed from where it is generated.
+//  DataCollect_Pull : Maintain a function to request new data (either individual values or sets)
+//
+//  DataProcess      : Keep raw data.
+//  DataProcess_Fun  : Maintain a Signal to trigger with collected data at Reset.  // @CAO: Default?
+//
+//  DataRecord        : Discard all data on Reset
+//  DataRecord_Prev   : Maintain previous values of data from last Reset // @CAO: Or X prev values?
+//  DataRecord_Store  : Maintain ALL previous data.
+//  DataRecord_Stream : Send all data into a stream of some kind.
 
 #ifndef EMP_DATA_NODE_H
 #define EMP_DATA_NODE_H
