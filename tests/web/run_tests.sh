642b4344c03a47be8cd704c776062e2286f07f14
Emily Dolson <EmilyLDolson@gmail.com>
2016-09-22 14:50:35 -0400

    Maybe if we delegate it to a new script...

---
+++ tests/web/run_tests.sh
@@ -0,0 +1,9 @@
+source ../third-party/emsdk_portable_repo/emsdk_portable/emsdk_env.sh
+for filename in *.js; 
+do 
+    node $filename; 
+    if [ $$? -gt 0 ]; 
+    then 
+	exit 1; 
+    fi; 
+done
