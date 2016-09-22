1c956a9861f10b4d993700ede023b0cf4dacb709
Emily Dolson <EmilyLDolson@gmail.com>
2016-09-22 12:22:01 -0400

    give emsdk its own repo

--- third-party/install_emscripten.sh
+++ third-party/install_emscripten.sh
@@ -1,8 +1,12 @@
-wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
-gunzip emsdk-portable.tar.gz -v
-tar -xf emsdk-portable.tar -v
-cd emsdk_portable
-./emsdk update
-./emsdk install latest
-./emsdk activate latest
+#wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
+#gunzip emsdk-portable.tar.gz -v
+#tar -xf emsdk-portable.tar -v
+#cd emsdk_portable
+#./emsdk update
+#./emsdk install latest
+#./emsdk activate latest
+#source ./emsdk_env.sh
+
+git clone https://github.com/emilydolson/emsdk_portable_repo.git
+cd emsdk_portable_repo/emsdk_portable
 source ./emsdk_env.sh
