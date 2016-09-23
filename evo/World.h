85ca56effe9c03771845adb8556e8b44764066f0
Charles Ofria <charles.ofria@gmail.com>
2016-09-23 17:22:04 -0400

    Allow any World to use a custom Print conversion from ORG.

--- evo/World.h
+++ evo/World.h
@@ -325,6 +325,13 @@ namespace evo {
       return MutatePop(orgM.GetMutFun(), first_mut, last_mut);
     }
 
+    void Print(const std::function<std::string(ORG*)> & print_fun,
+               std::ostream & os=std::cout,
+               const std::string & empty="X",
+               const std::string & spacer=" ") {
+      popM.Print(print_fun, os, empty, spacer);             
+    }
+
     void Print(std::ostream & os=std::cout, const std::string & empty="X", const std::string & spacer=" ") {
       popM.Print(os, empty, spacer);
     }
