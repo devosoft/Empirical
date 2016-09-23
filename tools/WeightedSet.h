cd1854fd6c3f6f59dbec8e8291a66bcdda2b5656
Charles Ofria <charles.ofria@gmail.com>
2016-09-22 22:36:33 -0400

    Divided up weight vector into item_weight and tree_weight vectors.

--- tools/WeightedSet.h
+++ tools/WeightedSet.h
@@ -35,14 +35,14 @@ namespace emp {
 
   class WeightedSet {
   private:
-    struct WeightInfo { double item=0.0; double tree=0.0; };
-    emp::vector<WeightInfo> weight;
+    emp::vector<double> item_weight;
+    emp::vector<double> tree_weight;
     bool needs_refresh;
 
     int ParentID(int id) const { return (id-1) / 2; }
     int LeftID(int id) const { return 2*id + 1; }
     int RightID(int id) const { return 2*id + 2; }
-    bool IsLeaf(int id) const { return 2*id >= (int) weight.size(); }
+    bool IsLeaf(int id) const { return 2*id >= (int) item_weight.size(); }
 
     class Proxy {
     private:
@@ -62,123 +62,125 @@ namespace emp {
 
       const int pivot = GetSize()/2 - 1; // Transition between internal and leaf nodes.
       // For a leaf, tree weight = item weight.
-      for (int i = weight.size()-1; i > pivot; i--) weight[i].tree = weight[i].item;
+      for (int i = item_weight.size()-1; i > pivot; i--) tree_weight[i] = item_weight[i];
       // The pivot node may have one or two sub-trees.
       if (pivot > 0) {
-        weight[pivot].tree = weight[pivot].item + weight[LeftID(pivot)].tree;
-        if (RightID(pivot) < GetSize()) weight[pivot].tree += weight[RightID(pivot)].tree;
+        tree_weight[pivot] = item_weight[pivot] + tree_weight[LeftID(pivot)];
+        if (RightID(pivot) < GetSize()) tree_weight[pivot] += tree_weight[RightID(pivot)];
       }
       // Internal nodes sum their two sub-tree and their own weight.
       for (int i = pivot-1; i >= 0; i--) {
-        weight[i].tree = weight[i].item + weight[LeftID(i)].tree + weight[RightID(i)].tree;
+        tree_weight[i] = item_weight[i] + tree_weight[LeftID(i)] + tree_weight[RightID(i)];
       }
 
       needs_refresh = false;
     }
 
   public:
-    WeightedSet(int num_items=0) : weight(num_items), needs_refresh(false) {;}
+    WeightedSet(int num_items=0)
+      : item_weight(num_items), tree_weight(num_items), needs_refresh(false) {;}
     WeightedSet(const WeightedSet &) = default;
     WeightedSet(WeightedSet &&) = default;
     ~WeightedSet() = default;
     WeightedSet & operator=(const WeightedSet &) = default;
     WeightedSet & operator=(WeightedSet &&) = default;
 
-    int GetSize() const { return (int) weight.size(); }
-    double GetWeight() const { ResolveRefresh(); return weight[0].tree; }
-    double GetWeight(int id) const { return weight[id].item; }
-    double GetProb(int id) const { ResolveRefresh(); return weight[id].item / weight[0].tree; }
+    int GetSize() const { return (int) item_weight.size(); }
+    double GetWeight() const { ResolveRefresh(); return tree_weight[0]; }
+    double GetWeight(int id) const { return item_weight[id]; }
+    double GetProb(int id) const { ResolveRefresh(); return item_weight[id] / tree_weight[0]; }
 
     void Resize(int new_size) {
-      const int old_size = weight.size();
-      weight.resize(new_size);             // Update the size (new weights default to zero)
+      const int old_size = item_weight.size();
+      item_weight.resize(new_size, 0.0);   // Update the size (new weights default to zero)
+      tree_weight.resize(new_size, 0.0);   // Update the size (new weights default to zero)
       if (new_size < old_size) Refresh();  // Update the tree weights if some have disappeared.
     }
 
     void Resize(int new_size, double def_value) {
-      const int old_size = weight.size();
-      weight.resize(new_size);  // Update the size (new weights default to zero)
-      for (int i=old_size; i < new_size; i++) weight[i].item = def_value;
+      const int old_size = item_weight.size();
+      item_weight.resize(new_size, 0.0);   // Update the size (new weights default to zero)
+      tree_weight.resize(new_size, 0.0);   // Update the size (new weights default to zero)
+      for (int i=old_size; i < new_size; i++) item_weight[i] = def_value;
       Refresh();                // Update the tree weights.
     }
 
     // Standard library compatibility
-    size_t size() const { return weight.size(); }
+    size_t size() const { return item_weight.size(); }
     void resize(int new_size) { Resize(new_size); }
 
     void Clear() {
-      for (auto & x : weight) { x.item = 0.0; x.tree = 0.0; }
+      for (auto & x : item_weight) { x = 0.0; }
+      for (auto & x : tree_weight) { x = 0.0; }
       needs_refresh = false;  // If all weights are zero, no refresh needed.
     }
 
     void ResizeClear(size_t new_size) {
-      weight.resize(new_size);
-      for (auto & x : weight) { x.item = 0.0; x.tree = 0.0; }
-      needs_refresh = false;  // If all weights are zero, no refresh needed.
+      item_weight.resize(new_size);
+      tree_weight.resize(new_size);
+      Clear();
     }
 
     void Adjust(int id, const double new_weight) {
       // Update this node.
-      const double weight_diff = new_weight - weight[id].item;
-      weight[id].item = new_weight;       // Update item weight
+      const double weight_diff = new_weight - item_weight[id];
+      item_weight[id] = new_weight;  // Update item weight
 
-      if (needs_refresh) return;          // If we already need a refresh don't update tree weights!
+      if (needs_refresh) return;     // If we already need a refresh don't update tree weights!
 
-      weight[id].tree += weight_diff;
+      tree_weight[id] += weight_diff;
       // Update tree to root.
       while (id > 0) {
         id = ParentID(id);
-        weight[id].tree += weight_diff;
+        tree_weight[id] += weight_diff;
       }
     }
 
     void Adjust(const emp::vector<double> & new_weights) {
-      weight.resize(new_weights.size());
-      for (size_t i=0; i < weight.size(); i++) {
-        weight[i].item = new_weights[i];
-      }
+      item_weight = new_weights;
       Refresh();
     }
 
     size_t Insert(double in_weight) {
-      size_t id = weight.size();
-      weight.emplace_back();
+      size_t id = item_weight.size();
+      item_weight.emplace_back(0.0);
+      tree_weight.emplace_back(0.0);
       Adjust(id, in_weight);
       return id;
     }
 
     int Index(double index, int cur_id=0) {
       ResolveRefresh();
-      emp_assert(index < weight[0].tree);  // Cannot index beyond end of set.
+      emp_assert(index < tree_weight[0]);  // Cannot index beyond end of set.
 
       // If our target is in the current node, return it!
-      const double cur_weight = weight[cur_id].item;
+      const double cur_weight = item_weight[cur_id];
       if (index < cur_weight) return cur_id;
 
       // Otherwise determine if we need to recurse left or right.
       index -= cur_weight;
       const int left_id = LeftID(cur_id);
-      const double left_weight = weight[left_id].tree;
+      const double left_weight = tree_weight[left_id];
 
       return (index < left_weight) ? Index(index, left_id) : Index(index-left_weight, left_id+1);
     }
 
     // int operator[](double index) { return Index(index,0); }
     Proxy operator[](int id) { return Proxy(*this,id); }
-    double operator[](int id) const { return weight[id].item; }
+    double operator[](int id) const { return item_weight[id]; }
 
     WeightedSet & operator+=(WeightedSet & in_set) {
-      emp_assert(weight.size() == in_set.weight.size());
+      emp_assert(size() == in_set.size());
       for (size_t i = 0; i < in_set.size(); i++) {
-        weight[i].item += in_set.weight[i].item;
+        item_weight[i] += in_set.item_weight[i];
       }
       Refresh();
       return *this;
     }
     WeightedSet & operator-=(WeightedSet & in_set) {
-      emp_assert(weight.size() == in_set.weight.size());
+      emp_assert(size() == in_set.size());
       for (size_t i = 0; i < in_set.size(); i++) {
-        weight[i].item -= in_set.weight[i].item;
+        item_weight[i] -= in_set.item_weight[i];
       }
       Refresh();
       return *this;
