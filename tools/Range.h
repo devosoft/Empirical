02e64cf5da18ce031663f4b62ae536e29c869b95
Charles Ofria <charles.ofria@gmail.com>
2016-09-25 23:33:36 -0400

    Added an emp::Range class to track value ranges.

---
+++ tools/Range.h
@@ -0,0 +1,26 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  A simple way to track value ranges [lower, upper)
+
+#ifndef EMP_RANGE_H
+#define EMP_RANGE_H
+
+namespace emp {
+
+  template <typename T>
+  struct Range {
+    T lower;
+    T upper;
+
+    bool Valid(T value) { return value >= lower && value < upper; }
+  };
+
+  template <typename T> Range<T> MakeRange(T _l, T _u) { return Range<T>(_l,_u); }
+  Range<int> IntRange(int _l, int _u) { return Range<int>(_l,_u); }
+  Range<double> DRange(double _l, double _u) { return Range<double>(_l,_u); }
+}
+
+#endif
