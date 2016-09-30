b76a7e045a70ec0d38a11fb59f15ce24e765caf4
Charles Ofria <charles.ofria@gmail.com>
2016-09-29 22:32:19 -0400

    New example file for emp::Cache

---
+++ examples/tools/Cache.cc
@@ -0,0 +1,14 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some examples code for using emp::Cache
+
+#include <iostream>
+#include "../../tools/Cache.h"
+
+int main()
+{
+  std::cout << "Testing." << std::endl;
+}
