9f1fd547ffe8a22008f16813eb3a2b7376df05d7
Charles Ofria <charles.ofria@gmail.com>
2016-10-01 00:07:14 -0400

    Cleanup of memo_function; still need tuple hash.

--- tools/memo_function.h
+++ tools/memo_function.h
@@ -13,41 +13,31 @@
 
 namespace emp {
 
-  template <class R,                                          // memo_function::mapped_type (was T)
-	    class... ARGS,                                          // memo_function::key_type (was Key)
-	    class HASH=std::hash<std::tuple<ARGS...>>,              // memo_function::hasher
-	    class PRED=std::equal_to<std::tuple<ARGS...>>,                      // memo_function::key_equal
-	    class ALLOC=std::allocator<std::pair<const std::tuple<ARGS...>,R>>  // memo_function::allocator_type
-	   >
+  template <class T> class memo_function;    // Not defined.
+
+  template <class R, class... ARGS>
   class memo_function<R(ARGS...)> {
   public:
     using key_type = std::tuple<ARGS...>;
     using mapped_type = R;
-    using hasher = HASH;
-    using key_equal = PRED;
-    using allocator_type = ALLOC;
+    using fun_t = std::function<R(ARGS...)>;
 
   private:
-    std::unordered_map<key_type, mapped_type, HASH, PRED, ALLOC> cache_map;
+    std::unordered_map<key_type, R> cache_map;
 
   public:
     size_t size() const { return cache_map.size(); }
 
-    bool Has(const Key & k) const { return cache_map.find(k) != cache_map.end(); }
-    void Clear() { cache_map.clear(); }
-    void Erase(const Key & k) { cache_map.erase(k); }
-
-    T Get(Key k, const std::function<T(Key k)> & calc_fun) {
-      auto cache_it = cache_map.find(k);
-      if (cache_it != cache_map.end()) return cache_it->second;
-      return cache_map.emplace(k, calc_fun(k)).first->second;
+    bool Has(const ARGS &... k) const {
+      return cache_map.find(key_type(k...)) != cache_map.end();
     }
+    void Clear() { cache_map.clear(); }
+    void Erase(const ARGS &... k) { cache_map.erase(key_type(k...)); }
 
-    // A version of Get that allows calls with const references instead of pass-by-value.
-    const T & GetRef(const Key & k, const std::function<T(const Key & k)> & calc_fun) {
-      auto cache_it = cache_map.find(k);
+    R Get(ARGS... k, const fun_t & calc_fun) {
+      auto cache_it = cache_map.find(key_type(k...));
       if (cache_it != cache_map.end()) return cache_it->second;
-      return cache_map.emplace(k, calc_fun(k)).first->second;
+      return cache_map.emplace(key_type(k...), calc_fun(k...)).first->second;
     }
 
   };
