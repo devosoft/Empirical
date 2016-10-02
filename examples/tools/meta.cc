797ed5143acb980390f521b68f646788217bb123
Charles Ofria <charles.ofria@gmail.com>
2016-10-01 21:35:24 -0400

    Setup examples for CombineHash and demonstrate the it works properly.

--- examples/tools/meta.cc
+++ examples/tools/meta.cc
@@ -26,4 +26,14 @@ int main()
 
   std::cout << map2.a << std::endl;
   std::cout << map2.b << std::endl;
+
+  std::cout << "hash(2) = " << std::hash<int>()(2) << std::endl
+	    << "hash(3) = " << std::hash<int>()(3) << std::endl
+	    << "hash(4) = " << std::hash<int>()(4) << std::endl
+	    << "CombineHash(4) = " << emp::CombineHash(4) << std::endl
+	    << "CombineHash(2,3) = " << emp::CombineHash(2,3) << std::endl
+	    << "CombineHash(2,3) = " << emp::CombineHash(2,3) << std::endl
+	    << "CombineHash(3,2) = " << emp::CombineHash(3,2) << std::endl
+	    << "CombineHash(3,4) = " << emp::CombineHash(3,4) << std::endl
+	    << "CombineHash(2,3,4) = " << emp::CombineHash(2,3,4) << std::endl;
 }
