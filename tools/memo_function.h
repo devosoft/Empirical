756cb5a790ca3c0cae97fc063a0400e9833fa8d0
Charles Ofria <charles.ofria@gmail.com>
2016-10-10 23:33:15 -0400

    Setup a version of memo_function to avoid tuple if there's only a single function input.

--- tools/memo_function.h
+++ tools/memo_function.h
@@ -19,15 +19,69 @@ namespace emp {
 
   template <class T> class memo_function;    // Not defined.
 
-  template <class R, class... ARGS>
-  class memo_function<R(ARGS...)> {
+  // Single argument functions don't need a tuple...
+  template <class R, class ARG>
+  class memo_function<R(ARG)> {
   public:
     using size_t = std::size_t;
     using return_t = R;
-    using fun_t = std::function<R(ARGS...)>;
-    using hash_t = emp::TupleHash<ARGS...>;
-    using this_t = memo_function<R(ARGS...)>;
-    using tuple_t = std::tuple<std::decay_t<ARGS>...>;
+    using index_t = std::decay_t<ARG>;
+    using fun_t = std::function<R(ARG)>;
+    using this_t = memo_function<R(ARG)>;
+
+  private:
+    std::unordered_map<index_t, return_t> cache_map;
+    fun_t fun;
+
+  public:
+    template <typename T>
+    memo_function(T && fun_info) : fun(std::forward<T>(fun_info)) { ; }
+    memo_function(const this_t &) = default;
+    memo_function(this_t &&) = default;
+    memo_function() { ; }
+
+    this_t & operator=(const this_t &) = default;
+    this_t & operator=(this_t &&) = default;
+    this_t & operator=(const fun_t & _f) { cache_map.clear(); fun=_f; return *this; }
+    this_t & operator=(fun_t && _f) { cache_map.clear(); fun=std::move(_f); return *this; }
+    template <typename T>
+    this_t & operator=(T && arg) { cache_map.clear(); fun = std::forward<T>(arg); return *this; }
+
+    size_t size() const { return cache_map.size(); }
+
+    bool Has(const ARG & k) const { return cache_map.find(k) != cache_map.end(); }
+    void Clear() { cache_map.clear(); }
+    void Erase(const ARG & k) { cache_map.erase(k); }
+
+    return_t operator()(const ARG & k) {
+      emp_assert(fun);
+      auto cache_it = cache_map.find(k);
+      if (cache_it != cache_map.end()) return cache_it->second;
+      return cache_map.emplace(k, fun(k)).first->second;
+    }
+
+    operator bool() { return (bool) fun; }
+
+    // A memo_function can be converted to a regular std::function for function calls.
+    operator std::function<R(ARG)>() {
+      return [this](const ARG & arg){ return operator()(arg); };
+    }
+    std::function<R(ARG)> to_function() {
+      return [this](const ARG & arg){ return operator()(arg); };
+    }
+  };
+
+  // Specialization for when we have more than one argument...  we need to convert inputs
+  // to a tuple to make this work.
+  template <class R, class A1, class A2, class... EXTRA>
+  class memo_function<R(A1,A2,EXTRA...)> {
+  public:
+    using size_t = std::size_t;
+    using return_t = R;
+    using fun_t = std::function<R(A1,A2,EXTRA...)>;
+    using hash_t = emp::TupleHash<A1,A2,EXTRA...>;
+    using this_t = memo_function<R(A1,A2,EXTRA...)>;
+    using tuple_t = std::tuple<std::decay_t<A1>,std::decay_t<A2>,std::decay_t<EXTRA>...>;
 
   private:
     std::unordered_map<tuple_t, return_t, hash_t> cache_map;
@@ -48,26 +102,37 @@ namespace emp {
 
     size_t size() const { return cache_map.size(); }
 
-    inline static size_t Hash(const ARGS &... k) { return CombineHash(k...); }
-    bool Has(const ARGS &... k) const { return cache_map.find(std::make_tuple(k...)) != cache_map.end(); }
+    inline static size_t Hash(const A1 & k1, const A2 & k2, const EXTRA &... k_extra) {
+      return CombineHash(k1,k2,k_extra...);
+    }
+    bool Has(const A1 & k1, const A2 & k2, const EXTRA &... k_extra) const {
+      return cache_map.find(std::make_tuple(k1,k2,k_extra...)) != cache_map.end();
+    }
     void Clear() { cache_map.clear(); }
-    void Erase(const ARGS &... k) { cache_map.erase(std::make_tuple(k...)); }
+    void Erase(const A1 & k1, const A2 & k2, const EXTRA &... k_extra) {
+      cache_map.erase(std::make_tuple(k1,k2,k_extra...));
+    }
 
-    return_t operator()(ARGS... k) {
+    return_t operator()(const A1 & k1, const A2 & k2, const EXTRA &... k_extra) {
       emp_assert(fun); // Function must be specified with Get() -or- already set.
-      auto cache_it = cache_map.find(std::make_tuple(k...));
+      auto cache_it = cache_map.find(std::make_tuple(k1,k2,k_extra...));
       if (cache_it != cache_map.end()) return cache_it->second;
-      return cache_map.emplace(std::make_tuple(k...), fun(std::forward<ARGS>(k)...)).first->second;
+      return cache_map.emplace(std::make_tuple(k1,k2,k_extra...),
+                               fun(k1, k2, k_extra...)).first->second;
     }
 
     operator bool() { return (bool) fun; }
 
     // A memo_function can be converted to a regular std::function for function calls.
-    operator std::function<R(ARGS...)>() {
-      return [this](ARGS... args){ return operator()(std::forward<ARGS>(args)...); };
+    operator std::function<R(A1,A2,EXTRA...)>() {
+      return [this](A1 k1, A2 k2, EXTRA... k_extra) {
+         return operator()(k1, k2, k_extra...);
+      };
     }
-    std::function<R(ARGS...)> to_function() {
-      return [this](ARGS... args){ return operator()(std::forward<ARGS>(args)...); };
+    std::function<R(A1,A2,EXTRA...)> to_function() {
+      return [this](A1 k1, A2 k2, EXTRA... k_extra) {
+         return operator()(k1, k2, k_extra...);
+      };
     }
   };
 
