b872897cf153005b3693466ef1caf25ab5bccbb4
Charles Ofria <charles.ofria@gmail.com>
2016-10-22 17:42:02 -0400

    Setup TypeSet to be able to take no type.  Added ::merge_t for joining types.

--- tools/TypeSet.h
+++ tools/TypeSet.h
@@ -11,8 +11,25 @@
 
 namespace emp {
 
+  // Pre-declaration of TypeSet
+  template <typename... Ts> struct TypeSet;
+
+  // Anonymous helpers for TypeSet
+  namespace {
+    template <typename... Ts> struct ts_merge_impl;
+
+    template <typename... IN, typename... Ts>
+    struct ts_merge_impl<TypeSet<IN...>, Ts...> {
+      using type = TypeSet<Ts..., IN...>;
+    };
+  }
+
+  // Generic TypeSet (not implemented since specializations cover all cases)
+  template <typename... Ts> struct TypeSet;
+
+  // Specialized TypeSet with at least one type.
   template <typename T1, typename... Ts>
-  struct TypeSet {
+  struct TypeSet<T1, Ts...> {
     template <typename T> constexpr static int Has() { return has_type<T,T1,Ts...>(); }
 
     // Type ID's can be retrieved with
@@ -26,12 +43,30 @@ namespace emp {
     constexpr static bool IsEmpty() { return false; }
     constexpr static bool IsUnique() { return has_unique_types<T1,Ts...>(); }
 
-    template <typename T> using add_t = TypeSet<T1,Ts...,T>;
     using first_t = T1;
     using last_t = last_type<T1,Ts...>;
     using pop_front_t = TypeSet<Ts...>;
+
+    template <typename T> using add_t = TypeSet<T1,Ts...,T>;
+    template <typename IN> using merge_t = typename ts_merge_impl<IN, T1, Ts...>::type;
   };
 
+  // Specialized TypeSet with no types.
+  template <>
+  struct TypeSet<> {
+    template <typename T> constexpr static int Has() { return false; }
+
+    // GetID() NOT IMPLEMENTED since no ID's are available.
+    constexpr static int GetSize() { return 0; }
+    constexpr static bool IsEmpty() { return true; }
+    constexpr static bool IsUnique() { return true; }
+
+    // first_t, last_t, and pop_front_t not implemented, since no types are available.
+    // (could implement with null_t)
+
+    template <typename T> using add_t = TypeSet<T>;
+    template <typename IN> using merge_t = IN;
+  };
 }
 
 #endif
