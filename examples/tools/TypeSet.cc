cbfd2d107a404c313c499a2f8c78ad5a27f816e1
Charles Ofria <charles.ofria@gmail.com>
2016-10-23 23:50:50 -0400

    Added examples for new TypeSet features.

--- examples/tools/TypeSet.cc
+++ examples/tools/TypeSet.cc
@@ -10,6 +10,8 @@
 
 #include "../../tools/TypeSet.h"
 
+int Sum4(int a, int b, int c, int d) { return a+b+c+d; }
+
 int main()
 {
   std::cout << "Testing!" << std::endl;
@@ -19,7 +21,7 @@ int main()
   std::cout << "float pos = " << test_t::GetID<float>() << std::endl;
 
   std::cout << "Num types (with add) = " << test_t::add_t<long long>::GetSize() << std::endl;
-  std::cout << "float pos (with pop) = " << test_t::pop_front_t::GetID<float>() << std::endl;
+  std::cout << "float pos (with pop) = " << test_t::pop_t::GetID<float>() << std::endl;
 
   using test2_t = emp::TypeSet<int, int, int, int>;
   using test3_t = emp::TypeSet<uint64_t>;
@@ -28,4 +30,14 @@ int main()
   std::cout << "Num test_t types:  " << test_t::GetSize() << std::endl;
   std::cout << "Num test2_t types: " << test2_t::GetSize() << std::endl;
   std::cout << "Num test3_t types: " << test3_t::GetSize() << std::endl;
+
+  // Make sure we can use a TypeSet to properly set a function type.
+  using fun_t = test2_t::to_function_t<int>;
+  std::function< fun_t > fun(Sum4);
+  std::cout << "Sum4(1,2,3,4) = " << fun(1,2,3,4) << std::endl;
+
+  using test4_t = test2_t::crop_t<2>;
+  std::cout << "After crop_t<2>, size of type = " << test4_t::GetSize() << std::endl;
+  std::cout << "After merge of t1 and t2, size = "
+	    << test_t::merge_t<test2_t>::GetSize() << std::endl;
 }
