ce44b312cd2058bdb002f99fab18b452b387c34a
Charles Ofria <charles.ofria@gmail.com>
2016-11-03 14:03:03 -0400

    Added example file for IntPack

---
+++ examples/meta/IntPack.cc
@@ -0,0 +1,17 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some example code for using IntPack
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../../meta/IntPack.h"
+
+int main()
+{
+  std::cout << "Testing!" << std::endl;
+}
