3ff026362f45a0da903eedcb47c47a36809f29d2
Charles Ofria <charles.ofria@gmail.com>
2016-11-03 15:06:37 -0400

    Basic examples in IntPack

--- examples/meta/IntPack.cc
+++ examples/meta/IntPack.cc
@@ -11,7 +11,15 @@
 
 #include "../../meta/IntPack.h"
 
+#define EMP_PRINT_VAL(X) std::cout << #X << " = " << X << std::endl;
+
 int main()
 {
-  std::cout << "Testing!" << std::endl;
+  using rise_t = emp::IntPack<0,1,2,3,4,5,6,7,8,9,10>;
+  using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>;
+  std::cout << "using rise_t = emp::IntPack<1,2,3,4,5,6,7,8,9,10>" << std::endl;
+  std::cout << "using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>" << std::endl;
+
+  EMP_PRINT_VAL(rise_t::GetSize());
+  EMP_PRINT_VAL(fib_t::GetSize());
 }
