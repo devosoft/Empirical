ddbb81ddbc73b949f0570a914a675547c2b08414
Charles Ofria <charles.ofria@gmail.com>
2016-11-03 16:13:15 -0400

    Added lots more examples for IntPack

--- examples/meta/IntPack.cc
+++ examples/meta/IntPack.cc
@@ -16,10 +16,23 @@
 int main()
 {
   using rise_t = emp::IntPack<0,1,2,3,4,5,6,7,8,9,10>;
-  using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>;
   std::cout << "using rise_t = emp::IntPack<1,2,3,4,5,6,7,8,9,10>" << std::endl;
-  std::cout << "using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>" << std::endl;
-
   EMP_PRINT_VAL(rise_t::GetSize());
+  EMP_PRINT_VAL(rise_t::Has<0>());
+  EMP_PRINT_VAL(rise_t::Has<13>());
+  EMP_PRINT_VAL(rise_t::Has(10));
+  EMP_PRINT_VAL(rise_t::Count<1>());
+  EMP_PRINT_VAL(rise_t::GetID<5>());
+  EMP_PRINT_VAL(rise_t::GetID(8));
+
+  using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>;
+  std::cout << "using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>" << std::endl;
   EMP_PRINT_VAL(fib_t::GetSize());
+  EMP_PRINT_VAL(fib_t::GetSize());
+  EMP_PRINT_VAL(fib_t::Has<0>());
+  EMP_PRINT_VAL(fib_t::Has<13>());
+  EMP_PRINT_VAL(fib_t::Has(10));
+  EMP_PRINT_VAL(fib_t::Count<1>());
+  EMP_PRINT_VAL(fib_t::GetID<5>());
+  EMP_PRINT_VAL(fib_t::GetID(8));
 }
