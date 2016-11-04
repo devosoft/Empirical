84f928cd349cb57cea4f0f0d69e5a916c754982b
Charles Ofria <charles.ofria@gmail.com>
2016-11-03 23:06:45 -0400

    Added IntPackRange, IsUnique, Sum, and Product to IntPack.cc example.

--- examples/meta/IntPack.cc
+++ examples/meta/IntPack.cc
@@ -15,7 +15,8 @@
 
 int main()
 {
-  using rise_t = emp::IntPack<0,1,2,3,4,5,6,7,8,9,10>;
+  // using rise_t = emp::IntPack<0,1,2,3,4,5,6,7,8,9,10>;
+  using rise_t = emp::IntPackRange<1,11>;
   std::cout << "using rise_t = emp::IntPack<1,2,3,4,5,6,7,8,9,10>" << std::endl;
   EMP_PRINT_VAL(rise_t::GetSize());
   EMP_PRINT_VAL(rise_t::Has<0>());
@@ -24,10 +25,12 @@ int main()
   EMP_PRINT_VAL(rise_t::Count<1>());
   EMP_PRINT_VAL(rise_t::GetID<5>());
   EMP_PRINT_VAL(rise_t::GetID(8));
+  EMP_PRINT_VAL(rise_t::IsUnique());
+  EMP_PRINT_VAL(rise_t::Sum());
+  EMP_PRINT_VAL(rise_t::Product());
 
   using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>;
   std::cout << "using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>" << std::endl;
-  EMP_PRINT_VAL(fib_t::GetSize());
   EMP_PRINT_VAL(fib_t::GetSize());
   EMP_PRINT_VAL(fib_t::Has<0>());
   EMP_PRINT_VAL(fib_t::Has<13>());
@@ -35,4 +38,7 @@ int main()
   EMP_PRINT_VAL(fib_t::Count<1>());
   EMP_PRINT_VAL(fib_t::GetID<5>());
   EMP_PRINT_VAL(fib_t::GetID(8));
+  EMP_PRINT_VAL(fib_t::IsUnique());
+  EMP_PRINT_VAL(fib_t::Sum());
+  EMP_PRINT_VAL(fib_t::Product());
 }
