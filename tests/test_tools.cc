06f22ecc40d3f3c2f790bafe3639b6bc92fef101
Charles Ofria <charles.ofria@gmail.com>
2016-11-12 23:58:53 -0500

    Adjusted other tests and Makefil to make room for meta tests.

--- tests/test_tools.cc
+++ tests/test_tools.cc
@@ -11,9 +11,6 @@
 #include <sstream>
 #include <string>
 
-#include "../meta/meta.h"
-#include "../meta/reflection.h"
-
 #include "../tools/BitMatrix.h"
 #include "../tools/BitSet.h"
 #include "../tools/BitVector.h"
@@ -1087,60 +1084,6 @@ TEST_CASE("Test emp::memo_function", "[tools]")
 }
 
 
-char result_char;
-void TestFun(int x, int y, char z) {
-  result_char = z + x*y;
-}
-
-template <typename A, typename B>
-struct MetaTestClass {
-  A a;
-  B b;
-};
-
-TEST_CASE("Test meta-programming helpers", "[tools]")
-{
-  // TEST FOR VARIADIC HELPER FUNCTIONS:
-
-  REQUIRE((emp::get_type_index<char, char, bool, int, double>()) == 0);
-  REQUIRE((emp::get_type_index<int, char, bool, int, double>()) == 2);
-  REQUIRE((emp::get_type_index<double, char, bool, int, double>()) == 3);
-  REQUIRE((emp::get_type_index<std::string, char, bool, int, double>()) < 0);
-
-  REQUIRE((emp::has_unique_first_type<int, bool, std::string, bool, char>()) == true);
-  REQUIRE((emp::has_unique_first_type<bool, int, std::string, bool, char>()) == false);
-  REQUIRE((emp::has_unique_types<bool, int, std::string, emp::vector<bool>, char>()) == true);
-  REQUIRE((emp::has_unique_types<int, bool, std::string, bool, char>()) == false);
-
-
-  std::tuple<int, int, char> test_tuple(3,2,'a');
-  emp::ApplyTuple(TestFun, test_tuple);
-
-  REQUIRE(result_char == 'g');
-
-  using meta1_t = MetaTestClass<int, double>;
-  using meta2_t = emp::AdaptTemplate<meta1_t, char, bool>;
-  using meta3_t = emp::AdaptTemplate_Arg1<meta1_t, std::string>;
-
-  meta1_t meta1;
-  meta2_t meta2;
-  meta3_t meta3;
-
-  meta1.a = (decltype(meta1.a)) 65.5;
-  meta1.b = (decltype(meta1.b)) 65.5;
-  meta2.a = (decltype(meta2.a)) 65.5;
-  meta2.b = (decltype(meta2.b)) 65.5;
-  meta3.a = (decltype(meta3.a)) "65.5";
-  meta3.b = (decltype(meta3.b)) 65.5;
-
-  REQUIRE( meta1.a == 65 );
-  REQUIRE( meta1.b == 65.5 );
-  REQUIRE( meta2.a == 'A' );
-  REQUIRE( meta2.b == true );
-  REQUIRE( meta3.a == "65.5" );
-  REQUIRE( meta3.b == 65.5 );
-}
-
 TEST_CASE("Test NFA", "[tools]")
 {
   emp::NFA nfa(10);
@@ -1353,22 +1296,6 @@ TEST_CASE("Test random", "[tools]")
   REQUIRE(choices.size() == 10);
 }
 
-
-struct HasA { static int A; };
-struct HasA2 { static char A; };
-template <typename T> using MemberA = decltype(T::A);
-
-TEST_CASE("Test reflection", "[tools]")
-{
-  REQUIRE((emp::test_type<MemberA,int>()) == false);
-  REQUIRE((emp::test_type<MemberA,HasA>()) == true);
-  REQUIRE((emp::test_type<MemberA,HasA2>()) == true);
-  REQUIRE((emp::test_type<std::is_integral,int>()) == true);
-  REQUIRE((emp::test_type<std::is_integral,HasA>()) == false);
-  REQUIRE((emp::test_type<std::is_integral, HasA2>()) == false);
-  
-  // @CAO Need to build more reflection tests, once reflection is restructured.
-}
 
 TEST_CASE("Test regular expressions (RegEx)", "[tools]")
 {
