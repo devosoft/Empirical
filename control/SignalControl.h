2666c908fb3485dbc3d8837241a9c1eb2b80369e
Charles Ofria <charles.ofria@gmail.com>
2016-11-30 16:49:10 -0500

    Added initial code for SignalControl.h

---
+++ control/SignalControl.h
@@ -0,0 +1,17 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  The SignalControl class manages all of the signals and actions, linking them together
+//  upon request (by name, base class, or derived class).
+
+#include "Signal.h"
+
+namespace emp {
+
+  class SignalControl {
+  };
+
+}
+
