df2d58320dbb23b998ca5ef690931769eb6c230f
Charles Ofria <charles.ofria@gmail.com>
2016-11-29 23:14:31 -0500

    Added definition of SignalKey

--- control/Signal.h
+++ control/Signal.h
@@ -5,14 +5,48 @@
 //
 //  The Signal class allows functions to be bundled and triggered enmasse.
 
+#include <map>
 #include <string>
 
 namespace emp {
 
-  class SignalBase {
+  // SignalKey tracks a specific function triggered by a signal. For now, its just an integer.
+  class SignalKey {
   private:
-    std::string name;
+    uint64_t id;
   public:
+    SignalKey(uint64_t _id=0) : id(_id) { ; }
+    SignalKey(const SignalKey &) = default;
+    SignalKey & operator=(const SignalKey &) = default;
+    ~SignalKey() { ; }
+
+    bool operator==(const SignalKey& in) const { return id == in.id; }
+    bool operator!=(const SignalKey& in) const { return id != in.id; }
+    bool operator<(const SignalKey& in)  const { return id < in.id; }
+    bool operator>(const SignalKey& in)  const { return id > in.id; }
+    bool operator<=(const SignalKey& in) const { return id <= in.id; }
+    bool operator>=(const SignalKey& in) const { return id >= in.id; }
+
+    uint64_t GetID() const { return id; }
+    bool IsActive() const { return id > 0; }
+
+    // These functions require additional classes and are defined later in this file...
+    // internal::Signal_Base * GetSignal();
+    // void Delete();
+    // void Replace(internal::Action_Base & new_action);
+    // template <typename... ARGS> void Replace(const std::function<void(ARGS...)> & fun);
+  };
+
+  class SignalBase {
+  protected:
+    std::string name;                      // What is the unique name of this signal?
+    std::map<SignalKey, int> link_key_map; // Map unique link keys to link index for actions.
+    const int num_args;                    // How many arguments does this signal provide?
+
+    // SignalBase should only be constructable from derrived classes.
+    SignalBase(const std::string & n, int a) : name(n), num_args(a) { ; }
+  public:
+    virtual ~SignalBase() { ; }
   };
 
   template <typename... ARGS>
