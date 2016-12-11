0fdd6ee34bb7aa54ada951675aeed3a9d2cde97e
Charles Ofria <charles.ofria@gmail.com>
2016-12-10 23:06:53 -0500

    Fixed Shuffle and GetPermutation in random_utils to use size_t

--- tools/random_utils.h
+++ tools/random_utils.h
@@ -17,23 +17,23 @@ namespace emp {
   /// If max_count is provided, just make sure that the first max_count entries are randomly
   /// drawn from entire vector.
   template <typename T>
-  inline void Shuffle(Random & random, emp::vector<T> & v, int max_count=-1)
+  inline void Shuffle(Random & random, emp::vector<T> & v, size_t max_count)
   {
-    if (max_count < 0) max_count = (int) v.size();
-    for (uint32_t i = 0; i < (uint32_t) max_count; i++) {
-      const uint32_t pos = random.GetUInt(i, v.size());
+    for (size_t i = 0; i < max_count; i++) {
+      const size_t pos = random.GetUInt(i, v.size());
       if (pos == i) continue;
       std::swap(v[i], v[pos]);
     }
   }
-
+  template <typename T>
+  inline void Shuffle(Random & random, emp::vector<T> & v) { Shuffle(random, v, v.size()); }
 
   /// Return an emp::vector<int> numbered 0 through size-1 in a random order.
 
   inline emp::vector<size_t> GetPermutation(Random & random, size_t size) {
     emp::vector<size_t> seq(size);
     seq[0] = 0;
-    for (uint32_t i = 1; i < size; i++) {
+    for (size_t i = 1; i < size; i++) {
       size_t val_pos = random.GetUInt(i+1);
       seq[i] = seq[val_pos];
       seq[val_pos] = i;
@@ -44,7 +44,7 @@ namespace emp {
   /// Choose K positions from N possibilities.
 
   inline void Choose(Random & random, size_t N, size_t K, std::vector<size_t> & choices) {
-    emp_assert (N >= K) return;
+    emp_assert (N >= K);
 
     choices.resize(K);
     while (K) {
