cf0fcca12510e6b94a8f1030363a51beb5576361
Charles Ofria <charles.ofria@gmail.com>
2016-12-13 11:24:15 -0500

    Setup simple ways to add actions to signals.

--- control/SignalControl.h
+++ control/SignalControl.h
@@ -6,8 +6,8 @@
 //  The SignalControl class manages all of the signals and actions, linking them together
 //  upon request (by name, base class, or derived class).
 
-#ifndef EMP_CONTROL_SIGNAL_CONTROL
-#define EMP_CONTROL_SIGNAL_CONTROL
+#ifndef EMP_SIGNAL_CONTROL
+#define EMP_SIGNAL_CONTROL
 
 #include <string>
 #include <unordered_map>
@@ -20,16 +20,28 @@ namespace emp {
   class SignalControl {
   private:
     ActionManager action_m;
+    SignalManager signal_m;
     uint32_t next_signal_id;                                 // Give each signal a unique id.
-    std::unordered_map<std::string, SignalBase *> signals;   // Map names to signals
     std::unordered_map<uint32_t, SignalBase *> id_to_signal; // Map signal ID to associated pointer
 
   public:
     SignalControl() : next_signal_id(1) { ; }
     ~SignalControl() { ; }
 
-    size_t GetNumActions() { return action_m.GetSize(); }
-    size_t GetNumSignals() { return signals.size(); }
+    size_t GetNumActions() const { return action_m.GetSize(); }
+    size_t GetNumSignals() const { return signal_m.GetSize(); }
+
+    ActionBase & GetAction(const std::string & name) const { return action_m.Get(name); }
+    SignalBase & GetSignal(const std::string & name) const { return signal_m.Get(name); }
+
+    template <typename RETURN, typename... ARGS>
+    auto & AddAction(const std::function<RETURN(ARGS...)> & fun, const std::string & name) {
+      return action_m.Add(fun, name);
+    }
+    template <typename RETURN, typename... ARGS>
+    auto & AddAction(const std::function<RETURN(ARGS...)> & fun) {
+      return action_m.Add(fun);
+    }
   };
 
 }
