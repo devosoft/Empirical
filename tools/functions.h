7648e91cb386906b950c394c77de78406baeeee6
Charles Ofria <charles.ofria@gmail.com>
2016-12-13 18:17:09 -0500

    Updated functions in functions.h to proper EMP CamelCase.

--- tools/functions.h
+++ tools/functions.h
@@ -15,6 +15,7 @@
 #include <vector>
 
 #include "assert.h"
+#include "const.h"
 
 #define EMP_FUNCTION_TIMER(TEST_FUN) {                                       \
     std::clock_t emp_start_time = std::clock();                              \
@@ -28,7 +29,7 @@
 
 namespace emp {
 
-  static inline double time_fun(std::function<void()> test_fun) {
+  static inline double TimeFun(std::function<void()> test_fun) {
     std::clock_t start_time = std::clock();
     test_fun();
     std::clock_t tot_time = std::clock() - start_time;
@@ -36,30 +37,30 @@ namespace emp {
   }
 
   // Toggle an input bool.
-  inline bool toggle(bool & in_bool) { return (in_bool = !in_bool); }
+  inline bool Toggle(bool & in_bool) { return (in_bool = !in_bool); }
 
   /// % is actually remainder; this is a proper modulus command that handles negative #'s correctly
-  inline constexpr int mod(int in_val, int mod_val) {
+  inline constexpr int Mod(int in_val, int mod_val) {
     return (in_val < 0) ? (in_val % mod_val + mod_val) : (in_val % mod_val);
   }
 
   // A fast (O(log p)) integer-power command.
-  static constexpr int pow(int base, int p) {
-    return (p <= 0) ? 1 : base * pow(base, p-1);
+  static constexpr int Pow(int base, int p) {
+    return (p <= 0) ? 1 : base * Pow(base, p-1);
   }
 
 
   // Run both min and max on a value to put it into a desired range.
-  template <typename TYPE> constexpr TYPE to_range(const TYPE & value, const TYPE & in_min, const TYPE & in_max) {
+  template <typename TYPE> constexpr TYPE ToRange(const TYPE & value, const TYPE & in_min, const TYPE & in_max) {
     return (value < in_min) ? in_min : ((value > in_max) ? in_max : value);
   }
 
-  template <typename T> constexpr const T & min(const T& in1, const T& in2, const T& in3) {
+  template <typename T> constexpr const T & Min(const T& in1, const T& in2, const T& in3) {
     return std::min(std::min(in1,in2), in3);
   }
 
   // Build a min and max that allows a variable number of inputs to be compared.
-  template <typename T> const T & min(std::initializer_list<const T&> lst) {
+  template <typename T> const T & Min(std::initializer_list<const T&> lst) {
     emp_assert(lst.size > 0); // Nothing to return if nothing in the list!
     auto min_found = lst.begin();
     for (auto it = lst.begin()+1; it < lst.end(); it++) {
@@ -68,7 +69,7 @@ namespace emp {
     return *min_found;
   }
 
-  template <typename T> const T & max(std::initializer_list<const T&> lst) {
+  template <typename T> const T & Max(std::initializer_list<const T&> lst) {
     emp_assert(lst.size > 0); // Nothing to return if nothing in the list!
     auto max_found = lst.begin();
     for (auto it = lst.begin()+1; it < lst.end(); it++) {
@@ -78,7 +79,7 @@ namespace emp {
   }
 
 
-  static inline std::vector<int> build_range(int min, int max, int step=1) {
+  static inline std::vector<int> BuildRange(int min, int max, int step=1) {
     int size = (max-min) / step;
     emp_assert(size >= 0);
     std::vector<int> out_v((size_t) size);
@@ -92,7 +93,15 @@ namespace emp {
 
   // Build a function to determine the size of a built-in array.
   template <typename T, size_t N>
-  constexpr size_t size(T (&)[N]) { return N; }
+  constexpr size_t GetSize(T (&)[N]) { return N; }
+
+
+  // Build a function that will always return a unique value (and trip an assert if it can't...)
+  static size_t UniqueVal() {
+    static size_t val = 0;
+    emp_assert(val < MaxValue<size_t>() && "Ran out of unique values in size_t!");
+    return val++;
+  }
 
 }
 
