f4715534dfa04e6768367643d3f1563b7529dadd
Charles Ofria <charles.ofria@gmail.com>
2016-12-14 07:18:58 -0500

    Updated examples/evo/World.cc to use size_t

--- examples/evo/World.cc
+++ examples/evo/World.cc
@@ -29,13 +29,13 @@ int main()
   for (int i = 0; i < 100; i++) world.Insert(i+100);
 
   std::cout << "Start Size = " << world.GetSize() << std::endl;
-  for (int i = 0; i < world.GetSize(); i++) std::cout << world[i] << " ";
+  for (size_t i = 0; i < world.GetSize(); i++) std::cout << world[i] << " ";
   std::cout << std::endl;
 
   world.TournamentSelect([](int * i){ return (double) *i; }, 5, 100);
   world.Update();
   std::cout << "Post-Tourney Size = " << world.GetSize() << std::endl;
-  for (int i = 0; i < world.GetSize(); i++) std::cout << world[i] << " ";
+  for (size_t i = 0; i < world.GetSize(); i++) std::cout << world[i] << " ";
   std::cout << std::endl;
 
   // Populations can be supplied a default fitness function so that it doesn't
@@ -46,7 +46,7 @@ int main()
   world.EliteSelect(10, 10);
   world.Update();
   std::cout << "Post-Elite Size = " << world.GetSize() << std::endl;
-  for (int i = 0; i < world.GetSize(); i++) std::cout << world[i] << " ";
+  for (size_t i = 0; i < world.GetSize(); i++) std::cout << world[i] << " ";
   std::cout << std::endl << std::endl;
 
 
@@ -55,20 +55,20 @@ int main()
   for (int i = 0; i < 100; i++) ea_world.Insert(i+200);
 
   std::cout << "Start Size = " << ea_world.GetSize() << std::endl;
-  for (int i = 0; i < ea_world.GetSize(); i++) std::cout << ea_world[i].Fitness() << " ";
+  for (size_t i = 0; i < ea_world.GetSize(); i++) std::cout << ea_world[i].Fitness() << " ";
   std::cout << std::endl;
 
   ea_world.TournamentSelect(5, 100);
   ea_world.Update();
   std::cout << "Post-Tourney Size = " << ea_world.GetSize() << std::endl;
-  for (int i = 0; i < ea_world.GetSize(); i++) std::cout << ea_world[i].Fitness() << " ";
+  for (size_t i = 0; i < ea_world.GetSize(); i++) std::cout << ea_world[i].Fitness() << " ";
   std::cout << std::endl;
 
   // world.EliteSelect([](int * i){ return (double) *i; }, 10, 10);
   ea_world.EliteSelect(10, 10);
   ea_world.Update();
   std::cout << "Post-Elite Size = " << ea_world.GetSize() << std::endl;
-  for (int i = 0; i < ea_world.GetSize(); i++) std::cout << ea_world[i].Fitness() << " ";
+  for (size_t i = 0; i < ea_world.GetSize(); i++) std::cout << ea_world[i].Fitness() << " ";
   std::cout << std::endl << std::endl;
 
 
@@ -77,8 +77,8 @@ int main()
   for (int i = 0; i < 10; i++) grid_world.Insert(i);
   grid_world.Print();
 
-  for (int i = 0; i < grid_world.GetSize(); ++i) {
-    int id = random.GetInt(grid_world.GetSize());
+  for (size_t i = 0; i < grid_world.GetSize(); ++i) {
+    size_t id = random.GetUInt(grid_world.GetSize());
     if (grid_world.IsOccupied(id)) grid_world.InsertBirth(grid_world[id], id);
   }
   std::cout << std::endl;
@@ -96,23 +96,23 @@ int main()
     for (int i=0;i<20;i++) pi_world.popM.AddOrgAppend(new int (i+100));
   });
 
-  pi_world.popM.OnAddOrg( [&pi_world](int* org, int& pos) {
+  pi_world.popM.OnAddOrg( [&pi_world](int* org, size_t& pos) {
     pos = pi_world.popM.AddOrgAppend(org);
   });
 
-  pi_world.popM.OnAddOrgBirth( [&pi_world](int* org, int parent_pos, int& pos) {
-    pos=parent_pos-1;
-    if (pos < 0) pos = pi_world.popM.size() - 1;
+  pi_world.popM.OnAddOrgBirth( [&pi_world](int* org, size_t parent_pos, size_t& pos) {
+    if (parent_pos == 0) pos = pi_world.popM.size() - 1;
+    else pos=parent_pos-1;
     pi_world.popM.AddOrgAt(org, pos);
   });
 
   pi_world.Clear();
   std::cout << "Starting PIWorld size: " << pi_world.GetSize() << std::endl;
-  for (int i = 0; i < pi_world.GetSize(); i++) std::cout << pi_world[i] << " ";
+  for (size_t i = 0; i < pi_world.GetSize(); i++) std::cout << pi_world[i] << " ";
   std::cout << std::endl;
 
   pi_world.TournamentSelect([](int * i){ return (double) *i; }, 2, 100);
   std::cout << "Post-Tourney Size = " << pi_world.GetSize() << std::endl;
-  for (int i = 0; i < pi_world.GetSize(); i++) std::cout << pi_world[i] << " ";
+  for (size_t i = 0; i < pi_world.GetSize(); i++) std::cout << pi_world[i] << " ";
   std::cout << std::endl;
 }
