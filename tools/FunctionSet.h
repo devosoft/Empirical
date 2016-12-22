f3dccd39f48b2827e1474f1d8f7034ee55c04689
Charles Ofria <charles.ofria@gmail.com>
2016-12-21 23:42:59 -0500

    Shifted FunctionSet to be derived from emp::vector (thus gaining functionality).

--- tools/FunctionSet.h
+++ tools/FunctionSet.h
@@ -3,6 +3,7 @@
 //  Released under the MIT Software license; see doc/LICENSE
 //
 //  Setup a collection of functions, all with the same signature, that can be run as a group.
+//  Derived from emp::vector, hence with all of the same methods as vector.
 
 #ifndef EMP_FUNCTION_SET_H
 #define EMP_FUNCTION_SET_H
@@ -12,28 +13,29 @@
 
 namespace emp {
 
-  template <typename RETURN_T, typename... ARG_TYPES> class FunctionSet {
+  template <typename RETURN_T, typename... ARG_TYPES>
+  class FunctionSet : public emp::vector<std::function<RETURN_T(ARG_TYPES...)>> {
   private:
-    emp::vector<std::function<RETURN_T(ARG_TYPES...)> > fun_set;
     mutable emp::vector<RETURN_T> return_vals;
 
   public:
     FunctionSet() { ; }
     ~FunctionSet() { ; }
 
+    using base_t = emp::vector<std::function<RETURN_T(ARG_TYPES...)>>;
+    using value_type = typename base_t::value_type;
     using return_t = RETURN_T;
 
-    size_t GetSize() const { return fun_set.size(); }
-    size_t size() const { return fun_set.size(); }        // For standard lib compatability
+    size_t GetSize() const { return base_t::size(); }
 
-    void Add(const std::function<RETURN_T(ARG_TYPES...)> & in_fun) { fun_set.push_back(in_fun); }
-    void Remove(size_t pos) { fun_set.erase(fun_set.begin()+pos); }
+    void Add(const value_type & in_fun) { base_t::push_back(in_fun); }
+    void Remove(size_t pos) { base_t::erase(base_t::begin()+pos); }
 
     const emp::vector<RETURN_T> & Run(ARG_TYPES... args) const {
-      const size_t num_tests = fun_set.size();
+      const size_t num_tests = base_t::size();
       return_vals.resize(num_tests);
       for (size_t i = 0; i < num_tests; i++) {
-        return_vals[i] = (fun_set[i])(args...);
+        return_vals[i] = (*this)[i](args...);
       }
       return return_vals;
     }
@@ -42,7 +44,7 @@ namespace emp {
     // The filter should take in two return values and indicate which is "better".
     RETURN_T Run(ARG_TYPES... params, std::function<RETURN_T(RETURN_T, RETURN_T)> comp_fun,
                     RETURN_T default_val=0) const {
-      if (fun_set.size() == 0) return default_val;  // If we have no entries, return the default.
+      if (base_t::size() == 0) return default_val;  // If we have no entries, return the default.
 
       Run(params...);
 
@@ -68,22 +70,22 @@ namespace emp {
 
   // A specialized version for void functions.
 
-  template <typename... ARG_TYPES> class FunctionSet<void, ARG_TYPES...> {
-  private:
-    emp::vector<std::function<void(ARG_TYPES...)> > fun_set;
-
+  template <typename... ARG_TYPES>
+  class FunctionSet<void, ARG_TYPES...> : public emp::vector<std::function<void(ARG_TYPES...)>> {
   public:
     FunctionSet() { ; }
     ~FunctionSet() { ; }
 
-    size_t GetSize() const { return fun_set.size(); }
-    size_t size() const { return fun_set.size(); }     // For compatability with STL
+    using base_t = emp::vector<std::function<void(ARG_TYPES...)>>;
+    using value_type = typename base_t::value_type;
+    using return_t = void;
 
-    void Add(const std::function<void(ARG_TYPES...)> & in_fun) { fun_set.push_back(in_fun); }
-    void Remove(size_t pos) { fun_set.erase(fun_set.begin()+pos); }
+    size_t GetSize() const { return base_t::size(); }
+    void Add(const std::function<void(ARG_TYPES...)> & in_fun) { base_t::push_back(in_fun); }
+    void Remove(size_t pos) { base_t::erase(base_t::begin()+pos); }
 
     void Run(ARG_TYPES... params) const {
-      for (const std::function<void(ARG_TYPES...)> & cur_fun : fun_set) {
+      for (const std::function<void(ARG_TYPES...)> & cur_fun : *this) {
         cur_fun(params...);
       }
     }
