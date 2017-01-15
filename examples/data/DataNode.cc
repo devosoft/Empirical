0cd9ea6374e5e711ff30a6ae3a81372acfb19fb9
Charles Ofria <charles.ofria@gmail.com>
2017-01-15 13:55:52 -0500

    Show examples of adding bulk data, adding a single datum, resetting, and then adding more data.

--- examples/data/DataNode.cc
+++ examples/data/DataNode.cc
@@ -14,6 +14,32 @@ int main()
   emp::DataNode<int, emp::DataCurrent, emp::DataStats> data;
   data.Add(27, 28, 29);
 
+  std::cout << "=> Added 27, 28, and 29" << std::endl;
+  std::cout << "Current = " << data.GetCurrent() << std::endl;
+  std::cout << "Total   = " << data.GetTotal() << std::endl;
+  std::cout << "Mean    = " << data.GetMean() << std::endl;
+  std::cout << "Min     = " << data.GetMin() << std::endl;
+  std::cout << "Max     = " << data.GetMax() << std::endl;
+
+  data.Add(32);
+  std::cout << "\n=> Added 32" << std::endl;
+  std::cout << "Current = " << data.GetCurrent() << std::endl;
+  std::cout << "Total   = " << data.GetTotal() << std::endl;
+  std::cout << "Mean    = " << data.GetMean() << std::endl;
+  std::cout << "Min     = " << data.GetMin() << std::endl;
+  std::cout << "Max     = " << data.GetMax() << std::endl;
+
+
+  data.Reset();
+  std::cout << "\n=> Reset!" << std::endl;
+  std::cout << "Current = " << data.GetCurrent() << std::endl;
+  std::cout << "Total   = " << data.GetTotal() << std::endl;
+  std::cout << "Mean    = " << data.GetMean() << std::endl;
+  std::cout << "Min     = " << data.GetMin() << std::endl;
+  std::cout << "Max     = " << data.GetMax() << std::endl;
+
+  data.Add(100,200,300,400,500);
+  std::cout << "\nAdded 100,200,300,400,500" << std::endl;
   std::cout << "Current = " << data.GetCurrent() << std::endl;
   std::cout << "Total   = " << data.GetTotal() << std::endl;
   std::cout << "Mean    = " << data.GetMean() << std::endl;
