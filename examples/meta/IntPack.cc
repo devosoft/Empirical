15c780e4fa4276f159f45f897f40212a41954978
Charles Ofria <charles.ofria@gmail.com>
2017-01-18 09:54:33 -0500

    Added macro for printing full IntPack to example file; added examples for Min, Max, and remove

--- examples/meta/IntPack.cc
+++ examples/meta/IntPack.cc
@@ -11,13 +11,17 @@
 
 #include "../../meta/IntPack.h"
 
-#define EMP_PRINT_VAL(X) std::cout << #X << " = " << X << std::endl;
+#define EMP_PRINT_VAL(X) std::cout << #X << " = " << X << std::endl
+#define EMP_PRINT_PACK(X) std::cout << #X << " = "; X::PrintInts(); std::cout << std::endl
 
 int main()
 {
   // using rise_t = emp::IntPack<0,1,2,3,4,5,6,7,8,9,10>;
   using rise_t = emp::IntPackRange<1,11>;
   std::cout << "using rise_t = emp::IntPack<1,2,3,4,5,6,7,8,9,10>" << std::endl;
+  std::cout << "Printing Pack: emp::IntPack<";
+  rise_t::PrintInts();
+  std::cout << '>' << std::endl;
   EMP_PRINT_VAL(rise_t::GetSize());
   EMP_PRINT_VAL(rise_t::Has<0>());
   EMP_PRINT_VAL(rise_t::Has<13>());
@@ -30,7 +34,10 @@ int main()
   EMP_PRINT_VAL(rise_t::Product());
 
   using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>;
-  std::cout << "using fib_t = emp::IntPack<0,1,1,2,3,5,8,13,21,34>" << std::endl;
+  std::cout << "\nusing fib_t  = emp::IntPack<0,1,1,2,3,5,8,13,21,34>" << std::endl;
+  std::cout << "Printing Pack: emp::IntPack<";
+  fib_t::PrintInts();
+  std::cout << '>' << std::endl;
   EMP_PRINT_VAL(fib_t::GetSize());
   EMP_PRINT_VAL(fib_t::Has<0>());
   EMP_PRINT_VAL(fib_t::Has<13>());
@@ -41,4 +48,12 @@ int main()
   EMP_PRINT_VAL(fib_t::IsUnique());
   EMP_PRINT_VAL(fib_t::Sum());
   EMP_PRINT_VAL(fib_t::Product());
+
+  std::cout << "\nTesting sorting and removing.\n";
+  using test_t = emp::IntPack<0,1,2,1,3,4,1,5,6,1,-1,0>;
+  std::cout << "Starting pack: ";  test_t::PrintInts(); std::cout << std::endl;
+  EMP_PRINT_VAL(test_t::Min());
+  EMP_PRINT_VAL(test_t::Max());
+  EMP_PRINT_PACK(test_t);
+  EMP_PRINT_PACK(test_t::remove<1>);
 }
