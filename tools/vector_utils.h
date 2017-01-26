2971a5a918991a848294fa8fa336b7c0ee1ee188
Charles Ofria <charles.ofria@gmail.com>
2017-01-25 22:16:59 -0500

    Added Heapify to vector_utils.

--- tools/vector_utils.h
+++ tools/vector_utils.h
@@ -15,4 +15,31 @@ namespace emp {
   constexpr size_t tree_left(size_t id) { return id*2+1; }
   constexpr size_t tree_right(size_t id) { return id*2+2; }
   constexpr size_t tree_parent(size_t id) { return (id-1)/2; }
+
+  // Heap manipulation.
+  template <typename T>
+  void Heapify(emp::vector<T> & v, size_t id) {
+    const size_t id_left = tree_left(id);
+    if (id_left >= v.size()) return;  // Nothing left to heapify.
+
+    const T val = v[id];
+    const T val_left = v[id_left];
+
+    const size_t id_right = tree_right(id);
+    if (id_right < v.size()) {
+      const T val_right = v[id_right];
+      if (val_right > val_left && val_right > val) {
+        v[id] = val_right;
+        v[right_id] = val;
+        Heapify(v, right_id);
+        return;
+      }
+    }
+
+    if (val_left > val) {
+      v[id] = val_left;
+      v[left_id] = val;
+      Heapify(v, left_id);
+    }
+  }
 }
