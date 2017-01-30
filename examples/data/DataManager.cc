e615596196179acd4473d1932a1f673dfd543aec
Charles Ofria <charles.ofria@gmail.com>
2017-01-29 23:21:26 -0500

    Added modification of nodes with DataManager

--- examples/data/DataManager.cc
+++ examples/data/DataManager.cc
@@ -13,5 +13,19 @@ using data = emp::data;
 
 int main()
 {
-  emp::DataManager<int, data::Current, data::Range, data::Pull, data::Log> dataM;
+  emp::DataManager<double, data::Current, data::Range, data::Pull, data::Log> dataM;
+
+  dataM.New("fitness");
+  dataM.New("merit");
+  dataM.New("fidelity");
+
+  dataM.AddData("fitness", 3.0);
+  dataM.Get("fitness").Add(6.5);
+  auto & fit_node = dataM.Get("fitness");
+  fit_node.Add(10.0);
+
+  auto & merit_node = dataM.Get("merit");
+  merit_node.Add(300, 650, 500);
+
+  dataM.AddData("fidelity", 1.0, 0.8, 0.9);
 }
