fdf0d6af72c0f8815edc635b255831a5157d0818
Charles Ofria <charles.ofria@gmail.com>
2017-02-01 08:58:47 -0500

    Added a skeleton for DataFile.h

---
+++ data/DataFile.h
@@ -0,0 +1,26 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016-2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  DataNode objects track a set of functions to run to build an output file.
+
+#ifndef EMP_DATA_FILE_H
+#define EMP_DATA_FILE_H
+
+#include "../base/vector.h"
+#include "../tools/assert.h"
+#include "../tools/FunctionSet.h"
+#include "../tools/string_utils.h"
+
+namespace emp {
+
+  class DataFile {
+  private:
+
+  public:
+  };
+
+}
+
+#endif
