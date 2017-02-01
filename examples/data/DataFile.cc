3bb0662549ac19f26536827ac95a0d91df56b895
Charles Ofria <charles.ofria@gmail.com>
2017-02-01 13:20:42 -0500

    Setup a real DataFile example with seven columns and three DataNodes

--- examples/data/DataFile.cc
+++ examples/data/DataFile.cc
@@ -3,7 +3,7 @@
 //  Released under the MIT Software license; see doc/LICENSE
 //
 //
-//  Examples for DataFile demonstrating how to generate 
+//  Examples for DataFile demonstrating how to generate
 
 #include <iostream>
 
@@ -13,5 +13,27 @@ using data = emp::data;
 
 int main()
 {
+  emp::DataFile dfile("test_file.dat");
 
+  emp::DataMonitor<double> data_fracs;
+  emp::DataMonitor<int> data_squares;
+  emp::DataMonitor<uint64_t> data_cubes;
+
+  dfile.AddCurrent(data_fracs);
+  dfile.AddCurrent(data_squares);
+  dfile.AddCurrent(data_cubes);
+  dfile.AddMean(data_cubes);
+  dfile.AddTotal(data_cubes);
+  dfile.AddMin(data_cubes);
+  dfile.AddMax(data_cubes);
+
+  double frac = 0.0;
+  for (size_t i = 0; i < 10; i++) {
+    data_fracs.Add(frac += 0.01);
+    data_squares.Add((int)i*i);
+    data_cubes.Add(i*i*i);
+    dfile.Update();
+
+    std::cout << i << std::endl;
+  }
 }
