1aee3ecba032069b95cde5d15478a8dddb87de99
Charles Ofria <charles.ofria@gmail.com>
2017-02-07 12:09:36 -0500

    Changed WeightedSet to IndexMap in examples.

--- examples/tools/IndexMap.cc
+++ examples/tools/IndexMap.cc
@@ -1,5 +1,5 @@
 //  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2016.
+//  Copyright (C) Michigan State University, 2016-2017.
 //  Released under the MIT Software license; see doc/LICENSE
 //
 //
@@ -8,42 +8,42 @@
 #include <iostream>
 #include "../../tools/Random.h"
 #include "../../tools/vector.h"
-#include "../../tools/WeightedSet.h"
+#include "../../tools/IndexMap.h"
 
 int main()
 {
   const int SET_SIZE = 10000;
 
-  emp::WeightedSet test_set(SET_SIZE);
-  test_set[0] = 0.0;
-  test_set[1] = 0.5;
-  test_set[2] = 1.0;
-  test_set[3] = 1.5;
-  test_set[4] = 2.0;
-  test_set[5] = 2.5;
-  test_set[6] = 3.0;
-  test_set[7] = 3.5;
-  test_set[8] = 4.0;
-  test_set[9] = 4.5;
+  emp::IndexMap test_imap(SET_SIZE);
+  test_imap[0] = 0.0;
+  test_imap[1] = 0.5;
+  test_imap[2] = 1.0;
+  test_imap[3] = 1.5;
+  test_imap[4] = 2.0;
+  test_imap[5] = 2.5;
+  test_imap[6] = 3.0;
+  test_imap[7] = 3.5;
+  test_imap[8] = 4.0;
+  test_imap[9] = 4.5;
   std::cout << "Ping!" << std::endl;
 
   emp::Random random;
   emp::vector<int> count(10,0);
   for (size_t i = 0; i < 45000; i++) {
-    size_t id = test_set.Index(random.GetDouble(0,test_set.GetWeight()));
+    size_t id = test_imap.Index(random.GetDouble(0,test_imap.GetWeight()));
     count[id]++;
   }
   for (size_t id = 0; id < 10; id++) {
     std::cout << id << " : " << count[id] << std::endl;
   }
 
-  std::cout << "Start weight = " << test_set.GetWeight() << std::endl;
+  std::cout << "Start weight = " << test_imap.GetWeight() << std::endl;
 
   std::cout << "\n... adjusting set weights ...\n";
   for (size_t i = 0; i < 100000000; i++) {
-    test_set[random.GetUInt(SET_SIZE)] = random.GetDouble(0.0,5000.0);
+    test_imap[random.GetUInt(SET_SIZE)] = random.GetDouble(0.0,5000.0);
   }
 
-  std::cout << "End weight = " << test_set.GetWeight() << std::endl;
+  std::cout << "End weight = " << test_imap.GetWeight() << std::endl;
 
 }
