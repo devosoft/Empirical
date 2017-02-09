cf2954debab9f17d2cb951508d406f940b77b1e8
Charles Ofria <charles.ofria@gmail.com>
2017-02-08 19:19:14 -0500

    Updated header

--- evo/PopulationManager.h
+++ evo/PopulationManager.h
@@ -1,5 +1,5 @@
 //  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2016.
+//  Copyright (C) Michigan State University, 2016-2017.
 //  Released under the MIT Software license; see doc/LICENSE
 //
 //  This file defines built-in population managers for use with emp::evo::World
