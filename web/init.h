eadd94b1ca24b4338d3dd496e72e8084268ae589
Charles Ofria <charles.ofria@gmail.com>
2017-02-25 14:31:18 -0500

    Moved main web_init.h functionality to init.h

--- web/init.h
+++ web/init.h
@@ -1,5 +1,5 @@
 //  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2015-2016.
+//  Copyright (C) Michigan State University, 2015-2017.
 //  Released under the MIT Software license; see doc/LICENSE
 //
 //
@@ -30,10 +30,49 @@ namespace emp {
     // Setup everything that needs to be initialized on the JS side...
     EMP_Initialize();
 
+    // Setup the animation callback in Javascript
+    EM_ASM({
+      window.requestAnimFrame = (function(callback) {
+          return window.requestAnimationFrame
+            || window.webkitRequestAnimationFrame
+            || window.mozRequestAnimationFrame
+            || window.oRequestAnimationFrame
+            || window.msRequestAnimationFrame
+            || function(callback) { window.setTimeout(callback, 1000 / 60); };
+        })();
+    });
+
     return true;
   }
 
+  namespace web {
+    // Some helper functions.
+    // Live keyword means that whatever is passed in needs to be re-evaluated every update.
+    namespace {
+      // If a variable is passed in to live, construct a function to look up its current value.
+      template <typename VAR_TYPE>
+      std::function<std::string()> Live_impl(VAR_TYPE & var, bool) {
+        return [&var](){ return emp::to_string(var); };
+      }
+
+      // If anything else is passed in, assume it is a function!
+      template <typename IN_TYPE>
+      std::function<std::string()> Live_impl(IN_TYPE && fun, int) {
+        return [fun](){ return emp::to_string(fun()); };
+      }
+    }
+
+    template <typename T>
+    std::function<std::string()> Live(T && val) {
+      return Live_impl(std::forward<T>(val), true);
+    }
+  }
+
 }
+
+
+///////////////////////////////////////////////////////////////////
+//  Initialization for NON emscripten to ignore macros.
 
 #else
 
--- web/web_init.h
+++ web/web_init.h
@@ -25,41 +25,9 @@ namespace web {
     // Run the base-level initialize in case it hasn't be run yet.
     emp::Initialize();
 
-    // Setup the animation callback in Javascript
-    EM_ASM({
-        window.requestAnimFrame = (function(callback) {
-            return window.requestAnimationFrame
-              || window.webkitRequestAnimationFrame
-              || window.mozRequestAnimationFrame
-              || window.oRequestAnimationFrame
-              || window.msRequestAnimationFrame
-              || function(callback) { window.setTimeout(callback, 1000 / 60); };
-          })();
-      });
-
     return true;
   }
 
-  // Some helper functions.
-  // Live keyword means that whatever is passed in needs to be re-evaluated every update.
-  namespace {
-    // If a variable is passed in to live, construct a function to look up its current value.
-    template <typename VAR_TYPE>
-    std::function<std::string()> Live_impl(VAR_TYPE & var, bool) {
-      return [&var](){ return emp::to_string(var); };
-    }
-
-    // If anything else is passed in, assume it is a function!
-    template <typename IN_TYPE>
-    std::function<std::string()> Live_impl(IN_TYPE && fun, int) {
-      return [fun](){ return emp::to_string(fun()); };
-    }
-  }
-
-  template <typename T>
-  std::function<std::string()> Live(T && val) {
-    return Live_impl(std::forward<T>(val), true);
-  }
 }
 }
 
