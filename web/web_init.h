85da68e7287a152629317ce703241d3b9ce1d0d1
Charles Ofria <charles.ofria@gmail.com>
2017-02-24 23:44:43 -0500

    Moved all functionaity from web.h to web_init.h. Now web.h is just an alias for Document.h

--- web/web_init.h
+++ web/web_init.h
@@ -9,6 +9,7 @@
 #ifndef EMP_WEB_INIT_H
 #define EMP_WEB_INIT_H
 
+#include "../tools/string_utils.h"
 #include "init.h"
 
 namespace emp {
@@ -39,6 +40,26 @@ namespace web {
     return true;
   }
 
+  // Some helper functions.
+  // Live keyword means that whatever is passed in needs to be re-evaluated every update.
+  namespace {
+    // If a variable is passed in to live, construct a function to look up its current value.
+    template <typename VAR_TYPE>
+    std::function<std::string()> Live_impl(VAR_TYPE & var, bool) {
+      return [&var](){ return emp::to_string(var); };
+    }
+
+    // If anything else is passed in, assume it is a function!
+    template <typename IN_TYPE>
+    std::function<std::string()> Live_impl(IN_TYPE && fun, int) {
+      return [fun](){ return emp::to_string(fun()); };
+    }
+  }
+
+  template <typename T>
+  std::function<std::string()> Live(T && val) {
+    return Live_impl(std::forward<T>(val), true);
+  }
 }
 }
 
