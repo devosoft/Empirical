8be3d60c5c850b6abbe879433e5be0782bd53db0
Charles Ofria <charles.ofria@gmail.com>
2017-02-26 23:35:03 -0500

    Expanded Attributes.cc to handle clicks; added tests for Listeners.

--- examples/web/Attributes.cc
+++ examples/web/Attributes.cc
@@ -15,8 +15,19 @@ int main()
   doc << UI::Text("test3") << "Testing 3.<br>";
   doc << UI::Text("test4").SetAttr("class", "make_red") << "Testing 4.<br>";
   doc << UI::Text("test5") << "Testing 5.<br>";
+  doc << UI::Text("test6") << "Click Me.<br>";
+  doc << UI::Text("test7") << "Want to know the time?<br>";
 
   // CSS settings can also be changed after being added to a doc.
   doc.Text("test3").SetCSS("color", "blue");
   doc.Text("test5").SetAttr("class", "make_red make_large");
+  doc.Text("test6").SetAttr("onclick", "this.innerHTML='You Clicked!<br>'");
+  doc.Text("test7").SetAttr("onclick", "this.innerHTML=Date()");
+
+
+  doc.Text("test2").On("click", [](UI::MouseEvent evt){ doc.Text("test2")
+    .SetCSS("color", "red") << "You Clicked! " << evt.shiftKey << "<br>"; });
+  UI::Listeners listeners;
+  listeners.Set("click", std::function<void()>([](){ doc.Text("test2").SetAttr("color", "red"); }));
+  listeners.Apply("test2");
 }
