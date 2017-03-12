9ca6883da9cf7548a234b363044960547c5aac70
Charles Ofria <charles.ofria@gmail.com>
2017-03-12 17:49:40 -0400

    Modularized Sudoku cell drawing into UpdateCell()

--- examples/web/Sudoku.cc
+++ examples/web/Sudoku.cc
@@ -13,6 +13,29 @@ class SudokuBoard : public UI::Slate {
 private:
   std::string name;
   UI::Table table;
+  emp::vector<char> state;
+
+  void UpdateCell(size_t r, size_t c) {
+    auto cell = table.GetCell(r, c);
+    cell.SetCSS("border", "1px solid black"); // Make a think black line between cells
+    cell.SetCSS("width", "45px");             // Make cells all 45px by 45px squares
+    cell.SetCSS("height", "45px");
+
+    emp_assert(state.size() == 81);
+    const char cur_symbol = state[r*9+c];
+    switch (cur_symbol) {
+    case '-':   // Empty cell!
+    case ' ':
+      break;
+    case '*':   // List of options!
+      // If we're adding a list of all nine options, use a smaller font.
+      cell.SetCSS("font", "15px Calibri, sans-serif")
+        << "<center>1 2 3<br>4 5 6<br>7 8 9</center>";
+      break;
+    default:    // Locked cell!
+      cell <<  "<center>" << cur_symbol << "</center>";
+    };
+  }
 public:
   SudokuBoard(const std::string & in_name)
   : UI::Slate(in_name)
@@ -28,23 +51,20 @@ public:
     // Add the table.
     *this << table;
 
-    table.SetCSS("border-collapse", "collapse")    // Remove gaps between cells in the puzzle
-      .SetCSS("font", "35px Calibri, sans-serif")  // Use a nice 35pt font for numbers in cells
-      .CellsCSS("border", "1px solid black")       // Make a think black line between cells
-      .CellsCSS("width", "45px")                   // Make cells all 45px by 45px squares
-      .CellsCSS("height", "45px");
+    table.SetCSS("border-collapse", "collapse")     // Remove gaps between cells in the puzzle
+      .SetCSS("font", "35px Calibri, sans-serif");  // Use a nice 35pt font for numbers in cells
 
     // Setup some values to go in the puzzle.
     // Note that '-' is an empty cell, and '*' shows all cell options.
-    std::vector<char> puzzle = {{ '-','1','2', '3','4','5', '6','7','8',
-                                  '5','-','4', '6','9','8', '1','2','3',
-                                  '3','8','-', '1','7','2', '9','5','4',
-                                  '8','2','9', '7','3','6', '4','1','5',
-                                  '1','5','3', '8','2','4', '7','6','9',
-                                  '6','4','7', '9','5','1', '3','8','2',
-                                  '7','9','1', '5','8','3', '2','4','6',
-                                  '4','6','5', '2','1','9', '8','*','7',
-                                  '2','3','8', '4','6','7', '5','9','1'
+    state = {{ '-','1','2', '3','4','5', '6','7','8',
+               '5','-','4', '6','9','8', '1','2','3',
+               '3','8','-', '1','7','2', '9','5','4',
+               '8','2','9', '7','3','6', '4','1','5',
+               '1','5','3', '8','2','4', '7','6','9',
+               '6','4','7', '9','5','1', '3','8','2',
+               '7','9','1', '5','8','3', '2','4','6',
+               '4','6','5', '2','1','9', '8','*','7',
+               '2','3','8', '4','6','7', '5','9','1'
       }};
 
     // Make regions have a dark background.
@@ -64,40 +84,28 @@ public:
           // cell.SetCSS("BackgroundColor", "grey");
           cell.Clear();
         });
-        cell.On("mouseup", [cell,r,c]() mutable {
+        cell.On("mouseup", [this,r,c]() mutable {
           // cell.SetCSS("BackgroundColor", "white");
           // doc.Slate("table_bg").SetBackground("white");
-          cell.SetCSS("font", "15px Calibri, sans-serif")
-            << "<center>1 2 3<br>4 5 6<br>7 8 9</center>";
+          state[r*9+c] = '*';
+          UpdateCell(r,c);
         });
         // cell.On("mousemove", [cell]() mutable {
         //   cell.SetCSS("BackgroundColor", "pink");
         //   doc.Slate("table_bg").SetBackground("pink");
         // });
 
-        const char cur_symbol = puzzle[r*9+c];
-        switch (cur_symbol) {
-        case '-':   // Empty cell!
-        case ' ':
-          break;
-        case '*':   // List of options!
-          // If we're adding a list of all nine options, use a smaller font.
-          cell.SetCSS("font", "15px Calibri, sans-serif")
-            << "<center>1 2 3<br>4 5 6<br>7 8 9</center>";
-          break;
-        default:    // Locked cell!
-          cell <<  "<center>" << cur_symbol << "</center>";
-        };
+        UpdateCell(r,c);
       }
     }
 
   }
 };
 
+SudokuBoard board("test_board");
+
 int main()
 {
   doc << "<h1>Sudoku!</h1>";
-
-  SudokuBoard board("test_board");
   doc << board;
 }
