f9f608247a6eb80ea4dc31d9050bedc82e84c975
Charles Ofria <charles.ofria@gmail.com>
2017-03-17 21:30:37 -0400

    Turned Range into a proper class; added accessors a Spread() method.

--- tools/Range.h
+++ tools/Range.h
@@ -3,28 +3,47 @@
 //  Released under the MIT Software license; see doc/LICENSE
 //
 //
-//  A simple way to track value ranges [lower, upper)
+//  A simple way to track value ranges
 
 #ifndef EMP_RANGE_H
 #define EMP_RANGE_H
 
-#include "assert.h"
+#include "../base/assert.h"
+#include "../base/vector.h"
 
 namespace emp {
 
   template <typename T>
-  struct Range {
+  class Range {
+  private:
     T lower;
     T upper;
 
+  public:
     Range(T _l, T _u) : lower(_l), upper(_u) { emp_assert(_l < _u); }
+
+    T GetLower() const { return lower; }
+    T GetUpper() const { return upper; }
 
     Range & operator=(const Range&) = default;
     bool operator==(const Range& _in) const { return lower==_in.lower && upper==_in.upper; }
     bool operator!=(const Range& _in) const { return lower!=_in.lower || upper!=_in.upper; }
 
     void Set(T _l, T _u) { emp_assert(_l < _u); lower = _l; upper = _u; }
-    bool Valid(T value) { return value >= lower && value < upper; }
+    bool Valid(T value) { return value >= lower && value <= upper; }
+
+    emp::vector<T> Spread(size_t s) {
+      emp_assert(s >= 1);
+      emp::vector<T> out(s);
+      out[0] = lower;
+      if (s > 1) {
+        T range = upper - lower;
+        for (size_t i = 1; i < s; i++) {
+          out[i] = lower + (T) ((((double) i) / (double)(s-1)) * range);
+        }
+      }
+      return out;
+    }
   };
 
   template <typename T> Range<T> MakeRange(T _l, T _u) { return Range<T>(_l,_u); }
