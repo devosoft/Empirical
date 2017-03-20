8fa252780d1b876a1bd62e548dedaf5d552e130e
Charles Ofria <charles.ofria@gmail.com>
2017-03-19 23:28:07 -0400

    Added classes for TableColGroup and TableRowGroup

---
+++ web/_TableColGroup.h
@@ -0,0 +1,69 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  The TableColGroup widget, which behaves like the Table widget, but focuses on a column group.
+//
+//  DO NOT include directly.  All files begining with '_' are for internal use only.
+
+class TableColGroup : public Table {
+public:
+  TableColGroup(size_t r, size_t c, const std::string & in_id="") : Table(r,c,in_id) { ; }
+  TableColGroup(const Table & in) : Table(in) { ; }
+  TableColGroup(const Widget & in) : Table(in) { ; }
+  TableColGroup(internal::TableInfo * in_info, size_t _col=0) : Table(in_info, 0, _col) { ; }
+
+  // Apply CSS to appropriate component based on current state.
+  void DoCSS(const std::string & setting, const std::string & value) override {
+    if (Info()->col_groups.size() == 0) Info()->col_groups.resize(GetNumCols());
+    Info()->col_groups[cur_col].extras.style.Set(setting, value);
+    if (IsActive()) Info()->ReplaceHTML();   // @CAO only should replace cell's CSS
+  }
+
+  void DoAttr(const std::string & setting, const std::string & value) override {
+    if (Info()->col_groups.size() == 0) Info()->col_groups.resize(GetNumCols());
+    Info()->col_groups[cur_col].extras.attr.Set(setting, value);
+    if (IsActive()) Info()->ReplaceHTML();   // @CAO only should replace cell's CSS
+  }
+
+  void DoListen(const std::string & event_name, size_t fun_id) override {
+    if (Info()->col_groups.size() == 0) Info()->col_groups.resize(GetNumCols());
+    Info()->col_groups[cur_col].extras.listen.Set(event_name, fun_id);
+    if (IsActive()) Info()->ReplaceHTML();   // @CAO only should replace cell's CSS
+  }
+
+  bool InStateColGroup() const { return true; }
+
+  TableColGroup Clear() { Info()->ClearColGroup(cur_col); return *this; }
+  TableColGroup ClearStyle() { Info()->ClearColGroupStyle(cur_col); return *this; }
+  TableColGroup ClearChildren() { Info()->ClearColGroupChildren(cur_col); return *this; }
+  TableColGroup ClearCells() { Info()->ClearColGroup(cur_col); return *this; }
+
+  std::string GetCSS(const std::string & setting) override {
+    return Info()->col_groups[cur_col].extras.GetStyle(setting);
+  }
+
+  TableColGroup SetColSpan(size_t new_span) {
+    emp_assert((cur_col + new_span <= GetNumCols()) && "Col span too wide for table!",
+               cur_col, new_span, GetNumCols(), GetID());
+
+    // If we haven't setup columns at all yet, do so.
+    if (Info()->col_groups.size() == 0) Info()->col_groups.resize(GetNumCols());
+
+    const size_t old_span = Info()->col_groups[cur_col].span;
+    Info()->col_groups[cur_col].span = new_span;
+
+    if (old_span != new_span) {
+      for (size_t i=old_span; i<new_span; i++) { Info()->col_groups[cur_col+i].masked = true; }
+      for (size_t i=new_span; i<old_span; i++) { Info()->col_groups[cur_col+i].masked = false; }
+    }
+
+    // Redraw the entire table to fix col span information.
+    if (IsActive()) Info()->ReplaceHTML();
+
+    return *this;
+  }
+
+  TableColGroup SetSpan(size_t new_span) { SetColSpan(new_span); return *this; }
+};
---
+++ web/_TableRowGroup.h
@@ -0,0 +1,68 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  The TableRowGroup widget, which behaves like the Table widget, but focuses on a row group.
+//
+//  DO NOT include directly.  All files begining with '_' are for internal use only.
+
+class TableRowGroup : public Table {
+public:
+  TableRowGroup(size_t r, size_t c, const std::string & in_id="") : Table(r,c,in_id) { ; }
+  TableRowGroup(const Table & in) : Table(in) { ; }
+  TableRowGroup(const Widget & in) : Table(in) { ; }
+  TableRowGroup(internal::TableInfo * in_info, size_t _row=0) : Table(in_info, _row, 0) { ; }
+
+  // Apply CSS to appropriate component based on current state.
+  void DoCSS(const std::string & setting, const std::string & value) override {
+    if (Info()->row_groups.size() == 0) Info()->row_groups.resize(GetNumRows());
+    Info()->row_groups[cur_row].extras.style.Set(setting, value);
+    if (IsActive()) Info()->ReplaceHTML();   // @CAO only should replace cell's CSS
+  }
+
+  void DoAttr(const std::string & setting, const std::string & value) override {
+    if (Info()->row_groups.size() == 0) Info()->row_groups.resize(GetNumRows());
+    Info()->row_groups[cur_row].extras.attr.Set(setting, value);
+    if (IsActive()) Info()->ReplaceHTML();   // @CAO only should replace cell's CSS
+  }
+
+  void DoListen(const std::string & event_name, size_t fun_id) override {
+    if (Info()->row_groups.size() == 0) Info()->row_groups.resize(GetNumRows());
+    Info()->row_groups[cur_row].extras.listen.Set(event_name, fun_id);
+    if (IsActive()) Info()->ReplaceHTML();   // @CAO only should replace cell's CSS
+  }
+
+  bool InStateRowGroup() const { return true; }
+
+  TableRowGroup & Clear() { Info()->ClearRowGroup(cur_row); return *this; }
+  TableRowGroup & ClearStyle() { Info()->ClearRowGroupStyle(cur_row); return *this; }
+  TableRowGroup & ClearChildren() { Info()->ClearRowGroupChildren(cur_row); return *this; }
+  TableRowGroup & ClearCells() { Info()->ClearRowGroup(cur_row); return *this; }
+
+  std::string GetCSS(const std::string & setting) override {
+    return Info()->row_groups[cur_row].extras.GetStyle(setting);
+  }
+
+  TableRowGroup & SetRowSpan(size_t new_span) {
+    emp_assert((cur_row + new_span <= GetNumRows()) && "Row span too wide for table!");
+
+    // If we haven't setup columns at all yet, do so.
+    if (Info()->row_groups.size() == 0) Info()->row_groups.resize(GetNumRows());
+
+    const size_t old_span = Info()->row_groups[cur_row].span;
+    Info()->row_groups[cur_row].span = new_span;
+
+    if (old_span != new_span) {
+      for (size_t i=old_span; i<new_span; i++) { Info()->row_groups[cur_row+i].masked = true; }
+      for (size_t i=new_span; i<old_span; i++) { Info()->row_groups[cur_row+i].masked = false; }
+    }
+
+    // Redraw the entire table to fix row span information.
+    if (IsActive()) Info()->ReplaceHTML();
+
+    return *this;
+  }
+
+  TableRowGroup SetSpan(size_t new_span) { SetRowSpan(new_span); return *this; }
+};
