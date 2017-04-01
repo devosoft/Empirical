c49662c7b2621e4be70bae3ee4f7c6517b349b73
Charles Ofria <charles.ofria@gmail.com>
2017-03-31 21:48:11 -0400

    Setup loading args from command line

--- papers/SpatialCoop2017/main.cc
+++ papers/SpatialCoop2017/main.cc
@@ -3,12 +3,6 @@
 #include "../../base/vector.h"
 #include "../../tools/Random.h"
 
-// Parameters
-double r = 0.02;    // Neighborhood radius
-//double k = 30;       // Number of neighbors
-double u = 0.025;   // cost / benefit ratio (-1 to +1)
-size_t N = 6400;    // Population size
-size_t E = 5000;     // How many epochs should a popuilation run for?
 
 struct Org {
   double x;
@@ -19,16 +13,102 @@ struct Org {
   emp::vector<size_t> neighbors;
 };
 
-// Calculations we'll need later.
-double r_sqr = r * r;  // r squared (for comparisons)
-emp::vector<Org> pop(N);
+// Create a class to maintain a simple Prisoner's Dilema world.
+class SimplePDWorld {
+private:
+  // Parameters
+  double r = 0.02;    // Neighborhood radius
+  double u = 0.025;   // cost / benefit ratio
+  size_t N = 6400;    // Population size
+  size_t E = 5000;     // How many epochs should a popuilation run for?
 
-double payoff_CC = 1.0;
-double payoff_CD = 0.0;
-double payoff_DC = 1.0 + u;
-double payoff_DD = u;
+  emp::Random random;
 
-void CalcFitness(size_t id) {
+  // Calculations we'll need later.
+  double r_sqr;          // r squared (for comparisons)
+  emp::vector<Org> pop;
+
+  double payoff_CC;
+  double payoff_CD;
+  double payoff_DC;
+  double payoff_DD;
+
+  // Helper functions
+  void CalcFitness(size_t id);
+  void Repro();
+public:
+  SimplePDWorld(double _r=0.02, double _u=0.025, size_t _N=6400, size_t _E=5000, int seed=0)
+   : random(seed)
+  {
+    Setup(_r, _u, _N, _E);
+  }
+
+  void Setup(double _r=0.02, double _u=0.0025, size_t _N=6400, size_t _E=5000) {
+    // Store the input values.
+    r = _r;
+    u = _u;
+    N = _N;
+    E = _E;
+
+    // Calculations we'll need later.
+    r_sqr = r * r;  // r squared (for comparisons)
+    pop.resize(N);
+
+    payoff_CC = 1.0;
+    payoff_CD = 0.0;
+    payoff_DC = 1.0 + u;
+    payoff_DD = u;
+
+    // Initialize each organism
+    for (Org & org : pop) {
+      org.x = random.GetDouble(1.0);
+      org.y = random.GetDouble(1.0);
+      org.coop = random.P(0.5);
+      org.neighbors.resize(0);
+    }
+
+    // Determine if pairs of organisms are neighbors;
+    for (size_t i = 1; i < N; i++) {
+      for (size_t j = 0; j < i; j++) {
+        Org & org1 = pop[i];
+        Org & org2 = pop[j];
+        double x_dist = org1.x - org2.x;
+        double y_dist = org1.y - org2.y;
+        double dist_sqr = x_dist * x_dist + y_dist * y_dist;
+
+        // Test if this pair are within neighbor radius...
+        if (dist_sqr < r_sqr) {
+          org1.neighbors.push_back(j);
+          org2.neighbors.push_back(i);
+        }
+      }
+    }
+
+    // Calculate the initial fitness for each organism in the population.
+    for (size_t id = 0; id < N; id++) {
+      CalcFitness(id);
+    }
+  }
+
+  void Reset(double r, double u, size_t N, size_t E) { Setup(r,u,N,E); }
+
+  void Run() {
+    // Run the organisms!
+    for (size_t epoch = 0; epoch < E; epoch++) {
+      std::cout << "Epoch = " << epoch
+  	      << ";  #Coop = " << CountCoop()
+  	      << std::endl;
+      for (size_t o = 0; o < N; o++) Repro();
+    }
+  }
+
+  size_t CountCoop();
+  void PrintNeighborInfo();
+};
+
+
+
+void SimplePDWorld::CalcFitness(size_t id) {
   Org & org = pop[id];
   size_t num_neighbors = org.neighbors.size();
 
@@ -62,10 +142,10 @@ void CalcFitness(size_t id) {
 
 
 // Reproduce into a single, random cell.
-void Repro(emp::Random & random) {
+void SimplePDWorld::Repro() {
   size_t id = random.GetUInt(N);
   Org & org = pop[id];
-  
+
   // Determine the total fitness of neighbors.
   double total_fitness = 0.0;
   for (size_t n : org.neighbors) {
@@ -81,7 +161,7 @@ void Repro(emp::Random & random) {
 	break;
       }
       choice -= pop[n].fitness;
-    }    
+    }
   }
 
   // Now that we have updated the organism, calculate its fitness again
@@ -90,13 +170,13 @@ void Repro(emp::Random & random) {
 }
 
 
-size_t CountCoop() {
+size_t SimplePDWorld::CountCoop() {
   size_t count = 0.0;
   for (Org & org : pop) if (org.coop) count++;
   return count;
 }
 
-void PrintNeighborInfo() {
+void SimplePDWorld::PrintNeighborInfo() {
   size_t total = 0;
   size_t max_size = 0;
   size_t min_size = pop[0].neighbors.size();
@@ -120,52 +200,33 @@ void PrintNeighborInfo() {
   }
 }
 
-int main()
+
+
+#include "../../config/ArgManager.h"
+
+EMP_BUILD_CONFIG( PDWorldConfig,
+  GROUP(DEFAULT, "Default settings for SimplePDWorld"),
+  VALUE(SEED, int, 0, "Random number seed (0 for based on time)"),
+  VALUE(r, double, 0.02, "Neighborhood radius, in fraction of world."),
+  VALUE(u, double, 0.0025, "cost / benefit ratio"),
+  VALUE(N, size_t, 6400, "Number of organisms in the popoulation."),
+  VALUE(E, size_t, 5000, "How many epochs should we process?"),
+)
+
+int main(int argc, char * argv[])
 {
-  emp::Random random;
+  PDWorldConfig config;
+  config.Read("PDWorld.cfg");
 
-  // Initialize each organism
-  for (Org & org : pop) {
-    org.x = random.GetDouble(1.0);
-    org.y = random.GetDouble(1.0);
-    org.coop = random.P(0.5);
-  }
+  auto args = emp::cl::ArgManager(argc, argv);
+  if (args.ProcessConfigOptions(config, std::cout, "PDWorld.cfg") == false) exit(1);
+  if (args.TestUnknown() == false) exit(2);  // If there are leftover args, throw an error.
 
-  // Determine if pairs of organisms are neighbors;
-  for (size_t i = 1; i < N; i++) {
-    for (size_t j = 0; j < i; j++) {
-      Org & org1 = pop[i];
-      Org & org2 = pop[j];
-      double x_dist = org1.x - org2.x;
-      double y_dist = org1.y - org2.y;
-      double dist_sqr = x_dist * x_dist + y_dist * y_dist;
-
-      // Test if this pair are within neighbor radius...
-      if (dist_sqr < r_sqr) {   
-	org1.neighbors.push_back(j);
-	org2.neighbors.push_back(i);
-      }
-    }
-  }
-
-
-  // Calculate the initial fitness for each organism in the population.
-  for (size_t id = 0; id < N; id++) {
-    CalcFitness(id);
-  }
-
-
-  // Run the organisms!
-  for (size_t epoch = 0; epoch < E; epoch++) {
-    std::cout << "Epoch = " << epoch
-	      << ";  #Coop = " << CountCoop()
-	      << std::endl;
-    for (size_t o = 0; o < N; o++) Repro(random);
-  }
-
+  SimplePDWorld world(config.r(), config.u(), config.N(), config.E(), config.SEED());
+  world.Run();
 
   // Print extra info?
-  PrintNeighborInfo();
+  world.PrintNeighborInfo();
 
   return 0;
 }
