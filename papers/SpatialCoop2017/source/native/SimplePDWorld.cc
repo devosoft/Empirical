cbaaa9eb3bff15d1b1c2457cd982ce60bd9c64c2
Charles Ofria <charles.ofria@gmail.com>
2017-04-04 22:24:06 -0400

    Cleaned up native setup for SimplePDWorld.

--- papers/SpatialCoop2017/source/native/SimplePDWorld.cc
+++ papers/SpatialCoop2017/source/native/SimplePDWorld.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "config/ArgManager.h"
 
 #include "../SimplePDWorld.h"
@@ -10,6 +11,7 @@ EMP_BUILD_CONFIG( PDWorldConfig,
   VALUE(u, double, 0.0025, "cost / benefit ratio"),
   VALUE(N, size_t, 6400, "Number of organisms in the popoulation."),
   VALUE(E, size_t, 5000, "How many epochs should we process?"),
+  VALUE(STEP, size_t, 10, "How often should we update data?"),
 )
 
 int main(int argc, char * argv[])
@@ -21,8 +23,24 @@ int main(int argc, char * argv[])
   if (args.ProcessConfigOptions(config, std::cout, "PDWorld.cfg") == false) exit(1);
   if (args.TestUnknown() == false) exit(2);  // If there are leftover args, throw an error.
 
-  SimplePDWorld world(config.r(), config.u(), config.N(), config.E(), config.SEED());
-  world.Run();
+  const size_t step = config.STEP();
+  const double r = config.r();
+  const double u = config.u();
+  const size_t N = config.N();
+  const size_t E = config.E();
+  const size_t seed = config.SEED();
+
+  //  std::ofstream f_data(filename_data);
+  //  std::ofstream f_dist(filename_dist);
+
+  SimplePDWorld world(r, u, N, E, seed);
+
+  for (size_t e = 0; e < E; e += step) {
+    world.Run(step);
+    const size_t num_coop = world.CountCoop();
+    const size_t num_defect = N - num_coop;
+    std::cout << e*step << ' ' << num_coop << ' ' << num_defect << '\n';
+  }
 
   // Print extra info?
   world.PrintNeighborInfo();
