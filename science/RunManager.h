9083cd083f58da7e6adde0e8f834b0f88009719a
Charles Ofria <charles.ofria@gmail.com>
2017-04-09 09:13:25 -0400

    Started building a generic RunManager

---
+++ science/RunManager.h
@@ -0,0 +1,19 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  A tool to control a series of runs and keep them updated.
+
+#ifndef EMP_RUN_MANAGER_H
+#define EMP_RUN_MANAGER_H
+
+namespace emp {
+
+  template <typename T>
+  class RunManager {
+  };
+
+}
+
+#endif
