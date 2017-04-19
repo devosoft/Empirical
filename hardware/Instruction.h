ca05c881c04f2f5b5531396045ef20d4fabb4898
Charles Ofria <charles.ofria@gmail.com>
2017-04-18 23:47:31 -0400

    Locked in final edits to Instruction.h before deleting.

--- hardware/Instruction.h
+++ hardware/Instruction.h
@@ -1,15 +1,19 @@
 //  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2015-2017.
+//  Copyright (C) Michigan State University, 2017.
 //  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  An emp::Instruction object hold all of the information for a single CPU Instruction.
+
 
 #ifndef EMP_INSTRUCTION_H
 #define EMP_INSTRUCTION_H
 
 namespace emp {
 
-  template <size_t ARGS>
+  template <typename T, size_t ARGS>
   struct Instruction {
-    size_t id;
+    T id;
     int args[ARGS];
 
     constexpr static size_t GetNumArgs() { return ARGS; }
