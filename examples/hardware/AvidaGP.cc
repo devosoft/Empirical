3921dfa050e6c80db48c8ebc035b67444a9cbcc5
Charles Ofria <charles.ofria@gmail.com>
2017-04-18 23:29:35 -0400

    Added AvidaGP test file.

---
+++ examples/hardware/AvidaGP.cc
@@ -0,0 +1,15 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2017.
+//  Released under the MIT Software license; see doc/LICENSE
+
+#include <iostream>
+
+#include "../../hardware/AvidaGP.h"
+
+int main()
+{
+  std::cout << "Test." << std::endl;
+
+  return 0;
+}
+
