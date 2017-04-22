896c98702f0f961409071cd92275a061580e8a4b
Charles Ofria <charles.ofria@gmail.com>
2017-04-21 23:27:47 -0400

    Setup converters in InstLib to move two and from symbol representations for instructions.

--- hardware/InstLib.h
+++ hardware/InstLib.h
@@ -31,14 +31,27 @@ namespace emp {
 
     emp::vector<InstDef> inst_lib;
     std::map<std::string, size_t> name_map;
-    std::array<size_t,128> symbol_chart;
+
+    static constexpr char ID2Symbol(ID_TYPE inst_id) {
+      const size_t id = (size_t) inst_id;
+      if (id < 26) return ('a' + id);
+      if (id < 52) return ('A' + (id - 26));
+      if (id < 62) return ('0' + (id - 52));
+      return '+';
+    }
+    static constexpr ID_TYPE Symbol2ID(char symbol) {
+      if (symbol >= 'a' && symbol <= 'z') return (ID_TYPE) (symbol - 'a');
+      if (symbol >= 'A' && symbol <= 'Z') return (ID_TYPE) (symbol - 'A' + 26);
+      if (symbol >= '0' && symbol <= '9') return (ID_TYPE) (symbol - '0' + 52);
+      return (ID_TYPE) 62;
+    }
 
   public:
     InstLib() { ; }
     ~InstLib() { ; }
 
     size_t AddInst(ID_TYPE inst_id, const std::string & name, const std::string & desc) {
-      size_t id = (size_t) inst_id;
+      const size_t id = (size_t) inst_id;
       if (inst_lib.size() <= id) inst_lib.resize(id+1);
       inst_lib[id] = InstDef(inst_id, name, desc);
       name_map[name] = id;
