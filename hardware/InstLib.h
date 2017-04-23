f0a98848bce36f0ff02d48f1bb42fca490ecd70a
Charles Ofria <charles.ofria@gmail.com>
2017-04-23 19:08:30 -0400

    Added accessors to InstLib for converting inst info.

--- hardware/InstLib.h
+++ hardware/InstLib.h
@@ -12,6 +12,7 @@
 #include <string>
 
 #include "../base/vector.h"
+#include "../tools/map_utils.h"
 
 namespace emp {
 
@@ -32,30 +33,36 @@ namespace emp {
     emp::vector<InstDef> inst_lib;
     std::map<std::string, size_t> name_map;
 
-    static constexpr char ID2Symbol(ID_TYPE inst_id) {
+  public:
+    InstLib() { ; }
+    ~InstLib() { ; }
+
+    size_t AddInst(ID_TYPE inst_id, const std::string & name, const std::string & desc="") {
       const size_t id = (size_t) inst_id;
+      if (inst_lib.size() <= id) inst_lib.resize(id+1);
+      inst_lib[id] = InstDef(inst_id, name, desc);
+      name_map[name] = id;
+    }
+
+    const std::string & GetName(ID_TYPE id) const { return inst_lib[id].name; }
+    const std::string & GetDesc(ID_TYPE id) const { return inst_lib[id].desc; }
+    static constexpr char GetSymbol(ID_TYPE id) {
       if (id < 26) return ('a' + id);
       if (id < 52) return ('A' + (id - 26));
       if (id < 62) return ('0' + (id - 52));
       return '+';
     }
-    static constexpr ID_TYPE Symbol2ID(char symbol) {
+    ID_TYPE GetID(const std::string name) const {
+      emp_assert(Has(name_map, name), name);
+      return name_map[name];
+    }
+    static constexpr ID_TYPE GetID(char symbol) {
       if (symbol >= 'a' && symbol <= 'z') return (ID_TYPE) (symbol - 'a');
       if (symbol >= 'A' && symbol <= 'Z') return (ID_TYPE) (symbol - 'A' + 26);
       if (symbol >= '0' && symbol <= '9') return (ID_TYPE) (symbol - '0' + 52);
       return (ID_TYPE) 62;
     }
 
-  public:
-    InstLib() { ; }
-    ~InstLib() { ; }
-
-    size_t AddInst(ID_TYPE inst_id, const std::string & name, const std::string & desc) {
-      const size_t id = (size_t) inst_id;
-      if (inst_lib.size() <= id) inst_lib.resize(id+1);
-      inst_lib[id] = InstDef(inst_id, name, desc);
-      name_map[name] = id;
-    }
   };
 
 }
