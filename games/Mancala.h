95b2b789a4ed29259847818ac9a6a6226d84e855
Charles Ofria <charles.ofria@gmail.com>
2017-05-09 03:46:39 -0400

    Brought Mancala into interface specification.

--- games/Mancala.h
+++ games/Mancala.h
@@ -19,9 +19,21 @@ namespace emp {
 
   class Mancala {
   private:
-    emp::array<int, 14> board;   // Current board state.
-    bool over = false;           // Has the game ended?
-    int curr_player = 0;         // Which player goes next?
+    emp::array<size_t, 14> board;   // Current board state.
+    bool over = false;              // Has the game ended?
+    size_t curr_player = 0;         // Which player goes next?
+
+    void TestOver() {
+      bool side_1_empty = true;
+      bool side_2_empty = true;
+
+      for (size_t i = 1; i < 7; i++) {
+        if (board[i] > 0) { side_1_empty = false; }
+        if (board[i+7] > 0) { side_2_empty = false; }
+      }
+
+      over = ( (!curr_player && side_1_empty) || (curr_player && side_2_empty));
+    }
 
   public:
 
@@ -35,37 +47,25 @@ namespace emp {
       curr_player = 0;
     }
 
-    int operator[](int i) const { return board[i]; }
+    size_t operator[](size_t i) const { return board[i]; }
 
-    const emp::array<int, 14>& GetBoard() const { return board; }
+    const emp::array<size_t, 14>& GetBoard() const { return board; }
 
-    emp::array<int, 14> GetFlippedBoard() {
-      emp::array<int, 14> flipped(board);
+    emp::array<size_t, 14> GetFlippedBoard() {
+      emp::array<size_t, 14> flipped(board);
       for (size_t i = 0; i < 7; i++) std::swap(flipped[i], flipped[i+7]);
       return flipped;
     }
 
-    void TestOver() {
-      bool side_1_empty = true;
-      bool side_2_empty = true;
-
-      for (int i = 1; i < 7; i++) {
-        if (board[i] > 0) { side_1_empty = false; }
-        if (board[i+7] > 0) { side_2_empty = false; }
-      }
-
-      over = ( (!curr_player && side_1_empty) || (curr_player && side_2_empty));
-    }
-
     // Returns bool indicating whether player can go again
-    bool ChooseCell(int cell) {
+    bool DoMove(size_t cell) {
       emp_assert(cell != 0 && cell != 7); // You can't choose the end cells
       emp_assert((cell < 7 && !curr_player) || (cell > 7 && curr_player));
 
-      int stone_count = board[cell];
-      int curr_cell = cell;
-      size_t home_cell = 7 * (size_t) (cell > 7);  // Track own store; base on start.
-      size_t skip_cell = 7 * (size_t) (cell < 7);  // Skip opponent's store; base on start.
+      size_t stone_count = board[cell];
+      size_t curr_cell = cell;
+      size_t home_cell = 7 * (size_t) (cell < 7);  // Track own store; base on start.
+      size_t skip_cell = 7 * (size_t) (cell > 7);  // Skip opponent's store; base on start.
 
       board[cell] = 0;
 
@@ -91,12 +91,21 @@ namespace emp {
         board[opposite] = 0;
       }
 
-      curr_player = (int)!curr_player;
+      curr_player = (size_t)!curr_player;
       TestOver();
       return false;
     }
 
-    bool IsMoveValid(int move) const {
+    // Setup a DoMove from either player's viewpoint.
+    bool DoMove(size_t player, size_t cell) {
+      emp_assert(cell > 0 && cell < 7, cell);  // Moves can only be 1 through 6.
+      if (player == 1) cell += 7;
+      return DoMove(cell);
+    }
+
+    bool IsDone() const { return over; }
+
+    bool IsMoveValid(size_t move) const {
       // Exclude never-valid moves or empty pits
       if (move >= 13 || move <= 0 || !board[move]) { return false; }
 
@@ -106,51 +115,45 @@ namespace emp {
       return false;
     }
 
-    bool IsOver() const { return over; }
 
-
-    void PrintBoard(std::ostream & os=std::cout) {
+    void Print(std::ostream & os=std::cout) {
       os << "  ";
-      for (int i = 6; i > 0; i--) {
+      for (size_t i = 6; i > 0; i--) {
         os << board[i] << " ";
       }
       os << std::endl;
       os << board[7] << "              " << board[0] << std::endl;
       os << "  ";
-      for (int i = 8; i < 14; i++) {
+      for (size_t i = 8; i < 14; i++) {
         os << board[i] << " ";
       }
       os << std::endl;
     }
 
-    int GetWinner() {
-      int player1 = board[0];
-      int player0 = board[7];
-
-      for (int i = 1; i < 7; i++) { player0 += board[i]; }
-      for (int i = 8; i < 14; i++) { player1 += board[i]; }
-
-      return (int) (player1 > player0);
-    }
-
-    int GetCurrPlayer() const { return curr_player; }
+    size_t GetCurrPlayer() const { return curr_player; }
     bool IsTurnA() const { return curr_player == 0; }
     bool IsTurnB() const { return curr_player == 1; }
 
     size_t ScoreA() const {
-      int score = board[7];
+      size_t score = board[7];
       for (size_t i = 1; i < 7; i++) {
         score += board[i];
       }
       return score;
     }
 
-    size_t ScoreB(int player) const {
-      int score = board[0];
+    size_t ScoreB() const {
+      size_t score = board[0];
       for (size_t) i = 8; i < 14; i++) {
         score += board[i];
       }
       return score;
+    }
+
+    double GetScore(size_t player) {
+      if (player == 0) return (double) ScoreA();
+      else if (player == 1) return (double) ScoreB();
+      emp_assert(false);  // Only a two player game!
     }
 
   };
