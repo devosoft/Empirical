0b86c4284e6ef27ca3933894b61ef05877d1024e
Charles Ofria <charles.ofria@gmail.com>
2017-05-11 08:02:22 -0400

    Setup UniqueName to be inline to prevent compile errors during debug.

--- tools/functions.h
+++ tools/functions.h
@@ -68,8 +68,8 @@ namespace emp {
     return val++;
   }
 
-  static std::string UniqueName(const std::string & prefix="",
-                                        const std::string & postfix="") {
+  static inline std::string UniqueName(const std::string & prefix="",
+                                       const std::string & postfix="") {
     std::stringstream ss;
     ss << prefix << UniqueVal() << postfix;
     return ss.str();
