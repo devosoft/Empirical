736edf4ac0577a952be02efcf8c192d47af24f7b
Charles Ofria <charles.ofria@gmail.com>
2017-05-18 12:02:13 -0400

    Working state grid; building a StateGridInfo class

--- evo/StateGrid.h
+++ evo/StateGrid.h
@@ -8,20 +8,50 @@
 #ifndef EMP_EVO_STATE_GRID_H
 #define
 
+#include "../base/vector.h"
+
 namespace emp {
 namespace evo {
 
-  class StateGrid {
-  private:
-    std::vector<int> states;
-    size_t width;
-    size_t height;
+  /// Full information about a state grid and meanings of each state.
+  class StateGridInfo {
+  protected:
+
+    /// Information about what a particular state type means in a state grid.
+    struct StateInfo {
+      int state_id;
+      char symbol;
+      std::string name;
+      std::string desc;
+    };
 
   public:
-    StateGrid(size_t _w, size_t _h) : width(_w), height(_h) { ; }
+    StateGridInfo() { ; }
+    ~StateGridInfo() { ; }
+
+  };
+
+  /// A StateGrid describes a map of grid positions to the current state of each position.
+  class StateGrid {
+  protected:
+    size_t width;
+    size_t height;
+    emp::vector<int> states;
+
+    StateGridInfo & info;
+
+  public:
+    StateGrid(StateGridInfo & _i, size_t _w, size_t _h)
+      : width(_w), height(_h), states(_w*_h,0), info(_i) { ; }
     StateGrid(const StateGrid &) = default;
     StateGrid(StateGrid &&) = default;
     ~StateGrid() { ; }
+
+    StateGrid & operator=(const StateGrid &) = default;
+    StateGrid & operator=(StateGrid &&) = default;
+
+    int & operator()(size_t x, size_t y) { return states[y*width+x]; }
+    int operator()(size_t x, size_t y) const { return states[y*width+x]; }
   };
 
 }
