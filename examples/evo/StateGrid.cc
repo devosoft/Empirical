2d2fda398b778175bec70a24e6efe8f9fce89df6
Charles Ofria <charles.ofria@gmail.com>
2017-05-19 17:15:35 -0400

    Added exmaple case for using StateGrid.

---
+++ examples/evo/StateGrid.cc
@@ -0,0 +1,20 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  This file provides an example of using the StateGrid class.
+
+#include "../../evo/StateGrid.h"
+
+int main()
+{
+  emp::evo::StateGridInfo info;
+  info.AddState(-1, '!', "Poison", "Damages any organism that moves to this position.");
+  info.AddState(0, '-', "Empty", "Uninteresting position");
+  info.AddState(1, '+', "Food", "Moving to this position increases fitness (at least once.");
+
+  emp::evo::StateGrid sg(info, "test.sgrid");
+  std::cout << "Created a " << sg.GetWidth() << "x" << sg.GetHeight()
+	    << " state grid." << std::endl;
+}
