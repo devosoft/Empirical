49ca4606ab69bf572413ef03342dea3922df385b
Charles Ofria <charles.ofria@gmail.com>
2017-05-21 15:55:24 -0400

    Started fleshing out EcoResource

--- evo/EcoResource.h
+++ evo/EcoResource.h
@@ -13,12 +13,28 @@ namespace emp {
 namespace evo {
 
   class EcoResource {
+  private:
     double inflow;   // Number of resources added per unit time.
     double outflow;  // Proportion of existing resource removed per unit time.
+
+    double level;
+
+  public:
+    EcoResource() : inflow(0.0), outflow(0.0), level(0.0) { ; }
+
+    double GetInflow() const { return inflow; }
+    double GetOutflow() const { return outflow; }
+    double GetLevel() const { return level; }
+
+    EcoResource & SetInflow(double _in) { inflow = _in; return *this; }
+    EcoResource & SetOutflow(double _in) { outflow = _in; return *this; }
+    EcoResource & SetLevel(double _in) { level = _in; return *this; }
+
+    void Update(double time) { ; }
+    void Inject(double amt) { level += amt; }
   };
 
 }
 }
 
 #endif
-
