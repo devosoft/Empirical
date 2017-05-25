fc2d29698af36a728275bf0426bb4a2b1cafa65f
Charles Ofria <charles.ofria@gmail.com>
2017-05-24 23:24:23 -0400

    Setup StateGrid example to provide score multiple info (and print a grid)

--- examples/evo/StateGrid.cc
+++ examples/evo/StateGrid.cc
@@ -10,11 +10,13 @@
 int main()
 {
   emp::evo::StateGridInfo info;
-  info.AddState(-1, '!', "Poison", "Damages any organism that moves to this position.");
-  info.AddState(0, '-', "Empty", "Uninteresting position");
-  info.AddState(1, '+', "Food", "Moving to this position increases fitness (at least once.");
+  info.AddState(-1, '!', 0.9, "Poison", "Damages any organism that moves to this position.");
+  info.AddState(0, '-', 1.0, "Empty", "Uninteresting position");
+  info.AddState(1, '+', 1.1, "Food", "Moving to this position increases fitness (at least once.");
 
   emp::evo::StateGrid sg(info, "test.sgrid");
   std::cout << "Created a " << sg.GetWidth() << "x" << sg.GetHeight()
 	    << " state grid." << std::endl;
+
+  sg.Write(std::cout);
 }
