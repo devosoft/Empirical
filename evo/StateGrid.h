f2c619c49fa6493afdcc54ce2bb86a909e77bfae
Charles Ofria <charles.ofria@gmail.com>
2017-05-24 23:22:41 -0400

    Added 'score_mult' to StateGrid to specify the effect of landing on a space; +more accessors.

--- evo/StateGrid.h
+++ evo/StateGrid.h
@@ -8,6 +8,11 @@
 //
 //  Developer Notes:
 //  * Functions such as Load() should throw exceptions (or equilv.), not use asserts.
+//  * Need to figure out a default mapping for how outputs translate to moves around a state grid.
+//    -1 = Back up
+//     0 = Turn left
+//     1 = Move fast-forwards
+//     2 = Turn right
 
 #ifndef EMP_EVO_STATE_GRID_H
 #define EMP_EVO_STATE_GRID_H
@@ -28,13 +33,15 @@ namespace evo {
 
     /// Information about what a particular state type means in a state grid.
     struct StateInfo {
-      int state_id;
-      char symbol;
-      std::string name;
-      std::string desc;
+      int state_id;       // Ordinal id for this state.
+      char symbol;        // Symbol for printing this state.
+      double score_mult;  // Change factor for organism score by stepping on this squre.
+      std::string name;   // Name of this state.
+      std::string desc;   // Explanation of this state.
 
-      StateInfo(int _id, char _sym, const std::string & _name, const std::string & _desc)
-      : state_id(_id), symbol(_sym), name(_name), desc(_desc) { ; }
+      StateInfo(int _id, char _sym, double _mult,
+                const std::string & _name, const std::string & _desc)
+      : state_id(_id), symbol(_sym), score_mult(_mult), name(_name), desc(_desc) { ; }
     };
 
     emp::vector<StateInfo> states;         // All available states.  Position is key ID
@@ -54,16 +61,17 @@ namespace evo {
 
     // Convert from state ids...
     char GetSymbol(int state_id) const { return states[ GetKey(state_id) ].symbol; }
-    std::string GetName(int state_id) const { return states[ GetKey(state_id) ].name; }
-    std::string GetDesc(int state_id) const { return states[ GetKey(state_id) ].desc; }
+    double GetScoreMult(int state_id) const { return states[ GetKey(state_id) ].score_mult; }
+    const std::string & GetName(int state_id) const { return states[ GetKey(state_id) ].name; }
+    const std::string & GetDesc(int state_id) const { return states[ GetKey(state_id) ].desc; }
 
     // Convert to state ids...
     int GetState(char symbol) const { return states[ GetKey(symbol) ].state_id; }
     int GetState(const std::string & name) const { return states[ GetKey(name) ].state_id; }
 
-    void AddState(int id, char symbol, std::string name="", std::string desc="") {
+    void AddState(int id, char symbol, double mult=1.0, std::string name="", std::string desc="") {
       size_t key_id = states.size();
-      states.emplace_back(id, symbol, name, desc);
+      states.emplace_back(id, symbol, mult, name, desc);
       state_map[id] = key_id;
       symbol_map[symbol] = key_id;
       name_map[name] = key_id;
@@ -94,9 +102,16 @@ namespace evo {
     size_t GetWidth() const { return width; }
     size_t GetHeight() const { return height; }
     size_t GetSize() const { return states.size(); }
+    const StateGridInfo & GetInfo() const { return info; }
 
     int & operator()(size_t x, size_t y) { return states[y*width+x]; }
     int operator()(size_t x, size_t y) const { return states[y*width+x]; }
+    int GetState(size_t x, size_t y) const { return states[y*width+x]; }
+    StateGrid & SetState(size_t x, size_t y, int in) { states[y*width+x] = in; return *this; }
+
+    char GetSymbol(size_t x, size_t y) const { return info.GetSymbol(GetState(x,y)); }
+    double GetScoreMult(size_t x, size_t y) const { return info.GetScoreMult(GetState(x,y)); }
+    const std::string & GetName(size_t x, size_t y) const { return info.GetName(GetState(x,y)); }
 
     template <typename... Ts>
     StateGrid & Load(Ts &&... args) {
