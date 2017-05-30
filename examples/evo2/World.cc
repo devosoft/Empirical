fe000f157ef67ac0d97ae3ea215d2212aeb9d819
Charles Ofria <charles.ofria@gmail.com>
2017-05-29 23:38:12 -0400

    Added example files for new evo2/WorldModule

---
+++ examples/evo2/World.cc
@@ -0,0 +1,14 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  This file is an example for using the re-vamped World template.
+
+#include <iostream>
+
+#include "../../evo2/World_Base.h"
+
+int main() {
+}
+
