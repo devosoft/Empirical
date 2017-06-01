a164f43366e2b84fbe9865be7e8024a03c7c6601
Charles Ofria <charles.ofria@gmail.com>
2017-06-01 12:39:02 -0400

    TypeTracker can now manage functions with ANY numeber of arguments, though for the moment they all have to be called with tracked types.

--- tools/TypeTracker.h
+++ tools/TypeTracker.h
@@ -22,7 +22,9 @@
 #include "../base/array.h"
 #include "../base/assert.h"
 #include "../meta/meta.h"
-#include "../tools/map_utils.h"
+
+#include "GenericFunction.h"
+#include "map_utils.h"
 
 namespace emp {
 
@@ -54,7 +56,6 @@ namespace emp {
     using this_t = TypeTracker<TYPES...>;
     template <typename REAL_T>
     using wrap_t = TypeTracker_Class< REAL_T, get_type_index<REAL_T,TYPES...>() >;
-    using fun2_t = std::function<void(TrackedType*, TrackedType*)>;
 
     // How many types are we working with?
     constexpr static size_t GetNumTypes() { return sizeof...(TYPES); }
@@ -114,9 +115,8 @@ namespace emp {
       return GetCumCombos(sizeof...(Ts)-1) + GetTrackedID(ARGS...);
     }
 
-    // The redirects points a function call to the appropriate function, giving the input types.
-    //emp::array< std::function<void(TrackedType*, TrackedType*)>, GetNumCombos(2) > redirects;
-    std::unordered_map<size_t, fun2_t> fun_map;
+    // fun_map is a hash table that maps a set of inputs to the appropriate function.
+    std::unordered_map<size_t, emp::GenericFunction *> fun_map;
 
     // Constructors!
     TypeTracker() { ; }
@@ -124,6 +124,11 @@ namespace emp {
     TypeTracker(TypeTracker &&) = default;
     TypeTracker & operator=(const TypeTracker &) = default;
     TypeTracker & operator=(TypeTracker &&) = default;
+
+    // Destructor!
+    ~TypeTracker() {
+      for (auto x : fun_map) delete x.second;  // Clear out Functions.
+    }
 
     template <typename REAL_T> wrap_t<REAL_T> Wrap(REAL_T && val) {
       emp_assert((has_type<REAL_T,TYPES...>()));    // Make sure we're wrapping a legal type.
@@ -161,27 +166,44 @@ namespace emp {
     template <typename OUT_T>
     OUT_T Cast( TrackedType * tt ) { return Cast(*tt); }
 
-    template <typename T1, typename T2>
-    this_t & AddFunction( std::function<void(T1,T2)> fun ) {
-      constexpr size_t POS = GetID<T1,T2>();
-      fun_map[POS] = [fun](TrackedType* b1, TrackedType* b2) {
-        emp_assert(dynamic_cast<wrap_t<T1> *>(b1) != nullptr);
-        emp_assert(dynamic_cast<wrap_t<T2> *>(b2) != nullptr);
-        fun( ((wrap_t<T1> *) b1)->value, ((wrap_t<T2> *) b2)->value );
+    template <typename... Ts>
+    this_t & AddFunction( std::function<void(Ts...)> fun ) {
+      constexpr size_t ID = GetComboID<Ts...>();
+
+      // We need to ensure there are the same number of TrackedType parameters in the wrapped
+      // function as there were typed parameters in the original.  To accomplish this task, we
+      // will expand the original type pack, but use decoys to convert to TrackedType.
+
+      auto fun_wrap = [fun](emp::type_decoy<TrackedType *,Ts>... args) {
+        // Ensure all types can be cast appropriately
+        emp_assert( AllTrue( dynamic_cast<wrap_t<Ts> *>(args)... ) );
+
+        // Now run the function with the correct type conversions
+        fun( ((wrap_t<Ts> *) args)->value... );
       };
+
+      fun_map[ID] = new Function<void(emp::type_decoy<TrackedType *,Ts>...)>(fun_wrap);
+
       return *this;
     }
 
-    template <typename T1, typename T2>
-    this_t & AddFunction( void (*fun)(T1,T2) ) {
-      return AddFunction( std::function<void(T1,T2)>(fun) );
+    template <typename... Ts>
+    this_t & AddFunction( void (*fun)(Ts...) ) {
+      return AddFunction( std::function<void(Ts...)>(fun) );
     }
 
-    void RunFunction( TrackedType * b1, TrackedType * b2 ) {
-      const size_t pos = GetTrackedID(b1, b2);
-      if (Has(fun_map, pos)) fun_map[pos](b1,b2);  // If a redirect exists, use it!
+    // void RunFunction( TrackedType * b1, TrackedType * b2 ) {
+    //   const size_t pos = GetTrackedID(b1, b2);
+    //   if (Has(fun_map, pos)) fun_map[pos](b1,b2);  // If a redirect exists, use it!
+    // }
+    template <typename... Ts>
+    void RunFunction( Ts... args ) {                 // args must all be TrackedType pointers!
+      const size_t pos = GetTrackedComboID(args...);
+      if (Has(fun_map, pos)) fun_map[pos]->Call(args...);  // If a redirect exists, use it!
     }
-    void operator()( TrackedType * b1, TrackedType * b2 ) { RunFunction(b1,b2); }
+
+    template <typename... Ts>
+    void operator()(Ts &&... args) { RunFunction(std::forward<Ts>(args)...); }
   };
 
 }
