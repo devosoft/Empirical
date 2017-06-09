31eb5d387a9ddc321a802d09c1596e957a680fbd
Charles Ofria <charles.ofria@gmail.com>
2017-06-08 23:28:44 -0400

    Setup proper handling of multiple WorldModules to build hierarchy.

--- evo2/World.h
+++ evo2/World.h
@@ -8,14 +8,25 @@
 #ifndef EMP_EVO_WORLD_H
 #define EMP_EVO_WORLD_H
 
+#include "../meta/IntPack.h"
+
 #include "WorldModule.h"
+#include "WorldModule_Select.h"
 
 namespace emp {
+
+  // An interface to setup the proper class heirarchy for World objects
+  template <typename ORG, typename MOD_PACK> class World_Interface;   // Generic interface stub
+
+  template <typename ORG, int... IMODS>
+  class World_Interface<ORG, IntPack<IMODS...>> : public WorldModule<ORG, (emp::evo) IMODS...> {
+    using parent_t = WorldModule<ORG, (emp::evo) IMODS...>;
+  };
 
   // The outer-most level of World objects...
 
   template <typename ORG, emp::evo... MODS>
-  class World : public WorldModule<ORG, MODS...> {
+  class World : public World_Interface<ORG, pack::RUsort<IntPack<(int) MODS...>>> {
   private:
     using parent_t = WorldModule<ORG, MODS...>;
     using typename parent_t::fit_fun_t;
