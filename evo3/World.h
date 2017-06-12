654aa32d1dca4fb92fb3a3e2ffcda08648f2f8ee
Charles Ofria <charles.ofria@gmail.com>
2017-06-11 23:11:10 -0400

    New attempt at a World object...

---
+++ evo3/World.h
@@ -0,0 +1,24 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  This file defines the base class for a World template for use in evolutionary algorithms.
+
+#ifndef EMP_EVO_WORLD_H
+#define EMP_EVO_WORLD_H
+
+namespace emp {
+
+  template <typename ORG, typename GENOTYPE=ORG>
+  class World {
+  private:
+    
+
+  public:
+    World() { ; }
+  };
+
+}
+
+#endif
