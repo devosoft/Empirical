606223dddba847094ae6c4e30df02515cc5da8e4
Charles Ofria <charles.ofria@gmail.com>
2017-06-21 14:38:04 -0400

    Updated new AvidaGP example file to work with CPU and InstSet changes.

--- examples/hardware2/AvidaGP.cc
+++ examples/hardware2/AvidaGP.cc
@@ -24,14 +24,14 @@ int main()
 
   emp::AvidaGP cpu;
   Print(cpu);
-  cpu.PushInst( emp::AvidaGP::InstID::Countdown, 8, 4 );
-  cpu.PushInst( emp::AvidaGP::InstID::Mult, 6, 2, 6 );
-  cpu.PushInst( emp::AvidaGP::InstID::Scope, 0 );
+  cpu.PushInst( "Countdown", 8, 4 );
+  cpu.PushInst( "Mult", 6, 2, 6 );
+  cpu.PushInst( "Scope", 0 );
 
-  cpu.PushInst( emp::AvidaGP::InstID::Inc, 1 );
-  cpu.PushInst( emp::AvidaGP::InstID::Inc, 1 );
-  cpu.PushInst( emp::AvidaGP::InstID::Mult, 1, 2, 3 );
-  cpu.PushInst( emp::AvidaGP::InstID::Add, 3, 4, 5 );
+  cpu.PushInst( "Inc", 1 );
+  cpu.PushInst( "Inc", 1 );
+  cpu.PushInst( "Mult", 1, 2, 3 );
+  cpu.PushInst( "Add", 3, 4, 5 );
 
   for (int i = 0; i < 43; i++) cpu.PushRandom(random);
 
@@ -45,12 +45,13 @@ int main()
   cpu.Trace(200);
 
   // Run a ton of organisms.
-  for (int t = 0; t < 100000; t++) {
-    //if (t % 10000 == 0) std::cout << t << std::endl;
+  for (int t = 0; t < 1000000; t++) {
+    if (t % 10000 == 0) std::cout << t << std::endl;
     cpu.Reset();
     for (int i = 0; i < 100; i++) cpu.PushRandom(random);
     cpu.Process(200);
   }
+  std::cout << "NEW AvidaGP" << std::endl;
 
   return 0;
 }
