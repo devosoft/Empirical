e3c7b37ddf063e87e1941d7c0f69f7969e89a7bf
Charles Ofria <charles.ofria@gmail.com>
2017-06-22 17:16:27 -0400

    Moved new AvidaGP hardware into place.

--- OLD/examples/hardware/AvidaGP.cc
+++ OLD/examples/hardware/AvidaGP.cc
@@ -4,8 +4,8 @@
 
 #include <iostream>
 
-#include "../../hardware2/AvidaGP.h"
-#include "../../hardware2/InstLib.h"
+#include "../../hardware/AvidaGP.h"
+#include "../../hardware/InstLib.h"
 #include "../../tools/Random.h"
 
 void Print(const emp::AvidaGP & cpu) {
@@ -24,14 +24,14 @@ int main()
 
   emp::AvidaGP cpu;
   Print(cpu);
-  cpu.PushInst( "Countdown", 8, 4 );
-  cpu.PushInst( "Mult", 6, 2, 6 );
-  cpu.PushInst( "Scope", 0 );
+  cpu.PushInst( emp::AvidaGP::InstID::Countdown, 8, 4 );
+  cpu.PushInst( emp::AvidaGP::InstID::Mult, 6, 2, 6 );
+  cpu.PushInst( emp::AvidaGP::InstID::Scope, 0 );
 
-  cpu.PushInst( "Inc", 1 );
-  cpu.PushInst( "Inc", 1 );
-  cpu.PushInst( "Mult", 1, 2, 3 );
-  cpu.PushInst( "Add", 3, 4, 5 );
+  cpu.PushInst( emp::AvidaGP::InstID::Inc, 1 );
+  cpu.PushInst( emp::AvidaGP::InstID::Inc, 1 );
+  cpu.PushInst( emp::AvidaGP::InstID::Mult, 1, 2, 3 );
+  cpu.PushInst( emp::AvidaGP::InstID::Add, 3, 4, 5 );
 
   for (int i = 0; i < 43; i++) cpu.PushRandom(random);
 
@@ -45,13 +45,12 @@ int main()
   cpu.Trace(200);
 
   // Run a ton of organisms.
-  for (int t = 0; t < 1000000; t++) {
-    if (t % 10000 == 0) std::cout << t << std::endl;
+  for (int t = 0; t < 100000; t++) {
+    //if (t % 10000 == 0) std::cout << t << std::endl;
     cpu.Reset();
     for (int i = 0; i < 100; i++) cpu.PushRandom(random);
     cpu.Process(200);
   }
-  std::cout << "NEW AvidaGP" << std::endl;
 
   return 0;
 }
--- hardware/InstLib.h
+++ hardware/InstLib.h
@@ -16,77 +16,99 @@
 
 namespace emp {
 
-  template <typename INST_TYPE>
+  // ScopeType is used for scopes that we need to do something special at the end.
+  // Eg: LOOP needs to go back to beginning of loop; FUNCTION needs to return to call.
+  enum class ScopeType { NONE=0, ROOT, BASIC, LOOP, FUNCTION };
+
+  template <typename HARDWARE_T, typename ARG_T=size_t, size_t ARG_COUNT=3>
   class InstLib {
-  protected:
-    using inst_t = INST_TYPE;
-    using inst_id_t = typename INST_TYPE::id_t;
-    using inst_arg_t = typename INST_TYPE::arg_t;
+  public:
+    using hardware_t = HARDWARE_T;
+    using inst_t = typename hardware_t::inst_t;
     using genome_t = emp::vector<inst_t>;
+    using arg_t = ARG_T;
+    using fun_t = std::function<void(hardware_t &, const emp::array<arg_t, ARG_COUNT> &)>;
 
     struct InstDef {
-      inst_id_t id;
-      std::string name;
-      std::string desc;
-      size_t num_args;
+      std::string name;       // Name of this instruction.
+      fun_t fun_call;         // Function to call when executing.
+      size_t num_args;        // Number of args needed by function.
+      std::string desc;       // Description of function.
+      ScopeType scope_type;   // How does this instruction affect scoping?
+      size_t scope_arg;       // Which arg indictes new scope (if any).
 
-      InstDef() : id(inst_id_t::Unknown), name(), desc(), num_args(0) { ; }
-      InstDef(inst_id_t _id, const std::string & _n, const std::string & _d, size_t _args)
-        : id(_id), name(_n), desc(_d), num_args(_args) { ; }
+      InstDef(const std::string & _n, fun_t _fun, size_t _args, const std::string & _d,
+              ScopeType _s_type, size_t _s_arg)
+        : name(_n), fun_call(_fun), num_args(_args), desc(_d)
+        , scope_type(_s_type), scope_arg(_s_arg) { ; }
       InstDef(const InstDef &) = default;
     };
 
-    emp::vector<InstDef> inst_lib;
-    std::map<std::string, size_t> name_map;
-    std::map<std::string, inst_arg_t> arg_map;
+  protected:
+    emp::vector<InstDef> inst_lib;            // Full definitions for instructions.
+    emp::vector<fun_t> inst_funs;             // Map of instruction IDs to their functions.
+    std::map<std::string, size_t> name_map;   // How do names link to instructions?
+    std::map<std::string, arg_t> arg_map;     // How are different arguments named?
 
   public:
-    InstLib() : inst_lib(), name_map(), arg_map() { ; }
+    InstLib() : inst_lib(), inst_funs(), name_map(), arg_map() { ; }
     ~InstLib() { ; }
 
-    const std::string & GetName(inst_id_t id) const { return inst_lib[(size_t) id].name; }
-    const std::string & GetDesc(inst_id_t id) const { return inst_lib[(size_t) id].desc; }
-    size_t GetNumArgs(inst_id_t id) const { return inst_lib[(size_t) id].num_args; }
-    static constexpr char GetSymbol(inst_id_t id) {
+    const std::string & GetName(size_t id) const { return inst_lib[id].name; }
+    const fun_t & GetFunction(size_t id) const { return inst_lib[id].fun_call; }
+    size_t GetNumArgs(size_t id) const { return inst_lib[id].num_args; }
+    const std::string & GetDesc(size_t id) const { return inst_lib[id].desc; }
+    ScopeType GetScopeType(size_t id) const { return inst_lib[id].scope_type; }
+    size_t GetScopeArg(size_t id) const { return inst_lib[id].scope_arg; }
+    size_t GetSize() const { return inst_lib.size(); }
+
+    static constexpr char GetSymbol(size_t id) {
       if (id < 26) return ('a' + id);
       if (id < 52) return ('A' + (id - 26));
       if (id < 62) return ('0' + (id - 52));
       return '+';
     }
-    inst_id_t GetID(const std::string name) const {
+    size_t GetID(const std::string & name) const {
       emp_assert(Has(name_map, name), name);
-      return Find(name_map, name, inst_id_t::Unknown);
+      return Find(name_map, name, (size_t) -1);
     }
-    static constexpr inst_id_t GetID(char symbol) {
-      if (symbol >= 'a' && symbol <= 'z') return (inst_id_t) (symbol - 'a');
-      if (symbol >= 'A' && symbol <= 'Z') return (inst_id_t) (symbol - 'A' + 26);
-      if (symbol >= '0' && symbol <= '9') return (inst_id_t) (symbol - '0' + 52);
-      return (inst_id_t) 62;
+    static constexpr size_t GetID(char symbol) {
+      if (symbol >= 'a' && symbol <= 'z') return (size_t) (symbol - 'a');
+      if (symbol >= 'A' && symbol <= 'Z') return (size_t) (symbol - 'A' + 26);
+      if (symbol >= '0' && symbol <= '9') return (size_t) (symbol - '0' + 52);
+      return (size_t) 62;
     }
 
-    inst_arg_t GetArg(const std::string & name) {
+    arg_t GetArg(const std::string & name) {
       emp_assert(Has(arg_map, name));
       return arg_map[name];
     }
 
-    void AddInst(inst_id_t inst_id, const std::string & name,
+    void AddInst(const std::string & name,
+                 const fun_t & fun_call,
                  size_t num_args=0,
-                 const std::string & desc="")
+                 const std::string & desc="",
+                 ScopeType scope_type=ScopeType::NONE,
+                 size_t scope_arg=(size_t) -1)
     {
-      const size_t id = (size_t) inst_id;
-      if (inst_lib.size() <= id) inst_lib.resize(id+1);
-      inst_lib[id] = InstDef(inst_id, name, desc, num_args);
+      const size_t id = inst_lib.size();
+      inst_lib.emplace_back(name, fun_call, num_args, desc, scope_type, scope_arg);
+      inst_funs.emplace_back(fun_call);
       name_map[name] = id;
     }
 
-    void AddArg(const std::string & name, inst_arg_t value) {
+    void AddArg(const std::string & name, arg_t value) {
       emp_assert(!Has(arg_map, name));
       arg_map[name] = value;
     }
 
+    void ProcessInst(hardware_t & hw, const inst_t & inst) const {
+      inst_funs[inst.id](hw, inst.args);
+    }
+
     void WriteGenome(const genome_t & genome, std::ostream & os=std::cout) const {
       for (const inst_t & inst : genome) {
-        os << ((int) inst.id) << " " << GetName(inst.id);
+        os << inst.id << " " << GetName(inst.id);
         const size_t num_args = GetNumArgs(inst.id);
         for (size_t i = 0; i < num_args; i++) {
           os << ' ' << inst.args[i];
@@ -97,12 +119,15 @@ namespace emp {
 
     void ReadInst(genome_t & genome, std::string info) const {
       std::string name = emp::string_pop_word(info);
-      inst_id_t id = GetID(name);
+      size_t id = GetID(name);
       genome.emplace_back(id);
       size_t num_args = GetNumArgs(id);
       for (size_t i = 0; i < num_args; i++) {
         std::string arg_name = emp::string_pop_word(info);
         // @CAO: Should check to make sure arg name is real.
+        if (emp::Has(arg_map, arg_name) == false) {
+          std::cerr << "Unknown argument '" << arg_name << "'.  Ignoring." << std::endl;
+        }
         genome.back().args[i] = arg_map[arg_name];
       }
     }
