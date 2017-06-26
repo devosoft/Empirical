063ff649faef011722cfd94ba095e8e4f189812e
Charles Ofria <charles.ofria@gmail.com>
2017-06-25 23:41:56 -0400

    Building a new LineagerTracker.

---
+++ evo3/LineageTracker.h
@@ -0,0 +1,24 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical/
+//  Copyright (C) Michigan State University, 2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Track the lineages of organisms in a world.
+//
+//
+//  Options include:
+//  * Prune evolutionary dead-ends (to reduce size of tree).
+//  * Backup to file (anything older than a spcified level)
+
+
+#ifndef EMP_LINEAGE_TRACKER_H
+#define EMP_LINEAGE_TRACKER_H
+
+namespace emp {
+
+  class LineageTracker {
+  };
+
+}
+
+#endif
