d4d4e3614fa805053b213f2c5385d45805861cb1
Charles Ofria <charles.ofria@gmail.com>
2017-06-25 23:55:30 -0400

    Starting to flesh out lineage tracker.

--- evo3/LineageTracker.h
+++ evo3/LineageTracker.h
@@ -14,9 +14,40 @@
 #ifndef EMP_LINEAGE_TRACKER_H
 #define EMP_LINEAGE_TRACKER_H
 
+#include <unordered_map>
+
+#include "../base/Ptr.h"
+
 namespace emp {
 
+  template <typename ORG>
   class LineageTracker {
+  private:
+    struct Node {
+      size_t id;
+      size_t parent_id;
+    };
+
+    unordered_map< size_t, Ptr<Node> > node_map;
+
+  public:
+    LineageTracker() : node_map() { ; }
+    ~LineageTracker() {
+      for (auto x : node_map) x.second.Delete();
+      node_map.clear();
+    }
+
+    /// Add information about a new organism; return id for this new entry.
+    size_t AddOrg(size_t parent_id, ORG & org) {
+    }
+
+    /// Remove an instance of an organism; track when it's gone.
+    size_t RemoveOrg(ORG & org) {
+    }
+
+    /// Climb up a lineage...
+    size_t ParentID(size_t org_id) {
+    }
   };
 
 }
