8139e13ddcb86ca5c5376c4dd9ecef43e14e08d7
Steven Jorgensen <spjorgensen94@gmail.com>
2017-06-27 15:25:31 -0400

    Organism can take in input

--- examples/hardware/OthelloAI.cc
+++ examples/hardware/OthelloAI.cc
@@ -4,7 +4,7 @@
 
 #include <iostream>
 
-#include "../../hardware/AvidaGP.h"
+#include "../../hardware/AvidaGPMem.h"
 #include "../../hardware/InstLib.h"
 #include "../../tools/Random.h"
 
@@ -23,34 +23,47 @@ int main()
   emp::Random random;
 
   emp::AvidaGP cpu;
-  Print(cpu);
-  cpu.PushInst( "Countdown", 8, 4 );
-  cpu.PushInst( "Mult", 6, 2, 6 );
-  cpu.PushInst( "Scope", 0 );
+
+  for (int i = 0; i < 64; i++) {
+    double input = 0;
+    if (i == 27 || i == 36) input = 1;
+    else if (i == 28 || i == 35) input = -1;
+    cpu.SetInput(i, input);
+  }
+
+  //Print(cpu);
+  cpu.PushInst("SetReg", 0, 64);
+  cpu.PushInst("SetReg", 2, 0);
+  cpu.PushInst("While", 1, 1 );
+  cpu.PushInst("Input", 2, 3);
+  cpu.PushInst("Inc", 2);
+  cpu.PushInst("TestNEqu", 2, 0, 1);
+  cpu.PushInst("Scope", 0);
+/*  cpu.PushInst( "Scope", 0 );
 
   cpu.PushInst( "Inc", 1 );
   cpu.PushInst( "Inc", 1 );
   cpu.PushInst( "Mult", 1, 2, 3 );
-  cpu.PushInst( "Add", 3, 4, 5 );
+  cpu.PushInst( "Add", 3, 4, 5 );*/
 
-  for (int i = 0; i < 43; i++) cpu.PushRandom(random);
+  //for (int i = 0; i < 43; i++) cpu.PushRandom(random);
 
-  for (size_t i = 0; i < 100; i++) {
+  /*for (size_t i = 0; i < 100; i++) {
     cpu.Process(1);
     Print(cpu);
-  }
+  }*/
 
   cpu.PrintGenome();
-  cpu.ResetHardware();
+//  cpu.ResetHardware();
   cpu.Trace(200);
 
   // Run a ton of organisms.
-  for (int t = 0; t < 1000000; t++) {
-    if (t % 10000 == 0) std::cout << t << std::endl;
+  /*for (int t = 0; t < 1000000; t++) {
+    //if (t % 10000 == 0) std::cout << t << std::endl;
     cpu.Reset();
     for (int i = 0; i < 100; i++) cpu.PushRandom(random);
     cpu.Process(200);
-  }
+  }*/
   std::cout << "NEW AvidaGP" << std::endl;
 
   return 0;
