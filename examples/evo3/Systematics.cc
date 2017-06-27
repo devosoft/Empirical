f73637fc0c16224e1b001b71914fa6447df908c4
Charles Ofria <charles.ofria@gmail.com>
2017-06-26 23:59:05 -0400

    Building an initial Systematics example file.

---
+++ examples/evo3/Systematics.cc
@@ -0,0 +1,17 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016-2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  This file explores the grid options for emp::World.h
+
+#include <iostream>
+
+#include "../../evo3/Systematics.h"
+
+int main()
+{
+  emp::Systematics<int> sys;
+
+  std::cout << "Test." <<  std::endl;
+}
