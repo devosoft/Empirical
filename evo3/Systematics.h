1f77d74eeea05a2af371076c2ad5ac53f6547b78
Charles Ofria <charles.ofria@gmail.com>
2017-06-27 23:01:18 -0400

    One more adjustment of Systematics terminology.

--- evo3/Systematics.h
+++ evo3/Systematics.h
@@ -31,9 +31,9 @@
 namespace emp {
 
   template <typename ORG_INFO>
-  class Taxon {
+  class TaxaGroup {
   private:
-    const ORG_INFO info;      // Details for the organims associated with this taxanomic group.
+    const ORG_INFO info;      // Details for the organims associated within this taxanomic group.
     const size_t id;          // Unique ID for this taxonomic group.
     const size_t parent_id;   // ID of parent group (MAX_ID if injected)
     size_t num_orgs;          // How many organisms currently exist of this group?
@@ -42,12 +42,12 @@ namespace emp {
     size_t tot_offspring;     // How many direct offspring have ever existed.
 
   public:
-    Taxon(const ORG_INFO & _info, size_t _id, size_t _pid=0)
+    TaxaGroup(const ORG_INFO & _info, size_t _id, size_t _pid=0)
     : info(_info), id(_id), parent_id(_pid), num_orgs(0), tot_orgs(0), num_offspring(0) { ; }
-    Taxon(const Taxon &) = delete;
-    Taxon(Taxon &&) = default;
-    Taxon & operator=(const Taxon &) = delete;
-    Taxon & operator=(Taxon &&) = default;
+    TaxaGroup(const TaxaGroup &) = delete;
+    TaxaGroup(TaxaGroup &&) = default;
+    TaxaGroup & operator=(const TaxaGroup &) = delete;
+    TaxaGroup & operator=(TaxaGroup &&) = default;
 
     const ORG_INFO & GetInfo() const { return info; }
     size_t GetID() const { return id; }
@@ -65,14 +65,14 @@ namespace emp {
       emp_assert(num_orgs > 0, id, num_orgs);
       --num_orgs;
 
-      // If we are out of organisms and offspring, this Taxon should deactivate.
+      // If we are out of organisms and offspring, this TaxaGroup should deactivate.
       if (num_orgs == 0) return num_offspring;
     }
     bool RemoveOffspring() {
       emp_assert(num_offspring > 0, id);
       --num_offspring;
 
-      // If we are out of offspring and organisms, this Taxon should deactivate.
+      // If we are out of offspring and organisms, this TaxaGroup should deactivate.
       if (num_offspring == 0) return num_orgs;
     }
   };
@@ -80,66 +80,66 @@ namespace emp {
   template <typename ORG_INFO>
   class Systematics {
   private:
-    using taxon_t = Taxon<ORG_INFO>;
+    using taxon_t = TaxaGroup<ORG_INFO>;
 
-    std::unordered_map< size_t, Ptr<taxon_t> > taxon_map;
+    std::unordered_map< size_t, Ptr<taxon_t> > taxa_map;
     size_t next_taxon_id;
     constexpr static size_t null_id = 0;
 
-    size_t NextTaxonID() { return ++next_taxon_id; }  // Return a unique Taxon ID.
+    size_t NextTaxaGroupID() { return ++next_taxon_id; }  // Return a unique TaxaGroup ID.
 
     void Deactivate(Ptr<taxon_t> taxon) {
-      taxon_map.remove(taxon->id);   // Remove taxon from map since it's no longer active.
-      Ptr<taxon_t> p_taxon = taxon_map[taxon->parent_id];
+      taxa_map.remove(taxon->id);   // Remove taxon from map since it's no longer active.
+      Ptr<taxon_t> p_taxon = taxa_map[taxon->parent_id];
       bool p_active = p_taxon->RemoveOffspring();   // Cascade up
       if (p_active == false) Deactivate(p_taxon);
       taxon->Delete();                              // Delete this taxon.
     }
 
   public:
-    Systematics() : taxon_map(), next_taxon_id(0) { ; }
+    Systematics() : taxa_map(), next_taxon_id(0) { ; }
     ~Systematics() {
-      for (auto x : taxon_map) x.second.Delete();
-      taxon_map.clear();
+      for (auto x : taxa_map) x.second.Delete();
+      taxa_map.clear();
     }
 
-    const taxon_t & GetTaxon(size_t id) const { return *taxon_map[id]; }
+    const taxon_t & GetTaxaGroup(size_t id) const { return *taxa_map[id]; }
 
     /// Add information about a newly-injected taxon; return unique taxon id.
     size_t InjectOrg(const ORG_INFO & info) {
-      const size_t id = NextTaxonID();
-      taxon_map[id] = NewPtr<taxon_t>(info, id);
-      taxon_map[id]->AddOrg();
+      const size_t id = NextTaxaGroupID();
+      taxa_map[id] = NewPtr<taxon_t>(info, id);
+      taxa_map[id]->AddOrg();
       return id;
     }
 
     /// Add information about a new organism; return a unique id for the associated taxon.
     size_t AddOrg(size_t parent_id, const ORG_INFO & info) {
-      emp_assert(Has(taxon_map, parent_id), parent_id);
-      Ptr<taxon_t> p_taxon = taxon_map[parent_id];
+      emp_assert(Has(taxa_map, parent_id), parent_id);
+      Ptr<taxon_t> p_taxon = taxa_map[parent_id];
       if (p_taxon->GetInfo() == info) {   // Adding another org of this taxon.
         p_taxon->AddOrg();
         return parent_id;
       }
       // This is a new taxon.
-      const size_t id = NextTaxonID();
-      taxon_map[id] = NewPtr<taxon_t>(info, id, parent_id);
-      taxon_map[id]->AddOrg();
+      const size_t id = NextTaxaGroupID();
+      taxa_map[id] = NewPtr<taxon_t>(info, id, parent_id);
+      taxa_map[id]->AddOrg();
       return id;
     }
 
     /// Remove an instance of an organism; track when it's gone.
     bool RemoveOrg(size_t id) {
-      emp_assert(Has(taxon_map, id), id);
-      Ptr<taxon_t> taxon = taxon_map[id];
+      emp_assert(Has(taxa_map, id), id);
+      Ptr<taxon_t> taxon = taxa_map[id];
       const bool active = taxon->RemoveOrg();
       if (active == false) Deactivate(taxon);
     }
 
     /// Climb up a lineage...
     bool ParentID(size_t id) const {
-      emp_assert(Has(taxon_map, id), id);
-      return *(taxon_map.find(id))->GetParentID();
+      emp_assert(Has(taxa_map, id), id);
+      return *(taxa_map.find(id))->GetParentID();
     }
   };
 
