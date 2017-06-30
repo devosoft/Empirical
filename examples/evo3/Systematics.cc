850225461fc85a5353ebca536a1bc00f2f6a3c52
Charles Ofria <charles.ofria@gmail.com>
2017-06-29 21:54:21 -0400

    Setup Systematics example to handle Taxon poitnters.

--- examples/evo3/Systematics.cc
+++ examples/evo3/Systematics.cc
@@ -13,10 +13,10 @@ int main()
 {
   emp::Systematics<int> sys;
 
-  size_t id1 = sys.InjectOrg(25);
-  size_t id2 = sys.AddOrg(id1, -10);
-  size_t id3 = sys.AddOrg(id1, 25);
-  size_t id4 = sys.AddOrg(id2, 25);
+  auto id1 = sys.InjectOrg(25);
+  auto id2 = sys.AddOrg(id1, -10);
+  auto id3 = sys.AddOrg(id1, 25);
+  auto id4 = sys.AddOrg(id2, 25);
 
   std::cout << "id1 = " << id1 << std::endl;
   std::cout << "id2 = " << id2 << std::endl;
