4f9c7c0896dcba5230a00af7bf592836af4ea368
Charles Ofria <charles.ofria@gmail.com>
2017-07-03 22:39:00 -0400

    added more removals in Systematics example.

--- examples/evo3/Systematics.cc
+++ examples/evo3/Systematics.cc
@@ -11,13 +11,20 @@
 
 int main()
 {
-  emp::Systematics<int> sys;
+  emp::Systematics<int> sys(true, true, true);
 
+  std::cout << "\nInject 25 (id1)\n";
   auto id1 = sys.InjectOrg(25);
+  std::cout << "\nAddOrg -10 (id2; parent id1)\n";
   auto id2 = sys.AddOrg(id1, -10);
+  std::cout << "\nAddOrg 25 (id3; parent id1)\n";
   auto id3 = sys.AddOrg(id1, 25);
+  std::cout << "\nAddOrg 25 (id4; parent id2)\n";
   auto id4 = sys.AddOrg(id2, 25);
+  std::cout << "\nRemoveOrg (id2)\n";
   sys.RemoveOrg(id2);
+  std::cout << "\nRemoveOrg (id4)\n";
+  sys.RemoveOrg(id4);
 
   std::cout << "id1 = " << id1 << std::endl;
   std::cout << "id2 = " << id2 << std::endl;
@@ -26,4 +33,5 @@ int main()
 
   std::cout << "\nLineage:\n";
   sys.PrintLineage(id4);
+  sys.PrintStatus();
 }
