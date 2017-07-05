da110d3fe4e2f1af405923fb96890d5b80594b72
Charles Ofria <charles.ofria@gmail.com>
2017-07-05 18:12:17 -0400

    Setup Systematics example to use new version of AddOrg

--- examples/evo3/Systematics.cc
+++ examples/evo3/Systematics.cc
@@ -13,14 +13,14 @@ int main()
 {
   emp::Systematics<int> sys(true, true, true);
 
-  std::cout << "\nInject 25 (id1)\n";
-  auto id1 = sys.InjectOrg(25);
+  std::cout << "\nAddOrg 25 (id1, no parent)\n";
+  auto id1 = sys.AddOrg(25);
   std::cout << "\nAddOrg -10 (id2; parent id1)\n";
-  auto id2 = sys.AddOrg(id1, -10);
+  auto id2 = sys.AddOrg(-10, id1);
   std::cout << "\nAddOrg 25 (id3; parent id1)\n";
-  auto id3 = sys.AddOrg(id1, 25);
+  auto id3 = sys.AddOrg(25, id1);
   std::cout << "\nAddOrg 25 (id4; parent id2)\n";
-  auto id4 = sys.AddOrg(id2, 25);
+  auto id4 = sys.AddOrg(25, id2);
   std::cout << "\nRemoveOrg (id2)\n";
   sys.RemoveOrg(id2);
   std::cout << "\nRemoveOrg (id4)\n";
