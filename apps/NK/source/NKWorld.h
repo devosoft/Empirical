0c75fcdb27a7f5f47f6cf1f198f66ae8686ba25c
Charles Ofria <charles.ofria@gmail.com>
2017-08-02 22:56:51 -0400

    Added an inial NKWorld.h file to merge web and native functionality.

---
+++ apps/NK/source/NKWorld.h
@@ -0,0 +1,9 @@
+#include "config/ArgManager.h"
+#include "evo/NK.h"
+#include "evo3/World.h"
+#include "tools/BitVector.h"
+#include "tools/Random.h"
+
+class NKWorld {
+  emp::World<BitOrg> world;
+};
