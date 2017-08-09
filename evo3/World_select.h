c011ca0be5bbd95959ef9f441adcdb20e67a910f
Charles Ofria <charles.ofria@gmail.com>
2017-08-09 15:09:43 -0400

    Added TournamentSelect to World_select.h

--- evo3/World_select.h
+++ evo3/World_select.h
@@ -16,8 +16,8 @@ namespace emp {
 
   template<typename ORG> class World;
 
-  // Elite Selection picks a set of the most fit individuals from the population to move to
-  // the next generation.  Find top e_count individuals and make copy_count copies of each.
+  /// ==ELITE== Selection picks a set of the most fit individuals from the population to move to
+  /// the next generation.  Find top e_count individuals and make copy_count copies of each.
   template<typename ORG>
   void EliteSelect(World<ORG> & world, size_t e_count, size_t copy_count) {
     emp_assert(e_count > 0 && e_count <= world.GetNumOrgs(), e_count);
@@ -38,6 +38,39 @@ namespace emp {
       const size_t repro_id = m->second;
       world.DoBirth( world[repro_id], repro_id, copy_count);
       ++m;
+    }
+  }
+
+
+  /// ==TOURNAMENT== Selection creates a tournament with a random sub-set of organisms,
+  /// finds the one with the highest fitness, and moves it to the next generation.
+  /// User provides the fitness function, the tournament size, and (optionally) the
+  /// number of tournaments to run.
+  template<typename ORG>
+  void TournamentSelect(World<ORG> & world, size_t t_size, size_t tourny_count) {
+    emp_assert(t_size > 0 && t_size <= world.GetNumOrgs(), t_size, world.GetNumOrgs());
+    emp_assert(tourny_count > 0);
+
+    emp::vector<size_t> entries;
+    for (size_t T = 0; T < tourny_count; T++) {
+      entries.resize(0);
+      // Choose organisms for this tournament (with replacement!)
+      for (size_t i=0; i < t_size; i++) entries.push_back( world.GetRandomOrgID() );
+
+      double best_fit = world.CalcFitnessID(entries[0]);
+      size_t best_id = entries[0];
+
+      // Search for a higher fit org in the tournament.
+      for (size_t i = 1; i < t_size; i++) {
+        const double cur_fit = world.CalcFitnessID(entries[i]);
+        if (cur_fit > best_fit) {
+          best_fit = cur_fit;
+          best_id = entries[i];
+        }
+      }
+
+      // Place the highest fitness into the next generation!
+      world.DoBirth( world[best_id], best_id, 1 );
     }
   }
 
