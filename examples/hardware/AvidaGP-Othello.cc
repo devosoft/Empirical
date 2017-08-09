ce434ea95060028b65f04760bd82d9335ccc92ab
Steven Jorgensen <spjorgensen94@gmail.com>
2017-08-09 13:24:05 -0400

    changed to use memo functions. Also added seed arg

--- examples/hardware/AvidaGP-Othello.cc
+++ examples/hardware/AvidaGP-Othello.cc
@@ -8,17 +8,17 @@
 #include "../../hardware/AvidaGPOthello.h"
 #include "../../hardware/InstLib.h"
 #include "../../tools/Random.h"
+#include "../../tools/memo_function.h"
 #include "../../evo/World.h"
 #include "../../../eco-ea-mancala/source/TestcaseSet.h"
 #include <stdlib.h>
 #include <utility>
 
-constexpr size_t POP_SIZE = 1000;
+constexpr size_t POP_SIZE = 200;
 constexpr size_t GENOME_SIZE = 100;
 constexpr size_t EVAL_TIME = 3500;
-constexpr size_t UPDATES = 250;
+constexpr size_t UPDATES = 1000;
 constexpr size_t TOURNY_SIZE = 4;
-constexpr bool CACHE = 1;
 
 // Determine the next move of a human player.
 size_t EvalMove(emp::Othello & game, std::ostream & os=std::cout, std::istream & is=std::cin) {
@@ -112,7 +112,7 @@ using othello_ai_t = std::function< size_t(emp::Othello & game) >;
 // Setup the fitness function for a whole game.
 double EvalGame(othello_ai_t & player0, othello_ai_t & player1,
                 bool cur_player=0, bool verbose=false) { 
-  emp::Othello game(cur_player==0);
+  emp::Othello game(cur_player==0); // Check to see if Black's turn
   int boardSize = game.GetBoardSize();
   size_t round = 0, errors = 0; 
   while (game.IsDone() == false) {
@@ -186,9 +186,14 @@ double EvalGame(emp::AvidaGP & org, bool cur_player=0) {
 using input_t = emp::array<int, 64>;
 using output_t = std::set<int>;
 
-int main()
+int main(int argc, char* argv[])
 {
+  size_t seed = 0;
+  if (argc == 2) seed = std::atoi( argv[1] );
+
   emp::Random random;
+  random.ResetSeed(seed);
+  std::cout<<"SEED: "<<random.GetSeed()<<std::endl;
   //emp::evo::EAWorld<emp::AvidaGP, emp::evo::FitCacheOn> world(random, "AvidaWorld"); // FitCache on
   emp::evo::EAWorld<emp::AvidaGP> world(random, "AvidaWorld"); //FitCache off
   auto testcases = TestcaseSet<64>("../games/data/game_0.csv", &random);
@@ -287,11 +292,16 @@ int main()
     } );
 
   // Setup the fitness function.
-  std::function<double(emp::AvidaGP*)> fit_fun =
+  emp::memo_function<double(emp::AvidaGP*)> fit_fun =
     [&random, &world](emp::AvidaGP * org) {
       emp::AvidaGP & rand_org = world.GetRandomOrg();
       bool cur_player = random.P(0.5);
-      return EvalGame(*org, rand_org, cur_player);
+      double best = EvalGame(*org, rand_org, cur_player);
+      for (int i = 0; i < 4; i++){
+        double temp = EvalGame(*org, rand_org, cur_player);
+        if (temp > best) best = temp;
+      }
+      return best;
     };
 
   emp::vector< std::function<double(emp::AvidaGP*)> > fit_set(testcases.GetNFuncs());
@@ -330,9 +340,9 @@ int main()
     world.EliteSelect(fit_fun, 1, 1);
     // Run a tournament for each spot.
     //world.TournamentSelect(fit_fun, TOURNY_SIZE, POP_SIZE-1);
-    fit_set.push_back(fit_fun);
-    world.LexicaseSelect(fit_set, POP_SIZE-1);
-    // world.EcoSelect(fit_fun, fit_set, 100, TOURNY_SIZE, POP_SIZE-1);
+    //fit_set.push_back(fit_fun);
+    //world.LexicaseSelect(fit_set, POP_SIZE-1);
+    world.EcoSelect(fit_fun, fit_set, 100, TOURNY_SIZE, POP_SIZE-1);
     //world.EcoSelectGradation(fit_fun, fit_set, 100, TOURNY_SIZE, POP_SIZE-1);
     world.Update();
     std::cout << (ud+1) << " : " << 0 << " : " << fit_fun(&(world[0])) << std::endl;
