ec94a8dfb1af69aaa436df202137b6f10ea3414a
Charles Ofria <charles.ofria@gmail.com>
2017-08-27 21:24:32 -0400

    Updated header and added numerous Doxygen comments to InstLib

--- source/hardware/InstLib.h
+++ source/hardware/InstLib.h
@@ -1,9 +1,11 @@
-//  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2017.
-//  Released under the MIT Software license; see doc/LICENSE
-//
-//
-//  This file maintains information about instructions availabel in virtual hardware.
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2017
+ *
+ *  @file  Systematics.h
+ *  @brief This file maintains information about instructions availabel in virtual hardware.
+ */
 
 #ifndef EMP_INST_LIB_H
 #define EMP_INST_LIB_H
@@ -11,15 +13,21 @@
 #include <map>
 #include <string>
 
+#include "../base/array.h"
 #include "../base/vector.h"
 #include "../tools/map_utils.h"
+#include "../tools/string_utils.h"
 
 namespace emp {
 
-  // ScopeType is used for scopes that we need to do something special at the end.
-  // Eg: LOOP needs to go back to beginning of loop; FUNCTION needs to return to call.
+  /// ScopeType is used for scopes that we need to do something special at the end.
+  /// Eg: LOOP needs to go back to beginning of loop; FUNCTION needs to return to call.
   enum class ScopeType { NONE=0, ROOT, BASIC, LOOP, FUNCTION };
 
+  /// @brief InstLib maintains a set of instructions for use in virtual hardware.
+  /// @param HARDWARE_T Type of the virtual hardware class to track instructions.
+  /// @param ARG_T What types of arguments are associated with instructions.
+  /// @param ARG_COUNT Max number of arguments on an instruction.
   template <typename HARDWARE_T, typename ARG_T=size_t, size_t ARG_COUNT=3>
   class InstLib {
   public:
@@ -30,12 +38,12 @@ namespace emp {
     using fun_t = std::function<void(hardware_t &, const emp::array<arg_t, ARG_COUNT> &)>;
 
     struct InstDef {
-      std::string name;       // Name of this instruction.
-      fun_t fun_call;         // Function to call when executing.
-      size_t num_args;        // Number of args needed by function.
-      std::string desc;       // Description of function.
-      ScopeType scope_type;   // How does this instruction affect scoping?
-      size_t scope_arg;       // Which arg indictes new scope (if any).
+      std::string name;       ///< Name of this instruction.
+      fun_t fun_call;         ///< Function to call when executing.
+      size_t num_args;        ///< Number of args needed by function.
+      std::string desc;       ///< Description of function.
+      ScopeType scope_type;   ///< How does this instruction affect scoping?
+      size_t scope_arg;       ///< Which arg indictes new scope (if any).
 
       InstDef(const std::string & _n, fun_t _fun, size_t _args, const std::string & _d,
               ScopeType _s_type, size_t _s_arg)
@@ -45,33 +53,51 @@ namespace emp {
     };
 
   protected:
-    emp::vector<InstDef> inst_lib;            // Full definitions for instructions.
-    emp::vector<fun_t> inst_funs;             // Map of instruction IDs to their functions.
-    std::map<std::string, size_t> name_map;   // How do names link to instructions?
-    std::map<std::string, arg_t> arg_map;     // How are different arguments named?
+    emp::vector<InstDef> inst_lib;           ///< Full definitions for instructions.
+    emp::vector<fun_t> inst_funs;            ///< Map of instruction IDs to their functions.
+    std::map<std::string, size_t> name_map;  ///< How do names link to instructions?
+    std::map<std::string, arg_t> arg_map;    ///< How are different arguments named?
 
   public:
     InstLib() : inst_lib(), inst_funs(), name_map(), arg_map() { ; }
     ~InstLib() { ; }
 
+    /// Return the name associated with the specified instruction ID.
     const std::string & GetName(size_t id) const { return inst_lib[id].name; }
+
+    /// Return the function associated with the specified instruction ID.
     const fun_t & GetFunction(size_t id) const { return inst_lib[id].fun_call; }
+
+    /// Return the number of arguments expected for the specified instruction ID.
     size_t GetNumArgs(size_t id) const { return inst_lib[id].num_args; }
+
+    /// Return the provided description for the provided instruction ID.
     const std::string & GetDesc(size_t id) const { return inst_lib[id].desc; }
+
+    /// What type of scope does this instruction state?  ScopeType::NONE is default.
     ScopeType GetScopeType(size_t id) const { return inst_lib[id].scope_type; }
+
+    /// If this instruction alters scope, identify which argument does so.
     size_t GetScopeArg(size_t id) const { return inst_lib[id].scope_arg; }
+
+    /// Get the number of instructions in this set.
     size_t GetSize() const { return inst_lib.size(); }
 
+    /// Retrieve a unique letter associared with the specified instruction ID.
     static constexpr char GetSymbol(size_t id) {
       if (id < 26) return ('a' + id);
       if (id < 52) return ('A' + (id - 26));
       if (id < 62) return ('0' + (id - 52));
       return '+';
     }
+
+    /// Return the ID of the instruction that has the specified name.
     size_t GetID(const std::string & name) const {
       emp_assert(Has(name_map, name), name);
       return Find(name_map, name, (size_t) -1);
     }
+
+    /// Return the ID of the instruction associated with the specified symbol.
     static constexpr size_t GetID(char symbol) {
       if (symbol >= 'a' && symbol <= 'z') return (size_t) (symbol - 'a');
       if (symbol >= 'A' && symbol <= 'Z') return (size_t) (symbol - 'A' + 26);
@@ -79,11 +105,19 @@ namespace emp {
       return (size_t) 62;
     }
 
+    /// Return the argument value associated with the provided keyword.
     arg_t GetArg(const std::string & name) {
       emp_assert(Has(arg_map, name));
       return arg_map[name];
     }
 
+    /// @brief Add a new instruction to the set.
+    /// @param name A unique string name for this instruction.
+    /// @param fun_call The function that should be called when this instruction is executed.
+    /// @param num_args How many arguments does this function require? (default=0)
+    /// @param desc A description of how this function operates. (default="")
+    /// @param scope_type Type of scope does this instruction creates. (default=ScopeType::NONE)
+    /// @param scope_arg If instruction changes scope, which argument specified new scope? (defualt=-1)
     void AddInst(const std::string & name,
                  const fun_t & fun_call,
                  size_t num_args=0,
