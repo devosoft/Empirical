ee1e655432b4e4fd378f4a8e5f81ceffadbfc81b
Charles Ofria <charles.ofria@gmail.com>
2017-08-28 23:11:04 -0400

    Setup Avida organisms to track energy availability.

--- apps/Avida/source/AvidaOrg.h
+++ apps/Avida/source/AvidaOrg.h
@@ -1,6 +1,21 @@
-#include "hardware/AvidaGP.h"
+/**
+ *  @note This file is part of The Avida Digital Evolution Platform
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2017
+ *
+ *  @file  AvidaOrg.h
+ *  @brief This is the default, Avida-specific organism.
+ */
+
+ #include "hardware/AvidaGP.h"
 
 class AvidaOrg : public emp::AvidaGP {
 private:
+  double energy;
 public:
+  AvidaOrg() : energy(0) { ; }
+
+  double GetEnergy() const { return energy; }
+
+  void AdjustEnergy(double shift) { energy += shift; }
 };
