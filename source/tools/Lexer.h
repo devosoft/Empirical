b9d919e7634ff8f9f9aefd06b47970a4f56655cc
Charles Ofria <charles.ofria@gmail.com>
2017-09-29 09:32:18 -0400

    Updated documentation for Lexer.h

--- source/tools/Lexer.h
+++ source/tools/Lexer.h
@@ -1,10 +1,12 @@
-//  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2016-2017.
-//  Released under the MIT Software license; see doc/LICENSE
-//
-//
-//  A general-purpose, fast lexer.
-//  Status: BETA
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2016-2017
+ *
+ *  @file  Lexer.h
+ *  @brief A general-purpose, fast lexer.
+ *  @note Status: BETA
+ */
 
 #ifndef EMP_LEXER_H
 #define EMP_LEXER_H
@@ -19,11 +21,12 @@
 
 namespace emp {
 
+  /// Information about an individual token type to be processed within a Lexer.
   struct TokenInfo {
-    std::string name;    // Name of this token type.
-    RegEx regex;         // Pattern to describe token type.
-    size_t id;              // Unique id for token.
-    bool save_lexeme;    // Should we preserve the lexeme for this token?
+    std::string name;    ///< Name of this token type.
+    RegEx regex;         ///< Pattern to describe token type.
+    size_t id;           ///< Unique id for token.
+    bool save_lexeme;    ///< Should we preserve the lexeme for this token?
 
     // TokenInfo() : name(""), regex(""), id(-1), save_lexeme(false) { ; }
     TokenInfo(const std::string & n, const std::string & r, size_t i, bool s=false)
@@ -31,6 +34,7 @@ namespace emp {
     TokenInfo(const TokenInfo &) = default;
     TokenInfo & operator=(const TokenInfo &) = default;
 
+    /// Print out the status of this token (for debugging)
     void Print(std::ostream & os=std::cout) const {
       os << "Name:" << name
          << "  RegEx:" << regex.AsString()
@@ -40,25 +44,30 @@ namespace emp {
     }
   };
 
+  /// Information about a token instance from an input stream.
   struct Token {
-    size_t token_id;
-    std::string lexeme;
+    size_t token_id;     ///< Which type of token is this?
+    std::string lexeme;  ///< The specific sequence matched by this token.
 
     Token(size_t id, const std::string & str="") : token_id(id), lexeme(str) { ; }
     Token(const Token &) = default;
     Token & operator=(const Token &) = default;
 
+    /// Token will automatically convert to its ID if used as an unsigned int.
     operator size_t() { return token_id; }
+
+    /// Token will automatically convert to its matched sequence (lexeme) is used as a string.
     operator const std::string &() { return lexeme; }
   };
 
+  /// A lexer with a set of token types (and associated regular expressions)
   class Lexer {
   private:
-    emp::vector<TokenInfo> token_set;     // List of all active tokens.
-    size_t cur_token_id;                  // Which ID should the next new token get?
-    mutable bool generate_lexer;          // Do we need to regenerate the lexer?
-    mutable DFA lexer_dfa;                // Table driven lexer implementation.
-    std::string lexeme;                   // Current state of lexeme being generated.
+    emp::vector<TokenInfo> token_set;     ///< List of all active tokens.
+    size_t cur_token_id;                  ///< Which ID should the next new token get?
+    mutable bool generate_lexer;          ///< Do we need to regenerate the lexer?
+    mutable DFA lexer_dfa;                ///< Table driven lexer implementation.
+    std::string lexeme;                   ///< Current state of lexeme being generated.
 
   public:
     static const size_t MAX_TOKEN_ID = 256;      // How many token IDs are possible?
@@ -69,8 +78,10 @@ namespace emp {
     : token_set(), cur_token_id(MAX_TOKEN_ID), generate_lexer(false), lexer_dfa(), lexeme() { }
     ~Lexer() { ; }
 
+    /// How many types of tokens can be identified in this Lexer?
     size_t GetNumTokens() const { return token_set.size(); }
 
+    /// Add a new token, specified by a name and the regex used to identify it.
     size_t AddToken(const std::string & in_name, const std::string & in_regex) {
       --cur_token_id;
       generate_lexer = true;
@@ -78,13 +89,18 @@ namespace emp {
       return cur_token_id;
     }
 
+    /// How many total token types are allowed in this lexer?
     constexpr static size_t MaxTokenID() { return MAX_TOKEN_ID; }
+
+    /// Get the ID associated with a token type (you provide the token name)
     size_t GetTokenID(const std::string & name) const {
       for (const auto & t : token_set) {
         if (t.name == name) return t.id;
       }
       return ERROR_ID;
     }
+
+    /// Get the name associated with a token type (you provide the ID)
     std::string GetTokenName(size_t id) const {
       if (id >= MAX_TOKEN_ID) return "Error";
       if (id == 0) return "EOF";
@@ -94,6 +110,8 @@ namespace emp {
       }
       return "Unknown";
     }
+
+    /// Get the full information about a token (you provide the name)
     TokenInfo GetTokenInfo(const std::string & name) const {
       for (const auto & t : token_set) {
         if (t.name == name) return t;
@@ -101,6 +119,7 @@ namespace emp {
       return TokenInfo("", "", ERROR_ID);
     }
 
+    /// Create the NFA that will identify the current set of tokens in a sequence.
     void Generate() const {
       NFA lexer_nfa;
       for (const auto & t : token_set) {
@@ -110,7 +129,7 @@ namespace emp {
       lexer_dfa = to_DFA(lexer_nfa);
     }
 
-    // Get the next token found in an input stream.
+    /// Get the next token found in an input stream.
     Token Process(std::istream & is) {
       if (generate_lexer) Generate();
       size_t cur_pos = 0;
@@ -148,7 +167,7 @@ namespace emp {
       return { (size_t) best_stop, lexeme };
     }
 
-    // Shortcut to process a string rather than a stream.
+    /// Shortcut to process a string rather than a stream.
     Token Process(std::string & in_str) {
       std::stringstream ss;
       ss << in_str;
@@ -157,13 +176,14 @@ namespace emp {
       return out_val;
     }
 
-    // Get the lexeme associated with the last token.
+    /// Get the lexeme associated with the last token identified.
     const std::string & GetLexeme() { return lexeme; }
 
+    /// Print the full information about this lexer (for debugging)
     void Print(std::ostream & os=std::cout) const {
       for (const auto & t : token_set) t.Print(os);
-      if (generate_lexer) Generate();                   // Do we need to regenerate the lexer?
-      lexer_dfa.Print(os);                         // Table driven lexer implementation.
+      if (generate_lexer) Generate();               // Do we need to regenerate the lexer?
+      lexer_dfa.Print(os);                          // Table driven lexer implementation.
     }
   };
 
