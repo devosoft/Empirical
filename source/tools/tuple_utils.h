a6222905a60809612d040a758d868a5a8414e1d9
Charles Ofria <charles.ofria@gmail.com>
2017-10-12 14:02:37 -0400

    Updated documentation for tuple_utils.h (for real this time)

--- source/tools/tuple_utils.h
+++ source/tools/tuple_utils.h
@@ -3,8 +3,8 @@
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
  *  @date 2016-2017
  *
- *  @file SolveState.h
- *  @brief Simple functions to manipulate strings.
+ *  @file tuple_utils.h
+ *  @brief Functions to simplify the use of std::tuple
  *  @note Status: RELEASE
  */
 
@@ -19,24 +19,26 @@
 
 namespace emp {
 
-  // Quick way to calculate tuple size.
+  /// Quick way to calculate tuple size.
   template <typename TUPLE_T>
   constexpr inline int tuple_size() { return std::tuple_size<TUPLE_T>::value; }
 
 
-  // Reorganize the entries in tuple.
+  /// Reorganize the entries in tuple; the provided int pack must specify the new ordering.
   template <typename... Ts, int... Ps>
   auto shuffle_tuple(const std::tuple<Ts...> & tup, IntPack<Ps...>) {
     return std::make_tuple( std::get<Ps>(tup)... );
   }
 
 
-  // Apply a tuple as arguments to a function!
+  /// Apply a tuple as arguments to a function, where all argument positions in function are
+  /// specified with and IntPack
   template < typename FUN_T, typename TUPLE_T, int... N >   // Specify positions to apply...
   auto ApplyTuple(const FUN_T & fun, const TUPLE_T & tup, IntPack<N...>) {
     return fun(std::get<N>(tup)...);
   }
 
+  /// Apply a tuple as arguments to a function, in order.
   template <typename FUN_T, typename TUPLE_T>              // Apply whole tuple
   auto ApplyTuple(const FUN_T & fun, const TUPLE_T & tup) {
     return ApplyTuple(fun, tup, IntPackRange<0,tuple_size<TUPLE_T>()>());
@@ -44,8 +46,7 @@ namespace emp {
 
 
 
-  // Setup tuples to be able to be used in hash tables.
-
+  /// Setup tuples to be able to be used in hash tables.
   template <typename... TYPES>
   struct TupleHash {
     using tuple_t = std::tuple<TYPES...>;
