236ad0404aa951a7a54153e3cae5b9c7bb6e3a0d
Robin Miller <grenewodemiller@gmail.com>
2017-10-12 15:15:17 -0400

    Started adding opengl support

---
+++ examples/opengl/SimpleWindow.cc
@@ -0,0 +1,138 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016-2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  This file explores the template defined in evo::Population.h with an NK
+//  landscape.
+
+#include <iostream>
+
+#include "Evo/NK.h"
+#include "Evo/World.h"
+#include "config/ArgManager.h"
+#include "tools/BitVector.h"
+#include "tools/Random.h"
+
+#include "opengl/glcanvas.h"
+
+EMP_BUILD_CONFIG(
+  NKConfig, GROUP(DEFAULT, "Default settings for NK model"),
+  VALUE(K, uint32_t, 10, "Level of epistasis in the NK model"),
+  VALUE(N, uint32_t, 20, "Number of bits in each organisms (must be > K)"),
+  ALIAS(GENOME_SIZE),
+  VALUE(SEED, int, 0, "Random number seed (0 for based on time) "),
+  ALIAS(NUM_MUTS),
+  VALUE(TEST, std::string, "TestString", "This is a test string."))
+
+using BitOrg = emp::BitVector;
+
+// float vertices[] = {
+//     0.0f,  0.5f,  1.0f, 1.0f, 1.0f, 1.0f,  // Vertex 1 (X, Y)
+//     0.5f,  -0.5f, 1.0f, 0.0f, 1.0f, 1.0f,  // Vertex 2 (X, Y)
+//     -0.5f, -0.5f, 1.0f, 1.0f, 0.0f, 1.0f   // Vertex 3 (X, Y)
+// };
+
+const char* vertexSource = R"glsl(
+    attribute vec2 position;
+    attribute vec4 color;
+
+    varying vec4 fcolor;
+
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+        fcolor = color;
+    }
+)glsl";
+
+const char* fragmentSource = R"glsl(
+    precision mediump float;
+    varying vec4 fcolor;
+
+    void main()
+    {
+        gl_FragColor = fcolor;
+    }
+)glsl";
+
+// struct Test {
+//   emp::web::Document doc;
+//
+//   Test() : doc("doc") {
+//     std::cout << "Starting up..." << std::endl;
+//     NKConfig config;
+//     // config.Read("NK.cfg");
+//
+//     const uint32_t N = config.N();
+//     const uint32_t K = config.K();
+//
+//     emp::Random random(config.SEED());
+//     emp::NKLandscape landscape(N, K, random);
+//
+//     BitOrg start(N);
+//     for (uint32_t j = 0; j < N; j++) start[j] = random.P(0.5);
+//
+//     auto fitnessLandscape = CreateFitnessLandscape<BitOrg>(
+//       [N, &landscape](const BitOrg& root) {
+//         std::vector<BitOrg> neighbors;
+//         neighbors.reserve(N);
+//
+//         for (size_t i = 0; i < N; ++i) {
+//           auto neighbor = root;
+//           neighbor.Set(i, !root.Get(i));
+//
+//           neighbors.push_back(neighbor);
+//         }
+//         return neighbors;
+//       },
+//       [&](const BitOrg& org) { return landscape.GetFitness(org); });
+//     std::cout << "Displaying fitnessLandscape..." << std::endl;
+//
+//     auto fitnessNeighborhood =
+//     fitnessLandscape.GetFitnessNeighborhood(start); doc <<
+//     SimpleDisplayForFitnessNeighborhood("fn", fitnessNeighborhood);
+//   }
+// };
+//
+// Test test;
+
+int main(int argc, char* argv[]) {
+  emp::opengl::GLCanvas canvas;
+
+  auto shaderProgram = canvas.makeShaderProgram(vertexSource, fragmentSource);
+  shaderProgram.use();
+
+  VertexArrayObject vao =
+    canvas.makeVAO()
+      .with(BufferType::Array,
+            shaderProgram.attribute<float[2]>("position", false,
+                                              6 * sizeof(float), (void*)0),
+            shaderProgram.attribute<float[4]>("color", false, 6 * sizeof(float),
+                                              (void*)(2 * sizeof(float))))
+      .with(BufferType::ElementArray);
+
+  vao.bind();
+  vao.getBuffer<BufferType::Array>().push(
+    {
+      -50.5f,  100.5f,  1.0f, 1.0f, 1.0f, 1.0f,  // Vertex 1 (X, Y)
+      100.5f,  100.5f,  1.0f, 0.0f, 1.0f, 1.0f,  // Vertex 2 (X, Y)
+      100.5f,  -100.5f, 1.0f, 1.0f, 0.0f, 1.0f,  // Vertex 3 (X, Y)
+      -100.5f, -100.5f, 1.0f, 1.0f, 1.0f, 1.0f,  // Vertex 3 (X, Y)
+    },
+    BufferUsage::StaticDraw);
+  vao.getBuffer<BufferType::ElementArray>().push(
+    {
+      0, 1, 2,  // First Triangle
+      2, 3, 0   // Second Triangle
+    },
+    BufferUsage::StaticDraw);
+
+  canvas.runForever([](auto&&) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  });
+
+  return 0;
+}
---
+++ source/opengl/shaders.h
@@ -0,0 +1,184 @@
+#ifndef SHADERS_H
+#define SHADERS_H
+
+#include "shaders.h"
+
+#include <iostream>
+
+enum class ShaderType : GLenum {
+  Vertex = GL_VERTEX_SHADER,
+  Fragment = GL_FRAGMENT_SHADER,
+};
+
+class Shader {
+  private:
+  GLuint handle = 0;
+
+  public:
+  Shader(GLuint handle) : handle(handle) {}
+  Shader(const char* source, ShaderType type)
+    : handle(glCreateShader(static_cast<GLenum>(type))) {
+    glShaderSource(handle, 1, &source, nullptr);
+    glCompileShader(handle);
+
+    GLint success;
+    glGetShaderiv(handle, GL_COMPILE_STATUS, &success);
+    if (success == GL_FALSE) {
+      GLsizei maxLogLength = 0;
+      glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &maxLogLength);
+      maxLogLength += 1;
+      auto log = new GLchar[maxLogLength];
+
+      GLsizei logLength;
+      glGetShaderInfoLog(handle, maxLogLength, &logLength, log);
+
+      std::cout << std::string(log, logLength) << std::endl;
+    }
+  }
+
+  Shader(const Shader&) = delete;
+  Shader(Shader&& other) : handle(other.handle) { other.handle = 0; }
+
+  Shader& operator=(const Shader&) = delete;
+  Shader& operator=(Shader&& other) {
+    if (this != &other) {
+      destroy();
+      std::swap(handle, other.handle);
+    }
+
+    return *this;
+  }
+
+  ~Shader() { destroy(); }
+
+  operator GLuint() const { return handle; }
+  operator bool() const { return handle != 0; }
+
+  void destroy() {
+    if (handle != 0) {
+      glDeleteShader(handle);
+      handle = 0;
+    }
+  }
+};
+
+class VertexShader : public Shader {
+  public:
+  VertexShader(GLuint handle) : Shader(handle) {}
+  VertexShader(const char* source) : Shader(source, ShaderType::Vertex) {}
+  VertexShader(const VertexShader&) = delete;
+  VertexShader(VertexShader&& other) : Shader(std::move(other)) {}
+
+  VertexShader& operator=(VertexShader&& other) {
+    Shader::operator=(std::move(other));
+    return *this;
+  }
+
+  VertexShader& operator=(const VertexShader&) = delete;
+};
+
+class FragmentShader : public Shader {
+  public:
+  FragmentShader(GLuint handle) : Shader(handle) {}
+  FragmentShader(const char* source) : Shader(source, ShaderType::Fragment) {}
+  FragmentShader(const FragmentShader&) = delete;
+  FragmentShader(FragmentShader&& other) : Shader(std::move(other)) {}
+
+  FragmentShader& operator=(FragmentShader&& other) {
+    Shader::operator=(std::move(other));
+    return *this;
+  }
+
+  FragmentShader& operator=(const FragmentShader&) = delete;
+};
+
+class ShaderProgram {
+  private:
+  GLuint handle;
+
+  public:
+  ShaderProgram(const VertexShader& vertexShader,
+                const FragmentShader& fragmentShader)
+    : ShaderProgram(glCreateProgram()) {
+    glAttachShader(handle, vertexShader);
+    glAttachShader(handle, fragmentShader);
+    glLinkProgram(handle);
+
+    GLint linkStatus;
+    glGetProgramiv(handle, GL_LINK_STATUS, &linkStatus);
+    if (linkStatus == GL_FALSE) {
+      GLsizei maxLogLength = 0;
+      glGetProgramiv(handle, GL_INFO_LOG_LENGTH, &maxLogLength);
+      maxLogLength += 1;
+      auto log = new GLchar[maxLogLength];
+
+      GLsizei logLength;
+      glGetProgramInfoLog(handle, maxLogLength, &logLength, log);
+
+      std::cout << std::string(log, logLength) << std::endl;
+    }
+  }
+
+  explicit ShaderProgram(GLuint handle) : handle(handle) {}
+
+  ShaderProgram(const ShaderProgram&) = delete;
+  ShaderProgram(ShaderProgram&& other) : handle(other.handle) {
+    other.handle = 0;
+  }
+
+  ShaderProgram& operator=(const ShaderProgram&) = delete;
+  ShaderProgram& operator=(ShaderProgram&& other) {
+    if (this != &other) {
+      destory();
+      std::swap(handle, other.handle);
+    }
+    return *this;
+  }
+
+  ~ShaderProgram() {}
+
+  void destory() {
+    if (handle != 0) {
+      glDeleteProgram(handle);
+      handle = 0;
+    }
+  }
+
+  void use() {
+    if (handle != 0) {
+      glUseProgram(handle);
+    }
+  }
+
+  operator GLuint() const { return handle; }
+  operator bool() const { return handle != 0; }
+
+  template <typename... Args>
+  VertexAttribute getAttribute(const std::string& name,
+                               VertexAttributeSize size,
+                               VertexAttributeType type, Args&&... args) {
+    auto loc = glGetAttribLocation(handle, name.c_str());
+
+    return VertexAttribute(loc, size, type, std::forward<Args>(args)...);
+  }
+
+  template <typename... Args>
+  FloatingVertexAttribute getAttribute(const std::string& name,
+                                       VertexAttributeSize size,
+                                       FloatingVertexAttributeType type,
+                                       Args&&... args) {
+    auto loc = glGetAttribLocation(handle, name.c_str());
+
+    return FloatingVertexAttribute(loc, size, type,
+                                   std::forward<Args>(args)...);
+  }
+
+  template <typename T, typename... Args>
+  FloatingVertexAttribute attribute(const std::string& name, Args&&... args) {
+    using attribs = VertexAttributes<T>;
+    return getAttribute(name, attribs::size, attribs::type,
+                        std::forward<Args>(args)...);
+  }
+};
+
+#endif
