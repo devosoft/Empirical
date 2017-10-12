ad3c8b40bb84253fcf7455d54c96641567cc9fe5
Robin Miller <grenewodemiller@gmail.com>
2017-10-12 15:15:17 -0400

    Started adding opengl support

---
+++ source/opengl/glcanvas.h
@@ -0,0 +1,133 @@
+#ifndef GLCANVAS_H
+#define GLCANVAS_H
+
+#define GLFW_INCLUDE_ES3
+#include <GLES3/gl3.h>
+
+#include <GLFW/glfw3.h>
+#include "glwrap.h"
+#include "shaders.h"
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#else
+#include <thread>
+#endif
+
+namespace emp {
+  namespace opengl {
+
+#ifdef __EMSCRIPTEN__
+    unsigned int getWindowWidth() {
+      return EM_ASM_INT({ return window.innerWidth; });
+    }
+
+    unsigned int getWindowHeight() {
+      return EM_ASM_INT({ return window.innerHeight; });
+    }
+#endif
+
+    class GLCanvas {
+      private:
+      unsigned int width, height;
+#ifdef __EMSCRIPTEN__
+      EMSCRIPTEN_WEBGL_CONTEXT_HANDLE context;
+#else
+      GLFWwindow* window = nullptr;
+#endif
+
+      public:
+      GLCanvas(int width, int height, const char* title = "empirical")
+        : width(width), height(height) {
+#ifdef __EMSCRIPTEN__
+        EmscriptenWebGLContextAttributes attrs;
+        emscripten_webgl_init_context_attributes(&attrs);
+        context = emscripten_webgl_create_context(title, &attrs);
+#else
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_SAMPLES, 8);
+
+        window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+#endif
+
+        makeCurrent();
+      }
+
+#ifdef __EMSCRIPTEN__
+      GLCanvas(const char* title = "empirical")
+        : GLCanvas(getWindowWidth(), getWindowHeight(), title) {}
+#else
+      GLCanvas(const char* title = "empirical") : GLCanvas(800, 600, title) {}
+#endif
+      GLCanvas(const GLCanvas&) = delete;
+      GLCanvas(GLCanvas&& other) = delete;
+      GLCanvas& operator=(const GLCanvas&) = delete;
+      GLCanvas& operator=(GLCanvas&& other) = delete;
+
+      ~GLCanvas() {
+#ifdef __EMSCRIPTEN__
+        emscripten_cancel_main_loop();
+#else
+        if (window != nullptr) {
+          glfwDestroyWindow(window);
+        }
+        glfwTerminate();
+#endif
+      }
+
+      template <typename R>
+      void runForever(R&& onUpdate, int fps = -1, bool forever = true) {
+        makeCurrent();
+#ifdef __EMSCRIPTEN__
+        auto args = std::make_tuple(std::forward<R>(onUpdate), this);
+        emscripten_set_main_loop_arg(
+          [](void* argsUnsafe) {
+            auto a = reinterpret_cast<decltype(args)*>(argsUnsafe);
+            std::get<0> (*a)(*std::get<1>(*a));
+          },
+          &args, fps, forever);
+#else
+        if (fps <= 0) fps = 60;
+
+        const int frameLength = 1000.f / fps;
+        while (!glfwWindowShouldClose(window)) {
+          onUpdate(*this);
+
+          glfwSwapBuffers(window);
+          glfwPollEvents();
+
+          std::this_thread::sleep_for(std::chrono::milliseconds(frameLength));
+        }
+#endif
+      }
+
+      void makeCurrent() {
+#ifdef __EMSCRIPTEN__
+        emscripten_webgl_make_context_current(context);
+#else
+        glfwMakeContextCurrent(window);
+#endif
+      }
+
+      template <typename... Args>
+      VertexArrayObjectConfigurator makeVAO(Args&&... args) const {
+        return {std::forward<Args>(args)...};
+      }
+      template <typename... Args>
+      ShaderProgram makeShaderProgram(Args&&... args) const {
+        return {std::forward<Args>(args)...};
+      }
+
+      auto getWidth() const { return width; }
+      auto getHeight() const { return height; }
+    };
+
+  }  // namespace opengl
+}  // namespace emp
+
+#endif
