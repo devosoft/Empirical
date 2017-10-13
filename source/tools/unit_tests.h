cf21d64567083c3e2ab7ee598d8a429606a72813
Charles Ofria <charles.ofria@gmail.com>
2017-10-13 13:44:28 -0400

    Updated documentation for unit_tests.h

--- source/tools/unit_tests.h
+++ source/tools/unit_tests.h
@@ -1,28 +1,21 @@
-//  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2015-2017.
-//  Released under the MIT Software license; see doc/LICENSE
-//
-//
-//  Macros to facilitate unit testing.
-//  Status: DEPRECATED (now using Catch, but may revert back)
-//
-//
-//  EMP_TEST_MACRO(MACRO, EXP_RESULT)
-//    Input:  A macro call and an expected result.
-//    Output: Code to test the macro and optionally print it (if either in verbose mode
-//            or the macro does not produce the expected result.)
-//
-//  EMP_TEST_VALUE(VALUE, EXP_RESULT) make sure function prints expect result; print if verbose.
-//    Input:  An expression and an expected evaluation.
-//    Output: Code to evaluate the expression and optionally print it (if either in verbose mode
-//            or the macro does not produce the expected result).
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2015-2017
+ *
+ *  @file unit_tests.h
+ *  @brief Macros to facilitate unit testing.
+ *  @note Status: technically DEPRECATED (now using Catch, but may revert back)
+ */
 
 
 #ifndef EMP_UNIT_TESTS_H
 #define EMP_UNIT_TESTS_H
 
-// Take in a macro call and a string representing the expected result; verify that the
-// stringifyied macro result is the expected string AND print this info if -v flag is set.
+///  Input:  A macro call and a strings indicating the expected result.
+///  Output: Code that tests of the macro result matches the expected results, and optionally
+///          print it (if in verbose mode or if the macro fails to produce the expected result.)
+
 #define EMP_TEST_MACRO( MACRO, EXP_RESULT )                             \
   do {                                                                  \
     std::string result = std::string(EMP_STRINGIFY( MACRO ));           \
@@ -37,8 +30,13 @@
     }                                                                   \
   } while (false)
 
-// Take in an expression and a string representing the expected result; verify that the
-// stringifyied result is the expected string AND print this info if -v flag is set.
+
+///  Take in an expression and a string representing the expected result; verify that the
+///  stringifyied result is the expected string AND print this info if -v flag is set.
+///     Input:  An expression and an expected evaluation.
+///     Output: Code to evaluate the expression and optionally print it (if either in verbose mode
+///             or the macro does not produce the expected result).
+
 #define EMP_TEST_VALUE( VALUE, EXP_RESULT )                             \
   do {                                                                  \
     std::stringstream ss;                                               \
