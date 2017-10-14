689e7c56ac373d59aa97988b128fa2c9ec83d1c5
Charles Ofria <charles.ofria@gmail.com>
2017-10-14 10:47:00 -0400

    Updated documentation for array.h.

--- source/base/array.h
+++ source/base/array.h
@@ -1,9 +1,16 @@
-//  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2016-2017.
-//  Released under the MIT Software license; see doc/LICENSE
-//
-//  This class is a drop-in wrapper for std::array, adding on bounds checking.
-//  If EMP_NDEBUG is set then it reverts back to std::array.
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2016-2017
+ *
+ *  @file array.h
+ *  @brief A drop-in wrapper for std::array, adding on bounds checking.
+ *  @note Status: RELEASE
+ *
+ *  If EMP_NDEBUG is set, emp::array is just an alias for std::array.
+ *  Otherwise, every time an array is accessed, tests are done to make sure that the
+ *  access is legal.
+ */
 
 
 #ifndef EMP_ARRAY_H
@@ -19,6 +26,7 @@
 #ifdef EMP_NDEBUG
 
 namespace emp {
+  /// We are in release mode, so emp::array is simply an alias for std::array.
   template <typename T, size_t N> using array = std::array<T,N>;
 }
 
@@ -27,6 +35,8 @@ namespace emp {
 
 namespace emp {
 
+  /// We are in debug mode, so emp::array has the same interface as std::array, but with extra
+  /// bounds checking.
   template <typename T, size_t N>
   class array : public std::array<T,N> {
   private:
@@ -41,11 +51,13 @@ namespace emp {
     array(const std::array<T,N> & in) : stda_t(in) { ; }         // Emergency fallback conversion.
     ~array() = default;
 
+    /// Access array with extra bounds checking.
     T & operator[](size_t pos) {
       emp_assert(pos < (int) N, pos);
       return stda_t::operator[](pos);
     }
 
+    /// Const access of array with extra bounds checking.
     const T & operator[](size_t pos) const {
       emp_assert(pos < (int) N, pos);
       return stda_t::operator[](pos);
@@ -58,7 +70,7 @@ namespace emp {
 #endif
 
 namespace emp{
-  // Ensure that emp::array works with TypeID
+  /// Ensure that emp::array works with TypeID
   template <typename T, size_t N> struct TypeID<emp::array<T,N>> {
     static std::string GetName() {
       std::stringstream ss;
