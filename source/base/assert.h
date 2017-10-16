a3e38a02568bf72c2d4ba0205ecbd3406bdc6b8b
Charles Ofria <charles.ofria@gmail.com>
2017-10-15 21:20:18 -0400

    Updated documentation for assert.h

--- source/base/assert.h
+++ source/base/assert.h
@@ -1,22 +1,29 @@
-//  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2016-2017.
-//  Released under the MIT Software license; see doc/LICENSE
-//
-//
-///  A replacement for the system-level assert.h, called "emp_assert"
-///  It behaves nearly identically, but with some additional functionality:
-///  - If compiled with Emscripten, will provide pop-up alerts in a web browser.
-///  - emp_assert can take additional arguments.  If the assert is triggered, those arguments
-///    will be evaluated and printed.
-///  - if NDEBUG -or- EMP_NDEBUG is defined, the expression in emp_assert() is not evaluated.
-///  - if EMP_TDEBUG is defined, emp_assert() goes into test mode and records failures, but
-///    does not abort.  (useful for unit tests of asserts)
-///
-///  Example:
-///  `int a = 6;
-///  emp_assert(a==5, a);`
-///  When compiled in debug mode (i.e. without the -DNDEBUG flag), this will trigger an assertion
-///  error and print the value of a.
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2016-2017
+ *
+ *  @file array.h
+ *  @brief A more dynamic replacement for standard library asserts.
+ *  @note Status: RELEASE
+ *
+ *  A replacement for the system-level assert.h, called "emp_assert"
+ *  Added functionality:
+ *   - If compiled with Emscripten, will provide pop-up alerts in a web browser.
+ *   - emp_assert can take additional arguments.  If the assert is triggered, those extra
+ *     arguments will be evaluated and printed.
+ *   - if NDEBUG -or- EMP_NDEBUG is defined, the expression in emp_assert() is not evaluated.
+ *   - if EMP_TDEBUG is defined, emp_assert() goes into test mode and records failures, but
+ *     does not abort.  (useful for unit tests of asserts)
+ *
+ *  Example:
+ *
+ *     int a = 6;
+ *     emp_assert(a==5, a);
+ *
+ *  When compiled in debug mode (i.e. without the -DNDEBUG flag), this will trigger an assertion
+ *  error and print the value of a.
+ */
 
 #ifndef EMP_ASSERT_H
 #define EMP_ASSERT_H
@@ -29,12 +36,12 @@
 
 /// @cond DEFINES
 
-// If we are in emscripten, make sure to include the header.
+/// If we are in emscripten, make sure to include the header.
 #ifdef EMSCRIPTEN
 #include <emscripten.h>
 #endif
 
-// NDEBUG and TDEBUG should trigger their EMP equivilents.
+/// NDEBUG and TDEBUG should trigger their EMP equivilents.
 #ifdef NDEBUG
 #define EMP_NDEBUG
 #endif
@@ -44,9 +51,10 @@
 #endif
 
 
-// It's normally not possible to put an assert in a constexpr function because printing is not
-// available at compile time.  As such, we've added emp_constexpr which is empty in debug more
-// but becomes constexpr with NDEBUG.  Of course, this requires testing with NDEBUG turned on!
+/// It's normally not possible to put an assert in a constexpr function because printing is not
+/// available at compile time.  The "emp_constexpr" macro can replace "constexpr";
+/// emp_constexpr ignores constexpr in debug mode; regular assers disappear outside of debug mode.
+/// Of course, this requires testing with NDEBUG turned on to make sure compilation works!
 
 #ifdef EMP_NDEBUG
 #define emp_constexpr constexpr
@@ -55,15 +63,15 @@
 #endif
 
 
-// Turn off all asserts in EMP_NDEBUG
+/// Turn off all asserts in EMP_NDEBUG
 #ifdef EMP_NDEBUG
 namespace emp {
   const bool assert_on = false;
   const int assert_count = 0;
 }
 
-// Ideally, this assert should use the expression (to prevent compiler error), but should not
-// generate any assembly code.  For now, just make it blank (other options commented out)
+/// Ideally, this assert should use the expression (to prevent compiler error), but should not
+/// generate any assembly code.  For now, just make it blank (other options commented out)
 #define emp_assert(...)
 // #define emp_assert(EXPR) ((void) sizeof(EXPR) )
 // #define emp_assert(EXPR, ...) { constexpr bool __emp_assert_tmp = false && (EXPR); (void) __emp_assert_tmp; }
