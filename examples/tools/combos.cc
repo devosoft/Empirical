f7b5d1bf5e32165ac7851a35997fbcd6eacbcbd9
Charles Ofria <charles.ofria@gmail.com>
2017-10-16 22:20:34 -0400

    Added example code for combos.

---
+++ examples/tools/combos.cc
@@ -0,0 +1,16 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Example code for emp::ComboIDs class, emp::ComboGenerator class, and emp::combos function.
+
+#include <iostream>
+
+#include "tools/combos.h"
+
+int main()
+{
+  std::cout << "Temp." << std::endl;
+}
+
