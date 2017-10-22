91f1e4ea3510636fd94b79ab4903868ddef0c610
Robin Miller <grenewodemiller@gmail.com>
2017-10-22 10:11:07 -0400

    Removed namespace prefixes

--- tests/test_math.cc
+++ tests/test_math.cc
@@ -7,18 +7,19 @@ constexpr std::size_t rowsCheckGenerator(std::size_t r, std::size_t) {
   return r + 1;
 }
 
-TEST_CASE("Test Matrices", "[math]") {
-  constexpr auto rowsCheck =
-    emp::math::MatUtils<3, 3>::generate(&rowsCheckGenerator);
+using namespace emp::math;
 
-  REQUIRE((rowsCheck == emp::math::Mat3x3<std::size_t>{
+TEST_CASE("Test Matrices", "[math]") {
+  constexpr auto rowsCheck = MatUtils<3, 3>::generate(&rowsCheckGenerator);
+
+  REQUIRE((rowsCheck == Mat3x3<std::size_t>{
                           1u, 1u, 1u,  // row 1
                           2u, 2u, 2u,  // row 2
                           3u, 3u, 3u   // row 3
                         }));
 
-  constexpr auto ident = emp::math::MatUtils<3, 3>::identity<float>;
-  auto identManual = emp::math::Mat3x3<float>{
+  constexpr auto ident = MatUtils<3, 3>::identity<float>;
+  auto identManual = Mat3x3<float>{
     1.0f, 0.0f, 0.0f,  // row 1
     0.0f, 1.0f, 0.0f,  // row 2
     0.0f, 0.0f, 1.0f   // row 3
@@ -26,8 +27,8 @@ TEST_CASE("Test Matrices", "[math]") {
 
   REQUIRE(ident == identManual);
 
-  constexpr auto rowVec = emp::math::Mat<int, 1, 3>{1, 2, 3};
-  constexpr auto colVec = emp::math::Mat<int, 3, 1>{1, 2, 3};
+  constexpr auto rowVec = Mat<int, 1, 3>{1, 2, 3};
+  constexpr auto colVec = Mat<int, 3, 1>{1, 2, 3};
   REQUIRE(rowVec(0, 0) == 1);
   REQUIRE(rowVec(0, 1) == 2);
   REQUIRE(rowVec * colVec == 1 + 2 * 2 + 3 * 3);
@@ -42,12 +43,12 @@ TEST_CASE("Test Matrices", "[math]") {
   REQUIRE(ident * rowsCheck == rowsCheck);
   REQUIRE(rowsCheck * ident == rowsCheck);
 
-  REQUIRE((rowsCheck * rowsCheck == emp::math::Mat3x3<std::size_t>{
+  REQUIRE((rowsCheck * rowsCheck == Mat3x3<std::size_t>{
                                       6u, 6u, 6u,     // row 1
                                       12u, 12u, 12u,  // row 2
                                       18u, 18u, 18u   // row 3
                                     }));
-  REQUIRE((rowsCheck.transpose() == emp::math::Mat3x3<std::size_t>{
+  REQUIRE((rowsCheck.transpose() == Mat3x3<std::size_t>{
                                       1u, 2u, 3u,  // row 1
                                       1u, 2u, 3u,  // row 2
                                       1u, 2u, 3u   // row 3
