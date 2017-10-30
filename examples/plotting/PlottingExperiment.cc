1ee04ab1d455f0491c3049181b06428e2e30ca61
Robin Miller <grenewodemiller@gmail.com>
2017-10-30 16:40:21 -0400

    Starting a scratch branch to explore a plotting API

---
+++ examples/plotting/PlottingExperiment.cc
@@ -0,0 +1,7 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016-2017.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+
+int main(int argc, char* argv[]) { return 0; }
