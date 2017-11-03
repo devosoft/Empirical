6cee7bff7aace0b40e501f0dc389a17b22a05c31
Charles Ofria <charles.ofria@gmail.com>
2017-11-02 23:34:53 -0400

    Added a simple test for AvidaHardware (and added a Makefile entry for it.)

---
+++ examples/hardware/AvidaHardware.cc
@@ -0,0 +1,20 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2017
+ *
+ *  @file  AvidaGP.h
+ *  @brief This is example code for using LinearCode.h
+ */
+
+#include <iostream>
+
+#include "hardware/AvidaHardware.h"
+#include "tools/Random.h"
+
+int main()
+{
+  std::cout << "Test." << std::endl;
+
+  emp::AvidaHardware<> test_hw;
+}
