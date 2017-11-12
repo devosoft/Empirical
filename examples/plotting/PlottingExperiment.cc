d840a1fd59425bcd728c36f3d3ab12e6ad7583fa
Robin Miller <grenewodemiller@gmail.com>
2017-11-11 19:47:55 -0500

    Implemented data-property based api

--- examples/plotting/PlottingExperiment.cc
+++ examples/plotting/PlottingExperiment.cc
@@ -75,19 +75,41 @@ namespace detail {
 }  // namespace detail
 
 namespace properties {
-  struct Fill {};
-  struct Stroke {};
-  struct X {};
-  struct Y {};
-  struct Parent {};
 
+  template <typename P>
+  struct PropertyName {
+    template <typename V>
+    static constexpr auto from(V&& map) {
+      return [map = std::forward<V>(map)](auto properties) {
+        return properties.template set<V>(map(properties));
+      };
+    }
+
+    template <typename Props>
+    static constexpr auto& get(Props& properties) {
+      return properties.template get<P>();
+    }
+
+    template <typename Props>
+    static constexpr const auto& get(const Props& properties) {
+      return properties.template get<P>();
+    }
+  };
+
+  struct Fill : PropertyName<Fill> {};
+  struct Stroke : PropertyName<Stroke> {};
+  struct X : PropertyName<X> {};
+  struct Y : PropertyName<Y> {};
+  struct FillShader : PropertyName<FillShader> {};
+  struct StrokeShader : PropertyName<StrokeShader> {};
+  struct Value : PropertyName<Value> {};
 };  // namespace properties
 
 template <typename K, typename P>
-class Bundle;
+class Props;
 
 template <typename... K, typename... P>
-class Bundle<std::tuple<K...>, std::tuple<P...>> {
+class Props<std::tuple<K...>, std::tuple<P...>> {
   private:
   using keys_type = std::tuple<K...>;
   using properties_type = std::tuple<P...>;
@@ -98,6 +120,16 @@ class Bundle<std::tuple<K...>, std::tuple<P...>> {
   properties_type properties;
 
   public:
+  Props() = default;
+  Props(const Props&) = default;
+  Props(Props&&) = default;
+
+  Props(const properties_type& properties) : properties(properties) {}
+  Props(properties_type&& properties) : properties(std::move(properties)) {}
+
+  Props& operator=(const Props&) = default;
+  Props& operator=(Props&&) = default;
+
   template <typename U>
   static constexpr bool has() {
     return detail::TupleHas<std::decay_t<U>, decayed_keys_type>::value;
@@ -105,7 +137,7 @@ class Bundle<std::tuple<K...>, std::tuple<P...>> {
 
   template <typename U>
   constexpr decltype(auto) get() const {
-    static_assert(Bundle::has<U>(), "No such property");
+    static_assert(Props::has<U>(), "No such property");
     constexpr auto index =
       detail::IndexOf<std::decay_t<U>, decayed_keys_type>::value;
     return std::get<index>(properties);
@@ -113,7 +145,7 @@ class Bundle<std::tuple<K...>, std::tuple<P...>> {
 
   template <typename U>
   constexpr decltype(auto) get() {
-    static_assert(Bundle::has<U>(), "No such property");
+    static_assert(Props::has<U>(), "No such property");
     constexpr auto index =
       detail::IndexOf<std::decay_t<U>, decayed_keys_type>::value;
     return std::get<index>(properties);
@@ -121,54 +153,51 @@ class Bundle<std::tuple<K...>, std::tuple<P...>> {
 
   template <typename U, typename D>
   constexpr decltype(auto) get(
-    D&& defaultCallback, std::enable_if<Bundle::has<U>(), char> = 0) const {
+    D&& defaultCallback, std::enable_if<Props::has<U>(), char> = 0) const {
     return get<U>();
   }
 
   template <typename U, typename D>
   constexpr decltype(auto) get(
-    D&& defaultCallback, std::enable_if<!Bundle::has<U>(), char> = 0) const {
+    D&& defaultCallback, std::enable_if<!Props::has<U>(), char> = 0) const {
     return std::forward<D>(defaultCallback)();
   }
 
   template <typename U, typename D>
   constexpr decltype(auto) get(D&& defaultCallback,
-                               std::enable_if<Bundle::has<U>(), char> = 0) {
+                               std::enable_if<Props::has<U>(), char> = 0) {
     return get<U>();
   }
 
   template <typename U, typename D>
   constexpr decltype(auto) get(D&& defaultCallback,
-                               std::enable_if<!Bundle::has<U>(), char> = 0) {
+                               std::enable_if<!Props::has<U>(), char> = 0) {
     return std::forward<D>(defaultCallback)();
   }
 
   template <typename U>
-  constexpr auto set(U&& value) -> std::enable_if<Bundle::has<U>(), Bundle> {
+  constexpr auto set(U&& value) -> std::enable_if<Props::has<U>(), Props&> {
     get<U>() = std::forward<U>(value);
-
-    return std::move(*this);
+    return *this;
   }
 
   template <typename Key, typename Value>
-  constexpr auto set(Value&& value)
-    -> std::enable_if<!Bundle::has<Key>(),
-                      Bundle<std::tuple<Key, K...>, std::tuple<Value, P...>>> {
-    return std::tuple_cat(std::move(properties),
-                          std::forward_as_tuple<Value>(value));
+  constexpr auto set(Value value) -> typename std::enable_if<
+    !Props::has<Key>(),
+    Props<std::tuple<Key, K...>, std::tuple<Value, P...>>>::type {
+    return Props<std::tuple<Key, K...>, std::tuple<Value, P...>>{
+      std::tuple_cat(std::make_tuple(value), properties)};
   }
 };
 
-template <typename T>
+Props<std::tuple<>, std::tuple<>> nullProps() { return {}; };
+
 class Scatter {
   private:
   gl::shaders::SimpleSolidColor shader;
 
-  std::function<Vec4f(const T&)> color;
-
   public:
-  Scatter(gl::GLCanvas& canvas, std::function<Vec4f(const T&)> color)
-    : shader(canvas), color(color) {
+  Scatter(gl::GLCanvas& canvas) : shader(canvas) {
     shader.vao.bind();
     shader.shader.use();
     shader.vao.getBuffer<gl::BufferType::Array>().set(
@@ -192,63 +221,64 @@ class Scatter {
     shader.view = view;
 
     for (auto iter = begin; iter != end; ++iter) {
-      shader.model = Mat4x4f::translation(iter->x, iter->y);
-      shader.color = color(iter->value);
+      shader.model = Mat4x4f::translation(iter->template get<properties::X>(),
+                                          iter->template get<properties::Y>());
+      shader.color = iter->template get<properties::Fill>();
 
       glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     }
   }
 };
 
-template <typename T>
-class Line {
-  private:
-  gl::shaders::SimpleSolidColor shader;
-
-  public:
-  Line(gl::GLCanvas& canvas) : shader(canvas) {}
-
-  template <typename Iter>
-  void show(const Mat4x4f& projection, const Mat4x4f& view, Iter begin,
-            Iter end) {
-    if (begin == end) return;
-    Vec2f last{begin->x, begin->y};
-    ++begin;
-    if (begin == end) return;
-
-    shader.vao.bind();
-    shader.shader.use();
-    shader.proj = projection;
-    shader.view = view;
-    shader.color = Vec4f{1, 1, 0, 1};
-    shader.model = Mat4x4f::translation(0, 0);
-
-    std::vector<Vec3f> verts{{last.x(), last.y() + 2, 0},
-                             {last.x(), last.y() - 2, 0}};
-    std::vector<GLuint> triangles;
-    size_t i = 0;
-    for (auto iter = begin; iter != end; ++iter) {
-      verts.emplace_back(iter->x, iter->y + 2, 0);
-      verts.emplace_back(iter->x, iter->y - 2, 0);
-
-      triangles.push_back(i);
-      triangles.push_back(i + 1);
-      triangles.push_back(i + 2);
-
-      triangles.push_back(i + 2);
-      triangles.push_back(i + 3);
-      triangles.push_back(i + 1);
-      i += 2;
-    }
-
-    shader.vao.getBuffer<gl::BufferType::Array>().set(
-      verts, gl::BufferUsage::DynamicDraw);
-    shader.vao.getBuffer<gl::BufferType::ElementArray>().set(
-      triangles, gl::BufferUsage::DynamicDraw);
-
-    glDrawElements(GL_TRIANGLES, triangles.size(), GL_UNSIGNED_INT, 0);
-  }
-};
+// template <typename T>
+// class Line {
+//   private:
+//   gl::shaders::SimpleSolidColor shader;
+//
+//   public:
+//   Line(gl::GLCanvas& canvas) : shader(canvas) {}
+//
+//   template <typename Iter>
+//   void show(const Mat4x4f& projection, const Mat4x4f& view, Iter begin,
+//             Iter end) {
+//     if (begin == end) return;
+//     Vec2f last{begin->x, begin->y};
+//     ++begin;
+//     if (begin == end) return;
+//
+//     shader.vao.bind();
+//     shader.shader.use();
+//     shader.proj = projection;
+//     shader.view = view;
+//     shader.color = Vec4f{1, 1, 0, 1};
+//     shader.model = Mat4x4f::translation(0, 0);
+//
+//     std::vector<Vec3f> verts{{last.x(), last.y() + 2, 0},
+//                              {last.x(), last.y() - 2, 0}};
+//     std::vector<GLuint> triangles;
+//     size_t i = 0;
+//     for (auto iter = begin; iter != end; ++iter) {
+//       verts.emplace_back(iter->x, iter->y + 2, 0);
+//       verts.emplace_back(iter->x, iter->y - 2, 0);
+//
+//       triangles.push_back(i);
+//       triangles.push_back(i + 1);
+//       triangles.push_back(i + 2);
+//
+//       triangles.push_back(i + 2);
+//       triangles.push_back(i + 3);
+//       triangles.push_back(i + 1);
+//       i += 2;
+//     }
+//
+//     shader.vao.getBuffer<gl::BufferType::Array>().set(
+//       verts, gl::BufferUsage::DynamicDraw);
+//     shader.vao.getBuffer<gl::BufferType::ElementArray>().set(
+//       triangles, gl::BufferUsage::DynamicDraw);
+//
+//     glDrawElements(GL_TRIANGLES, triangles.size(), GL_UNSIGNED_INT, 0);
+//   }
+// };
 
 namespace __impl {
   template <typename F, typename T>
@@ -275,35 +305,25 @@ void allDo(F&& callback, T&& tuple) {
                   std::tuple_size<typename std::decay<T>::type>::value>{});
 }
 
-template <typename T, typename... L>
-class DataSet {
+template <typename T, typename C>
+class Map {
   public:
-  std::function<float(const T&)> x;
-  std::function<float(const T&)> y;
-
-  struct DataPoint {
-    const T& value;
-    float x;
-    float y;
-  };
-
-  std::tuple<L...> layers;
+  T next;
+  C change;
 
   public:
-  template <typename X, typename Y, typename... L1>
-  DataSet(X&& x, Y&& y, L1&&... layers)
-    : x(std::forward<X>(x)),
-      y(std::forward<Y>(y)),
-      layers(std::forward<L1>(layers)...) {}
+  template <typename T1, typename C1>
+  Map(T1&& next, C1&& change)
+    : next(std::forward<T1>(next)), change(std::forward<C1>(change)) {}
 
   template <typename Iter>
   void show(const Mat4x4f& projection, const Mat4x4f& view, Iter begin,
             Iter end) {
-    std::vector<DataPoint> dataPoints;
-    std::transform(begin, end, std::back_inserter(dataPoints),
-                   [this](const T& data) {
-                     return DataPoint{data, this->x(data), this->y(data)};
-                   });
+    using data_point_type = decltype(change(*begin));
+
+    std::vector<data_point_type> dataPoints;
+    std::transform(begin, end, std::back_inserter(dataPoints), change);
+    next.show(projection, view, dataPoints.begin(), dataPoints.end());
 
     // float dminX = std::numeric_limits<float>::max();
     // float dminY = std::numeric_limits<float>::max();
@@ -318,29 +338,78 @@ class DataSet {
     //   if (dp.y > dmaxY) dmaxY = dp.y;
     //   if (dp.y < dminY) dminY = dp.y;
     // }
-    allDo(
-      [&](auto&& layer) {
-        std::forward<decltype(layer)>(layer).show(
-          projection, view, dataPoints.begin(), dataPoints.end());
-      },
-      layers);
+    // allDo(
+    //   [&](auto&& layer) {
+    //     std::forward<decltype(layer)>(layer).show(
+    //       projection, view, dataPoints.begin(), dataPoints.end());
+    //   },
+    //   layers);
   }
 };
 
-template <typename T, typename X, typename Y, typename... L>
-DataSet<T, L...> dataset(X&& x, Y&& y, L&&... layers) {
-  return DataSet<T, L...>(std::forward<X>(x), std::forward<Y>(y),
-                          std::forward<L>(layers)...);
+template <typename T, typename C>
+auto map(T&& next, C&& change) {
+  return Map<T, C>{std::forward<T>(next), std::forward<C>(change)};
+}
+
+template <typename T>
+class Graph {
+  private:
+  T next;
+
+  public:
+  template <typename T1>
+  Graph(T1&& next) : next(std::forward<T1>(next)) {}
+
+  template <typename Iter>
+  void show(const Mat4x4f& projection, const Mat4x4f& view, Iter begin,
+            Iter end) {
+    auto map = [](auto&& value) {
+      return nullProps().set<properties::Value>(value);
+    };
+    using data_point_type = decltype(map(*begin));
+
+    std::vector<data_point_type> dataPoints;
+    std::transform(begin, end, std::back_inserter(dataPoints), map);
+    next.show(projection, view, dataPoints.begin(), dataPoints.end());
+
+    // float dminX = std::numeric_limits<float>::max();
+    // float dminY = std::numeric_limits<float>::max();
+    //
+    // float dmaxX = std::numeric_limits<float>::lowest();
+    // float dmaxY = std::numeric_limits<float>::lowest();
+    //
+    // for (auto& dp : dataPoints) {
+    //   if (dp.x > dmaxX) dmaxX = dp.x;
+    //   if (dp.x < dminX) dminX = dp.x;
+    //
+    //   if (dp.y > dmaxY) dmaxY = dp.y;
+    //   if (dp.y < dminY) dminY = dp.y;
+    // }
+    // allDo(
+    //   [&](auto&& layer) {
+    //     std::forward<decltype(layer)>(layer).show(
+    //       projection, view, dataPoints.begin(), dataPoints.end());
+    //   },
+    //   layers);
+  }
+};
+
+template <typename T>
+auto graph(T&& next) {
+  return Graph<T>{std::forward<T>(next)};
 }
 
 int main(int argc, char* argv[]) {
   gl::GLCanvas canvas(1000, 1000);
+  using namespace properties;
+  auto g = graph(map(Scatter(canvas), [](auto& props) {
+    auto& value = Value::get(props);
 
-  auto graph = dataset<Vec2f>(
-    [](auto& v) { return v.x(); }, [](auto& v) { return v.y(); },
-    Line<Vec2f>{canvas}, Scatter<Vec2f>{canvas, [](auto&) {
-                                          return Vec4f{1, 0, 1, 1};
-                                        }});
+    return props.template set<X>(value.x())
+      .template set<Y>(value.y())
+      .template set<Fill>(Vec4f{1, 1, 1, 1});
+  }));
 
   std::vector<Vec2f> data;
   for (int i = 0; i < 100; ++i) {
@@ -355,7 +424,7 @@ int main(int argc, char* argv[]) {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    graph.show(proj, view, data.begin(), data.end());
+    g.show(proj, view, data.begin(), data.end());
   });
 
   return 0;
