84658e7bd1209411e8519d239215c40ba044b841
Robin Miller <grenewodemiller@gmail.com>
2017-11-11 23:16:02 -0500

    Enable Line Plotting Experiment

--- examples/plotting/PlottingExperiment.cc
+++ examples/plotting/PlottingExperiment.cc
@@ -228,55 +228,57 @@ class Scatter {
   }
 };
 
-// template <typename T>
-// class Line {
-//   private:
-//   gl::shaders::SimpleSolidColor shader;
-//
-//   public:
-//   Line(gl::GLCanvas& canvas) : shader(canvas) {}
-//
-//   template <typename Iter>
-//   void show(const Mat4x4f& projection, const Mat4x4f& view, Iter begin,
-//             Iter end) {
-//     if (begin == end) return;
-//     Vec2f last{begin->x, begin->y};
-//     ++begin;
-//     if (begin == end) return;
-//
-//     shader.vao.bind();
-//     shader.shader.use();
-//     shader.proj = projection;
-//     shader.view = view;
-//     shader.color = Vec4f{1, 1, 0, 1};
-//     shader.model = Mat4x4f::translation(0, 0);
-//
-//     std::vector<Vec3f> verts{{last.x(), last.y() + 2, 0},
-//                              {last.x(), last.y() - 2, 0}};
-//     std::vector<GLuint> triangles;
-//     size_t i = 0;
-//     for (auto iter = begin; iter != end; ++iter) {
-//       verts.emplace_back(iter->x, iter->y + 2, 0);
-//       verts.emplace_back(iter->x, iter->y - 2, 0);
-//
-//       triangles.push_back(i);
-//       triangles.push_back(i + 1);
-//       triangles.push_back(i + 2);
-//
-//       triangles.push_back(i + 2);
-//       triangles.push_back(i + 3);
-//       triangles.push_back(i + 1);
-//       i += 2;
-//     }
-//
-//     shader.vao.getBuffer<gl::BufferType::Array>().set(
-//       verts, gl::BufferUsage::DynamicDraw);
-//     shader.vao.getBuffer<gl::BufferType::ElementArray>().set(
-//       triangles, gl::BufferUsage::DynamicDraw);
-//
-//     glDrawElements(GL_TRIANGLES, triangles.size(), GL_UNSIGNED_INT, 0);
-//   }
-// };
+class Line {
+  private:
+  gl::shaders::SimpleSolidColor shader;
+
+  public:
+  Line(gl::GLCanvas& canvas) : shader(canvas) {}
+
+  template <typename Iter>
+  void show(const Mat4x4f& projection, const Mat4x4f& view, Iter begin,
+            Iter end) {
+    using namespace properties;
+
+    if (begin == end) return;
+    Vec2f last{X::get(*begin), Y::get(*begin)};
+    ++begin;
+    if (begin == end) return;
+
+    shader.shader.use();
+    shader.vao.bind();
+    shader.proj = projection;
+    shader.view = view;
+    shader.color = Vec4f{1, 1, 0, 1};
+    shader.model = Mat4x4f::translation(0, 0);
+
+    std::vector<Vec3f> verts{{last.x(), last.y() + 2, 0},
+                             {last.x(), last.y() - 2, 0}};
+    std::vector<GLuint> triangles;
+    size_t i = 0;
+    for (auto iter = begin; iter != end; ++iter) {
+      verts.emplace_back(X::get(*iter), Y::get(*iter) + 2, 0);
+      verts.emplace_back(X::get(*iter), Y::get(*iter) - 2, 0);
+
+      triangles.push_back(i);
+      triangles.push_back(i + 1);
+      triangles.push_back(i + 2);
+
+      triangles.push_back(i + 2);
+      triangles.push_back(i + 3);
+      triangles.push_back(i + 1);
+      i += 2;
+    }
+
+    shader.vao.getBuffer<gl::BufferType::Array>().set(
+      verts, gl::BufferUsage::DynamicDraw);
+    shader.vao.getBuffer<gl::BufferType::ElementArray>().set(
+      triangles, gl::BufferUsage::DynamicDraw);
+
+    glDrawElements(GL_TRIANGLES, triangles.size(), GL_UNSIGNED_INT, 0);
+    shader.vao.unbind();
+  }
+};
 
 namespace __impl {
   template <typename F, typename T>
