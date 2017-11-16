6f018e19671d21339d94a99935b7d1d8146a25d7
Charles Ofria <charles.ofria@gmail.com>
2017-11-16 15:15:47 -0500

    Started building a specialized instruction library for AvidaGP.

---
+++ source/hardware/AvidaGP_InstLib.h
@@ -0,0 +1,21 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2017
+ *
+ *  @file  AvidaGP_InstLib.h
+ *  @brief A specialized version of InstLib to handle AvidaGP Instructions.
+ */
+
+#ifndef EMP_AVIDA_GP_INST_LIB_H
+#define EMP_AVIDA_GP_INST_LIB_H
+
+namespace emp {
+
+  template <typename HARDWARE>
+  struct AvidaGP_InstLib {
+  };
+
+}
+
+#endif
