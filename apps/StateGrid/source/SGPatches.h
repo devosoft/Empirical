6555bdb8aa8e4831f2237ba4549ee69af4a69ad6
Charles Ofria <charles.ofria@gmail.com>
2017-12-06 21:57:45 -0500

    Added a patch-based state grid.

---
+++ apps/StateGrid/source/SGPatches.h
@@ -0,0 +1,26 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2017
+ *
+ *  @file  SGPatches.h
+ *  @brief A specialized StateGrid for handling resource patches.
+ *
+ *  This file manages StateGrids composed of resource patchs in any topology (to be loaded).
+ */
+
+#include "Evo/StateGrid.h"
+
+class SGPatches : public emp::StateGrid {
+public:
+  SGPatches() : StateGrid() {
+    AddState(-1, '-', -0.5, "None",          "Empty space; poisonous.");
+    AddState( 0, '.',  0.0, "Consumed Food", "Previously had sustanance for an organism.");
+    AddState( 1, '#', +1.0, "Food",          "Sustanance to an org.");
+    AddState( 2, 'x',  0.0, "Consumed Edge", "Edge marker; previously had food.");
+    AddState( 3, 'X', +1.0, "Edge",          "Edge marker with food.");
+
+    Load("state_grids/islands_50x50.cfg");
+  }
+
+};
