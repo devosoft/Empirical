a789eb580028b0636e7dc8e8f23d624c1ce6cf3c
Charles Ofria <charles.ofria@gmail.com>
2017-12-28 23:19:22 -0500

    Setup SGWorld to always place orgs in a rectilinar facing.

--- apps/StateGrid/source/SGWorld.h
+++ apps/StateGrid/source/SGWorld.h
@@ -61,7 +61,12 @@ class SGWorld : public emp::World<SGOrg> {
           pop[pos]->SetStateGrid(state_grid);      // Ensure new org has access to state grid.
           pop[pos]->GetSGStatus().SetPos(4,4);     // Start at position (4,4)
           pop[pos]->GetSGStatus().TrackMoves();    // Make sure we have a history to refer to.
-          if (pos && random.P(0.8)) GetOrg(pos).GetSGStatus().Randomize(state_grid, random);
+
+          // Setup a random position and rectilinear facing.
+          if (pos) {
+            GetOrg(pos).GetSGStatus().Randomize(state_grid, random);
+            GetOrg(pos).GetSGStatus().SetFacing(random.GetUInt(4)*2+1);
+          }
         } );
 
       // Setup the mutation function.
