9b74837278058dcf0ff94ba7ca7cbfaa52df5ef7
Charles Ofria <charles.ofria@gmail.com>
2017-12-29 21:30:27 -0500

    Streamlined web interface for new SGWorld.

--- apps/StateGrid/source/web/StateGrid-web.cc
+++ apps/StateGrid/source/web/StateGrid-web.cc
@@ -11,39 +11,24 @@ namespace UI = emp::web;
 
 UI::Document doc("emp_base");
 
-constexpr size_t POP_SIZE = 1000;
-constexpr size_t GENOME_SIZE = 50;
 constexpr size_t UPDATES = 10000;
 
 int main()
 {
+  UI::Canvas canvas(400,400);
+
   doc << "<h1>Hello, world!</h1>";
+  doc << canvas;
+
 
   emp::Random random;
   SGWorld world(random, "AvidaWorld");
+  UI::Draw(canvas, (emp::StateGrid &) world.GetStateGrid(), emp::GetHueMap(5));
 
-  world.SetWellMixed(true);
-
-  // Build a random initial popoulation.
-  for (size_t i = 0; i < POP_SIZE; i++) {
-    SGOrg cpu(&(world.inst_lib));
-    cpu.PushRandom(random, GENOME_SIZE);
-    world.Inject(cpu.GetGenome());
-  }
 
   // Do the run...
   for (size_t ud = 0; ud < UPDATES; ud++) {
-    // Keep the best individual.
-    EliteSelect(world, 1, 1);
-
-    // Run a tournament for the rest...
-    TournamentSelect(world, 4, POP_SIZE-1);
-
-    // Put new organisms is place.
-    world.Update();
-
-    // Mutate all but the first organism.
-    world.DoMutations(1);
+    world.RunUpdate();
   }
 
   return 0;
