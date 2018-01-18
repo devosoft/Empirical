3f27a14e3b1a3c027762e4582382b24aadf700dd
Charles Ofria <charles.ofria@gmail.com>
2018-01-17 21:36:35 -0500

    Added Set accessors and Eval functions to Trait.

--- source/data/Trait.h
+++ source/data/Trait.h
@@ -25,11 +25,12 @@ namespace emp {
     using value_t = VALUE_T;
     using fun_t = std::function<value_t(target_t &)>;
     using range_t = emp::Range<value_t>;
+
   protected:
-    std::string name;              ///< Unique name for this trait.
-    std::string desc;              ///< Full description explaining this trait.
-    fun_t fun;                     ///< Function to retrieve trait value from target.
-    range_t range;     ///< Limits on legal values (defaults to extremes)
+    std::string name;   ///< Unique name for this trait.
+    std::string desc;   ///< Full description explaining this trait.
+    fun_t fun;          ///< Function to retrieve trait value from target.
+    range_t range;      ///< Limits on legal values (defaults to extremes)
 
   public:
     Trait(const std::string & _n, const fun_t & _f)
@@ -43,6 +44,17 @@ namespace emp {
     const std::string & GetDesc() const { return desc; }
     const fun_t & GetFun() const { return fun; }
     const range_t & GetRange() const { return range; }
+
+    void SetName(const std::string & _in) { name = _in; }
+    void SetDesc(const std::string & _in) { desc = _in; }
+    void SetFun(const fun_t & _in) { fun = _in; }
+    void SetRange(const range_t & _in) { range = _in; }
+    void SetRange(value_t min, value_t max) { range.Set(min, max); }
+    void SetMin(value_t min) { range.SetLower(min); }
+    void SetMax(value_t max) { range.SetUpper(max); }
+
+    value_t Eval(target_t & target) { return fun(target); }
+    value_t EvalLimit(target_t & target) { return range.Limit(fun(target)); }
   };
 
 
