3102e9321d85237c7ffd333c945547cd13972153
Charles Ofria <charles.ofria@gmail.com>
2018-01-21 12:26:56 -0500

    Added initial file to handle spatial structure for worlds.

---
+++ source/Evo/World_structure.h
@@ -0,0 +1,23 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2017-2018
+ *
+ *  @file  World_structure.h
+ *  @brief Functions for popular world structure methods.
+ */
+
+#ifndef EMP_EVO_WORLD_STRUCTURE_H
+#define EMP_EVO_WORLD_STRUCTURE_H
+
+#include "../base/assert.h"
+#include "../base/vector.h"
+#include "../tools/Random.h"
+
+namespace emp {
+
+  template<typename ORG> class World;
+
+}
+
+#endif
