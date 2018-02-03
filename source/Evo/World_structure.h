277518dda1ccddb610bb0c1a118cb20422299cf6
Charles Ofria <charles.ofria@gmail.com>
2018-02-03 08:08:35 -0500

    Fixed World_structure.h so that file now compiles.

--- source/Evo/World_structure.h
+++ source/Evo/World_structure.h
@@ -22,7 +22,7 @@ namespace emp {
   /// migtation.  Arguments are the number of pools, the size of each pool, and whether the
   /// generations should be synchronous (true) or not (false, default).
   template<typename ORG>
-  void World<ORG>::SetPools(World<ORG> & world, size_t num_pools,
+  void SetPools(World<ORG> & world, size_t num_pools,
                             size_t pool_size, bool synchronous_gen=false) {
     world.Resize(pool_size, num_pools);
     world.MarkSynchronous(synchronous_gen);
@@ -49,7 +49,7 @@ namespace emp {
         emp_assert(new_org);                                  // New organism must exist.
         const size_t pool_id = parent_id / pool_size;
         const size_t start_id = pool_id * pool_size;
-        for (size_t id = start_id; id < start_id+size_x; id++) {
+        for (size_t id = start_id; id < start_id+pool_size; id++) {
           if (world.IsOccupied(id) == false) {  // Search for an open positions...
             return world.AddNextOrgAt(new_org, id, world.GetGenotypeAt(parent_id));
           }
@@ -57,7 +57,7 @@ namespace emp {
         const size_t id = world.GetRandomNeighborID(parent_id);     // Placed near parent, in next pop.
         return world.AddNextOrgAt(new_org, id, world.GetGenotypeAt(parent_id));
       });
-      SetAttribute("SynchronousGen", "True");
+      world.SetAttribute("SynchronousGen", "True");
     } else {
       // Asynchronous: always go to a neigbor in current population.
       world.SetAddBirthFun( [world](Ptr<ORG> new_org, size_t parent_id) {
@@ -72,28 +72,28 @@ namespace emp {
   }
 
 
-  template<typename ORG>
-  void EliteSelect(World<ORG> & world, size_t e_count=1, size_t copy_count=1) {
-    emp_assert(e_count > 0 && e_count <= world.GetNumOrgs(), e_count);
-    emp_assert(copy_count > 0);
+  // template<typename ORG>
+  // void EliteSelect(World<ORG> & world, size_t e_count=1, size_t copy_count=1) {
+  //   emp_assert(e_count > 0 && e_count <= world.GetNumOrgs(), e_count);
+  //   emp_assert(copy_count > 0);
 
-    // Load the population into a multimap, sorted by fitness.
-    std::multimap<double, size_t> fit_map;
-    for (size_t id = 0; id < world.GetSize(); id++) {
-      if (world.IsOccupied(id)) {
-        const double cur_fit = world.CalcFitnessID(id);
-        fit_map.insert( std::make_pair(cur_fit, id) );
-      }
-    }
+  //   // Load the population into a multimap, sorted by fitness.
+  //   std::multimap<double, size_t> fit_map;
+  //   for (size_t id = 0; id < world.GetSize(); id++) {
+  //     if (world.IsOccupied(id)) {
+  //       const double cur_fit = world.CalcFitnessID(id);
+  //       fit_map.insert( std::make_pair(cur_fit, id) );
+  //     }
+  //   }
 
-    // Grab the top fitnesses and move them into the next generation.
-    auto m = fit_map.rbegin();
-    for (size_t i = 0; i < e_count; i++) {
-      const size_t repro_id = m->second;
-      world.DoBirth( world.GetGenomeAt(repro_id), repro_id, copy_count);
-      ++m;
-    }
-  }
+  //   // Grab the top fitnesses and move them into the next generation.
+  //   auto m = fit_map.rbegin();
+  //   for (size_t i = 0; i < e_count; i++) {
+  //     const size_t repro_id = m->second;
+  //     world.DoBirth( world.GetGenomeAt(repro_id), repro_id, copy_count);
+  //     ++m;
+  //   }
+  // }
 
 }
 
