21463598fcc27e348329d1f87603c6eacae1ced1
Charles Ofria <charles.ofria@gmail.com>
2018-02-06 10:20:20 -0500

    Setup world to be captured by reference in lambdas for creation of SetPools()

--- source/Evo/World_structure.h
+++ source/Evo/World_structure.h
@@ -23,14 +23,14 @@ namespace emp {
   /// generations should be synchronous (true) or not (false, default).
   template<typename ORG>
   void SetPools(World<ORG> & world, size_t num_pools,
-                            size_t pool_size, bool synchronous_gen=false) {
+                size_t pool_size, bool synchronous_gen=false) {
     world.Resize(pool_size, num_pools);
     world.MarkSynchronous(synchronous_gen);
     world.MarkSpaceStructured(true).MarkPhenoStructured(false);
 
     // -- Setup functions --
     // Inject in a empty pool -or- randomly if none empty
-    world.SetAddInjectFun( [world,pool_size](Ptr<ORG> new_org) {
+    world.SetAddInjectFun( [&world,pool_size](Ptr<ORG> new_org) {
       for (size_t id = 0; id < world.GetSize(); id += pool_size) {
         if (world.IsOccupied(id) == false) return world.AddOrgAt(new_org, id);
       }
@@ -38,14 +38,14 @@ namespace emp {
     });
 
     // Neighbors are everyone in the same pool.
-    world.SetGetNeighborFun( [world,pool_size](size_t id) {
+    world.SetGetNeighborFun( [&world,pool_size](size_t id) {
       const size_t pool_start = (id / pool_size) * pool_size;
-      return pool_start + world.GetRandom().GetUint(pool_size);
+      return pool_start + world.GetRandom().GetUInt(pool_size);
     });
 
     if (synchronous_gen) {
       // Place births in the next open spot in the new pool (or randomly if full!)
-      world.SetAddBirthFun( [world,pool_size](Ptr<ORG> new_org, size_t parent_id) {
+      world.SetAddBirthFun( [&world,pool_size](Ptr<ORG> new_org, size_t parent_id) {
         emp_assert(new_org);                                  // New organism must exist.
         const size_t pool_id = parent_id / pool_size;
         const size_t start_id = pool_id * pool_size;
@@ -60,7 +60,7 @@ namespace emp {
       world.SetAttribute("SynchronousGen", "True");
     } else {
       // Asynchronous: always go to a neigbor in current population.
-      world.SetAddBirthFun( [world](Ptr<ORG> new_org, size_t parent_id) {
+      world.SetAddBirthFun( [&world](Ptr<ORG> new_org, size_t parent_id) {
         auto id = world.GetRandomNeighborID(parent_id);
         auto p_genotype = world.GetGenotypeAt(parent_id);
         return world.AddOrgAt(new_org, id, p_genotype); // Place org in existing population.
