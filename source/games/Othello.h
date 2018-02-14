955ea7af50e6041e4c5dfbd0cd93ce5a57936f33
Alex Lalejini <amlalejini@gmail.com>
2018-02-14 00:12:12 -0500

    Working on Othello.h rewrite.

--- source/games/Othello.h
+++ source/games/Othello.h
@@ -21,228 +21,245 @@
 namespace emp {
 
   class Othello {
-  private:
-    vector<size_t> game_board;  /// Current board state
-    bool game_over = false;     /// Has the game ended?
-    size_t cur_player = 1;      /// ID of player using board
-    size_t board_size = 8;      /// Length of one side of N x N board
+  public:
+    enum BoardSpace { DARK, LIGHT, OPEN };
+
+    using board_t = emp::vector<BoardSpace>;
+
+
+  protected:
+
+    emp::vector<size_t> ALL_DIRECTIONS;
+
+    static constexpr size_t PLAYER_ID__DARK = 0;
+    static constexpr size_t PLAYER_ID__LIGHT = 1;
+    static constexpr size_t DIRECTION_ID__N = 0;
+    static constexpr size_t DIRECTION_ID__S = 1;
+    static constexpr size_t DIRECTION_ID__E = 2;
+    static constexpr size_t DIRECTION_ID__W = 3;
+    static constexpr size_t DIRECTION_ID__NE = 4;
+    static constexpr size_t DIRECTION_ID__NW = 5;
+    static constexpr size_t DIRECTION_ID__SE = 6;
+    static constexpr size_t DIRECTION_ID__SW = 7;
+
+    board_t game_board;  ///< Game board
+
+    bool over = false;    ///< Is the game over?
+    size_t cur_player;    ///< Is it DARK player's turn?
+
+    size_t board_size;    ///< Game board is board_size X board_size
 
   public:
-    using move_t = size_t;
 
-    Othello(size_t side_len, size_t first_player=1) {
-      Reset(first_player);
-      board_size = side_len;
+    Othello(size_t side_len)
+      : board_size(side_len), game_board(board_size)
+    {
+      emp_assert(board_size > 0);
+      ALL_DIRECTIONS = {N(), S(), E(), W(), NE(), NW(), SE(), SW()};
+      Reset();
     }
 
     ~Othello() { ; }
 
-    move_t GetIndex(int x, int y) { return ((y - 1) * board_size) + (x - 1); }
+    static inline constexpr size_t DarkPlayerID() { return PLAYER_ID__DARK; }
+    static inline constexpr size_t LightPlayerID() { return PLAYER_ID__LIGHT; }
+    static inline constexpr BoardSpace DarkDisk() { return BoardSpace::DARK; }
+    static inline constexpr BoardSpace LightDisk() { return BoardSpace::LIGHT; }
+    static inline constexpr BoardSpace OpenSpace() { return BoardSpace::OPEN; }
+    static inline constexpr size_t N() { return DIRECTION_ID__N; }
+    static inline constexpr size_t S() { return DIRECTION_ID__S; }
+    static inline constexpr size_t E() { return DIRECTION_ID__E; }
+    static inline constexpr size_t W() { return DIRECTION_ID__W; }
+    static inline constexpr size_t NE() { return DIRECTION_ID__NE; }
+    static inline constexpr size_t NW() { return DIRECTION_ID__NW; }
+    static inline constexpr size_t SE() { return DIRECTION_ID__SE; }
+    static inline constexpr size_t SW() { return DIRECTION_ID__SW; }
 
-    size_t GetOpponent(size_t player) {
-      if (player == 1) { return 2; }
-      else if (player == 2) { return 1; }
-      else { return -1; }
+
+    void Reset() {
+      // Reset the board.
+      for (size_t i = 0; i < game_board.size(); ++i) game_board[i] = OpenSpace();
+
+      // Setup Initial board
+      //  ........
+      //  ...LD...
+      //  ...DL...
+      //  ........
+      SetPos(board_size/2, (board_size/2)+1, DarkDisk());
+      SetPos(board_size/2, board_size/2, LightDisk());
+      SetPos((board_size/2)+1, board_size/2, DarkDisk());
+      SetPos((board_size/2)+1, (board_size/2)+1, LightDisk());
+
+      over = false;
+      cur_player = DarkPlayerID();
     }
 
-    size_t GetCurrPlayer() { return cur_player; }
+    ///
+    size_t GetBoardWidth() const { return board_size; }
+    size_t GetBoardHeight() const { return board_size; }
+    size_t GetBoardSize() const { return game_board.size(); }
 
-    size_t GetTile(move_t tile) { return game_board[tile]; }
+    size_t GetCurPlayer() const { return cur_player; }
 
-    size_t GetBoardSize() { return board_size; }
+    size_t OpponentID(size_t playerID) const {
+      emp_assert(playerID == DarkPlayerID() || playerID == LightPlayerID());
+      if (playerID == DarkPlayerID()) return LightPlayerID();
+      return DarkPlayerID();
+    }
 
-    void SetTurn(size_t player) { cur_player = player; }
+    /// Get x location in board grid given loc ID.
+    size_t GetPosX(size_t id) const { return id % board_size; }
 
-    void SetBoard(emp::array<int, 64> new_board) {
-      for (int i = 0; i < board_size; i++){
-        int tile = new_board[i];
-        if (tile == -1) tile = 2;
-        game_board[i] = tile;
+    /// Get Y location in board grid given loc ID.
+    size_t GetPosY(size_t id) const { return id / board_size; }
+
+    /// Get board ID of  given an x, y position.
+    size_t GetPosID(size_t x, size_t y) const { return (y * board_size) + x; }
+
+    bool IsValidPos(size_t x, size_t y) const {
+      return x < board_size && y < board_size;
+    }
+
+    bool IsValidPos(size_t id) const { return id < game_board.size(); }
+
+    /// Get location adjacent to ID in direction dir.
+    int GetNeighbor(size_t id, size_t dir) const {
+      emp_assert(dir >= 0 && dir <= 7 && id < game_board.size());
+      size_t x = GetPosX(id);
+      size_t y = GetPosY(id);
+      return GetNeighbor(x, y, dir);
+    }
+
+    int GetNeighbor(size_t x, size_t y, size_t dir) const {
+      emp_assert(dir >= 0 && dir <= 7);
+      int facing_x = 0, facing_y = 0;
+      switch(dir) {
+        case N():  facing_x = x;   facing_y = y+1; break;
+        case E():  facing_x = x-1; facing_y = y;   break;
+        case W():  facing_x = x+1; facing_y = y;   break;
+        case S():  facing_x = x;   facing_y = y-1; break;
+        case NE(): facing_x = x-1; facing_y = y+1; break;
+        case NW(): facing_x = x+1; facing_y = y+1; break;
+        case SE(): facing_x = x-1; facing_y = y-1; break;
+        case SW(): facing_x = x+1; facing_y = y-1; break;
+        default:
+          std::cout << "Bad direction!" << std::endl;
+          break;
+      }
+      if (!IsValidPos(facing_x, facing_y)) return -1;
+      return GetPosID(facing_x, facing_y);
+    }
+
+    /// Given a player ID (0 or 1 are valid), what Disk type does that player use?
+    BoardSpace GetDiskType(size_t player_id) {
+      emp_assert(player_id == 0 || player_id == 1);
+      if (player_id == DarkPlayerID()) return DarkDisk();
+      else return LightDisk();
+    }
+
+    /// Given a disk type (light or dark), get the player id that uses that disk type.
+    size_t GetPlayerID(BoardSpace disk_type) {
+      emp_assert(disk_type == DarkDisk() || disk_type == LightDisk());
+      if (disk_type == DarkDisk()) return DarkPlayerID();
+      else return LightPlayerID();
+    }
+
+    /// Get the value (light, dark or open) at an x,y location on the board.
+    BoardSpace GetPosValue(size_t x, size_t y) const {
+      return GetPosValue(GetPosID(x, y));
+    }
+
+    /// Get the value (light, dark, or open) at a position on the board.
+    BoardSpace GetPosValue(size_t id) const {
+      emp_assert(id < game_board.size());
+      return game_board[id];
+    }
+
+    /// Get the owner (playerID) at a position on the board.
+    /// Returns a -1 if no owner.
+    int GetPosOwner(size_t id) {
+      if (GetPosValue(id) == DarkDisk()) {
+        return (int)DarkPlayerID();
+      } else if (GetPosValue(id) == LightDisk()) {
+        return (int)LightPlayerID();
+      } else {
+        return -1;
       }
     }
 
-    bool IsOutOfBounds(int move) {
-      return (move < 0 || move >= board_size * board_size);
+    /// Get the owner (playerID) at an x,y position on the board.
+    /// Returns a -1 if no owner.
+    int GetPosOwner(size_t x, size_t y) {
+      return GetPosOwner(GetPosID(x, y));
     }
 
-    bool IsDone() { return game_over; }
+    // TODO: BoardAsInput ==> Should not be in Othello.h
+    board_t & GetBoard() { return game_board; }
 
-    int GetNeighbor(std::string location, move_t move) {
-      int neighbor = -1;
-
-      if (location == "above") {
-        neighbor = int(move) - board_size;
-      }
-      else if (location == "below") {
-        neighbor = int(move) + board_size;
-      }
-      else if (location == "left") {
-        neighbor = int(move) - 1;
-        if (move % board_size == 0) { neighbor = -1; }
-      }
-      else if (location == "right") {
-        neighbor = int(move) + 1;
-        if (move % board_size == (board_size - 1)) { neighbor = -1; }
-      }
-      else if (location == "AboveLeft") {
-        neighbor = int(move) - board_size - 1;
-        if (move % board_size == 0) { neighbor = -1; }
-      }
-      else if (location == "BelowLeft") {
-        neighbor = int(move) + board_size - 1;
-        if (move % board_size == 0) { neighbor = -1; }
-      }
-      else if (location == "AboveRight") {
-        neighbor = int(move) - board_size + 1;
-        if (move % board_size == (board_size - 1)) { neighbor = -1; }
-      }
-      else if (location == "BelowRight") {
-        neighbor = int(move) + board_size + 1;
-        if (move % board_size == (board_size - 1)) { neighbor = -1; }
-      }
-
-      if (IsOutOfBounds(neighbor)) { neighbor = -1; }
-
-      return neighbor;
+    bool IsMoveValid(size_t playerID, size_t move_x, size_t move_y) {
+      return IsMoveValid(playerID, GetPosID(move_x, move_y));
     }
 
-    void Reset(size_t first_player) {
-      for (int i = 0; i < board_size * board_size; i++){
-        game_board.push_back(0);
-      }
-
-      // Setup initial board for player 1
-      vector<move_t> move_list;
-      move_list.push_back( GetIndex(board_size / 2, board_size / 2 + 1) );
-      move_list.push_back( GetIndex(board_size / 2 + 1, board_size / 2) );
-      SetTiles(move_list, 1);
-
-      // Setup initial board for player 2
-      move_list.clear();
-      move_list.push_back( GetIndex(board_size / 2, board_size / 2) );
-      move_list.push_back( GetIndex(board_size / 2 + 1, board_size / 2 + 1) );
-      SetTiles(move_list, 2);
-
-      game_over = false;
-      cur_player = first_player;
+    /// Is given move valid?
+    bool IsMoveValid(size_t playerID, size_t move_id) {
+      emp_assert(playerID == DarkPlayerID() || playerID == LightPlayerID());
+      // 1) The move position must be empty.
+      if (GetPosValue(move_id) != OpenSpace()) return false;
+      // 2) A non-zero number of tiles must flip.
+      return (bool)GetFlipList(playerID, move_id, true).size();
     }
 
-    std::unordered_map<int, double> AsInput(size_t player_id) const {;} // TODO
-
-    bool IsMoveValid(size_t player, move_t move) {
-      vector<move_t> flip_list = GetFlipList(player, move, true);
-
-      if (GetTile(move) != 0) { return false; } // Tile must be empty
-
-      return flip_list.size() != 0; // If no tiles flip, not a valid move
-    }
-
-    emp::vector<size_t> GetMoveOptions(size_t player) {
-      vector<size_t> valid_moves;
-      for (size_t i = 0; i < board_size * board_size; i++) {
-        if (IsMoveValid(player, i)) { valid_moves.push_back(i); }
-      }
-
-      return valid_moves;
-    }
-
-    bool DoMove(size_t player, move_t move) {
-      SetTile(move, player);
-      FlipTiles(player, move);
-      return CheckGoAgain(player);
-    }
-
-    bool CheckGoAgain(size_t player){
-      vector<size_t> player_moves = GetMoveOptions(player);
-      vector<size_t> opp_moves = GetMoveOptions(GetOpponent(player));
-      bool again = false;
-
-      if (player_moves.size() <= 0 && opp_moves.size() <= 0) { game_over = true; }
-
-      // If opponent has no moves, play again
-      else if (opp_moves.size() <= 0) { again = true; }
-
-      return again;
-    }
-
-    void SetTiles(vector<move_t> move_list, size_t player){
-      for (move_t move : move_list){
-        SetTile(move, player);
-      }
-    }
-
-    void SetTile(size_t move, size_t player){
-      game_board[move] = player;
-    }
-
-    void FlipTiles(size_t player, move_t move) {
-      vector<move_t> flip_list = GetFlipList(player, move, false);
-      for (move_t tile : flip_list) { SetTile(tile, player); }
-    }
-
-    double GetScore(size_t player) {
-      double score = 0;
-
-      for (size_t i = 0; i < board_size * board_size; i++){
-        if (GetTile(i) == player) { score++; }
-      }
-
-      return score;
-    }
-
-    void Print(std::ostream & os=std::cout) {
-      os<<std::endl<<"  ";
-      unsigned char letter = 'A';
-      for (int i = 0; i < board_size; i++){ std::cout<<char(letter + i) <<" "; }
-      os<<std::endl;
-
-      for (int y = 1; y <= board_size; y++) {
-        os<< y << " ";
-        for (int x = 1; x <= board_size; x++) {
-          char piece;
-          int tile = GetTile(GetIndex(x,y));
-
-          if (tile == 0) { piece = '-'; }
-          else if (tile == 1) { piece = 'X'; }
-          else { piece = 'O'; }
-
-          os<<piece<<" ";
-        }
-        os<<std::endl;
-      }
-
-      os<<std::endl;
-    }
-
-    vector<move_t> GetFlipList(size_t player, move_t move, bool check_valid=false){
-      vector<std::string> directions = {"above", "below", "left", "right",
-                                        "AboveLeft", "AboveRight",
-                                        "BelowLeft", "BelowRight"};
-      vector<move_t> flip_list;
-
-      for (std::string direction : directions) {
-        int pos = GetNeighbor(direction, move);
-        vector<move_t> temp_list;
-
-        while (pos != -1){
-          if (GetTile(pos) == 0) { break; }
-
-          else if (GetTile(pos) != player) { temp_list.push_back(pos); }
-
-          else {
-            for (move_t move : temp_list) { flip_list.push_back(move); }
+    emp::vector<size_t> GetFlipList(size_t playerID, size_t move_id, bool only_first_valid=false) {
+      emp::vector<size_t> flip_list;
+      size_t prev_len = 0;
+      for (size_t dir : ALL_DIRECTIONS) {
+        int neighborID = GetNeighbor(move_id, dir);
+        while (neighborID != -1) {
+          if (GetPosValue(neighborID) == OpenSpace()) {
+            flip_list.resize(prev_len);
+            break;
+          } else if (GetPosOwner(neighborID) == OpponentID(playerID)) {
+            flip_list.emplace_back(neighborID);
+          } else {
+            prev_len = flip_list.size();
             break;
           }
-
-          pos = GetNeighbor(direction, pos);
+          neighborID = GetNeighbor((size_t)neighborID, dir);
         }
-
-        temp_list.clear();
-
-        // If just checking that move is valid, we don't need full flip list
-        if (check_valid && flip_list.size() > 0) { break; }
+        if (only_first_valid && flip_list.size()) break;
       }
       return flip_list;
+    }
+
+    /// Set board position (ID) to given space value.
+    void SetPos(size_t id, BoardSpace space) {
+      emp_assert(id < game_board.size());
+      game_board[id] = space;
+    }
+
+    /// Set board position (x,y) to given space value.
+    void SetPos(size_t x, size_t y, BoardSpace space) {
+      SetPos(GetPosID(x, y), space);
+    }
+
+    /// Set board position (ID) to disk type used by playerID.
+    void SetPos(size_t id, size_t playerID) {
+      emp_assert(playerID == LightPlayerID() || playerID == DarkPlayerID());
+      SetPos(id, GetDiskType(playerID));
+    }
+
+    void SetPositions(emp::vector<size_t> ids, size_t playerID) {
+      for (size_t i = 0; i < ids.size(); ++i) SetPos(ids[i], playerID);
+    }
+
+    void SetPositions(emp::vector<size_t> ids, BoardSpace space) {
+      for (size_t i = 0; i < ids.size(); ++i) SetPos(ids[i], space);
+    }
+
+    /// Set board position (x,y) to disk type used by playerID.
+    void SetPos(size_t x, size_t y, size_t playerID) {
+      emp_assert(playerID == LightPlayerID() || playerID == DarkPlayerID());
+      SetPos(GetPosID(x, y), GetDiskType(playerID));
     }
 
   };
