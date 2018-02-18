c5aecb0e23b042ff65fb1394893ee2b7d030c115
Charles Ofria <charles.ofria@gmail.com>
2018-02-17 23:20:39 -0500

    Updated comments in DataInterface.h

--- source/data/DataInterface.h
+++ source/data/DataInterface.h
@@ -1,9 +1,11 @@
-//  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2016-2017.
-//  Released under the MIT Software license; see doc/LICENSE
-//
-//
-//  DataInterface is a *generic* interface to a DataNode
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2016-2018
+ *
+ *  @file  DataInterface.h
+ *  @brief DataInterface is a *generic* interface to a DataNode.
+ */
 
 
 #ifndef EMP_DATA_INTERFACE_H
@@ -13,8 +15,7 @@
 
 namespace emp {
 
-  /** A generic interface to a DataNode (so that you don't need to 
-   * worry about the node's exact type)*/
+  /// A generic interface to a DataNode (so that you don't need to know the node's exact type)
   class DataInterface {
   private:
   public:
@@ -62,82 +63,67 @@ namespace emp {
     DataInterface_Impl & operator=(const DataInterface_Impl &) = delete;
     DataInterface_Impl & operator=(DataInterface_Impl &&) = delete;
 
-    /** Returns the number values added to this node since the last reset.*/
+    /// Returns the number values added to this node since the last reset.
     size_t GetCount() const { return node->GetCount(); }
 
-    /** Returns the number of times this node has been reset.*/
+    /// Returns the number of times this node has been reset.
     size_t GetResetCount() const { return node->GetResetCount(); }
 
-    /** Returns the sum of values added since the last reset.
-     * Requires that the data::Range or data::FullRange be added
-     * to the DataNode */
+    /// Returns the sum of values added since the last reset.
+    /// Requires that the data::Range or data::FullRange be added to the DataNode 
     double GetTotal() const { return node->GetTotal(); }
 
-    /** Returns the mean of the values added since the last reset.
-     * Requires that the data::Range or data::FullRange be added
-     * to the DataNode */
+    /// Returns the mean of the values added since the last reset.
+    /// Requires that the data::Range or data::FullRange be added to the DataNode 
     double GetMean() const { return node->GetMean(); }
 
-    /** Returns the minimum of the values added since the last reset.
-     * Requires that the data::Range or data::FullRange be added
-     * to the DataNode */
+    /// Returns the minimum of the values added since the last reset.
+    /// Requires that the data::Range or data::FullRange be added to the DataNode 
     double GetMin() const { return node->GetMin(); }
 
-    /** Returns the maximum of the values added since the last reset.
-     * Requires that the data::Range or data::FullRange be added
-     * to the DataNode */
+    /// Returns the maximum of the values added since the last reset.
+    /// Requires that the data::Range or data::FullRange be added to the DataNode 
     double GetMax() const { return node->GetMax(); }
 
-    /** Returns the variance of the values added since the last reset.
-     * Requires that the data::Stats or data::FullStats be added
-     * to the DataNode */
+    /// Returns the variance of the values added since the last reset.
+    /// Requires that the data::Stats or data::FullStats be added to the DataNode 
     double GetVariance() const { return node->GetVariance(); }
 
-    // /** Returns the standard deviation of the values added since the last reset.
-    //  * Requires that the data::Stats or data::FullStats be added
-    //  * to the DataNode */
+    /// Returns the standard deviation of the values added since the last reset.
+    /// Requires that the data::Stats or data::FullStats be added to the DataNode 
     double GetStandardDeviation() const { return node->GetStandardDeviation(); }
 
-    // /** Returns the skewness of the values added since the last reset.
-    //  * Requires that the data::Stats or data::FullStats be added
-    //  * to the DataNode */
+    /// Returns the skewness of the values added since the last reset.
+    /// Requires that the data::Stats or data::FullStats be added to the DataNode 
     double GetSkew() const { return node->GetSkew(); }
 
-    // /** Returns the kurtosis of the values added since the last reset.
-    //  * Requires that the data::Stats or data::FullStats be added
-    //  * to the DataNode */
+    /// Returns the kurtosis of the values added since the last reset.
+    /// Requires that the data::Stats or data::FullStats be added to the DataNode 
     double GetKurtosis() const { return node->GetKurtosis(); }
 
-    /** Runs the Pull function for this DataNode and records the
-     * resulting values. Requires that the data::Pull module was
-     * added to this DataNode, and that a pull function was specified.
-    */
+    /// Runs the Pull function for this DataNode and records the resulting values.
+    /// Requires that the data::Pull module was added to this DataNode, and that a pull function
+    /// was specified.
     void PullData() { node->PullData(); }
     
-    /** Reset this node. The exact effects of this depend on the
-     * modules that this node has, but in general it prepares the
-     * node to recieve a new set of data.
-    */
+    /// Reset this node. The exact effects of this depend on the modules that this node has,
+    /// but in general it prepares the node to recieve a new set of data.
     void Reset() { node->Reset(); }
 
-    /** Print debug information about this node to @param os. 
-     * Useful for tracking which modifiers are included.
-    */
+    /// Print debug information about this node to @param os. 
+    /// Useful for tracking which modifiers are included.
     void PrintDebug(std::ostream & os=std::cout) { node->PrintDebug(os); }
 
-    /** Returns this node's name. Requires that the data::Info module was
-     * added to this DataNode, and that a name was set.
-    */
+    /// Returns this node's name. Requires that the data::Info module was
+    /// added to this DataNode, and that a name was set.
     void GetName() { node->GetName(); }
 
-    /** Returns this node's description. Requires that the data::Info module was
-     * added to this DataNode, and that a description was set.
-    */
+    /// Returns this node's description. Requires that the data::Info module was
+    /// added to this DataNode, and that a description was set.
     void GetDescription() { node->GetDescription(); }
 
-    /** Returns this node's keyword. Requires that the data::Info module was
-     * added to this DataNode, and that a keyword was set.
-    */
+    /// Returns this node's keyword. Requires that the data::Info module was
+    /// added to this DataNode, and that a keyword was set.
     void GetKeyword() { node->GetKeyword(); }
 
   };
