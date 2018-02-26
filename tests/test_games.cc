63a6ad85fd1fdeff54413edd4b766189ca9c022f
Charles Ofria <charles.ofria@gmail.com>
2018-02-26 14:01:16 -0500

    Made othello unit tests consistent with new interface.

--- tests/test_games.cc
+++ tests/test_games.cc
@@ -43,44 +43,44 @@ TEST_CASE("Test Othello", "[games]") {
 
   // On initial board:
   // - Make sure all positions we expect to be valid are valid.
-  size_t darkID = emp::Othello::DarkPlayerID;
-  size_t lightID = emp::Othello::LightPlayerID;
-  REQUIRE(othello_w8.IsMoveValid(lightID, 4,2));
-  REQUIRE(othello_w8.IsMoveValid(lightID, 2,4));
-  REQUIRE(othello_w8.IsMoveValid(lightID, 5,3));
-  REQUIRE(othello_w8.IsMoveValid(lightID, 3,5));
-  REQUIRE(othello_w8.IsMoveValid(darkID, 3,2));
-  REQUIRE(othello_w8.IsMoveValid(darkID, 2,3));
-  REQUIRE(othello_w8.IsMoveValid(darkID, 5,4));
-  REQUIRE(othello_w8.IsMoveValid(darkID, 4,5));
+  auto player_dark = emp::Othello::Player::DARK;
+  auto player_light = emp::Othello::Player::LIGHT;
+  REQUIRE(othello_w8.IsValidMove(player_light, 4,2));
+  REQUIRE(othello_w8.IsValidMove(player_light, 2,4));
+  REQUIRE(othello_w8.IsValidMove(player_light, 5,3));
+  REQUIRE(othello_w8.IsValidMove(player_light, 3,5));
+  REQUIRE(othello_w8.IsValidMove(player_dark, 3,2));
+  REQUIRE(othello_w8.IsValidMove(player_dark, 2,3));
+  REQUIRE(othello_w8.IsValidMove(player_dark, 5,4));
+  REQUIRE(othello_w8.IsValidMove(player_dark, 4,5));
   // - And all positions are owned by who we expect.
   size_t l0id = othello_w8.GetPosID(3,3); // light
   size_t d0id = othello_w8.GetPosID(3,4); // dark
   size_t d1id = othello_w8.GetPosID(4,3);  // dark
   size_t l1id = othello_w8.GetPosID(4,4); // light
-  REQUIRE(othello_w8.GetPosOwner(l0id) == lightID);
-  REQUIRE(othello_w8.GetPosOwner(d0id) == darkID);
-  REQUIRE(othello_w8.GetPosOwner(d1id) == darkID);
-  REQUIRE(othello_w8.GetPosOwner(l1id) == lightID);
+  REQUIRE(othello_w8.GetPosOwner(l0id) == player_light);
+  REQUIRE(othello_w8.GetPosOwner(d0id) == player_dark);
+  REQUIRE(othello_w8.GetPosOwner(d1id) == player_dark);
+  REQUIRE(othello_w8.GetPosOwner(l1id) == player_light);
   // Check initial board setup.
   // -- Score should be 2,2.
-  REQUIRE(othello_w8.GetScore(darkID) == 2);
-  REQUIRE(othello_w8.GetScore(lightID) == 2);
-  REQUIRE(othello_w1024.GetScore(darkID) == 2);
-  REQUIRE(othello_w1024.GetScore(lightID) == 2);
+  REQUIRE(othello_w8.GetScore(player_dark) == 2);
+  REQUIRE(othello_w8.GetScore(player_light) == 2);
+  REQUIRE(othello_w1024.GetScore(player_dark) == 2);
+  REQUIRE(othello_w1024.GetScore(player_light) == 2);
   // -- Check get Frontier position count.
-  REQUIRE(othello_w8.GetFrontierPosCnt(darkID) == 10);
-  REQUIRE(othello_w8.GetFrontierPosCnt(lightID) == 10);
+  REQUIRE(othello_w8.GetFrontierPosCnt(player_dark) == 10);
+  REQUIRE(othello_w8.GetFrontierPosCnt(player_light) == 10);
   // -- Check get flip list for 1 valid move.
-  REQUIRE(othello_w8.GetFlipList(darkID, othello_w8.GetPosID(3,2)).size() == 1);
+  REQUIRE(othello_w8.GetFlipList(player_dark, othello_w8.GetPosID(3,2)).size() == 1);
 
   // Make some moves. Check that they altered the board properly.
-  othello_w8.DoMove(darkID, 3,2);
-  REQUIRE(othello_w8.GetPosOwner(3,2) == darkID);
-  REQUIRE(othello_w8.GetPosOwner(3,3) == darkID);
-  REQUIRE(othello_w8.GetScore(darkID) == 4);
-  REQUIRE(othello_w8.GetScore(lightID) == 1);
-  REQUIRE(othello_w8.GetCurPlayer() == lightID);
+  othello_w8.DoMove(player_dark, 3,2);
+  REQUIRE(othello_w8.GetPosOwner(3,2) == player_dark);
+  REQUIRE(othello_w8.GetPosOwner(3,3) == player_dark);
+  REQUIRE(othello_w8.GetScore(player_dark) == 4);
+  REQUIRE(othello_w8.GetScore(player_light) == 1);
+  REQUIRE(othello_w8.GetCurPlayer() == player_light);
 
   // Play the rest of the game randomly a bunch of times.
   size_t rounds;
@@ -89,7 +89,7 @@ TEST_CASE("Test Othello", "[games]") {
     rounds = 0;
     while (!othello_w8.IsOver()) {
       REQUIRE(rounds <= 60);  //< Max of 60 rounds possible.
-      size_t player = othello_w8.GetCurPlayer();
+      auto player = othello_w8.GetCurPlayer();
       emp::vector<size_t> options = othello_w8.GetMoveOptions(player);
       size_t move = options[random.GetUInt(0, options.size())];
       othello_w8.DoMove(player, move);
