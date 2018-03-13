cb9bf81091acafaa575834fe66c055c18e7abd00
Robin Miller <grenewodemiller@gmail.com>
2018-03-13 16:52:25 -0400

    Remove the directive to use the GL core profile
    Apparently, not all GPUs like the core profile directive, and will
    refuse to initialize the context if it is requested.

--- source/opengl/glcanvas.h
+++ source/opengl/glcanvas.h
@@ -173,7 +173,6 @@ namespace emp {
         glfwInit();
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         glfwWindowHint(GLFW_SAMPLES, 8);
 
