3610b2c6a6818ace4c75cc4d9611bd1dafeab74c
Charles Ofria <charles.ofria@gmail.com>
2018-03-25 00:48:49 -0400

    Added Process() and ExtractCol() to File.h

--- source/tools/File.h
+++ source/tools/File.h
@@ -228,8 +228,26 @@ namespace emp {
       } );
       return *this;
     }
-  };
 
+    /// Run a function on each line of a file and return the restults as a vector.
+    /// Note: Function is allowed to modify string.
+    template <typename T>
+    emp::vector<T> Process(const std::function<T(std::string &)> & fun) {
+      emp::vector<T> results(lines.size());
+      for (size_t i = 0; i < lines.size(); i++) {
+        results[i] = fun(lines[i]);
+      }
+      return results;
+    }
+
+    emp::vector<std::string> ExtractCol(char delim=',') {
+      return Process<std::string>( [delim](std::string & line){
+        return string_pop(line, delim);
+      });
+    }
+
+  };
+  
 }
 
 #endif
