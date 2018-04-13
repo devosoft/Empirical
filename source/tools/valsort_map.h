2ec7302937f41fdad41520439a5d1268e159501d
Charles Ofria <charles.ofria@gmail.com>
2018-04-12 21:36:08 -0400

    Starting to build a map that can be sorted by value.

---
+++ source/tools/valsort_map.h
@@ -0,0 +1,25 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  valsort_map.h
+ *  @brief This file defines a map that is sorted by value, not key.
+ *  @note Status: ALPHA
+ */
+
+#ifndef EMP_VALSORT_MAP_H
+#define EMP_VALSORT_MAP_H
+
+#include <map>
+
+#include "../base/vector.h"
+
+template <typename KEY, typename VALUE>
+class valsort_map {
+  
+};
+
+#endif
+
+
