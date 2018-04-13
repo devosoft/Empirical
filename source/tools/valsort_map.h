3048c704d6c7018653b311df71e76f914ce0532d
Charles Ofria <charles.ofria@gmail.com>
2018-04-13 19:02:52 -0400

    Added to valsort_map Get, Set, and various begin/end iterator accessors.

--- source/tools/valsort_map.h
+++ source/tools/valsort_map.h
@@ -33,6 +33,30 @@ public:
   valsort_map(const valsort_map &) = default;
   valsort_map(valsort_map &&) = default;
   ~valsort_map() { ; }
+
+  const mapped_type & Get(key_type key) {
+    return item_map[key];
+  }
+
+  void Set(key_type key, const mapped_type & value) {
+    auto map_it = item_map.find(key);
+    if (map_it != item_map.end()) val_set.remove(*map_it);
+    map_it->second = value;
+    val_set.insert(*map_it);
+  }
+
+  // For now, don't change values using iterators, just look at them.
+  auto cbegin() { return item_map.cbegin(); }
+  auto cend() { return item_map.cend(); }
+  auto crbegin() { return item_map.crbegin(); }
+  auto crend() { return item_map.crend(); }
+
+  // Add iterators that are ordred by value instead of key.
+  auto cvbegin() { return val_set.cbegin(); }
+  auto cvend() { return val_set.cend(); }
+  auto crvbegin() { return val_set.crbegin(); }
+  auto crvend() { return val_set.crend(); }
+
 };
 
 #endif
