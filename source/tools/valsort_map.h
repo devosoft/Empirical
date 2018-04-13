f0ea5852e4fed856df1df5947e42ac78255d5b0c
Charles Ofria <charles.ofria@gmail.com>
2018-04-13 10:42:04 -0400

    Building out internals of valsort_map; internal types and data.

--- source/tools/valsort_map.h
+++ source/tools/valsort_map.h
@@ -15,9 +15,26 @@
 
 #include "../base/vector.h"
 
-template <typename KEY, typename VALUE>
+template < class Key,                                     // map::key_type
+           class T,                                       // map::mapped_type
+           class Compare = less<Key>,                     // map::key_compare
+           class Alloc = allocator<pair<const Key,T> >    // map::allocator_type
+           >
 class valsort_map {
-  
+public: // internal types
+  using key_type = Key;
+  using mapped_type = T;
+  using value_type = pair<const Key,T>;
+  using key_compare = Compare;
+  using allocator_type = Alloc;
+
+  using reference = value_type&;
+  using const_reference = const value_type&;
+private:
+  std::map<Key,T,Compare,Alloc> item_map;
+  std::set< value_type > val_sort;
+
+public:
 };
 
 #endif
