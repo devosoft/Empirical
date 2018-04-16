7058f1fd13d4469e8baae39be0c89925ffe5b0c9
Charles Ofria <charles.ofria@gmail.com>
2018-04-15 23:55:58 -0400

    Removed DiverseElites_Kill for now; it's too slow and awkward.

--- source/Evolve/World_structure.h
+++ source/Evolve/World_structure.h
@@ -174,45 +174,6 @@ namespace emp {
   /// Note: Since organisms compete with their predecessors for space in the populations,
   /// synchronous generations do not make sense.
 
-  /// Function to kill off an organism in DiverseElites to help maintain population size.
-  /// Go one trait at a time and repeatedly use it to cut trait-space into segments, keeping the
-  /// most populated group each time.
-  tempalte <typename ORG>
-  auto DiverseElites_Kill(World<ORG> & world, TraitSet<ORG> traits) {
-    emp::vector<Ptr<ORG>> orgs = world.GetFullPop();
-    constexpr size_t num_groups = 4;
-    emp::array<size_t, num_groups> group_sizes;
-    size_t trait_id = 0;
-
-    // Loop until we've found the most dense portion of the population.
-    while (orgs.size() > num_groups) {
-      // Determine the range on the current trait.
-      Trait<ORG, double> & trait = traits[trait_id];
-      double min_val = trait.Eval(*orgs[0]);
-      double max_val = min_val
-      for (size_t i = 1; i < orgs.size(); i++) {
-        double val = trait.Eval(*orgs[i]);
-        if (val < min_val) min_val = val;
-        if (val > max_val) max_val = val;
-      }
-      
-      // Determine how population divides into groups.
-      double trait_width = (max_val - min_val) * 1.0000001;
-      double group_width = trait_width / (double) num_groups;
-      group_sizes.fill(0);
-      for (size_t i = 0; i < orgs.size(); i++) {
-        double val = trait.Eval(*orgs[i]);
-        size_t group_id = (val - min_val) / group_width;   // @CAO Watch out for zero group_width!
-        group_sizes[group_id]++;
-      }
-
-      // @CAO: Keep only those orgs in the biggest bin and repeat!
-
-      // Shift to use the next trait.
-      if (++trait_id == traits.GetSize()) trait_id = 0;
-    }
-  }
-
   /// This first version will setup a Diverse-Elites world and specify traits to use.
   template <typename ORG>
   void SetDiverseElites(World<ORG> & world, TraitSet<ORG> traits, size_t world_size) { 
