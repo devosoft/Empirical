707f1950accc56dd8ac671ec22c198aedfe12d05
Charles Ofria <charles.ofria@gmail.com>
2018-04-16 23:40:50 -0400

    Added ArmOrg functions for fitness and mutation rate; built ArmWorld constructor and CalcPoints function.

--- apps/MAP-Elites-Arm/source/ArmWorld.h
+++ apps/MAP-Elites-Arm/source/ArmWorld.h
@@ -3,34 +3,66 @@
 
 #include "base/vector.h"
 #include "Evolve/World.h"
-#include "genometry/Angle.h"
-#include "genometry/Point2D.h"
+#include "geometry/Angle2D.h"
+#include "geometry/Point2D.h"
 #include "tools/Random.h"
 
 struct ArmOrg {
   emp::vector<emp::Angle> angles;
+
+  double GetFitness() const {
+    double sqr_diffs = 0.0;  // Total the squares of all of the differences between angles.
+    for (size_t i = 1; i < angles.size(); i++) {
+      double angle_diff = (angles[i] - angles[i-1]).AsPortion();
+      if (angle_diff > 0.5) angle_diff = 1.0 - angle_diff;
+      sqr_diffs += angle_diff * angle_diff;
+    }
+    return ((double) angles.size()) - sqr_diffs;
+  }
+
+  void DoMutations(emp::Random & random) {
+    if (random.P(0.5)) {
+      size_t pos = random.GetUInt(angles.size());
+      angles[pos].SetPortion(random.GetDouble());
+    }
+  }
 };
 
 class ArmWorld : public emp::World<ArmOrg> {
 private:
   emp::vector<double> segments;
-  size_t num_angles;
-
-  emp::Random random;
 public:
-  ArmWorld(emp::vector<double> in_segments)
-    : segments(in_segments), num_angles(segments.size()) {
+  ArmWorld(emp::Random random, emp::vector<double> in_segments)
+    : emp::World<ArmOrg>(random, "ArmWorld"), segments(in_segments)
+  {
+    SetupFitnessFile().SetTimingRepeat(10);
+    SetupSystematicsFile().SetTimingRepeat(10);
+    SetupPopulationFile().SetTimingRepeat(10);
+    SetWellMixed(true);
+    SetCache();
+
+    Inject({2.0,1.0,4.0,1.0,3.0});
   }
   ~ArmWorld() { ; }
 
-  void SetSegments(
+  emp::vector<emp::Point> CalcPoints(const ArmOrg & org) {
+    emp::Angle facing(0);
+    emp::Point position(0.0, 0.0);
+    emp::vector<emp::Point> points(segments.size());
+    for (size_t i = 0; i < segments.size(); i++) {
+      facing += org.angles[i];
+      position = facing.GetPoint(position, segments[i]);
+      points[i] = position;
+    }
+    return points;
+  }
 
   emp::Point CalcEndPoint(const ArmOrg & org) {
     emp::Angle facing(org.angles[0]);
     emp::Point position(facing.GetPoint(segments[0]));
     for (size_t i = 1; i < segments.size(); i++) {
       facing += org.angles[i];
-      position = facing.GetPoint(position, segments[0]);
+      position = facing.GetPoint(position, segments[i]);
     }
     return position;
   }
