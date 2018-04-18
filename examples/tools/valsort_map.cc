26829c72a92d4b1b2e312cdd6921ac1515040bd9
Charles Ofria <charles.ofria@gmail.com>
2018-04-17 22:57:06 -0400

    Expanded valsort_map example.

--- examples/tools/valsort_map.cc
+++ examples/tools/valsort_map.cc
@@ -34,10 +34,18 @@ int main()
   emp::valsort_map<std::string, int> score_map;
   score_map.Set("Tytalus", 11);
   score_map.Set("Tremere", 10);
-  score_map.Set("Bonesagus", 12);
-  score_map.Set("Verditius", 3);
+  score_map.Set("Bonisagus", 12);
+  score_map.Set("Verditius", 7);
   score_map.Set("Mercere", 8);
-  score_map.Set("ExMisc", 7);
+  score_map.Set("ExMisc", 3);
+  score_map.Set("Criamon", -608);
+  score_map.Set("Diedne", 4);
+  score_map.Set("Flambeau", 5);
+  score_map.Set("Jerbiton", 8);
+  score_map.Set("Merinita", 4);
+  score_map.Set("Quaesitor", 0);
+
+  
 
   std::cout << "\n\nBY NAME:\n";
   for (auto it = score_map.cbegin(); it != score_map.cend(); it++) {
