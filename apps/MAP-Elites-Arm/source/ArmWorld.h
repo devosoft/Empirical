c04042e492145bc5a9f066b26ef72082710828f9
Charles Ofria <charles.ofria@gmail.com>
2018-04-21 23:23:43 -0400

    Reset end_point when mutations occur.

--- apps/MAP-Elites-Arm/source/ArmWorld.h
+++ apps/MAP-Elites-Arm/source/ArmWorld.h
@@ -33,6 +33,7 @@ struct ArmOrg {
     if (random.P(0.5)) {
       size_t pos = random.GetUInt(angles.size());
       angles[pos].SetPortion(random.GetDouble());
+      end_point.ToOrigin();
       return 1;
     }
     return 0;
