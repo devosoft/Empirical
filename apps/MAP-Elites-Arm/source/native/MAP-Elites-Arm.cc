e13c72364d3b667890e83e6abbc7ecd3630d1104
Charles Ofria <charles.ofria@gmail.com>
2018-04-21 23:08:38 -0400

    Setup print function and added PrintGrid() in MAP-ElitesArm.cc

--- apps/MAP-Elites-Arm/source/native/MAP-Elites-Arm.cc
+++ apps/MAP-Elites-Arm/source/native/MAP-Elites-Arm.cc
@@ -18,14 +18,25 @@ int main(int argc, char* argv[])
 
   emp::Random random;
   ArmWorld world(random);
-  world.SetMutateBeforeBirth();
+
+  // Setup the print function to indicate if a function exists.
+  std::function<void(ArmOrg&,std::ostream &)> print_fun = [](ArmOrg & val, std::ostream & os) {
+    std::string out_str = "!"; // emp::to_string(val);
+    // while (out_str.size() < 4) out_str = emp::to_string('!');
+    os << out_str;
+  };
+  world.SetPrintFun(print_fun);
+
 
   // Loop through updates
   for (size_t ud = 0; ud < MAX_GENS; ud++) {
     // Print current state.
-    std::cout << "UD " << ud << ": " << world[0].ToString()
-              << "  pop_size = " << world.GetSize()
-              << std::endl;
+    std::cout << "UD " << ud << std::endl;
+    
+    //  << ": " << world[0].ToString()
+    //           << "  pop_size = " << world.GetSize()
+    //           << std::endl;
+    world.PrintGrid();
 
     // Keep the best individual.
     emp::EliteSelect(world, 1, 1);
@@ -34,5 +45,8 @@ int main(int argc, char* argv[])
     TournamentSelect(world, 5, POP_SIZE-1);
     world.Update();
   }
+
+  std::cout << "\nFINAL\n";
+  world.PrintGrid();
 
 }
