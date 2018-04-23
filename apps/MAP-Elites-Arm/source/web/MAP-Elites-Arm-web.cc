40dfe010b574c7c1db8a1d92902d86a634b00c7e
Charles Ofria <charles.ofria@gmail.com>
2018-04-22 20:22:01 -0400

    Setup colorful organisms and a cross-bar in MAP-Elites app.

--- apps/MAP-Elites-Arm/source/web/MAP-Elites-Arm-web.cc
+++ apps/MAP-Elites-Arm/source/web/MAP-Elites-Arm-web.cc
@@ -16,6 +16,7 @@ namespace UI = emp::web;
 UI::Document doc("emp_base");
 ArmWorld world;
 const double world_size = 600;
+size_t target_id = 0;
 
 void DrawWorldCanvas() {
   UI::Canvas canvas = doc.Canvas("world_canvas");
@@ -26,27 +27,42 @@ void DrawWorldCanvas() {
   const double canvas_x = (double) canvas.GetWidth();
   const double canvas_y = (double) canvas.GetHeight();
 
-  const auto & pop = world.GetFullPop();
-  (void) pop;
-
   const double org_x = canvas_x / (double) world_x;
   const double org_y = canvas_y / (double) world_y;
   const double org_r = emp::Min(org_x, org_y) / 2.0;
 
-  for (size_t x = 0; x < world_x; x++) {
-    for (size_t y = 0; y < world_y; y++) {
+  for (size_t y = 0; y < world_y; y++) {
+    for (size_t x = 0; x < world_x; x++) {
       const size_t org_id = y * world_x + x;
       const size_t cur_x = org_x * (0.5 + (double) x);
       const size_t cur_y = org_y * (0.5 + (double) y);
       const double fitness = world.CalcFitnessID(org_id);
       if (fitness == 0.0) {
         canvas.Circle(cur_x, cur_y, org_r, "black");
-      } else {
+      } else if (fitness < 0.6) {
         canvas.Circle(cur_x, cur_y, org_r, "pink");
-        
+      } else if (fitness < 0.8) {
+        canvas.Circle(cur_x, cur_y, org_r, "#EEEE33");  // Pale Yellow
+      } else if (fitness < 0.95) {
+        canvas.Circle(cur_x, cur_y, org_r, "#88FF88");  // Pale green
+      } else {
+        canvas.Circle(cur_x, cur_y, org_r, "green");
       }
+
+      if (!target_id && fitness > 0.0) target_id = org_id;
     }
   }
+
+  // Setup the arm.
+  const std::string arm_color = "#8888FF";
+
+  // Add a plus sign in the middle.
+  const double mid_x = org_x * world_x / 2.0;
+  const double mid_y = org_y * world_y / 2.0;
+  const double plus_bar = org_r * world_x;
+  canvas.Line(mid_x, mid_y-plus_bar, mid_x, mid_y+plus_bar, arm_color);
+  canvas.Line(mid_x-plus_bar, mid_y, mid_x+plus_bar, mid_y, arm_color);
+
 
   // for (const Org & org : pop) {
   //   if (org.coop) {
