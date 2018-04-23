070caa78ceb2eeb7685cd8e42c74eed4ee6c1f13
Charles Ofria <charles.ofria@gmail.com>
2018-04-23 12:25:39 -0400

    Allow CanvasShape to be positioned by a Point rather than separate x-y cooedinates.

--- source/web/CanvasShape.h
+++ source/web/CanvasShape.h
@@ -30,18 +30,23 @@ namespace web {
   /// Define an arbitrary shape to draw on a canvas (base clase)
   class CanvasShape : public CanvasAction {
   protected:
-    double x; double y;      ///< Anchor point for this shape.
+    Point p;                 ///< Anchor point for this shape.
     std::string fill_color;  ///< Internal color to fill shape with.
     std::string line_color;  ///< Border color for shape.
     double line_width;       ///< How wide should lines be?
 
   public:
     CanvasShape(double _x, double _y, const std::string & fc="", const std::string & lc="", double lw=1.0)
-      : x(_x), y(_y), fill_color(fc), line_color(lc), line_width(lw) { ; }
+      : p(_x,_y), fill_color(fc), line_color(lc), line_width(lw) { ; }
+    CanvasShape(Point _p, const std::string & fc="", const std::string & lc="", double lw=1.0)
+      : p(_p), fill_color(fc), line_color(lc), line_width(lw) { ; }
     virtual ~CanvasShape() { ; }
 
-    /// Shift the position of this shape.
-    void MoveTo(double _x, double _y) { x=_x; y=_y; }
+    /// Shift the position of this shape to a point.
+    void MoveTo(Point _p) { p  = _p; }
+
+    /// Shift the position of this shape to coordinates.
+    void MoveTo(double _x, double _y) { p.Set(_x,_y); }
 
     /// Setup details needed before drawing lines.
     void SetLineWidth(double lw = 1.0) { line_width = lw; }
@@ -65,18 +70,21 @@ namespace web {
     double radius;  ///< Circle radius
   public:
     CanvasCircle(double _x, double _y, double _r,
-                 const std::string & fc="", const std::string & lc="")
-      : CanvasShape(_x, _y, fc, lc), radius(_r) { ; }
+                 const std::string & fc="", const std::string & lc="", double lw=1.0)
+      : CanvasShape(_x, _y, fc, lc, lw), radius(_r) { ; }
 
-    CanvasCircle(emp::Circle circle, const std::string & fc="", const std::string & lc="")
-      : CanvasShape(circle.GetCenterX(), circle.GetCenterY(), fc, lc)
+    CanvasCircle(Point _p, double _r, const std::string & fc="", const std::string & lc="", double lw=1.0)
+      : CanvasShape(_p, fc, lc, lw), radius(_r) { ; }
+
+    CanvasCircle(emp::Circle circle, const std::string & fc="", const std::string & lc="", double lw=1.0)
+      : CanvasShape(circle.GetCenterX(), circle.GetCenterY(), fc, lc, lw)
       , radius(circle.GetRadius()) { ; }
 
     void Apply() {
       EM_ASM_ARGS({
           emp_i.ctx.beginPath();
           emp_i.ctx.arc($0, $1, $2, 0, Math.PI*2);
-        }, x, y, radius);  // Draw the circle
+        }, p.GetX(), p.GetY(), radius);  // Draw the circle
         ApplyColor();
     }
     CanvasAction * Clone() const { return new CanvasCircle(*this); }
@@ -95,7 +103,7 @@ namespace web {
       EM_ASM_ARGS({
           emp_i.ctx.beginPath();
           emp_i.ctx.rect($0, $1, $2, $3);
-        }, x, y, w, h);  // Draw the rectangle
+        }, p.GetX(), p.GetY(), w, h);  // Draw the rectangle
       ApplyColor();
     }
     CanvasAction * Clone() const { return new CanvasRect(*this); }
@@ -112,7 +120,7 @@ namespace web {
     void Apply() {
       EM_ASM_ARGS({
           emp_i.ctx.clearRect($0, $1, $2, $3);
-        }, x, y, w, h);  // Draw the rectangle
+        }, p.GetX(), p.GetY(), w, h);  // Draw the rectangle
     }
     CanvasAction * Clone() const { return new CanvasClearRect(*this); }
   };
@@ -137,7 +145,7 @@ namespace web {
         emp_i.ctx.translate($0,$1);
         emp_i.ctx.beginPath();
         emp_i.ctx.moveTo($2, $3);
-      }, x, y, points[0].GetX(), points[0].GetY());  // Setup the polygon
+      }, p.GetX(), p.GetY(), points[0].GetX(), points[0].GetY());  // Setup the polygon
       for (size_t i = 1; i < points.size(); i++) {
         EM_ASM_ARGS({
           emp_i.ctx.lineTo($0, $1);
@@ -146,7 +154,7 @@ namespace web {
       EM_ASM_ARGS({
         emp_i.ctx.closePath();
         emp_i.ctx.translate($0,$1);
-      }, -x, -y);  // Close the polygon
+      }, -p.GetX(), -p.GetY());  // Close the polygon
       ApplyColor();
     }
     CanvasAction * Clone() const { return new CanvasPolygon(*this); }
@@ -169,8 +177,8 @@ namespace web {
         emp_i.ctx.beginPath();
         emp_i.ctx.moveTo($0, $1);
         emp_i.ctx.lineTo($2, $3);
-        emp_i.ctx.closePath();
-      }, x, y, x2, y2);
+//        emp_i.ctx.closePath();
+      }, p.GetX(), p.GetY(), x2, y2);
       // ApplyColor();
       LineWidth(line_width);
       Stroke(line_color);
@@ -197,7 +205,7 @@ namespace web {
       EM_ASM_ARGS({
         emp_i.ctx.beginPath();
         emp_i.ctx.moveTo($0, $1);
-      }, x, y);
+      }, p.GetX(), p.GetY());
       // Loop through all internal points...
       for (auto p : points) {
         EM_ASM_ARGS({ emp_i.ctx.lineTo($0, $1); }, p.GetX(), p.GetY());
@@ -228,7 +236,7 @@ namespace web {
         emp_i.ctx.fillStyle = Pointer_stringify($3);
         var text = Pointer_stringify($2);
         emp_i.ctx.fillText(text,$0,$1);
-      }, x, y, text.c_str(), fill_color.c_str());
+      }, p.GetX(), p.GetY(), text.c_str(), fill_color.c_str());
     }
 
     /// Center this text.
