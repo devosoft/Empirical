336fb0249e6280876b75738a774b9d94b28135a1
Charles Ofria <charles.ofria@gmail.com>
2018-05-01 16:50:18 -0400

    Setup TraitSet::CalcOffsets() To be const.

--- source/data/Trait.h
+++ source/data/Trait.h
@@ -174,7 +174,7 @@ namespace emp {
       return id;
     }
 
-    emp::vector<double> CalcOffsets(target_t & target1, target_t & target2) {
+    emp::vector<double> CalcOffsets(target_t & target1, target_t & target2) const {
       emp::vector<double> offsets(traits.size());
       for (size_t i = 0; i < traits.size(); i++) {
         offsets[i] = traits[i]->EvalValue(target2) - traits[i]->EvalValue(target1);
