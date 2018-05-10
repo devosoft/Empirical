1ae84988882eb3f6ad26689cae39401c230bf33e
Charles Ofria <charles.ofria@gmail.com>
2018-05-10 11:30:02 -0400

    Added example of MinIndex to vector_utils.cc

--- examples/tools/vector_utils.cc
+++ examples/tools/vector_utils.cc
@@ -12,20 +12,26 @@
 int main()
 {
 
-  emp::vector<int> v = { 1, 2, 3, 4, 22, 5, 6, 7, 8, 9, 10, 12 };
+  emp::vector<int> v = { 14, 13, 1, 2, 3, 4, 22, 5, 6, 7, 8, 9, 10, 12 };
+
   
   emp::Print(v);  std::cout << std::endl;
 
+  std::cout << "Min index = " << emp::MinIndex(v) << std::endl;
+
   emp::Heapify(v);
 
+  std::cout << "\nPost Heapify:\n";
   emp::Print(v);  std::cout << std::endl;
 
   int result = emp::HeapExtract(v);
-  std::cout << "Max = " << result << std::endl;
+  std::cout << "Max value = " << result << std::endl;
 
+  std::cout << "\nPost Extract-Max:\n";
   emp::Print(v);  std::cout << std::endl;
   
   emp::HeapInsert(v, 11);
 
+  std::cout << "\nPost Insert(11):\n";
   emp::Print(v);  std::cout << std::endl;
 }
