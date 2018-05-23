59e4fcde52fe354bd77d65ccd4e3e8c3efb0c79e
Charles Ofria <charles.ofria@gmail.com>
2018-05-22 23:59:32 -0400

    Setup AagosWorld in native mode.

--- apps/Aagos/source/native/Aagos.cc
+++ apps/Aagos/source/native/Aagos.cc
@@ -3,8 +3,13 @@
 #include "base/vector.h"
 #include "config/command_line.h"
 
+#include "../AagosOrg.h"
+#include "../AagosWorld.h"
+
 int main(int argc, char* argv[])
 {
+  AagosWorld world;
+
   emp::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
 
   std::cout << "Hello World!" << std::endl;
