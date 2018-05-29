3b705ddeff2173c351e71217acd5e375228298d4
Charles Ofria <charles.ofria@gmail.com>
2018-05-29 15:07:40 -0400

    Hooked in ImageManager to run RawImage.

--- source/web/RawImage.h
+++ source/web/RawImage.h
@@ -27,7 +27,7 @@ namespace emp {
 
   namespace internal {
     /// Detailed information about an image
-    struct Image_Info {
+    struct ImageInfo {
       int img_id;                    ///< Unique ID for this image.
       std::string filename;          ///< Full URL of file containing image.
       mutable bool has_loaded;       ///< Is this image finished loading?
@@ -35,11 +35,11 @@ namespace emp {
       Signal<void()> on_load;        ///< Actions for when image is finished loading.
       Signal<void()> on_error;       ///< Actions for when image has trouble loading.
 
-      Image_Info(const std::string & _filename)
+      ImageInfo(const std::string & _filename)
         : img_id(-1), filename(_filename), has_loaded(false), has_error(false), on_load(), on_error()
       {
-        size_t loaded_callback = JSWrapOnce( std::function<void()>(std::bind(&Image_Info::MarkLoaded, this)) );
-        size_t error_callback = JSWrapOnce( std::function<void()>(std::bind(&Image_Info::MarkError, this)) );
+        size_t loaded_callback = JSWrapOnce( std::function<void()>(std::bind(&ImageInfo::MarkLoaded, this)) );
+        size_t error_callback = JSWrapOnce( std::function<void()>(std::bind(&ImageInfo::MarkError, this)) );
 
         img_id = EM_ASM_INT({
           var file = Pointer_stringify($0);
@@ -87,14 +87,14 @@ namespace emp {
       }
     };
 
-    class Image_Manager {
+    class ImageManager {
     private:
-      emp::vector<Ptr<Image_Info>> image_info;            ///< Information about each loaded image.
+      emp::vector<Ptr<ImageInfo>> image_info;            ///< Information about each loaded image.
       std::map<std::string, size_t> image_id_map;  ///< Map of filenames to loaded image ids.
 
     public:
-      Image_Manager() : image_info(0), image_id_map() { ; }
-      ~Image_Manager() {
+      ImageManager() : image_info(0), image_id_map() { ; }
+      ~ImageManager() {
         for (auto ptr : image_info) ptr.Delete();
       }
 
@@ -102,129 +102,55 @@ namespace emp {
       bool Has(const std::string & filename) { return emp::Has(image_id_map, filename); }
 
       /// Create a new image with the provided name.
-      Image_Info & Add(const std::string & filename) {
+      Ptr<ImageInfo> Add(const std::string & filename) {
         emp_assert(Has(image_id_map, filename) == false);
         size_t img_id = image_info.size();
-        Ptr<Image_Info> new_info = NewPtr<Image_Info>(filename);
+        Ptr<ImageInfo> new_info = NewPtr<ImageInfo>(filename);
         image_info.push_back(new_info);
         image_id_map[filename] = img_id;
-        return *image_info[img_id];
+        return image_info[img_id];
       }
 
       /// Get the info about a specified image (loading it only if needed!)
-      Image_Info & GetInfo(const std::string & filename) {
-        if (Has(filename)) return *image_info[ image_id_map[filename] ];
+      Ptr<ImageInfo> GetInfo(const std::string & filename) {
+        if (Has(filename)) return image_info[ image_id_map[filename] ];
         return Add(filename);
       }
     };
-  }
+
+  } // End internal namespace
 
   /// Fundamental information about a single image.
   class RawImage {
   private:
-    std::string filename;                             ///< Name of the file image was loaded from.
-    int img_id;                                       ///< Unique ID for this image.
-    mutable bool has_loaded;                          ///< Is this image finished loading?
-    mutable bool has_error;                           ///< Were there any errors in loading image?
-    mutable emp::vector<uint32_t> callbacks_on_load;  ///< Callbacks to be done when image loaded.
-    mutable emp::vector<uint32_t> callbacks_on_error; ///< Callbacks to be done if load error.
+    Ptr<internal::ImageInfo> info;
 
-    uint32_t loaded_callback;                         ///< Internal callback when image loaded.
-    uint32_t error_callback;                          ///< Internal callback when image error.
-  public:
-    RawImage(const std::string & _filename)
-      : filename(_filename), has_loaded(false), has_error(false)
-    {
-      size_t loaded_callback = JSWrapOnce( std::function<void()>(std::bind(&RawImage::MarkLoaded, this)) );
-      size_t error_callback = JSWrapOnce( std::function<void()>(std::bind(&RawImage::MarkError, this)) );
-
-      img_id = EM_ASM_INT({
-        var file = Pointer_stringify($0);
-        var img_id = emp_info.images.length;
-        emp_info.images[img_id] = new Image();
-        emp_info.images[img_id].src = file;
-
-        emp_info.images[img_id].onload = function() {
-            emp_info.image_load_count += 1;
-            emp.Callback($1);
-        };
-
-        emp_info.images[img_id].onerror = function() {
-            emp_info.image_error_count += 1;
-            emp.Callback($2);
-        };
-
-        return img_id;
-      }, filename.c_str(), loaded_callback, error_callback);
-
+    static internal::ImageManager & GetManager() {
+      static internal::ImageManager manager;
+      return manager;
     }
+  public:
+    RawImage(const std::string & filename) : info(GetManager().GetInfo(filename)) { ; }
     ~RawImage() { ; }
 
-    const std::string & GetFilename() const { return filename; }
-    int GetImgID() const { return img_id; }
-    bool HasLoaded() const { return has_loaded; }
-    bool HasError() const { return has_error; }
-
-    /// Trigger this image as loaded.
-    void MarkLoaded() {
-      has_loaded = true;
-      for (uint32_t id : callbacks_on_load) empCppCallback(id);
-      callbacks_on_load.resize(0);
-    }
-
-    /// Trigger this image as having an error.
-    void MarkError() {
-      has_error = true;
-      emp::Alert(std::string("Error loading image: ") + filename);
-
-      for (uint32_t id : callbacks_on_error) empCppCallback(id);
-      callbacks_on_error.resize(0);
-    }
+    int GetImgID() const { return info->img_id; }
+    const std::string & GetFilename() const { return info->filename; }
+    bool HasLoaded() const { return info->has_loaded; }
+    bool HasError() const { return info->has_error; }
 
     /// Add a new function to be called when the image finishes loading.
-    void AddLoadCallback(const std::function<void()> & callback_fun) {
-      callbacks_on_load.push_back( JSWrapOnce(callback_fun) );
+    void OnLoad(const std::function<void()> & callback_fun) {
+      if (HasLoaded()) callback_fun();
+      else info->on_load.AddAction(callback_fun);
     }
 
     /// Add a new function to be called if an image load has an error.
-    void AddErrorCallback(const std::function<void()> & callback_fun) {
-      callbacks_on_error.push_back( JSWrapOnce(callback_fun) );
+    void OnError(const std::function<void()> & callback_fun) {
+      if (HasError()) callback_fun();
+      else info->on_error.AddAction(callback_fun);
     }
   };
 
-  namespace internal {
-    static std::map<std::string, RawImage *> & RawImageMap() {
-      static std::map<std::string, RawImage *> raw_image_map;
-      return raw_image_map;
-    }
-  }
-
-  /// Initiate the loading of a new image.
-  RawImage & LoadRawImage(const std::string & filename,
-                          const std::function<void()> & load_callback=NULL,
-                          const std::function<void()> & error_callback=NULL)
-  {
-    auto & raw_image_map = internal::RawImageMap();
-    Ptr<RawImage> raw_image;
-    if (!Has(raw_image_map, filename)) {        // New filename
-      raw_image = new RawImage(filename);
-      raw_image_map[filename] = raw_image;
-    }
-    else raw_image = raw_image_map[filename];   // Pre-existing filename
-
-    if (load_callback) {
-      if (raw_image->HasLoaded()) load_callback();
-      else raw_image->AddLoadCallback(load_callback);
-    }
-
-    if (error_callback) {
-      if (raw_image->HasError()) error_callback();
-      else raw_image->AddErrorCallback(error_callback);
-    }
-
-    return *raw_image;
-  }
-
-};
+}
 
 #endif
