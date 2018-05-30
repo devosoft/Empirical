ab217f3f70c57a31d492227a9f417e356c2025db
Charles Ofria <charles.ofria@gmail.com>
2018-05-29 21:26:29 -0400

    Cleaned up CanvasImage action.

--- source/web/CanvasAction.h
+++ source/web/CanvasAction.h
@@ -126,19 +126,21 @@ namespace web {
   public:
     CanvasImage(const RawImage & raw_image, double _x=0.0, double _y=0.0, double _w=0.0, double _h=0.0)
       : image(raw_image), x(_x), y(_y), width(_w), height(_h) { ; }
-    CanvasImage(const std::string & filename, double _x=0.0, double _y=0.0, double _w=0.0, double _h=0.0)
-      : image(filename), x(_x), y(_y), width(_w), height(_h) { ; }
+    CanvasImage(const std::string & url, double _x=0.0, double _y=0.0, double _w=0.0, double _h=0.0)
+      : image(url), x(_x), y(_y), width(_w), height(_h) { ; }
 
     void Apply() {
-      if (width == 0.0) {
-        EM_ASM_ARGS({
-          emp_i.ctx.drawImage(emp_info.images[$0], $1, $2);
-        }, image.GetID(), x, y);
-      } else {
-        EM_ASM_ARGS({
-          emp_i.ctx.drawImage(emp_info.images[$0], $1, $2, $3, $4);
-        }, image.GetID(), x, y, width, height);
-      }
+      image.OnLoad([this](){
+        if (width == 0.0) {
+          EM_ASM_ARGS({
+            emp_i.ctx.drawImage(emp_i.images[$0], $1, $2);
+          }, image.GetID(), x, y);
+        } else {
+          EM_ASM_ARGS({
+            emp_i.ctx.drawImage(emp_i.images[$0], $1, $2, $3, $4);
+          }, image.GetID(), x, y, width, height);
+        }
+      });
     }
     
     CanvasAction * Clone() const { return new CanvasImage(*this); }
