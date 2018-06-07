c19c1670ffd6f30f47813c44c5b840299c12e072
Charles Ofria <charles.ofria@gmail.com>
2018-06-07 10:29:55 -0400

    Cleaned up buttons for OpenWorld web interface.

--- apps/OpenWorld/source/web/OpenWorld-web.cc
+++ apps/OpenWorld/source/web/OpenWorld-web.cc
@@ -21,9 +21,38 @@ void Animate(const UI::Animate & anim) {
             emp::GetHueMap(360));
 }
 
+void DoStart() {
+  anim.ToggleActive();
+  auto start_but = doc.Button("start_but");
+  auto step_but = doc.Button("step_but");
+
+  if (anim.GetActive()) {
+    start_but.SetLabel("Stop");    // If animation is running, button should read "Stop"
+    step_but.SetDisabled(true);    // Cannot step animation already running.
+  }
+  else {
+    start_but.SetLabel("Start");     // If animation is stopped, button should read "Start"
+    step_but.SetDisabled(false);    // Can step stopped animation.
+  }
+}
+
+void DoStep() {
+  emp_assert(anim.GetActive() == false); // Step is only meaningful if the run is stopped.
+  Animate(anim);
+}
+
+void DoReset() {
+  world.Reset();
+
+  // Redraw the world.
+  UI::Draw( doc.Canvas("pop_view"),
+            world.GetSurface(),
+            emp::GetHueMap(360));
+}
+
 int main()
 {
-  config.Read("OpenWorld.cfg");
+  config.Read("OpenWorld.cfg", false);
 
   doc << "<h1>Hello, Open World!</h1>";
 
@@ -41,7 +70,7 @@ int main()
   // Add buttons.
   auto control_set = doc.AddDiv("buttons");
   control_set.SetPosition(10, 70+config.WORLD_Y());
-  control_set << UI::Button([](){ anim.Start(); }, "Start", "start_but");
-  control_set << UI::Button([](){ Animate(anim); }, "Step", "step_but");
-  control_set << UI::Button([](){emp::Alert("Pressed Button!");}, "Reset", "reset_but");
+  control_set << UI::Button(DoStart, "Start", "start_but");
+  control_set << UI::Button(DoStep, "Step", "step_but");
+  control_set << UI::Button(DoReset, "Reset", "reset_but");
 }
