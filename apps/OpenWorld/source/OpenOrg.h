bd71d71b9a682a20f4c2a4bd26e599b3adb4c128
Charles Ofria <charles.ofria@gmail.com>
2018-06-08 09:54:34 -0400

    Start OpenOrgs with a random facing; calc mass and other info.

--- apps/OpenWorld/source/OpenOrg.h
+++ apps/OpenWorld/source/OpenOrg.h
@@ -21,15 +21,19 @@ struct OpenOrg {
   hardware_t brain;
 
   double energy;
-  double size;
+  double radius;
   double time_born;
   double time_dead;
 
   emp::CircleBody2D body;
 
+  /// Values calculated from the above:
+  double mass;         /// Total size if organism (and cost of offspring!)
+  double basic_cost;   /// How much energy is spent each update?
+
   OpenOrg(inst_lib_t & inst_lib, event_lib_t & event_lib, emp::Ptr<emp::Random> random_ptr)
-    : id(0), brain(inst_lib, event_lib, random_ptr), energy(0.0), size(0.0)
-	  , time_born(0.0), time_dead(-1.0), body({100.0,100.0,5.0})
+    : id(0), brain(inst_lib, event_lib, random_ptr), energy(0.0), radius(5.0)
+	  , time_born(0.0), time_dead(-1.0), body({100.0,100.0,radius})
   { ; }
   OpenOrg(const OpenOrg &) = default;
   OpenOrg(OpenOrg &&) = default;
@@ -38,9 +42,13 @@ struct OpenOrg {
   OpenOrg & operator=(const OpenOrg &) = default;
   OpenOrg & operator=(OpenOrg &&) = default;
 
-  void Setup(emp::WorldPosition pos) {
+  void Setup(emp::WorldPosition pos, emp::Random & random) {
     // std::cerr << "Seting up at pos " << pos.GetIndex() << std::endl;
+    mass = radius * radius;
+    basic_cost = mass * 0.01;
+    body.RotateDegrees(random.GetDouble(360.0));
   }
+
 
   void Process(size_t exe_count) {
     brain.Process(exe_count);
