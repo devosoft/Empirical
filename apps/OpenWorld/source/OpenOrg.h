01cb64c6813c2daeb39444c94889bc3f8fe46ef2
Charles Ofria <charles.ofria@gmail.com>
2018-06-09 09:51:14 -0400

    Changed OpenOrg to use CircleBody2D as a base class; updated variable usage accordingly.

--- apps/OpenWorld/source/OpenOrg.h
+++ apps/OpenWorld/source/OpenOrg.h
@@ -6,8 +6,9 @@
 #include "geometry/Point2D.h"
 #include "hardware/EventDrivenGP.h"
 
-struct OpenOrg {
+struct OpenOrg : public emp::CircleBody2D {
   static constexpr size_t TAG_WIDTH = 16;
+  using base_t = emp::CircleBody2D;
   using hardware_t = emp::EventDrivenGP_AW<TAG_WIDTH>;
   using event_lib_t = hardware_t::event_lib_t;
   using inst_t = hardware_t::inst_t;
@@ -21,19 +22,16 @@ struct OpenOrg {
   hardware_t brain;
 
   double energy;
-  double radius;
   double time_born;
   double time_dead;
 
-  emp::CircleBody2D body;
-
   /// Values calculated from the above:
-  double mass;         /// Total size if organism (and cost of offspring!)
   double basic_cost;   /// How much energy is spent each update?
 
   OpenOrg(inst_lib_t & inst_lib, event_lib_t & event_lib, emp::Ptr<emp::Random> random_ptr)
-    : id(0), brain(inst_lib, event_lib, random_ptr), energy(0.0), radius(5.0)
-	  , time_born(0.0), time_dead(-1.0), body({100.0,100.0,radius})
+    : base_t({100.0,100.0,5.0})
+    , id(0), brain(inst_lib, event_lib, random_ptr), energy(0.0)
+	  , time_born(0.0), time_dead(-1.0)
   { ; }
   OpenOrg(const OpenOrg &) = default;
   OpenOrg(OpenOrg &&) = default;
@@ -44,9 +42,9 @@ struct OpenOrg {
 
   void Setup(emp::WorldPosition pos, emp::Random & random) {
     // std::cerr << "Seting up at pos " << pos.GetIndex() << std::endl;
-    mass = radius * radius;
-    basic_cost = mass * 0.01;
-    body.RotateDegrees(random.GetDouble(360.0));
+    SetMass(GetRadius() * GetRadius());
+    basic_cost = GetMass() * 0.01;
+    RotateDegrees(random.GetDouble(360.0));
   }
 
 
