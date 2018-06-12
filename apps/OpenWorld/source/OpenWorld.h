9691c0f2306d306709a89021ae67d0700e5260ac
Charles Ofria <charles.ofria@gmail.com>
2018-06-12 16:28:39 -0400

    Switched OpenWorld to use Surface.h instead of Surface2D.h.

--- apps/OpenWorld/source/OpenWorld.h
+++ apps/OpenWorld/source/OpenWorld.h
@@ -4,7 +4,7 @@
 #define OPEN_WORLD_H
 
 #include "Evolve/World.h"
-#include "geometry/Surface2D.h"
+#include "geometry/Surface.h"
 
 #include "config.h"
 #include "OpenOrg.h"
@@ -12,7 +12,7 @@
 class OpenWorld : public emp::World<OpenOrg> {
 private:
   static constexpr size_t TAG_WIDTH = 16;
-  using hardware_t = emp::EventDrivenGP_AW<TAG_WIDTH>;
+  using hardware_t = OpenOrg::hardware_t;
   using program_t = hardware_t::Program;
   using prog_fun_t = hardware_t::Function;
   using prog_tag_t = hardware_t::affinity_t;
@@ -21,7 +21,7 @@ private:
   using inst_lib_t = hardware_t::inst_lib_t;
   using hw_state_t = hardware_t::State;
  
-  using surface_t = emp::Surface2D<OpenOrg>;
+  using surface_t = emp::Surface<OpenOrg>;
 
   OpenWorldConfig & config;
   inst_lib_t inst_lib;
@@ -37,7 +37,7 @@ private:
 public:  
   OpenWorld(OpenWorldConfig & _config)
     : config(_config), inst_lib(), event_lib(),
-      surface(config.WORLD_X(), config.WORLD_Y()),
+      surface({config.WORLD_X(), config.WORLD_Y()}),
       next_id(1), id_map()
   {
     SetPopStruct_Grow(false); // Don't automatically delete organism when new ones are born.
@@ -45,16 +45,16 @@ public:
     // Make sure that we are tracking organisms by their IDs.
     OnPlacement( [this](size_t pos){
       size_t id = next_id++;
-      GetOrg(pos).brain.SetTrait((size_t)OpenOrg::Trait::ORG_ID, id);
+      GetOrg(pos).GetBrain().SetTrait((size_t)OpenOrg::Trait::ORG_ID, id);
       id_map[id] = &GetOrg(pos);
     });
-    OnOrgDeath( [this](size_t pos){ id_map.erase( GetOrg(pos).id ); } );
+    OnOrgDeath( [this](size_t pos){ id_map.erase( GetOrg(pos).GetID() ); } );
 
     // Setup new instructions for the instruction set.
     inst_lib.AddInst("Vroom", [this](hardware_t & hw, const inst_t & inst) {
       const size_t id = (size_t) hw.GetTrait((size_t) OpenOrg::Trait::ORG_ID);
       emp::Ptr<OpenOrg> org_ptr = id_map[id];
-      emp::Angle facing = org_ptr->GetOrientation();
+      emp::Angle facing = org_ptr->GetFacing();
       org_ptr->Translate( facing.GetPoint(1.0) );
     }, 1, "Move forward.");
 
@@ -74,13 +74,13 @@ public:
     OnUpdate([this](size_t){
       Process(5);
       for (auto & org : *this) {
-        double x = org.GetPosition().GetX();
-        double y = org.GetPosition().GetY();
+        double x = org.GetCenter().GetX();
+        double y = org.GetCenter().GetY();
         if (x < 0.0) x += config.WORLD_X();
         if (y < 0.0) y += config.WORLD_Y();
         if (x >= config.WORLD_X()) x -= config.WORLD_X();
         if (y >= config.WORLD_Y()) y -= config.WORLD_Y();
-        org.SetPosition({x,y});
+        org.SetCenter({x,y});
       }
     });
 
@@ -95,9 +95,9 @@ public:
     for (size_t i = 0; i < config.INIT_POP_SIZE(); i++) {
       double x = random_ptr->GetDouble(config.WORLD_X());
       double y = random_ptr->GetDouble(config.WORLD_Y());
-      GetOrg(i).SetPosition({x,y});
+      GetOrg(i).SetCenter({x,y});
       surface.AddBody(&GetOrg(i));
-      GetOrg(i).brain.SetProgram(GenerateRandomProgram());
+      GetOrg(i).GetBrain().SetProgram(GenerateRandomProgram());
     }
   }
   ~OpenWorld() { ; }
