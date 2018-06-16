9af75553bd407ff1c27e1dfe53dc0caa04c5a176
Emily Dolson <emilyldolson@gmail.com>
2018-06-16 03:30:12 -0400

    Switch to backticks

--- tests/convert_for_tests.sh
+++ tests/convert_for_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp -r ../source ../coverage_source
-for filename in `find ../coverage_source -name "*.h" ! -name "_*" ! -path "*/OLD/*" ! -path "*/in_progress/*" ! -name "*template_instantiations.h"`
+for filename in $(find ../coverage_source -name "*.h" ! -name "_*" ! -path "*/OLD/*" ! -path "*/in_progress/*" ! -name "*template_instantiations.h")
 do
     echo $filename
     ../third-party/force-cover/force_cover $filename -- -I. -I../coverage_source --language c++ -std=c++14 -DEMP_TRACK_MEM | xargs -0 echo > $filename.temp
