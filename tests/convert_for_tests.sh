910116c91fd0960e5f422fd3a1931209ed103c3c
Emily Dolson <emilyldolson@gmail.com>
2018-06-16 22:57:53 -0400

    Update convert_for_tests.sh

--- tests/convert_for_tests.sh
+++ tests/convert_for_tests.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+which clang
+ls /usr/lib
+
 cp -r ../source ../coverage_source
 for filename in $(find ../coverage_source -name "*.h" ! -name "_*" ! -path "*/OLD/*" ! -path "*/in_progress/*" ! -name "*template_instantiations.h")
 do
     echo $filename
-    ../third-party/force-cover/force_cover $filename -- -I. -I../coverage_source --language c++ -std=c++14 -DEMP_TRACK_MEM -stdlib=libc++ | xargs -0 echo > $filename.temp
+    ../third-party/force-cover/force_cover $filename -- -I. -I../coverage_source --language c++ -std=c++14 -DEMP_TRACK_MEM -I/usr/lib/clang/*/include | xargs -0 echo > $filename.temp
     mv $filename.temp $filename
 
 done
