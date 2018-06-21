e7dfa8e8f4396fc5139a5257f9775687c83e78b0
Charles Ofria <charles.ofria@gmail.com>
2018-06-21 00:15:53 -0400

    Trimmed down OpenOrg to ignore details handled in Surface.

--- apps/OpenWorld/source/OpenOrg.h
+++ apps/OpenWorld/source/OpenOrg.h
@@ -23,11 +23,9 @@ public:
 
 private:
   size_t id;
-  size_t color_id;
+  size_t surface_id;
   hardware_t brain;
 
-  emp::Point center;
-  double radius;
   emp::Angle facing;
 
   double energy;
@@ -39,9 +37,8 @@ private:
 
 public:
   OpenOrg(inst_lib_t & inst_lib, event_lib_t & event_lib, emp::Ptr<emp::Random> random_ptr)
-    : id(0), color_id(0), brain(inst_lib, event_lib, random_ptr)
-    , center(1.0, 1.0), radius(5.0), facing()
-    , energy(0.0), time_born(0.0), time_dead(-1.0), base_cost(radius*radius*0.01)
+    : id(0), brain(inst_lib, event_lib, random_ptr)
+    , facing(), energy(0.0), time_born(0.0), time_dead(-1.0), base_cost(0.01)
   {
     brain.SetMinBindThresh(HW_MIN_SIM_THRESH);
     brain.SetMaxCores(HW_MAX_THREADS);
@@ -55,24 +52,18 @@ public:
   OpenOrg & operator=(OpenOrg &&) = default;
 
   size_t GetID() const { return id; }
-  size_t GetColorID() const { return color_id; }
+  size_t GetSurfaceID() const { return surface_id; }
   hardware_t & GetBrain() { return brain; }
   const hardware_t & GetBrain() const { return brain; }
-  emp::Point GetCenter() const { return center; }
-  double GetRadius() const { return radius; }
   emp::Angle GetFacing() const { return facing; }
   double GetEnergy() const { return energy; }
   double GetTimeBorn() const { return time_born; }
   double GetTimeDead() const { return time_dead; }
   double GetBaseCost() const { return base_cost; }
 
-  double GetMass() const { return radius * radius; }
-
   OpenOrg & SetID(size_t _in) { id = _in; return *this; }
-  OpenOrg & GetColorID(size_t _in) { color_id = _in; return *this; }
+  OpenOrg & SetSurfaceID(size_t _in) { surface_id = _in; return *this; }
   // OpenOrg & SetBrain(const hardware_t & _in) { brain = _in; return *this; }
-  OpenOrg & SetCenter(emp::Point _in) { center = _in; return *this; }
-  OpenOrg & SetRadius(double _in) { radius = _in; return *this; }
   OpenOrg & SetFacing(emp::Angle _in) { facing = _in; return *this; }
   OpenOrg & SetEnergy(double _in) { energy = _in; return *this; }
   OpenOrg & SetTimeBorn(double _in) { time_born = _in; return *this; }
@@ -82,11 +73,9 @@ public:
   OpenOrg & AdjustEnergy(double _in) { energy += _in; return *this; }
 
   OpenOrg & RotateDegrees(double degrees) { facing.RotateDegrees(degrees); return *this; }
-  OpenOrg & Translate(emp::Point shift) { center += shift; return *this; }
 
   void Setup(emp::WorldPosition pos, emp::Random & random) {
     // std::cerr << "Seting up at pos " << pos.GetIndex() << std::endl;
-    base_cost = GetMass() * 0.01;
     RotateDegrees(random.GetDouble(360.0));
   }
 
