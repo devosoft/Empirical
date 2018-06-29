2681cf99e4636f101f6380c79499562a955a7bdb
Charles Ofria <charles.ofria@gmail.com>
2018-06-28 22:41:59 -0400

    Added a blank Template.cc file for unit tests.

---
+++ emp_tests/Template.cc
@@ -0,0 +1,16 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file Template.cc
+ *  @brief A starting point for writing unit tests.
+ */
+
+#include "../source/tools/unit_tests.h"
+
+
+int emp_test_main()
+{
+  std::cout << "Run with --verbose flag in order to get more detailed output." << std::endl;
+}
