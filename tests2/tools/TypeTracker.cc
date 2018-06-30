8ab1ea56111e36fe2f1e06250bfe6b3ff01238bc
Charles Ofria <charles.ofria@gmail.com>
2018-06-29 22:37:55 -0400

    Fleshed out TypeTracker unit tests.

--- tests2/tools/TypeTracker.cc
+++ tests2/tools/TypeTracker.cc
@@ -7,15 +7,26 @@
  *  @brief Unit tests for TypeTracker class.
  */
 
+#include "../../source/base/vector.h"
 #include "../../source/tools/TypeTracker.h"
 #include "../../source/tools/unit_tests.h"
 
 std::string tt_result;
+
+// Some functions to print a single type and its value
+void fun_int(int x) { tt_result = emp::to_string("int:", x); }
+void fun_double(double x) { tt_result = emp::to_string("double:", x); }
+void fun_string(std::string x) { tt_result = emp::to_string("string:", x); }
+
+// And some silly ways to combine types.
 void fun_int_int(int x, int y) { tt_result = emp::to_string(x+y); }
 void fun_int_double(int x, double y) { tt_result = emp::to_string(y * (double) x); }
 void fun_string_int(std::string x, int y) {
   tt_result = "";
   for (int i=0; i < y; i++) tt_result += x;
+}
+void fun_5ints(int v, int w, int x, int y, int z) {
+  tt_result = emp::to_string(v, '+', w, '+', x, '+', y, '+', z, '=', v+w+x+y+z);
 }
 
 int emp_test_main()
@@ -24,9 +35,13 @@ int emp_test_main()
   tt_t tt;                                                   // Build the tracker.
 
   // Add some functions.
+  tt.AddFunction( [](int x){ tt_result = emp::to_string("int:", x); } );
+  tt.AddFunction(fun_double);
+  tt.AddFunction(fun_string);
   tt.AddFunction(fun_int_int);
   tt.AddFunction(fun_int_double);
   tt.AddFunction(fun_string_int);
+  tt.AddFunction(fun_5ints);
 
   emp::TrackedVar tt_int1 = tt.Convert<int>(1);
   emp::TrackedVar tt_int2 = tt.Convert<int>(2);
@@ -47,9 +62,30 @@ int emp_test_main()
   tt.RunFunction(tt_str, tt_int3);    // A string an an int should duplicate the string.
   EMP_TEST_VALUE( tt_result, "FOURFOURFOUR" );
 
+  tt.RunFunction(tt_int1, tt_int2, tt_int3, tt_int2, tt_int1);  // Add five ints!
+  EMP_TEST_VALUE( tt_result, "1+2+3+2+1=9" );
 
+
+  // Load all types into a vector and then experiment with them.
+  emp::vector<emp::TrackedVar> vars;
+  vars.push_back(tt_int1);
+  vars.push_back(tt_int2);
+  vars.push_back(tt_int3);
+  vars.push_back(tt_str);
+  vars.push_back(tt_doub);
+
+  emp::vector<std::string> results = { "int:1", "int:2", "int:3", "string:FOUR", "double:5.5" };
+
+  for (size_t i = 0; i < vars.size(); i++) {
+    tt(vars[i]);
+    EMP_TEST_VALUE(tt_result, results[i]);
+  }
+
+  // Make sure TypeTracker can determine consistant IDs.
   EMP_TEST_VALUE( (tt_t::GetID<int,std::string,double>()), (tt_t::GetTrackedID(tt_int1, tt_str, tt_doub)) );
   EMP_TEST_VALUE( (tt_t::GetComboID<int,std::string,double>()), (tt_t::GetTrackedComboID(tt_int1, tt_str, tt_doub)) );
+
+
 
   // Make sure a TypeTracker can work with a single type.
   size_t num_args = 0;
