5014c96c6dc4ebaf29d71a90e7240af576cc5db6
Charles Ofria <charles.ofria@gmail.com>
2018-06-30 20:55:54 -0400

    Filled out OpenResource struct.

--- apps/OpenWorld/source/OpenResource.h
+++ apps/OpenWorld/source/OpenResource.h
@@ -3,7 +3,9 @@
 #ifndef OPEN_RESOURCE_H
 #define OPEN_RESOURCE_H
 
-class OpenResource {
+struct OpenResource {
+  size_t resource_type;  // What type of resource is this?
+  size_t surface_id;     // Which surface object represents this resource?
 };
 
 #endif
