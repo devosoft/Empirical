8c27b89b6abcaef7ddb3a75c77370654e4f0ebcd
Charles Ofria <charles.ofria@gmail.com>
2018-07-14 23:31:51 -0400

    Added a Binomial test to unit tests.

--- tests/test_tools.cc
+++ tests/test_tools.cc
@@ -15,6 +15,7 @@
 #include <string>
 #include <deque>
 
+#include "tools/Binomial.h"
 #include "tools/BitMatrix.h"
 #include "tools/BitSet.h"
 #include "tools/BitVector.h"
@@ -64,6 +65,47 @@
     static_assert(A == B, #A " == " #B); \
     REQUIRE(A == B);                     \
   }
+
+
+TEST_CASE("Test Binomial", "[tools]")
+{
+  emp::Random random;
+
+  const double flip_prob = 0.03;
+  const size_t num_flips = 100;
+
+  const size_t num_tests = 100000;
+  const size_t view_count = 10;
+
+  emp::Binomial bi100(flip_prob, num_flips);
+
+  emp::vector<size_t> counts(num_flips+1, 0);
+
+  for (size_t test_id = 0; test_id < num_tests; test_id++) {
+    size_t win_count = 0;
+    for (size_t i = 0; i < num_flips; i++) {
+      if (random.P(0.03)) win_count++;
+    }
+    counts[win_count]++;
+  }
+
+  // Print out the first values in the distribution.
+  for (size_t i = 0; i < view_count; i++) {
+    // std::cout << "bi100[" << i << "] = " << bi100[i]
+    //           << "  test_count = " << counts[i]
+    //           << "\n";
+    REQUIRE(bi100[i] < ((double) counts[i]) / (double) num_tests + 0.02);
+    REQUIRE(bi100[i] > ((double) counts[i]) / (double) num_tests - 0.02);
+  }
+  // std::cout << "Total = " << bi100.GetTotalProb() << std::endl;
+
+  // // Pick some random values...
+  // std::cout << "\nSome random values:";
+  // for (size_t i = 0; i < 100; i++) {
+  //   std::cout << " " << bi100.PickRandom(random);
+  // }
+  // std::cout << std::endl;
+}
 
 // this templating is necessary to force full coverage of templated classes.
 // Since c++ doesn't generate code for templated methods if those methods aren't
