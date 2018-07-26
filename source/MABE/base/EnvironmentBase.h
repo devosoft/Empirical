33e50f7c72434ba170cb5cf2035e154617aafcc0
Charles Ofria <charles.ofria@gmail.com>
2018-07-25 21:09:22 -0400

    Added a base class for MABE environments.

---
+++ source/MABE/base/EnvironmentBase.h
@@ -0,0 +1,24 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  EnvironmentBase.h
+ *  @brief Base class for all Environments in MABE
+ *
+ *  This file details all of the basic functionality that all environments MUST have, providing
+ *  reasonable defaults when such are possible.
+ */
+
+#ifndef MABE_ENVIRONMENT_BASE_H
+#define MABE_ENVIRONMENT_BASE_H
+
+namespace mabe {
+
+  class EnvironmentBase {
+  };
+
+}
+
+#endif
+
