d512861dd741148063514c36d3437af691320a80
Charles Ofria <charles.ofria@gmail.com>
2018-07-26 01:38:11 -0400

    Added example file for MABE Worlds.

---
+++ source/MABE/examples/World.cc
@@ -0,0 +1,14 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  World.cc
+ *  @brief An example for using a MABE World.
+ */
+
+#include "../core/World.h"
+
+int main(int argc, char *argvp[])
+{
+}
