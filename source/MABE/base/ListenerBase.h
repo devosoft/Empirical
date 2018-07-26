23690bfba1667e85206ef543ab98d22e87e8307a
Charles Ofria <charles.ofria@gmail.com>
2018-07-25 21:13:48 -0400

    Added a base class for MABE listeners.

---
+++ source/MABE/base/ListenerBase.h
@@ -0,0 +1,24 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  ListenerBase.h
+ *  @brief Base class for all Listeners in MABE
+ *
+ *  This file details all of the basic functionality that all listeners MUST have, providing
+ *  reasonable defaults when such are possible.
+ */
+
+#ifndef MABE_LISTENER_BASE_H
+#define MABE_LISTENER_BASE_H
+
+namespace mabe {
+
+  class ListenerBase {
+  };
+
+}
+
+#endif
+
