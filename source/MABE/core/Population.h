495afab400800f888eecd3df849126707bcabc11
Charles Ofria <charles.ofria@gmail.com>
2018-07-26 22:48:28 -0400

    Added PopulationBase class for consistency.

--- source/MABE/core/Population.h
+++ source/MABE/core/Population.h
@@ -16,22 +16,24 @@
 #include "../../base/Ptr.h"
 #include "../../base/vector.h"
 
+#include "../base/ModuleBase.h"
+
 namespace mabe {
 
+  class PopulationBase : public ModuleBase {
+  public:
+    static constexpr mabe::ModuleType GetModuleType() { return ModuleType::POPULATION; }
+  };
+
   template <typename ORG_T>
-  class Population {
+  class Population : public PopulationBase {
   public:
     using this_t = Population<ORG_T>;            ///< Resolved type of this templated class.
     using org_t = ORG_T;                         ///< Type of organisms in this population.
     using value_type = org_t;                    ///< Identical to org_t; vector compatibility.
     using vec_t = emp::vector<emp::Ptr<org_t>>;  ///< Type for storing all organisms.
-    using iterator_t = Pop_iterator<this_t>;     ///< Type for this world's iterators.
+    //using iterator_t = Pop_iterator<this_t>;     ///< Type for this world's iterators.
 
-  private:
-    
-
-  public:
-    static constexpr mabe::ModuleType GetModuleType() { return ModuleType::POPULATION; }
   };
 
 }
