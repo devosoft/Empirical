6cf12df6be353d4e14f9a17be7d144160ffe76d3
Charles Ofria <charles.ofria@gmail.com>
2018-08-03 12:45:57 -0400

    Added base classes for contollers and genomes for organisms in MABE.

---
+++ apps/MABE/core/ControllerBase.h
@@ -0,0 +1,27 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  ControllerBase.h
+ *  @brief Base class for all controller types.
+ *
+ *  Controllers are generic forms of agent brains.
+ */
+
+#ifndef MABE_CONTROLLER_BASE_H
+#define MABE_CONTROLLER_BASE_H
+
+namespace mabe {
+
+  class ControllerBase {
+  private:
+  public:
+    ControllerBase() { ; }
+    virtual ~ControllerBase() { ; }
+  };
+
+}
+
+#endif
+
---
+++ apps/MABE/core/GenomeBase.h
@@ -0,0 +1,27 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  GenomeBase.h
+ *  @brief Base class for all genome types.
+ *
+ *  Genomes are generic ways of representing controllers.
+ */
+
+#ifndef MABE_GENOME_BASE_H
+#define MABE_GENOME_BASE_H
+
+namespace mabe {
+
+  class GenomeBase {
+  private:
+  public:
+    GenomeBase() { ; }
+    virtual ~GenomeBase() { ; }
+  };
+
+}
+
+#endif
+
