90542876e134df9f772becfb62ad886a9c76eba0
Charles Ofria <charles.ofria@gmail.com>
2018-08-03 12:44:50 -0400

    Added a (mostly empty, but compiles) TrackLineage watcher class as a starting point in MABE.

---
+++ apps/MABE/watchers/TrackLineage.h
@@ -0,0 +1,26 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  TrackLineage.h
+ *  @brief Tools for tracking the lineages leading to the active population.
+ */
+
+#ifndef MABE_TRACK_LINEAGE_H
+#define MABE_TRACK_LINEAGE_H
+
+#include "core/WatcherBase.h"
+
+namespace mabe {
+
+  class TrackLineage : public WatcherBase {
+  private:
+  public:
+    TrackLineage(const std::string & name) : WatcherBase(name) { ; }
+  };
+
+}
+
+#endif
+
