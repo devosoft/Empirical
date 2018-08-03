a98261d9e7ae5ef0a28a1e0903882eaf79d8ea64
Charles Ofria <charles.ofria@gmail.com>
2018-08-03 12:46:29 -0400

    Added a (mostly empty, but compiles) AvidaGP controller class as a starting point in MABE.

---
+++ apps/MABE/controllers/AvidaGP.h
@@ -0,0 +1,26 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  AvidaGP.h
+ *  @brief Contoller usng AvidaGP virtual CPU
+ */
+
+#ifndef MABE_AVIDA_GP_H
+#define MABE_AVIDA_GP_H
+
+#include "core/ControllerBase.h"
+
+namespace mabe {
+
+  class AvidaGP : public ControllerBase {
+  private:
+  public:
+    AvidaGP() { ; }
+  };
+
+}
+
+#endif
+
