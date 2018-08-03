9682de555c9ea166da5adc28ed82b73055775895
Charles Ofria <charles.ofria@gmail.com>
2018-08-03 12:44:05 -0400

    Added a (mostly empty, but compiles) ResourceGrid.h environment class as a starting point in MABE.

---
+++ apps/MABE/environments/ResourceGrid.h
@@ -0,0 +1,26 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  ResourceGrid.h
+ *  @brief A grid-based world where agents must collect pools of resources.
+ */
+
+#ifndef MABE_RESOURCE_GRID_H
+#define MABE_RESOURCE_GRID_H
+
+#include "core/EnvironmentBase.h"
+
+namespace mabe {
+
+  class ResourceGrid : public EnvironmentBase {
+  private:
+  public:
+    ResourceGrid(const std::string & name) : EnvironmentBase(name) { ; }
+  };
+
+}
+
+#endif
+
