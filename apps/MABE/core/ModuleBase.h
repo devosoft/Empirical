afa71dc6b7741b5c0bbb333a428fb098881059e0
Charles Ofria <charles.ofria@gmail.com>
2018-08-05 06:56:07 -0400

    Setup module base to require GetClassName() to be defined for all modules; added docs.

--- apps/MABE/core/ModuleBase.h
+++ apps/MABE/core/ModuleBase.h
@@ -5,6 +5,18 @@
  *
  *  @file  ModuleBase.h
  *  @brief Base class for high-level Module types in MABE (Environments, OrganismTypes, etc.)
+ *
+ *  This class provides a base class for ALL modules in MABE, specifying common functionality.
+ * 
+ *  The author of a new MABE module *must* override:
+ *
+ *    const std::string & GetClassName() const
+ *      Provide a unique name for module class, usually the same as in code (used for debugging).
+ * 
+ *  The author of a new MABE module *may* also choose to override:
+ * 
+ *    The distructor.
+ *    
  */
 
 #ifndef MABE_MODULE_BASE_H
@@ -16,7 +28,7 @@ namespace mabe {
 
   /// Track the specific type associated with a module.
   enum class ModuleType {
-    NONE=0, BASE, ENVIRONMENT, ORGANISM_TYPE, POPULATION, SCHEMA, WATCHER, UNKNOWN
+    NONE=0, BASE, ENVIRONMENT, ORGANISM_TYPE, SCHEMA, WATCHER, UNKNOWN
   };
 
   /// Base class for all major MABE modules that can receive names.
@@ -28,9 +40,15 @@ namespace mabe {
     ModuleBase(const std::string & in_name) : name(in_name) { ; }
     virtual ~ModuleBase() { ; }  ///< Make sure all modules can be deleted properly.
 
+    /// Identify the broad type of this module at compile time; ModuleType is enumerated above.
+    static constexpr mabe::ModuleType GetModuleType() { return ModuleType::BASE; }
+
+    /// Every module must have a unique name to identify its section of config files.
     const std::string & GetName() const { return name; }
 
-    static constexpr mabe::ModuleType GetModuleType() { return ModuleType::BASE; }
+    /// Every module type needs to specify its derived class name as a string.
+    virtual std::string GetClassName() const = 0;
+
   };
 
 }
