c4b3498ac102a2ae1eebc918fe162427dca98811
Charles Ofria <charles.ofria@gmail.com>
2018-08-16 23:34:05 -0400

    Added config files to derived genome types in MABE.

--- apps/MABE/genomes/LinearGenome.h
+++ apps/MABE/genomes/LinearGenome.h
@@ -16,10 +16,19 @@ namespace mabe {
 
   class LinearGenome : public GenomeBase {
   private:
+    EMP_BUILD_CONFIG( LGConfig,
+      GROUP(DEFAULT_GROUP, "Linear Genome Settings"),
+      VALUE(ALPHABET_SIZE, size_t, 256, "Number of possible values in genome."),
+      VALUE(MIN_LENGTH, size_t, 8, "Minimum number of sites in a genome."),
+      VALUE(MAX_LENGTH, size_t, 1024, "Maximum number of sites in a genome.")
+    )
+
+    LGConfig config;
   public:
     LinearGenome() { ; }
 
     std::string GetClassName() const override { return "LinearGenome"; }
+    LGConfig & GetConfig() override { return config; }
 
     struct data_t {
       emp::vector<unsigned char> data;
