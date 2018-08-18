05860b4c0449c8ccb590ebd9c0dc854ba6c06234
Charles Ofria <charles.ofria@gmail.com>
2018-08-18 13:38:54 -0400

    Moved environment functions to base; clean up functions in destructor.

--- apps/MABE/core/EnvironmentBase.h
+++ apps/MABE/core/EnvironmentBase.h
@@ -15,7 +15,7 @@
 #ifndef MABE_ENVIRONMENT_BASE_H
 #define MABE_ENVIRONMENT_BASE_H
 
-#include "tools/GenericFunction.h"
+#include "base/vector.h"
 
 #include "ModuleBase.h"
 
@@ -23,8 +23,6 @@ namespace mabe {
 
   class EnvironmentBase : public ModuleBase {
   private:
-    using fun_ptr_t = emp::Ptr<emp::GenericFunction>;
-
     /// These are functions built by the derived environment that will be called when specific
     /// events are triggered.  To setup these functions (since different organism types will
     /// have different ways of calling them) they are passed to an organism type which builds
@@ -32,17 +30,22 @@ namespace mabe {
     /// the correct result.  For simplicity, the only return type allowed is double.  Anything
     /// more complex should be handled with a callback using one of the action function in the
     /// next group.
-    std::map< std::string, fun_ptr_t > event_fun_map;
+    emp::vector<FunctionInfo> event_funs;
 
     /// These are functions that will be provided to the Organisms in this environment.  The
     /// organisms can call these functions (with the appropriate arguments) in order to
     /// senese or act in their environment.  The only return type allowed is a double; anything
     /// more complex should be handed with a callback using one of the event functions in the
     /// previous group.
-    std::map< std::string, fun_ptr_t > action_fun_map;
+    emp::vector<FunctionInfo> action_funs;
 
   public:
     EnvironmentBase(const std::string & in_name) : ModuleBase(in_name) { ; }
+    virtual ~EnvironmentBase() {
+      // Clean up the functions that were created in this environment.
+      for (auto & fun_info : event_funs) fun_info.fun_ptr.Delete();
+      for (auto & fun_info : action_funs) fun_info.fun_ptr.Delete();
+    }
 
     static constexpr mabe::ModuleType GetModuleType() { return ModuleType::ENVIRONMENT; }
   };
