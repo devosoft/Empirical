9a2b614c90f45912ce4ea904c9eed321aa9f6ec3
Charles Ofria <charles.ofria@gmail.com>
2018-08-18 13:00:02 -0400

    Provided substantial comments about Environment functions.

--- apps/MABE/core/EnvironmentBase.h
+++ apps/MABE/core/EnvironmentBase.h
@@ -24,8 +24,22 @@ namespace mabe {
   class EnvironmentBase : public ModuleBase {
   private:
     using fun_ptr_t = emp::Ptr<emp::GenericFunction>;
-    std::map< std::string, fun_ptr_t > event_fun_map;   ///< Functions triggered by environment.
-    std::map< std::string, fun_ptr_t > action_fun_map;  ///< Functions orgs trigger to take actions.
+
+    /// These are functions built by the derived environment that will be called when specific
+    /// events are triggered.  To setup these functions (since different organism types will
+    /// have different ways of calling them) they are passed to an organism type which builds
+    /// a new version of the function that always takes a mabe::Organsm reference and returns
+    /// the correct result.  For simplicity, the only return type allowed is double.  Anything
+    /// more complex should be handled with a callback using one of the action function in the
+    /// next group.
+    std::map< std::string, fun_ptr_t > event_fun_map;
+
+    /// These are functions that will be provided to the Organisms in this environment.  The
+    /// organisms can call these functions (with the appropriate arguments) in order to
+    /// senese or act in their environment.  The only return type allowed is a double; anything
+    /// more complex should be handed with a callback using one of the event functions in the
+    /// previous group.
+    std::map< std::string, fun_ptr_t > action_fun_map;
 
   public:
     EnvironmentBase(const std::string & in_name) : ModuleBase(in_name) { ; }
