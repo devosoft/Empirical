f38d3692fd4ade54feee53e7758d2455b8c82998
Charles Ofria <charles.ofria@gmail.com>
2018-08-23 15:48:18 -0400

    Setup copy constructor and Clone pure virtual function in OrganismBase.

--- apps/MABE/core/OrganismBase.h
+++ apps/MABE/core/OrganismBase.h
@@ -8,6 +8,8 @@
  *
  *  This file details all of the basic functionality that all organisms MUST have, providing
  *  reasonable defaults when such are possible.
+ * 
+ *  These include a required Clone() member function to produce a duplicate of an organism.
  */
 
 #ifndef MABE_ORGANISM_BASE_H
@@ -23,9 +25,12 @@ namespace mabe {
 
   public:
     OrganismBase(emp::Ptr<OrganismTypeBase> _type_ptr) : type_ptr(_type_ptr) { type_ptr->IncCount(); }
+    OrganismBase(const OrganismBase & in_org) : type_ptr(in_org.type_ptr) { type_ptr->IncCount(); }
     virtual ~OrganismBase() { type_ptr->DecCount(); }
 
     emp::Ptr<OrganismTypeBase> GetTypePtr() const { return type_ptr; }
+
+    virtual emp::Ptr<OrganismBase> Clone() = 0;
   };
 
 }
