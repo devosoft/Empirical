4f2f2332eaf2fc4167b6dd69035569c8ee831559
Charles Ofria <charles.ofria@gmail.com>
2018-08-23 21:11:01 -0400

    Setup GenomeTypeBase to require a Randomize() function.

--- apps/MABE/core/GenomeTypeBase.h
+++ apps/MABE/core/GenomeTypeBase.h
@@ -9,7 +9,10 @@
  *  Genomes are generic ways of encoding info for Brains or other aspects of organisms.
  *
  *  Each derived genome class much have a data_t type indicating a struct with the set of
- *  information it needs to store in organisms.
+ *  information it needs to store in organisms.  It must also have the following functions:
+ *    std::string GetClassName()
+ *    emp::Config & GetConfig()
+ *    void Randomize(emp::Random &)
  */
 
 #ifndef MABE_GENOME_TYPE_BASE_H
@@ -30,6 +33,8 @@ namespace mabe {
 
     virtual std::string GetClassName() const = 0;
     virtual emp::Config & GetConfig() = 0;
+
+    virtual void Randomize(emp::Random &) = 0;
 
     const std::string & GetName() { return name; }
     void SetName(const std::string & in_name) { name = in_name; }
