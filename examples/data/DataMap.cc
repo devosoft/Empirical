382e335650c548d8a59f55d165adc2626106b41f
Charles Ofria <charles.ofria@gmail.com>
2018-08-27 22:14:17 -0400

    Initial DataMap example file; for now just includes DataMap.h and compiles.

---
+++ examples/data/DataMap.cc
@@ -0,0 +1,17 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  DataMap.cc
+ *  @brief An example file for using DataMaps
+ */
+
+#include <iostream>
+
+#include "data/DataMap.h"
+
+int main()
+{
+  std::cout << "Testing!" << std::endl;
+}
