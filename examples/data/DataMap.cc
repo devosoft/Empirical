153910ff2a795239466f2c296aec18fc314dd317
Charles Ofria <charles.ofria@gmail.com>
2018-08-27 23:37:30 -0400

    Added various Get() tests in DataMap example file.

--- examples/data/DataMap.cc
+++ examples/data/DataMap.cc
@@ -21,5 +21,14 @@ int main()
   dmap.Add<std::string>("name2", "MyOrg2");
   dmap.Add<std::string>("name3", "MyOrg3");
 
-  std::cout << "Testing!" << std::endl;
+  dmap.Get<std::string>("name") = "FirstOrg";
+  dmap.Get<std::string>(1) = "Org TWO!";
+  dmap.Get<std::string>("name3") = "Test Output!";
+  dmap.Get<double>("fitness") = 1000000.1;
+
+  std::cout << "Name 1 = " << dmap.Get<std::string>("name") << std::endl;
+  std::cout << "Name 2 = " << dmap.Get<std::string>("name2") << std::endl;
+  std::cout << "Name 3 = " << dmap.Get<std::string>("name3") << std::endl;
+  std::cout << "Name 3 = " << dmap.Get<std::string>(2) << std::endl;
+
 }
