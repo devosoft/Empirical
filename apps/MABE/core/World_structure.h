5def26b9ffb32bbf00b1749c8bfa275f5523981a
Charles Ofria <charles.ofria@gmail.com>
2018-09-08 22:53:35 -0400

    Removed WorldPosition from World_structure.h; use EvolverID throughout.

--- apps/MABE/core/World_structure.h
+++ apps/MABE/core/World_structure.h
@@ -26,55 +26,24 @@ namespace mabe {
 
   template <typename ENV_T> class Evolver;
 
-  /// A class to track positions in Evolver.
-  /// For the moment, the only informaiton beyond index is active (vs. next) population when
-  /// using synchronous generations.
-  //
-  //  Developer NOTE: For efficiency, internal class members are uint32_t, but to prevent compiler
-  //                  warnings, size_t values are accepted; asserts ensure safe conversions.
-  class WorldPosition {
-  private:
-    uint32_t index;   ///<  Position of this organism in the population.
-    uint32_t pop_id;  ///<  ID of the population we are in; 0 is always the active population.
 
-  public:
-    static constexpr size_t invalid_id = (uint32_t) -1;
-
-    WorldPosition() : index(invalid_id), pop_id(invalid_id) { ; }
-    WorldPosition(size_t _id, size_t _pop_id=0) : index((uint32_t) _id), pop_id((uint32_t) _pop_id) {
-      emp_assert(_id <= invalid_id);
-      emp_assert(_pop_id <= invalid_id);
-    }
-    WorldPosition(const WorldPosition &) = default;
-
-    uint32_t GetIndex() const { return index; }
-    uint32_t GetPopID() const { return pop_id; }
-
-    bool IsActive() const { return pop_id == 0; }
-    bool IsValid() const { return index != invalid_id; }
-
-    WorldPosition & SetActive(bool _active=true) { pop_id = _active ? 0 : 1; return *this; }
-    WorldPosition & SetPopID(size_t _id) { emp_assert(_id <= invalid_id); pop_id = (uint32_t) _id; return *this; }
-    WorldPosition & SetIndex(size_t _id) { emp_assert(_id <= invalid_id); index = (uint32_t) _id; return *this; }
-    WorldPosition & MarkInvalid() { index = invalid_id; pop_id = invalid_id; return *this; }
-  };
-
-  /// A vector that can be indexed with a WorldPosition
+  /// A vector that can be indexed with an EvolverID
   class WorldVector : public emp::array<emp::vector< emp::Ptr<mabe::OrganismBase> >, 2> {
   public:
     using ptr_t = emp::Ptr<mabe::OrganismBase>;
     using base_t = emp::array<emp::vector< ptr_t >, 2>;
 
-    /// Test if a position is currently valid.
-    bool IsValid(WorldPosition pos) const {
+    /// Test if an id is currently valid.
+    bool IsValid(EvolverID pos) const {
       const size_t pop_id = pos.GetPopID();
       const size_t id = pos.GetIndex();
       emp_assert(pop_id < 2);
       return id < base_t::operator[](pop_id).size();
     }
 
-    /// Make sure position is valid; if not expand relevant vector.
-    void MakeValid(WorldPosition pos) {
+    /// Make sure ID is in vector range; if not expand relevant vector.
+    void MakeValid(EvolverID pos) {
+      emp_assert(pos.IsValid());
       const size_t pop_id = pos.GetPopID();
       const size_t id = pos.GetIndex();
       emp_assert(pop_id < 2);
@@ -83,12 +52,12 @@ namespace mabe {
       }
     }
 
-    ptr_t & operator()(WorldPosition pos) {
+    ptr_t & operator()(EvolverID pos) {
       const size_t pop_id = pos.GetPopID();
       const size_t id = pos.GetIndex();
       return base_t::operator[](pop_id)[id];
     }
-    const ptr_t & operator()(WorldPosition pos) const {
+    const ptr_t & operator()(EvolverID pos) const {
       const size_t pop_id = pos.GetPopID();
       const size_t id = pos.GetIndex();
       return base_t::operator[](pop_id)[id];
