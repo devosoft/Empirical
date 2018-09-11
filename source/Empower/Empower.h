65b2de26c361e939585ce7c132207de0f3a54baa
Charles Ofria <charles.ofria@gmail.com>
2018-09-11 17:09:56 -0400

    Added a MemoryImage class to Empower; cleaned up other classes.

--- source/Empower/Empower.h
+++ source/Empower/Empower.h
@@ -18,6 +18,7 @@
 #include <functional>
 #include <string>
 
+#include "../base/Ptr.h"
 #include "../base/vector.h"
 
 namespace emp {
@@ -27,32 +28,42 @@ namespace emp {
     using byte_t = unsigned char;
     using memory_t = emp::vector<byte_t>;
 
+    /// A MemoryImage is a full set of variable values stored in an Empower instance.
+    class MemoryImage {
+      memory_t memory;                 ///< The specific memory values.
+      emp::Ptr<Empower> empower_ptr;   ///< A pointer back to Empower instance this memory uses.
+    }
+
+    /// A Var is an internal variable that has a run-time determined type (which is tracked).
     class Var {
     private:
-      size_t info_id;              ///< Which variable ID is this var associated with?
-      emp::Ptr<memory_t> mem_ptr;  ///< Which memory blob is this variable using (by default)
+      size_t info_id;                 ///< Which variable ID is this var associated with?
+      emp::Ptr<MemoryImage> mem_ptr;  ///< Which memory image is variable using (by default)
     public:
       Var() : info_id(0) { ; }
     };
 
   protected:
+    /// Information about a single Empower variable, including its type, name, and where to
+    /// find it in a memory image.
     struct VarInfo {
-      size_t type_id;              ///< What type is this variable?
-      std::string var_name;        ///< What is the unique name for this variable?
-      size_t mem_pos;              ///< Where in memory is this variable stored?
+      size_t type_id;          ///< What type is this variable?
+      std::string var_name;    ///< What is the unique name for this variable?
+      size_t mem_pos;          ///< Where in memory is this variable stored?
     };
 
+    /// Information about a single type used in Empower.
     struct TypeInfo {
-      size_t type_id;              ///< Unique ID for this type.
-      std::string type_name;       ///< Name of this type (from std::typeid)
-      size_t mem_size;             ///< Bytes needed for this type (from sizeof)
+      size_t type_id;          ///< Unique ID for this type.
+      std::string type_name;   ///< Name of this type (from std::typeid)
+      size_t mem_size;         ///< Bytes needed for this type (from sizeof)
 
       // Core conversion functions for this type.
       std::function<double(Var &)> to_double;      ///< Fun to convert type to double (empty=>none)
       std::function<std::string(Var &)> to_string; ///< Fun to convert type to string (empty=>none)
     };
 
-    memory_t memory;
+    memory_t memory;  /// The default memory image.
 
   public:
     Empower() : memory() { ; }
