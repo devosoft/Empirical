e9013f77147d00fda769111612d8376a29dd72ef
Charles Ofria <charles.ofria@gmail.com>
2018-09-11 23:42:00 -0400

    Added in variables to Empower; fleshed out some types; added AddType function.

--- source/Empower/Empower.h
+++ source/Empower/Empower.h
@@ -16,6 +16,7 @@
 #define EMP_EMPOWER_H
 
 #include <functional>
+#include <map>
 #include <string>
 
 #include "../base/Ptr.h"
@@ -30,9 +31,18 @@ namespace emp {
 
     /// A MemoryImage is a full set of variable values stored in an Empower instance.
     class MemoryImage {
+    private:
       memory_t memory;                 ///< The specific memory values.
       emp::Ptr<Empower> empower_ptr;   ///< A pointer back to Empower instance this memory uses.
-    }
+
+    public:
+      MemoryImage(emp::Ptr<Empower> _ptr) : memory(), empower_ptr(_ptr) { ; }
+      MemoryImage(const MemoryImage &) = default;
+      MemoryImage(MemoryImage &&) = default;
+
+      const memory_t & GetMemory() const { return memory; }
+      const emp::Ptr<Empower> GetEmpowerPtr() const { return empower_ptr; }
+    };
 
     /// A Var is an internal variable that has a run-time determined type (which is tracked).
     class Var {
@@ -61,15 +71,37 @@ namespace emp {
       // Core conversion functions for this type.
       std::function<double(Var &)> to_double;      ///< Fun to convert type to double (empty=>none)
       std::function<std::string(Var &)> to_string; ///< Fun to convert type to string (empty=>none)
+
+      TypeInfo(size_t _id, const std::string & _name, size_t _size)
+       : type_id(_id), type_name(_name), mem_size(_size) { ; }
     };
 
     memory_t memory;  /// The default memory image.
+    emp::vector<VarInfo> vars;
+    emp::vector<TypeInfo> types;
+
+    std::map<std::string, size_t> var_map;   ///< Map variable names to index in vars
+    std::map<std::string, size_t> type_map;  ///< Map type names (from typeid) to index in types
 
   public:
     Empower() : memory() { ; }
     ~Empower() { ; }
 
+    template <typename T>
+    void AddType() {
+      using base_t = typename std::decay<T>::type;
 
+      // size_t type_hash = typeid(T).hash_code();
+      std::string type_name = typeid(base_t).name();
+
+      // If this type already exists stop here!
+      if (emp::Has(type_map, type_name)) return;
+
+      size_t type_id = types.size();
+      size_T mem_size = sizeof(base_t);
+      types.emplace_back(type_id, type_name, mem_size);
+      type_map[type_name] = type_id;
+    }
   };
 
 }
