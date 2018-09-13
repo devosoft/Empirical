6a015a8643990775cc0a959c23c9adf6aac888a6
Charles Ofria <charles.ofria@gmail.com>
2018-09-12 23:08:06 -0400

    Added initial example file for Empower.

---
+++ examples/Empower/Empower.cc
@@ -0,0 +1,18 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2018.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some examples code for using emp::Random
+
+#include <iostream>
+
+#include "../../source/Empower/Empower.h"
+
+int main()
+{
+  emp::Empower empower;
+
+  empower.NewVar<int>("test", 5);
+}
+
