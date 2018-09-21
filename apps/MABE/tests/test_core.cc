7d20a97346b020983a429e6b39543a1b9579a246
Charles Ofria <charles.ofria@gmail.com>
2018-09-20 23:07:38 -0400

    Fleshed out initial GenomeWrapper test case.

--- apps/MABE/tests/test_core.cc
+++ apps/MABE/tests/test_core.cc
@@ -2,9 +2,23 @@
 #include "third-party/Catch/single_include/catch.hpp"
 
 #include "../core/GenomeWrapper.h"
+#include <utility>
+
+// A test genome with no special functions.
+struct TestGenomeA { std::string foo = "This is my member var."; };
+
+// A test genome that uses all special functions.
+struct TestGenomeB {
+  std::string name = "My Name";
+
+  std::string GetClassName() const { return "TestGenomeB"; }
+};
 
 TEST_CASE("Test GenomeWrapper", "[core]")
 {
-  int x = 1;
-  REQUIRE(x == 1);
+  mabe::GenomeWrapper<TestGenomeA> genA;
+  mabe::GenomeWrapper<TestGenomeB> genB;
+
+  REQUIRE(genA.GetClassName() == "NoName");
+  REQUIRE(genB.GetClassName() == "TestGenomeB");
 }
