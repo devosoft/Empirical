dcb254bccdfa3636e343721a6cd75062a3e5e74c
Charles Ofria <charles.ofria@gmail.com>
2018-09-22 19:30:21 -0400

    Added GenomeWrapper test cases (with an argument) for Randomize()

--- apps/MABE/tests/test_core.cc
+++ apps/MABE/tests/test_core.cc
@@ -1,24 +1,37 @@
 #define CATCH_CONFIG_MAIN
 #include "third-party/Catch/single_include/catch.hpp"
 
-#include "../core/GenomeWrapper.h"
 #include <utility>
+
+#include "tools/Random.h"
+
+#include "../core/GenomeWrapper.h"
 
 // A test genome with no special functions.
 struct TestGenomeA { std::string foo = "This is my member var."; };
 
 // A test genome that uses all special functions.
 struct TestGenomeB {
-  std::string name = "My Name";
+  std::string name = "Start Name";
 
   std::string GetClassName() const { return "TestGenomeB"; }
+  bool Randomize(emp::Random &) { name = "Randomized!"; return true; }
 };
+
 
 TEST_CASE("Test GenomeWrapper", "[core]")
 {
+  emp::Random random(1);
+
   mabe::GenomeWrapper<TestGenomeA> genA;
   mabe::GenomeWrapper<TestGenomeB> genB;
 
   REQUIRE(genA.GetClassName() == "NoName");
   REQUIRE(genB.GetClassName() == "TestGenomeB");
+
+  REQUIRE(genB.name == "Start Name");
+  REQUIRE(genA.Randomize(random) == false );
+  REQUIRE(genB.Randomize(random) == true );
+  REQUIRE(genB.name == "Randomized!");
+
 }
