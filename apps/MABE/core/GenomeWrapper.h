020484e9ec909838b7bb498294600d1c795393e8
Charles Ofria <charles.ofria@gmail.com>
2018-09-21 20:09:51 -0400

    Automated more of function forwarding in macro

--- apps/MABE/core/GenomeWrapper.h
+++ apps/MABE/core/GenomeWrapper.h
@@ -27,25 +27,30 @@
 #define MABE_GENOME_WRAPPER_H
 
 #include <string>
+#include <utility>
 
 #include "config/config.h"
 #include "meta/meta.h"
 
-#define MABE_GENOME_TEST_FUN(NAME, ...)                                       \
-    template <typename T>                                                     \
-    using call_t_ ## NAME = decltype( ((T*) nullptr)->NAME(__VA_ARGS__) );    \
-    static constexpr bool has_fun_ ## NAME() {                                \
-      return emp::test_type<call_t_ ## NAME, GENOME_T>();                     \
+#define MABE_GENOME_TEST_FUN(NAME, RETURN, DEFAULT, ...)                        \
+    template <typename T>                                                       \
+    using return_t_ ## NAME = decltype( std::declval<T>().NAME(__VA_ARGS__) );  \
+    static constexpr bool HasFun_ ## NAME() {                                   \
+      return emp::test_type<return_t_ ## NAME, GENOME_T>();                     \
+    }                                                                           \
+    RETURN NAME() const {                                                       \
+      if constexpr (HasFun_ ## NAME()) { return GENOME_T::NAME(); }             \
+      else { return DEFAULT; }                                                  \
     }
 
 namespace mabe {
 
   template <typename GENOME_T>
   class GenomeWrapper : public GENOME_T {
-  private:
+  public:
     // Create a set of functions to determine if a memeber exists on GENOME_T in the form of
     // constexpr bool has_fun_X()
-    MABE_GENOME_TEST_FUN(GetClassName);
+    MABE_GENOME_TEST_FUN(GetClassName, std::string, "NoName");
     // MABE_GENOME_TEST_FUN(GetConfig);
     // MABE_GENOME_TEST_FUN(Randomize);
     // MABE_GENOME_TEST_FUN(Print);
@@ -56,10 +61,10 @@ namespace mabe {
     // MABE_GENOME_TEST_FUN(OnPlacement);
     // MABE_GENOME_TEST_FUN(OnOrgDeath);
   public:
-    std::string GetClassName() const {
-      if constexpr (has_fun_GetClassName()) { return GENOME_T::GetClassName(); }
-      else { return std::string("NoName"); }
-    }
+    // std::string GetClassName() const {
+    //   if constexpr (HasFun_GetClassName()) { return GENOME_T::GetClassName(); }
+    //   else { return std::string("NoName"); }
+    // }
   };
 
 }
