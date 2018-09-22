ef88c009ae11ab1bb70a87835547ca600245b942
Charles Ofria <charles.ofria@gmail.com>
2018-09-22 19:29:31 -0400

    Setup GenomeWrapper to work with member functions that have arguments.

--- apps/MABE/core/GenomeWrapper.h
+++ apps/MABE/core/GenomeWrapper.h
@@ -35,9 +35,10 @@
 // Macros to take a variable list of macro arguments and wrap them all in declval.  These mock
 // instances of the types will then be used in a function call in a decltype to determine if
 // that specific type of call is legal.
-#define MABE_TYPE_TO_VAL(TYPE) std::declval<TYPE>()
-//#define MABE_TYPES_TO_VALS(...) EMP_WRAP_ARGS(MABE_TYPE_TO_VAL, __VA_ARGS__)
-#define MABE_TYPES_TO_VALS(...) EMP_WRAP_ARGS(MABE_TYPE_TO_VAL, __VA_ARGS__)
+//#define MABE_TYPE_TO_VAL(TYPE) std::declval< std::decay<TYPE> >()
+//#define MABE_TYPE_TO_VAL(TYPE) *((std::decay<TYPE> *) nullptr)
+#define MABE_TYPE_TO_VAL(TYPE) std::declval< TYPE >()
+#define MABE_TYPES_TO_VALS(...) EMP_WRAP_ARGS(MABE_TYPE_TO_VAL, EMP_POP_ARG(__VA_ARGS__))
 
 // Macro to dynamically call a function either in the wrapped type (if it exists)
 // or return the default provided (otherwise).  The first two arguments are the
@@ -53,8 +54,11 @@
     static constexpr bool HasFun_ ## NAME() {                                 \
       return emp::test_type<return_t_ ## NAME, GENOME_T>();                   \
     }                                                                         \
-    EMP_GET_ARG(1, __VA_ARGS__) NAME() const {                                \
-      if constexpr (HasFun_ ## NAME()) { return GENOME_T::NAME(); }           \
+    template <typename... Ts>                                                 \
+    EMP_GET_ARG(1, __VA_ARGS__) NAME(Ts &&... args) {                         \
+      if constexpr (HasFun_ ## NAME()) {                                      \
+        return GENOME_T::NAME( std::forward<Ts>(args)... );                   \
+      }                                                                       \
       else { return DEFAULT; }                                                \
     }
 
@@ -64,6 +68,8 @@ namespace mabe {
   template <typename GENOME_T>
   class GenomeWrapper : public GENOME_T {
   private:
+    using this_t = GenomeWrapper<GENOME_T>;
+
     static emp::Config empty_config;
 
   public:
@@ -72,13 +78,13 @@ namespace mabe {
     MABE_GENOME_TEST_FUN(GetClassName, "NoName", std::string);
     MABE_GENOME_TEST_FUN(GetConfig, empty_config, emp::Config);
     MABE_GENOME_TEST_FUN(Randomize, false, bool, emp::Random &);
-    // MABE_GENOME_TEST_FUN(Print);
-    // MABE_GENOME_TEST_FUN(OnBeforeRepro);
-    // MABE_GENOME_TEST_FUN(OnOffspringReady);
-    // MABE_GENOME_TEST_FUN(OnInjectReady);
-    // MABE_GENOME_TEST_FUN(OnBeforePlacement);
-    // MABE_GENOME_TEST_FUN(OnPlacement);
-    // MABE_GENOME_TEST_FUN(OnOrgDeath);
+    MABE_GENOME_TEST_FUN(Print, false, bool);
+    MABE_GENOME_TEST_FUN(OnBeforeRepro, false, bool);              // Genome about to be reproduced.
+    MABE_GENOME_TEST_FUN(OnOffspringReady, false, bool, this_t &); // Genome offspring; arg is parent genome
+    MABE_GENOME_TEST_FUN(OnInjectReady, false, bool);              // Genome about to be injected.
+    MABE_GENOME_TEST_FUN(OnBeforePlacement, false, bool);          // Genome about to be placed
+    MABE_GENOME_TEST_FUN(OnPlacement, false, bool);                // Genome just placed.
+    MABE_GENOME_TEST_FUN(OnOrgDeath, false, bool);                 // Genome about to die.
   };
 
 }
