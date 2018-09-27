21d2eea76276e1830dd4e407a91877aa7af0660c
Charles Ofria <charles.ofria@gmail.com>
2018-09-26 23:04:46 -0400

    Extended ConceptWrapper example to use two required functions.

--- examples/meta/ConceptWrapper.cc
+++ examples/meta/ConceptWrapper.cc
@@ -10,17 +10,20 @@
 #include "../../source/meta/ConceptWrapper.h"
 
 EMP_BUILD_CONCEPT( TestConcept,
-                   REQUIRED_FUN(RequiredFun, "Missing RequiredFun function!", void)
+                   REQUIRED_FUN(RequiredFun1, "Missing RequiredFun function 1!", void),
+                   REQUIRED_FUN(RequiredFun2, "Missing RequiredFun function 2!", void)
                  );
 
 
 struct MinimalClass {
-  void RequiredFun() { std::cout << "In MinimalClass::RequiredFun()" << std::endl; }
+  void RequiredFun1() { std::cout << "In MinimalClass::RequiredFun1()" << std::endl; }
+  void RequiredFun2() { std::cout << "In MinimalClass::RequiredFun2()" << std::endl; }
 };
 
 int main() {
   TestConcept<MinimalClass> test_class;
-  test_class.RequiredFun();
+  test_class.RequiredFun1();
+  test_class.RequiredFun2();
 
   std::cout << "Done!" << std::endl;
 }
