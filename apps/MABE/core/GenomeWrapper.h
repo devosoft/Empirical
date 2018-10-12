ea4c3128b3905d6f69eba7aade4b6dd214298448
Charles Ofria <charles.ofria@gmail.com>
2018-10-11 20:00:23 -0400

    Collapsed GenomeWrapper all the way down by using ConceptWrapper macro.

--- apps/MABE/core/GenomeWrapper.h
+++ apps/MABE/core/GenomeWrapper.h
@@ -31,78 +31,25 @@
 
 #include "base/macros.h"
 #include "config/config.h"
+#include "config/config_utils.h"
+#include "meta/ConceptWrapper.h"
 #include "meta/meta.h"
-
-// Macro to dynamically call a function either in the wrapped type (if it exists)
-// or return the default provided (otherwise).  The first two arguments are the
-// function name and its default return.  The remaining arguments in the ... must
-// be the return type (required) and all argument types (if any exist)
-
-#define MABE_GENOME_TEST_FUN_impl(NAME, DEFAULT, NO_ARGS, RETURN_T, ...)      \
-    /* Determine the return type if we call this function in the base class.  \
-       It should be undefined if the member functon does not exist!        */ \
-    template <typename T>                                                     \
-    using return_t_ ## NAME =                                                 \
-      EMP_IF( NO_ARGS,                                                        \
-        decltype( std::declval<T>().NAME() );,                                \
-        decltype( std::declval<T>().NAME(EMP_TYPES_TO_VALS(__VA_ARGS__)) );   \
-      )                                                                       \
-    /* Test whether function exists, based on SFINAE in using return type. */ \
-    static constexpr bool HasFun_ ## NAME() {                                 \
-      return emp::test_type<return_t_ ## NAME, GENOME_T>();                   \
-    }                                                                         \
-    /* Call appropriate version of the function.  First determine if there    \
-       is a non-void return type (i.e., do we return th result?) then         \
-       check if we can call the function in the wrapped class (if it          \
-       exists) or should we call/return the default (otherwise).           */ \
-    template <typename... Ts>                                                 \
-    RETURN_T NAME(Ts &&... args) {                                            \
-      EMP_IF( EMP_TEST_IF_VOID(RETURN_T),                                     \
-        {                                                                     \
-          if constexpr (HasFun_ ## NAME()) {                                  \
-            GENOME_T::NAME( std::forward<Ts>(args)... );                      \
-          }                                                                   \
-          else { DEFAULT; }                                                   \
-        },                                                                    \
-        {                                                                     \
-          if constexpr (HasFun_ ## NAME()) {                                  \
-            return GENOME_T::NAME( std::forward<Ts>(args)... );               \
-          }                                                                   \
-          else { return DEFAULT; }                                            \
-        }                                                                     \
-      )                                                                       \
-    }
-
-#define MABE_GENOME_TEST_FUN(NAME, DEFAULT, ...)                                              \
-  MABE_GENOME_TEST_FUN_impl(NAME, DEFAULT,                                                    \
-                            EMP_EQU( EMP_COUNT_ARGS(__VA_ARGS__), 1), /* Are there args? */   \
-                            EMP_GET_ARG(1, __VA_ARGS__),              /* Return type */       \
-                            EMP_POP_ARG(__VA_ARGS__) )                /* Arg types */
-
 
 namespace mabe {
 
-  template <typename GENOME_T>
-  class GenomeWrapper : public GENOME_T {
-  private:
-    using this_t = GenomeWrapper<GENOME_T>;
+  EMP_BUILD_CONCEPT( GenomeWrapper, GenomeBase,
+    OPTIONAL_FUN(GetClassName, "NoName", std::string),
+    OPTIONAL_FUN(GetConfig, emp::GetEmptyConfig(), emp::Config &),
+    OPTIONAL_FUN(Randomize, false, bool, emp::Random &),
+    OPTIONAL_FUN(Print, false, bool),
+    OPTIONAL_FUN(OnBeforeRepro, , void),                  // Genome about to be reproduced.
+    OPTIONAL_FUN(OnOffspringReady, , void, GenomeBase &),     // Genome offspring; arg is parent genome
+    OPTIONAL_FUN(OnInjectReady, , void),                  // Genome about to be injected.
+    OPTIONAL_FUN(OnBeforePlacement, , void),              // Genome about to be placed
+    OPTIONAL_FUN(OnPlacement, , void),                    // Genome just placed.
+    OPTIONAL_FUN(OnOrgDeath, , void)                      // Genome about to die.
+  );
 
-    static emp::Config empty_config;
-
-  public:
-    // Create a set of functions to determine if a memeber exists on GENOME_T in the form of
-    // constexpr bool has_fun_X()
-    MABE_GENOME_TEST_FUN(GetClassName, "NoName", std::string);
-    MABE_GENOME_TEST_FUN(GetConfig, empty_config, auto &);
-    MABE_GENOME_TEST_FUN(Randomize, false, bool, emp::Random &);
-    MABE_GENOME_TEST_FUN(Print, false, bool);
-    MABE_GENOME_TEST_FUN(OnBeforeRepro, , void);              // Genome about to be reproduced.
-    MABE_GENOME_TEST_FUN(OnOffspringReady, , void, this_t &); // Genome offspring; arg is parent genome
-    MABE_GENOME_TEST_FUN(OnInjectReady, , void);              // Genome about to be injected.
-    MABE_GENOME_TEST_FUN(OnBeforePlacement, , void);          // Genome about to be placed
-    MABE_GENOME_TEST_FUN(OnPlacement, , void);                // Genome just placed.
-    MABE_GENOME_TEST_FUN(OnOrgDeath, , void);                 // Genome about to die.
-  };
 
 }
 
