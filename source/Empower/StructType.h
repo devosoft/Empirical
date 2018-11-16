83488638a2e31e8e7cf2499e85492ddc19e15b2f
Charles Ofria <charles.ofria@gmail.com>
2018-11-16 16:35:12 -0500

    Add comments throughout StructType.

--- source/Empower/StructType.h
+++ source/Empower/StructType.h
@@ -22,18 +22,19 @@ namespace emp {
 
   class StructType {
   private:
-    emp::vector<VarInfo> vars;
-    TypeManager & type_manager;
-    size_t num_bytes;
-    bool active;
+    emp::vector<VarInfo> vars;   ///< Set of member variables declared in this structure.
+    TypeManager & type_manager;  ///< TypeManager to track type information in this structure.
+    size_t num_bytes;            ///< How big are structs of this type?
+    bool active;                 ///< Have Structs of this type been built?  If so, do not extend.
 
   public:
     StructType(TypeManager & _tmanager) : type_manager(_tmanager), num_bytes(0), active(false) { ; }
     ~StructType() { ; }
 
-    size_t GetSize() const { return num_bytes; }
-    bool IsActive() const { return active; }
+    size_t GetSize() const { return num_bytes; }  ///< How many bytes in Structs of this type?
+    bool IsActive() const { return active; }      ///< Have any Structs of this type been built?
 
+    /// And a new member variable to structs of this type.
     template <typename T>
     void AddMemberVar(const std::string & name) {
       emp_assert(active == false, "Cannot add member variables to an instantiated struct!");
@@ -42,6 +43,7 @@ namespace emp {
       num_bytes += type.GetSize();
     }
 
+    /// Construct a memory image using all default constructors.
     void DefaultConstruct(MemoryImage & memory) const {
       memory.resize(num_bytes);
       for (auto & x : vars) x.DefaultConstruct(memory);
@@ -49,10 +51,11 @@ namespace emp {
     }
   };
 
+  /// An instance of a struct type.
   class Struct {
   private:
-    const StructType & type;
-    MemoryImage memory;
+    const StructType & type;  ///< What type is this Struct (i.e., what members does it have?)
+    MemoryImage memory;       ///< Raw memory for storing struct information.
   public:
     Struct(const StructType & _type) : type(_type), memory(type.GetSize()) {
       type.Initialize(memory);
