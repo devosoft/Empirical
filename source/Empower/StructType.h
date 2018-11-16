a79d60036ff408d431ded47af139ec586476c339
Charles Ofria <charles.ofria@gmail.com>
2018-11-16 16:20:43 -0500

    Added active and default construction to StructType; added Struct class.

--- source/Empower/StructType.h
+++ source/Empower/StructType.h
@@ -25,22 +25,39 @@ namespace emp {
     emp::vector<VarInfo> vars;
     TypeManager & type_manager;
     size_t num_bytes;
+    bool active;
 
   public:
-    StructType(TypeManager & _tmanager) : type_manager(_tmanager), num_bytes(0) { ; }
+    StructType(TypeManager & _tmanager) : type_manager(_tmanager), num_bytes(0), active(false) { ; }
     ~StructType() { ; }
 
     size_t GetSize() const { return num_bytes; }
+    bool IsActive() const { return active; }
 
     template <typename T>
     void AddMemberVar(const std::string & name) {
+      emp_assert(active == false, "Cannot add member variables to an instantiated struct!");
       const Type & type = type_manager.GetType<T>();
       vars.push_back(type, name, num_bytes);
       num_bytes += type.GetSize();
     }
+
+    void DefaultConstruct(MemoryImage & memory) const {
+      memory.resize(num_bytes);
+      for (auto & x : vars) x.DefaultConstruct(memory);
+      active = true;
+    }
   };
 
-
+  class Struct {
+  private:
+    const StructType & type;
+    MemoryImage memory;
+  public:
+    Struct(const StructType & _type) : type(_type), memory(type.GetSize()) {
+      type.Initialize(memory);
+    }
+  };
 }
 
 #endif
