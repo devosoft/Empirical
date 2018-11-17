1b694b66f5901d10082b83dffdc37919338b130f
Charles Ofria <charles.ofria@gmail.com>
2018-11-16 19:28:12 -0500

    Setup all functions in Type to be const.

--- source/Empower/Type.h
+++ source/Empower/Type.h
@@ -29,10 +29,10 @@ namespace emp {
     virtual size_t GetID() const = 0;
     virtual size_t GetSize() const = 0;
 
-    virtual void DefaultConstruct(size_t mem_pos, MemoryImage & mem_image) = 0;
-    virtual void CopyConsturct(size_t mem_pos, const MemoryImage & mem_from, MemoryImage & mem_to) = 0;
-    virtual void CopyAssign(size_t mem_pos, const MemoryImage & mem_from, MemoryImage & mem_to) = 0;
-    virtual void Destruct(size_t mem_pos, MemoryImage & mem_image) = 0;
+    virtual void DefaultConstruct(size_t mem_pos, MemoryImage & mem_image) const = 0;
+    virtual void CopyConstruct(size_t mem_pos, const MemoryImage & mem_from, MemoryImage & mem_to) const = 0;
+    virtual void CopyAssign(size_t mem_pos, const MemoryImage & mem_from, MemoryImage & mem_to) const = 0;
+    virtual void Destruct(size_t mem_pos, MemoryImage & mem_image) const = 0;
 
     template <typename T> bool IsType() const;
   };
@@ -50,22 +50,22 @@ namespace emp {
     size_t GetSize() const { return sizeof(T); };               ///< How many bytes is this type?
 
     // Construct an object of type T at a specified MemoryImage position.
-    void DefaultConstruct(size_t mem_pos, MemoryImage & mem_image) {
+    void DefaultConstruct(size_t mem_pos, MemoryImage & mem_image) const {
       new (mem_image.GetPtr<T>(mem_pos).Raw()) T;
     }
 
     /// Copy constructor for type T at a specified MemoryImage position.
-    void CopyConsturct(size_t mem_pos, const MemoryImage & mem_from, MemoryImage & mem_to) {
+    void CopyConstruct(size_t mem_pos, const MemoryImage & mem_from, MemoryImage & mem_to) const {
       new (mem_to.GetPtr<T>(mem_pos).Raw()) T(mem_from.GetRef<T>(mem_pos));
     }
 
     /// Copy assignment for type T at a specified MemoryImage position.
-    void CopyAssign(size_t mem_pos, const MemoryImage & mem_from, MemoryImage & mem_to) {
+    void CopyAssign(size_t mem_pos, const MemoryImage & mem_from, MemoryImage & mem_to) const {
       mem_to.GetRef<T>(mem_pos) = mem_from.GetRef<T>(mem_pos);
     }
 
     /// Destructor for type T at a specified MemoryImage position
-    void Destruct(size_t mem_pos, MemoryImage & mem_image) {
+    void Destruct(size_t mem_pos, MemoryImage & mem_image) const {
 	    mem_image.GetPtr<T>(mem_pos)->~T();
     }
 
