da6d94a655fcd2d2c565058fbfed7bbde2cfa385
Charles Ofria <charles.ofria@gmail.com>
2018-11-25 19:36:47 -0500

    Starting to build a type that holds information about a string.

---
+++ source/meta/StringType.h
@@ -0,0 +1,22 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  StringType.h
+ *  @brief A type that maintains compile-time information about a string sequence.
+ *
+ */
+
+
+#ifndef EMP_STRING_TYPE_H
+#define EMP_STRING_TYPE_H
+
+#include "meta.h"
+#include "IntPack.h"
+
+namespace emp {
+
+}
+
+#endif
