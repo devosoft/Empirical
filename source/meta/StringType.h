c074d3e708db6105c2d412d120856cf9092d33bf
Charles Ofria <charles.ofria@gmail.com>
2018-11-25 23:40:55 -0500

    Initial draft of StringType code; not working due to unevaluated lambda.

--- source/meta/StringType.h
+++ source/meta/StringType.h
@@ -16,24 +16,31 @@
 #include "IntPack.h"
 
 #define EMP_TEXT_PACKET(MSG) [](){ return MSG; }
+#define EMP_TEXT_TYPE(MSG) decltype( StringPacketToIntPack( [](){ return MSG } ) )
 
 namespace emp {
 
   template <typename T, size_t pos>
-  struct StringPacketToIntPack {
+  struct StringPacketToIntPack_impl {
     static constexpr auto BuildPack(T packet) {
       constexpr std::string_view text = packet();
-      using recurse_t = decltype( StringPacketToIntPack<T,pos-1>::BuildPack(packet) );
-      return recurse_t::push_back< (int) text[pos-1] >();
+      using recurse_t = decltype( StringPacketToIntPack_impl<T,pos-1>::BuildPack(packet) );
+      return recurse_t::template push_back< (int) text[pos-1] >();
     }
   };
 
   template <typename T>
-  struct StringPacketToIntPack<T,0> {
+  struct StringPacketToIntPack_impl<T,0> {
     static constexpr auto BuildPack(T packet) {
       return IntPack<>();
     }
   };
+
+  template <typename T>
+  constexpr auto StringPacketToIntPack(T packet) {
+    constexpr std::string_view text = packet();
+    return emp::StringPacketToIntPack_impl<T,text.size()>::BuildPack(packet);
+  }
 
 }
 
