d5f42f62aed3c793ed01d77ae1f07f1f60d839b0
Charles Ofria <charles.ofria@gmail.com>
2018-11-25 23:28:40 -0500

    Added example file for StringType.

---
+++ examples/meta/StringType.cc
@@ -0,0 +1,16 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2016.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//
+//  Some example code for using IntPack
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "meta/StringType.h"
+
+int main()
+{
+}
