a31bcb2bcc369d67ae89db6ca75ef27ad452a5b9
Charles Ofria <charles.ofria@gmail.com>
2018-11-26 23:39:42 -0500

    Demonstrate working return values for EMP_TEXT_HASH

--- examples/meta/StringType.cc
+++ examples/meta/StringType.cc
@@ -1,5 +1,5 @@
 //  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2016.
+//  Copyright (C) Michigan State University, 2018.
 //  Released under the MIT Software license; see doc/LICENSE
 //
 //
@@ -15,4 +15,11 @@ int main()
 {
   EMP_TEXT_TYPE(test_t, "This is a test!");
   std::cout << test_t::ToString() << std::endl;
+
+
+  std::cout << EMP_TEXT_HASH("Test Hash!") << std::endl;
+  std::cout << EMP_TEXT_HASH("Test Hash2!") << std::endl;
+  std::cout << EMP_TEXT_HASH("Test Hash3!") << std::endl;
+  std::cout << EMP_TEXT_HASH("Test Hash!") << std::endl;
+  std::cout << EMP_TEXT_HASH("Test Hash3!") << std::endl;
 }
