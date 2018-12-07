5e0488a9cfba578f49053f90eddd1b8891b2ff0d
Emily Dolson <emilyldolson@gmail.com>
2018-12-07 12:32:22 -0500

    Old link to doxygen broke

--- third-party/download_doxygen.sh
+++ third-party/download_doxygen.sh
@@ -1,7 +1,7 @@
 if [ -d doxygen ]; then                                                         
         echo "doxygen directory exists!"                                        
 else                                                                            
-        curl -O http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.11.src.tar.gz
+        curl -O -L https://sourceforge.net/projects/doxygen/files/rel-1.8.11/doxygen-1.8.11.src.tar.gz/download
         tar -xvf doxygen-1.8.11.src.tar.gz  
         rm doxygen-1.8.11.src.tar.gz                                            
         mv doxygen-1.8.11 doxygen;                                              
