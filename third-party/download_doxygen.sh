1e992d6e5e97010dc0d87875e36591c13848a510
Emily Dolson <emilyldolson@gmail.com>
2018-12-07 12:42:40 -0500

    Fix file name

--- third-party/download_doxygen.sh
+++ third-party/download_doxygen.sh
@@ -2,7 +2,7 @@ if [ -d doxygen ]; then
         echo "doxygen directory exists!"                                        
 else                                                                            
         curl -O -L https://sourceforge.net/projects/doxygen/files/rel-1.8.11/doxygen-1.8.11.src.tar.gz/download
-        tar -xvf doxygen-1.8.11.src.tar.gz  
-        rm doxygen-1.8.11.src.tar.gz                                            
+        tar -xvf download  
+        rm download                                            
         mv doxygen-1.8.11 doxygen;                                              
 fi
