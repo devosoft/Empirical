f2278401067f993aeda105d525bdcf15ae88dc32
Charles Ofria <charles.ofria@gmail.com>
2018-12-12 23:11:52 -0500

    Continued building ValPack; now main templated class is in place.

--- source/meta/ValPack.h
+++ source/meta/ValPack.h
@@ -114,97 +114,97 @@ namespace emp {
   template <auto START, auto END, auto STEP=1>
   using ValPackRange = typename internal::vp_range<(START >= END), START, END, STEP>::type;
 
-  // IntPack with at least one value.
-  template <int V1, int... Vs>
-  struct IntPack<V1,Vs...> {
-    /// First value in IntPack
-    static constexpr int first = V1;
+  // ValPack with at least one value.
+  template <auto V1, auto... Vs>
+  struct ValPack<V1,Vs...> {
+    /// First value in ValPack
+    static constexpr auto first = V1;
 
-    /// Number of values in IntPack    
-    constexpr static int SIZE = 1+sizeof...(Vs);
+    /// Number of values in ValPack    
+    constexpr static size_t SIZE = 1+sizeof...(Vs);
 
-    using this_t = IntPack<V1,Vs...>;
-    using pop = IntPack<Vs...>;
+    using this_t = ValPack<V1,Vs...>;
+    using pop = ValPack<Vs...>;
 
-    /// Add an int to the front of an IntPack
-    template <int V> using push = IntPack<V, V1, Vs...>;
+    /// Add a value to the front of an ValPack
+    template <auto V> using push = ValPack<V, V1, Vs...>;
 
-    /// Add an int to the back of an IntPack
-    template <int V> using push_back = IntPack<V1, Vs..., V>;
+    /// Add a value to the back of an ValPack
+    template <auto V> using push_back = ValPack<V1, Vs..., V>;
 
-    /// Push V onto front of an IntPack if it does not equal X
-    template <int V, int X> using push_if_not = typename internal::ip_push_if_not<V,X,this_t>::result;
+    /// Push V onto front of an ValPack if it does not equal X
+    template <auto V, auto X> using push_if_not = typename internal::vp_push_if_not<V,X,this_t>::result;
 
-    /// Push V onto back of an IntPack if it does not equal X
-    template <int V, int X> using push_back_if_not = typename internal::ip_push_if_not<V,X,this_t>::back;
+    /// Push V onto back of an ValPack if it does not equal X
+    template <auto V, auto X> using push_back_if_not = typename internal::vp_push_if_not<V,X,this_t>::back;
 
-    /// Remove the first time value V appears from an IntPack
-    template <int V> using pop_val = typename internal::ip_loop<this_t, IntPack<>, false, 2>::template pop_val<V>;
+    /// Remove the first time value V appears from an ValPack
+    template <auto V> using pop_val = typename internal::vp_loop<this_t, ValPack<>, false, 2>::template pop_val<V>;
 
-    /// Remove the all appearances of value V from an IntPack
-    template <int V> using remove = typename internal::ip_loop<this_t, IntPack<>, false, 3>::template remove<V>;
+    /// Remove the all appearances of value V from an ValPack
+    template <auto V> using remove = typename internal::vp_loop<this_t, ValPack<>, false, 3>::template remove<V>;
 
-    /// Append one whole IntPack to the end of another.
-    template <typename T> using append = typename internal::ip_concat<this_t,T>::result;
+    /// Append one whole ValPack to the end of another.
+    template <typename T> using append = typename internal::vp_concat<this_t,T>::result;
 
-    /// Apply to a specified template with IntPack as template arguments.
+    /// Apply to a specified template with ValPack as template arguments.
     template <template <int...> class TEMPLATE> using apply = TEMPLATE<V1, Vs...>;
 
 
     /// ---=== Member Functions ===---
 
-    /// Return wheter an IntPack contains the value V.
-    constexpr static bool Has(int V) { return (V==V1) | pop::Has(V); }
+    /// Return wheter an ValPack contains the value V.
+    constexpr static bool Has(auto V) { return (V==V1) | pop::Has(V); }
 
-    /// Count the number of occurances of value V in IntPack.
-    constexpr static int Count(int V) { return pop::Count(V) + (V==V1); }
+    /// Count the number of occurances of value V in ValPack.
+    constexpr static size_t Count(auto V) { return pop::Count(V) + (V==V1); }
 
-    /// Determine the position at which V appears in IntPack.
-    constexpr static int GetID(int V) { return (V==V1) ? 0 : (1+pop::GetID(V)); }
+    /// Determine the position at which V appears in ValPack.
+    constexpr static int GetID(auto V) { return (V==V1) ? 0 : (1+pop::GetID(V)); }
 
-    /// Function to retrieve number of elements in IntPack
-    constexpr static int GetSize() { return SIZE; }
+    /// Function to retrieve number of elements in ValPack
+    constexpr static size_t GetSize() { return SIZE; }
 
-    /// Determine if there are NO value in an IntPack
+    /// Determine if there are NO value in an ValPack
     constexpr static bool IsEmpty() { return false; }
 
-    /// Determine if all values in IntPack are different from each other.
+    /// Determine if all values in ValPack are different from each other.
     constexpr static bool IsUnique() { return pop::IsUnique() && !pop::Has(V1); }
 
-    /// Add up all values in an IntPack
-    constexpr static int Sum() { return V1 + pop::Sum(); }
+    /// Add up all values in an ValPack
+    constexpr static auto Sum() { return V1 + pop::Sum(); }
 
-    /// Multiply together all value in an IntPack
-    constexpr static int Product() { return V1 * pop::Product(); }
+    /// Multiply together all value in an ValPack
+    constexpr static auto Product() { return V1 * pop::Product(); }
 
-    /// Find the smallest value in an IntPack, to a maximum of cap.
-    constexpr static int Min(int cap) { return cap < pop::Min(V1) ? cap : pop::Min(V1); }
+    /// Find the smallest value in an ValPack, to a maximum of cap.
+    constexpr static auto Min(auto cap) { return cap < pop::Min(V1) ? cap : pop::Min(V1); }
 
-    /// Find the overall smallest value in an IntPack
-    constexpr static int Min() { return pop::Min(V1); }
+    /// Find the overall smallest value in an ValPack
+    constexpr static auto Min() { return pop::Min(V1); }
 
-    /// Find the maximum value in an IntPack, to a minimum of floor.
-    constexpr static int Max(int floor) { return floor > pop::Max(V1) ? floor : pop::Max(V1); }
+    /// Find the maximum value in an ValPack, to a minimum of floor.
+    constexpr static auto Max(auto floor) { return floor > pop::Max(V1) ? floor : pop::Max(V1); }
 
-    /// Find the overall maximum value in an IntPack.
-    constexpr static int Max() { return pop::Max(V1); }
+    /// Find the overall maximum value in an ValPack.
+    constexpr static auto Max() { return pop::Max(V1); }
 
-    /// Use each value in an IntPack as an index and return results as a tuple.
+    /// Use each value in an ValPack as an index and return results as a tuple.
     template <typename T>
     constexpr static auto ApplyIndex(T && container) {
       return std::make_tuple(container[V1], container[Vs]...);
     }
 
-    /// Convert all values from an IntPack into a string, treating each as a char.
+    /// Convert all values from an ValPack into a string, treating each as a char.
     static std::string ToString() {
       return std::string(1, (char) V1) + pop::ToString();
     }
 
-    /// Print all values in an IntPack into a stream.
-    static void PrintInts(std::ostream & os=std::cout) {
+    /// Print all values in an ValPack into a stream.
+    static void PrintVals(std::ostream & os=std::cout) {
       os << V1;
       if (GetSize() > 1) os << ',';
-      pop::PrintInts(os);
+      pop::PrintVals(os);
     }
   };
 
