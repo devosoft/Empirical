32dc001a1a1329f70ee1ca76e1e0a1e061d73ced
Charles Ofria <charles.ofria@gmail.com>
2018-12-27 11:29:01 -0500

    stripped StringID class from string_utils.h since it's now in StringMap.h

--- source/tools/string_utils.h
+++ source/tools/string_utils.h
@@ -24,15 +24,6 @@
 #include "../meta/reflection.h"
 #include "../meta/StringType.h"
 
-/// Macro to covert a literal string to a unique ID, mostly at compile time.  Specifically,
-/// the string is converted to a unique type at compile time, which is then mapped to a unique
-/// function.  That function is run a run-time, but preserves the id to return so it is
-/// calculated only once.
-#define EMP_STRING_ID(STR)                         \
-  ([](){                                           \
-    constexpr auto temp = EMP_TEXT_PACK(STR);      \
-    return emp::StringID::Get<decltype(temp)>();   \
-  }())
 namespace emp {
 
   /// Return a const reference to an empty string.  This function is useful to implement other
@@ -564,40 +555,6 @@ namespace emp {
     return vals;
   }
 
-
-  /// A small class for maintaining unique string IDs.
-  class StringID {
-  private:
-    emp::Ptr<const std::string> str_ptr;   /// Pointer to a unique instance of this string.
-
-    static auto & GetStringSet() { 
-      static std::unordered_set< std::string > str_set;
-      return str_set;
-    }
-  public:
-    StringID(const StringID &) = default;
-    StringID(const std::string & in_string) {
-      auto [str_it, success] = GetStringSet().insert(in_string);
-      (void) success;           // Prevent unused variable error when not in debug mode.
-      str_ptr = &(*str_it);
-    }
-
-    size_t ToValue() const { return (size_t) str_ptr; }
-    const std::string & ToString() const { return *str_ptr; }
-
-    /// Get a StringID based on a StringType or another type with a 
-    /// static ToString() member function.
-    template <typename T>
-    static StringID Get() {
-      static StringID out(T::ToString());
-      return out;
-    }
-
-    /// Get a StringID based on a string (for completeness; this is the same as the constructor.)
-    static StringID Get(const std::string & str) {
-      return StringID(str);
-    }
-  };
 }
 
 #endif
