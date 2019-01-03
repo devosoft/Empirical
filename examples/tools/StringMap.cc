defc3660c30ba07b76a3ad589a0899c87ef49258
Charles Ofria <charles.ofria@gmail.com>
2019-01-02 22:47:25 -0500

    Added more examples for StringMap examples.

--- examples/tools/StringMap.cc
+++ examples/tools/StringMap.cc
@@ -15,10 +15,19 @@ int main()
   emp::StringMap<int> test_map;
 
   test_map["Ten"] = 10;
+  test_map["Five"] = 5;
   test_map["Zero"] = 0;
   test_map["NegTen"] = -10;
 
-  test_map["twenty"] = 20;
+  test_map["Fifteen"] = test_map["Ten"] + test_map["Five"];
+  test_map["Twenty"] = test_map["Ten"] * 2;
 
-  PRINT_VAL( test_map["Ten"] );
-}
+  PRINT_VAL( test_map["Fifteen"] );
+
+  test_map[EMP_STRING("Alpha")] = 1;
+  test_map[EMP_STRING("Beta")] = 2;
+  test_map[EMP_STRING("Gamma")] = 3;
+
+  PRINT_VAL(test_map[EMP_STRING("Beta")]);
+  PRINT_VAL(test_map["Beta"]);
+ }
