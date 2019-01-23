81dac6c4f6f676d9fd8a00309eadb96a8bee464e
Charles Ofria <charles.ofria@gmail.com>
2019-01-22 23:55:48 -0500

    Started building fitness analysis for Roulette Selection.

---
+++ apps/SelectionAnalyze/Roulette.cc
@@ -0,0 +1,14 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2018.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//  Calculate probabilities for selection using Roulette Selection (easy!)
+
+
+#include "SelectionData.h"
+
+int main()
+{
+  SelectionData data;
+}
+
