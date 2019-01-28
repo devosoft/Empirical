4298203e1f61915e7b54b864035b4f4d4a58e2b0
Riley Hoffman <umbrellacycles@gmail.com>
2019-01-28 15:58:26 -0500

    added test for generic function, updated test lexer

--- tests/ToolsTests/test_lexer.cc
+++ tests/ToolsTests/test_lexer.cc
@@ -14,11 +14,24 @@ TEST_CASE("Test Lexer", "[tools]")
   REQUIRE(lx.GetNumTokens() == 1);
   REQUIRE(lx.GetTokenID("vowel") == 255);
   REQUIRE(lx.GetTokenName(255) == "vowel");
+  REQUIRE(lx.MaxTokenID() == 256);
+  REQUIRE(!lx.TokenOK(257));
+  
   std::string word = "hello";
   lx.Process(word);
   REQUIRE(lx.GetLexeme() == word);
   word = "why";
   emp::Token t = lx.Process(word);
   REQUIRE(t.lexeme != word);
-  REQUIRE(t.lexeme == "");
+  REQUIRE((std::string)t == "");
+  
+  std::stringstream ss;
+  emp::TokenInfo ti = lx.GetTokenInfo("vowel");
+  ti.Print(ss);
+  REQUIRE(ss.str() == "Name:vowel  RegEx:\"[a-z]*([aeiou])+[a-z]*\"  ID:255  save_lexeme:0\n");
+  ss.str(std::string());
+  
+  emp::Lexer lxr;
+  lxr.Print(ss);
+  REQUIRE(ss.str() == "Num states = 0\nStop IDs:\n");
 }
