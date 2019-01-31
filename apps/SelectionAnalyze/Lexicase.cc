3208c24b0bdee09ddc4bce3dd4d5439b00500a29
Charles Ofria <charles.ofria@gmail.com>
2019-01-31 15:54:05 -0500

    Setup Lexicase.cc analysis to take file as command-line argument.

--- apps/SelectionAnalyze/Lexicase.cc
+++ apps/SelectionAnalyze/Lexicase.cc
@@ -1,23 +1,30 @@
 //  This file is part of Empirical, https://github.com/devosoft/Empirical
-//  Copyright (C) Michigan State University, 2018.
+//  Copyright (C) Michigan State University, 2019.
 //  Released under the MIT Software license; see doc/LICENSE
 //
 //  Calculate probabilities for selection using Roulette Selection (easy!)
 
+#include "../../source/config/command_line.h"
 
 #include "SelectionData.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-  SelectionData data("example.dat");
+  emp::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
+  std::string filename = "example.dat";
+  if (args.size() > 1) filename = args[1];
+  SelectionData data(filename);
 
   if (data.GetNumCriteria() == 0) {
     std::cout << "Warning: No criteria data found." << std::endl;
   }
 
   data.AnalyzeDominance();
-  std::cout << "Fitnesses:" << std::endl;
-  data.PrintFitnesses();
+  std::cout << "By organism (" << data.GetNumOrgs() << "):" << std::endl;
+  data.PrintOrgs();
+  std::cout << std::endl;
+  std::cout << "By criterion (" << data.GetNumCriteria() << "):" << std::endl;
+  data.PrintCriteria();
 
   // emp::vector< double > fit_data = data.GetFitData(0);
   // emp::IndexMap fit_map(num_orgs);
