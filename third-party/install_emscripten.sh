ce04610863665d7b2edcde5ea2c0720f5404fc0e
Emily Dolson <emilyldolson@gmail.com>
2019-02-25 15:57:44 -0500

    Emscripten now installs fast enough to smooth this out?

--- third-party/install_emscripten.sh
+++ third-party/install_emscripten.sh
@@ -7,7 +7,25 @@
 #./emsdk activate latest
 #source ./emsdk_env.sh
 
-git clone https://github.com/emilydolson/emsdk_portable_repo.git
-cd emsdk_portable_repo/emsdk_portable
-./emsdk activate
+#git clone https://github.com/emilydolson/emsdk_portable_repo.git
+#cd emsdk_portable_repo/emsdk_portable
+#./emsdk activate
+#source ./emsdk_env.sh
+
+# Get the emsdk repo
+git clone https://github.com/emscripten-core/emsdk.git
+
+# Enter that directory
+cd emsdk
+
+# Fetch the latest version of the emsdk (not needed the first time you clone)
+git pull
+
+# Download and install the latest SDK tools.
+./emsdk install latest
+
+# Make the "latest" SDK "active" for the current user. (writes ~/.emscripten file)
+./emsdk activate latest
+
+# Activate PATH and other environment variables in the current terminal
 source ./emsdk_env.sh
