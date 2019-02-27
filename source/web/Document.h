299f3f0f5e24c691c376f4c1aa3bfdac1fc3eee4
Matthew Andres Moreno <m.more500@gmail.com>
2019-02-26 22:32:05 -0500

    Add missing Input function to Document

--- source/web/Document.h
+++ source/web/Document.h
@@ -113,6 +113,7 @@ namespace web {
     web::Button Button (const std::string & in_id) { return web::Button(Find(in_id)); }
     web::Canvas Canvas (const std::string & in_id) { return web::Canvas(Find(in_id)); }
     web::FileInput FileInput (const std::string & in_id) { return web::FileInput(Find(in_id)); }
+    web::Input Input (const std::string & in_id) { return web::Input(Find(in_id)); }
     web::Image Image (const std::string & in_id) { return web::Image(Find(in_id)); }
     web::Selector Selector (const std::string & in_id) { return web::Selector(Find(in_id)); }
     web::Div Div (const std::string & in_id) { return web::Div(Find(in_id)); }
