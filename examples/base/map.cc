7bfa1ed35c1707c15cda14738edfd2bd0dac7b87
Charles Ofria <charles.ofria@gmail.com>
2019-03-05 12:03:21 -0500

    Added example file for emp::map.

---
+++ examples/base/map.cc
@@ -0,0 +1,12 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2019.
+//  Released under the MIT Software license; see doc/LICENSE
+
+#include <algorithm>
+
+#include "base/map.h"
+
+int main()
+{
+  emp::map<int, std::string> test_map;
+}
