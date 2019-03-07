208315f4661ecb151ffd20199b8da66d46b39a1b
Charles Ofria <charles.ofria@gmail.com>
2019-03-06 23:13:42 -0500

    Added map accessors to example file.

--- examples/base/map.cc
+++ examples/base/map.cc
@@ -9,4 +9,19 @@
 int main()
 {
   emp::map<int, std::string> test_map;
+
+  test_map[3] = "Three";
+  test_map[0] = "Zero";
+  test_map[1000] = "One Thousand";
+
+  std::cout << "3 = " << test_map[3] << std::endl << std::endl;
+
+  test_map[-10] = "Minus Ten";
+  test_map[1000000001] = "One Billion One";
+
+  for (auto [key, value] : test_map) {
+    std::cout << "  key = " << key
+	      << "  value = " << value
+	      << std::endl;
+  }
 }
