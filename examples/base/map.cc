91232bf7449ada2e8fcb51949819812edd18ac27
Charles Ofria <charles.ofria@gmail.com>
2019-03-11 23:48:17 -0400

    Added emp::map examples that use strings as keys.

--- examples/base/map.cc
+++ examples/base/map.cc
@@ -21,7 +21,27 @@ int main()
 
   for (auto [key, value] : test_map) {
     std::cout << "  key = " << key
-	      << "  value = " << value
-	      << std::endl;
+ 	      << "  value = " << value
+ 	      << std::endl;
   }
+
+
+  emp::map<std::string, int> test_map2;
+
+  test_map2["one"] = 1;
+  test_map2["two"] = 2;
+  test_map2["three"] = 3;
+  test_map2["counter"] = 0;
+
+  std::cout << test_map2["one"] << " + "
+	    << test_map2["two"] << " = "
+	    << test_map2["three"] << std::endl;
+
+  test_map2["counter"] = test_map2["one"] + test_map2["two"];
+  test_map2["counter"] += test_map2["three"];
+
+  std::cout << (2 * test_map2["counter"] * 2) << " = " << 24
+	    << std::endl;
+
+
 }
