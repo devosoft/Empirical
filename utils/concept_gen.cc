159cd48050084d07c435c8add1766df4a0dab44c
Charles Ofria <charles.ofria@gmail.com>
2019-04-05 22:40:30 -0400

    Started building a concept generator.

---
+++ utils/concept_gen.cc
@@ -0,0 +1,17 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019
+ *
+ *  @file  concept_gen.cc
+ *  @brief A system to generate dynamic concept code for C++17.
+ *  @note Status: PLANNING
+ */
+
+#include <iostream>
+
+#include "../source/config/ArgManager.h"
+
+int main(int argc, char *argv[])
+{
+}
