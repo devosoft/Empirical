fae256dc61f98ebcb968f78706d8cc49881745ec
Charles Ofria <charles.ofria@gmail.com>
2019-05-06 23:24:10 -0400

    Fixed Emphatic lexer to identify single-character identifiers

--- apps/Emphatic/EmphaticLexer.h
+++ apps/Emphatic/EmphaticLexer.h
@@ -25,7 +25,7 @@ public:
     IgnoreToken("/*...*/-Comments", "/[*]([^*]|([*]+[^*/]))*[*]+/");
 
     // Meaningful tokens have next priority.
-    token_identifier = AddToken("Identifier", "[a-zA-Z_][a-zA-Z0-9_]+");
+    token_identifier = AddToken("Identifier", "[a-zA-Z_][a-zA-Z0-9_]*");
     token_number = AddToken("Literal Number", "[0-9]+(.[0-9]+)?");
     token_string = AddToken("Literal String", "\\\"[^\"]*\\\"");
 
