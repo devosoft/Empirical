6d2f3fc34358d6e5ea14a6417b40cd6ef422d790
Charles Ofria <charles.ofria@gmail.com>
2019-05-16 00:10:02 -0400

    Setup initital use of ArgManager in SelectionAnalysis.

--- apps/SelectionAnalyze/Lexicase.cc
+++ apps/SelectionAnalyze/Lexicase.cc
@@ -5,15 +5,37 @@
 //  Calculate probabilities for selection using Roulette Selection (easy!)
 
 #include "config/command_line.h"
+#include "config/ArgManager.h"
 
 #include "SelectionData.h"
 
 int main(int argc, char* argv[])
 {
+  auto specs = emp::ArgManager::make_builtin_specs();
+  specs["no_row_headings"] = emp::ArgSpec(0, "Turn off headings on each row.");
+  specs["no_col_headings"] = emp::ArgSpec(0, "Turn off headings on each column.");
+  specs["no_headings"] = emp::ArgSpec(0, "Turn off all headings.");
+
   emp::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
-  std::string filename = "example.dat";
-  if (args.size() > 1) filename = args[1];
-  SelectionData data(filename);
+
+  emp::ArgManager am(argc, argv, specs);
+  am.PrintDiagnostic(std::cout);
+
+  bool use_row_headings = !am.UseArg("no_row_headings");
+  bool use_col_headings = !am.UseArg("no_col_headings");
+  if (am.UseArg("no_headings")) {
+    use_row_headings = use_col_headings = false;
+  }
+
+  std::string command = argv[0];
+  std::vector<std::string> filenames = *am.UseArg("_positional");
+
+  if (filenames.size() <= 1) {
+    std::cerr << "Format: " << command << " [input file] {output file} {flags}." << std::endl;
+    exit(1);
+  }
+
+  SelectionData data(filenames[1], use_row_headings, use_col_headings);
 
   if (data.GetNumCriteria() == 0) {
     std::cout << "Warning: No criteria data found." << std::endl;
