02a1faf097f94f32eb3d78bfdcb2cd05adf43e4c
Matthew Andres Moreno <m.more500@gmail.com>
2019-05-23 18:31:10 -0400

    Rearrange and document code

--- source/tools/MatchBin.h
+++ source/tools/MatchBin.h
@@ -3,8 +3,8 @@
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
  *  @date 2019
  *
- *  @file  TODO MatchBin.h
- *  @brief TODO A drop-in replacement for std::bitset, with additional bit magic features.
+ *  @file MatchBin.h
+ *  @brief A container that supports flexible tag-based lookup. .
  *
  */
 
@@ -30,6 +30,8 @@ namespace emp {
     (emp::vector<size_t>&, std::unordered_map<size_t, double>&, size_t)
   >;
 
+  /// Factory function that generates an exact match-order selector function
+  /// configured to filter out matches below a minimum-match threshold.
   selector_t ThreshSelector(double thresh){
 
     return [thresh](
@@ -54,7 +56,8 @@ namespace emp {
       };
   }
 
-
+  /// Factory function that generates a selector function that chooses
+  /// probabilisticly based on match quality with replacement.
   selector_t RouletteSelector(
     emp::Random &random,
     const double skew=0.1
@@ -88,18 +91,28 @@ namespace emp {
       };
   }
 
-  ///  TODO
+  /// A data container that allows lookup by tag similarity. It can be templated
+  /// on different tag types and is configurable on construction for (1) the
+  /// distance metric used to compute similarity between tags and (2) the
+  /// selector that is used to select which matches to return. Regulation
+  /// functionality is also provided, allowing dynamically adjustment of match
+  /// strength to a particular item (i.e., making all matches stronger/weaker).
+  /// A unique identifier is generated upon tag/item placement in the container.
+  /// This unique identifier can be used to view or edit the stored items and
+  /// their corresponding tags. Tag-based lookups return a list of matched
+  /// unique identifiers.
   template <typename Val, typename Tag> class MatchBin {
   private:
 
     const std::function<double(Tag, Tag)> metric;
     const selector_t selector;
-    size_t uid_stepper;
 
     std::unordered_map<size_t, Val> values;
     std::unordered_map<size_t, double> regulators;
     std::unordered_map<size_t, Tag> tags;
     emp::vector<size_t> uids;
+
+    size_t uid_stepper;
 
   public:
 
@@ -110,8 +123,12 @@ namespace emp {
     , selector(selector_)
     , uid_stepper(0) { ; }
 
+    /// Compare a query tag to all stored tags using the distance metric
+    /// function and return a vector of unique IDs chosen by the selector
+    /// function.
     emp::vector<size_t> Match(const Tag & query, size_t n=1) {
 
+      // compute distance between query and all stored tags
       std::unordered_map<Tag, double> matches;
       for (const auto &[uid, tag] : tags) {
         if (matches.find(tag) == matches.end()) {
@@ -119,6 +136,7 @@ namespace emp {
         }
       }
 
+      // apply regulation to generate match scores
       std::unordered_map<size_t, double> scores;
       for (auto uid : uids) {
         scores[uid] = matches[tags[uid]] * regulators[uid] + regulators[uid];
@@ -127,29 +145,8 @@ namespace emp {
       return selector(uids, scores, n);
     }
 
-    void Delete(size_t uid) {
-      values.erase(uid);
-      regulators.erase(uid);
-      tags.erase(uid);
-      uids.erase(std::remove(uids.begin(), uids.end(), uid), uids.end());
-    }
-
-    void Clear() {
-      values.clear();
-      regulators.clear();
-      tags.clear();
-      uids.clear();
-    }
-
-    void AdjRegulator(size_t uid, double amt) {
-      regulators[uid] = std::max(0.0, regulators.at(uid) + amt);
-    }
-
-    void SetRegulator(size_t uid, double amt) {
-      emp_assert(amt >= 0.0);
-      regulators.at(uid) = amt;
-    }
-
+    /// Put an item and associated tag in the container. Returns the uid for
+    /// that entry.
     size_t Put(const Val & v, const Tag & t) {
 
       const size_t orig = uid_stepper;
@@ -166,14 +163,34 @@ namespace emp {
       return uid_stepper;
     }
 
+
+    /// Delete an item and its associated tag.
+    void Delete(size_t uid) {
+      values.erase(uid);
+      regulators.erase(uid);
+      tags.erase(uid);
+      uids.erase(std::remove(uids.begin(), uids.end(), uid), uids.end());
+    }
+
+    /// Clear all items and tags.
+    void Clear() {
+      values.clear();
+      regulators.clear();
+      tags.clear();
+      uids.clear();
+    }
+
+    /// Access a reference single stored value by uid.
     Val & GetVal(const size_t uid) {
       return values.at(uid);
     }
 
+    /// Access a reference to a single stored tag by uid.
     Tag & GetTag(const size_t uid) {
       return tags.at(uid);
     }
 
+    /// Generate a vector of values corresponding to a vector of uids.
     emp::vector<Val> GetVals(const emp::vector<size_t> & uids) {
       emp::vector<Val> res;
       std::transform(
@@ -185,6 +202,7 @@ namespace emp {
       return res;
     }
 
+    /// Generate a vector of tags corresponding to a vector of uids.
     emp::vector<Tag> GetTags(const emp::vector<size_t> & uids) {
       emp::vector<Tag> res;
       std::transform(
@@ -196,11 +214,25 @@ namespace emp {
       return res;
     }
 
-
+    /// Get the number of items stored in the container.
     size_t Size() {
       return values.size();
     }
 
+    /// Add an amount to an item's regulator value. Positive amounts
+    /// downregulate the item and negative amounts upregulate it.
+    void AdjRegulator(size_t uid, double amt) {
+      regulators[uid] = std::max(0.0, regulators.at(uid) + amt);
+    }
+
+    /// Set an item's regulator value. Provided value must be greater than or
+    /// equal to zero. A value between zero and one upregulates the item, a
+    /// value of exactly one is neutral, and a value greater than one
+    /// upregulates the item.
+    void SetRegulator(size_t uid, double amt) {
+      emp_assert(amt >= 0.0);
+      regulators.at(uid) = amt;
+    }
 
   };
 
