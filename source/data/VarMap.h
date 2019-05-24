15506976f3fc1690cc35255207357ce2b4d838fe
Charles Ofria <charles.ofria@gmail.com>
2019-05-24 14:51:43 -0400

    Added a level of type-checking to VarMap.

--- source/data/VarMap.h
+++ source/data/VarMap.h
@@ -17,6 +17,7 @@
 #include "../base/Ptr.h"
 #include "../base/unordered_map.h"
 #include "../base/vector.h"
+#include "../meta/TypeID.h"
 #include "../tools/map_utils.h"
 #include "../tools/string_utils.h"
 
@@ -26,8 +27,10 @@ namespace emp {
   private:
     struct VarBase {
       std::string name;                              ///< Name of this variable.
+      size_t type_value;                             ///< Unique ID for the underlying type.
 
-      VarBase(const std::string & in_name) : name(in_name) { ; }
+      VarBase(const std::string & in_name, size_t in_type_value)
+      : name(in_name), type_value(in_type_value) { ; }
     };
 
     template <typename T>
@@ -35,7 +38,7 @@ namespace emp {
       T value;                                       ///< Current value of this variable.
 
       VarInfo(const std::string & name, T && in_value)
-      : VarBase(name)
+      : VarBase(name, GetTypeValue<T>())
       , value( std::forward<T>(in_value) )
       { ; }
     };
@@ -46,11 +49,12 @@ namespace emp {
   public:
     VarMap() { ; }
 
-    const std::string & GetName(size_t id) const { return vars[id].name; }
+    const std::string & GetName(size_t id) const { return vars[id]->name; }
     size_t GetID(const std::string & name) const { return Find(id_map, name, (size_t) -1); }
 
     template <typename T>
     size_t Add(const std::string & name, T && value) {
+      emp_assert( emp::Has(id_map, name) == false );
       const size_t id = vars.size();
       emp::Ptr<VarInfo<T>> new_ptr = NewPtr<VarInfo<T>>( name, std::forward<T>(value) );
       vars.push_back(new_ptr);
@@ -60,6 +64,8 @@ namespace emp {
 
     template <typename T>
     T & Get(size_t id) {
+      emp_assert(id < vars.size());
+      emp_assert(vars[id].type_value = emp::GetTypeValue<T>());
       emp::Ptr<VarInfo<T>> ptr = vars[id].Cast<VarInfo<T>>();
       return ptr->value;
     }
