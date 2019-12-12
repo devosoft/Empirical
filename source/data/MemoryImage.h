4cb20e0f1ad4bdcfd85755399f37a8f4a652c1da
Charles Ofria <charles.ofria@gmail.com>
2019-12-12 13:27:20 -0500

    Added Construct(), Destruct(), and CopyObj() to  MemoryImage.

--- source/data/MemoryImage.h
+++ source/data/MemoryImage.h
@@ -33,18 +33,42 @@ namespace emp {
     ~MemoryImage() { emp_assert(memory.size() == 0, "Must manually delete memory before destructing."); }
 
     template <typename T> emp::Ptr<T> GetPtr(size_t pos) {
+      emp_assert(pos + sizeof(T) <= GetSize(), pos, sizeof(T), GetSize());
       return reinterpret_cast<T*>(&memory[pos]);
     }
     template <typename T> T & GetRef(size_t pos) {
+      emp_assert(pos + sizeof(T) <= GetSize(), pos, sizeof(T), GetSize());
       return *(reinterpret_cast<T*>(&memory[pos]));
     }
     template <typename T> const T & GetRef(size_t pos) const {
+      emp_assert(pos + sizeof(T) <= GetSize(), pos, sizeof(T), GetSize());
       return *(reinterpret_cast<const T*>(&memory[pos]));
     }
 
     byte_t & operator[](size_t pos) { return memory[pos]; }
     const byte_t & operator[](size_t pos) const { return memory[pos]; }
     size_t size() const { return memory.size(); }
+    size_t GetSize() const { return memory.size(); }
+
+    // Build a new object of the provided type at the memory position indicated.
+    template <typename T, typename... ARGS>
+    void Construct(size_t pos, ARGS &&... args) {
+      emp_assert(pos + sizeof(T) <= GetSize(), pos, sizeof(T), GetSize());
+      new (GetPtr<T>(pos).Raw( std::forward<ARGS>(args)... )) T;
+    }
+
+    // Destruct an object of the provided type at the memory position indicated; don't release memory!
+    template <typename T>
+    void Destruct(size_t pos) {
+      emp_assert(pos + sizeof(T) <= GetSize(), pos, sizeof(T), GetSize());
+    	GetPtr<T>(pos)->~T();
+    }
+
+    // Copy an object from another MemoryImage with an identical layout.
+    template<typename T>
+    void CopyObj(size_t pos, MemoryImage & image2) {
+      Construct<T, const T &>(pos, image2.GetRef<T>(pos));
+    }
   };
 
   template <unsigned int SIZE>
@@ -54,12 +78,16 @@ namespace emp {
 
   public:
     ~MemoryArray() { }
+
   };
 
-  class MemoryVector : MemoryImage< emp::array<std::byte, SIZE> > {
+  class MemoryVector : MemoryImage< emp::vector<std::byte> > {
   protected:
 
   public:
+    MemoryVector(size_t num_bytes) {
+      memory.resize(num_bytes);
+    }
     ~MemoryVector() { }
 
     void resize(size_t new_size) { memory.resize(new_size); }
