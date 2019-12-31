723ec76ef2cab66f9fd41bc8418d00675870b7a8
Charles Ofria <charles.ofria@gmail.com>
2019-12-31 01:00:22 -0500

    Moved all DataMap2 code to DataMap.

--- examples/data/DataMap.cc
+++ examples/data/DataMap.cc
@@ -29,16 +29,16 @@ int main()
             << " id5 = " << id5
             << std::endl;
 
-  dmap.GetDefault<std::string>("name") = "FirstOrg";
-  dmap.GetDefault<std::string>(id4) = "Org TWO!";
-  dmap.GetDefault<std::string>("name3") = "Test Output!";
-  dmap.GetDefault<double>("fitness") = 1000000.1;
+  dmap.Get<std::string>("name") = "FirstOrg";
+  dmap.Get<std::string>(id4) = "Org TWO!";
+  dmap.Get<std::string>("name3") = "Test Output!";
+  dmap.Get<double>("fitness") = 1000000.1;
 
-  std::cout << "Name 1 = " << dmap.GetDefault<std::string>("name") << std::endl;
-  std::cout << "Name 2 = " << dmap.GetDefault<std::string>("name2") << std::endl;
-  std::cout << "Name 3 = " << dmap.GetDefault<std::string>("name3") << std::endl;
-  std::cout << "Name 3 = " << dmap.GetDefault<std::string>(id5) << std::endl;
-  std::cout << "Name 3 = " << dmap.GetDefault<std::string>( dmap.GetID("name3") ) << std::endl;
+  std::cout << "Name 1 = " << dmap.Get<std::string>("name") << std::endl;
+  std::cout << "Name 2 = " << dmap.Get<std::string>("name2") << std::endl;
+  std::cout << "Name 3 = " << dmap.Get<std::string>("name3") << std::endl;
+  std::cout << "Name 3 = " << dmap.Get<std::string>(id5) << std::endl;
+  std::cout << "Name 3 = " << dmap.Get<std::string>( dmap.GetID("name3") ) << std::endl;
 
   std::cout << "\nTypes:\n";
   std::cout << "fitness: " << dmap.GetType("fitness") << std::endl;
@@ -48,114 +48,121 @@ int main()
   std::cout << "name3:   " << dmap.GetType("name3") << std::endl;
 
   
-  // -------------- Examples of EXTERNAL images --------------
+  // -------------- Examples of MULTIPLE DataMaps --------------
 
   emp::DataMap data_map;
 
-  const auto & default_image = data_map.GetDefaultImage();
-
-  std::cout << "\nAt start:\n  default_image.GetSize() == " << data_map.GetImageSize()
+  std::cout << "\nAt start:\n  data_map.GetSize() == " << data_map.GetSize()
             << std::endl;
 
   // Add some values...
 
-  size_t idA = data_map.Add<int>("test_int", 20);
+  size_t idA = data_map.Add<int>("test_int", 10);
   size_t idB = data_map.Add<double>("test_double", 111.111);
   size_t idC = data_map.Add<std::string>("short_string", "string1");
   size_t idD = data_map.Add<std::string>("long_string", "This is a much longer string that shouldn't be used for short-string optimization.");
 
   std::cout << "\nAfter inserting an int, a double, and two strings:"
-            << "\n  default_image.GetSize() == " << data_map.GetImageSize()
-            << "\n  (A) Default: " << default_image.Get<int>(idA)
-            << "\n  (B) Default: " << default_image.Get<double>(idB)
-            << "\n  (C) Default: " << default_image.Get<std::string>(idC)
-            << "\n  (D) Default: " << default_image.Get<std::string>(idD)
+            << "\n  data_map.GetSize() == " << data_map.GetSize()
+            << "\n  (A) data_map : " << data_map.Get<int>(idA)
+            << "\n  (B) data_map : " << data_map.Get<double>(idB)
+            << "\n  (C) data_map : " << data_map.Get<std::string>(idC)
+            << "\n  (D) data_map : " << data_map.Get<std::string>(idD)
             << std::endl;
 
-  emp::DataMap::MemoryImage image1(data_map);
+  std::cout << "\nAnd calling by NAME rather than ID:"
+            << "\n  data_map.GetSize() == " << data_map.GetSize()
+            << "\n  (A) data_map : " << data_map.Get<int>("test_int")
+            << "\n  (B) data_map : " << data_map.Get<double>("test_double")
+            << "\n  (C) data_map : " << data_map.Get<std::string>("short_string")
+            << "\n  (D) data_map : " << data_map.Get<std::string>("long_string")
+            << std::endl;
+
+  emp::DataMap data_map2(data_map);
 
   std::cout << "\nAfter initializing image 1:"
-            << "\n  image1.GetSize() == " << image1.GetSize()
-            << "\n  default_image.GetSize() == " << data_map.GetImageSize()
-            << "\n  (A) Default: " << default_image.Get<int>(idA)
-            << "\n      Image1 : " << image1.Get<int>(idA)
-            << "\n  (B) Default: " << default_image.Get<double>(idB)
-            << "\n      Image1 : " << image1.Get<double>(idB)
-            << "\n  (C) Default: " << default_image.Get<std::string>(idC)
-            << "\n      Image1 : " << image1.Get<std::string>(idC)
-            << "\n  (D) Default: " << default_image.Get<std::string>(idD)
-            << "\n      Image1 : " << image1.Get<std::string>(idD)
+            << "\n  data_map.GetSize() == " << data_map.GetSize()
+            << "\n  data_map2.GetSize() == " << data_map2.GetSize()
+            << "\n  (A) data_map : " << data_map.Get<int>(idA)
+            << "\n      data_map2: " << data_map2.Get<int>(idA)
+            << "\n  (B) data_map : " << data_map.Get<double>(idB)
+            << "\n      data_map2: " << data_map2.Get<double>(idB)
+            << "\n  (C) data_map : " << data_map.Get<std::string>(idC)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idC)
+            << "\n  (D) data_map : " << data_map.Get<std::string>(idD)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idD)
             << std::endl;
 
-  data_map.GetDefault<double>(idB) = 222.222;
+  data_map.Set<int>("test_int", 20);
+  data_map.Set<double>(idB, 222.222);
 
-  std::cout << "\nAfter changing the default double value to 222.222:"
-            << "\n  image1.GetSize() == " << image1.GetSize()
-            << "\n  default_image.GetSize() == " << data_map.GetImageSize()
-            << "\n  (A) Default: " << default_image.Get<int>(idA)
-            << "\n      Image1 : " << image1.Get<int>(idA)
-            << "\n  (B) Default: " << default_image.Get<double>(idB)
-            << "\n      Image1 : " << image1.Get<double>(idB)
-            << "\n  (C) Default: " << default_image.Get<std::string>(idC)
-            << "\n      Image1 : " << image1.Get<std::string>(idC)
-            << "\n  (D) Default: " << default_image.Get<std::string>(idD)
-            << "\n      Image1 : " << image1.Get<std::string>(idD)
+  std::cout << "\nAfter changing the test_int to 20 and test_double value to 222.222:"
+            << "\n  data_map.GetSize() == " << data_map.GetSize()
+            << "\n  data_map2.GetSize() == " << data_map2.GetSize()
+            << "\n  (A) data_map : " << data_map.Get<int>(idA)
+            << "\n      data_map2: " << data_map2.Get<int>(idA)
+            << "\n  (B) data_map : " << data_map.Get<double>(idB)
+            << "\n      data_map2: " << data_map2.Get<double>(idB)
+            << "\n  (C) data_map : " << data_map.Get<std::string>(idC)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idC)
+            << "\n  (D) data_map : " << data_map.Get<std::string>(idD)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idD)
             << std::endl;
 
-  image1.Get<std::string>(idC)[6] = '2';
+  data_map2.Get<std::string>(idC)[6] = '2';
 
-  std::cout << "\nAfter changing the image1 short-string value to 'string2':"
-            << "\n  image1.GetSize() == " << image1.GetSize()
-            << "\n  default_image.GetSize() == " << data_map.GetImageSize()
-            << "\n  (A) Default: " << default_image.Get<int>(idA)
-            << "\n      Image1 : " << image1.Get<int>(idA)
-            << "\n  (B) Default: " << default_image.Get<double>(idB)
-            << "\n      Image1 : " << image1.Get<double>(idB)
-            << "\n  (C) Default: " << default_image.Get<std::string>(idC)
-            << "\n      Image1 : " << image1.Get<std::string>(idC)
-            << "\n  (D) Default: " << default_image.Get<std::string>(idD)
-            << "\n      Image1 : " << image1.Get<std::string>(idD)
+  std::cout << "\nAfter changing the data_map2 short-string value to 'string2':"
+            << "\n  data_map.GetSize() == " << data_map.GetSize()
+            << "\n  data_map2.GetSize() == " << data_map2.GetSize()
+            << "\n  (A) data_map : " << data_map.Get<int>(idA)
+            << "\n      data_map2: " << data_map2.Get<int>(idA)
+            << "\n  (B) data_map : " << data_map.Get<double>(idB)
+            << "\n      data_map2: " << data_map2.Get<double>(idB)
+            << "\n  (C) data_map : " << data_map.Get<std::string>(idC)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idC)
+            << "\n  (D) data_map : " << data_map.Get<std::string>(idD)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idD)
             << std::endl;
 
 
-  emp::DataMap::MemoryImage image2(data_map);
+  emp::DataMap data_map3(data_map);
 
-  std::cout << "\nAfter initializing image2 with current defaults:"
-            << "\n  image1.GetSize() == " << image1.GetSize()
-            << "\n  image2.GetSize() == " << image2.GetSize()
-            << "\n  default_image.GetSize() == " << data_map.GetImageSize()
-            << "\n  (A) Default: " << default_image.Get<int>(idA)
-            << "\n      Image1 : " << image1.Get<int>(idA)
-            << "\n      Image2 : " << image2.Get<int>(idA)
-            << "\n  (B) Default: " << default_image.Get<double>(idB)
-            << "\n      Image1 : " << image1.Get<double>(idB)
-            << "\n      Image2 : " << image2.Get<double>(idB)
-            << "\n  (C) Default: " << default_image.Get<std::string>(idC)
-            << "\n      Image1 : " << image1.Get<std::string>(idC)
-            << "\n      Image2 : " << image2.Get<std::string>(idC)
-            << "\n  (D) Default: " << default_image.Get<std::string>(idD)
-            << "\n      Image1 : " << image1.Get<std::string>(idD)
-            << "\n      Image2 : " << image2.Get<std::string>(idD)
+  std::cout << "\nAfter initializing data_map3 with current defaults:"
+            << "\n  data_map.GetSize() == " << data_map.GetSize()
+            << "\n  data_map2.GetSize() == " << data_map2.GetSize()
+            << "\n  data_map3.GetSize() == " << data_map3.GetSize()
+            << "\n  (A) data_map : " << data_map.Get<int>(idA)
+            << "\n      data_map2: " << data_map2.Get<int>(idA)
+            << "\n      data_map3: " << data_map3.Get<int>(idA)
+            << "\n  (B) data_map : " << data_map.Get<double>(idB)
+            << "\n      data_map2: " << data_map2.Get<double>(idB)
+            << "\n      data_map3: " << data_map3.Get<double>(idB)
+            << "\n  (C) data_map : " << data_map.Get<std::string>(idC)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idC)
+            << "\n      data_map3: " << data_map3.Get<std::string>(idC)
+            << "\n  (D) data_map : " << data_map.Get<std::string>(idD)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idD)
+            << "\n      data_map3: " << data_map3.Get<std::string>(idD)
             << std::endl;
 
-  image1.Get<std::string>(idD)[6] = '2';
+  data_map2.Get<std::string>(idD)[6] = '2';
 
-  std::cout << "\nAfter changing the image1 LONG-string value to have a '2':"
-            << "\n  image1.GetSize() == " << image1.GetSize()
-            << "\n  image2.GetSize() == " << image2.GetSize()
-            << "\n  default_image.GetSize() == " << data_map.GetImageSize()
-            << "\n  (A) Default: " << default_image.Get<int>(idA)
-            << "\n      Image1 : " << image1.Get<int>(idA)
-            << "\n      Image2 : " << image2.Get<int>(idA)
-            << "\n  (B) Default: " << default_image.Get<double>(idB)
-            << "\n      Image1 : " << image1.Get<double>(idB)
-            << "\n      Image2 : " << image2.Get<double>(idB)
-            << "\n  (C) Default: " << default_image.Get<std::string>(idC)
-            << "\n      Image1 : " << image1.Get<std::string>(idC)
-            << "\n      Image2 : " << image2.Get<std::string>(idC)
-            << "\n  (D) Default: " << default_image.Get<std::string>(idD)
-            << "\n      Image1 : " << image1.Get<std::string>(idD)
-            << "\n      Image2 : " << image2.Get<std::string>(idD)
+  std::cout << "\nAfter changing the data_map2 LONG-string value to have a '2':"
+            << "\n  data_map.GetSize() == " << data_map.GetSize()
+            << "\n  data_map2.GetSize() == " << data_map2.GetSize()
+            << "\n  data_map3.GetSize() == " << data_map3.GetSize()
+            << "\n  (A) data_map : " << data_map.Get<int>(idA)
+            << "\n      data_map2: " << data_map2.Get<int>(idA)
+            << "\n      data_map3: " << data_map3.Get<int>(idA)
+            << "\n  (B) data_map : " << data_map.Get<double>(idB)
+            << "\n      data_map2: " << data_map2.Get<double>(idB)
+            << "\n      data_map3: " << data_map3.Get<double>(idB)
+            << "\n  (C) data_map : " << data_map.Get<std::string>(idC)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idC)
+            << "\n      data_map3: " << data_map3.Get<std::string>(idC)
+            << "\n  (D) data_map : " << data_map.Get<std::string>(idD)
+            << "\n      data_map2: " << data_map2.Get<std::string>(idD)
+            << "\n      data_map3: " << data_map3.Get<std::string>(idD)
             << std::endl;
 
 }
