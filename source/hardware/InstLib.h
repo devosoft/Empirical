f37babea5d85e4aa7f8eb54e0855c91812cfd3b2
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-01-07 00:07:55 -0500

    Format inst lib manifest as csv

--- source/hardware/InstLib.h
+++ source/hardware/InstLib.h
@@ -200,8 +200,9 @@ namespace emp {
 
     /// Print out summary of instruction library.
     void PrintManifest(std::ostream & os=std::cout) const {
+      os << "id" << "," << "name" << std::endl;
       for (size_t i = 0; i < inst_lib.size(); ++i) {
-        os << i << " " << inst_lib[i].name << std::endl;
+        os << i << "," << inst_lib[i].name << std::endl;
       }
     }
 
