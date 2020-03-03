b9bd935c1d041b02ecf09b8fe7edfe17e467e3cc
Charles Ofria <charles.ofria@gmail.com>
2020-03-02 23:55:32 -0500

    Added examples of using input arguments.

--- examples/config/SettingCombos.cc
+++ examples/config/SettingCombos.cc
@@ -17,9 +17,23 @@ int main()
   config_set.AddSetting<int>("int1") = { 1, 2, 3, 4 };
   config_set.AddSetting<std::string>("string") = { "a", "b", "cde" };
   config_set.AddSetting<int>("int2") = { 5 };
-  config_set.AddSetting<double>("double") = { 1.1, 2.2 };
+  config_set.AddSetting<double>("double", "A double value!", "-d") = { 1.1, 2.2 };
 
   do {
     std::cout << config_set.CurString() << std::endl;
   } while (config_set.Next());
+
+  std::cout << "------------ Process Args ------------\n";
+
+  auto out_args =
+    config_set.ProcessOptions( { "testing", "--int1", "5,6,7", "unused", "-d", "3.3,4.4,5.5"} );
+
+  do {
+    std::cout << config_set.CurString() << std::endl;
+  } while (config_set.Next());
+
+  std::cout << "Remaining Args:\n";
+
+  for (auto x : out_args) std::cout << "  " << x << std::endl;
+
 }
