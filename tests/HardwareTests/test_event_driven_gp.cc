43c659962f621bd6da882da941f8b54bf9dc10fc
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-03-10 15:26:13 -0400

    Apply stylistic and formatting tweaks

--- tests/HardwareTests/test_event_driven_gp.cc
+++ tests/HardwareTests/test_event_driven_gp.cc
@@ -10,22 +10,65 @@
 TEST_CASE("Test Event Driven GP", "[Hardware]")
 {
 	// Event lib
-	std::function<void(emp::EventDrivenGP&, const emp::EventDrivenGP::Event &)>  HandleEvent_Message = [](emp::EventDrivenGP & hw, const emp::EventDrivenGP::Event & event) { hw.SpawnCore(event.affinity, hw.GetMinBindThresh(), event.msg); };
+	auto  HandleEvent_Message = [](
+		emp::EventDrivenGP & hw,
+		const emp::EventDrivenGP::Event & event
+	) {
+		hw.SpawnCore(event.affinity, hw.GetMinBindThresh(), event.msg);
+	};
 	emp::EventDrivenGP::Instruction inst(0, 1); // increment id = 0, inc arg 0 = 1
-	std::function<void(emp::EventDrivenGP &, const emp::EventDrivenGP::Event &)> func = [inst](emp::EventDrivenGP & hd, const emp::EventDrivenGP::Event & ev){ if(ev.HasProperty("add")){ hd.ProcessInst(inst); } };
+	auto func = [inst](
+		emp::EventDrivenGP & hd,
+		const emp::EventDrivenGP::Event & ev
+	){
+		if (ev.HasProperty("add")) hd.ProcessInst(inst);
+	};
 	emp::EventLib<emp::EventDrivenGP> event_lib;
 	event_lib.AddEvent("Message", HandleEvent_Message, "Event for exchanging messages (agent-agent, world-agent, etc.)");
 	event_lib.RegisterDispatchFun(0, func);
-    
+
 	// Instruction lib
-	std::function<void(emp::EventDrivenGP&, const emp::EventDrivenGP::Instruction &)>  Inst_Inc = [](emp::EventDrivenGP & hw, const emp::EventDrivenGP::Instruction & inst) { emp::EventDrivenGP::State & state = hw.GetCurState(); ++state.AccessLocal(inst.args[0]);};
-	std::function<void(emp::EventDrivenGP&, const emp::EventDrivenGP::Instruction &)>  Inst_Dec = [](emp::EventDrivenGP & hw, const emp::EventDrivenGP::Instruction & inst) { emp::EventDrivenGP::State & state = hw.GetCurState(); --state.AccessLocal(inst.args[0]);};
-	std::function<void(emp::EventDrivenGP&, const emp::EventDrivenGP::Instruction &)>  Inst_Not = [](emp::EventDrivenGP & hw, const emp::EventDrivenGP::Instruction & inst) { emp::EventDrivenGP::State & state = hw.GetCurState(); state.SetLocal(inst.args[0], state.GetLocal(inst.args[0]) == 0.0);};
+	auto  Inst_Inc = [](
+		emp::EventDrivenGP & hw,
+		const emp::EventDrivenGP::Instruction & inst
+	) {
+		emp::EventDrivenGP::State & state = hw.GetCurState();
+		++state.AccessLocal(inst.args[0]);
+	};
+	auto Inst_Dec = [](
+		emp::EventDrivenGP & hw,
+		const emp::EventDrivenGP::Instruction & inst
+	) {
+		emp::EventDrivenGP::State & state = hw.GetCurState();
+		--state.AccessLocal(inst.args[0]);
+	};
+	auto Inst_Not = [](
+		emp::EventDrivenGP & hw,
+		const emp::EventDrivenGP::Instruction & inst
+	) {
+		emp::EventDrivenGP::State & state = hw.GetCurState();
+		state.SetLocal(inst.args[0], state.GetLocal(inst.args[0]) == 0.0);
+	};
 	emp::InstLib<emp::EventDrivenGP> inst_lib;
-	inst_lib.AddInst("Inc", Inst_Inc, 1, "Increment value in local memory Arg1");
-	inst_lib.AddInst("Dec", Inst_Dec, 1, "Decrement value in local memory Arg1");
-	inst_lib.AddInst("Not", Inst_Not, 1, "Logically toggle value in local memory Arg1");
-	
+	inst_lib.AddInst(
+		"Inc",
+		Inst_Inc,
+		1,
+		"Increment value in local memory Arg1"
+	);
+	inst_lib.AddInst(
+		"Dec",
+		Inst_Dec,
+		1,
+		"Decrement value in local memory Arg1"
+	);
+	inst_lib.AddInst(
+		"Not",
+		Inst_Not,
+		1,
+		"Logically toggle value in local memory Arg1"
+	);
+
 	// Constructing EventDrivenGP
 	emp::EventDrivenGP gp_default;
 	REQUIRE( gp_default.GetInstLib() == gp_default.DefaultInstLib() );
@@ -36,7 +79,7 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	REQUIRE( (gp.GetRandom().GetSeed() == 50) );
 	REQUIRE( gp.GetRandomPtr() == rndp );
 	REQUIRE( gp.IsStochasticFunCall() );
-	
+
 	// Getters
 	std::stringstream ss;
 	emp::EventDrivenGP::Program c_prgm = gp.GetConstProgram();
@@ -49,7 +92,7 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	gp.PrintProgram(ss);
 	REQUIRE(ss.str() == "");
 	ss.str(std::string());
-	
+
 	// Pushing a function with an instruction
 	emp::EventDrivenGP::Function fx;
 	fx.PushInst(inst);
@@ -61,7 +104,7 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	std::string savedProgram = ss.str();	// save program for loading later
 	REQUIRE(savedProgram == "Fn-00000000:\n  Inc[00000000](1,0,0)\n\n");
 	ss.str(std::string());
-	
+
 	// More instructions
 	emp::EventDrivenGP::Instruction i_dec(1, 1); // decrement id = 1, dec arg 0 = 1
 	gp.SetInst(0, 0, i_dec); // do same but just with inst params
@@ -69,7 +112,7 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	gp.PrintInst(i_dec, ss);
 	REQUIRE(ss.str() == "Dec 1");
 	ss.str(std::string());
-	
+
 	// Testing states and SingleProcess
 	emp::EventDrivenGP::Instruction i_not(2, 0); // not id = 2, not arg 0 = 0
 	gp.PushInst(i_not); // will be pushed onto the end of the last function
@@ -85,7 +128,7 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	gp.PrintState(ss);
 	REQUIRE(ss.str() == "Shared memory: \nTraits: []\nErrors: 0\nEvent queue: \nCore 0(CID=0):\n  Call stack (2):\n    --TOP--\n    Inst ptr: 2 (NONE)\n    Func ptr: 0\n    Input memory: \n    Local memory: {0:1}{1:-1}\n    Output memory: \n    ---\n    Inst ptr: 0 (Dec 1)\n    Func ptr: 0\n    Input memory: \n    Local memory: \n    Output memory: \n    ---\n");
 	ss.str(std::string());
-	
+
 	// Testing default mem value
 	REQUIRE(gp.GetCurState().GetLocal(1) == -1.0);
 	gp.ProcessInst(inst); // will increment local mem 1
@@ -98,11 +141,11 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	inst.Set(0, 2);
 	gp.ProcessInst(inst);
 	REQUIRE(gp.GetCurState().GetLocal(2) == 6.0);
-	
+
 	// New Random
 	gp.NewRandom(150);
 	(gp.GetRandom().GetSeed() == 150);
-	
+
 	// Events
 	emp::EventDrivenGP::Event ev(0); // event 0 in event lib is message
 	ev.properties.insert("add");
@@ -113,7 +156,7 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	gp.PrintEvent(ev, ss);
 	REQUIRE(ss.str() == "[Message,00000000,(),(Properties: add)]");
 	ss.str(std::string());
-	
+
 	// Traits
 	gp.GetTrait().push_back(2.0);
 	REQUIRE(gp.GetTrait()[0] == 2.0);
@@ -131,7 +174,7 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	gp.PrintTraits(ss);
 	REQUIRE(ss.str() == "6 3 ");
 	ss.str(std::string());
-	
+
 	// Loading a program
 	gp.PrintProgramFull(ss);
 	REQUIRE(ss.str() == "Fn-00000000:\n  Dec[00000000](1,0,0)\n  Not[00000000](0,0,0)\n\n");
@@ -144,7 +187,7 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	REQUIRE(os.str() == savedProgram);
 	os.str(std::string());
 	REQUIRE(gp.GetProgram().GetSize() == 1);
-	
+
 	// Resetting the current state
 	gp.GetCurState().Reset();
 	for(size_t i=0;i<3;i++)
@@ -159,7 +202,7 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 		REQUIRE(gp.GetCurState().AccessOutput(i) == 0.0);
 		REQUIRE(gp.GetCurState().AccessLocal(i) == 0.0);
 	}
-	
+
 	// Set/Push Inst
 	REQUIRE(gp.GetFunction(0)[0].affinity == inst.affinity);
 	REQUIRE(gp.GetFunction(0)[0].id == inst.id);
@@ -173,22 +216,22 @@ TEST_CASE("Test Event Driven GP", "[Hardware]")
 	gp.PrintProgram(ss1);
 	REQUIRE(ss1.str() == "Fn-0 00000000:\n  Dec 1\n  Inc 0\n\n");
 	ss1.str(std::string());
-	
+
 	// Set Program
 	REQUIRE(gp.GetProgram().GetSize() == 1);
 	gp.SetProgram(c_prgm);
 	REQUIRE(gp.GetProgram().GetSize() == 0);
 	gp.SetShared(0, 2.5);
 	REQUIRE(gp.GetShared(0) == 2.5);
-	
+
 	// Max cores
 	gp.SetMaxCores(6);
 	REQUIRE(gp.GetMaxCores() == 6);
-	
+
 	// StochasticFunCall
 	gp.SetStochasticFunCall(false);
 	REQUIRE(gp.IsStochasticFunCall() == false);
-	
+
 	// Copy constructor
 	emp::EventDrivenGP gp2(gp);
 	REQUIRE(gp2.GetMaxCores() == 6);
