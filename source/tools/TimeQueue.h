5c6800a64a099ea2087df98feb28958d41fd8466
Charles Ofria <charles.ofria@gmail.com>
2020-03-29 23:45:08 -0400

    Started building a TimeQueue tool.

---
+++ source/tools/TimeQueue.h
@@ -0,0 +1,23 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2020
+ *
+ *  @file  TimeQueue.h
+ *  @brief A priority queue for timings, always marching forward.
+ *  @note Status: ALPHA
+ *
+ */
+
+
+#ifndef EMP_TIME_QUEUE_H
+#define EMP_TIME_QUEUE_H
+
+#include "../base/assert.h"
+
+namespace emp {
+
+  class TimeQueue {
+  };
+
+}
