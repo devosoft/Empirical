36d78d88a72bfa9616aa81ceb7bb39e4b6f2074e
Charles Ofria <charles.ofria@gmail.com>
2020-03-30 12:53:38 -0400

    Added a RefillQueue() helper function to TimeQueue class.

--- source/tools/TimeQueue.h
+++ source/tools/TimeQueue.h
@@ -33,11 +33,35 @@ namespace emp {
 
     double cur_time = 0.0;               ///< What time are we up to?
     double min_wait = 1.0;               ///< Minimum amount of time for the next event.
-    emp::vector<EventInfo> cell_queue;   ///< Sorted events to be triggered.
-    emp::vector<EventInfo> cell_buffer;  ///< Unsorted events out of current range.
+    emp::vector<EventInfo> item_queue;   ///< Sorted events to be triggered.
+    emp::vector<EventInfo> item_buffer;  ///< Unsorted events out of current range.
 
+    // Helper function to move more items into the queue.
+    bool RefillQueue() {
+      emp_assert(item_queue.size() == 0);
+      if (item_buffer.size() == 0) return false;  // No cells left!
+
+      // Scan the buffer to determine the earliest time.
+      const double first_time = FindMin(item_buffer).timing;
+
+      // Move all cells that will replicate in the next "min_wait" timesteps to item_queue.
+      double last_time = first_time + min_wait;
+      size_t keep_count = 0;
+      for (size_t i = 0; i < item_buffer.size(); i++) {
+        if (item_buffer[i].timing <= last_time) item_queue.push_back(item_buffer[i]);
+        else item_buffer[keep_count++] = item_buffer[i];
+      }
+      item_buffer.resize(keep_count);
+
+      // Sort the cell queue so that it's ready to go.
+      emp::Sort(item_queue);
+
+      return true;
+    }
   public:
-    TimeQueue(size_t _min_wait=1.0) min_wait(_min_wait) { }
+    TimeQueue(size_t _min_wait=1.0) min_wait(_min_wait) {
+      emp_assert(min_wait > 0.0);
+    }
 
     double GetTime() const { return cur_time; }
     double GetMinWait() const { return min_wait; }
