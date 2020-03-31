c65fb80a3e1ff1a171771bcd836cfd855819d3ce
Charles Ofria <charles.ofria@gmail.com>
2020-03-31 10:53:35 -0400

    Building a TimeQueue example file.

---
+++ examples/tools/TimeQueue.cc
@@ -0,0 +1,13 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2020.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//  Some examples code for using emp::TimeQueue
+
+#include <iostream>
+#include "tools/TimeQueue.h"
+
+int main()
+{
+  emp::TimeQueue time_queue(100.0);
+}
