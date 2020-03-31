f995d0bfec932f97b48b8b7c5f1999e6683f9661
Charles Ofria <charles.ofria@gmail.com>
2020-03-31 10:54:00 -0400

    Minor fixes throughout TimeQueue.h so that it compiles.

--- source/tools/TimeQueue.h
+++ source/tools/TimeQueue.h
@@ -15,10 +15,11 @@
 
 #include "../base/assert.h"
 #include "../base/vector.h"
+#include "vector_utils.h"
 
 namespace emp {
 
-  template <typename T>
+  template <typename T=size_t>
   class TimeQueue {
   private:
     struct ItemInfo {
@@ -29,23 +30,23 @@ namespace emp {
         if (timing == _in.timing) return (item < _in.item);
         return (timing < _in.timing);
       }
-    } 
+    };
 
     double cur_time = 0.0;               ///< What time are we up to?
     double min_wait = 1.0;               ///< Minimum amount of time for the next event.
-    emp::vector<EventInfo> item_queue;   ///< Sorted events to be triggered.
-    emp::vector<EventInfo> item_buffer;  ///< Unsorted events out of current range.
+    emp::vector<ItemInfo> item_queue;   ///< Sorted events to be triggered.
+    emp::vector<ItemInfo> item_buffer;  ///< Unsorted events out of current range.
     size_t pos = 0;                      ///< What position are we up to in the item_queue?
 
     // Helper function to move more items into the queue.
     bool RefillQueue() {
       emp_assert(item_queue.size() == 0);
-      if (item_buffer.size() == 0) return false;  // No cells left!
+      if (item_buffer.size() == 0) return false;  // No items left!
 
       // Scan the buffer to determine the earliest time.
       const double first_time = FindMin(item_buffer).timing;
 
-      // Move all cells that will replicate in the next "min_wait" timesteps to item_queue.
+      // Move all items that will triegger in the next "min_wait" timesteps to item_queue.
       double last_time = first_time + min_wait;
       size_t keep_count = 0;
       for (size_t i = 0; i < item_buffer.size(); i++) {
@@ -54,20 +55,20 @@ namespace emp {
       }
       item_buffer.resize(keep_count);
 
-      // Sort the cell queue so that it's ready to go.
+      // Sort the item queue so that it's ready to go.
       emp::Sort(item_queue);
       pos = 0;
 
       return true;
     }
   public:
-    TimeQueue(size_t _min_wait=1.0) min_wait(_min_wait) {
+    TimeQueue(size_t _min_wait=1.0) : min_wait(_min_wait) {
       emp_assert(min_wait > 0.0);
     }
 
     double GetTime() const { return cur_time; }
     double GetMinWait() const { return min_wait; }
-    double GetSize() const { return cell_queue.size() + cell_buffer.size() - pos; }
+    double GetSize() const { return item_queue.size() + item_buffer.size() - pos; }
 
     /// Reset and empty the TimeQueue.
     void Clear() {
@@ -110,3 +111,5 @@ namespace emp {
   };
 
 }
+
+#endif
