0cfb957fc990a40222f76dfbec4c78d61740093d
Charles Ofria <charles.ofria@gmail.com>
2020-03-31 11:20:03 -0400

    Built working TimeQueue example.

--- examples/tools/TimeQueue.cc
+++ examples/tools/TimeQueue.cc
@@ -10,4 +10,21 @@
 int main()
 {
   emp::TimeQueue time_queue(100.0);
+
+  time_queue.Insert(0, 130.0);
+  time_queue.Insert(1, 150.0);
+  time_queue.Insert(2, 140.0);
+  time_queue.Insert(3, 120.0);
+  time_queue.Insert(4, 260.0);
+
+  // Print the first item; advance time to 120.
+  std::cout << time_queue.Next() << std::endl;
+  time_queue.Insert(5, 110.0);  // Should be inserted at 120+110 = 230 (before 4).
+  
+  
+  while (time_queue.GetSize()) {
+    std::cout << time_queue.Next() << std::endl;
+  }
+
+  std::cout << "End time = " << time_queue.GetTime() << std::endl;
 }
