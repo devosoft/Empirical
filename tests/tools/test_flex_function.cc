c910273701bb7d5105676adc85bae4d4935b898f
Katherine Perry <perryk12@msu.edu>
2020-04-07 14:55:55 -0400

    Transfer flex_function test cases to proper file in tools

--- tests/tools/test_flex_function.cc
+++ tests/tools/test_flex_function.cc
@@ -19,3 +19,19 @@ TEST_CASE("Test flex_function", "[tools]")
   
   // TODO: test 'template <int ID> void SetDefault(pack_id<ID,ARGS...> & in_default)'
 }
+
+int Sum4(int a1, int a2, int a3, int a4) {
+  return a1 + a2 + a3 + a4;
+}
+
+TEST_CASE("Another Test flex_function", "[tools]")
+{
+  emp::flex_function<int(int,int,int,int)> ff = Sum4;
+  ff.SetDefaults(10, 100,1000,10000);
+
+  REQUIRE( ff(1,2,3,4) == 10 );
+  REQUIRE( ff(1,2,3) == 10006 );
+  REQUIRE( ff(1,2) == 11003 );
+  REQUIRE( ff(1) == 11101 );
+  REQUIRE( ff() == 11110 );
+}
