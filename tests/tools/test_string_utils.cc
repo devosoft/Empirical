bcc4eca62f1894610e930f8c950d85c6038d379f
Katherine Perry <perryk12@msu.edu>
2020-04-20 13:52:35 -0400

    Tranfer string_utils test cases to proper file in tools

--- tests/tools/test_string_utils.cc
+++ tests/tools/test_string_utils.cc
@@ -134,3 +134,280 @@ TEST_CASE("Test string_utils", "[tools]")
 	emp::from_string<int>("1", one);
 	REQUIRE(one == 1);
 }
+
+
+TEST_CASE("Another Test string_utils", "[tools]")
+{
+
+  // TEST1: lets test our conversion to an escaped string.
+  const std::string special_string = "This\t5tr1ng\nis\non THREE (3) \"lines\".";
+  std::string escaped_string = emp::to_escaped_string(special_string);
+
+  // note: we had to double-escape the test to make sure this worked.
+  REQUIRE(escaped_string == "This\\t5tr1ng\\nis\\non THREE (3) \\\"lines\\\".");
+
+  // TEST2: Test more general conversion to literals.
+  REQUIRE(emp::to_literal(42) == "42");
+  REQUIRE(emp::to_literal('a') == "'a'");
+  REQUIRE(emp::to_literal('\t') == "'\\t'");
+  REQUIRE(emp::to_literal(1.234) == "1.234000");
+
+  // TEST3: Make sure that we can properly identify different types of characters.
+  int num_ws = 0;
+  int num_cap = 0;
+  int num_lower = 0;
+  int num_let = 0;
+  int num_num = 0;
+  int num_alphanum = 0;
+  int num_i = 0;
+  int num_vowel = 0;
+  for (char cur_char : special_string) {
+    if (emp::is_whitespace(cur_char)) num_ws++;
+    if (emp::is_upper_letter(cur_char)) num_cap++;
+    if (emp::is_lower_letter(cur_char)) num_lower++;
+    if (emp::is_letter(cur_char)) num_let++;
+    if (emp::is_digit(cur_char)) num_num++;
+    if (emp::is_alphanumeric(cur_char)) num_alphanum++;
+    if (emp::is_valid(cur_char, [](char c){ return c=='i'; })) num_i++;
+    if (emp::is_valid(cur_char, [](char c){return c=='a' || c=='A';},
+                      [](char c){return c=='e' || c=='E';},
+                      [](char c){return c=='i' || c=='I';},
+                      [](char c){return c=='o' || c=='O';},
+                      [](char c){return c=='u' || c=='U';},
+                      [](char c){return c=='y';}
+                      )) num_vowel++;
+  }
+  int num_other = ((int) special_string.size()) - num_alphanum - num_ws;
+
+
+  REQUIRE(num_ws == 6);
+  REQUIRE(num_cap == 6);
+  REQUIRE(num_lower == 16);
+  REQUIRE(num_let == 22);
+  REQUIRE(num_num == 3);
+  REQUIRE(num_alphanum == 25);
+  REQUIRE(num_other == 5);
+  REQUIRE(num_i == 3);
+  REQUIRE(num_vowel == 7);
+
+  std::string base_string = "This is an okay string.\n  \tThis\nis   -MY-    very best string!!!!   ";
+
+  REQUIRE(
+    emp::slugify(base_string)
+    == "this-is-an-okay-string-this-is-my-very-best-string"
+  );
+
+  std::string first_line = emp::string_pop_line(base_string);
+
+  REQUIRE(first_line == "This is an okay string.");
+  REQUIRE(emp::string_get_word(first_line) == "This");
+
+  emp::string_pop_word(first_line);
+
+  REQUIRE(first_line == "is an okay string.");
+
+  emp::remove_whitespace(first_line);
+
+  REQUIRE(first_line == "isanokaystring.");
+
+  std::string popped_str = emp::string_pop(first_line, "ns");
+
+  REQUIRE(popped_str == "i");
+  REQUIRE(first_line == "anokaystring.");
+
+
+  popped_str = emp::string_pop(first_line, "ns");
+
+
+  REQUIRE(popped_str == "a");
+  REQUIRE(first_line == "okaystring.");
+
+
+  popped_str = emp::string_pop(first_line, 'y');
+
+  REQUIRE(popped_str == "oka");
+  REQUIRE(first_line == "string.");
+
+  emp::left_justify(base_string);
+  REQUIRE(base_string == "This\nis   -MY-    very best string!!!!   ");
+
+  emp::right_justify(base_string);
+  REQUIRE(base_string == "This\nis   -MY-    very best string!!!!");
+
+  emp::compress_whitespace(base_string);
+  REQUIRE(base_string == "This is -MY- very best string!!!!");
+
+
+  std::string view_test = "This is my view test!";
+  REQUIRE( emp::view_string(view_test) == "This is my view test!" );
+  REQUIRE( emp::view_string(view_test, 5) == "is my view test!" );
+  REQUIRE( emp::view_string(view_test, 8, 2) == "my" );
+  REQUIRE( emp::view_string_front(view_test,4) == "This" );
+  REQUIRE( emp::view_string_back(view_test, 5) == "test!" );
+  REQUIRE( emp::view_string_range(view_test, 11, 15) == "view" );
+  REQUIRE( emp::view_string_to(view_test, ' ') == "This" );
+  REQUIRE( emp::view_string_to(view_test, ' ', 5) == "is" );
+
+  emp::vector<std::string_view> slice_view = emp::view_slices(view_test, ' ');
+  REQUIRE( slice_view.size() == 5 );
+  REQUIRE( slice_view[0] == "This" );
+  REQUIRE( slice_view[1] == "is" );
+  REQUIRE( slice_view[2] == "my" );
+  REQUIRE( slice_view[3] == "view" );
+  REQUIRE( slice_view[4] == "test!" );
+
+
+  auto slices = emp::slice(
+    "This is a test of a different version of slice.",
+    ' '
+  );
+  REQUIRE(slices.size() == 10);
+  REQUIRE(slices[8] == "of");
+
+  slices = emp::slice(
+    "This is a test of a different version of slice.",
+    ' ',
+    101
+  );
+  REQUIRE(slices.size() == 10);
+  REQUIRE(slices[8] == "of");
+
+  slices = emp::slice(
+    "This is a test.",
+    ' ',
+    0
+  );
+  REQUIRE(slices.size() == 1);
+  REQUIRE(slices[0] == "This is a test.");
+
+  slices = emp::slice(
+    "This is a test.",
+    ' ',
+    1
+  );
+  REQUIRE(slices.size() == 2);
+  REQUIRE(slices[0] == "This");
+  REQUIRE(slices[1] == "is a test.");
+
+  slices = emp::slice(
+    "This is a test.",
+    ' ',
+    2
+  );
+  REQUIRE(slices.size() == 3);
+  REQUIRE(slices[0] == "This");
+  REQUIRE(slices[1] == "is");
+  REQUIRE(slices[2] == "a test.");
+
+  // Try other ways of using slice().
+  emp::slice(base_string, slices, 's');
+
+  REQUIRE(slices.size() == 5);
+  REQUIRE(slices[1] == " i");
+  REQUIRE(slices[3] == "t ");
+
+
+  // Some tests of to_string() function.
+  REQUIRE(emp::to_string((int) 1) == "1");
+  REQUIRE(emp::to_string("2") == "2");
+  REQUIRE(emp::to_string(std::string("3")) == "3");
+  REQUIRE(emp::to_string('4') == "4");
+  REQUIRE(emp::to_string((int16_t) 5) == "5");
+  REQUIRE(emp::to_string((int32_t) 6) == "6");
+  REQUIRE(emp::to_string((int64_t) 7) == "7");
+  REQUIRE(emp::to_string((uint16_t) 8) == "8");
+  REQUIRE(emp::to_string((uint32_t) 9) == "9");
+  REQUIRE(emp::to_string((uint64_t) 10) == "10");
+  REQUIRE(emp::to_string((size_t) 11) == "11");
+  REQUIRE(emp::to_string((long) 12) == "12");
+  REQUIRE(emp::to_string((unsigned long) 13) == "13");
+  REQUIRE(emp::to_string((float) 14.0) == "14");
+  REQUIRE(emp::to_string((float) 14.1) == "14.1");
+  REQUIRE(emp::to_string((float) 14.1234) == "14.1234");
+  REQUIRE(emp::to_string((double) 15.0) == "15");
+  REQUIRE(emp::to_string(16.0) == "16");
+  REQUIRE(emp::to_string(emp::vector<size_t>({17,18,19})) == "[ 17 18 19 ]");
+  REQUIRE(emp::to_string((char) 32) == " ");
+  REQUIRE(emp::to_string((unsigned char) 33) == "!");
+
+  std::string cat_a = "ABC";
+  bool cat_b = true;
+  char cat_c = '2';
+  int cat_d = 3;
+
+  std::string cat_full = emp::to_string(cat_a, cat_b, cat_c, cat_d);
+
+  REQUIRE(cat_full == "ABC123");
+  emp::array<int, 3> test_arr({{ 4, 2, 5 }});
+  REQUIRE(emp::to_string(test_arr) == "[ 4 2 5 ]");
+
+  // tests adapted from https://stackoverflow.com/questions/5288396/c-ostream-out-manipulation/5289170#5289170
+  std::string els[] = { "aap", "noot", "mies" };
+
+  typedef emp::vector<std::string> strings;
+
+  REQUIRE( ""  == emp::join_on(strings(), "") );
+  REQUIRE( "" == emp::join_on(strings(), "bla") );
+  REQUIRE( "aap" == emp::join_on(strings(els, els + 1), "") );
+  REQUIRE( "aap" == emp::join_on(strings(els, els + 1), "#") );
+  REQUIRE( "aap" == emp::join_on(strings(els, els + 1), "##") );
+  REQUIRE( "aapnoot" == emp::join_on(strings(els, els + 2), "") );
+  REQUIRE( "aap#noot" == emp::join_on(strings(els, els + 2), "#") );
+  REQUIRE( "aap##noot" == emp::join_on(strings(els, els + 2), "##") );
+  REQUIRE( "aapnootmies" == emp::join_on(strings(els, els + 3), "") );
+  REQUIRE( "aap#noot#mies" == emp::join_on(strings(els, els + 3), "#") );
+  REQUIRE( "aap##noot##mies" == emp::join_on(strings(els, els + 3), "##") );
+  REQUIRE( "aap  noot  mies" == emp::join_on(strings(els, els + 3), "  ") );
+  REQUIRE( "aapnootmies" == emp::join_on(strings(els, els + 3), "\0"));
+  REQUIRE(
+    "aapnootmies"
+    ==
+    emp::join_on(strings(els, els + 3), std::string("\0" , 1).c_str())
+  );
+  REQUIRE(
+    "aapnootmies"
+    ==
+    emp::join_on(strings(els, els + 3), std::string("\0+", 2).c_str())
+  );
+  REQUIRE(
+    "aap+noot+mies"
+    ==
+    emp::join_on(strings(els, els + 3), std::string("+\0", 2).c_str())
+  );
+
+  emp::string_vec_t string_v;
+
+  REQUIRE( emp::to_english_list(string_v) == "" );
+
+  string_v.push_back("one");
+
+  REQUIRE( emp::to_english_list(string_v) == "one" );
+
+  string_v.push_back("two");
+
+  REQUIRE( emp::to_english_list(string_v) == "one and two" );
+
+  string_v.push_back("three");
+
+  REQUIRE( emp::to_english_list(string_v) == "one, two, and three" );
+
+  string_v.push_back("four");
+
+  REQUIRE( emp::to_english_list(string_v) == "one, two, three, and four" );
+
+  emp::string_vec_t quoted_strings = emp::quote_strings(string_v);
+
+  REQUIRE( quoted_strings[0] == "'one'" );
+  REQUIRE( quoted_strings[2] == "'three'" );
+
+  quoted_strings = emp::quote_strings(string_v, "***");
+
+  REQUIRE( quoted_strings[1] == "***two***" );
+  REQUIRE( quoted_strings[3] == "***four***" );
+
+  quoted_strings = emp::quote_strings(string_v, "([{<", ">}])");
+
+  REQUIRE( quoted_strings[0] == "([{<one>}])" );
+  REQUIRE( quoted_strings[2] == "([{<three>}])" );
+
+}
