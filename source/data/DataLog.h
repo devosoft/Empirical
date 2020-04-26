8537f673f6b10b98d19760ad752703fb0180f08f
Charles Ofria <charles.ofria@gmail.com>
2020-04-25 23:34:40 -0400

    Moved bar graph and histogram functionality over to ascii_utils.h; call those versions.

--- source/data/DataLog.h
+++ source/data/DataLog.h
@@ -17,6 +17,7 @@
 
 #include "../base/assert.h"
 #include "../base/vector.h"
+#include "../tools/ascii_utils.h"
 #include "../tools/vector_utils.h"
 
 namespace emp {
@@ -57,19 +58,7 @@ namespace emp {
                         bool max_scale_1=true,        ///< Should we limit scaling to 1:1?
                         std::ostream & os=std::cout)  ///< Where to output the bar graph?
     {
-      T min_size = emp::FindMin(data);
-      T max_size = emp::FindMax(data);
-      double scale = ((double) max_width) / ((double) max_size);
-      if (max_scale_1 && scale > 1.0) scale = 1.0;
-      for (T datum : data) {
-        double bar_width = datum * scale;
-        while (bar_width >= 1.0) { os << '='; bar_width -= 1.0; }
-        if (bar_width > 0.0) os << '~';
-        os << "  (" << datum << ")\n";
-      }
-      if (show_scale) {
-        os << "SCALE: = -> " << (1.0 / scale) << std::endl;
-      }
+      emp::AsciiBarGraph(data, max_width, show_scale, max_scale_1, os);
     }
 
 
@@ -79,19 +68,7 @@ namespace emp {
                         bool show_scale=true,          ///< Should we show the scale at bottom?
                         std::ostream & os=std::cout)   ///< Where to output the bar graph?
     {
-      T min_val = emp::FindMin(data);
-      T max_val = emp::FindMax(data);
-      T val_range = max_val - min_val;
-      T bin_width = val_range / (T) num_bins;
-
-      emp::vector<size_t> bins(num_bins, 0);
-      for (T d : data) {
-        size_t bin_id = (size_t) ( (d - min_val) / bin_width );
-        if (bin_id == num_bins) bin_id--;
-        bins[bin_id]++;
-      }
-
-      DataLog<size_t>(bins).AsciiBarGraph(max_width, show_scale, true, os);
+      emp::AsciiHistogram(data, num_bins, max_width, show_scale, os);
     }
   };   
 }
