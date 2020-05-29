9d17c4feae3e47bc6563a0611076e255c6eba33d
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-05-29 11:43:57 -0400

    Strip trailing whitespace

--- tests/Evolve/World_reflect.cc
+++ tests/Evolve/World_reflect.cc
@@ -9,13 +9,13 @@
 #include <sstream>
 #include <iostream>
 
-class MyOrg 
+class MyOrg
 {
 	private:
 		double value;
 	public:
 		explicit MyOrg(double val) : value(val) {}
-		
+
 		double GetFitness(){ return value; }
 		double DoMutations(emp::Random& r){ value = value+1.0; return value; }
 		double GetValue() { return value; }
@@ -28,42 +28,42 @@ TEST_CASE("Test World reflect", "[Evolve]")
 	world.InjectAt(2.2, 0);
 	world.InjectAt(8.5, 1);
 	world.InjectAt(9.3, 2);
-	
+
 	std::stringstream ss;
 	emp::SetDefaultPrintFun_impl<emp::World<double>, double>(world, true);
 	world.Print(ss, "", " ");
 	REQUIRE(ss.str() == "2.2 8.5 9.3 ");
 	ss.str(std::string());
-	
+
 	#ifdef EMP_TDEBUG
 	emp::SetDefaultFitFun_impl<emp::World<double>, double>(world);
 	double org = 2.2;
 	world.CalcFitnessOrg(org);
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	emp::SetDefaultMutFun_impl<emp::World<double>, double>(world);
 	world.DoMutationsOrg(org);
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	emp::SetDefaultPrintFun_impl<emp::World<double>, double>(world);
 	world.Print();
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
 	#endif
-	
+
 	emp::World<MyOrg> world1;
 	world1.InjectAt(MyOrg(7.1), 0);
 	world1.InjectAt(MyOrg(2.4), 1);
 	world1.InjectAt(MyOrg(11.8), 2);
-	
+
 	MyOrg org3(3.7);
 	REQUIRE(world1.CalcFitnessOrg(org3) == 3.7);
-	
+
 	world1.DoMutationsOrg(org3);
 	REQUIRE( (org3.GetValue() == 4.7) );
-	
+
 	world1.Print(ss, "", " ");
 	REQUIRE(ss.str() == "7.1 2.4 11.8 ");
 }
