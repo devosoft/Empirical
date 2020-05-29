474369aba20f6d3800d015ce9338330d87cf30c0
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-05-29 00:49:22 -0400

    Strip trailing whitespace

--- tests/base/array.cc
+++ tests/base/array.cc
@@ -24,58 +24,58 @@ TEST_CASE("Test array", "[base]")
 	REQUIRE(s_arr[8] == arr2[8]);
 	REQUIRE(arr.back() == 9);
 	REQUIRE(arr.front() == 0);
-	
+
 	// Testing emp::assert in array.h functions
 	arr.resize(12);
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	arr.resize(15, 10);
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	int three = arr[3];
 	REQUIRE(three == 3);
 	REQUIRE(!emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	arr.push_back(10);
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	arr.pop_back();
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	int one = arr[1];
 	REQUIRE(one == 1);
 	REQUIRE(!emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	std::stringstream ss;
 	ss << arr;
 	REQUIRE(ss.str() == "0 1 2 3 4 5 6 7 8 9 ");
-	
+
 	emp::array<int, SIZE> arr3;
 	ss >> arr3;
 	REQUIRE(arr3[2] == 2);
 	ss.str(std::string());
-	
+
 	// Seems like it shouldn't work, throws emp_assert false, but actually inserts...
 	arr.insert(arr.begin(), 10);
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	// Same issue as insert
 	arr.erase(arr.begin());
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	// Same issue as insert
 	arr.emplace(arr.end(), 11);
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
-	
+
 	arr.emplace_back(12);
 	REQUIRE(emp::assert_last_fail);
 	emp::assert_clear();
