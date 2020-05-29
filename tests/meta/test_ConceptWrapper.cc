e18440f75c48b04cfb4695c0166a895826480656
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-05-28 20:10:41 -0400

    Break meta tests into multiple files

---
+++ tests/meta/test_ConceptWrapper.cc
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+#include "third-party/Catch/single_include/catch.hpp"
+
+#include "meta/ConceptWrapper.h"
+
+TEST_CASE("Test ConceptWrapper", "[games]")
+{
+
+}
---
+++ tests/meta/test_StringType.cc
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+#include "third-party/Catch/single_include/catch.hpp"
+
+#include "meta/StringType.h"
+
+TEST_CASE("Test StringType", "[games]")
+{
+
+}
