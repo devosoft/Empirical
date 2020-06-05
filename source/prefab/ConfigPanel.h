c0edc6599ab3a8183fabbc9f6d2acd1d47f8d19b
kayakingCellist <boyds@southwestern.edu>
2020-06-05 17:56:22 -0500

    added css styling to form and dropdown box

--- source/prefab/ConfigPanel.h
+++ source/prefab/ConfigPanel.h
@@ -110,11 +110,17 @@ namespace emp {
                         std::string type = group->GetEntry(i)->GetType();
                         std::string value = group->GetEntry(i)->GetValue();
 
+                        web::Div mobile_dropdown("mobile_dropdown_" + name);
+                        mobile_dropdown.SetAttr("class", "d-md-none");
+
                         web::Div setting_element(name + "_row");
+                        setting_element.SetCSS("width", "100%");
+                        web::Element title_span("span");
                         web::Element title("a");
                         title.SetAttr("data-toggle", "collapse").SetAttr("href", "#" + name + "_dropdown");
                         title << format_label_fun(name);
-                        setting_element << title;
+                        title_span.SetCSS("width", "38%").SetCSS("display", "inline-block");
+                        setting_element << title_span << title;
 
                         if (Has(numeric_types, type)) {
                             // Seems more efficient to use web::Input, but it's not working
@@ -131,10 +137,20 @@ namespace emp {
                             
                         
                             web::Element number("input");
+                            web::Element spacer("span");
+                            spacer.SetCSS("width", "38%").SetAttr("class", "d-inline-block d-md-none");
                             number.SetAttr("type", "number");
+                            slider.SetCSS("width", "38%").SetAttr("class", "d-none d-md-inline-block");
+                            number.SetCSS("width", "18%").SetCSS("right", "10px");
+                            setting_element << spacer;
                             setting_element << slider;
                             setting_element << number;
-                            // NOTE: adding an id to these slider or number breaks it!
+
+                            web::Element mobile_slider("input");
+                            mobile_slider.SetAttr("type", "range");
+                            mobile_dropdown << "<hr>" << mobile_slider;
+
+                            // NOTE: adding an id to slider or number breaks it!
 
                             // Attempt to have intelligent defaults
                             // TODO: uncomment this area after onchange function is defined
@@ -166,6 +182,7 @@ namespace emp {
                         //     );
                             web::Element text_input("input");
                             text_input.SetAttr("type", "text");
+                            text_input.SetCSS("width", "39%");
                             setting_element << text_input;
                         }
 
@@ -176,7 +193,9 @@ namespace emp {
                         web::Div content(name + "_content");
                         triangle.SetCSS("width", "0").SetCSS("height", "0").SetCSS("border-left", "12px solid transparent").SetCSS("border-right", "12px solid transparent").SetCSS("border-bottom", "12px solid #ede9e8").SetCSS("margin-left", "15px");
                         drop_down << triangle;
+                        content.SetAttr("class", "p-3").SetCSS("font-style", "italic");
                         content << "hello world!";
+                        content << mobile_dropdown;
                         drop_down << triangle;
                         drop_down << content;
                         content.SetBackground("#ede9e8");
@@ -187,7 +206,7 @@ namespace emp {
                         input_divs[name] << drop_down;
 
                         // group_divs[group_name] << web::Element("form") << input_map[name];
-                        card_body << web::Element("form") << input_divs[name];
+                        card_body << web::Element("form").SetCSS("width", "100%") << input_divs[name];
                         group_divs[group_name] << card << card_body;
 
                     }
