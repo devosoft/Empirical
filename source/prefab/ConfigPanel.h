29eef50d3116df2fcd49da1962b140909f6e03e5
kayakingCellist <boyds@southwestern.edu>
2020-06-05 17:56:22 -0500

    added css styling to form and dropdown box

--- source/prefab/ConfigPanel.h
+++ source/prefab/ConfigPanel.h
@@ -109,6 +109,7 @@ namespace emp {
                         std::string type = group->GetEntry(i)->GetType();
                         std::string value = group->GetEntry(i)->GetValue();
 
+<<<<<<< HEAD
                         if (Has(numeric_types, type)) {
                             input_map[name] = emp::web::Input(
                                 [this, name](std::string val){
@@ -118,6 +119,48 @@ namespace emp {
                                                               on_change_fun(val);},
                                 "range", format_label_fun(name), name + "_input_slider", true
                             );
+=======
+                        web::Div mobile_dropdown("mobile_dropdown_" + name);
+                        mobile_dropdown.SetAttr("class", "d-md-none");
+
+                        web::Div setting_element(name + "_row");
+                        setting_element.SetCSS("width", "100%");
+                        web::Element title_span("span");
+                        web::Element title("a");
+                        title.SetAttr("data-toggle", "collapse").SetAttr("href", "#" + name + "_dropdown");
+                        title << format_label_fun(name);
+                        title_span.SetCSS("width", "38%").SetCSS("display", "inline-block");
+                        setting_element << title_span << title;
+
+                        if (Has(numeric_types, type)) {
+                            // Seems more efficient to use web::Input, but it's not working
+                            // TODO: need to define on change function to use built in Input
+                            // emp::web::Input slider([this, name](std::string val){
+                                                            //   std::cout << name << " " << val << " " <<config.Get(name) << std::endl;
+                                                            //   config.Set(name, val);
+                                                            //   std::cout << config.Get(name) << std::endl;
+                                                            //   on_change_fun(val);},
+                                                            // "range", format_label_fun(name), name + "_input_slider", true
+                                                            // );
+                            web::Element slider("input");
+                            slider.SetAttr("type", "range");
+
+
+                            web::Element number("input");
+                            web::Element spacer("span");
+                            spacer.SetCSS("width", "38%").SetAttr("class", "d-inline-block d-md-none");
+                            number.SetAttr("type", "number");
+                            slider.SetCSS("width", "38%").SetAttr("class", "d-none d-md-inline-block");
+                            number.SetCSS("width", "18%").SetCSS("right", "10px");
+                            setting_element << spacer;
+                            setting_element << slider;
+                            setting_element << number;
+
+                            web::Element mobile_slider("input");
+                            mobile_slider.SetAttr("type", "range");
+                            mobile_dropdown << "<hr>" << mobile_slider;
+
+                            // NOTE: adding an id to slider or number breaks it!
 
                             // Attempt to have intelligent defaults
                             if (type == "double") {
@@ -136,18 +179,39 @@ namespace emp {
                                 "checkbox", format_label_fun(name), name + "_input_checkbox"
                             );
                         } else {
-                            input_map[name] = emp::web::Input(
-                                [this, name](std::string val){config.Set(name, val);
-                                                               on_change_fun(val);},
-                                "text", format_label_fun(name), name + "_input_textbox"
-                            );
-
+                        //     input_map[name] = emp::web::Input(
+                        //         [this, name](std::string val){config.Set(name, val);
+                        //                                        on_change_fun(val);},
+                        //         "text", format_label_fun(name), name + "_input_textbox"
+                        //     );
+                            web::Element text_input("input");
+                            text_input.SetAttr("type", "text");
+                            text_input.SetCSS("width", "39%");
+                            setting_element << text_input;
                         }
 
-                        input_map[name].Value(value);
+                        // Dropdown bubble
+                        // Should probably be modularlized in the future
+                        web::Div drop_down(name + "_dropdown");
+                        web::Div triangle(name + "_triangle_up");
+                        web::Div content(name + "_content");
+                        triangle.SetCSS("width", "0").SetCSS("height", "0").SetCSS("border-left", "12px solid transparent").SetCSS("border-right", "12px solid transparent").SetCSS("border-bottom", "12px solid #ede9e8").SetCSS("margin-left", "15px");
+                        drop_down << triangle;
+                        content.SetAttr("class", "p-3").SetCSS("font-style", "italic");
+                        content << "hello world!";
+                        content << mobile_dropdown;
+                        drop_down << triangle;
+                        drop_down << content;
+                        content.SetBackground("#ede9e8");
+                        drop_down.SetAttr("class", "collapse");
+
+                        // input_map[name].Value(value);
+                        input_divs[name] << setting_element;
+                        input_divs[name] << drop_down;
 
                         // group_divs[group_name] << web::Element("form") << input_map[name];
-                        card_body << web::Element("form") << input_map[name];
+                        card_body << web::Element("form").SetCSS("width", "100%") << input_divs[name];
+>>>>>>> c0edc65... added css styling to form and dropdown box
                         group_divs[group_name] << card << card_body;
 
                     }
