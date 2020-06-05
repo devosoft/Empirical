f5bf0fb287d34f326da9a10ad61e996f71e28800
kayakingCellist <boyds@southwestern.edu>
2020-06-05 16:02:45 -0500

    added elements for form input and dropdown box, not styalized

--- source/prefab/ConfigPanel.h
+++ source/prefab/ConfigPanel.h
@@ -20,7 +20,8 @@ namespace emp {
             web::Div settings_div;
             std::set<std::string> exclude;
             std::map<std::string, web::Div> group_divs;
-            std::map<std::string, web::Input> input_map;
+            // std::map<std::string, web::Input> input_map;
+            std::map<std::string, web::Div> input_divs;
             std::function<void(const std::string & val)> on_change_fun = [](const std::string & val){;};
             std::function<std::string(std::string val)> format_label_fun = [](std::string name){
                 emp::vector<std::string> sliced = slice(name, '_');
@@ -109,49 +110,87 @@ namespace emp {
                         std::string type = group->GetEntry(i)->GetType();
                         std::string value = group->GetEntry(i)->GetValue();
 
+                        web::Div setting_element(name + "_row");
+                        web::Element title("a");
+                        title.SetAttr("data-toggle", "collapse").SetAttr("href", "#" + name + "_dropdown");
+                        title << format_label_fun(name);
+                        setting_element << title;
+
                         if (Has(numeric_types, type)) {
-                            input_map[name] = emp::web::Input(
-                                [this, name](std::string val){
-                                                              std::cout << name << " " << val << " " <<config.Get(name) << std::endl;
-                                                              config.Set(name, val);
-                                                              std::cout << config.Get(name) << std::endl;
-                                                              on_change_fun(val);},
-                                "range", format_label_fun(name), name + "_input_slider", true
-                            );
+                            // Seems more efficient to use web::Input, but it's not working
+                            // TODO: need to define on change function to use built in Input
+                            // emp::web::Input slider([this, name](std::string val){
+                                                            //   std::cout << name << " " << val << " " <<config.Get(name) << std::endl;
+                                                            //   config.Set(name, val);
+                                                            //   std::cout << config.Get(name) << std::endl;
+                                                            //   on_change_fun(val);}, 
+                                                            // "range", format_label_fun(name), name + "_input_slider", true
+                                                            // );
+                            web::Element slider("input");
+                            slider.SetAttr("type", "range");
+                            
+                        
+                            web::Element number("input");
+                            number.SetAttr("type", "number");
+                            setting_element << slider;
+                            setting_element << number;
+                            // NOTE: adding an id to these slider or number breaks it!
 
                             // Attempt to have intelligent defaults
-                            if (type == "double") {
-                                SetDefaultRangeFloatingPoint(input_map[name], emp::from_string<double>(value));
-                            } else if (type == "float") {
-                                SetDefaultRangeFloatingPoint(input_map[name], emp::from_string<float>(value));
-                            } else {
+                            // TODO: uncomment this area after onchange function is defined
+                            // if (type == "double") {
+                                // SetDefaultRangeFloatingPoint(input_map[name], emp::from_string<double>(value));
+                            // } else if (type == "float") {
+                                // SetDefaultRangeFloatingPoint(input_map[name], emp::from_string<float>(value));
+                            // } else {
                                 // TODO: Correctly handle all types (although I'm not sure it actually matters?)
-                                SetDefaultRangeFixedPoint(input_map[name], emp::from_string<int>(value));
-                            }
+                                // SetDefaultRangeFixedPoint(input_map[name], emp::from_string<int>(value));
+                            // }
 
-                        } else if (type == "bool") {
-                            input_map[name] = emp::web::Input(
-                                [this, name](std::string val){config.Set(name, val);
-                                                              on_change_fun(val);},
-                                "checkbox", format_label_fun(name), name + "_input_checkbox"
-                            );
+                        } 
+                        else if (type == "bool") {
+                        //     input_map[name] = emp::web::Input(
+                        //         [this, name](std::string val){config.Set(name, val);
+                        //                                       on_change_fun(val);},
+                        //         "checkbox", format_label_fun(name), name + "_input_checkbox"
+                        //     );
+                            web::Element bool_input("input");
+                            bool_input.SetAttr("type", "checkbox");
+                            setting_element << bool_input;
+
                         } else {
-                            input_map[name] = emp::web::Input(
-                                [this, name](std::string val){config.Set(name, val);
-                                                               on_change_fun(val);},
-                                "text", format_label_fun(name), name + "_input_textbox"
-                            );
-
+                        //     input_map[name] = emp::web::Input(
+                        //         [this, name](std::string val){config.Set(name, val);
+                        //                                        on_change_fun(val);},
+                        //         "text", format_label_fun(name), name + "_input_textbox"
+                        //     );
+                            web::Element text_input("input");
+                            text_input.SetAttr("type", "text");
+                            setting_element << text_input;
                         }
 
-                        input_map[name].Value(value); 
+                        // Dropdown bubble
+                        // Should probably be modularlized in the future
+                        web::Div drop_down(name + "_dropdown");
+                        web::Div triangle(name + "_triangle_up");
+                        web::Div content(name + "_content");
+                        triangle.SetCSS("width", "0").SetCSS("height", "0").SetCSS("border-left", "12px solid transparent").SetCSS("border-right", "12px solid transparent").SetCSS("border-bottom", "12px solid #ede9e8").SetCSS("margin-left", "15px");
+                        drop_down << triangle;
+                        content << "hello world!";
+                        drop_down << triangle;
+                        drop_down << content;
+                        content.SetBackground("#ede9e8");
+                        drop_down.SetAttr("class", "collapse");
+
+                        // input_map[name].Value(value); 
+                        input_divs[name] << setting_element;
+                        input_divs[name] << drop_down;
 
                         // group_divs[group_name] << web::Element("form") << input_map[name];
-                        card_body << web::Element("form") << input_map[name];
+                        card_body << web::Element("form") << input_divs[name];
                         group_divs[group_name] << card << card_body;
 
                     }
-                    // settings_div.SetAttr("class", "col-lg-6 col-md-12"); makes all CSS go away!!!!
                     settings_div << group_divs[group_name];
                 }
 
