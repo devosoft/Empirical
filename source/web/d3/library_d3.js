4fe1c8813348cd6d35672e1e72fc4535fde127bf
Alex Lalejini <amlalejini@gmail.com>
2020-06-09 18:22:51 -0400

    Add find_function to library_d3.js, start re-implementing SelectionOrTransition wrapper

--- source/web/d3/library_d3.js
+++ source/web/d3/library_d3.js
@@ -2,6 +2,12 @@ var D3Library = {
   $emp_d3: {objects: {}, counts: {}, next_id:0},
   get_emp_d3: function() {
     return this.emp_d3;
+  },
+  find_function: function(sel) {
+    return (window["d3"][sel] === "function") ? window["d3"][sel] :
+           (window["emp"][sel] === "function") ? window["emp"][sel] :
+           (window[sel] === "function") ? window[sel] :
+           sel;
   }
 };
 
--- source/web/d3/test.cc
+++ source/web/d3/test.cc
@@ -8,14 +8,21 @@
 #include "web/init.h"
 #include "web/Document.h"
 #include "d3_init.h"
+#include "selection.h"
 
 // namespace UI = emp::web;
 // UI::Document doc("emp_d3_test");
+
+// EM_JS(const char*, hello, (), {
+//   const js_str = "Hello World!";
+//   return js_str;
+// });
 
 int main() {
 
   D3::internal::get_emp_d3();
   size_t id = D3::internal::NextD3ID();
   std::cout << "This is the ID you got: " << id << std::endl;
+  // std::cout << "This string is from javascript: " << hello() << std::endl;
 
 }
