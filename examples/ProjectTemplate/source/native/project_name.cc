6d53f74a45c9816f14a5b3c9af17b4ec6282dc9b
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-06-12 20:01:06 -0400

    Add deprecation warnings to ProjectTemplates
    Refer users to Cookiecutter Empirical Project instead

--- examples/ProjectTemplate/source/native/project_name.cc
+++ examples/ProjectTemplate/source/native/project_name.cc
@@ -1,5 +1,11 @@
 // This is the main function for the NATIVE version of this project.
 
+/*
+ * WARNING: ProjectTemplate is deprecated in favor of the Cookiecutter Empirical
+ * Project, which you can find at
+ * https://github.com/devosoft/cookiecutter-empirical-project
+*/
+
 #include <iostream>
 
 #include "base/vector.h"
@@ -10,4 +16,7 @@ int main(int argc, char* argv[])
   emp::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
 
   std::cout << "Hello World!" << std::endl;
+
+  std::cout << "WARNING: ProjectTemplate is deprecated in favor of the Cookiecutter Empirical Project, which you can find at https://github.com/devosoft/cookiecutter-empirical-project" << std::endl;
+
 }
--- examples/ProjectTemplate/source/web/project_name-web.cc
+++ examples/ProjectTemplate/source/web/project_name-web.cc
@@ -2,6 +2,12 @@
 //  Copyright (C) Michigan State University, 2017.
 //  Released under the MIT Software license; see doc/LICENSE
 
+/*
+ * WARNING: ProjectTemplate is deprecated in favor of the Cookiecutter Empirical
+ * Project, which you can find at
+ * https://github.com/devosoft/cookiecutter-empirical-project
+*/
+
 #include "web/web.h"
 
 namespace UI = emp::web;
@@ -11,4 +17,7 @@ UI::Document doc("emp_base");
 int main()
 {
   doc << "<h1>Hello, world!</h1>";
+
+  doc << "WARNING: ProjectTemplate is deprecated in favor of the Cookiecutter Empirical Project, which you can find at https://github.com/devosoft/cookiecutter-empirical-project";
+
 }
