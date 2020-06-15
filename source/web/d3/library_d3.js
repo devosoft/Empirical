a11918fb3b2e3d854a9256a064ab5081b3843c55
Alex Lalejini <amlalejini@gmail.com>
2020-06-15 19:29:53 -0400

    add is_function to library_d3.js, start implementing Transition class

--- source/web/d3/library_d3.js
+++ source/web/d3/library_d3.js
@@ -3,11 +3,19 @@ var D3Library = {
   get_emp_d3: function() {
     return this.emp_d3;
   },
+  // If the 'func_name' is a function in the global namespace, the d3 namespace, or the emp namespace
+  // return the proper function, otherwise just return 'func_name'
   find_function: function(sel) {
     return (window["d3"][sel] === "function") ? window["d3"][sel] :
            (window["emp"][sel] === "function") ? window["emp"][sel] :
            (window[sel] === "function") ? window[sel] :
            sel;
+  },
+  // Is the 'func_name' a function in the global namespace, the d3 namespace, or the emp namespace?
+  is_function: function(func_name) {
+    return ((window["d3"][func_name] === "function") ||
+            (window["emp"][func_name] === "function") ||
+            (window[func_name] === "function"));
   }
 };
 
