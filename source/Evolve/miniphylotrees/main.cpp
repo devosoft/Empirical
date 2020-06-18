d0b9be32c4b765096f916ff62f64dd523f3eb5f8
abbywlsn <abbysk8@gmail.com>
2020-06-18 16:42:38 -0600

    added systematics manager tools, still broken though

--- source/Evolve/miniphylotrees/main.cpp
+++ source/Evolve/miniphylotrees/main.cpp
@@ -3,22 +3,35 @@
 #include <memory>
 #include <cstdlib>
 #include <fstream>
-//#include "../base/Ptr.h"
+//#include "base/Ptr.h"
+//#include "Evolve/Systematics.h"
 #include "../Systematics.h"
 
-//g++ -std=c++17 -I../../Evolve/ -I../../ -I../../base/ main.cpp -o main.o
+//g++ -std=c++17 -I../../ main.cpp -o main.o
+
 using namespace std;
 
 int randScope;
 int numOrgs = 10;
 int parentNum;
 int numRounds = 100;
+int nextClade = 0;
 
 class Organism{
 public:
-    void reproduce(vector<Organism> &childGen){
+    int clade;
+
+    Organism(){
+        clade = nextClade;
+        clade++;
+    }
+
+    void reproduce(vector<Organism> &childGen, emp::Systematics<Organism, int> &sys){
         for(int i = 0; i < 10; i++){
             childGen.emplace_back(); //fills childGen vector with Organisms
+
+            //ORG & org, WorldPosition pos, int update=-1
+            sys.AddOrg(childGen[i], i, 1); //removed brackets childGen[i], {i, 0}
         }
         //cout << "child generation created" << endl;
     }
@@ -30,9 +43,10 @@ int chooseOrg(vector<Organism> &currentGen){
     return parentNum;
 }
 
-void switchGens(vector<Organism> &currentGen, vector<Organism> &childGen){
+void switchGens(vector<Organism> &currentGen, vector<Organism> &childGen, emp::Systematics<Organism, int> &sys){
     currentGen.swap(childGen);
     childGen.clear();
+    sys.Update();
 }
 
 bool writeToFile(string filename, int field_one, int field_two);
@@ -40,18 +54,24 @@ bool writeToFile(string filename, int field_one, int field_two);
 
 
 int main() {
+
+    function<int(Organism)> taxonFunc = [](Organism org){return org.clade;};//takes org return int
+
+    //function<int(int)> square = [](int squaredNum){return (squaredNum*squaredNum);};
+
+    emp::Systematics<Organism, int> sys(taxonFunc); //optional 3rd arg
+    sys.SetTrackSynchronous(true);
+
     //current gen (vector)
     vector<Organism> currentGen; //begins with currentGen
     //child gen (vector)
     vector<Organism> childGen;
 
-    emp::GetTypeID<Organism>();
-
-
 
     currentGen.reserve(numOrgs);
     for (int i = 0; i < numOrgs; i++) {
         currentGen.emplace_back(); //currentGen is filled with 10 organism
+        sys.AddOrg(currentGen[i], i, 0); //parent is null (removed brackets)
     }
 
     for (int i = 0; i < numRounds; i++) {
@@ -60,14 +80,17 @@ int main() {
         chooseOrg(currentGen); //chooses the parent of the next generation
         int parent = parentNum; //records parent as the num chosen by chooseOrg
         cout << "parent: " << parent << endl;
-        currentGen[parentNum].reproduce(childGen); //fills childGen with 10 Organisms
+        sys.SetNextParent(0);
+        currentGen[parentNum].reproduce(childGen, sys); //fills childGen with 10 Organisms
         //cout << "size of child population: " << size(childGen) << endl;
+        for(i = 0; i < currentGen.size(); i++){
+            sys.RemoveOrg(i, 0); //removed brackets
+        }
         switchGens(currentGen,
-                   childGen); //puts contents of child vector into current vector and deletes content of child vector
+                   childGen, sys); //puts contents of child vector into current vector and deletes content of child vector
 
         bool writeFile = writeToFile("treedata.txt", i, parentNum);
     }
-
 
 
 //update,num_taxa,total_orgs,ave_depth,num_roots,mrca_depth,diversity
