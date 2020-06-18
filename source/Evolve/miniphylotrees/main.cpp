28240626e0346942d7a5405fc99e586040ec1b86
abbywlsn <abbysk8@gmail.com>
2020-06-18 11:03:11 -0600

    miniphylotrees prints data to csv file now

--- source/Evolve/miniphylotrees/main.cpp
+++ source/Evolve/miniphylotrees/main.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 #include <memory>
 #include <cstdlib>
-#include <ctime>
-#include "../base/Ptr.h"
+#include <fstream>
+//#include "../base/Ptr.h"
+#include "../Systematics.h"
 
+//g++ -std=c++17 -I../../Evolve/ -I../../ -I../../base/ main.cpp -o main.o
 using namespace std;
 
 int randScope;
@@ -18,7 +20,7 @@ public:
         for(int i = 0; i < 10; i++){
             childGen.emplace_back(); //fills childGen vector with Organisms
         }
-        cout << "child generation created" << endl;
+        //cout << "child generation created" << endl;
     }
 };
 
@@ -33,6 +35,9 @@ void switchGens(vector<Organism> &currentGen, vector<Organism> &childGen){
     childGen.clear();
 }
 
+bool writeToFile(string filename, int field_one, int field_two);
+
+
 
 int main() {
     //current gen (vector)
@@ -40,13 +45,16 @@ int main() {
     //child gen (vector)
     vector<Organism> childGen;
 
+    emp::GetTypeID<Organism>();
+
+
 
     currentGen.reserve(numOrgs);
-for (int i = 0; i < numOrgs; i++) {
+    for (int i = 0; i < numOrgs; i++) {
         currentGen.emplace_back(); //currentGen is filled with 10 organism
     }
 
-    for(int i = 0; i < numRounds; i++) {
+    for (int i = 0; i < numRounds; i++) {
         cout << "generation: " << i << endl;
         randScope = size(currentGen); //this tells the chooseOrg function how large the vector is
         chooseOrg(currentGen); //chooses the parent of the next generation
@@ -54,14 +62,26 @@ for (int i = 0; i < numOrgs; i++) {
         cout << "parent: " << parent << endl;
         currentGen[parentNum].reproduce(childGen); //fills childGen with 10 Organisms
         //cout << "size of child population: " << size(childGen) << endl;
-        switchGens(currentGen,childGen); //puts contents of child vector into current vector and deletes content of child vector
+        switchGens(currentGen,
+                   childGen); //puts contents of child vector into current vector and deletes content of child vector
+
+        bool writeFile = writeToFile("treedata.txt", i, parentNum);
     }
 
 
 
+//update,num_taxa,total_orgs,ave_depth,num_roots,mrca_depth,diversity
 
-//    for(int i = 0; i < size(currentGen); i++){
-//        cout << currentGen[i] << " ";
-//    }
-//    cout<<endl;
+    int total_orgs = numRounds * numOrgs;
+
+    cout << "generations: " << numRounds << " / total organisms: " << total_orgs << endl;
 };
+
+bool writeToFile(string filename, int field_one, int field_two){
+    ofstream file;
+    file.open(filename, ios_base::app);
+    file << field_one << "," << field_two << endl;
+    file.close();
+
+    return true;
+}
