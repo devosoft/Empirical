10ff45e9684d3eb74a62c73472e469081038c20c
abbywlsn <abbysk8@gmail.com>
2020-06-26 10:22:42 -0600

    new csv with percentile data and miniphylotrees is correctly outputting phylodiversity

--- source/Evolve/miniphylotrees/main.cpp
+++ source/Evolve/miniphylotrees/main.cpp
@@ -2,10 +2,14 @@
 #include <vector>
 #include <memory>
 #include <cstdlib>
+#include <assert.h>
 #include "../Systematics.h"
+#include "../../tools/Random.h"
 
 //g++ -std=c++17 -I../../ main.cpp -o main.o && ./main.o
 //for i in {1..5}; do g++ -std=c++17 -I../../ main.cpp -o main.o && ./main.o; done
+//for i in {1..100}; do ./main.o; done
+
 
 using namespace std;
 
@@ -23,6 +27,9 @@ public:
     Organism(){
         clade = nextClade;
         nextClade++;
+
+        assert(nextClade == clade + 1);
+
     }
 
     static const char * printVect(){
@@ -41,8 +48,8 @@ public:
 //    //}
 };
 
-int chooseOrg(vector<Organism> &currentGen){
-    parentNum = rand() % randScope; //chooses random spot in array for parent
+int chooseOrg(vector<Organism> &currentGen, emp::Random &randNum){
+    parentNum = randNum.GetInt(10);  //chooses random spot in array for parent
     //cout << "parent chosen is in spot " << parentNum << " in currentGen array which is " << size(currentGen) << " long" << endl;
     return parentNum;
 }
@@ -57,6 +64,8 @@ bool writeToFile(string filename, int field_one);
 
 
 int main() {
+
+    emp::Random randNum;
 
     function<int(Organism)> taxonFunc = [](Organism org){return org.clade;};//takes org return int
 
@@ -73,7 +82,7 @@ int main() {
 
     for (int i = 0; i < numOrgs; i++) {
         currentGen.emplace_back(); //currentGen is filled with 10 organism
-        sys.AddOrg(currentGen[i], i, 0); //parent is null (removed brackets)
+        sys.AddOrg(currentGen[i], i, systime); //parent is null (removed brackets)
     }
 
 //    for(int i = 0; i < currentGen.size(); i++){
@@ -83,9 +92,10 @@ int main() {
     for (int i = 0; i < numRounds; i++) {
         cout << "generation: " << i << endl;
         randScope = size(currentGen); //this tells the chooseOrg function how large the vector is
+        assert(currentGen.size() == 10);
 
         for(int r = 0; r < 10; r++){
-            chooseOrg(currentGen); //chooses the parent of the next generation
+            chooseOrg(currentGen, randNum); //chooses the parent of the next generation
             //cout << "parent: " << parentNum << endl;
             sys.SetNextParent(parentNum);
             //currentGen[parentNum].reproduce(childGen, sys); //fills childGen with 10 Organisms
@@ -94,15 +104,14 @@ int main() {
             emp::WorldPosition pos(r, 1);
             sys.AddOrg(childGen[r], pos, systime); //removed brackets childGen[i], {i, 0}
             //cout << "size of child population: " << size(childGen) << endl;
-
         }
         //sys.PrintStatus();
         cout << "phylogenetic diversity: " << sys.GetPhylogeneticDiversity() << endl;
-        bool writeFile = writeToFile("not_real_treedata.txt", sys.GetPhylogeneticDiversity());
+        bool writeFile = writeToFile("treedata.csv", sys.GetPhylogeneticDiversity());
 
 
         for(int j = 0; j < currentGen.size(); j++){
-            sys.RemoveOrg(j, 0);
+            sys.RemoveOrg(j, systime);
         }
 
         switchGens(currentGen,childGen, sys); //puts contents of child vector into current vector and deletes content of child vector
@@ -118,8 +127,25 @@ int main() {
 bool writeToFile(string filename, int field_one){
     ofstream file;
     file.open(filename, ios_base::app);
-    file << field_one << endl;
+    file << field_one << ",";
     file.close();
 
     return true;
 }
+
+//use only end phylo num
+
+//use python script to convert each value to percentile
+//order smallest to larget -- '
+
+//end up with file with 100 nums --star num and second num that starts percentile
+//in systematics class, we will want to modify some tree calcualtions
+
+//modify phpy diversity and others
+//give it optional arg, string pointing to file with percentiles
+//file as arg, should open file and read everything
+
+//check out file.h class (tools) -- use to read in file
+//at end of phylo funct, if data is providec to normalize, normalize
+
+//figure out which percentile it falls into
