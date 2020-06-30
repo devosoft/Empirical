e58e8d057c8d649705fc6c107b52b11c6fa75e98
Alex Lalejini <amlalejini@gmail.com>
2020-06-30 16:55:49 -0400

    add d3 web tests makefile and run tests script

---
+++ tests/web/d3/run_tests.sh
@@ -0,0 +1,13 @@
+source ../../../third-party/emsdk/emsdk_env.sh
+echo "Source loaded"
+make
+echo "compilation done"
+
+karmaWebTests=(d3_init transition)                  # Tests that should be run as compiled JS using Karma.
+
+# Run Empirical web tests that are compiled javascript and use Karma + Mocha
+echo "=== Running compiled JS web tests (via Karma) ==="
+for filename in ${karmaWebTests[@]}
+do
+    ../../../third-party/node_modules/karma/bin/karma start karma.conf.js --filename ${filename}
+done
