b92f1ff57a6f7380c3882b69bbc08a35461b321f
Alex Lalejini <amlalejini@gmail.com>
2020-06-29 21:19:31 -0400

    adjust makefiles for new web testing setup

--- tests/web/run_tests.sh
+++ tests/web/run_tests.sh
@@ -2,12 +2,17 @@ source ../../third-party/emsdk/emsdk_env.sh
 echo "Source loaded"
 make
 echo "compilation done"
-./color_map
-for filename in js_utils.js JSWrap.js GetUrlParams.js;
+# ./color_map
+# for filename in js_utils.js JSWrap.js GetUrlParams.js;
+# do
+#     node $filename;
+#     if [ $? -gt 0 ];
+#     then
+# 	exit 1;
+#     fi;
+# done
+
+for test in test_jswrap;
 do
-    node $filename;
-    if [ $? -gt 0 ];
-    then
-	exit 1;
-    fi;
+    ../../third-party/node_modules/karma/bin/karma start karma.conf.js --filename $test
 done
