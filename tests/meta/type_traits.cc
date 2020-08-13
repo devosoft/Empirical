0b4b3a27ed52e1fa814d344131504766d374f8ee
Charles Ofria <charles.ofria@gmail.com>
2020-08-13 15:03:26 -0400

    Added tests for new HasToString() type trait.

--- tests/meta/type_traits.cc
+++ tests/meta/type_traits.cc
@@ -3,7 +3,15 @@
 
 #include "meta/type_traits.h"
 
+struct ExampleType1 {
+  std::string ToString() { return "example"; }
+};
+struct ExampleType2 {
+  // std::string ToString() { return "example"; }
+};
+
 TEST_CASE("Test type_traits", "[games]")
 {
-
+  REQUIRE( emp::HasToString<ExampleType1>() == true );
+  REQUIRE( emp::HasToString<ExampleType2>() == false );
 }
