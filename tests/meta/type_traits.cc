c315f3cc4098283a4149d555cb4a8691d236c0c3
Charles Ofria <charles.ofria@gmail.com>
2020-08-15 21:46:17 -0400

    Updated type_traits.cc test file.

--- tests/meta/type_traits.cc
+++ tests/meta/type_traits.cc
@@ -5,13 +5,22 @@
 
 struct ExampleType1 {
   std::string ToString() { return "example"; }
+  // double ToDouble() { return 42.0; }
 };
 struct ExampleType2 {
   // std::string ToString() { return "example"; }
+  double ToDouble() { return 42.0; }
 };
 
 TEST_CASE("Test type_traits", "[games]")
 {
   REQUIRE( emp::HasToString<ExampleType1>() == true );
   REQUIRE( emp::HasToString<ExampleType2>() == false );
+  REQUIRE( emp::HasToString<std::string>() == false );
+  REQUIRE( emp::HasToString<int>() == false );
+
+  REQUIRE( emp::HasToDouble<ExampleType1>() == false );
+  REQUIRE( emp::HasToDouble<ExampleType2>() == true );
+  REQUIRE( emp::HasToDouble<std::string>() == false );
+  REQUIRE( emp::HasToDouble<int>() == false );
 }
