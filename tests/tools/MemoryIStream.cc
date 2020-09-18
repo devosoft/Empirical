997dd8e10d316b1e0866042089d1f0164d76d391
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-09-17 21:15:30 -0400

    Simplify random vector generation

--- tests/tools/MemoryIStream.cc
+++ tests/tools/MemoryIStream.cc
@@ -3,21 +3,24 @@
 #include "third-party/Catch/single_include/catch2/catch.hpp"
 
 #include <cstring>
+#include <limits>
 #include <ratio>
 
 #include "tools/ContiguousStream.h"
 #include "tools/MemoryIStream.h"
 #include "tools/Random.h"
+#include "tools/random_utils.h"
 
 TEST_CASE("MemoryIStream", "[tools]") {
 
   const size_t num_ints = std::kilo{}.num;
-  emp::vector<int> source(num_ints);
 
   emp::Random rng{ 1 };
-  rng.RandFill(
-    reinterpret_cast<unsigned char*>( source.data() ),
-    source.size() * sizeof(int)
+  emp::vector<int> source = emp::RandomVector<int>(
+    rng,
+    num_ints,
+    std::numeric_limits<int>::min(),
+    std::numeric_limits<int>::max()
   );
 
   emp::MemoryIStream is{
