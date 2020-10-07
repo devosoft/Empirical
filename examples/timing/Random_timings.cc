195b5c6903536e6ea9a1af6e47a2c9753d0dfbe1
Charles Ofria <charles.ofria@gmail.com>
2020-10-07 18:50:55 -0400

    Cleaned up outputs on Random_timings.cc outputs.

--- examples/timing/Random_timings.cc
+++ examples/timing/Random_timings.cc
@@ -9,24 +9,32 @@
 #include <vector>
 
 #include "tools/Random.h"
+#include "tools/Random2.h"
 
 constexpr const size_t N = 1000000;
 
 int main()
 {
   emp::Random random;
+  // emp2::Random random;
 
   size_t success_count = 0;
   std::clock_t start_time = std::clock();
 
   constexpr const size_t N_P = N * 500;
+  constexpr const double p = 0.345;
+  constexpr const size_t expected = N_P * p;
+
   for (size_t i = 0; i < N_P; i++) {
-    if (random.P(0.345)) success_count++;
+    if (random.P(p)) success_count++;
   }
   
   std::clock_t total_time = std::clock() - start_time;
 
-  std::cout << "num hits = " << success_count
+  std::cout << "Testing Random::P()."
+	    << "  Doing " << N_P << " tests;"
+	    << " expecting ~" << expected << " hits.\n"
+	    << "  num hits = " << success_count
             << ";  time = " << ((double) total_time) / (double) CLOCKS_PER_SEC
             << " seconds." << std::endl;
 
