68b1689a203a9c1ded17613b939b42d26f2bbbbb
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-10-07 11:24:04 -0400

    Capture this to use metric

--- source/tools/MatchBin.h
+++ source/tools/MatchBin.h
@@ -876,7 +876,7 @@ namespace emp {
           std::cbegin(target.data),
           std::cend(target.data),
           std::inserter(scores, std::begin(scores)),
-          [uid](const auto& target_pair){
+          [this, uid](const auto& target_pair){
             const auto& [target_uid, target_pack] = target_pair;
             return std::pair{
               target_uid,
