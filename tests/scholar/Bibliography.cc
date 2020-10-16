9da09373347781363d78fba9699a6cbe9f12d790
Alexander Lalejini <amlalejini@gmail.com>
2020-10-16 11:52:16 -0400

    Update include paths for scholar tests

--- tests/scholar/Bibliography.cc
+++ tests/scholar/Bibliography.cc
@@ -2,7 +2,7 @@
 #define CATCH_CONFIG_MAIN
 #include "third-party/Catch/single_include/catch2/catch.hpp"
 
-// #include "scholar/Bibliography.h"
+#include "emp/scholar/Bibliography.h"
 
 TEST_CASE("Test Bibliography", "[scholar]")
 {
--- tests/scholar/Citation.cc
+++ tests/scholar/Citation.cc
@@ -2,7 +2,7 @@
 #define CATCH_CONFIG_MAIN
 #include "third-party/Catch/single_include/catch2/catch.hpp"
 
-// #include "scholar/Citation.h"
+#include "emp/scholar/Citation.h"
 
 TEST_CASE("Test Citation", "[scholar]")
 {
