af63a738ad3456734c9f59d5bb715252b5d5441d
Alexander Lalejini <amlalejini@gmail.com>
2020-10-16 14:50:01 -0400

    Relocate GenericFunction test

--- tests/functional/GenericFunction.cc
+++ tests/functional/GenericFunction.cc
@@ -2,23 +2,23 @@
 
 #include "third-party/Catch/single_include/catch2/catch.hpp"
 
-#include "tools/GenericFunction.h"
+#include "emp/functional/GenericFunction.h"
 
 #include <sstream>
 
-TEST_CASE("Test GenericFunction", "[tools]")
+TEST_CASE("Test GenericFunction", "[functional]")
 {
 	emp::Function<double(double,double)> aFunction = [](double a, double b){ return a*b; };
 	REQUIRE(aFunction(2.0,2.0) == 4.0);
-	
+
 	std::function<double(double,double)> anotherFunction = aFunction.GetFunction();
 	REQUIRE(anotherFunction(5.0,5.0) == aFunction(5.0,5.0));
-	
+
 	REQUIRE(aFunction.CallOK<double>(2.0, 2.0));
 	REQUIRE(!aFunction.CallOK<double>(5, "string"));
-	
+
 	REQUIRE( (aFunction.CallTypeOK<double, double, double>()) );
 	REQUIRE( !(aFunction.CallTypeOK<double, std::string, int>()) );
-	
+
 	REQUIRE( (aFunction.ConvertOK<double(double,double)>()) );
 }
