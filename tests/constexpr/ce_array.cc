5e17c9a35961e417e52bbf5aee01fdfefc2bad62
Alexander Lalejini <amlalejini@gmail.com>
2020-10-16 10:36:22 -0400

    Move constexpr headers to in progress, remove tests
    Not everything in the constexpr/ directory worked, and most functionality will be redundant with C++20.
    To recover/refer to the removed files, see Empirical pre-release tag v0.0.3.

--- tests/constexpr/ce_array.cc
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "third-party/Catch/single_include/catch2/catch.hpp"
-
-#include "emp/constexpr/ce_array.h"
-
-TEST_CASE("Test ce_array", "[constexpr]")
-{
-
-}
--- tests/constexpr/ce_random.cc
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "third-party/Catch/single_include/catch2/catch.hpp"
-
-// #include "emp/constexpr/ce_random.h"
-
-TEST_CASE("Test ce_random", "[constexpr]")
-{
-
-}
