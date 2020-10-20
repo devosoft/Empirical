21bda043c3bee98e281a5340b4d5a9604612491d
Alexander Lalejini <amlalejini@gmail.com>
2020-10-20 14:04:25 -0400

    Update Sudoku demo for file extension changes

--- demos/Sudoku/Sudoku.cpp
+++ demos/Sudoku/Sudoku.cpp
@@ -2,8 +2,8 @@
 //  Copyright (C) Michigan State University, 2015-2017.
 //  Released under the MIT Software license; see doc/LICENSE
 
-#include "base/array.h"
-#include "web/web.h"
+#include "emp/base/array.hpp"
+#include "emp/web/web.hpp"
 
 namespace UI = emp::web;
 
