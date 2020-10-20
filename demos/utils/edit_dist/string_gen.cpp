7d32402f4d9df6bc240524e33c48b85f00ba59d6
Alexander Lalejini <amlalejini@gmail.com>
2020-10-20 17:55:52 -0400

    Fix include path in string_gen demo

--- demos/utils/edit_dist/string_gen.cpp
+++ demos/utils/edit_dist/string_gen.cpp
@@ -12,7 +12,7 @@
 #include "../../../include/emp/base/vector.hpp"
 #include "../../../include/emp/config/command_line.hpp"
 #include "../../../include/emp/tools/functions.hpp"
-#include "../../../include/emp/tools/Random.hpp"
+#include "../../../include/emp/math/Random.hpp"
 
 
 int main(int argc, char* argv[])
