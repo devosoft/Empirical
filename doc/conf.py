7c2c0c0e11133e19157625eeaf94a0b0f179c418
Alexander Lalejini <amlalejini@gmail.com>
2020-10-21 13:37:57 -0400

    Change source path in docs config

--- doc/conf.py
+++ doc/conf.py
@@ -61,7 +61,7 @@ exhale_args = {
     # TIP: if using the sphinx-bootstrap-theme, you need
     # "treeViewIsBootstrap": True,
     "exhaleExecutesDoxygen": True,
-    "exhaleDoxygenStdin":    "INPUT = ../source"
+    "exhaleDoxygenStdin":    "INPUT = ../include"
 }
 
 # Tell sphinx what the primary language being documented is.
