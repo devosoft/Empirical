8abc05c46556747d817f5019db64816539948063
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-25 17:28:14 -0400

    Implement SavePNG method on Canvas

--- source/web/Canvas.h
+++ source/web/Canvas.h
@@ -16,6 +16,7 @@
 
 #include "../base/vector.h"
 #include "../geometry/Circle2D.h"
+#include "../tools/string_utils.h"
 
 #include "CanvasAction.h"
 #include "CanvasShape.h"
@@ -284,8 +285,32 @@ namespace web {
 
     }
 
-  };
+    /// Save a PNG image of a canvas with node.js.
+    void SavePNG(const std::string& fname) {
+      const std::string command = R"(
+        setTimeout(function(){
 
+          fs = require('fs');
+
+          canvas = document.getElementById(%s);
+
+          // adapted from https://stackoverflow.com/a/11335500
+          var url = canvas.toDataURL('image/png');
+          var buffer = Buffer.from(data, 'base64');
+
+          fs.writeFileSync(%s, buffer);
+
+        }, 10);
+      )";
+
+      emscripten_run_script(
+        emp::format_string( command, Info()->id.c_str(), fname.c_str() ).c_str()
+      );
+
+
+    }
+
+  };
 
 }
 }
