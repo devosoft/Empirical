c349d32b1eee426ba028b94d74579d5f74e8c276
Charles Ofria <charles.ofria@gmail.com>
2020-11-25 11:39:39 -0500

    Sped up BitVector::Toggle for ranges by working with full fields.

--- include/emp/bits/BitVector.hpp
+++ include/emp/bits/BitVector.hpp
@@ -442,11 +442,42 @@ namespace emp {
     }
 
     /// Flips all the bits in a range [start, end)
-    BitVector & Toggle(size_t start, size_t end) {
-      emp_assert(start <= end && end <= num_bits);
-      for(size_t index = start; index < end; index++) {
-        Toggle(index);
+    BitVector & Toggle(size_t start, size_t stop) {
+      emp_assert(start <= stop, start, stop, num_bits);
+      emp_assert(stop <= num_bits, stop, num_bits);
+      const size_t start_pos = FieldPos(start);
+      const size_t stop_pos = FieldPos(stop);
+      size_t start_field = FieldID(start);
+      const size_t stop_field = FieldID(stop);
+      constexpr field_t val_one = 1;
+
+      // If the start field and stop field are the same, just step through the bits.
+      if (start_field == stop_field) {
+        const size_t num_bits = stop - start;
+        const field_t mask = ((val_one << num_bits) - 1) << start_pos;
+        bit_set[start_field] ^= mask;
       }
+
+      // Otherwise handle the ends and clear the chunks in between.
+      else {
+        // Toggle correct portions of start field
+        if (start_pos != 0) {
+          const size_t start_bits = FIELD_BITS - start_pos;
+          const field_t start_mask = ((val_one << start_bits) - 1) << start_pos;
+          bit_set[start_field] ^= start_mask;
+          start_field++;
+        }
+
+        // Middle fields
+        for (size_t cur_field = start_field; cur_field < stop_field; cur_field++) {
+          bit_set[cur_field] = ~bit_set[cur_field];
+        }
+
+        // Set portions of stop field
+        const field_t stop_mask = (val_one << stop_pos) - 1;
+        bit_set[stop_field] ^= stop_mask;
+      }
+
       return *this;
     }
 
