ad6634ffc41d0d3db80dcc849769a616c910c771
Charles Ofria <charles.ofria@gmail.com>
2020-12-04 11:16:17 -0500

    Expanding analysis of levelization files.

--- apps/utils/levelize/levelize.cpp
+++ apps/utils/levelize/levelize.cpp
@@ -1,9 +1,18 @@
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <set>
 #include <string>
 
 #include "../../../include/emp/base/vector.hpp"
+#include "../../../include/emp/io/File.hpp"
 #include "../../../include/emp/tools/string_utils.hpp"
+
+struct FileInfo {
+  std::string filename;
+  std::string path;
+  std::set<std::string> depends; // Which OTHER files does this one depend on?
+};
 
 int main(int argc, char * argv[])
 {
@@ -13,16 +22,24 @@ int main(int argc, char * argv[])
   for (size_t i = 0; i < num_files; i++) files[i] = argv[i+1];
 
   // Simplify to just the filenames (remove paths)
+  std::map<std::string, FileInfo> file_map;
   emp::vector<std::string> filenames;
   for (std::string & file : files) {
     emp::vector<std::string_view> dir_struct = emp::view_slices(file, '/');
-    std::string_view fileview = dir_struct.back();
-    std::string filename(fileview);
-    filenames.push_back( filename );
+    std::string filename(dir_struct.back());
+    file_map[filename].filename = filename;
+    file_map[filename].path = file;
+    filenames.push_back(filename);
   }
 
+  // For each file, scan for its dependencies.
+  for (auto & [filename, info] : file_map) {
+    emp::File file(info.path);
+    file.KeepIf( [](const std::string & line){ return line.find("#include") != std::string::npos; } );
+  }
 
-  for (size_t i = 0; i < num_files; i++) {
-    std::cout << files[i] << " : " << filenames[i] << "\n";
+  // List out the files.
+  for (auto [filename, info] : file_map) {
+    std::cout << info.path << " : " << filename << "\n";
   }
 }
