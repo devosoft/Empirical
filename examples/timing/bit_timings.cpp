1f8b01829fba4c9b1f757ce38b62e07dcf6d8d24
Charles Ofria <charles.ofria@gmail.com>
2020-12-17 09:50:51 -0500

    Setup structure for bit timing tests.

--- examples/timing/bit_timings.cpp
+++ examples/timing/bit_timings.cpp
@@ -12,6 +12,8 @@
 #include "emp/bits/BitVector.hpp"
 #include "emp/math/Random.hpp"
 
+#define TEST_SIZES 1, 8, 31, 32, 50, 63, 64, 100, 1000, 10000, 1000000, 1048576
+
 // Return the timing of a function in seconds.
 template <typename T>
 double TimeFunction(T && fun) {
@@ -21,11 +23,43 @@ double TimeFunction(T && fun) {
   return total_time / (double) CLOCKS_PER_SEC;
 }
 
+
+template <size_t... SIZES> struct SpeedTester { };
+
+template <size_t SIZE1, size_t... OTHER_SIZES>
+struct SpeedTester<SIZE1, OTHER_SIZES...> : public SpeedTester<OTHER_SIZES...>{
+  static constexpr size_t cur_size = SIZE1;
+  static constexpr size_t bs_count = sizeof...(OTHER_SIZES);
+  emp::BitSet<SIZE1> bs;
+  emp::BitVector bv;
+
+  template <size_t ID> auto GetBitSet() {
+    if constexpr(ID == 0) return bs;
+    else return SpeedTester<OTHER_SIZES...>::template GetBitSet<ID-1>();
+  }
+
+  template <size_t ID> auto GetBitVector() {
+    if constexpr(ID == 0) return bs;
+    else return SpeedTester<OTHER_SIZES...>::template GetBitVector<ID-1>();
+  }
+
+  SpeedTester() : bv(SIZE1) { }
+};
+
+template <>
+struct SpeedTester<> {
+  static constexpr size_t cur_size = 0;
+  static constexpr size_t bs_count = 0;
+
+  auto GetBitSet() { return 0; }
+  auto GetBitVector() { return 0; }
+};
+
 int main()
 {
   emp::Random random;
 
-  const emp::vector<size_t> test_sizes = { 1, 31, 32, 50, 63, 64, 100, 1000, 10000, 1000000 };
+  const emp::vector<size_t> test_sizes = { TEST_SIZES };
 
   for (size_t num_bits : test_sizes) {
     std::cout << "Testing with " << num_bits << " bits." << std::endl;
