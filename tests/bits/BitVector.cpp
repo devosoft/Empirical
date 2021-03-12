65bff243ab4c7770d315ce5bef89471255611b8f
Charles Ofria <charles.ofria@gmail.com>
2021-03-12 12:11:21 -0500

    Added bit-based Randomication distribution testing to BitVector.

--- tests/bits/BitVector.cpp
+++ tests/bits/BitVector.cpp
@@ -459,6 +459,34 @@ TEST_CASE("5: Test Randomize() and variants", "[bits]") {
     bv.SetRandomCount(random, 567);    // Half of the remaining ones should be set; 607.871 expected.
     num_ones = bv.CountOnes();         REQUIRE(num_ones == 567);
   }
+
+
+  // During randomization, make sure each bit position is set appropriately.
+  std::vector<size_t> one_counts(1000, 0);
+
+  for (size_t test_num = 0; test_num < 1000; ++test_num) {
+    // Set bits with different probabilities in different ranges.
+    bv.Clear();
+    bv.Randomize(random, 0.5,  100, 250);
+    bv.Randomize(random, 0.25, 250, 400);
+    bv.Randomize(random, 0.75, 400, 550);
+    bv.Randomize(random, 0.10, 550, 700);
+    bv.Randomize(random, 0.98, 700, 850);
+
+    // Keep count of how many times each position was a one.
+    for (size_t i = 0; i < bv.GetSize(); ++i) {
+      if (bv.Get(i)) one_counts[i]++;
+    }
+  }
+
+  // Check if the counts are reasonable.
+  for (size_t i = 0;   i < 100; i++)  { REQUIRE(one_counts[i] == 0); }
+  for (size_t i = 100; i < 250; i++)  { REQUIRE(one_counts[i] > 420);  REQUIRE(one_counts[i] < 580); }
+  for (size_t i = 250; i < 400; i++)  { REQUIRE(one_counts[i] > 190);  REQUIRE(one_counts[i] < 320); }
+  for (size_t i = 400; i < 550; i++)  { REQUIRE(one_counts[i] > 680);  REQUIRE(one_counts[i] < 810); }
+  for (size_t i = 550; i < 700; i++)  { REQUIRE(one_counts[i] >  60);  REQUIRE(one_counts[i] < 150); }
+  for (size_t i = 700; i < 850; i++)  { REQUIRE(one_counts[i] > 950);  REQUIRE(one_counts[i] < 999); }
+  for (size_t i = 850; i < 1000; i++) { REQUIRE(one_counts[i] == 0); }
 }
 
 TEST_CASE("6: Test getting and setting whole chunks of bits", "[bits]") {
