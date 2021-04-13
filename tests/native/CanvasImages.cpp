638ad3b839e27dc3db4b856e1d8abdbb8e3ab331
Katherine <perryk12@msu.edu>
2021-04-13 14:56:45 +0000

    Compare new point location pngs

--- tests/native/CanvasImages.cpp
+++ tests/native/CanvasImages.cpp
@@ -37,6 +37,10 @@ TEST_CASE("Test Images MSE", "[native]") {
   success = std::system( command.c_str() );
   REQUIRE(success == 0);
 
+  command = "python3 compare_images.py -f rect_location.png -s test_rect_location2.png";
+  success = std::system( command.c_str() );
+  REQUIRE(success == 0);
+
   command = "python3 compare_images.py -f rect_color.png -s test_rect_color.png";
   success = std::system( command.c_str() );
   REQUIRE(success == 0);
@@ -44,8 +48,20 @@ TEST_CASE("Test Images MSE", "[native]") {
   command = "python3 compare_images.py -f rect_size.png -s test_rect_size.png";
   success = std::system( command.c_str() );
   REQUIRE(success == 0);
+  
+  command = "python3 compare_images.py -f line_location.png -s test_line_location.png";
+  success = std::system( command.c_str() );
+  REQUIRE(success == 0);
+
+  command = "python3 compare_images.py -f line_location.png -s test_line_location2.png";
+  success = std::system( command.c_str() );
+  REQUIRE(success == 0);
 
   command = "python3 compare_images.py -f text_location.png -s test_text_location.png";
+  success = std::system( command.c_str() );
+  REQUIRE(success == 0);
+
+  command = "python3 compare_images.py -f text_location.png -s test_text_location2.png";
   success = std::system( command.c_str() );
   REQUIRE(success == 0);
 
@@ -54,6 +70,10 @@ TEST_CASE("Test Images MSE", "[native]") {
   REQUIRE(success == 0);
 
   command = "python3 compare_images.py -f centertext_location.png -s test_centertext_location.png";
+  success = std::system( command.c_str() );
+  REQUIRE(success == 0);
+
+  command = "python3 compare_images.py -f centertext_location.png -s test_centertext_location2.png";
   success = std::system( command.c_str() );
   REQUIRE(success == 0);
 
