1d198c698a6c217c9ead04de27c2525157c92cc9
Charles Ofria <charles.ofria@gmail.com>
2021-05-29 12:04:36 -0400

    Restructured genome to inheret from container.

--- include/emp/hardware/Genome.hpp
+++ include/emp/hardware/Genome.hpp
@@ -24,17 +24,16 @@
 namespace emp {
 
   template < typename INST_T, typename INST_LIB_T, typename SEQUENCE_T=emp::vector<INST_T> >
-  class Genome {
+  class Genome : public SEQUENCE_T {
   private:
     emp::Ptr<const INST_LIB_T> inst_lib;
-    SEQUENCE_T sequence;
 
   public:
     Genome() = default;
     Genome(emp::Ptr<const INST_LIB_T> _inst_lib, const SEQUENCE_T & _seq=SEQUENCE_T())
-      : inst_lib(_inst_lib), sequence(_seq) { ; }
+      : SEQUENCE_T(_seq), inst_lib(_inst_lib) { ; }
     Genome(const INST_LIB_T & _inst_lib, const SEQUENCE_T & _seq=SEQUENCE_T())
-      : inst_lib(&_inst_lib), sequence(_seq) { ; }
+      : SEQUENCE_T(_seq), inst_lib(&_inst_lib) { ; }
     Genome(const Genome &) = default;
     Genome(Genome &&) = default;
     ~Genome() { ; }
@@ -43,19 +42,44 @@ namespace emp {
     Genome & operator=(Genome &&) = default;
 
     auto GetInstLib() const { return inst_lib; }
-    INST_T & operator[](size_t id) { return sequence[id]; }
-    const INST_T & operator[](size_t id) const { return sequence[id]; }
 
-    bool operator==(const Genome& other) const { return sequence == other.sequence; }
-    bool operator!=(const Genome& other) const { return sequence != other.sequence; }
-    bool operator< (const Genome& other) const { return sequence <  other.sequence; }
-    bool operator<=(const Genome& other) const { return sequence <= other.sequence; }
-    bool operator> (const Genome& other) const { return sequence >  other.sequence; }
-    bool operator>=(const Genome& other) const { return sequence >= other.sequence; }
+    // INST_T & operator[](size_t id) { return sequence[id]; }
+    // const INST_T & operator[](size_t id) const { return sequence[id]; }
+
+    size_t GetSize() const { return SEQUENCE_T::size(); }
+    // size_t size() const { return sequence.size(); }    // For compatability with std.
+
+    void Resize(size_t new_size) { SEQUENCE_T::resize(new_size); }
+    // size_t resize(size_t new_size) { return sequence.resize(new_size); }    // For compatability with std.
+
+    bool operator==(const Genome& other) const { return SEQUENCE_T::operator==(*this, other); }
+    bool operator!=(const Genome& other) const { return SEQUENCE_T::operator!=(*this, other); }
+    bool operator< (const Genome& other) const { return SEQUENCE_T::operator<(*this,  other); }
+    bool operator<=(const Genome& other) const { return SEQUENCE_T::operator<=(*this, other); }
+    bool operator> (const Genome& other) const { return SEQUENCE_T::operator>(*this,  other); }
+    bool operator>=(const Genome& other) const { return SEQUENCE_T::operator>=(*this, other); }
+
+    // auto begin() { return sequence.begin(); }
+    // auto begin() const { return sequence.begin(); }
+    // auto end() { return sequence.end(); }
+    // auto end() const { return sequence.end(); }
+    
+    // auto rbegin() { return sequence.rbegin(); }
+    // auto rbegin() const { return sequence.rbegin(); }
+    // auto rend() { return sequence.rend(); }
+    // auto rend() const { return sequence.rend(); }
+
+    // auto cbegin() const { return sequence.cbegin(); }
+    // auto cend() const { return sequence.cend(); }
+    // auto crbegin() const { return sequence.crbegin(); }
+    // auto crend() const { return sequence.crend(); }
+
+    // template <typename... Ts>
+    // void emplace_back(Ts... args) { sequence.emplace_back(std::forward<Ts>(args)...); }
 
     size_t Hash() const {
-      std::size_t seed = sequence.size();
-      for(auto& i : sequence) {
+      std::size_t seed = GetSize();
+      for(auto& i : *this) {
         seed ^= i.id + 0x9e3779b9 + (seed << 6) + (seed >> 2);
       }
       return seed;
