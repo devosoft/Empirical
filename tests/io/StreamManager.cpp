3392bd685bfbe1c4cced91c4c1609e4d61db85e0
Charles Ofria <charles.ofria@gmail.com>
2021-08-13 10:16:11 -0400

    Added some quick tests from StreamManager

--- tests/io/StreamManager.cpp
+++ tests/io/StreamManager.cpp
@@ -6,4 +6,18 @@
 
 TEST_CASE("Test StreamManager", "[io]")
 {
+  emp::StreamManager sm;
+   
+  std::stringstream & test_stream = sm.AddStringStream("test_stream");
+  test_stream << "abc" << 123 << 'd' << 'e' << 'f';
+  REQUIRE( test_stream.str() == "abc123def" );
+
+  std::iostream & test_stream2 = sm.AddIOStream("test_stream2");
+  test_stream2 << "This is the second stream!!!";
+
+  std::iostream & test_stream_orig = sm.GetIOStream("test_stream");
+  std::string out_str = "";
+  test_stream_orig >> out_str;
+  REQUIRE( out_str == "abc123def" );
+
 }
