0d3256e749a90079f71c71c7b83bc5f72d729950
Tait Weicht <taitweicht@icloud.com>
2021-08-20 16:58:43 -0700

    Add documentation

--- include/emp/prefab/ButtonGroup.hpp
+++ include/emp/prefab/ButtonGroup.hpp
@@ -4,20 +4,33 @@
 #include "emp/web/Div.hpp"
 
 namespace emp::prefab {
-
+  /**
+   * Use a ButtonGroup to place buttons of a similar role into the same
+   * container or to save space by placing buttons without gaps between them.
+   */
   class ButtonGroup : public web::Div {
 
     protected:
+    /**
+     * The protected contructor for a ButtonGroup.
+     * @param info_ref shared pointer containing presistent state
+     */
     ButtonGroup(web::internal::DivInfo * info_ref) : web::Div(info_ref) {
       SetAttr("class", "btn-group");
     }
 
     public:
+    /**
+     * Constructor for a ButtonGroup.
+     * @param in_id HTML ID of ButtonGroup div
+     */
     ButtonGroup(const std::string & in_id="")
     : ButtonGroup(new web::internal::DivInfo(in_id)) { ; }
 
     /**
      * A function useful for joining two button groups together into one unit.
+     * Removes buttons from the ButtonGroup passed in and adds them to this
+     * button group group.
      * @param btn_group a button group
      */
     ButtonGroup & TakeChildren(ButtonGroup & btn_group) {
--- include/emp/prefab/ToggleButtonGroup.hpp
+++ include/emp/prefab/ToggleButtonGroup.hpp
@@ -12,7 +12,12 @@ namespace emp::prefab {
 
   namespace internal {
     using on_toggle_t = std::function<void(bool)>;
-
+    /**
+     * Shared pointer held by instances of ToggleButtonGroup class representing
+     * the same conceptual ToggleButtonGroup DOM object.
+     * Contains state that should persist while ToggleButtonGroup DOM object
+     * persists.
+     */
     class ToggleButtonGroupInfo : public web::internal::DivInfo {
       on_toggle_t callback;
       bool active;
@@ -21,24 +26,46 @@ namespace emp::prefab {
       ToggleButtonGroupInfo(const std::string & in_id)
       : web::internal::DivInfo(in_id), callback([](bool){ ; }), active(false) { ; }
 
+      /**
+       * Set the function to be called when the component toggles
+       * @param cb a callback function that accepts a boolean indicating
+       * whether the toggle is active or inactive
+       */
       void UpdateCallback(const on_toggle_t & cb) {
         callback = cb;
       }
 
+      /**
+       * Get the function to be called when the component toggles
+       * @return a callback function
+       */
       const on_toggle_t & GetCallback() const {
         return callback;
       }
 
+      /**
+       * Determines whether the toggle is in the active state
+       * @return boolean
+       */
       bool IsActive() const {
         return active;
       }
 
-      void SetActive(const bool & val) {
-        active = val;
+      /**
+       * Set the active state of the toggle
+       * @param is_active a boolean
+       */
+      void SetActive(const bool & is_active) {
+        active = is_active;
       }
     };
   }
 
+  /**
+   * Use a ToggleButtonGroup to create a control with two labeled, visually
+   * distinct states. Choose whether the button should display cassette-style
+   * with two separate buttons or as a single button that flip-flops state.
+   */
   class ToggleButtonGroup : public ButtonGroup {
     using on_toggle_t = internal::on_toggle_t;
 
@@ -74,6 +101,7 @@ namespace emp::prefab {
      * (both buttons visible) or do a swap on toggle (one button visible)
      * @param grayout in cassette mode, whether buttons should be
      * grayed out to further emphasize the current state
+     * @param info_ref shared pointer containing presistent state
      */
     template<typename L1_TYPE, typename L2_TYPE>
     ToggleButtonGroup(
@@ -111,6 +139,10 @@ namespace emp::prefab {
         tog.SetActive(true);
         handle_toggle(true);
       });
+      // OnClick used due to a strange bug(?) in which the input radios only
+      // fire their onchange function once
+      // Probably due to Bootstrap/jQuery weirdness and this is a last minute
+      // work around ¯\_(ツ)_/¯
 
       web::Input activate_radio(
         [](std::string){ ; },
@@ -140,6 +172,21 @@ namespace emp::prefab {
     }
 
     public:
+    /**
+     * @param activate_indicator a string, FontAwesomeIcon or other component
+     * indicating that the first button actives this toggle
+     * @param deactivate_indicator a string, FontAwesomeIcon or other component
+     * indicating that the second button deactivates this toggle
+     * @param activate_style a bootstrap style (primary, secondary, etc) for
+     * the first button
+     * @param deactivate_style a bootstrap style (primary, secondary, etc) for
+     * the second button
+     * @param cassette_style whether the toggle should display in cassette style
+     * (both buttons visible) or do a swap on toggle (one button visible)
+     * @param grayout in cassette mode, whether buttons should be
+     * grayed out to further emphasize the current state
+     * @param in_id HTML ID of ToggleButtonGroup div
+     */
     template<typename L1_TYPE, typename L2_TYPE>
     ToggleButtonGroup(
       L1_TYPE && activate_indicator,
@@ -157,18 +204,35 @@ namespace emp::prefab {
       new internal::ToggleButtonGroupInfo(in_id)
     ) { ; }
 
+    /**
+     * Determines whether the toggle is in the active state
+     * @return boolean
+     */
     bool IsActive() const {
       return Info()->IsActive();
     }
 
+    /**
+     * Set the active state of the toggle
+     * @param is_active a boolean
+     */
     void SetActive(const bool & val) {
       Info()->SetActive(val);
     }
 
+    /**
+     * Get the function to be called when the component toggles
+     * @return a callback function
+     */
     const on_toggle_t & GetCallback() const {
       return Info()->GetCallback();
     }
 
+    /**
+     * Set the function to be called when the component toggles
+     * @param cb a callback function that accepts a boolean indicating
+     * whether the toggle is active or inactive
+     */
     ToggleButtonGroup & SetCallback(const on_toggle_t & cb) {
       Info()->UpdateCallback(cb);
       return (*this);
