2100494ed659e2c5cf86f4ac1aa940eccde0ae35
Charles Ofria <charles.ofria@gmail.com>
2021-09-07 14:11:05 -0400

    A buch more unit tests for DataMapParser.

--- tests/data/DataMapParser.cpp
+++ tests/data/DataMapParser.cpp
@@ -7,22 +7,42 @@
 TEST_CASE("Test DataMap", "[data]")
 {
   emp::DataMap dmA;
-  dmA.AddVar<double>("val1", 1.234);
+  dmA.AddVar<double>("val1", 1.5);
   dmA.AddVar<double>("val2", 2.0);
   dmA.AddVar<int>("val3", 3);
   dmA.AddVar<char>("char", 'A');
 
   emp::DataMap dmB(dmA);
-  dmB.Get<double>("val1") = 4.44;
-  dmB.Get<double>("val2") = 55.5;
-  dmB.Get<int>("val3") = 100;
+  dmB.Get<double>("val1") = 0.125;
+  dmB.Get<double>("val2") = 64.25;
+  dmB.Get<int>("val3") = 4;
   dmB.Get<char>("char") = '%';
 
-  auto fun1 = emp::DataMapParser::BuildMathFunction(dmA, "5 * 5");
-  std::cout << "result1A = " << fun1(dmA) << std::endl;
-  std::cout << "result1B = " << fun1(dmB) << std::endl;
+  auto fun = emp::DataMapParser::BuildMathFunction(dmA, "5 * 5");
+  CHECK( fun(dmA) == 25.0 );
+  CHECK( fun(dmB) == 25.0 );
 
-  auto fun2 = emp::DataMapParser::BuildMathFunction(dmA, "val1 + val2 + 2*val3");
-  std::cout << "result2A = " << fun2(dmA) << std::endl;
-  std::cout << "result2B = " << fun2(dmB) << std::endl;
+  fun = emp::DataMapParser::BuildMathFunction(dmA, "val1 + val2 + 2*val3");
+  CHECK( fun(dmA) == 9.5 );
+  CHECK( fun(dmB) == 72.375 );
+
+  fun = emp::DataMapParser::BuildMathFunction(dmA, "1.5*val3");
+  CHECK( fun(dmA) == 4.5 );
+  CHECK( fun(dmB) == 6.0 );
+
+  fun = emp::DataMapParser::BuildMathFunction(dmA, "val1/0.5");
+  CHECK( fun(dmA) == 3.0 );
+  CHECK( fun(dmB) == 0.25 );
+
+  fun = emp::DataMapParser::BuildMathFunction(dmA, "1.5*val3 - val1/0.5");
+  CHECK( fun(dmA) == 1.5 );
+  CHECK( fun(dmB) == 5.75 );
+
+  fun = emp::DataMapParser::BuildMathFunction(dmA, "val2/64");
+  CHECK( fun(dmA) == 0.03125 );
+  CHECK( fun(dmB) == 1.00390625 );
+
+  fun = emp::DataMapParser::BuildMathFunction(dmA, "1.5*val3 - val1/0.5 - val2/64");
+  CHECK( fun(dmA) == 1.46875 );
+  CHECK( fun(dmB) == 4.74609375 );
 }
