ba7d7199b97727f149ead2b6a69eef14385819e9
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-09-25 14:53:42 -0400

    Ensure blank lines around header guards

--- ci/generate_boilerplate_headerguards.sh
+++ ci/generate_boilerplate_headerguards.sh
@@ -3,7 +3,7 @@
 
 for filename in $(cd include && find -- * -name '*.hpp' -type f); do
 
-  # grow file up to at least 4 lines past close of docstring
+  # grow file up to at least 6 lines past close of docstring
   # determined using first blank line or last line
   FILE_NUM_LINES="$(cat "include/${filename}" | wc -l)"
   ONE_PAST_LAST_LINE_NO="$((${FILE_NUM_LINES}+1))"
@@ -15,7 +15,7 @@ for filename in $(cd include && find -- * -name '*.hpp' -type f); do
 
   # grow the file, if required
   FILE_NUM_LINES="$(cat "include/${filename}" | wc -l)"
-  REQUIRED_NUM_LINES="$((${DOCSTRING_CLOSE_LINE_NO} + 4))"
+  REQUIRED_NUM_LINES="$((${DOCSTRING_CLOSE_LINE_NO} + 6))"
   for __ in $(seq "${FILE_NUM_LINES}" 1 "${REQUIRED_NUM_LINES}"); do
     echo >> "include/${filename}"
   done
@@ -27,6 +27,10 @@ for filename in $(cd include && find -- * -name '*.hpp' -type f); do
 
   sed -i "$((${DOCSTRING_CLOSE_LINE_NO}+2))s/^.*\$/${NDEF_LINE}/" "include/${filename}"
   sed -i "$((${DOCSTRING_CLOSE_LINE_NO}+3))s/^.*\$/${DEF_LINE}/" "include/${filename}"
+  sed -i "$((${DOCSTRING_CLOSE_LINE_NO}+4))s/^.*\$//" "include/${filename}"
+  # ensure blank second-to-last line
+  # adapted from https://stackoverflow.com/a/38649101
+  sed -i 'x; ${s/.*//;p;x}; 1d' "include/${filename}"
   sed -i '$ s,^.*$,'"${ENDIF_LINE}"',g' "include/${filename}"
 
 done
