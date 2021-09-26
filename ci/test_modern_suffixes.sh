ece5ffe84f4b1f6a050a5d1836da5ee0ff095bde
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-09-26 01:30:19 -0400

    Fix bad script path

--- ci/test_modern_suffixes.sh
+++ ci/test_modern_suffixes.sh
@@ -2,12 +2,12 @@
 
 SOURCE_HASH=$( find -path ./third-party -prune -false -o -type f | sort | xargs cat | sha1sum )
 
-./ci/modernize_suffixes.sh
+./ci/modernize_filename_suffixes.sh
 
 if [ "${SOURCE_HASH}" == "$( find -path ./third-party -prune -false -o -type f | sort | xargs cat | sha1sum )" ];
 then
   exit 0 # success
 else
-  echo "old-style suffixes in filenames detected, run ./ci/modernize_suffixes.sh locally to find & fix"
+  echo "old-style suffixes in filenames detected, run ./ci/modernize_filename_suffixes.sh locally to find & fix"
   exit 1 # failure
 fi
