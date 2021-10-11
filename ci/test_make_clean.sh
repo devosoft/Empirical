b391c06932f678627fd85e655bd7b17c697b497f
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-10-11 02:03:54 -0400

    Fix fail message for test_make_clean.sh

--- ci/test_make_clean.sh
+++ ci/test_make_clean.sh
@@ -8,6 +8,6 @@ if [ "${SOURCE_HASH}" == "$( find -path ./submodule -prune -false -o -type f | s
 then
   exit 0 # success
 else
-  echo "tab characters detected, run ./ci/replace_tabs.sh locally to find & fix"
+  echo "compilation artifacts detected, run make clean locally to find & fix"
   exit 1 # failure
 fi
