b1ce62e0c14fdfc276b84e63a445e2fb8f593ea6
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-10-14 12:00:56 -0400

    Add safety guards for uncommitted changes

--- ci/test_alphabetize_imports.sh
+++ ci/test_alphabetize_imports.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# refuse to continue if uncommitted changes are present
+# adapted from https://stackoverflow.com/a/40535565
+if ! [[ -z $(git status -s) ]];
+then
+  ./ci/print_uncommitted_changes_warning.sh
+  exit 1
+fi
+
 SOURCE_HASH=$( find -path ./third-party -prune -false -o -type f | sort | xargs cat | sha1sum )
 
 ./ci/alphabetize_imports.sh
