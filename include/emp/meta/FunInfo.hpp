e414a4b81f428896df079133e949a4221238a74e
Charles Ofria <charles.ofria@gmail.com>
2021-10-15 15:38:41 -0400

    Added FunInfo::WrapReturnType().

--- include/emp/meta/FunInfo.hpp
+++ include/emp/meta/FunInfo.hpp
@@ -64,17 +64,25 @@ namespace emp {
       return std::is_invocable<CLASS_T, ARG_Ts...>();
     }
 
+    template <typename FUN_T, typename CONVERTER_T>
+    static auto WrapReturnType(FUN_T fun, CONVERTER_T convert_fun)
+    {
+      return [fun=fun, c=convert_fun](PARAM1_T && arg1, PARAM_Ts &&... args) {
+        return c( fun(std::forward<PARAM1_T>(arg1), std::forward<PARAM_Ts>(args)...) );
+      };
+    }
+
     template <typename T>
     static auto BindFirst(CLASS_T fun, T && bound) {
       // If the function needs a reference for the parameter, send the supplied value through.
       if constexpr (std::is_reference<PARAM1_T>()) {
-        return [fun, &bound](PARAM_Ts... args) {
+        return [fun, &bound](PARAM_Ts &&... args) {
           return fun(std::forward<T>(bound), std::forward<PARAM_Ts>(args)...);
         };
       }
       // Otherwise, a copy is fine.
       else {
-        return [fun, bound](PARAM_Ts... args) {
+        return [fun, bound](PARAM_Ts &&... args) {
           return fun(std::forward<T>(bound), std::forward<PARAM_Ts>(args)...);
         };
       }
@@ -99,10 +107,27 @@ namespace emp {
     /// Test if this function can be called with a particular set of arguments.
     template <typename... ARG_Ts>
     static constexpr bool InvocableWith(ARG_Ts...) { return sizeof...(ARG_Ts) == 0; }
+
+    template <typename FUN_T, typename CONVERTER_T>
+    static auto WrapReturnType(FUN_T fun, CONVERTER_T convert_fun)
+    {
+      return [fun=fun, c=convert_fun]() {
+        return c(fun());
+      };
+    }
+
   };
 
 
-  // Stand-alone helper functions.
+  // === Stand-alone helper functions ===
+
+  template <typename FUN_T, typename CONVERTER_T>
+  static auto WrapReturnType(FUN_T fun, CONVERTER_T convert_fun)
+  {
+    return FunInfo<FUN_T>::WrapReturnType(fun, convert_fun);
+  }
+
+  /// Lock in the first argument of a function.
   template <typename FUN_T, typename T>
   auto BindFirst(FUN_T fun, T && bound) {
     return FunInfo<FUN_T>::BindFirst(fun, std::forward<T>(bound));
