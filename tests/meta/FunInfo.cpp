900ec86912b6c5fac26a27717b7e746a04deae4f
Charles Ofria <charles.ofria@gmail.com>
2021-10-14 23:43:37 -0400

    Added lots of checks for FunInfo correctly determining types.

--- tests/meta/FunInfo.cpp
+++ tests/meta/FunInfo.cpp
@@ -4,6 +4,7 @@
 
 #include <sstream>
 #include <string>
+#include <type_traits>
 
 #include "emp/base/vector.hpp"
 #include "emp/base/Ptr.hpp"
@@ -11,7 +12,7 @@
 
 std::string StringDup(std::string base, int dup) {
   std::string out = "";
-  for (int i =0; i < dup; i++) { out += base; }
+  for (int i = 0; i < dup; i++) { out += base; }
   return out;
 }
 
@@ -23,14 +24,64 @@ TEST_CASE("Test FunInfo", "[meta]")
 
   std::function<double(double,double,double)> fun4 = lambda2;
   std::function<double(double,double)> fun5 = lambda3;
+  std::function<std::string(std::string,int)> fun6 = StringDup;
 
-  using info0 = emp::FunInfo<decltype(StringDup)>;
-  using info1 = emp::FunInfo<decltype(lambda1)>;
-  using info2 = emp::FunInfo<decltype(lambda2)>;
-  using info3 = emp::FunInfo<decltype(lambda3)>;
-  using info4 = emp::FunInfo<decltype(fun4)>;
-  using info5 = emp::FunInfo<decltype(fun5)>;
+  std::function<size_t(std::string &)> fun7 = [](std::string & in){ in += '+'; return in.size(); };
+
+  using info0_t = emp::FunInfo<decltype(StringDup)>;
+  using info1_t = emp::FunInfo<decltype(lambda1)>;
+  using info2_t = emp::FunInfo<decltype(lambda2)>;
+  using info3_t = emp::FunInfo<decltype(lambda3)>;
+  using info4_t = emp::FunInfo<decltype(fun4)>;
+  using info5_t = emp::FunInfo<decltype(fun5)>;
+  using info6_t = emp::FunInfo<decltype(fun6)>;
+  using info7_t = emp::FunInfo<decltype(fun7)>;
 
   // Test NumArgs()
-  REQUIRE(info1::NumArgs() == 0);
+  CHECK(info0_t::NumArgs() == 2);
+  CHECK(info1_t::NumArgs() == 0);
+  CHECK(info2_t::NumArgs() == 3);
+  CHECK(info3_t::NumArgs() == 2);
+  CHECK(info4_t::NumArgs() == 3);
+  CHECK(info5_t::NumArgs() == 2);
+  CHECK(info6_t::NumArgs() == 2);
+  CHECK(info7_t::NumArgs() == 1);
+
+  // Check return types
+  CHECK(std::is_same<info0_t::return_t, std::string>() == true);
+  CHECK(std::is_same<info1_t::return_t, int>() == true);
+  CHECK(std::is_same<info2_t::return_t, int>() == true);
+  CHECK(std::is_same<info3_t::return_t, double>() == true);
+  CHECK(std::is_same<info4_t::return_t, double>() == true);
+  CHECK(std::is_same<info5_t::return_t, double>() == true);
+  CHECK(std::is_same<info6_t::return_t, std::string>() == true);
+  CHECK(std::is_same<info7_t::return_t, size_t>() == true);
+
+  // Check argument types
+  CHECK(std::is_same<info0_t::arg_t<0>, std::string>() == true);
+  CHECK(std::is_same<info0_t::arg_t<1>, int>() == true);
+  CHECK(std::is_same<info0_t::arg_t<1>, std::string>() == false);
+  CHECK(std::is_same<info0_t::arg_t<1>, char>() == false);
+
+  CHECK(std::is_same<info2_t::arg_t<0>, int>() == true);
+  CHECK(std::is_same<info2_t::arg_t<1>, int>() == true);
+  CHECK(std::is_same<info2_t::arg_t<2>, int>() == true);
+  CHECK(std::is_same<info2_t::arg_t<2>, double>() == false);
+
+  CHECK(std::is_same<info3_t::arg_t<0>, double>() == true);
+  CHECK(std::is_same<info3_t::arg_t<1>, double>() == true);
+
+  CHECK(std::is_same<info4_t::arg_t<0>, double>() == true);
+  CHECK(std::is_same<info4_t::arg_t<1>, double>() == true);
+  CHECK(std::is_same<info4_t::arg_t<2>, double>() == true);
+
+  CHECK(std::is_same<info5_t::arg_t<0>, double>() == true);
+  CHECK(std::is_same<info5_t::arg_t<1>, double>() == true);
+
+  CHECK(std::is_same<info6_t::arg_t<0>, std::string>() == true);
+  CHECK(std::is_same<info6_t::arg_t<1>, int>() == true);
+
+  CHECK(std::is_same<info7_t::arg_t<0>, std::string &>() == true);
+  CHECK(std::is_same<info7_t::arg_t<0>, std::string>() == false); // Must be reference!
+  CHECK(std::is_same<info7_t::arg_t<0>, const std::string &>() == false); // Not const!
 }
