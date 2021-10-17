65fe21e3f6a4195be75abce564bc3cbc8508be14
Charles Ofria <charles.ofria@gmail.com>
2021-10-17 16:25:23 -0400

    Added FunInfo::BindAt() to bind arbitrary arguments to a function.

--- include/emp/meta/FunInfo.hpp
+++ include/emp/meta/FunInfo.hpp
@@ -43,17 +43,40 @@ namespace emp {
   // Specialization for function objects with AT LEAST ONE parameter...
   template <typename CLASS_T, typename RETURN_T, typename PARAM1_T, typename... PARAM_Ts>
   struct FunInfo <RETURN_T(CLASS_T::*)(PARAM1_T, PARAM_Ts...) const> {
-  // private:
+  private:
   //   template <typename T> struct is_templated_converter : std::false_type{};
   //   template <typename T>
   //   struct is_templated_converter<emp::decoy_t<T, decltype(std::declval<T&>().template operator()<int>(0))>> : std::true_type{};
 
+    /// Helper function to lock an argument at a designated position in a function.
+    template <typename BOUND_T, typename... BEFORE_Ts, typename... AFTER_Ts>
+    static auto BindAt_impl(CLASS_T fun, BOUND_T && bound,
+                            TypePack<BEFORE_Ts...> before_tp, TypePack<AFTER_Ts...> after_tp) {
+      // If the function needs a reference for the parameter, send the supplied value through.
+      if constexpr (std::is_reference<PARAM1_T>()) {
+        return [fun, &bound](BEFORE_Ts &&... before_args, AFTER_Ts &&... after_args) {
+          return fun(std::forward<BEFORE_Ts>(before_args)...,
+                     std::forward<BOUND_T>(bound),
+                     std::forward<AFTER_Ts>(after_args)...);
+        };
+      }
+      // Otherwise, a copy is fine.
+      else {
+        return [fun, bound](BEFORE_Ts &&... before_args, AFTER_Ts &&... after_args) {
+          return fun(std::forward<BEFORE_Ts>(before_args)...,
+                     bound,
+                     std::forward<AFTER_Ts>(after_args)...);
+        };
+      }
+    }
+
+
   public:
     using return_t = RETURN_T;
-    using param_t = TypePack<PARAM1_T, PARAM_Ts...>;
+    using params_t = TypePack<PARAM1_T, PARAM_Ts...>;
 
     template <size_t ID>
-    using arg_t = typename param_t::template get<ID>;
+    using arg_t = typename params_t::template get<ID>;
 
     static constexpr size_t NumArgs() { return 1 + sizeof...(PARAM_Ts); }
 
@@ -97,27 +120,14 @@ namespace emp {
       };
     }
 
-    /// Lock in the first argument of a function.
-    template <typename T>
-    static auto BindFirst(CLASS_T fun, T && bound) {
-      // If the function needs a reference for the parameter, send the supplied value through.
-      if constexpr (std::is_reference<PARAM1_T>()) {
-        return [fun, &bound](PARAM_Ts &&... args) {
-          return fun(std::forward<T>(bound), std::forward<PARAM_Ts>(args)...);
-        };
-      }
-      // Otherwise, a copy is fine.
-      else {
-        return [fun, bound](PARAM_Ts &&... args) {
-          return fun(std::forward<T>(bound), std::forward<PARAM_Ts>(args)...);
-        };
-      }
-    }
 
-    // template <size_t... POS, typename... Ts>
-    // static auto BindAt(CLASS_T fun, Ts &&... bound) {
-    //   return [fun, bound](PARAM_Ts... args) { return fun(bound, args...); };
-    // }
+    /// Lock in a specified argument of a function.
+    template <size_t POS, typename T>
+    static auto BindAt(CLASS_T fun, T && bound) {
+      using before_pack = typename params_t::template shrink<POS>;
+      using after_pack = typename params_t::template popN<POS+1>;
+      return BindAt_impl(fun, std::forward<T>(bound), before_pack(), after_pack());
+    }
 
   };
 
@@ -126,7 +136,7 @@ namespace emp {
   struct FunInfo <RETURN_T(CLASS_T::*)() const>
   {
     using return_t = RETURN_T;
-    using param_t = TypePack<>;
+    using params_t = TypePack<>;
 
     static constexpr size_t NumArgs() { return 0; }
 
@@ -185,10 +195,16 @@ namespace emp {
     return FunInfo<FUN_T>::template ConvertParameterTypes<NEW_T>(fun, convert_fun);
   }
 
+  /// Lock in a specified argument of a function.
+  template <size_t POS, typename FUN_T, typename BOUND_T>
+  auto BindAt(FUN_T fun, BOUND_T && bound) {
+    return FunInfo<FUN_T>::template BindAt<POS>(fun, std::forward<BOUND_T>(bound));
+  }
+
   /// Lock in the first argument of a function.
-  template <typename FUN_T, typename T>
-  auto BindFirst(FUN_T fun, T && bound) {
-    return FunInfo<FUN_T>::BindFirst(fun, std::forward<T>(bound));
+  template <typename FUN_T, typename BOUND_T>
+  auto BindFirst(FUN_T fun, BOUND_T && bound) {
+    return FunInfo<FUN_T>::template BindAt<0>(fun, std::forward<BOUND_T>(bound));
   }
 
 }
