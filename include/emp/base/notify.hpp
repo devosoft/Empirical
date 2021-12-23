5e8d94e06f688c83446d4afb03553dca508ec1b1
Charles Ofria <charles.ofria@gmail.com>
2021-12-23 16:25:08 -0500

    Setup exceptions to actually come with extra data.

--- include/emp/base/notify.hpp
+++ include/emp/base/notify.hpp
@@ -30,6 +30,9 @@
  *  - Whenever possible, exceptions should be preferred.  They are more specific than warnings
  *    and can be responded to rather than automatically halting execution like errors.
  *  - Warnings should always detail what should be done differently to surpress that warning.
+ * 
+ *  DEVELOPER NOTES:
+ *  - Add a Verbose() notification type?
  *
  */
 
@@ -48,22 +51,27 @@
 
 namespace emp {
 namespace notify {
-  using response_t = bool(const std::string & /*id*/, const std::string & /*message*/);
-  using no_id_response_t = bool(const std::string & /*message*/);
+  using id_t = std::string;
+  using message_t = std::string;
+  using except_data_t = std::any;
+
+  using id_arg_t = const id_t &;
+  using message_arg_t = const message_t &;
+  using response_t = bool(id_arg_t, message_arg_t, except_data_t);
   using exit_fun_t = void(size_t);
 
   /// Information about an exception that has occurred.
   struct ExceptInfo {
-    std::string id = "__NONE__";  ///< Which exception was triggered?
-    std::string message = "";     ///< A detailed message of this exception.
-    std::any data;                ///< Extra data needed to resolve this exception.
+    id_t id = "__NONE__";     ///< Which exception was triggered?
+    message_t message = "";   ///< A detailed message of this exception.
+    except_data_t data;       ///< Extra data needed to resolve this exception.
   };
 
   enum class Type { MESSAGE=0, DEBUG, WARNING, ERROR, EXCEPTION, NUM_TYPES };
   static constexpr size_t num_types = static_cast<size_t>(Type::NUM_TYPES);
 
   /// Convert a type to a human-readable string.
-  static std::string TypeName(Type type) {
+  static id_t TypeID(Type type) {
     switch (type) {
       case Type::MESSAGE: return "Message";
       case Type::DEBUG: return "Debug";
@@ -78,6 +86,8 @@ namespace notify {
   class HandlerSet {
   private:
     using fun_t = std::function<response_t>;
+    using fun_no_data_t = std::function<bool(id_arg_t, message_arg_t)>;
+    using fun_msg_only_t = std::function<bool(message_arg_t)>;
     emp::vector<fun_t> handlers;
 
   public:
@@ -87,13 +97,13 @@ namespace notify {
     ~HandlerSet() { }
 
     /// Trigger all handlers associated with a given ID.
-    bool Trigger(const std::string & id, const std::string & message, std::any data) {
+    bool Trigger(id_arg_t id, message_arg_t message, except_data_t except_data) {
       // Run handlers from most recently added to oldest.
       for (auto it = handlers.rbegin();
             it != handlers.rend();
             ++it) {
         // Run until "true" result
-        bool result = (*it)(id, message);
+        bool result = (*it)(id, message, except_data);
         if (result) return true;           // Stop if any handler succeeded.
       }
 
@@ -101,12 +111,29 @@ namespace notify {
     }
 
     // Trigger without providing data.
-    bool Trigger(const std::string & id, const std::string & message)  {
+    bool Trigger(id_arg_t id, message_arg_t message)  {
       return Trigger(id, message, 0);
     }
 
     // Add a function to this set.
     HandlerSet & Add(fun_t in) { handlers.push_back(in); return *this; }
+
+    // Add a function with no data.
+    HandlerSet & Add(fun_no_data_t in) {
+      handlers.push_back(
+        [fun=in](id_arg_t id, message_arg_t msg, except_data_t){ return fun(id,msg); }
+      );
+      return *this;
+    }
+
+    // Add a function with only a single message
+    HandlerSet & Add(fun_msg_only_t in) {
+      handlers.push_back(
+        [fun=in](id_arg_t, message_arg_t msg, except_data_t){ return fun(msg); }
+      );
+      return *this;
+    }
+
 
     // Clear all handlers associated with a given id.
     HandlerSet & Clear() { handlers.resize(0); return *this; }
@@ -126,16 +153,16 @@ namespace notify {
   struct NotifyData {
     // For each exception name we will keep a vector of handlers, appended to in the order
     // that they arrive (most recent will be last)
-    std::unordered_map<std::string, HandlerSet> handler_map;
+    std::unordered_map<id_t, HandlerSet> handler_map;
     emp::vector<std::function<exit_fun_t>> exit_funs;        // Set of handlers to run on exit.
     emp::vector<ExceptInfo> except_queue;                    // Unresolved exceptions
 
-    HandlerSet & GetHandler(Type type) { return handler_map[TypeName(type)]; }
+    HandlerSet & GetHandler(Type type) { return handler_map[TypeID(type)]; }
 
     NotifyData() {
       // Setup the default handlers and exit rules.
       GetHandler(Type::MESSAGE).Add(
-        [](const std::string & /*id*/, const std::string & msg) {
+        [](id_arg_t, message_arg_t msg) {
           std::cout << msg << std::endl;
           return true;
         }
@@ -143,9 +170,9 @@ namespace notify {
 
       GetHandler(Type::DEBUG).Add(
 #ifdef NDEBUG
-        [](const std::string & /*id*/, const std::string & msg){ return true; }
+        [](id_arg_t, message_arg_t){ return true; }
 #else
-        [](const std::string & /*id*/, const std::string & msg) {
+        [](id_arg_t,  message_arg_t msg) {
           std::cout << "Debug: " << msg << std::endl;
           return true;
         }
@@ -153,21 +180,21 @@ namespace notify {
       );
 
       GetHandler(Type::WARNING).Add(
-        [](const std::string & /*id*/, const std::string & msg) {
+        [](id_arg_t,  message_arg_t msg) {
           std::cerr << "WARNING: " << msg << std::endl;
           return true;
         }
       );
 
       GetHandler(Type::ERROR).Add(
-        [](const std::string & /*id*/, const std::string & msg) {
+        [](id_arg_t,  message_arg_t msg) {
           std::cerr << "ERROR: " << msg << std::endl;
           return true;
         }
       );
 
       GetHandler(Type::EXCEPTION).Add(
-        [](const std::string & id, const std::string & msg) {
+        [](id_arg_t id,  message_arg_t msg) {
           std::cerr << "EXCEPTION (" << id << "): " << msg << std::endl;
           return false;
         }
@@ -184,7 +211,6 @@ namespace notify {
   auto & DebugHandlers() { return GetData().GetHandler(Type::DEBUG); }
   auto & WarningHandlers() { return GetData().GetHandler(Type::WARNING); }
   auto & ErrorHandlers() { return GetData().GetHandler(Type::ERROR); }
-  auto & ExceptionHandlers() { return GetData().GetHandler(Type::EXCEPTION); }
   auto & ExitHandlers() { return GetData().exit_funs; }
 
   /// Generic exit handler that calls all of the provided functions.
@@ -199,7 +225,7 @@ namespace notify {
   template <typename... Ts>
   static bool Notify(Type type, Ts... args) {
     NotifyData & data = GetData();
-    const std::string id = TypeName(type);
+    const std::string id = TypeID(type);
 
     // Setup the message in a string stream.
     std::stringstream ss;
@@ -235,18 +261,14 @@ namespace notify {
 
   /// Send out a notification of an Exception.
   template <typename... Ts>
-  static bool Exception(const std::string & id, Ts... args) { 
+  static bool Exception(id_arg_t id, message_arg_t message, except_data_t except_data) { 
     NotifyData & data = GetData();
 
-    // Setup the message in a string stream.
-    std::stringstream ss;
-    ((ss << std::forward<Ts>(args)), ...);
-
     // Retrieve the exception handlers that we have for this type of exception.
-    bool result = ExceptionHandlers().Trigger(id, ss.str());
+    bool result = data.handler_map[id].Trigger(id, message, except_data);
 
     // If unresolved, either give up or save the exception for later analysis.
-    if (!result) data.except_queue.push_back(ExceptInfo{id, ss.str()});
+    if (!result) data.except_queue.push_back(ExceptInfo{id, message, except_data});
 
     return result;
   }
@@ -255,7 +277,7 @@ namespace notify {
   static emp::vector<ExceptInfo> & GetExceptions() { return GetData().except_queue; }
 
   /// Retrieve the first unresolved exception with a given id.
-  static ExceptInfo GetException(const std::string & id) {
+  static ExceptInfo GetException(id_arg_t id) {
     for (ExceptInfo & x : GetData().except_queue) if (x.id == id) return x;
     return ExceptInfo{};
   }
@@ -264,7 +286,7 @@ namespace notify {
   static size_t CountExceptions() { return GetData().except_queue.size(); }
 
   /// Return a total count of how many unresolved exceptions have a given id.
-  static size_t CountExceptions(const std::string & id) {
+  static size_t CountExceptions(id_arg_t id) {
     size_t count = 0;
     for (ExceptInfo & x : GetData().except_queue) if (x.id == id) ++count;
     return count;
@@ -274,7 +296,7 @@ namespace notify {
   static bool HasExceptions() { return CountExceptions(); }
 
   /// Identify whether there are any unresolved exceptions with a given id.
-  static bool HasException(const std::string & id) {
+  static bool HasException(id_arg_t id) {
     for (ExceptInfo & x : GetData().except_queue) if (x.id == id) return true;
     return false;
   }
@@ -283,7 +305,7 @@ namespace notify {
   static void ClearExceptions() { GetData().except_queue.resize(0); }
 
   /// Remove first exception with a given id.
-  static void ClearException(const std::string & id) {
+  static void ClearException(id_arg_t id) {
     auto & except_queue = GetData().except_queue;
     for (size_t i = 0; i < except_queue.size(); ++i) {
       if (except_queue[i].id == id) {
