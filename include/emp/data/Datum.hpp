6e00da9b3ea2ade472417c0a36b91a56e32340f6
Charles Ofria <charles.ofria@gmail.com>
2021-12-31 17:06:20 -0500

    Added an inital Datum class to efficiently hold a string or double.

---
+++ include/emp/data/Datum.hpp
@@ -0,0 +1,80 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2021.
+ *
+ *  @file  Datum.hpp
+ *  @brief A single piece of data, either a value or a string.
+ *  @note Status: ALPHA
+ * 
+ *  DEVELOPER NOTES:
+ *  - For now, using unions, but this creates complications with non-trivial constructor/destructor
+ *    for strings, so could try shifting over.
+ */
+
+#ifndef EMP_DATUM_HPP
+#define EMP_DATUM_HPP
+
+#include <string>
+
+#include "../base/assert.hpp"
+
+namespace emp {
+
+  class Datum {
+  private:
+    union {
+      double num;
+      std::string str;
+    };
+    bool is_num = true;
+
+    void InitString() { new (&str) std::string; }
+    void InitString(const std::string & in) { new (&str) auto(in); }
+    void FreeString() { str.~basic_string(); }
+  public:
+    Datum() : num(0.0), is_num(true) { }
+    Datum(double in) : num(in), is_num(true) { }
+    Datum(const std::string & in) : is_num(false) { InitString(in); }
+    Datum(const Datum & in) {
+      is_num = in.is_num;
+      if (is_num) num = in.num;
+      else InitString(in.str);
+    }
+    ~Datum() { if (!is_num) FreeString(); }
+
+    double AsDouble() const {
+      if (is_num) return num;
+      return std::stod(str);
+    }
+
+    const std::string & AsString() const {
+      if (is_num) return std::to_string(num);
+      return str;
+    }
+
+    Datum & Set(double in) {
+      if (!is_num) FreeString();  // If this were previously a string, clean it up!
+      num = in;
+      return *this;
+    }
+
+    Datum & Set(const std::string & in) {
+      if (is_num) InitString(in);  // If this were previously a num, change to string.
+      else str = in;               // Already a string; just change its value.
+      return *this;
+    }
+
+    Datum & Set(const Datum & in) {
+      if (in.is_num) return Set(in.num);
+      else return Set(in.str);
+    }
+
+    Datum & operator=(double in) { return Set(in); }
+    Datum & operator=(const std::string & in) { return Set(in); }
+    Datum & operator=(const Datum & in) { return Set(in); }
+  };
+
+}
+
+#endif
