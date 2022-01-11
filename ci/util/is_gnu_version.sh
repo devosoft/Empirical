0fb5da8c01fe881b09d8a9b883d8b878ccaffe59
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-01-10 19:10:26 -0500

    Handle case where raw util unavailable

--- ci/util/is_gnu_version.sh
+++ ci/util/is_gnu_version.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
-# adapted from https://stackoverflow.com/a/57102498/
-if ! [[ "$("${1}" --version)" =~ "Free Software Foundation" ]];
+# adapted from https://stackoverflow.com/a/677212/17332200
+if command -v "${1}" &> /dev/null;
+then
+  echo "${1} unavailable"
+  exit 1
+  # adapted from https://stackoverflow.com/a/57102498/
+elif ! [[ "$("${1}" --version)" =~ "Free Software Foundation" ]];
 then
   echo "non-GNU ${1} detected"
   exit 1
