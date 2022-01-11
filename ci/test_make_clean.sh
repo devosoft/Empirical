b986f22a3521d3e1a5e085d8fec5fece1316e24d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-01-10 20:17:32 -0500

    Enforce availaiblity of sphinx-build

--- ci/test_make_clean.sh
+++ ci/test_make_clean.sh
@@ -4,6 +4,9 @@ set -e
 # enforce use of GNU version of coreutils
 . ./ci/util/enforce_gnu_utils.sh
 
+# enforce availability of dependencies
+. ./ci/util/enforce_dependency.sh sphinx-build
+
 SOURCE_HASH=$( find -path ./third-party -prune -false -o -type f | sort | xargs cat | sha1sum )
 
 make clean
