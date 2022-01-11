cf1c44de08b733e9bc61733db018e806dd61ab0e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-01-10 20:43:16 -0500

    Bugfix: don't fail on empty grep

--- ci/test_pragma_once.sh
+++ ci/test_pragma_once.sh
@@ -5,8 +5,7 @@ set -e
 . ./ci/util/enforce_gnu_utils.sh
 
 # adapted from https://www.linsoftware.com/how-to-check-for-git-merge-conflicts-in-travis-ci/
-
-found_pragma_onces=$(grep -EHlr --exclude=test_pragma_once.sh --exclude=getting-started.md --exclude-dir=third-party '#pragma once' .)
+found_pragma_onces=$(grep -EHlr --exclude=test_pragma_once.sh --exclude=getting-started.md --exclude-dir=third-party '#pragma once' . || :)
 
 if [ -z "$found_pragma_onces" ]
 then
