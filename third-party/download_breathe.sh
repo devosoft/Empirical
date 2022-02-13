92f7af80c3f20f8f55edf56da007e3124bf5e0ea
Emily Dolson <EmilyLDolson@gmail.com>
2022-02-13 01:11:24 -0500

    Update npm and doc toolchain versions

--- third-party/download_breathe.sh
+++ third-party/download_breathe.sh
@@ -1,8 +1,8 @@
 if [ -d breathe ]; then                                                         
         echo "breathe directory exists!"                                        
 else                                                                            
-        wget https://github.com/michaeljones/breathe/archive/v4.1.0.tar.gz
-        tar -xvf v4.1.0.tar.gz                                      
-        rm v4.1.0.tar.gz
+        wget https://github.com/michaeljones/breathe/archive/refs/tags/v4.32.0.tar.gz
+        tar -xvf v4.32.0.tar.gz                                      
+        rm v4.32.0.tar.gz
         mv breathe-* breathe                                            
 fi
