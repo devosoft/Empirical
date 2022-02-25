be8265022d36f6ce125db73b4991e12bea9e12ea
Charles Ofria <charles.ofria@gmail.com>
2022-02-24 21:25:32 -0500

    Added some examples of using Random::GetExponential()

--- examples/math/Random.cpp
+++ examples/math/Random.cpp
@@ -20,6 +20,8 @@ int main()
   std::cout << "Digits in random orders:" << std::endl;
 
   size_t num_samples = 10;
+
+  std::cout << "Permutations: " << std::endl;
   for (size_t s  = 0; s < num_samples; s++) {
     emp::vector<size_t> permut = emp::GetPermutation(random, 10);
     for (size_t i = 0; i < 10; i++) {
@@ -27,4 +29,17 @@ int main()
     }
     std::cout << std::endl;
   }
+
+  std::cout << "Exponentials: " << std::endl;
+  double p = 0.5;
+  std::cout << "p = " << p << std::endl;
+  for (size_t s  = 0; s < num_samples; s++) std::cout << random.GetExponential(p) << std::endl;
+
+  p = 0.1;
+  std::cout << "\np = " << p << std::endl;
+  for (size_t s  = 0; s < num_samples; s++) std::cout << random.GetExponential(p) << std::endl;
+
+  p = 0.9;
+  std::cout << "\np = " << p << std::endl;
+  for (size_t s  = 0; s < num_samples; s++) std::cout << random.GetExponential(p) << std::endl;
 }
