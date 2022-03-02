d2eee15fe5c99c41a686c7a6a01639fee3ddcb3a
Charles Ofria <charles.ofria@gmail.com>
2022-03-01 22:25:53 -0500

    Added tests for regular binomial distributions

--- examples/timing/Binomial.cpp
+++ examples/timing/Binomial.cpp
@@ -42,7 +42,7 @@ void TestGeometric(emp::Random & random, const double p, const size_t num_tests=
   result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
 
   std::cout << "random.GetGeometric(p) with p = " << p << "\n"
-            << "  time = " << result << " seconds.\n"
+            << "  time = " << emp::ANSI_Bold() << result << " seconds." << emp::ANSI_NoBold() << "\n"
             << "  average = " << (total / num_tests) << "\n"
             << std::endl;
 
@@ -92,7 +92,57 @@ void TestNegBinomial(
   result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
 
   std::cout << "N = " << N << " calls to random.GetGeometric(p) with p = " << p << "\n"
-            << "  time = " << result << " seconds.\n"
+            << "  time = " << emp::ANSI_Bold() << result << " seconds." << emp::ANSI_NoBold() << "\n"
+            << "  average = " << (total / num_tests) << "\n"
+            << std::endl;
+}
+
+void TestBinomial(
+  emp::Random & random,
+  const double p,
+  const size_t N,
+  const size_t num_tests=1000000)
+{
+  std::cout << emp::ANSI_MagentaBG()
+            << "---- Binomial Tests: p = " << p << " ; N = " << N << " ----"
+            << emp::ANSI_Reset()
+            << std::endl;
+
+  ////////- Pre-processed distribution
+  emp::Binomial dist(p, N);
+
+  std::clock_t start_time = std::clock();
+
+  double total = 0;
+  for (size_t i = 0; i < num_tests; i++) {
+    total += dist.PickRandom(random);
+  }
+
+  std::clock_t tot_time = std::clock() - start_time;
+  double result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
+
+  std::cout << "Binomial Distribution with p = " << p << " and N = " << N << "\n"
+            << "  time = " << emp::ANSI_Bold() << result << " seconds." << emp::ANSI_NoBold() << "\n"
+            << "  dist size = " << dist.GetSize() << "\n"
+            << "  average = " << (total / num_tests) << "\n"
+            << std::endl;
+
+  ////////- Random call (no pre-process)
+  start_time = std::clock();
+
+  total = 0;
+  for (size_t i = 0; i < num_tests; i++) {
+    size_t pos = 0;
+    while( (pos += random.GetGeometric(p)) < N ) {
+      total++;
+    }
+  }
+
+  tot_time = std::clock() - start_time;
+  result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
+
+  std::cout << "N = " << N << " calls to random.GetGeometric(p) with p = " << p << "\n"
+            << "  time = " << emp::ANSI_Bold() << result << " seconds." << emp::ANSI_NoBold() << "\n"
             << "  average = " << (total / num_tests) << "\n"
             << std::endl;
 }
@@ -115,4 +165,25 @@ int main()
   TestNegBinomial(random, 0.01, 10, num_tests);
   TestNegBinomial(random, 0.001, 10, num_tests);
   TestNegBinomial(random, 0.0001, 10, num_tests);
+
+  TestNegBinomial(random, 0.9, 100, num_tests);
+  TestNegBinomial(random, 0.5, 100, num_tests);
+  TestNegBinomial(random, 0.1, 100, num_tests);
+  TestNegBinomial(random, 0.01, 100, num_tests);
+  TestNegBinomial(random, 0.001, 100, num_tests);
+  TestNegBinomial(random, 0.0001, 100, num_tests);
+
+  TestBinomial(random, 0.9, 100, num_tests);
+  TestBinomial(random, 0.5, 100, num_tests);
+  TestBinomial(random, 0.1, 100, num_tests);
+  TestBinomial(random, 0.01, 100, num_tests);
+  TestBinomial(random, 0.001, 100, num_tests);
+  TestBinomial(random, 0.0001, 100, num_tests);
+
+  TestBinomial(random, 0.9, 1000, num_tests);
+  TestBinomial(random, 0.5, 1000, num_tests);
+  TestBinomial(random, 0.1, 1000, num_tests);
+  TestBinomial(random, 0.01, 1000, num_tests);
+  TestBinomial(random, 0.001, 1000, num_tests);
+  TestBinomial(random, 0.0001, 1000, num_tests);
 }
