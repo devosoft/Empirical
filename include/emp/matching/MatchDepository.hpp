02d53900f09caff20419c7f0d145a3b434e8e65a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-05-30 00:49:28 -0400

    fixup! Optimize downregulation cache adjustment

--- include/emp/matching/MatchDepository.hpp
+++ include/emp/matching/MatchDepository.hpp
@@ -53,7 +53,7 @@ private:
 
   // Cache of match results with regulation.
   robin_hood::unordered_flat_map< query_t, res_t> cache_regulated;
-  std::multimap< uid_t, query_t> cache_regulated_bwd;
+  std::unordered_multimap< uid_t, query_t> cache_regulated_bwd;
 
   robin_hood::unordered_flat_set<query_t> has_shifted;
 
@@ -149,6 +149,7 @@ private:
 
       for (const auto& query : has_shifted) {
 
+        if (cache_regulated.count(query) == 0) continue;
         auto& existing_response = cache_regulated.at(query);
         // for now, assume this only iterates zero times or one time
         for (const auto& uid_ : existing_response) {
@@ -172,8 +173,6 @@ private:
             }
             existing_response[0] = uid;
             cache_regulated_bwd.insert({uid, query});
-
-            // TODO cache_regulated_bwd
           }
 
           if (cache_regulated[query] == cache_raw[query]) has_shifted.erase(
