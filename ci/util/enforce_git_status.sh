b26716936d2bcc9a81d3ed215c249078b9d28a51
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-06-02 10:20:52 -0400

    Don't tidy if gitignored files are present

--- ci/util/enforce_git_status.sh
+++ ci/util/enforce_git_status.sh
@@ -15,3 +15,12 @@ then
   ./ci/util/print_uncommitted_changes_warning.sh
   exit 1
 fi
+
+# refuse to continue if gitignored files are present
+# adapted from https://stackoverflow.com/a/467053
+if ! [[ -z $(git check-ignore $(\
+  find -path ./third-party -prune -false -o -type f)) ]];
+then
+  ./ci/util/print_gitignored_files_warning.sh
+  exit 1
+fi
