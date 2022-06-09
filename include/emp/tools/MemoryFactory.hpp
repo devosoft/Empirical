909dfd5cb8929c63e9298fc295eb3ba9303eeea1
Charles Ofria <charles.ofria@gmail.com>
2022-06-09 17:37:20 -0400

    Setup MemoryFactory to handle dynamic pool sizes.

--- include/emp/tools/MemoryFactory.hpp
+++ include/emp/tools/MemoryFactory.hpp
@@ -15,6 +15,7 @@
 #include "../base/array.hpp"
 #include "../base/assert.hpp"
 #include "../base/error.hpp"
+#include "../base/notify.hpp"
 #include "../base/Ptr.hpp"
 #include "../base/vector.hpp"
 #include "../polyfill/span.hpp"
@@ -24,6 +25,8 @@ namespace emp {
   namespace internal {
     template <typename T, size_t MEM_COUNT, size_t POOL_COUNT>
     struct MemFactory_StaticData {
+      static constexpr const bool is_resizable = false;
+
       // Config
       static constexpr const size_t mem_count = MEM_COUNT;
       static constexpr const size_t pool_count = POOL_COUNT;
@@ -37,6 +40,7 @@ namespace emp {
 
       // Interface
       emp::Ptr<T> PoolPtr() const { return &pool; }
+      emp::Ptr<T> EndPoolPtr() const { return &((&pool)[1]); }
       void Initialize(size_t _mem_count, size_t _pool_count) {
         emp_error("Cannot re-initialize a static MemoryFactory.");
       }
@@ -45,11 +49,13 @@ namespace emp {
 
     template <typename T>
     struct MemFactory_DynamicData {
+      static constexpr const bool is_resizable = true;
+
       // Config
       size_t mem_count = 0;
       size_t pool_count = 0;
-      size_t chunk_size = 0;
       size_t pool_size = 0;
+      size_t chunk_size = 0;
 
       // Data
       emp::Ptr<T> pool;
@@ -57,15 +63,19 @@ namespace emp {
 
       // Interface
       emp::Ptr<T> PoolPtr() const { return pool; }
-      void Initialize(size_t _mem_count, size_t _pool_count) {
-        emp_assert(mem_count == 0, "Cannot (currently) re-initialize a memory factory.");
-        mem_count = _mem_count;
+      emp::Ptr<T> EndPoolPtr() const { return pool + pool_size; }
+      void InitPool(size_t _pool_count) {
         pool_count = _pool_count;
-        chunk_size = sizeof(T) * mem_count;
         pool_size = chunk_size * pool_count;
         pool.NewArray(pool_size);
         free_ids.resize(pool_count);
-        std::iota(free_ids.begin(), free_ids.end(), 0);
+        std::iota(free_ids.begin(), free_ids.end(), 0);        
+      }
+      void Initialize(size_t _mem_count, size_t _pool_count) {
+        emp_assert(mem_count == 0, "Cannot (currently) re-initialize a memory factory.");
+        mem_count = _mem_count;
+        chunk_size = sizeof(T) * mem_count;
+        InitPool(_pool_count);
       }
       auto FreeBegin() const { return free_ids.begin(); }
     };
@@ -76,8 +86,17 @@ namespace emp {
       DATA_T data;
       size_t free_count = data.pool_count;  // Start all positions free.
 
+      struct RetiredMem {
+        emp::Ptr<T> begin_ptr; // Where are these memory chunks?
+        emp::Ptr<T> end_ptr;   // How far do this memory chunks extend?
+        size_t reserve_count;  // How many memory chunks are still outstanding?
+
+        bool Included(emp::Ptr<T> in) { return in >= begin_ptr && in < end_ptr; }
+      };
+      emp::vector<RetiredMem> retired_mem;  // Track now-unused memory to free it properly as needed.
+
       size_t ToID(emp::Ptr<T> in) {
-        return (in.Raw() - data.PoolPtr().Raw()) / data.chunk_size;
+        return (in.Raw() - data.PoolPtr().Raw()) / data.mem_count;
       }
     public:
       MemFactory_impl() {
@@ -97,13 +116,24 @@ namespace emp {
       emp::Ptr<T> GetAtID(size_t id) { return &data.pool[id]; }
       std::span<T> GetSpanAtID(size_t id) { return std::span<T>(GetAtID(), data.mem_count); }
 
+      bool IsCurrent(emp::Ptr<T> in) {
+        if (in < data.PoolPtr()) return false;
+        return IsValidID(ToID(in));
+      }
+
       void Initialize(size_t  _mem_count, size_t _pool_count=100) {
         data.Initialize(_mem_count, _pool_count);
         free_count = data.pool_count;
       }
 
       emp::Ptr<T> Reserve() {
-        emp_assert(free_count > 0);
+        if constexpr (DATA_T::is_resizable) {
+          if (free_count == 0) { // If we are out of memory, retire current back and request more!
+            // Backup the old memory and initialize (bigger) replacement.
+            retired_mem.push_back(RetiredMem{data.PoolPtr(), data.EndPoolPtr(), data.pool_count});
+            data.InitPool(data.pool_count * 2);
+          }
+        }
         const size_t id = data.free_ids[--free_count];
         return &data.pool[id];
       }
@@ -117,6 +147,23 @@ namespace emp {
       }
 
       void Release(emp::Ptr<T> in) {
+        // If data is resizable, we need to worry about retired memory.
+        if constexpr (data.is_resizable) {
+          if (!IsCurrent(in)) {
+            // Determine which retired pool we are in.
+            size_t pool_id;
+            while (pool_id < retired_mem.size() && !retired_mem[pool_id].Included(in)) ++pool_id;
+            if (pool_id == retired_mem.size()) {
+              notify::Exception("MemoryFactory::Release", "Trying to release unknown memory.", in);
+            }
+            // Release this memory and check if this retired pool is now fully released.
+            if (--retired_mem[pool_id].reserve_count == 0) {
+              retired_mem.erase(retired_mem.begin() + pool_id);
+            }
+            return;
+          }
+        }
+        
         const size_t id = ToID(in);
         emp_assert(!IsFreeID(id), "Trying to release ID that is already free.", id);
         emp_assert(IsValidID(id), "Trying to release invalid ID", id);
@@ -133,119 +180,6 @@ namespace emp {
   using MemoryFactory =
     internal::MemFactory_impl<T, internal::MemFactory_DynamicData<T>>;
 
-
-  // template <typename T, size_t MEM_COUNT, size_t POOL_COUNT>
-  // class MemoryFactory {
-  // private:
-  //   static constexpr const size_t chunk_size = sizeof(T) * MEM_COUNT;
-  //   static constexpr const size_t pool_size = chunk_size * POOL_COUNT;
-  //   using chunk_t = emp::array<T, MEM_COUNT>;
-  //   emp::array<chunk_t, POOL_COUNT> pool;
-  //   emp::array<size_t, POOL_COUNT> free_ids;
-  //   size_t free_count = POOL_COUNT;
-
-  //   size_t ToID(chunk_t in) {
-  //     return (in.data - pool[0].data) / chunk_size;
-  //   }
-
-  // public:
-  //   MemoryFactory() {
-  //     std::iota(free_ids.begin(), free_ids.end(), 0);
-  //   }
-  //   MemoryFactory(MemoryFactory &) = delete; // Do not copy memory pools!
-
-  //   size_t GetChunkSize() const constexpr { return chunk_size; }
-  //   size_t GetPoolSize() const constexpr { return pool_size; }
-
-  //   bool IsFreeID(size_t id) const { return std::any_of(free_ids.begin(), free_ids.begin()+free_count, id); }
-  //   bool IsValidID(size_t id) const constexpr { return id < POOL_COUNT; }
-  //   emp::Ptr<T> GetAtID(size_t id) { return &pool[id]; }
-  //   std::span<T> GetSpanAtID(size_t id) { return pool[id]; }
-
-  //   emp::Ptr<T> Reserve() {
-  //     emp_assert(free_count > 0);
-  //     const size_t id = free_ids[--free_count];
-  //     return &pool[id];
-  //   }
-  //   emp::Ptr<T> Reserve(size_t id) {
-  //     auto it = find(free_ids.begin(), free_ids.end(), id); // Find ID in free list.
-  //     emp_assert(it != free_ids.end());                     // Make sure it's there!
-  //     *it = free_ids.back();                                // Move last ID into its position.
-  //     free_count--;                                         // Eliminate old final ID (now moved).
-  //     return &pool[id];                                     // Return reserved memory.
-  //   }
-
-  //   void Release(emp::Ptr<T> in) {
-  //     const size_t id = ToID(in);
-  //     emp_assert(!IsFreeID(id), "Trying to release ID that is already free.", id);
-  //     emp_assert(IsValidID(id), "Trying to release invalid ID", id);
-  //     free_ids[free_count] = id;
-  //     ++free_count;
-  //   }
-  // };
-
-  // // Where memory requirements are NOT known at compile time.
-  // template <typename T>
-  // class MemoryFactory {
-  // private:
-  //   size_t mem_count;
-  //   size_t pool_count;
-  //   size_t chunk_size;
-  //   size_t pool_size;
-
-  //   emp::Ptr<T> pool;
-  //   emp::vector<size_t> free_ids;
-
-  //   size_t ToID(emp::Ptr<T> in) {
-  //     return (in.Raw() - pool.Raw()) / chunk_size;
-  //   }
-
-  // public:
-  //   MemoryFactory(size_t _mem_count, size_t _pool_count)
-  //   : mem_count(_mem_count) 
-  //   , pool_count(_pool_count);
-  //   , chunk_size(sizeof(T) * mem_count);
-  //   , pool_size(chunk_size * pool_count);
-  //   {
-  //     pool.NewArray(pool_size);
-  //     free_ids.resize(pool_count);
-  //     std::iota(free_ids.begin(), free_ids.end(), 0);
-  //   }
-  //   MemoryFactory(MemoryFactory &) = delete; // Do not copy memory pools!
-
-  //   size_t GetChunkSize() const { return chunk_size; }
-  //   size_t GetPoolSize() const { return pool_size; }
-
-  //   bool IsFreeID(size_t id) const {
-  //     return std::any_of(free_ids.begin(), free_ids.end(), id);
-  //   }
-  //   bool IsValidID(size_t id) const constexpr { return id < pool_count; }
-  //   emp::Ptr<T> GetAtID(size_t id) { return pool[id]; }
-  //   std::span<T> GetSpanAtID(size_t id) { return std::span(&pool[id],mem_count); }
-
-  //   emp::Ptr<T> Reserve() {
-  //     emp_assert(free_count > 0);
-  //     const size_t id = free_ids.back();
-  //     free_ids.resize(free_ids.size()-1);
-  //     return &pool[id*chunk_size];
-  //   }
-  //   emp::Ptr<T> Reserve(size_t id) {
-  //     auto it = find(free_ids.begin(), free_ids.end(), id); // Find ID in free list.
-  //     emp_assert(it != free_ids.end());                     // Make sure it's there!
-  //     *it = free_ids.back();                                // Move last ID into its position.
-  //     free_ids.resize(free_ids.size()-1);                   // Eliminate old final ID (now moved).
-  //     return &pool[id*chunk_size];
-  //   }
-
-  //   void Release(emp::Ptr<T> in) {
-  //     const size_t id = ToID(in);
-  //     emp_assert(!IsFreeID(id), "Trying to release ID that is already free.", id);
-  //     emp_assert(IsValidID(id), "Trying to release invalid ID", id);
-  //     free_ids[free_count] = id;
-  //     ++free_count;
-  //   }
-  // };
-  
 }
 
 #endif
