45b41baca23bd4e00b85d2f15cc0fce5a2acecea
Charles Ofria <charles.ofria@gmail.com>
2022-06-10 16:34:50 -0400

    Bug fixes to MemoryFactory, plus now construct and destruct managed memory.

--- include/emp/tools/MemoryFactory.hpp
+++ include/emp/tools/MemoryFactory.hpp
@@ -12,6 +12,8 @@
 #ifndef EMP_TOOLS_MEMORY_FACTORY_HPP_INCLUDE
 #define EMP_TOOLS_MEMORY_FACTORY_HPP_INCLUDE
 
+#include <type_traits>
+
 #include "../base/array.hpp"
 #include "../base/assert.hpp"
 #include "../base/error.hpp"
@@ -28,11 +30,11 @@ namespace emp {
       static constexpr const bool is_resizable = false;
 
       // Config
-      static constexpr const size_t mem_count = MEM_COUNT;
-      static constexpr const size_t pool_count = POOL_COUNT;
-      static constexpr const size_t total_count = MEM_COUNT * POOL_COUNT;
-      static constexpr const size_t chunk_size = sizeof(T) * MEM_COUNT;
-      static constexpr const size_t pool_size = chunk_size * POOL_COUNT;
+      static constexpr const size_t mem_count = MEM_COUNT;                // # of element per reserve.
+      static constexpr const size_t pool_count = POOL_COUNT;              // # of reserves allows.
+      static constexpr const size_t total_count = MEM_COUNT * POOL_COUNT; // Total # of elements.
+      static constexpr const size_t chunk_size = sizeof(T) * MEM_COUNT;   // Byte count per reserve.
+      static constexpr const size_t pool_size = chunk_size * POOL_COUNT;  // Total byte count.
 
       // Data
       emp::array<T, total_count> pool;
@@ -83,6 +85,8 @@ namespace emp {
     template <typename T, typename DATA_T>
     class MemFactory_impl {
     protected:
+      static constexpr const bool is_trivial = std::is_trivial<T>();
+
       DATA_T data;
       size_t free_count = data.pool_count;  // Start all positions free.
 
@@ -100,7 +104,7 @@ namespace emp {
       }
     public:
       MemFactory_impl() {
-        for (size_t i = 0; i < data.pool_size; ++i) data.pool[i] = i;
+        for (size_t i = 0; i < data.pool_size; ++i) data.free_ids[i] = i;
       }
       MemFactory_impl(size_t _mem_count, size_t _pool_count=100) {
         Initialize(_mem_count, _pool_count);
@@ -113,7 +117,7 @@ namespace emp {
 
       bool IsFreeID(size_t id) const { return std::any_of(data.FreeBegin(), data.FreeBegin()+free_count, id); }
       bool IsValidID(size_t id) const { return id < data.pool_count; }
-      emp::Ptr<T> GetAtID(size_t id) { return &data.pool[id]; }
+      emp::Ptr<T> GetAtID(size_t id) { return &data.pool[id*data.mem_count]; }
       std::span<T> GetSpanAtID(size_t id) { return std::span<T>(GetAtID(), data.mem_count); }
 
       bool IsCurrent(emp::Ptr<T> in) {
@@ -126,7 +130,8 @@ namespace emp {
         free_count = data.pool_count;
       }
 
-      emp::Ptr<T> Reserve() {
+      template <typename... ARGS>
+      emp::Ptr<T> Reserve(ARGS &&... args) {
         if constexpr (DATA_T::is_resizable) {
           if (free_count == 0) { // If we are out of memory, retire current back and request more!
             // Backup the old memory and initialize (bigger) replacement.
@@ -135,18 +140,41 @@ namespace emp {
           }
         }
         const size_t id = data.free_ids[--free_count];
-        return &data.pool[id];
+        const size_t id_pos = id * data.mem_count;
+
+        // Run the constructor if we need to.
+        if constexpr (!is_trivial || sizeof...(ARGS) > 0) {
+          for (size_t i = 0; i < data.mem_count; ++i) {
+            new (&data.pool[id_pos+i]) T(std::forward<ARGS>(args)...);
+          }
+        }
+        return &data.pool[id_pos];
       }
-      emp::Ptr<T> Reserve(size_t id) {
+
+      template <typename... ARGS>
+      emp::Ptr<T> ReserveID(size_t id, ARGS &&... args) {
         // Find ID in free list.
         auto it = find(data.free_ids.begin(), data.free_ids.begin()+free_count, id);
         emp_assert(it != data.free_ids.begin()+free_count);   // Make sure it's there!
         *it = data.free_ids.back();                           // Move last ID into its position.
         free_count--;                                         // Eliminate old final ID (now moved).
-        return &data.pool[id];                                // Return reserved memory.
+        
+        const size_t id_pos = id * data.mem_size;
+        if constexpr (!is_trivial || sizeof...(ARGS) > 0) {
+          for (size_t i = 0; i < data.mem_count; ++i) {
+            new (&data.pool[id_pos+i]) T(std::forward<ARGS>(args)...);
+          }
+        }
+
+        return &data.pool[id_pos];                                // Return reserved memory.
       }
 
       void Release(emp::Ptr<T> in) {
+        // Run the destructor if we need to.
+        if constexpr (!is_trivial) {
+          std::destroy(in.Raw(), (in+data.mem_count).Raw());
+        }
+
         // If data is resizable, we need to worry about retired memory.
         if constexpr (data.is_resizable) {
           if (!IsCurrent(in)) {
@@ -158,6 +186,7 @@ namespace emp {
             }
             // Release this memory and check if this retired pool is now fully released.
             if (--retired_mem[pool_id].reserve_count == 0) {
+              retired_mem[pool_id].begin_ptr.DeleteArray();
               retired_mem.erase(retired_mem.begin() + pool_id);
             }
             return;
