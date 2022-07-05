9a45e4f665236449d2994dffe55220d4880b6a2d
Charles Ofria <charles.ofria@gmail.com>
2022-07-04 23:54:38 -0400

    Lots of bug fixes; should compile and run now.

--- include/emp/datastructs/ra_map.hpp
+++ include/emp/datastructs/ra_map.hpp
@@ -31,14 +31,15 @@ namespace emp {
 
   template <typename KEY_T,
             typename T,
-            typename Hash = std::hash<Key>,
-            typename KeyEqual = std::equal_to<Key>,
-            typename Allocator = std::allocator< std::pair<const Key, T> >
+            typename Hash = std::hash<KEY_T>,
+            typename KeyEqual = std::equal_to<KEY_T>,
+            typename Allocator = std::allocator< std::pair<const KEY_T, T> >
+           >
   class ra_map {
   public:
     using key_type = KEY_T;
     using mapped_type = T;
-    using value_type = std::pair<const Key, T>;
+    using value_type = std::pair<const KEY_T, T>;
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
     using hasher = Hash;
@@ -46,19 +47,20 @@ namespace emp {
     using allocator_type = Allocator;
     using reference = value_type&;
     using const_reference = const value_type&;
-    using pointer = std::allocator_traits<Allocator>::pointer;
-    using const_pointer = std::allocator_traits<Allocator>::const_pointer;
+    using pointer = typename std::allocator_traits<Allocator>::pointer;
+    using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
 
   private:
     emp::unordered_map<KEY_T,size_t,Hash,KeyEqual> id_map;  ///< Map to find keys in vector.
     emp::vector<value_type> vals;                           ///< Vector of all values.
 
+    using this_t = ra_map<KEY_T,T,Hash,KeyEqual,Allocator>;
   public:
     ra_map() = default;
     ra_map(const ra_map &) = default;
     ra_map(ra_map &&) = default;
-    ra_map<T> & operator=(const ra_map &) = default;
-    ra_map<T> & operator=(ra_map &&) = default;
+    this_t & operator=(const ra_map &) = default;
+    this_t & operator=(ra_map &&) = default;
 
     // -- Iterators --
     auto begin() { return vals.begin(); }
@@ -101,61 +103,62 @@ namespace emp {
       vals.emplace_back(std::forward<Ts>(args)...);
       auto old_pos_it = id_map.find(vals.back().first);
       if (old_pos_it != id_map.end()) {
-        vals.resize(vals.size()-1); // Destroy newly created instance.
-        return old_pos_it->second;  // Return old position in map.
+        vals.resize(vals.size()-1);     // Destroy newly created instance.
+        return old_pos_it->second;      // Return old position in map.
       }
-      id_map[v.first] = pos;        // Save new position for later lookup.
-      return new_pos;               // And return it.
+      id_map[vals.back().first] = new_pos;  // Save new position for later lookup.
+      return new_pos;                   // And return it.
     }
 
     /// Erase a specific value from the container.
-    bool erase(const T & v) {
-      if (!count(v)) return false;   // Not in map.
+    bool erase(const KEY_T & key) {
+      if (!count(key)) return false;   // Not in map.
 
-      // Find out where v is in id_map and clear it.
-      const size_t pos = id_map[v];
-      id_map.erase(v);
+      // Find out where key is in id_map and clear it.
+      const size_t pos = id_map[key];
+      id_map.erase(key);
 
       // Move the former last value to the now-empty spot.
       const size_t last_pos = vals.size() - 1;
       if (pos != last_pos) {
-        vals[pos] = vals[last_pos];
-        id_map[vals[pos]] = pos;
+        const_cast<KEY_T&>(vals[pos].first) = vals[last_pos].first;
+        vals[pos].second = vals[last_pos].second;
+        id_map[vals[pos].first] = pos;
       }
       vals.resize(last_pos);
       return true;
     }
 
     
-    size_t count(const T & v) const { return id_map.count(v); } /// Is value included? (0 or 1).
+    size_t count(const KEY_T & key) const { return id_map.count(key); } /// Is value included? (0 or 1).
 
     /// Index into the ra_map by key.
-    T & operator[](key_t key) {
-      size_t key_it = id_map.find(key);
+    T & operator[](key_type key) {
+      auto key_it = id_map.find(key);
       if (key_it == id_map.end()) {
         return NewEntry(key);
       }
-      return vals[key_it->second];
+      return vals[key_it->second].second;
     }
 
     // --- Empirical only commands ---
 
-    T & NewEntry(key_t key) {
-      emp_assert(id_map.find(key) != id_map.end(), "ra_map::NewEntry must be an unused key!", key);
+    T & NewEntry(key_type key) {
+      emp_assert(id_map.find(key) == id_map.end(), "ra_map::NewEntry must be an unused key!", key);
       const size_t pos = vals.size();
-      id_map[v.first] = pos;
+      id_map[key] = pos;
       vals.emplace_back();
       return vals.back().second;
     }
 
-    bool Has(key_t key) const { return id_map.find(key) != id_map.end(); }
+    bool Has(key_type key) const { return id_map.find(key) != id_map.end(); }
 
-    size_t GetID(key_t key) {
+    size_t GetID(key_type key) {
       auto key_it = id_map.find(key);
       return (key_it == id_map.end()) ? emp::MAX_SIZE_T : key_it->second;
     }
 
-    key_t & KeyAtID(size_t id) { return vals[id]->first; }
+    key_type & KeyAtID(size_t id) { return vals[id]->first; }
 
     T & AtID(size_t id) { return vals[id]->second; }
     const T & AtID(size_t id) const { return vals[id]->second; }
