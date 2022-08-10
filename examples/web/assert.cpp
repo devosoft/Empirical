748a25b3954066beab5fdf4717728c2371ff875d
Charles Ofria <charles.ofria@gmail.com>
2022-08-10 14:24:06 -0400

    Cleaned up unused variable in non-debug form of assert example.

--- examples/web/assert.cpp
+++ examples/web/assert.cpp
@@ -1,7 +1,7 @@
 /**
  *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
- *  @date 2020
+ *  @date 2020-2022.
  *
  *  @file assert.cpp
  */
@@ -11,7 +11,7 @@
 
 int main() {
 
-  int x{ 42 };
+  [[maybe_unused]]] int x{ 42 };
 
   emp_assert(x > 1, "This assert passes in debug mode!", x);
 
