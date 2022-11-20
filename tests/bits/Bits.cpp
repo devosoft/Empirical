369eda634ed9520f6c4d564630dbc120662242a7
Charles Ofria <charles.ofria@gmail.com>
2022-11-19 23:10:36 -0500

    Added macros to test whole ranges of Bits object sizes

--- tests/bits/Bits.cpp
+++ tests/bits/Bits.cpp
@@ -21,6 +21,7 @@
 #include "emp/base/vector.hpp"
 #include "emp/bits/Bits.hpp"
 #include "emp/math/Random.hpp"
+#include "emp/meta/macros.hpp"
 
 #define BITS_TEST(TYPE, CONSTRUCT, ...) { TYPE CONSTRUCT; __VA_ARGS__ }
 
@@ -31,6 +32,9 @@
   BITS_TEST(emp::StaticBitValue<BASE_SIZE>, CONSTRUCT, __VA_ARGS__)       \
   BITS_TEST(emp::BitArray<BASE_SIZE>, CONSTRUCT, __VA_ARGS__)             \
   BITS_TEST(emp::BitSet<BASE_SIZE>, CONSTRUCT, __VA_ARGS__)
+
+#define BITS_TEST_ALL_ZEROS(SIZE) BITS_TEST_ALL(SIZE, bits(SIZE, false), TestBasics( bits, SIZE, "Zeros");)
+#define BITS_TEST_ALL_ONES(SIZE) BITS_TEST_ALL(SIZE, bits(SIZE, true), TestBasics( bits, SIZE, "Ones");)
 
 template<typename T>
 void TestBasics(const T & bits, size_t _size, std::string vals="Any") {
@@ -49,22 +53,26 @@ TEST_CASE("1: Test Bits Constructors", "[bits]"){
   emp::BitArray<50>        ba0;    TestBasics( ba0, 50, "Zeros" );
   emp::BitSet<50>          bs0;    TestBasics( bs0, 50, "Zeros" );
 
-  // Create a size 50 bits objects, default to all zeros.
-  BITS_TEST_ALL(50,   bits(50),         TestBasics( bits, 50, "Zeros");)
+  // Create size 50 bits objects, default to all zeros.
+  BITS_TEST_ALL(50, bits(50), TestBasics( bits, 50, "Zeros");)
 
-  // Create a size 5000 bits objects, default to all ones.
+  // Create larger (size 5000) bits objects, default to all ones.
   BITS_TEST_ALL(5000, bits(5000, true), TestBasics( bits, 5000, "Ones");)
 
+  // Create huge (size 100,000) bits objects.
+  BITS_TEST_ALL(100000, bits(100000), TestBasics( bits, 100000, "Ones");)
 
+  // Try a full range of BitVector sizes, from 1 to 200.
+  EMP_LAYOUT( BITS_TEST_ALL_ZEROS, , 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50 )
+  EMP_LAYOUT( BITS_TEST_ALL_ZEROS, , 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 )
+  EMP_LAYOUT( BITS_TEST_ALL_ZEROS, , 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150 )
+  EMP_LAYOUT( BITS_TEST_ALL_ZEROS, , 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 100 )
 
-  // Try a range of BitVector sizes, from 0 to 200.
-  for (size_t bv_size = 0; bv_size <= 200; bv_size++) {
-    emp::BitVector bv3(bv_size);
-    CHECK( bv3.GetSize() == bv_size );
-    CHECK( bv3.CountOnes() == 0 );
-    for (size_t i = 0; i < bv_size; i++) bv3[i] = true;
-    CHECK( bv3.CountOnes() == bv_size );
-  }
+  EMP_LAYOUT( BITS_TEST_ALL_ONES, , 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50 )
+  EMP_LAYOUT( BITS_TEST_ALL_ONES, , 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 )
+  EMP_LAYOUT( BITS_TEST_ALL_ONES, , 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150 )
+  EMP_LAYOUT( BITS_TEST_ALL_ONES, , 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 100 )
+
 
   // Build a relatively large BitVector.
   emp::BitVector bv4(1000000);
