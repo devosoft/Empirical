2dd19f40e5fe1e1cf92f2e22c950f90453c97eaa
Charles Ofria <charles.ofria@gmail.com>
2022-12-31 11:56:22 -0500

    More development of EmphaticEncoding.

--- include/emp/text/EmphaticEncoding.hpp
+++ include/emp/text/EmphaticEncoding.hpp
@@ -20,6 +20,7 @@
 #include "../base/array.hpp"
 #include "../base/notify.hpp"
 #include "../compiler/Lexer.hpp"
+#include "../tools/string_utils.hpp"
 
 #include "Text.hpp"
 
@@ -27,11 +28,13 @@ namespace emp {
 
   class EmphaticEncoding : public emp::TextEncoding {
   private:
-    emp::array<std::string, 128> tag_map;
+    emp::array<std::string, 128> tag_map;     // Maps tag characters to style names
+    std::map<std::string, > style_map;    // Maps styles
     emp::array<std::string, 128> escape_map;
 
     emp::Lexer lexer;
-    enum EmphToken {
+
+    enum class EmphToken {
       text = 255,
       escape = 254,
       tag = 253,
@@ -43,7 +46,8 @@ namespace emp {
       plain = 247,
       exe = 246,
       eval = 245,
-      error = 244 };
+      EOL = 244,
+      error = 243 };
 
     std::set<std::string> active_styles; // Styles to use on appended text.
 
@@ -61,11 +65,12 @@ namespace emp {
       tag_map['-'] = "bullet";
       tag_map['+'] = "ordered";
       tag_map['>'] = "indent";
+      tag_map['"'] = "blockquote";
+      tag_map['|'] = "continue";  // Keep previous lines setup (for set of full-line tags)
 
-      // AVAILABLE TAGS: @&;:'",?()]\
+      // AVAILABLE TAGS: @&;:',?()]\
 
       // Start of more complex tags...
-      // tag_map['|'] = "continue";  // Keep previous lines setup (for set of full-line tags)
       // tag_map['%'] = "comment";   // `% Should be removed by the lexer.
       // tag_map['['] = "link";      // `[Include a link name here](http://and.its.url.here)
       // tag_map['<'] = "URL";       // `<http://just.a.url.here>
@@ -83,33 +88,35 @@ namespace emp {
       // Now that all of the tags are loaded, put them into the lexer.
       [[maybe_unused]] int token_id;
       token_id = lexer.AddToken("text","[^`\\]+");    // Non-tag or special characters.
-      emp_assert(token_id == EmphToken::text);
+      emp_assert(token_id == (int) EmphToken::text);
       token_id = lexer.AddToken("escape", "\\\\[\\\\` \n\t]");
-      emp_assert(token_id == EmphToken::escape);
-      token_id = lexer.AddToken("tag", "`[*`/~.\\^_#+>|-]");   // Tags
-      emp_assert(token_id == EmphToken::tag);
+      emp_assert(token_id == (int) EmphToken::escape);
+      token_id = lexer.AddToken("tag", "`[*`/~.\\^_#+>\"|-]");   // Tags
+      emp_assert(token_id == (int) EmphToken::tag);
 
       // Specialty tags...
       token_id = lexer.AddToken("code", "`[a-zA-Z0-9 \n\t]"); // Single ` okay if followed by alphanumeric or whitespace.
-      emp_assert(token_id == EmphToken::code);
+      emp_assert(token_id == (int) EmphToken::code);
       token_id = lexer.AddToken("comment", "`%.*");        // Clear whole line.
-      emp_assert(token_id == EmphToken::comment);
+      emp_assert(token_id == (int) EmphToken::comment);
       token_id = lexer.AddToken("link", "`\\[[^\\]\n]*\\]\\([^)\n]*\\)" );
-      emp_assert(token_id == EmphToken::link);
+      emp_assert(token_id == (int) EmphToken::link);
       token_id = lexer.AddToken("url", "`<[^>\n]*>" );
-      emp_assert(token_id == EmphToken::url);
+      emp_assert(token_id == (int) EmphToken::url);
       token_id = lexer.AddToken("image", "`!\\[[^\\]\n]*\\](\\([^)\n]*\\))?");
-      emp_assert(token_id == EmphToken::image);
+      emp_assert(token_id == (int) EmphToken::image);
       token_id = lexer.AddToken("plain", "`=.*");
-      emp_assert(token_id == EmphToken::plain);
+      emp_assert(token_id == (int) EmphToken::plain);
       token_id = lexer.AddToken("exe", "`{[^`]*`}");
-      emp_assert(token_id == EmphToken::exe);
+      emp_assert(token_id == (int) EmphToken::exe);
       token_id = lexer.AddToken("eval", "`$[a-zA-Z0-1_]*$");
-      emp_assert(token_id == EmphToken::eval);
+      emp_assert(token_id == (int) EmphToken::eval);
+      token_id = lexer.AddToken("EOL", "\n");
+      emp_assert(token_id == (int) EmphToken::EOL);
     }
 
     // Append a string that has already been otherwise processed.
-    void Append_String(const std::string & in) {
+    void Append_Text(const std::string & in) {
       size_t start = text.size();
       size_t end = start + in.size();
       text.Append_Raw(in);
@@ -118,27 +125,68 @@ namespace emp {
       }
     }
 
-    void Append_Tag(const std::string & tag) {
-      const auto & info = tag_info[tag];
-      switch (info.type) {
-        case TagType::OPEN:
-          active_styles.insert(info.style);
+    void Append_Tag(const std::string & in) {
+      emp_assert(in.size() >= 2);
+      emp_assert(in[0] == '`');
+      const std::string & style = tag_map[in[1]];
+      switch (in[1]) {
+        // -- Tags that toggle --
+        case '*': // Bold
+        case '`': // Code
+        case '/': // Italic
+        case '~': // Strike
+        case '.': // Subscript
+        case '^': // Superscript
+        case '_': // Underline
+          if (active_styles.count(style)) {
+            active_styles.erase(style);
+          } else {
+            active_styles.insert(style);
+          }
           break;
-        case TagType::CLOSE:
-          active_styles.erase(info.style);
+
+        // -- Tags that go to the end of line --
+        case '#': // Heading
+        case '-': // Bullet
+        case '+': // Number Bullet
+        case '>': // Indent
+        case '"': // Blockquote
+          active_styles.insert(style);
           break;
-        case TagType::REPLACE:
-          Append_String(info.text);
+
+        // -- Other special tags --
+        case '|': // Continue previous format.
           break;
-        default:
-          break;
+      }
+    }
+
+    void Append_Newline() {
+      // Terminate any style that only goes to the end of the current line.
+      active_styles.erase("heading");
+      active_styles.erase("bullet");
+      active_styles.erase("ordered");
+      active_styles.erase("indent");
+      active_styles.erase("blockquote");
+
+      // Pass along the newline.
+      Append_Text("\n");
+    }
+
+    void Append_Escape(const std::string & in) {
+      emp_assert(in[0] == '\\');
+      switch (in[1]) {
+        case '`': Append_Text("`"); break;
+        case '\\': Append_Text("\\"); break;
+        case ' ': Append_Text(" "); text.back().SetStyle("nobreak"); break;
+        case 'n': Append_Text("\n"); break;
+        case 't': Append_Text("\t"); break;
       }
     }
 
   public:
     EmphaticEncoding(Text & _text, const std::string _name="emphatic")
       : TextEncoding(_text, _name) { SetupTags(); }
-    ~EmphaticEncoding() = default;
+    ~EmphaticEncoding() override { };
 
     // Add new Emphatic encoded text into this object.
     void Append(std::string in) override {
@@ -146,28 +194,43 @@ namespace emp {
 
       auto tokens = lexer.Tokenize(in);
       for (const auto & token : tokens) {
-        switch (token.id) {
+        switch ((EmphToken) token.id) {
           case EmphToken::text:
+            Append_Text(token.lexeme);
             break;
           case EmphToken::escape:
+            Append_Escape(token.lexeme);
             break;
           case EmphToken::tag:
+            Append_Tag(token.lexeme);
             break;
           case EmphToken::code:
+            Append_Tag("``");
+            Append_Text(emp::to_string(token.lexeme[1]));
             break;
           case EmphToken::comment:
+            // Explicitly discard comment.
             break;
           case EmphToken::link:
+            // TODO
             break;
           case EmphToken::url:
+            // TODO
             break;
           case EmphToken::image:
+            // TODO
             break;
           case EmphToken::plain:
+            Append_Text(token.lexeme.substr(2,token.lexeme.size()-2));
             break;
           case EmphToken::exe:
+            // TODO
             break;
           case EmphToken::eval:
+            // TODO
+            break;
+          case EmphToken::EOL:
+            Append_Newline();
             break;
           default:
             notify::Error("Unknown token: ", token.id);
