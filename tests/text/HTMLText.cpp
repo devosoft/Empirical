b6470819ed11b0a9eb7b6bd5322a2611ac09f573
Emily Dolson <emilyldolson@gmail.com>
2023-02-28 17:16:23 -0500

    Fix license

--- tests/text/HTMLText.cpp
+++ tests/text/HTMLText.cpp
@@ -3,7 +3,7 @@
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
  *  @date 2022-23.
  *
- *  @file Text.cpp
+ *  @file HTMLText.cpp
  */
 
 #include <iostream>
