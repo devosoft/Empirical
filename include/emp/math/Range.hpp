99a0696b9ae3137cc301a1bd106209580f0bd4ac
Charles Ofria <charles.ofria@gmail.com>
2023-03-12 10:59:34 -0400

    Cleanup and new functionality on emp::Range.

--- include/emp/math/Range.hpp
+++ include/emp/math/Range.hpp
@@ -1,7 +1,7 @@
 /**
  *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
- *  @date 2016-2022.
+ *  @date 2016-2023.
  *
  *  @file Range.hpp
  *  @brief A simple way to track value ranges
@@ -27,19 +27,16 @@ namespace emp {
 
   public:
     Range() = default;
-    Range(T _l, T _u) : lower(_l), upper(_u) { emp_assert(_l < _u); }
+    Range(T _l, T _u) : lower(_l), upper(_u) { emp_assert(_l <= _u, _l, _u); }
     Range(const Range &) = default;
 
     T GetLower() const { return lower; }
     T GetUpper() const { return upper; }
-
-    size_t CalcBin(T value, size_t num_bins) const {
-      return (size_t) (((double) (value - lower)) / ((double) (upper - lower)) * (double) num_bins);
-    }
+    T GetSize() const { return upper - lower; }
 
     Range & operator=(const Range&) = default;
-    bool operator==(const Range& _in) const { return lower==_in.lower && upper==_in.upper; }
-    bool operator!=(const Range& _in) const { return lower!=_in.lower || upper!=_in.upper; }
+    bool operator==(const Range& _in) const = default;
+    bool operator!=(const Range& _in) const = default;
 
     void SetLower(T l) { lower = l; }
     void SetUpper(T u) { upper = u; }
@@ -59,22 +56,32 @@ namespace emp {
     bool Valid(T value) const { return value >= lower && value <= upper; }
 
     /// Force a value into range
-    T Limit(T _in) const { return (_in < lower) ? lower : ((_in > upper) ? upper : _in); }
+    T LimitValue(T _in) const { return (_in < lower) ? lower : ((_in > upper) ? upper : _in); }
+    double ToFraction(T _in) const {
+      emp_assert(GetSize() != 0);
+      return static_cast<double>(_in - lower) / static_cast<double>(GetSize());
+    }
+    T FromFraction(double frac) const { return frac * GetSize() + lower; }
 
     // Adjust the upper or lower if provided value is more limiting.
     void LimitLower(T in) { if (in > lower) lower = in; }
     void LimitUpper(T in) { if (in < upper) upper = in; }
 
+    size_t CalcBin(T value, size_t num_bins) const {
+      if (upper == lower) return 0;
+      return (size_t) (((double) (value - lower)) / ((double) (upper - lower)) * (double) num_bins);
+    }
+
     /// Produce a vector that spreads values evenly across the range.
-    emp::vector<T> Spread(size_t s) const {
+    emp::vector<T> Spread(const size_t s) const {
       emp_assert(s >= 1);
+      if (s == 1) return emp::vector<T>(1,lower/2+upper/2); // On point is in the middle of the range.
       emp::vector<T> out(s);
       out[0] = lower;
-      if (s > 1) {
-        T range = upper - lower;
-        for (size_t i = 1; i < s; i++) {
-          out[i] = lower + (T) ((((double) i) / (double)(s-1)) * range);
-        }
+      T range = upper - lower;
+      for (size_t i = 1; i < s; i++) {
+        double frac = static_cast<double>(i)/static_cast<double>(s-1);
+        out[i] = lower + static_cast<T>(frac * range);
       }
       return out;
     }
