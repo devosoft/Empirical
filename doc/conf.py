d003260633a62a35d4c948ebdab06b6abbdbba3a
Emily Dolson <emilyldolson@gmail.com>
2023-07-01 00:05:47 -0400

    Try removing autodoc

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,6 @@ import textwrap
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',
     'sphinx.ext.todo',
