81481d9af6c1fc7b8ee631f7e93b2f85034921b6
Emily Dolson <emilyldolson@gmail.com>
2023-07-02 14:39:35 -0400

    Fix systematics boilerplate

--- include/emp/Evolve/Systematics.hpp
+++ include/emp/Evolve/Systematics.hpp
@@ -1,9 +1,9 @@
 /*
- *  note: This file is part of Empirical, https://github.com/devosoft/Empirical
- *  copyright: Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
  *  date: 2017-2023
- */
- /**
+*/
+/**
  *  @file
  *  @brief Track genotypes, species, clades, or lineages of organisms in a world.
  *
