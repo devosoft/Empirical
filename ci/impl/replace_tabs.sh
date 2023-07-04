e36d8c003a3687270285f2713fce42ed9eb8e11b
Emily Dolson <emilyldolson@gmail.com>
2023-07-04 01:05:13 -0400

    exlude bibliography from replace_tabs

--- ci/impl/replace_tabs.sh
+++ ci/impl/replace_tabs.sh
@@ -6,7 +6,7 @@ set -e
 
 # greq -qI tests if file is binary
 # adapted from https://stackoverflow.com/a/29689345
-TARGETS=$(find . -type f ! -path "./third-party/*" ! -path "./.git/*" ! -path "*/assets/*" ! -path "*Makefile" ! -path "*Maketemplate*" ! -path "./node_modules/*" -exec grep -qI . {} ';' -print)
+TARGETS=$(find . -type f ! -path "./third-party/*" ! -path "./.git/*" ! -path "*/assets/*" ! -path "*Makefile" ! -path "*Maketemplate*" ! -path "./node_modules/*" ! -path ./doc/bibliography.bib -exec grep -qI . {} ';' -print)
 
 for filename in ${TARGETS}
 do
