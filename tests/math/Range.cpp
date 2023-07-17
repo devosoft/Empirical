26b0c301de3f249e781a0ea8e2b6db1a49ff0423
Charles Ofria <charles.ofria@gmail.com>
2023-07-17 10:53:53 -0400

    Added tests for exclusive end-point ranges.

--- tests/math/Range.cpp
+++ tests/math/Range.cpp
@@ -22,6 +22,7 @@ TEST_CASE("Test Range", "[math]")
   emp::Range<int> r(0,10);
   REQUIRE(r.GetLower() == 0);
   REQUIRE(r.GetUpper() == 10);
+  REQUIRE(r.GetSize() == 11); // 0 through 10 inclusive.
 
   // CalcBin
   REQUIRE(r.CalcBin(5,10) == 5);
@@ -63,18 +64,92 @@ TEST_CASE("Test Range", "[math]")
   emp::Range<char> r2 = emp::MakeRange('a','z');
   REQUIRE(r2.GetLower() == 'a');
   REQUIRE(r2.GetUpper() == 'z');
+  REQUIRE(r2.GetSize() == 26);
   REQUIRE(r2.CalcBin('g',26) == 6);
 
   // IntRange
   emp::Range<int> r3 = emp::IntRange(-5,5);
   REQUIRE(r3.GetLower() == -5);
   REQUIRE(r3.GetUpper() == 5);
+  REQUIRE(r3.GetSize() == 11);
   REQUIRE(r3.LimitValue(100) == 5);
 
   // DRange
   emp::Range<double> r4 = emp::DRange(0.1,23.5);
   REQUIRE(r4.GetLower() == 0.1);
   REQUIRE(r4.GetUpper() == 23.5);
+  REQUIRE(r4.GetSize() > 23.3999); REQUIRE(r4.GetSize() < 23.4001);
+  REQUIRE(!r4.Valid(0.0));
+  REQUIRE(r4.CalcBin(4.7,5) == 0);
+  REQUIRE(r4.CalcBin(4.8,5) == 1);
+}
+
+TEST_CASE("Test Range with non-inclusive endpoint", "[math]")
+{
+  // Basic constructor
+  [[maybe_unused]] emp::Range<int, false> rng;
+
+  // GetLower GetUpper
+  emp::Range<int, false> r(0,10);
+  REQUIRE(r.GetLower() == 0);
+  REQUIRE(r.GetUpper() == 10);
+  REQUIRE(r.GetSize() == 10); // 0 through 10 exclusive.
+
+  // CalcBin
+  REQUIRE(r.CalcBin(5,10) == 5);
+  REQUIRE(r.CalcBin(1,4) == 0);
+
+  // Comparison
+  emp::Range<int, false> r1 = r;
+  REQUIRE(r1 == r);
+  r1.SetLower(5);
+  REQUIRE(r1 != r);
+
+  // Valid Limit
+  REQUIRE(!r1.Valid(0));
+  REQUIRE(r1.LimitValue(0) == 5);
+
+  // Spread
+  emp::vector<int> s = r.Spread(6);
+  int j=0;
+  for(int i=0;i<6;i++){
+    REQUIRE(s[i] == j);
+    j+=2;
+  }
+
+  // SetLower SetUpper Set
+  r1.SetLower(0);
+  r1.SetUpper(15);
+  r.Set(0,15);
+  REQUIRE(r1 == r);
+  REQUIRE(r.GetLower() == 0);
+  REQUIRE(r.GetUpper() == 15);
+
+  // SetMaxLower SetMaxUpper
+  r.SetMaxLower();
+  r.SetMaxUpper();
+  REQUIRE(r.GetLower() == -2147483648);
+  REQUIRE(r.GetUpper() == 2147483647);
+
+  // MakeRange
+  emp::Range<char, false> r2 = emp::MakeRange<char,false>('a','z');
+  REQUIRE(r2.GetLower() == 'a');
+  REQUIRE(r2.GetUpper() == 'z');
+  REQUIRE(r2.GetSize() == 25);   // 'z' is excluded.
+  REQUIRE(r2.CalcBin('g',26) == 6);
+
+  // IntRange
+  emp::Range<int, false> r3 = emp::IntRange<false>(-5,5);
+  REQUIRE(r3.GetLower() == -5);
+  REQUIRE(r3.GetUpper() == 5);
+  REQUIRE(r3.GetSize() == 10);
+  REQUIRE(r3.LimitValue(100) == 5);
+
+  // DRange
+  emp::Range<double, false> r4 = emp::DRange<false>(0.1,23.5);
+  REQUIRE(r4.GetLower() == 0.1);
+  REQUIRE(r4.GetUpper() == 23.5);
+  REQUIRE(r4.GetSize() > 23.3999); REQUIRE(r4.GetSize() < 23.4001);
   REQUIRE(!r4.Valid(0.0));
   REQUIRE(r4.CalcBin(4.7,5) == 0);
   REQUIRE(r4.CalcBin(4.8,5) == 1);
