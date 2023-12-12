669cfd25688eeee23868c93b64494d1b24e8408d
Charles Ofria <charles.ofria@gmail.com>
2023-12-12 11:16:04 -0500

    Added stub functions for Load and Save to SerialPod.

--- include/emp/serialize/SerialPod.hpp
+++ include/emp/serialize/SerialPod.hpp
@@ -26,7 +26,7 @@
  *  Inside of MyClass we might have:
  * 
  *  void MyClass(SerialPod & pod) {
- *    pod.Serialize(member_a, member_b, member_c);  // List all members to save/load
+ *    pod(member_a, member_b, member_c);  // List all members to save/load
  *  }
  */
 
@@ -42,6 +42,7 @@
 
 #include "../base/Ptr.hpp"
 #include "../base/vector.hpp"
+#include "../meta/type_traits.hpp"
 
 namespace emp {
 
@@ -92,118 +93,30 @@ namespace emp {
 
     bool IsLoad() const { return std::holds_alternative<is_ptr>(stream_ptr); }
     bool IsSave() const { return std::holds_alternative<os_ptr>(stream_ptr); }
+
+    template <typename T>
+    SerialPod & Load(T & in) {
+      static_assert(!emp::is_ptr_type<std::decay<T>>(),
+        "SerialPod cannot load or save pointers without more information.\n"
+        "Use ManagePtr(value) for restoring pointers by first building the instance,\n"
+        "or LinkPtr(value) to use the value of a pointer that is managed elsewhere." );
+    }
+
+    template <typename T>
+    SerialPod & Save(T & in) {
+      static_assert(!emp::is_ptr_type<std::decay<T>>(),
+        "SerialPod cannot load or save pointers without more information.\n"
+        "Use ManagePtr(value) for restoring pointers by first building the instance,\n"
+        "or LinkPtr(value) to use the value of a pointer that is managed elsewhere." );
+    }
+
+    template <typename T>
+    SerialPod & operator()(T & in) {
+      if (IsLoad()) return Load(in);
+      else return Save(in);
+    }
   };
 
-
-  /// StoreVar() takes a SerialPod and a variable and stores that variable to the pod.
-  /// The third argument (bool vs. int) will receive a bool, and thus bool versions
-  /// are preferred in the case of a tie.  Specialized versions of this function can be
-  /// included elsewhere, as needed, and should take a bool as the third argument.
-
-  // Custom classes should run their built-in EMP_Store member function.
-  template <typename T>
-  auto StoreVar(SerialPod & pod, const T & var, bool) -> typename T::emp_load_return_type & {
-    var.EMP_Store(pod);
-    return pod;
-  }
-
-  // Special standard library types need to have a custom StoreVar method built.
-  template <typename T>
-  void StoreVar(SerialPod & pod, const emp::vector<T> & var, bool) {
-    StoreVar(pod, var.size(), true);
-    for (int i = 0; i < (int) var.size(); ++i) {
-      StoreVar(pod, var[i], true);
-    }
-
-    // @CAO for now use ':' separator, but more generally we need to ensure uniquness.
-    pod.OStream() << ':';
-    // emp_assert(pod.OStream()); // @ELD - this is throwing a compiler error
-  }
-
-  // As a fallback, just send the saved object to the SerialPod's output stream.
-  template <typename T>
-  void StoreVar(SerialPod & pod, const T & var, int) {
-    // @CAO for now use ':', but more generally we need to ensure uniquness.
-    pod.OStream() << var << ':';
-    // emp_assert(pod.OStream()); // @ELD - this is throwing a compiler error
-  }
-
-
-  // The SetupLoad() function determines what type of information a constructor needs from
-  // a SerialPod (based on the objects types) and returns that information.  By default, the
-  // function will produce an instance of the needed type to trigger the copy constructor,
-  // but if a constructor exists that takes a SerialPod it will use that instead.
-
-  // Use SFINAE technique to identify custom types.
-  template <typename T>
-  auto SetupLoad(SerialPod & pod, T*, bool) -> typename T::emp_load_return_type & {
-    return pod;
-  }
-
-  // Otherwise use default streams.
-  template <typename T>
-  auto SetupLoad(SerialPod & pod, const T*, int) -> T {
-    // std::decay<T> var;
-    T var;
-    pod.IStream() >> var;
-    pod.IStream().ignore(1);  // Ignore ':'
-    emp_assert(pod.IStream() && "Make sure the SerialPod is still okay.");
-    return var;
-  }
-
-  // Use special load for strings.
-  std::string SetupLoad(SerialPod & pod, std::string *, bool) {
-    std::string var;
-    std::getline(pod.IStream(), var, ':');
-    emp_assert(pod.IStream() && "Make sure the SerialPod is still okay.");
-    return var;
-  }
-
-  // Use special load for vectors of arbitrary type.
-  template <typename T>
-  emp::vector<T> SetupLoad(SerialPod & pod, emp::vector<T> *, bool) {
-    const uint32_t v_size(SetupLoad(pod, &v_size, true));
-    emp::vector<T> var;
-
-    // Create vector of correct size and create elements with pod.
-    for (uint32_t i = 0; i < v_size; i++) {
-      var.emplace_back(SetupLoad(pod, &(var[0]), true));
-    }
-    emp_assert(pod.IStream() && "Make sure the SerialPod is still okay.");
-    return var;
-  }
-
-
-
-  // Setup for a variadic Store() function that systematically save all variables in a SerialPod.
-  #ifndef DOXYGEN_SHOULD_SKIP_THIS
-  namespace internal {
-
-    // Base implementaion to specialize on.
-    template <typename... IGNORE> struct serial_impl;
-
-    // Specialized to recurse, storing or loading individual vars.
-    template <typename FIRST_TYPE, typename... OTHER_TYPES>
-    struct serial_impl<FIRST_TYPE, OTHER_TYPES...> {
-      static void Store(SerialPod & pod, FIRST_TYPE & arg1, OTHER_TYPES&... others) {
-        StoreVar(pod, arg1, true);
-        serial_impl<OTHER_TYPES...>::Store(pod, others...);
-      }
-    };
-
-    // End condition for recursion when no vars remaining.
-    template <> struct serial_impl<> {
-      static void Store(SerialPod &) { ; }
-    };
-  }
-  #endif // DOXYGEN_SHOULD_SKIP_THIS
-
-  template <typename... ARG_TYPES>
-  void Store(SerialPod & pod, ARG_TYPES&... args) {
-    internal::serial_impl<ARG_TYPES...>::Store(pod, args...);
-  }
-
-}
-}
+} // Close namespace emp
 
 #endif // #ifndef EMP_IO_SERIALIZE_HPP_INCLUDE
