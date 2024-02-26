bb50224fff4529d4a1338ccbefcb1bf4820a27be
Charles Ofria <charles.ofria@gmail.com>
2024-02-25 19:17:47 -0500

    Refactor of Text_Rule to be more modular.

--- include/emp/text/Text_Rule.hpp
+++ include/emp/text/Text_Rule.hpp
@@ -49,85 +49,122 @@ namespace emp {
   /// tag to be generated.  When the link ends, the close take will appear.
   /// If there is no style, the internal character will trigger this rule to apply.
 
+  // == DEVELOPER NOTES ==
+  // Types of rules we need to support:
+  //   Style (e.g., bold, font, link, etc)
+  //     Standard - open and close are individually specified
+  //     Toggles  - open and close are the same
+  //     Lines    - open is specified and close is always a newline
+  //     Control  - open may have a control sequence, but close is still simple
+  //
+  //   Object (e.g., Symbol, emoji, image, page break, etc)
+  //     Simple replacement - encoding tag becomes the object name (perhaps "type:name")
+  //     Parametered replacement - where a control parameter in the tag must be processed.
+
   class Text_Rule {
-  private:
-    size_t id = emp::MAX_SIZE_T;  ///< Unique ID for this rule (index in tag_map)
-    int open_token_id = -1;       ///< ID of open_tag_start in the lexer
-    int close_token_id = -1;      ///< ID of close_tag in the lexer
+  protected:
 
-    // - Target parsing info -
-    String open_tag_start;        ///< Beginning of open tag. (E.g. "<a" or "\bold{")
-    char open_tag_end = '\0';     ///< End of open tag (E.g., '>') or empty if no control used
-    String close_tag;             ///< Tag to indicate close of text, or empty if no text used
+    //  ====================
+    //  === HELPER TYPES ===
+    //  ====================
 
-    // - Styling info -
-    // A base style would be "font"
-    // Style argument could be "arial"
-    // This would generate a full style: "font:arial"
-    String base_style;          ///< What style is this rule associated with? (args may be added)
-    String symbol_type;         ///< Is there a special symbol associated with this rule?
-    char internal_char = '\0';  ///< What character is used to placehold internally in Text?
+    enum RuleType { UNKNOWN_RULE=0, STYLE_RULE, OBJECT_RULE };
 
-    std::function<String(String)> to_style_arg; ///< Convert CONTROL into STYLE arguments
-    std::function<String(String)> to_control;   ///< Convert STYLE into CONTROL
+    /// A helper tag type for tags with a control sequence to specify their operation.
+    struct TagInfo {
+      int token_id = -1;  ///< ID of this tag in the lexer
+      String start;   ///< Unique beginning of the tag (e.g., "<img " or "<a ")
+      char end='\0';  ///< End of tag (E.g., '>')
+
+      std::function<String(String)> to_arg_fun;     ///< Convert CONTROL into internal ARG info
+      std::function<String(String)> to_control_fun; ///< Convert ARG description into CONTROL
+
+      // Can this tag forgo a control sequence?
+      TagInfo() { }
+      TagInfo(String start, char end='\0') : start(start), end(end) { }
+      [[nodiscard]] bool IsUsed() const { return token_id >= 0; }
+      [[nodiscard]] bool IsSimple() const { return end == '\0'; }
+      [[nodiscard]] bool HasFuns() const { return to_arg_fun && to_control_fun; }
+    };
+
+
+    //  =====================
+    //  ===  MEMBER VARS  ===
+    //  =====================
+
+    size_t id = emp::MAX_SIZE_T;       ///< Unique ID for this rule (index in tag_map)
+    RuleType rule_type = UNKNOWN_RULE; ///< What type of rule is this?
+
+    TagInfo main_tag; ///< Identifier tag for an object or open tag for a style
+    String base_type; ///< Styles (eg, "bold", "font:...") or Objects (eg, "image", "symbol:theta")
+
+    // Style-only data
+    TagInfo close_tag; ///< Identifier tag to signal the end of a style.
+
+    // Object-only data.
+    char placeholder = ' ';  ///< Character to use internally in Text
 
   public:
     Text_Rule() { }
-    Text_Rule(String open_tag_start, char open_tag_end, String close_tag, String base_style, String symbol_type)
-      : open_tag_start(open_tag_start), open_tag_end(open_tag_end), close_tag(close_tag)
-      , base_style(base_style), symbol_type(symbol_type)
-    { }
+    Text_Rule(String open_tag_start, char open_tag_end, String close_tag,
+              String base_type, char placeholder=' ')
+      : main_tag(open_tag_start, open_tag_end), base_type(base_type), close_tag(close_tag)
+      , placeholder(placeholder)
+    {
+      // Styles have close tags, objects do not.
+      rule_type = close_tag.empty() ? RuleType::OBJECT_RULE : RuleType::STYLE_RULE;
+    }
 
     // === Accessors ===
-    size_t GetID() const { return id; }
-    const String & GetOpenTagStart() const { return open_tag_start; }
-    char GetOpenTagEnd() const { return open_tag_end; }
-    const String & GetCloseTag() const { return close_tag; }
-    const String & GetBaseStyle() const { return base_style; }
-    const String & GetSymbol() const { return symbol_type; }
-    char GetInternalChar() const { return internal_char; }
+    [[nodiscard]] size_t GetID()           const { return id; }
+    [[nodiscard]] String GetOpenTagStart() const { return main_tag.start; }
+    [[nodiscard]] char   GetOpenTagEnd()   const { return main_tag.end; }
+    [[nodiscard]] String GetCloseTag()     const { return close_tag.start; }
+    [[nodiscard]] String GetBaseStyle()    const { return rule_type == STYLE_RULE ? base_type : ""; }
+    [[nodiscard]] String GetSymbol()       const { return rule_type == OBJECT_RULE ? base_type : ""; }
+    [[nodiscard]] char   GetInternalChar() const { return placeholder; }
 
     void SetID(size_t in_id) {
       emp_assert(in_id < 1000000000, in_id, "Invalid rule ID");
       id = in_id;
     }
-    void SetInternalChar(char in) { internal_char = in; }
-    void SetConversions(auto _to_style_arg, auto _to_control) {
-      to_style_arg = _to_style_arg;
-      to_control = _to_control;
+    void SetConversions(auto _to_arg_fun, auto _to_control_fun) {
+      main_tag.to_arg_fun = _to_arg_fun;
+      main_tag.to_control_fun = _to_control_fun;
     }
 
     // === Helper functions ===
-    bool IsValid() const { return (id < emp::MAX_SIZE_T); }
-    bool UsesControl() const { return open_tag_end != '\0'; }
-    bool UsesText() const { return close_tag.size(); }
-    bool UsesStyle() const { return base_style.size(); }
-    bool UsesSymbol() const { return symbol_type.size(); }
+    [[nodiscard]] bool IsValid() const { return (id != emp::MAX_SIZE_T); }
+    [[nodiscard]] bool UsesControl() const { return !main_tag.IsSimple(); }
+    [[nodiscard]] bool UsesText() const { return close_tag.IsUsed(); }
+    [[nodiscard]] bool SetsStyle() const { return rule_type == RuleType::STYLE_RULE; }
+    [[nodiscard]] bool SetsObject() const { return rule_type == RuleType::OBJECT_RULE; }
+    [[nodiscard]] bool IsSimpleReplacement() const { return base_type.size() == 0; }
 
-    String MakeStyle(String control) const {
-      if (UsesControl() && to_style_arg) {
-        String style_arg = to_style_arg(control);
-        if (style_arg.size()) return MakeString(base_style, ':', style_arg);
+    [[nodiscard]] String MakeStyle(String control) const {
+      if (UsesControl()) {
+        emp_assert(main_tag.HasFuns());
+        String style_arg = main_tag.to_arg_fun(control);
+        if (style_arg.size()) return MakeString(base_type, ':', style_arg);
       }
-      return base_style;
+      return base_type;
     }
 
-    String MakeOpenTag(String style) const {
+    [[nodiscard]] String MakeOpenTag(String style) const {
       if (UsesControl()) {
-        return MakeString(open_tag_start, to_control(style), open_tag_end);
+        return MakeString(main_tag.start, main_tag.to_control_fun(style), main_tag.end);
       }
-      else return open_tag_start;
+      else return main_tag.start;
     }
 
     void PrintDebug(std::ostream & os = std::cout) const {
-      os << "Rule " << id << ": " << open_tag_start;
-      if (UsesControl()) os << "CONTROL" << open_tag_end;
-      if (UsesText()) os << "TEXT" << close_tag;
-      os << " : base_style='" << base_style
-         << "' ; symbol_type='" << symbol_type
-         << "' ; internal_char='" << internal_char
-         << "' ; open_token_id=" << open_token_id
-         << " ; close_token_id=" << close_token_id
+      os << "Rule " << id << ": " << main_tag.start;
+      if (UsesControl()) os << "CONTROL" << main_tag.end;
+      if (UsesText()) os << "TEXT" << close_tag.start;
+      os << " : base_type='" << base_type
+         << "' ; placeholder='" << placeholder
+         << "' ; open_token_id=" << main_tag.token_id
+         << " ; close_token_id=" << close_tag.token_id
          << std::endl;
     }
   };
