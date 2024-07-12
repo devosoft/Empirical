43592f4c38f36d9296e95e9b07dee24e26703579
Charles Ofria <charles.ofria@gmail.com>
2024-07-12 17:31:52 -0400

    All basic features now in place for Emplex.

--- demos/LexerGenerator/Emplex.cpp
+++ demos/LexerGenerator/Emplex.cpp
@@ -9,19 +9,25 @@
 #include "emp/io/File.hpp"
 #include "emp/io/CPPFile.hpp"
 
-void LoadTokens(emp::Lexer & lexer, emp::String filename) {
+void LoadTokens(emp::Lexer & lexer, emp::String filename, bool verbose) {
   emp::File file(filename);
   file.RemoveIfBegins("#");  // Remove all lines that are comments
 
   for (emp::String line : file) {
     if (line.OnlyWhitespace()) continue;
-    bool ignore = (line[0] == '-');
+    bool ignore = line.PopIf('-');
     emp::String name = line.PopWord();  // First entry on a line is the token name.
     emp::String regex = line.Trim();    // Regex is remainder, minus start & end whitespace.
 
     emp::notify::TestError(!regex.size(), "Token '", name, "' does not have an associated regex.");
 
-    lexer.AddToken(name, regex, !ignore);
+    if (ignore) lexer.IgnoreToken(name, regex);
+    else lexer.AddToken(name, regex);
+
+    if (verbose) {
+      emp::notify::Message("Added token '", name, "'; ignore=", ignore, "; regex: ", regex);
+    }
+
   }
 }
 
@@ -44,14 +50,21 @@ int main(int argc, char* argv[])
       << "  Tokens with names starting with a minus sign are consumed and ignored, e.g.:\n"
       << "    -whitespace : [ \\t\\n\\r]+\n"
       << "  Options are:\n"
-      << "    -h   Print HELP (this message)\n"
-      << "    -v   Print VERBOSE output\n"
+      << "    -f [filename]  Specify output filename (default: " << out_filename << ")\n"
+      << "    -h             Print HELP (this message)\n"
+      << "    -v             Print VERBOSE output\n"
       << std::endl;
     exit(1);
   }
 
   emp::Lexer lexer;
-  LoadTokens(lexer, args[1]);
+  LoadTokens(lexer, args[1], verbose);
+
+  emp::CPPFile file(out_filename);
+  file.SetGuards("__AUTOMATED_LEXER__");
+  file.SetNamespace("emplex");
+  lexer.WriteCPP(file, "Lexer");
+  file.Write();
 
   return 0;
 }
