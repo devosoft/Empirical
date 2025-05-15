bc5ce15c238e3a5a7c785a9bfca1e5454d32f6e5
Charles Ofria <charles.ofria@gmail.com>
2025-05-14 21:11:21 -0400

    Added ANSIOptionMenu class.

--- include/emp/io/ascii_utils.hpp
+++ include/emp/io/ascii_utils.hpp
@@ -13,6 +13,7 @@
 #ifndef EMP_IO_ASCII_UTILS_HPP_INCLUDE
 #define EMP_IO_ASCII_UTILS_HPP_INCLUDE
 
+#include <functional>
 #include <iostream>
 #include <ostream>
 #include <stddef.h>
@@ -20,8 +21,117 @@
 #include "../base/assert.hpp"
 #include "../base/vector.hpp"
 #include "../datastructs/vector_utils.hpp"
+#include "../tools/String.hpp"
+
+#include "ANSI.hpp"
+#include "io_utils.hpp"
 
 namespace emp {
+
+  emp::String IOCharName(IOChar key) {
+    emp::String key_name = key.GetName();
+    return emp::MakeString('\'', key_name.AsANSIMagenta(), '\'');
+  }
+
+  class ANSIOptionMenu {
+  private:
+    class Entry {
+    private:
+      IOChar key;                       // E.g., 'h'
+      emp::String effect_text;          // E.g., "Help"
+      std::function<bool()> trigger;    // E.g., [](){ PrintHelp(); return false; }
+      bool is_add_on = false;           // Should this option be an "or" on previous line?
+      bool is_active = true;            // Should this option be displayed at all?
+      bool is_visible = true;           // Should this be listed in a regular menu?
+      
+    public:
+      Entry(int key, emp::String effect_text, std::function<bool()> trigger,
+            bool is_add_on=false, bool is_active=true, bool is_visible=true)
+        : key(key), effect_text(effect_text), trigger(trigger),
+          is_add_on(is_add_on), is_active(is_active), is_visible(is_visible) { }
+
+      // Default trigger to false (not done) if no return value is provided.
+      Entry(int key, emp::String effect_text, std::function<void()> trigger,
+            bool is_add_on=false, bool is_active=true, bool is_visible=true)
+        : key(key), effect_text(effect_text), trigger([trigger](){ trigger(); return false; }),
+          is_add_on(is_add_on), is_active(is_active), is_visible(is_visible) { }
+
+      bool IsVisible() const { return is_active && is_visible; }
+
+      void Print() const {
+        if (is_add_on) emp::Print(" or ", IOCharName(key), " to ", effect_text);
+        else emp::Print("\n", IOCharName(key), " to ",  effect_text);
+      }
+
+      void PrintActive() const { if (is_active) Print(); }
+      void PrintVisible() const { if (IsVisible()) Print(); }
+
+      bool Trigger() const { return trigger(); }
+
+      void SetTrigger(std::function<bool()> new_trigger) { trigger = new_trigger; }
+    };
+
+    emp::String question;
+    emp::vector<Entry> entry_list;
+    std::unordered_map<int, size_t> key_map; // Map of keys to entires.
+
+    // Get an entry associated with a particular key.
+    Entry & GetEntryByKey(int key) {
+      emp_assert(key_map.contains(key));
+      return entry_list[ key_map[key] ];
+    }
+
+  public:
+    ANSIOptionMenu & SetQuestion(emp::String in_q) { question = in_q; return *this; }
+
+    ANSIOptionMenu & AddOption(int key, emp::String effect_text, std::function<bool()> trigger,
+                               bool is_add_on=false, bool is_active=true, bool is_visible=true) {
+      key_map[key] = entry_list.size();
+      entry_list.emplace_back(key, effect_text, trigger, is_add_on, is_active, is_visible);
+      return *this;
+    }
+
+    // And an option that allows for a keystroke, but does not print by default.
+    ANSIOptionMenu & AddSilent(int key, emp::String effect_text, std::function<bool()> trigger,
+                               bool is_add_on=false, bool is_active=true) {
+      return AddOption(key, effect_text, trigger, is_add_on, is_active, false);
+    }
+
+    ANSIOptionMenu & AddAlias(int key) {
+      emp_assert(entry_list.size() > 0); // No entries to alias!
+      key_map[key] = entry_list.size()-1;
+      return *this;
+    }
+
+    ANSIOptionMenu & SetTrigger(int key, std::function<bool()> trigger) {
+      emp_assert(key_map.contains(key));
+      GetEntryByKey(key).SetTrigger(trigger);
+      return *this;
+    }
+    
+    void PrintOptions() const {
+      for (const Entry & entry : entry_list) {
+        entry.PrintVisible();
+      }
+      PrintLn();
+    }
+
+    void Run() const {
+      if (question.size()) Print(question);
+      PrintOptions();
+      bool done = false;
+      while (!done) {
+        int key = GetIOChar();
+        auto it = key_map.find(key);
+        if (it == key_map.end()) {
+          PrintLn("Unknown key ", IOCharName(key), '.');
+          continue;
+        }
+        size_t id = it->second;
+        done = entry_list[id].Trigger();
+      }
+    }
+};
 
   /// The following function prints an ascii bar graph on to the screen (or provided stream).
   template <typename T>
