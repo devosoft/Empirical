543708dd0f4fe13186dd594157784904f07a0714
Charles Ofria <charles.ofria@gmail.com>
2025-05-28 16:17:32 -0400

    Set up RobinHoodMap timing to provide colorful results.

--- examples/timing/RobinHoodMap.cpp
+++ examples/timing/RobinHoodMap.cpp
@@ -5,147 +5,134 @@
 
 #include "../../include/emp/base/vector.hpp"
 #include "../../include/emp/datastructs/RobinHoodMap.hpp"
+#include "../../include/emp/io/ANSI.hpp"
 #include "../../include/emp/math/Random.hpp"
+#include "../../include/emp/math/random_utils.hpp"
 #include "../../include/emp/tools/String.hpp"
 #include "../../include/emp/tools/string_utils.hpp"
 
-int main() {
-  static constexpr size_t COUNT = 10000000;
+#include "../../third-party/robin-hood-hashing/src/include/robin_hood.h"
 
-  emp::Random random;
-  emp::vector<double> values(COUNT);
-  for (double & val : values) val = random.GetDouble(0.0, 1000000.0);
+static constexpr size_t COUNT = 10000000;
+static constexpr size_t MAX_STEP = 10;
+static constexpr size_t MULT = 1;
 
+emp::vector<size_t> keys(COUNT);
+emp::vector<double> values(COUNT);
 
+struct Results {
+  emp::String name;
+  double insert_time;
+  double search_time;
+  double erase_time;
+  size_t found_count;
+  size_t erase_count;
+  size_t start_size;
+  size_t final_size;
+};
 
-
-  emp::RobinHoodMap<size_t, double> test_rh_map;
+template <typename MAP_T>
+Results Test(emp::String name) {
+  MAP_T test_map;
+  Results results;
+  results.name = name;
 
   // Test inserts
   std::clock_t start_time = std::clock();
   for (size_t i = 0; i < values.size(); ++i) {
-    test_rh_map.Insert(i, values[i]);
+    test_map.insert({keys[i], values[i]});
   }
   std::clock_t tot_time = std::clock() - start_time;
-  double result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
+  results.insert_time = ((double) tot_time) / (double) CLOCKS_PER_SEC;
+  results.start_size = test_map.size();
 
-  std::cout << "Robin Hood Map with " << COUNT << " insertions\n"
-            << "  insert time = " << emp::MakeString(result, " seconds.").AsANSIBlue() << "\n"
-            << "  size = "     << test_rh_map.size() << std::endl
-            << "  capacity = " << test_rh_map.capacity() << std::endl;
+// std::cout << "Test: " << name << "\n"
+//           << "  insert time = " << emp::MakeString(result, " seconds.").AsANSIBlue() << "\n"
+//           << "  start size = "  << test_map.size() << std::endl;
+//            << "  capacity = "    << test_map.capacity() << std::endl;
 
   // Test searching
   start_time = std::clock();
   size_t found_count = 0;
   for (size_t i = 0; i < values.size() * 2; ++i) {
-    if (test_rh_map.FindPtr(i) != nullptr) ++found_count;
+    if (test_map.contains(i)) ++found_count;
   }
   tot_time = std::clock() - start_time;
-  result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
-  std::cout << "  search time = " << emp::MakeString(result, " seconds.").AsANSIGreen() << "\n"
-            << "  found_count = " << found_count << "\n";
+  results.search_time = ((double) tot_time) / (double) CLOCKS_PER_SEC;
+  results.found_count = found_count;
+
+// std::cout << "  search time = " << emp::MakeString(result, " seconds.").AsANSIGreen() << "\n"
+//           << "  found_count = " << found_count << "\n";
 
   // Test deletion
   start_time = std::clock();
   size_t erase_count = 0;
   for (size_t i = values.size(); i >= 500; --i) {
-    if (test_rh_map.erase(i)) ++erase_count;
+    if (test_map.erase(i)) ++erase_count;
   }
   tot_time = std::clock() - start_time;
-  result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
-  std::cout << "  erase time = " << emp::MakeString(result, " seconds.").AsANSIRed() << "\n"
-            << "  erase_count = " << erase_count << "\n"
-            << "  final size = " << test_rh_map.size() << std::endl;
+  results.erase_time = ((double) tot_time) / (double) CLOCKS_PER_SEC;
+  results.erase_count = erase_count;
+  results.final_size = test_map.size();
+  // std::cout << "  erase time = " << emp::MakeString(result, " seconds.").AsANSIRed() << "\n"
+  //           << "  erase_count = " << erase_count << "\n"
+  //           << "  final size = " << test_map.size() << std::endl;
 
+  std::cout << "Finished calculating: " << name << std::endl;
+
+  return results;
+}
+
+template <typename FUN_T>
+void PrintRow(const emp::vector<Results> & result_vec, emp::String trait_name,
+              FUN_T fun, const emp::String & color=emp::ANSI::DefaultColor) {
+  std::cout << trait_name.AsANSIBold();
+  for (Results result : result_vec) {
+    emp::String out = emp::MakeString(fun(result));
+    size_t width = result.name.size() + 1;
+    out.PadBack(' ', width);
+    std::cout << color << out << emp::ANSI::DefaultColor;
+  }
   std::cout << std::endl;
+}
 
+int main() {
+  emp::Random random;
+  for (double & val : values) val = random.GetDouble(0.0, 1000000.0);
 
-
-
-
-
-  std::unordered_map<size_t, double> test_uo_map;
-
-  start_time = std::clock();
-
-  for (size_t i = 0; i < values.size(); ++i) {
-    test_uo_map.insert({i, values[i]});
+  size_t cur_key = 0;
+  for (size_t & key : keys) {
+    cur_key += random.GetUInt(1, MAX_STEP);
+    key = cur_key;
   }
-  tot_time = std::clock() - start_time;
-  result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
+  emp::Shuffle(random, keys);
 
-  std::cout << "Unordered Map with " << COUNT << " insertions\n"
-            << "  time = " << emp::MakeString(result, " seconds.").AsANSIBlue() << "\n"
-            << "  size = "     << test_uo_map.size() << std::endl;
+  Results results;
+  emp::vector<Results> result_vec;
 
-  // Test searching
-  start_time = std::clock();
-  found_count = 0;
-  for (size_t i = 0; i < values.size() * 2; ++i) {
-    if (test_uo_map.contains(i)) ++found_count;
-  }
-  tot_time = std::clock() - start_time;
-  result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
-  std::cout << "  search time = " << emp::MakeString(result, " seconds.").AsANSIGreen() << "\n"
-            << "  found_count = " << found_count << "\n";
+  results = Test<emp::RobinHoodMap<size_t, double>>("emp::RobinHoodMap");
+  result_vec.push_back(results);
+  results = Test<std::unordered_map<size_t, double>>("std::unordered_map");
+  result_vec.push_back(results);
+  results = Test<robin_hood::unordered_map<size_t, double>>("robin_hood::unordered_map");
+  result_vec.push_back(results);
+  results = Test<std::map<size_t, double>>("std::map");
+  result_vec.push_back(results);
 
-  // Test deletion
-  start_time = std::clock();
-  erase_count = 0;
-  for (size_t i = values.size(); i >= 500; --i) {
-    if (test_uo_map.erase(i)) ++erase_count;
-  }
-  tot_time = std::clock() - start_time;
-  result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
-  std::cout << "  erase time = " << emp::MakeString(result, " seconds.").AsANSIRed() << "\n"
-            << "  erase_count = " << erase_count << "\n"
-            << "  final size = " << test_uo_map.size() << std::endl;
-
-  std::cout << std::endl;
-
-
-
-
-
-
-
-  std::map<size_t, double> test_sorted_map;
-
-  start_time = std::clock();
-
-  for (size_t i = 0; i < values.size(); ++i) {
-    test_sorted_map.insert({i, values[i]});
-  }
-  tot_time = std::clock() - start_time;
-  result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
-
-  std::cout << "Ordered Map with " << COUNT << " insertions\n"
-            << "  time = " << emp::MakeString(result, " seconds.").AsANSIBlue() << "\n"
-            << "  size = "     << test_sorted_map.size() << std::endl;
-
-  // Test searching
-  start_time = std::clock();
-  found_count = 0;
-  for (size_t i = 0; i < values.size() * 2; ++i) {
-    if (test_sorted_map.contains(i)) ++found_count;
-  }
-  tot_time = std::clock() - start_time;
-  result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
-  std::cout << "  search time = " << emp::MakeString(result, " seconds.").AsANSIGreen() << "\n"
-            << "  found_count = " << found_count << "\n";
-
-  // Test deletion
-  start_time = std::clock();
-  erase_count = 0;
-  for (size_t i = values.size(); i >= 500; --i) {
-    if (test_sorted_map.erase(i)) ++erase_count;
-  }
-  tot_time = std::clock() - start_time;
-  result = ((double) tot_time) / (double) CLOCKS_PER_SEC;
-  std::cout << "  erase time = " << emp::MakeString(result, " seconds.").AsANSIRed() << "\n"
-            << "  erase_count = " << erase_count << "\n"
-            << "  final size = " << test_sorted_map.size() << std::endl;
-
-  std::cout << std::endl;
-
+  PrintRow(result_vec, "name        ",
+    [](const Results & result){ return result.name; }, emp::ANSI::Yellow);
+  PrintRow(result_vec, "insert_time ",
+    [](const Results & result){ return result.insert_time; }, emp::ANSI::Green);
+  PrintRow(result_vec, "search_time ",
+    [](const Results & result){ return result.search_time; }, emp::ANSI::Blue);
+  PrintRow(result_vec, "erase_time  ",
+    [](const Results & result){ return result.erase_time; }, emp::ANSI::Red);
+  PrintRow(result_vec, "found_count ",
+    [](const Results & result){ return result.found_count; }, emp::ANSI::Cyan);
+  PrintRow(result_vec, "erase_count ",
+    [](const Results & result){ return result.erase_count; }, emp::ANSI::Cyan);
+  PrintRow(result_vec, "start_size  ",
+    [](const Results & result){ return result.start_size; }, emp::ANSI::Magenta);
+  PrintRow(result_vec, "final_size  ",
+    [](const Results & result){ return result.final_size; }, emp::ANSI::Magenta);
 }
