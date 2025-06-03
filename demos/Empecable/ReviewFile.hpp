42858d8f81e43e1d3327a9abec961649051115ac
Charles Ofria <charles.ofria@gmail.com>
2025-06-03 12:45:33 -0400

    Lots of cleanup on Empecable's ReviewFile.

--- demos/Empecable/ReviewFile.hpp
+++ demos/Empecable/ReviewFile.hpp
@@ -35,7 +35,11 @@ private:
   emp::vector<emp::String> issues;       // List of issues found with this file.
   size_t pragma_once_line = 0;           // Have we found a "#pragma once" in this file?
 
+  static constexpr emplex::Token empty_token{0,"",0};  // Token for out-of-range positions.
+
 public:
+  size_t npos = static_cast<size_t>(-1); // ID for "beyond the end of the file"
+
   ReviewFile(emp::String filename, Mode mode)
     : filename(filename), path(fs::absolute(filename.str())), mode(mode)
   {
@@ -43,8 +47,7 @@ public:
       path = fs::canonical(filename.str());
       valid = true;
     } else {
-      std::cerr << ToBoldRed("Error:") << " file does not exist or is not a regular file: "
-                << filename << "\n";
+      PrintError("file does not exist or is not a regular file: ", filename);
       valid = false;
     }
   }
@@ -66,42 +69,66 @@ public:
   void AddWord(emp::String word) {
     emp_assert(!HasWord(word), word); // Words should not be added more than once.
     words.insert(word);
-    PrintLn("Added '", word.AsANSICyan(), "' to file dictionary.");
+    emp::PrintLn("Added '", word.AsANSICyan(), "' to file dictionary.");
     save_required = true;
   }
 
   // ======= Token Management =======
 
-  emplex::Token NextToken() {
-    if (token_pos >= tokens.size()) return emplex::Token{0,"",0}; // No tokens left.
-    return tokens[token_pos++];
+  [[nodiscard]] const emplex::Token & GetToken(size_t pos) const {
+    if (token_pos >= tokens.size()) return empty_token; // Out of range.
+    return tokens[pos];
+  }
+  [[nodiscard]] emp::String GetLexeme(size_t pos) const { return GetToken(pos).lexeme; }
+  [[nodiscard]] size_t GetLineID(size_t pos) const { return GetToken(pos).line_id; }
+  [[nodiscard]] int GetTokenID(size_t pos) const { return GetToken(pos).id; }
+
+  [[nodiscard]] const emplex::Token & GetToken() const { return GetToken(token_pos); }
+  [[nodiscard]] emp::String GetLexeme() const { return GetLexeme(token_pos); }
+  [[nodiscard]] size_t GetLineID() const { return GetLineID(token_pos); }
+  [[nodiscard]] int GetTokenID() const { return GetTokenID(token_pos); }
+
+  [[nodiscard]] size_t NumTokens() const { return tokens.size(); }
+
+  void SetTokenPos(size_t pos) {
+    emp_assert(pos < tokens.size(), pos, tokens.size());
+    token_pos = pos;
   }
 
-  emplex::Token PrevToken() {
-    if (token_pos > 0) token_pos--;
-    return tokens[token_pos];
+  /// Find the position of the next token of the specified type (or npos if not found.)
+  [[nodiscard]] size_t FindTokenPos(int type_id, size_t start_pos=0) const {
+    for (size_t pos = start_pos; pos < tokens.size(); ++pos) {
+      if (tokens[pos] == type_id) return pos;
+    }
+    return npos;
   }
 
-  emplex::Token GetToken() const { return tokens[token_pos]; }
-  emp::String GetLexeme() const { return tokens[token_pos].lexeme; }
-  size_t GetLineID() const { return tokens[token_pos].line_id; }
-  int GetTokenID() const { return tokens[token_pos].id; }
-  emplex::Token GetToken(size_t pos) const { return tokens[pos]; }
-  emp::String GetLexeme(size_t pos) const { return tokens[pos].lexeme; }
-  size_t GetLineID(size_t pos) const { return tokens[pos].line_id; }
-  int GetTokenID(size_t pos) const { return tokens[pos].id; }
-  size_t NumTokens() const { return tokens.size(); }
+  const emplex::Token & ResetTokens() { token_pos = 0; return GetToken(); }
+  const emplex::Token & NextToken() { return GetToken(++token_pos); }
+  const emplex::Token & PrevToken() { return GetToken(--token_pos); }
 
   void InsertToken(size_t pos, emplex::Token token) {
+    emp_assert(pos <= tokens.size());
     tokens.insert(tokens.begin()+pos, token);
     save_required = true;
   }
 
+  void InsertLexeme(size_t pos, emp::String lexeme) {
+    emp_assert(pos <= tokens.size());
+    size_t line_id;
+    if (pos == 0) line_id = 0;
+    else if (pos < tokens.size()) line_id = tokens[pos].line_id;
+    else line_id = tokens[pos-1].line_id+1;
+    InsertToken(pos, emplex::Token{emplex::Lexer::ID_OTHER, lexeme, line_id});
+  }
+
+  void RemoveToken(size_t pos, size_t count=1) {
+    emp_assert(pos+count <= tokens.size());
+    tokens.erase(tokens.begin()+pos, tokens.begin()+pos+count);
+    save_required = true;
+  }
+
   void ClearLexeme(size_t pos) {
-    if (pos == 0) {
-      std::cerr << "ERROR ERROR ERROR!!!!!!!!!!!!!!\n";
-      abort();
-    }
     tokens[pos].lexeme.clear();
     save_required = true;
   }
@@ -115,7 +142,9 @@ public:
 
   // Find a token anywhere on the target line.
   size_t FindPos_Line(size_t target_line) const {
-    emp_assert(target_line >= tokens[0].line_id && target_line <= tokens.back().line_id);
+    emp_assert(target_line >= tokens[0].line_id &&
+               target_line <= tokens.back().line_id,
+              target_line, tokens[0].line_id, tokens.back().line_id);
     size_t min=0, max=tokens.size();
     while (tokens[min].line_id != target_line) {
       size_t mid = (min+max)/2;
@@ -153,7 +182,7 @@ public:
         result += ToBoldRed(tokens[i].lexeme).AsANSIUnderline();
       }
       else if (tokens[i].lexeme == target_word) {
-        result += target_word.AsANSIBlue();
+        result += target_word.AsANSIBlue().AsANSIUnderline();
       }
       else { result += tokens[i].lexeme; }
     }
@@ -164,28 +193,49 @@ public:
   void PrintLine(size_t line_id) const {
     emp::String line_num_str = emp::MakeString(line_id).PadFront(' ', 5)+':';
     emp::String code_line = GetTokenLine(line_id);
-    PrintLn(line_num_str.AsANSIBrightWhite().AsANSIBold(), code_line);
+    emp::PrintLn(line_num_str.AsANSIBrightWhite().AsANSIBold(), code_line);
   }
 
   // Print a range of lines the current token.
-  void PrintTokenRange(size_t range=1) {
+  void PrintTokenRange(size_t range=1) const {
     size_t line_id = tokens[token_pos].line_id;
     size_t min_line = (line_id > range) ? (line_id - range) : 0;
+    min_line = std::max(min_line, tokens[0].line_id); // Cannot start before beginning of file.
     size_t max_line = (line_id+range <= MaxLineID()) ? line_id+range : MaxLineID();
+    if (mode == Mode::DEBUG) {
+      emp::PrintLn("Printing ", range, "lines around ", line_id,
+                   "(from ", min_line, " to ", max_line, ").");
+    }
     for (size_t i = min_line; i <= max_line; ++i) {
       PrintLine(i);
     }
   }
 
+  // Print a range of lines the current token.
+  void PrintFront(size_t lines=1) const {
+    for (size_t i = 1; i <= lines; ++i) {
+      PrintLine(i);
+    }
+  }
+
+  // Try to open this file, read tokens, and load settings.
+  // Return whether additional analysis should continue.
   bool Load(emplex::Lexer & lexer, const std::unordered_set<emp::String> & project_words) {
     std::ifstream file(filename);
     if (!file.is_open()) {
-      PrintLn(ToBoldRed("ERROR: '", filename, "' failed to open."));
+      PrintError("'", filename, "' failed to open.");
+      valid = false;
       return false;
     }
     tokens = lexer.Tokenize(file);
     token_pos = 0;       // Reset the next token to be analyzed.
     issues.resize(0);    // Reset issues for new file.
+
+    if (NumTokens() == 0) {
+      PrintWarning("'", filename, "' is empty.");
+      valid = true;
+      return false;
+    }
 
     // Scan through file for Empecable instructions.
     for (auto & token : tokens) {
@@ -219,10 +269,14 @@ public:
 
   void Save() {
     if (!save_required) {
-      if (mode == Mode::Interactive) {
+      if (mode >= Mode::Interactive) {
         emp::PrintLn("No changes need to be saved in '", emp::ANSI::MakeGreen(filename), "'.");
       }
       return;
+    }
+    if (!valid) {
+      PrintError("Trying to save an invalid file: ", filename);
+      exit(1);
     }
     emp::PrintLn("Saving '", emp::ANSI::MakeGreen(filename),"'.");
 
@@ -250,6 +304,12 @@ public:
     file.close();
   }
 
+  // Clean up space for current file.
+  void Close() {
+    tokens.resize(0);
+    valid = false;
+  }
+
   // ========== Issue Tracking ==========
 
   size_t GetNumIssues() const { return issues.size(); }
@@ -263,7 +323,7 @@ public:
     // Report the issue.
     if (mode >= Mode::Verbose) {
       size_t line_num = GetLineID(token_pos);
-      PrintLn(emp::MakeString("Found in ", filename, ":", line_num, ": ").AsANSIYellow().AsANSIBold(), issue);
+      emp::PrintLn(ToBoldYellow("Found in ", filename, ":", line_num, ": "), issue);
 
       // Print the affected code.
       PrintTokenRange(1);
