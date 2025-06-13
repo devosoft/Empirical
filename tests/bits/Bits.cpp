56853b5f3cd261c3a7313da0853d857e91531d01
Charles Ofria <charles.ofria@gmail.com>
2025-06-13 11:54:12 -0400

    Adjusted bit test to (hopefully) print debug info before failing.

--- tests/bits/Bits.cpp
+++ tests/bits/Bits.cpp
@@ -71,7 +71,7 @@ TEST_CASE("0: Diagnosing current problems", "[bits]"){
   // Feel free to delete or change anything here; it's just to diagnose issues with CI.
   std::string init("10011001010000011101");
   emp::BitVector bv(init);
-  std::cout << "ONES(9): " << bv.CountOnes() << std::endl;
+  // std::cout << "ONES(9): " << bv.CountOnes() << std::endl;
   std::cout << "bv: " << bv << std::endl;
   std::cout << "DEBUG: ";
   bv.PrintDebug();
