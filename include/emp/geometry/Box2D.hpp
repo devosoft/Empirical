f8c68ab630585e956757f9f99948a3ef2e940983
Charles Ofria <charles.ofria@gmail.com>
2025-06-27 10:27:56 -0400

    Cleaned up and extended Box2D; made always use double.

--- include/emp/geometry/Box2D.hpp
+++ include/emp/geometry/Box2D.hpp
@@ -1,6 +1,6 @@
 /**
  * This file is part of Empirical, https://github.com/devosoft/Empirical
- * Copyright (C) 2024 Michigan State University
+ * Copyright (C) 2024-2025 Michigan State University
  * MIT Software license; see doc/LICENSE.md
  *
  * @file include/emp/geometry/Box2D.hpp
@@ -18,79 +18,70 @@
 
 namespace emp {
 
-  template <typename TYPE = double>
   class Box2D {
   private:
-    Point2D<TYPE> ul_corner;
-    Point2D<TYPE> size;
+    Point2D ul_corner;
+    Size2D size;
 
   public:
-    constexpr Box2D(const Point2D<TYPE> & ul_corner, const Point2D<TYPE> & size)
-      : ul_corner(ul_corner), size(size) {
-      ;
-    }
+    constexpr Box2D(const Point2D & ul_corner, const Size2D & size)
+      : ul_corner(ul_corner), size(size) {}
+    constexpr Box2D(const Size2D & size)
+      : ul_corner({0.0, 0.0}), size(size) {}
 
-    constexpr Box2D(TYPE _x, TYPE _y, TYPE _w, TYPE _h) : ul_corner(_x, _y), size(_w, _h) { ; }
+    constexpr Box2D(const Box2D &)       = default;
+    constexpr Box2D(Box2D &&)            = default;
+    Box2D & operator=(const Box2D & _in) = default;
+    Box2D & operator=(Box2D && _in)      = default;
 
-    constexpr Box2D(const Box2D &) = default;
-
-    Box2D & operator=(const Box2D & _in)           = default;
     constexpr int operator<=>(const Box2D &) const = default;
 
-    constexpr TYPE GetWidth() const { return size.GetX(); }
+    constexpr const Point2D & GetUL() const { return ul_corner; }
 
-    constexpr TYPE GetHeight() const { return size.GetY(); }
+    constexpr const Size2D & GetSize() const { return size; }
 
-    constexpr TYPE GetLeft() const { return ul_corner.GetX(); }
+    constexpr double GetLeft() const { return ul_corner.GetX(); }
 
-    constexpr TYPE GetRight() const { return ul_corner.GetX() + size.GetX(); }
+    constexpr double GetRight() const { return ul_corner.GetX() + size.Width(); }
 
-    constexpr TYPE GetTop() const { return ul_corner.GetY(); }
+    constexpr double GetTop() const { return ul_corner.GetY(); }
 
-    constexpr TYPE GetBottom() const { return ul_corner.GetY() + size.GetY(); }
+    constexpr double GetBottom() const { return ul_corner.GetY() + size.Height(); }
 
-    constexpr TYPE GetArea() const { return GetWidth() * GetHeight(); }
+    constexpr double GetArea() const { return size.Area(); }
 
-    constexpr TYPE GetPerimeter() const { return 2 * (GetWidth() + GetHeight()); }
+    constexpr double GetPerimeter() const { return 2.0 * (size.Width() + size.Height()); }
 
-    constexpr TYPE SetPosition(const Point2D<TYPE> & in) { ul_corner = in; }
+    constexpr void SetPosition(const Point2D & in) { ul_corner = in; }
 
-    constexpr TYPE SetSize(const Point2D<TYPE> & in) { size = in; }
+    constexpr void SetSize(const Size2D & in) { size = in; }
 
-    Box2D<TYPE> & Translate(Point2D<TYPE> shift) {
+    Box2D & Translate(Point2D shift) {
       ul_corner += shift;
       return *this;
     }
 
-    constexpr bool Contains(const Point2D<TYPE> & point) const {
+    constexpr bool Contains(const Point2D & point) const {
       return point.GetX() > GetLeft() && point.GetX() < GetRight() && point.GetY() > GetTop() &&
              point.GetY() < GetBottom();
     }
 
     // Is "other" fully contained inside of this box?
-    constexpr bool Contains(const Box2D<TYPE> & other) const {
+    constexpr bool Contains(const Box2D & other) const {
       return other.GetLeft() > GetLeft() && other.GetRight() < GetRight() &&
              other.GetTop() > GetTop() && other.GetBottom() < GetBottom();
     }
 
-    constexpr bool HasOverlap(const Box2D<TYPE> & other) const {
+    constexpr bool HasOverlap(const Box2D & other) const {
       // Make sure THIS is higher than OTHER.
       if (other.GetTop() < GetTop()) { return other.HasOverlap(*this); }
 
-      if (other.GetTop() > GetBottom()) {
-        return false;  // Other box starts BELOW this box.
-      }
-      if (other.GetLeft() > GetRight()) {
-        return false;  // Other box is to the RIGHT of this one.
-      }
-      if (other.GetRight() < GetLeft()) {
-        return false;  // Other box is to the LEFT of this one.
-      }
-      return true;
+      return (other.GetTop() <= GetBottom() &&  // Other box is not too low.
+              other.GetLeft() <= GetRight() &&  // ... not too far left
+              other.GetRight() >= GetLeft());   // ... and not too far right.
     }
   };
 
-  using Box = Box2D<double>;
 }  // namespace emp
 
 #endif  // #ifndef INCLUDE_EMP_GEOMETRY_BOX2D_HPP_GUARD
