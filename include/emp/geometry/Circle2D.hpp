8005fbd7be497b1bd5abf1d50e0e947e74e082ab
Charles Ofria <charles.ofria@gmail.com>
2025-06-29 14:00:28 -0400

    Improved Circle/Point interactions + constexpr.

--- include/emp/geometry/Circle2D.hpp
+++ include/emp/geometry/Circle2D.hpp
@@ -30,7 +30,7 @@ namespace emp {
 
     constexpr Circle2D(const Circle2D &) = default;
 
-    Circle2D & operator=(const Circle2D & _in)        = default;
+    constexpr Circle2D & operator=(const Circle2D &) = default;
 
     constexpr int operator<=>(const Circle2D &) const = default;
 
@@ -48,34 +48,54 @@ namespace emp {
 
     constexpr double GetCircumference() const { return PI * 2.0 * radius; }
 
-    Circle2D & SetCenter(const Point2D & c) {
+    constexpr Circle2D & SetCenter(const Point2D & c) {
       center = c;
       return *this;
     }
 
-    Circle2D & SetCenterX(double x) {
+    constexpr Circle2D & SetCenterX(double x) {
       center.SetX(x);
       return *this;
     }
 
-    Circle2D & SetCenterY(double y) {
+    constexpr Circle2D & SetCenterY(double y) {
       center.SetY(y);
       return *this;
     }
 
-    Circle2D & SetRadius(double new_radius) {
+    constexpr Circle2D & SetRadius(double new_radius) {
       radius = new_radius;
       return *this;
     }
 
-    Circle2D & Set(const Point2D & c, double r) {
+    constexpr Circle2D & Set(const Point2D & c, double r) {
       center = c;
       radius = r;
       return *this;
     }
 
-    Circle2D & Translate(Point2D shift) {
+    constexpr Circle2D operator+(Point2D shift) const { return {center + shift, radius}; }
+    constexpr Circle2D operator-(Point2D shift) const { return {center - shift, radius}; }
+    constexpr Circle2D operator*(double dilate) const { return {center, radius * dilate}; }
+    constexpr Circle2D operator/(double dilate) const { return {center, radius / dilate}; }
+
+    constexpr Circle2D & operator+=(Point2D shift) {
       center += shift;
+      return *this;
+    }
+
+    constexpr Circle2D & operator-=(Point2D shift) {
+      center -= shift;
+      return *this;
+    }
+
+    constexpr Circle2D & operator*=(double dilate) {
+      radius *= dilate;
+      return *this;
+    }
+
+    constexpr Circle2D & operator/=(double dilate) {
+      radius /= dilate;
       return *this;
     }
 
@@ -87,7 +107,7 @@ namespace emp {
       return (other.center.Distance(center) + other.GetRadius()) < GetRadius();
     }
 
-    bool HasOverlap(const Circle2D & other) const {
+    constexpr bool HasOverlap(const Circle2D & other) const {
       const double min_dist = radius + other.radius;
       return center.SquareDistance(other.center) < (min_dist * min_dist);
     }
