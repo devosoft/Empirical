93f3934ac5408565904551ce26359891af8bc6b4
Charles Ofria <charles.ofria@gmail.com>
2025-07-21 15:04:59 -0400

    Rebuild Canvas example to make good use of Physics/Surface.

--- examples/web/Canvas.cpp
+++ examples/web/Canvas.cpp
@@ -1,101 +1,81 @@
 /*
  *  This file is part of Empirical, https://github.com/devosoft/Empirical
  *  Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
- *  date: 2015-2018
+ *  date: 2015-2025
 */
 /**
  *  @file
  */
 
+#include "emp/geometry/Physics2D.hpp"
+#include "emp/geometry/Surface.hpp"
 #include "emp/math/Random.hpp"
 #include "emp/web/Animate.hpp"
 #include "emp/web/canvas_utils.hpp"
-#include "emp/web/color_map.hpp"
+#include "emp/web/CanvasShape.hpp"
+#include "emp/web/Color.hpp"
 #include "emp/web/emfunctions.hpp"
 #include "emp/web/web.hpp"
 
 namespace UI = emp::web;
 
-class MyAnimate : public UI::Animate {
-private:
-  UI::Document doc;
-  UI::CanvasPolygon poly;
-  UI::CanvasLine line;
-
-  emp::Random random;
-
-  double cx = 150;
-  double cy = 150;
-  double cr = 50;
-  double can_size = 400;
-  double poly_rot = 0.0;
-
-public:
-  MyAnimate() : doc("emp_base"), poly(200, 300, "red", "black"), line({5,5}, {395,395}, "green") {
-    // How big should each canvas be?
-    const double w = can_size;
-    const double h = can_size;
-
-    // Draw a simple circle animation on a canvas
-    auto mycanvas = doc.AddCanvas(w, h, "can");
-    targets.push_back(mycanvas);
-
-    // Add a button.
-    doc << "<br>";
-    doc.AddButton([this](){
-        ToggleActive();
-        auto but = doc.Button("toggle");
-        if (GetActive()) but.SetLabel("Pause");
-        else but.SetLabel("Start");
-      }, "Start", "toggle");
-
-    doc << UI::Text("fps") << "FPS = " << UI::Live( [this](){return 1000.0 / GetStepTime();} ) ;
-
-    // Draw some colors...
-    auto color_map = emp::GetHSLMap(20, 400.0, 100.0, 100, 100, 20, 100);
-
-    const double buffer = 20;
-    const double radius = (can_size - 2 * buffer)/(color_map.size()*2);
-    for (size_t i = 0; i < color_map.size(); i++) {
-      int x_pos = (int) (buffer + (2*i+1) * radius);
-      mycanvas.Circle(emp::Point(x_pos, 300), radius, color_map[i]);
-      doc << "<br>" << color_map[i];
-    }
-
-  }
-
-  void DoFrame() {
-    auto mycanvas = doc.Canvas("can");
-
-    // Update the circle position.
-    cx+=3;
-    if (cx >= can_size + cr) cx -= can_size;
-
-    // Draw the new circle.
-    mycanvas.Clear();
-    mycanvas.Circle(cx, cy, cr, "blue", "purple");
-    if (cx + cr > can_size) mycanvas.Circle(cx-can_size, cy, cr, "blue", "purple");
-
-    // Update the polygon position
-    poly_rot += 0.01;
-    mycanvas.Rotate(poly_rot);
-    mycanvas.Draw(poly);
-    mycanvas.Rotate(-poly_rot);
-
-    // Update the line.
-    mycanvas.Draw(line);
-
-    doc.Text("fps").Redraw();
-
-
-  }
+struct TestBody : public emp::PhysicsBody {
+  TestBody(emp::Circle circle = emp::Circle{1.0}, emp::Color color = emp::Palette::BLACK)
+    : PhysicsBody(circle, color) { }
 };
 
-MyAnimate anim;
+const emp::Size2D can_size{800,800};
+// emp::Surface<TestBody> surface(can_size);
+emp::Physics2D<TestBody> physics(can_size);
 
-int main()
+int emp_main()
 {
-}
+  DEBUG_STACK();
+  emp::Random random;
 
-// Local settings for Empecable file checker.
-// empecable_words: poly
+  UI::Document doc{"emp_base"};
+
+  emp::vector<size_t> body_ids;
+
+  body_ids.push_back( physics.AddBody(emp::Circle{{150,150}, 50}) );
+  body_ids.push_back( physics.AddBody(emp::Circle{{215,160}, 50}) );
+  body_ids.push_back( physics.AddBody(emp::Circle{{650,150}, 50}) );
+  body_ids.push_back( physics.AddBody(emp::Circle{{770,360}, 50}) ); // Off right
+  body_ids.push_back( physics.AddBody(emp::Circle{{200,785}, 50}) ); // Off bottom
+  body_ids.push_back( physics.AddBody(emp::Circle{{20,500}, 50}) ); // Off left
+  body_ids.push_back( physics.AddBody(emp::Circle{{550,45}, 50}) ); // Off top
+  body_ids.push_back( physics.AddBody(emp::Circle{{10,15}, 50}) ); // Off UL
+  body_ids.push_back( physics.AddBody(emp::Circle{{775,775}, 50}) ); // Off BR
+
+
+  // auto & body1 = physics.GetBody(id1);
+  // auto & body2 = physics.GetBody(id2);
+  // auto & body3 = physics.GetBody(id3);
+  // auto & body4 = physics.GetBody(id4);
+
+  auto canvas = doc.AddCanvas(can_size, "can");
+
+  // Draw the circles.
+  canvas.Clear();
+  UI::Draw(canvas, physics);
+
+  physics.Update_Bodies();
+  physics.Update_BodyCollisions();
+  physics.Update_EdgeCollisions();
+
+  for (size_t body_id : body_ids) {
+    auto & body = physics.GetBody(body_id);
+    // Draw trajectory.
+    canvas.Draw(body.GetCenter(), body.CalcShiftPos(), emp::Palette::MAGENTA);
+
+    // Draw body Center
+    canvas.Draw(emp::Circle{body.GetCenter(), 3}, emp::Palette::NONE, emp::Palette::YELLOW);
+
+    // Draw shift result
+    canvas.Draw(emp::Circle{body.CalcShiftPos(), 3}, emp::Palette::NONE, emp::Palette::ORANGE);
+  }
+
+  // // Draw a point directly between the centers of the two circles.
+  // canvas.Draw(emp::Circle{body1.GetCenter().Midpoint(body2.GetCenter()), 3},
+  //             emp::Palette::NONE, emp::Palette::CYAN);
+}
