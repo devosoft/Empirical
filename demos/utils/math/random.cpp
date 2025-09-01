20a5c51cfd0205743251df17f92b86e51b43e528
Charles Ofria <charles.ofria@gmail.com>
2025-09-01 15:39:19 -0400

    Stepped ranges are now working in random util.

--- demos/utils/math/random.cpp
+++ demos/utils/math/random.cpp
@@ -11,7 +11,7 @@ private:
   emp::Lexer lexer;
 
   enum class Type {
-    NONE=0, RANGE, DICE
+    NONE=0, RANGE, RANGE_STEP, DICE
   };
   emp::vector<double> vals;
   Type type = Type::NONE;
@@ -28,18 +28,23 @@ public:
     for (emp::Token token : ts) {
       if (token == NUM_id) { vals.push_back(token.lexeme.AsDouble()); }
       else if (token == ':') {
-        if (type != Type::NONE && type != Type::RANGE) {
+        if (type == Type::NONE) {
+          type = Type::RANGE;
+          if (vals.size() == 0) vals.push_back(0.0); // Start with implicit 0.
+        }
+        else if (type == Type::RANGE) type = Type::RANGE_STEP;
+        else {
           PrintLn("Cannot mix types for a random generator");
           exit(1);
         }
-        type = Type::RANGE;
+        
       }
       else if (token == 'd') {
         if (type != Type::NONE) {
           PrintLn("Cannot mix types or have multiple d's for a dice generator");
           exit(1);
         }
-        // If we start with a 'd', it means 1 die.
+        // If we start with a 'd', it implicitly means 1 die.
         if (vals.size() == 0) vals.push_back(1.0);
         type = Type::DICE;
       }
@@ -67,16 +72,24 @@ public:
       break;
     }
     case Type::RANGE: {
-      emp_assert(vals.size() >= 2);
+      emp_assert(vals.size() == 2);
       if (vals[0] >= vals[1]) {
         PrintLn("Invalid Range; min must be less than max.");
         exit(1);
       }
-      // if (vals.size() > 2) {
-      //   double step_size = vals[1];
-      //   int steps = static_cast<int>(range / step_size);
-      // }
       return random.GetDouble(vals[0], vals[1]);
+    }
+    case Type::RANGE_STEP: {
+      emp_assert(vals.size() == 3);
+      if (vals[0] >= vals[2]) {
+        PrintLn("Invalid Range; min must be less than max.");
+        exit(1);
+      }
+      const double range = vals[2] - vals[0];
+      const double step = vals[1];
+      uint32_t options = static_cast<uint32_t>(range/step + 1);
+      uint32_t step_id = random.GetUInt32(options);
+      return vals[0] + step*step_id;
     }
     }
 
@@ -88,12 +101,15 @@ int main(int argc, char * argv[]) {
   if (argc == 1){
     PrintLn("Format: ", argv[0], " [range]");
     PrintLn("Where range can be:");
-    PrintLn("  X     -> generate a uniform random float from 0 to X");
+    PrintLn("  X     -> always output value X");
+    PrintLn("  :X    -> generate a uniform random float from 0 to X");
     PrintLn("  X:Y   -> generate a uniform random float from X to Y");
     PrintLn("  X:Y:Z -> generate a uniform random float from X to Z in increments of Y");
     PrintLn("  dX    -> generate a uniform random integer from 1 to X (roll a die)");
     PrintLn("  XdY   -> roll X Y-sided dice and sum the result.");
-    exit(1);
+    PrintLn("\nYou can also use [range]+[range] to add multiple ranges together.");
+    PrintLn("Example: 3d6+5 will roll three dice and add five to the sum.");
+   exit(1);
   }
 
   RandUtil ru;
