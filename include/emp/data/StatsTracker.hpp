7ca0da84eabd2f188f9df5e8b771073db9687860
Charles Ofria <charles.ofria@gmail.com>
2026-04-26 18:18:16 -0400

    Added StatsTracker to simply track info about a value over time.

---
+++ include/emp/data/StatsTracker.hpp
@@ -0,0 +1,76 @@
+/**
+ * This file is part of Empirical, https://github.com/devosoft/Empirical
+ * Copyright (C) 2026 Michigan State University
+ * MIT Software license; see doc/LICENSE.md
+ *
+ * @file include/emp/data/StatsTracker.hpp
+ * @brief Track summary statistics for a series of numeric values.
+ * @note Status: BETA
+ */
+
+#pragma once
+
+#ifndef INCLUDE_EMP_DATA_STATS_TRACKER_HPP_GUARD
+#define INCLUDE_EMP_DATA_STATS_TRACKER_HPP_GUARD
+
+#include <concepts>
+#include <cstddef>
+#include <limits>
+#include <ostream>
+#include <string_view>
+
+namespace emp {
+
+  template <typename T>
+  concept Numeric = std::integral<T> || std::floating_point<T>;
+
+  /// Track count, total, minimum, maximum, and most recent value for a stream of numbers.
+  template <Numeric VALUE_T>
+  class StatsTracker {
+  private:
+    size_t count  = 0;
+    VALUE_T total = 0;
+    VALUE_T min   = std::numeric_limits<VALUE_T>::max();
+    VALUE_T max   = std::numeric_limits<VALUE_T>::lowest();
+    VALUE_T last  = 0;
+
+  public:
+    void Add(VALUE_T value) {
+      ++count;
+      last = value;
+      total += value;
+      if (value < min) { min = value; }
+      if (value > max) { max = value; }
+    }
+
+    void Reset() { *this = StatsTracker{}; }
+
+    size_t GetCount() const { return count; }
+
+    VALUE_T GetTotal() const { return total; }
+
+    VALUE_T GetMin() const { return count ? min : VALUE_T{}; }
+
+    VALUE_T GetMax() const { return count ? max : VALUE_T{}; }
+
+    VALUE_T GetLast() const { return last; }
+
+    double GetAverage() const { return count ? (double) total / (double) count : 0.0; }
+
+    bool HasData() const { return count > 0; }
+
+    void Print(std::ostream & os, std::string_view label = "Stats") const {
+      if (!HasData()) { return; }
+
+      os << label << ": "
+         << count << " sample" << (count == 1 ? "" : "s")
+         << "; min " << GetMin()
+         << "; max " << GetMax()
+         << "; avg " << GetAverage()
+         << "; total " << GetTotal();
+    }
+  };
+
+}  // namespace emp
+
+#endif  // #ifndef INCLUDE_EMP_DATA_STATS_TRACKER_HPP_GUARD
